c31c5c404168f4998051ff0a374d85ad19f21158
Otto Winter <otto@otto-winter.com>
2018-04-10 20:43:19 +0200

    Bump version to 1.2.2

--- esphomeyaml/const.py
+++ esphomeyaml/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 1
 MINOR_VERSION = 2
-PATCH_VERSION = '1'
+PATCH_VERSION = '2'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 
