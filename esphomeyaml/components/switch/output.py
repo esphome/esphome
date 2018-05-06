a88aad2179d9def43a9eb2f9c150d853b1143c91
Otto Winter <otto@otto-winter.com>
2018-05-06 17:40:27 +0200

    Fix generic output

--- esphomeyaml/components/switch/output.py
+++ esphomeyaml/components/switch/output.py
@@ -2,17 +2,17 @@ import voluptuous as vol
 
 import esphomeyaml.config_validation as cv
 from esphomeyaml.components import switch
-from esphomeyaml.const import CONF_ID, CONF_NAME
+from esphomeyaml.const import CONF_ID, CONF_NAME, CONF_OUTPUT
 from esphomeyaml.helpers import App, get_variable, variable
 
 PLATFORM_SCHEMA = switch.PLATFORM_SCHEMA.extend({
     cv.GenerateID('output_switch'): cv.register_variable_id,
-    vol.Required(CONF_ID): cv.variable_id,
+    vol.Required(CONF_OUTPUT): cv.variable_id,
 }).extend(switch.MQTT_SWITCH_SCHEMA.schema)
 
 
 def to_code(config):
-    output = get_variable(config[CONF_ID])
+    output = get_variable(config[CONF_OUTPUT])
     rhs = App.make_simple_switch(config[CONF_NAME], output)
     gpio = variable('Application::MakeSimpleSwitch', config[CONF_ID], rhs)
     switch.setup_switch(gpio.Pswitch_, config)
