ee502a7aaa04a24635922f752abb39fc219d696c
Otto Winter <otto@otto-winter.com>
2018-05-14 22:10:17 +0200

    Bump version to 1.5.0

--- esphomeyaml/const.py
+++ esphomeyaml/const.py
@@ -1,7 +1,7 @@
 """Constants used by esphomeyaml."""
 
 MAJOR_VERSION = 1
-MINOR_VERSION = 4
+MINOR_VERSION = 5
 PATCH_VERSION = '0'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
