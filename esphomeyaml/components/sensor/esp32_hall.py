4f3f460105a569d9c27419a9ed9e1237094bcccb
Otto Winter <otto@otto-winter.com>
2018-06-02 23:25:13 +0200

    ESP32 Hall Sensor

---
+++ esphomeyaml/components/sensor/esp32_hall.py
@@ -0,0 +1,25 @@
+import voluptuous as vol
+
+import esphomeyaml.config_validation as cv
+from esphomeyaml.components import sensor
+from esphomeyaml.const import CONF_MAKE_ID, CONF_NAME, CONF_UPDATE_INTERVAL, ESP_PLATFORM_ESP32
+from esphomeyaml.helpers import App, Application, variable
+
+ESP_PLATFORMS = [ESP_PLATFORM_ESP32]
+
+MakeESP32HallSensor = Application.MakeESP32HallSensor
+
+PLATFORM_SCHEMA = sensor.PLATFORM_SCHEMA.extend({
+    cv.GenerateID(CONF_MAKE_ID): cv.declare_variable_id(MakeESP32HallSensor),
+    vol.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
+}).extend(sensor.SENSOR_SCHEMA.schema)
+
+
+def to_code(config):
+    rhs = App.make_esp32_hall_sensor(config[CONF_NAME], config.get(CONF_UPDATE_INTERVAL))
+    make = variable(config[CONF_MAKE_ID], rhs)
+    for _ in sensor.setup_sensor(make.Phall, make.Pmqtt, config):
+        yield
+
+
+BUILD_FLAGS = '-DUSE_ESP32_HALL_SENSOR'
