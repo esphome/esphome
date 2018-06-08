0be29d27d5064e1f99ce118312db79979137d143
Otto Winter <otto@otto-winter.com>
2018-06-08 20:32:53 +0200

    Fixes esphomelib#72

--- esphomeyaml/components/output/__init__.py
+++ esphomeyaml/components/output/__init__.py
@@ -21,7 +21,7 @@ FLOAT_OUTPUT_SCHEMA = BINARY_OUTPUT_SCHEMA.extend({
 output_ns = esphomelib_ns.namespace('output')
 
 
-def setup_output_platform(obj, config, skip_power_supply=False):
+def setup_output_platform_(obj, config, skip_power_supply=False):
     if CONF_INVERTED in config:
         add(obj.set_inverted(config[CONF_INVERTED]))
     if not skip_power_supply and CONF_POWER_SUPPLY in config:
@@ -31,6 +31,11 @@ def setup_output_platform(obj, config, skip_power_supply=False):
         add(obj.set_power_supply(power_supply))
     if CONF_MAX_POWER in config:
         add(obj.set_max_power(config[CONF_MAX_POWER]))
+
+
+def setup_output_platform(obj, config, skip_power_supply=False):
+    for _ in setup_output_platform_(obj, config, skip_power_supply):
+        yield
 
 
 BUILD_FLAGS = '-DUSE_OUTPUT'
