433b605befef4088e7e2254e67d2707724e07849
Otto Winter <otto@otto-winter.com>
2018-09-26 19:00:41 +0200

    Bump version to 1.8.1

--- esphomeyaml/const.py
+++ esphomeyaml/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 1
 MINOR_VERSION = 8
-PATCH_VERSION = '0'
+PATCH_VERSION = '1'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 ESPHOMELIB_VERSION = '1.8.0'
