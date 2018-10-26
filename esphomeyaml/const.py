2fab7e73b91b8d4c965a4fe6aad5e19116d56d84
Otto Winter <otto@otto-winter.com>
2018-10-26 22:59:03 +0200

    Add send_first_at option to sliding window sensor filter (#207)
    * Add send_first_at option to sliding window sensor filter
    
    * Lint

--- esphomeyaml/const.py
+++ esphomeyaml/const.py
@@ -358,6 +358,7 @@ CONF_POSITION = 'position'
 CONF_STEP_PIN = 'step_pin'
 CONF_DIR_PIN = 'dir_pin'
 CONF_SLEEP_PIN = 'sleep_pin'
+CONF_SEND_FIRST_AT = 'send_first_at'
 
 ALLOWED_NAME_CHARS = u'abcdefghijklmnopqrstuvwxyz0123456789_'
 ARDUINO_VERSION_ESP32_DEV = 'https://github.com/platformio/platform-espressif32.git#feature/stage'
