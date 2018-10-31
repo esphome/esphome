4dce7fa103aa98fa675b49840cc9e65ca0189c87
Otto Winter <otto@otto-winter.com>
2018-10-31 16:35:45 +0100

    Bump version to 1.9.0b2

--- esphomeyaml/const.py
+++ esphomeyaml/const.py
@@ -1,11 +1,11 @@
 """Constants used by esphomeyaml."""
 
 MAJOR_VERSION = 1
-MINOR_VERSION = 10
-PATCH_VERSION = '0-dev'
+MINOR_VERSION = 9
+PATCH_VERSION = '0b2'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
-ESPHOMELIB_VERSION = 'dev'
+ESPHOMELIB_VERSION = '1.9.0b2'
 
 ESP_PLATFORM_ESP32 = 'ESP32'
 ESP_PLATFORM_ESP8266 = 'ESP8266'
