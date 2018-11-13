229f8d1414fd7793e58ca2bf3c783a1d73b849fc
Otto Winter <otto@otto-winter.com>
2018-11-13 13:56:52 +0100

    Deep Sleep Wake Up From Multiple Pins

--- esphomeyaml/const.py
+++ esphomeyaml/const.py
@@ -367,6 +367,7 @@ CONF_TIMING = 'timing'
 CONF_INVALID_COOLDOWN = 'invalid_cooldown'
 CONF_INITIAL_VALUE = 'initial_value'
 CONF_RESTORE_VALUE = 'restore_value'
+CONF_PINS = 'pins'
 
 
 ALLOWED_NAME_CHARS = u'abcdefghijklmnopqrstuvwxyz0123456789_'
