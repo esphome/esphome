c717b53e4b2b9cda51a5914bb18876f02f1ee3f9
escoand <escoand@users.noreply.github.com>
2018-11-20 10:11:29 +0100

    add default_transition_length to binary light

--- esphomeyaml/components/light/binary.py
+++ esphomeyaml/components/light/binary.py
@@ -2,12 +2,14 @@ import voluptuous as vol
 
 from esphomeyaml.components import light, output
 import esphomeyaml.config_validation as cv
-from esphomeyaml.const import CONF_EFFECTS, CONF_MAKE_ID, CONF_NAME, CONF_OUTPUT
+from esphomeyaml.const import CONF_DEFAULT_TRANSITION_LENGTH, CONF_EFFECTS, CONF_MAKE_ID, CONF_NAME, \
+    CONF_OUTPUT
 from esphomeyaml.helpers import App, get_variable, setup_component, variable
 
 PLATFORM_SCHEMA = cv.nameable(light.LIGHT_PLATFORM_SCHEMA.extend({
     cv.GenerateID(CONF_MAKE_ID): cv.declare_variable_id(light.MakeLight),
     vol.Required(CONF_OUTPUT): cv.use_variable_id(output.BinaryOutput),
+    vol.Optional(CONF_DEFAULT_TRANSITION_LENGTH): cv.positive_time_period_milliseconds,
     vol.Optional(CONF_EFFECTS): light.validate_effects(light.BINARY_EFFECTS),
 }).extend(cv.COMPONENT_SCHEMA.schema))
 
