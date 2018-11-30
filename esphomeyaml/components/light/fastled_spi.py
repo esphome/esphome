3b1cba7ad18839dc69d7b965fad1544fb0d801cb
Otto Winter <otto@otto-winter.com>
2018-11-30 12:41:34 +0100

    Only include FastLED if user says so

--- esphomeyaml/components/light/fastled_spi.py
+++ esphomeyaml/components/light/fastled_spi.py
@@ -84,6 +84,8 @@ def to_code(config):
 
 BUILD_FLAGS = '-DUSE_FAST_LED_LIGHT'
 
+LIB_DEPS = 'FastLED@3.2.0'
+
 
 def to_hass_config(data, config):
     return light.core_to_hass_config(data, config, brightness=True, rgb=True, color_temp=False,
