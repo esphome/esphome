dd801636afdf07d92f8cd9187ebb81073cb23852
Otto Winter <otto@otto-winter.com>
2019-01-02 16:49:26 +0100

    Fix Interval Trigger

--- esphomeyaml/components/interval.py
+++ esphomeyaml/components/interval.py
@@ -2,7 +2,7 @@ import voluptuous as vol
 
 from esphomeyaml import automation
 import esphomeyaml.config_validation as cv
-from esphomeyaml.const import CONF_ID
+from esphomeyaml.const import CONF_ID, CONF_INTERVAL
 from esphomeyaml.cpp_generator import Pvariable
 from esphomeyaml.cpp_helpers import setup_component
 from esphomeyaml.cpp_types import App, NoArg, PollingComponent, Trigger, esphomelib_ns
@@ -11,12 +11,13 @@ IntervalTrigger = esphomelib_ns.class_('IntervalTrigger', Trigger.template(NoArg
 
 CONFIG_SCHEMA = automation.validate_automation(vol.Schema({
     vol.Required(CONF_ID): cv.declare_variable_id(IntervalTrigger),
+    vol.Required(CONF_INTERVAL): cv.positive_time_period_milliseconds,
 }).extend(cv.COMPONENT_SCHEMA.schema))
 
 
 def to_code(config):
     for conf in config:
-        rhs = App.register_component(IntervalTrigger.new())
+        rhs = App.register_component(IntervalTrigger.new(config[CONF_INTERVAL]))
         trigger = Pvariable(conf[CONF_ID], rhs)
         setup_component(trigger, conf)
 
