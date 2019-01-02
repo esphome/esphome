a1109113711e3df879640a68a4f6fe7bb3246e3d
Otto Winter <otto@otto-winter.com>
2019-01-02 16:34:38 +0100

    Commit forgotten local change
    Fixes #295

--- esphomeyaml-edge/rootfs/etc/cont-init.d/30-esphomeyaml.sh
+++ esphomeyaml-edge/rootfs/etc/cont-init.d/30-esphomeyaml.sh
@@ -11,4 +11,4 @@ declare esphomeyaml_version
 if hass.config.has_value 'esphomeyaml_version'; then
     esphomeyaml_version=$(hass.config.get 'esphomeyaml_version')
     pip2 install --no-cache-dir --no-binary :all: "https://github.com/OttoWinter/esphomeyaml/archive/${esphomeyaml_version}.zip"
-end
+fi
