7b26ecc0dc125cb5f698b6c106f3120eed2b7887
Otto Winter <otto@otto-winter.com>
2019-01-29 16:45:50 +0100

    Remove Heartbeat Filter (#393)
    * Remove Heartbeat Filter
    
    * Fix tests

--- esphomeyaml/components/binary_sensor/__init__.py
+++ esphomeyaml/components/binary_sensor/__init__.py
@@ -60,8 +60,9 @@ FILTERS_SCHEMA = cv.ensure_list({
     vol.Optional(CONF_INVERT): None,
     vol.Optional(CONF_DELAYED_ON): cv.positive_time_period_milliseconds,
     vol.Optional(CONF_DELAYED_OFF): cv.positive_time_period_milliseconds,
-    vol.Optional(CONF_HEARTBEAT): cv.positive_time_period_milliseconds,
     vol.Optional(CONF_LAMBDA): cv.lambda_,
+
+    vol.Optional(CONF_HEARTBEAT): cv.invalid("The heartbeat filter has been removed in 1.11.0"),
 }, cv.has_exactly_one_key(*FILTER_KEYS))
 
 MULTI_CLICK_TIMING_SCHEMA = vol.Schema({
@@ -205,8 +206,6 @@ def setup_filter(config):
         yield App.register_component(DelayedOffFilter.new(config[CONF_DELAYED_OFF]))
     elif CONF_DELAYED_ON in config:
         yield App.register_component(DelayedOnFilter.new(config[CONF_DELAYED_ON]))
-    elif CONF_HEARTBEAT in config:
-        yield App.register_component(HeartbeatFilter.new(config[CONF_HEARTBEAT]))
     elif CONF_LAMBDA in config:
         for lambda_ in process_lambda(config[CONF_LAMBDA], [(bool_, 'x')],
                                       return_type=optional.template(bool_)):
