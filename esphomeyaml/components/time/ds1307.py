696a9862e0106a16bacf308e7be84ef69415b23f
Florian Moesch <florian@moesch.org>
2019-02-01 23:15:54 +0100

    Fixed review remarks.

--- esphomeyaml/components/time/ds1307.py
+++ esphomeyaml/components/time/ds1307.py
@@ -1,11 +1,13 @@
-from esphomeyaml.components import time as time_
+from esphomeyaml.components import i2c, time as time_
 import esphomeyaml.config_validation as cv
 from esphomeyaml.const import CONF_ID
 from esphomeyaml.cpp_generator import Pvariable
 from esphomeyaml.cpp_helpers import setup_component
 from esphomeyaml.cpp_types import App
 
-DS1307Time = time_.time_ns.class_('DS1307Time', time_.RealTimeClockComponent)
+DEPENDENCIES = ['i2c']
+
+DS1307Time = time_.time_ns.class_('DS1307Time', time_.RealTimeClockComponent, i2c.I2CDevice)
 
 PLATFORM_SCHEMA = time_.TIME_PLATFORM_SCHEMA.extend({
     cv.GenerateID(): cv.declare_variable_id(DS1307Time)
