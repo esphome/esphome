37153753e2f97ee3f94f7112e8f4dc5604cc5125
Otto Winter <otto@otto-winter.com>
2019-02-03 20:46:32 +0100

    Add Switch Interlocking

--- esphomeyaml/const.py
+++ esphomeyaml/const.py
@@ -399,6 +399,7 @@ CONF_DIRECTION = 'direction'
 CONF_VARIANT = 'variant'
 CONF_METHOD = 'method'
 CONF_FAST_CONNECT = 'fast_connect'
+CONF_INTERLOCK = 'interlock'
 
 ALLOWED_NAME_CHARS = u'abcdefghijklmnopqrstuvwxyz0123456789_'
 ARDUINO_VERSION_ESP32_DEV = 'https://github.com/platformio/platform-espressif32.git#feature/stage'
