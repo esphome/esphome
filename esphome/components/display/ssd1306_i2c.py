f311a1bb302b0acfe32eae58ff31dfdefbfaff5a
Otto Winter <otto@otto-winter.com>
2019-02-17 00:35:23 +0100

    Add display page abstraction (#435)

--- esphome/components/display/ssd1306_i2c.py
+++ esphome/components/display/ssd1306_i2c.py
@@ -5,7 +5,7 @@ from esphome.components import display
 from esphome.components.display import ssd1306_spi
 import esphome.config_validation as cv
 from esphome.const import CONF_ADDRESS, CONF_EXTERNAL_VCC, CONF_ID, CONF_LAMBDA, CONF_MODEL, \
-    CONF_RESET_PIN
+    CONF_PAGES, CONF_RESET_PIN
 from esphome.cpp_generator import Pvariable, add, process_lambda
 from esphome.cpp_helpers import gpio_output_pin_expression, setup_component
 from esphome.cpp_types import App, void
@@ -14,13 +14,13 @@ DEPENDENCIES = ['i2c']
 
 I2CSSD1306 = display.display_ns.class_('I2CSSD1306', ssd1306_spi.SSD1306)
 
-PLATFORM_SCHEMA = display.FULL_DISPLAY_PLATFORM_SCHEMA.extend({
+PLATFORM_SCHEMA = vol.All(display.FULL_DISPLAY_PLATFORM_SCHEMA.extend({
     cv.GenerateID(): cv.declare_variable_id(I2CSSD1306),
     vol.Required(CONF_MODEL): ssd1306_spi.SSD1306_MODEL,
     vol.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
     vol.Optional(CONF_EXTERNAL_VCC): cv.boolean,
     vol.Optional(CONF_ADDRESS): cv.i2c_address,
-}).extend(cv.COMPONENT_SCHEMA.schema)
+}).extend(cv.COMPONENT_SCHEMA.schema), cv.has_at_most_one_key(CONF_PAGES, CONF_LAMBDA))
 
 
 def to_code(config):
