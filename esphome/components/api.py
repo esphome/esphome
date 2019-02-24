4023725e4603cc9ea114a5ea7980bd54edf25da0
Otto Winter <otto@otto-winter.com>
2019-02-24 18:14:32 +0100

    Add native API user-defined services

--- esphome/components/api.py
+++ esphome/components/api.py
@@ -1,13 +1,15 @@
 import voluptuous as vol
 
+from esphome import automation
 from esphome.automation import ACTION_REGISTRY
 import esphome.config_validation as cv
 from esphome.const import CONF_DATA, CONF_DATA_TEMPLATE, CONF_ID, CONF_PASSWORD, CONF_PORT, \
-    CONF_REBOOT_TIMEOUT, CONF_SERVICE, CONF_VARIABLES
+    CONF_REBOOT_TIMEOUT, CONF_SERVICE, CONF_VARIABLES, CONF_SERVICES, CONF_TRIGGER_ID
 from esphome.core import CORE
 from esphome.cpp_generator import Pvariable, add, get_variable, process_lambda
 from esphome.cpp_helpers import setup_component
-from esphome.cpp_types import Action, App, Component, StoringController, esphome_ns
+from esphome.cpp_types import Action, App, Component, StoringController, esphome_ns, Trigger, bool_, \
+    int32, float_, std_string
 
 api_ns = esphome_ns.namespace('api')
 APIServer = api_ns.class_('APIServer', Component, StoringController)
@@ -15,11 +17,35 @@ HomeAssistantServiceCallAction = api_ns.class_('HomeAssistantServiceCallAction',
 KeyValuePair = api_ns.class_('KeyValuePair')
 TemplatableKeyValuePair = api_ns.class_('TemplatableKeyValuePair')
 
+UserService = api_ns.class_('UserService', Trigger)
+ServiceTypeArgument = api_ns.class_('ServiceTypeArgument')
+ServiceArgType = api_ns.enum('ServiceArgType')
+SERVICE_ARG_TYPES = {
+    'bool': ServiceArgType.SERVICE_ARG_TYPE_BOOL,
+    'int': ServiceArgType.SERVICE_ARG_TYPE_INT,
+    'float': ServiceArgType.SERVICE_ARG_TYPE_FLOAT,
+    'string': ServiceArgType.SERVICE_ARG_TYPE_STRING,
+}
+SERVICE_ARG_NATIVE_TYPES = {
+    'bool': bool_,
+    'int': int32,
+    'float': float_,
+    'string': std_string,
+}
+
+
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_variable_id(APIServer),
     vol.Optional(CONF_PORT, default=6053): cv.port,
     vol.Optional(CONF_PASSWORD, default=''): cv.string_strict,
     vol.Optional(CONF_REBOOT_TIMEOUT): cv.positive_time_period_milliseconds,
+    vol.Optional(CONF_SERVICES): automation.validate_automation({
+        cv.GenerateID(CONF_TRIGGER_ID): cv.declare_variable_id(UserService),
+        vol.Required(CONF_SERVICE): cv.valid_name,
+        vol.Optional(CONF_VARIABLES, default={}): cv.Schema({
+            cv.validate_id_name: cv.one_of(*SERVICE_ARG_TYPES, lower=True),
+        }),
+    }),
 }).extend(cv.COMPONENT_SCHEMA.schema)
 
 
@@ -33,6 +59,21 @@ def to_code(config):
         add(api.set_password(config[CONF_PASSWORD]))
     if CONF_REBOOT_TIMEOUT in config:
         add(api.set_reboot_timeout(config[CONF_REBOOT_TIMEOUT]))
+
+    for conf in config.get(CONF_SERVICES, []):
+        template_args = []
+        func_args = []
+        service_type_args = []
+        for name, var_ in conf[CONF_VARIABLES].items():
+            native = SERVICE_ARG_NATIVE_TYPES[var_]
+            template_args.append(native)
+            func_args.append((native, name))
+            service_type_args.append(ServiceTypeArgument(name, SERVICE_ARG_TYPES[var_]))
+        func = api.make_user_service_trigger.template(*template_args)
+        rhs = func(conf[CONF_SERVICE], service_type_args)
+        type_ = UserService.template(*template_args)
+        trigger = Pvariable(conf[CONF_TRIGGER_ID], rhs, type=type_)
+        automation.build_automations(trigger, func_args, conf)
 
     setup_component(api, config)
 
