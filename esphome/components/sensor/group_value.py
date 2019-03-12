cfda67347267afc6d4142d2079187ac6fecb55f6
mvturnho <qris.online@gmail.com>
2019-03-12 20:23:40 +0100

    value is required

--- esphome/components/sensor/group_value.py
+++ esphome/components/sensor/group_value.py
@@ -2,7 +2,7 @@ import voluptuous as vol
 
 from esphome.components import sensor, binary_sensor
 import esphome.config_validation as cv
-from esphome.const import  CONF_ID,CONF_NAME, CONF_CHANNELS, CONF_CHANNEL, CONF_VALUE
+from esphome.const import  CONF_ID, CONF_NAME, CONF_CHANNELS, CONF_CHANNEL, CONF_VALUE
 from esphome.cpp_generator import Pvariable, get_variable, variable, add
 from esphome.cpp_types import App, Component
 from array import array
@@ -17,7 +17,7 @@ GroupSensorComponent = sensor.sensor_ns.class_('GroupSensorComponent', sensor.Se
 
 entry = {
     vol.Required(CONF_CHANNEL): cv.use_variable_id(binary_sensor.BinarySensor),
-    vol.Optional(CONF_VALUE): vol.All(cv.positive_int, vol.Range(min=0, max=255)),
+    vol.Required(CONF_VALUE): vol.All(cv.positive_int, vol.Range(min=0, max=255)),
 }
 
 PLATFORM_SCHEMA = cv.nameable(sensor.SENSOR_PLATFORM_SCHEMA.extend({
@@ -30,19 +30,17 @@ def to_code(config):
     rhs = App.make_group_sensor(config[CONF_NAME])
     var = Pvariable(config[CONF_ID], rhs)
     setup_sensor(var, config)
-#    grsens_struct = variable(config[CONF_ID], rhs)
-
+    
     for ch in config[CONF_CHANNELS]:
         for input_var in  get_variable(ch[CONF_CHANNEL]):
             yield
-        add(var.add_sensor(input_var,ch[CONF_VALUE]))
-
-#          add(grsens_struct.add_sensor_channel(ch[CONF_CHANNEL], ch[CONF_VALUE]))
-    
+        add(var.add_sensor(input_var, ch[CONF_VALUE]))
+          
     add(App.register_sensor(var))
 
 
 def to_hass_config(data, config):
     return sensor.core_to_hass_config(data, config)
+
 
 BUILD_FLAGS = '-DUSE_GROUP_SENSOR'
