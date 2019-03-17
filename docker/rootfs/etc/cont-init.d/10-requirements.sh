3baaf6b7c41f497078256f15d1eccc914718542d
Otto Winter <otto@otto-winter.com>
2019-03-17 20:41:07 +0100

    Update Hassio base image to 1.4.1 (#484)
    * Update Hassio base image to 1.4.1
    
    * Fix rotary encoder typo
    
    Fixes https://github.com/esphome/issues/issues/136

--- docker/rootfs/etc/cont-init.d/10-requirements.sh
+++ docker/rootfs/etc/cont-init.d/10-requirements.sh
@@ -1,35 +1,41 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: ESPHome
 # This files check if all user configuration requirements are met
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
 
 # Check SSL requirements, if enabled
-if hass.config.true 'ssl'; then
-    if ! hass.config.has_value 'certfile'; then
-        hass.die 'SSL is enabled, but no certfile was specified.'
+if bashio::config.true 'ssl'; then
+    if ! bashio::config.has_value 'certfile'; then
+        bashio::fatal 'SSL is enabled, but no certfile was specified.'
+        bashio::exit.nok
     fi
 
-    if ! hass.config.has_value 'keyfile'; then
-        hass.die 'SSL is enabled, but no keyfile was specified'
+    if ! bashio::config.has_value 'keyfile'; then
+        bashio::fatal 'SSL is enabled, but no keyfile was specified'
+        bashio::exit.nok
     fi
 
-    if ! hass.file_exists "/ssl/$(hass.config.get 'certfile')"; then
-        if ! hass.file_exists "/ssl/$(hass.config.get 'keyfile')"; then
+
+    certfile="/ssl/$(bashio::config 'certfile')"
+    keyfile="/ssl/$(bashio::config 'keyfile')"
+
+    if ! bashio::fs.file_exists "${certfile}"; then
+        if ! bashio::fs.file_exists "${keyfile}"; then
             # Both files are missing, let's print a friendlier error message
-            text="You enabled encrypted connections using the \"ssl\": true option.
-            However, the SSL files \"$(hass.config.get 'certfile')\" and \"$(hass.config.get 'keyfile')\"
-            were not found. If you're using Hass.io on your local network and don't want
-            to encrypt connections to the ESPHome dashboard, you can manually disable
-            SSL by setting \"ssl\" to false."
-            hass.die "${text}"
+            bashio::log.fatal 'You enabled encrypted connections using the "ssl": true option.'
+            bashio::log.fatal "However, the SSL files '${certfile}' and '${keyfile}'"
+            bashio::log.fatal "were not found. If you're using Hass.io on your local network and don't want"
+            bashio::log.fatal 'to encrypt connections to the ESPHome dashboard, you can manually disable'
+            bashio::log.fatal 'SSL by setting "ssl" to false."'
+            bashio::exit.nok
         fi
-        hass.die 'The configured certfile is not found'
+        bashio::log.fatal "The configured certfile '${certfile}' was not found."
+        bashio::exit.nok
     fi
 
-    if ! hass.file_exists "/ssl/$(hass.config.get 'keyfile')"; then
-        hass.die 'The configured keyfile is not found'
+    if ! bashio::fs.file_exists "/ssl/$(bashio::config 'keyfile')"; then
+        bashio::log.fatal "The configured keyfile '${keyfile}' was not found."
+        bashio::exit.nok
     fi
 fi
