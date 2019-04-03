35fa02b941ed68268fe85d521e2505a0bc84771a
mvturnho <qris.online@gmail.com>
2019-04-03 16:49:43 +0200

    lint error

--- esphome/components/binary_sensor/ttp229_bsf.py
+++ esphome/components/binary_sensor/ttp229_bsf.py
@@ -7,11 +7,11 @@ from esphome.cpp_generator import get_variable
 
 DEPENDENCIES = ['ttp229_bsf']
 
-TTP229Channel = binary_sensor.binary_sensor_ns.class_(
-    'TTP229Channel', binary_sensor.BinarySensor)
+TTP229BSFChannel = binary_sensor.binary_sensor_ns.class_(
+    'TTP229BSFChannel', binary_sensor.BinarySensor)
 
 PLATFORM_SCHEMA = cv.nameable(binary_sensor.BINARY_SENSOR_PLATFORM_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_variable_id(TTP229Channel),
+    cv.GenerateID(): cv.declare_variable_id(TTP229BSFChannel),
     cv.GenerateID(CONF_TTP229_BSF_ID): cv.use_variable_id(TTP229BSFComponent),
     vol.Required(CONF_CHANNEL): vol.All(vol.Coerce(int), vol.Range(min=0, max=15))
 }))
@@ -20,5 +20,6 @@ PLATFORM_SCHEMA = cv.nameable(binary_sensor.BINARY_SENSOR_PLATFORM_SCHEMA.extend
 def to_code(config):
     for hub in get_variable(config[CONF_TTP229_BSF_ID]):
         yield
-    rhs = TTP229Channel.new(config[CONF_NAME], config[CONF_CHANNEL])
+    rhs = TTP229BSFChannel.new(config[CONF_NAME], config[CONF_CHANNEL])
     binary_sensor.register_binary_sensor(hub.add_channel(rhs), config)
+    
