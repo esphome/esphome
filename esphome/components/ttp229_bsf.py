4329f7b63ebd44ee5b578a7b253e2b17dab70ee5
Michiel van Turnhout <qris.online@gmail.com>
2019-04-07 22:10:51 +0200

    fixed issues from comments

--- esphome/components/ttp229_bsf.py
+++ esphome/components/ttp229_bsf.py
@@ -1,7 +1,7 @@
 import voluptuous as vol
 from esphome.components import binary_sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_SCL, CONF_SDO
+from esphome.const import CONF_ID, CONF_SCL_PIN, CONF_SDO_PIN
 from esphome.cpp_generator import Pvariable
 from esphome.cpp_helpers import setup_component
 from esphome.cpp_types import App, Component
@@ -15,15 +15,15 @@ TTP229BSFComponent = binary_sensor.binary_sensor_ns.class_('TTP229BSFComponent',
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_variable_id(TTP229BSFComponent),
-    vol.Required(CONF_SDO): gpio_input_pullup_pin_schema,
-    vol.Required(CONF_SCL): gpio_input_pullup_pin_schema,
+    vol.Required(CONF_SDO_PIN): gpio_input_pullup_pin_schema,
+    vol.Required(CONF_SCL_PIN): gpio_input_pullup_pin_schema,
 }).extend(cv.COMPONENT_SCHEMA.schema)
 
 
 def to_code(config):
-    for sdo_pin in gpio_output_pin_expression(config[CONF_SDO]):
+    for sdo_pin in gpio_output_pin_expression(config[CONF_SDO_PIN]):
         yield
-    for scl_pin in gpio_output_pin_expression(config[CONF_SCL]):
+    for scl_pin in gpio_output_pin_expression(config[CONF_SCL_PIN]):
         yield
     rhs = App.make_ttp229_bsf(sdo_pin, scl_pin)
     var = Pvariable(config[CONF_ID], rhs)
