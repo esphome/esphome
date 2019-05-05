62e7acd280df36b4b1a92b2fe546e2f1ab0fec4b
Igor Mammedov <imammedo@redhat.com>
2019-05-05 16:00:44 +0200

    mhz19: disable autocalibration for MH-Z19B by default
    MH-Z19B allows to enable/disable 'automatic baseline calibration',
    which is enabled by default (MH-Z19B v1.2 datasheet).
    It operates on too short 24hr interval. So if the place
    where sensor is located is not ventilated well "every" day, ABC will
    make sensor under-report CO2 value, since baseline (400ppm) is moved
    to lowest observed value in 24hr.
    
    With such short recalibrartion window, sensor often doesn't show
    correct CO2 levels when ABS is left enabled. Disable it on boot
    defaut and add 'automatic_baseline_calibration' option to allow
    override from config.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>

--- esphome/components/mhz19/sensor.py
+++ esphome/components/mhz19/sensor.py
@@ -2,10 +2,11 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
 from esphome.const import CONF_CO2, CONF_ID, CONF_TEMPERATURE, ICON_PERIODIC_TABLE_CO2, \
-    UNIT_PARTS_PER_MILLION, UNIT_CELSIUS, ICON_THERMOMETER
+    UNIT_PARTS_PER_MILLION, UNIT_CELSIUS, ICON_THERMOMETER, UNIT_EMPTY, ICON_EMPTY
 
 DEPENDENCIES = ['uart']
 
+CONF_ABC = "automatic_baseline_calibration"
 mhz19_ns = cg.esphome_ns.namespace('mhz19')
 MHZ19Component = mhz19_ns.class_('MHZ19Component', cg.PollingComponent, uart.UARTDevice)
 
@@ -13,6 +14,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MHZ19Component),
     cv.Required(CONF_CO2): sensor.sensor_schema(UNIT_PARTS_PER_MILLION, ICON_PERIODIC_TABLE_CO2, 0),
     cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(UNIT_CELSIUS, ICON_THERMOMETER, 0),
+    cv.Optional(CONF_ABC, default=False): cv.boolean,
 }).extend(cv.polling_component_schema('60s')).extend(uart.UART_DEVICE_SCHEMA)
 
 
@@ -28,3 +30,5 @@ def to_code(config):
     if CONF_TEMPERATURE in config:
         sens = yield sensor.new_sensor(config[CONF_TEMPERATURE])
         cg.add(var.set_temperature_sensor(sens))
+
+    cg.add(var.set_abc(config[CONF_ABC]))
