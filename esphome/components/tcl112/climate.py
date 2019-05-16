5a42d4187724673347cb9e05cbe71f9ca4db8cfb
Otto Winter <otto@otto-winter.com>
2019-05-16 19:11:41 +0200

    Update

--- esphome/components/tcl112/climate.py
+++ esphome/components/tcl112/climate.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate, remote_transmitter, sensor
-from esphome.const import CONF_ID, CONF_NAME, CONF_SENSOR
+from esphome.const import CONF_ID, CONF_SENSOR
 
 AUTO_LOAD = ['sensor']
 
@@ -22,7 +22,7 @@ CONFIG_SCHEMA = cv.All(climate.CLIMATE_SCHEMA.extend({
 
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_NAME])
+    var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield climate.register_climate(var, config)
 
