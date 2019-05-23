7d7060f83c1eeb23233fe426dc3dd1a9983002d9
Mikkonen Teemu <teemu.mikkonen@iki.fi>
2019-05-23 17:54:08 +0300

    Removed extra space

--- esphome/components/am2320/am2320.h
+++ esphome/components/am2320/am2320.h
@@ -11,7 +11,7 @@ class AM2320Component : public PollingComponent, public i2c::I2CDevice {
  public:
   void setup() override;
   void dump_config() override;
-  float get_setup_priority() const override; 
+  float get_setup_priority() const override;
   void update() override;
 
   void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
