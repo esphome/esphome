24ba2d839fb410633fc8d9a486076ab582d4d399
mvturnho <qris.online@gmail.com>
2019-05-26 11:32:04 +0200

    Revert "fixed code"
    This reverts commit 0c6eacb225522f7d738fa371d0db976a97f2f3e8.

--- esphome/components/mpr121/binary_sensor.py
+++ esphome/components/mpr121/binary_sensor.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import CONF_CHANNEL, CONF_ID
-from mpr121_const import CONF_TOUCH_THRESHOLD, CONF_RELEASE_THRESHOLD
 from . import mpr121_ns, MPR121Component, CONF_MPR121_ID
 
 DEPENDENCIES = ['mpr121']
@@ -12,24 +11,14 @@ CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(MPR121Channel),
     cv.GenerateID(CONF_MPR121_ID): cv.use_id(MPR121Component),
     cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=11),
-    cv.Optional(CONF_TOUCH_THRESHOLD): cv.All(cv.Coerce(int), cv.Range(min=0x05, max=0x30)),
-    cv.Optional(CONF_RELEASE_THRESHOLD): cv.All(cv.Coerce(int), cv.Range(min=0x05, max=0x30)),
 })
 
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield binary_sensor.register_binary_sensor(var, config)
-    hub = yield cg.get_variable(config[CONF_MPR121_ID])
+
+    cg.add(var.set_channel(config[CONF_CHANNEL]))
 
     hub = yield cg.get_variable(config[CONF_MPR121_ID])
-    cg.add(var.set_channel(config[CONF_CHANNEL]))
-    if CONF_TOUCH_THRESHOLD in config:
-        cg.add(var.set_touch_threshold(config[CONF_TOUCH_THRESHOLD]))
-    else:
-        cg.add(var.set_touch_threshold(hub.get_touch_threshold()))
-    if CONF_RELEASE_THRESHOLD in config:
-        cg.add(var.set_release_threshold(config[CONF_RELEASE_THRESHOLD]))
-    else:
-        cg.add(var.set_release_threshold(hub.get_release_threshold()))
     cg.add(hub.register_channel(var))
