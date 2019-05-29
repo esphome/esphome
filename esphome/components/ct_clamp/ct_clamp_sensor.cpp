8e1215d56c789058cc42ad6af358c54b4dc30a1e
Otto Winter <otto@otto-winter.com>
2019-05-29 08:34:45 +0200

    Update to use loop and not slow down main loop

--- esphome/components/ct_clamp/ct_clamp_sensor.cpp
+++ esphome/components/ct_clamp/ct_clamp_sensor.cpp
@@ -10,33 +10,57 @@ static const char *TAG = "ct_clamp";
 
 void CTClampSensor::dump_config() {
   LOG_SENSOR("", "CT Clamp Sensor", this);
-  ESP_LOGCONFIG(TAG, "  Sample Size: %u", this->sample_size_);
-
+  ESP_LOGCONFIG(TAG, "  Sample Duration: %.2fs", this->sample_duration_ / 1e3f);
   LOG_UPDATE_INTERVAL(this);
 }
 
 void CTClampSensor::update() {
-  float sum = 0;
-  for (unsigned int n = 0; n < this->sample_size_; n++) {
-    float value = this->source_->sample();
+  // Update only starts the sampling phase, in loop() the actual sampling is happening.
 
-    // Shift center offset based on values coming in.
-    offset_ = offset_ + (value - offset_) / 1024.0;
-    // Digital low pass filter to center on 0V.
-    float filtered = value - offset_;
+  // Request a high loop() execution interval during sampling phase.
+  this->high_freq_.start();
 
-    // Root-mean-square method current
-    // 1) square current values
-    float sq = filtered * filtered;
-    // 2) sum
-    sum += sq;
-  }
+  // Set timeout for ending sampling phase
+  this->set_timeout("read", this->sample_duration_, [this]() {
+    this->is_sampling_ = false;
+    this->high_freq_.stop();
 
-  float irms = std::sqrt(sum / this->sample_size_);
+    if (this->num_samples_ == 0) {
+      // Shouldn't happen, but let's not crash if it does.
+      this->publish_state(NAN);
+      return;
+    }
 
-  ESP_LOGD(TAG, "'%s' - Raw Value: %.2f", this->name_.c_str(), irms);
+    float raw = this->sample_sum_ / this->num_samples_;
+    float irms = std::sqrt(raw);
+    ESP_LOGD(TAG, "'%s' - Raw Value: %.2fA", this->name_.c_str(), irms);
+    this->publish_state(irms);
+  });
 
-  this->publish_state(irms);
+  // Set sampling values
+  this->is_sampling_ = true;
+  this->num_samples_ = 0;
+  this->sample_sum_ = 0.0f;
+}
+
+void CTClampSensor::loop() {
+  if (!this->is_sampling_)
+    return;
+
+  // Perform a single sample
+  float value = this->source_->sample();
+
+  // Adjust DC offset via low pass filter (exponential moving average)
+  const float alpha = 0.001f;
+  this->offset_ = this->offset_ * (1 - alpha) + value * alpha;
+
+  // Filtered value centered around the mid-point (0V)
+  float filtered = value - this->offset_;
+
+  // IRMS is sqrt(∑v_i²)
+  float sq = filtered * filtered;
+  this->sample_sum_ += sq;
+  this->num_samples_++;
 }
 
 }  // namespace ct_clamp
--- esphome/components/ct_clamp/sensor.py
+++ esphome/components/ct_clamp/sensor.py
@@ -5,7 +5,7 @@ from esphome.const import CONF_SENSOR, CONF_ID, ICON_FLASH, UNIT_AMPERE
 
 AUTO_LOAD = ['voltage_sampler']
 
-CONF_SAMPLE_SIZE = 'sample_size'
+CONF_SAMPLE_DURATION = 'sample_duration'
 
 ct_clamp_ns = cg.esphome_ns.namespace('ct_clamp')
 CTClampSensor = ct_clamp_ns.class_('CTClampSensor', sensor.Sensor, cg.PollingComponent)
@@ -13,7 +13,7 @@ CTClampSensor = ct_clamp_ns.class_('CTClampSensor', sensor.Sensor, cg.PollingCom
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_AMPERE, ICON_FLASH, 2).extend({
     cv.GenerateID(): cv.declare_id(CTClampSensor),
     cv.Required(CONF_SENSOR): cv.use_id(voltage_sampler.VoltageSampler),
-    cv.Optional(CONF_SAMPLE_SIZE, default=1480): cv.positive_int,
+    cv.Optional(CONF_SAMPLE_DURATION, default='200ms'): cv.positive_time_period_milliseconds,
 }).extend(cv.polling_component_schema('60s'))
 
 
@@ -23,6 +23,5 @@ def to_code(config):
     yield sensor.register_sensor(var, config)
 
     sens = yield cg.get_variable(config[CONF_SENSOR])
-
     cg.add(var.set_source(sens))
-    cg.add(var.set_sample_size(config[CONF_SAMPLE_SIZE]))
+    cg.add(var.set_sample_duration(config[CONF_SAMPLE_DURATION]))
