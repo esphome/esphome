c09ddb78166851e5d54342cf808b464c44b73abc
Otto Winter <otto@otto-winter.com>
2019-05-31 13:37:08 +0200

    Use abstract method

--- esphome/components/lcd_pcf8574/pcf8574_display.h
+++ esphome/components/lcd_pcf8574/pcf8574_display.h
@@ -9,6 +9,7 @@ namespace lcd_pcf8574 {
 
 class PCF8574LCDDisplay : public lcd_base::LCDDisplay, public i2c::I2CDevice {
  public:
+  void set_writer(std::function<void(PCF8574LCDDisplay &)> &&writer) { this->writer_ = std::move(writer); }
   void setup() override;
   void dump_config() override;
   void backlight();
@@ -19,8 +20,11 @@ class PCF8574LCDDisplay : public lcd_base::LCDDisplay, public i2c::I2CDevice {
   void write_n_bits(uint8_t value, uint8_t n) override;
   void send(uint8_t value, bool rs) override;
 
+  void call_writer() override { this->writer_(*this); }
+
   // Stores the current state of the backlight.
   uint8_t backlight_value_;
+  std::function<void(PCF8574LCDDisplay &)> writer_;
 };
 
 }  // namespace lcd_pcf8574
