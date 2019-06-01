8cf15c7f5cffc0e14003cd18cba981ed1307f108
Otto Winter <otto@otto-winter.com>
2019-06-01 22:02:10 +0200

    Bump version to v1.13.3

--- esphome/const.py
+++ esphome/const.py
@@ -3,7 +3,7 @@
 
 MAJOR_VERSION = 1
 MINOR_VERSION = 13
-PATCH_VERSION = '2'
+PATCH_VERSION = '3'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 
