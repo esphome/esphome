970838ed09359e4255e945cd5ec522d67f07a5ac
Major Péter <majorpeter29@gmail.com>
2019-06-04 12:11:59 +0200

    Scan length for AddressableScanEffect (#608)
    * Added scan_length to AddressableScanEffect (allow more than one LED)
    
    * Added check for scan length being longer than addressable light
    
    * Added config option 'scan_length' to AddressableScanEffect (default: 1)
    
    * Renamed scan_length to scan_width, removed erroneous length check
    
    * Fixed indentation issue in addressable_light_effect.h
    
    Co-Authored-By: Otto Winter <otto@otto-winter.com>

--- esphome/components/light/effects.py
+++ esphome/components/light/effects.py
@@ -16,6 +16,7 @@ from .types import LambdaLightEffect, RandomLightEffect, StrobeLightEffect, \
 CONF_ADD_LED_INTERVAL = 'add_led_interval'
 CONF_REVERSE = 'reverse'
 CONF_MOVE_INTERVAL = 'move_interval'
+CONF_SCAN_WIDTH = 'scan_width'
 CONF_TWINKLE_PROBABILITY = 'twinkle_probability'
 CONF_PROGRESS_INTERVAL = 'progress_interval'
 CONF_SPARK_PROBABILITY = 'spark_probability'
@@ -179,10 +180,12 @@ def addressable_color_wipe_effect_to_code(config, effect_id):
 
 @register_effect('addressable_scan', AddressableScanEffect, "Scan", {
     cv.Optional(CONF_MOVE_INTERVAL, default='0.1s'): cv.positive_time_period_milliseconds,
+    cv.Optional(CONF_SCAN_WIDTH, default=1): cv.int_range(min=1),
 })
 def addressable_scan_effect_to_code(config, effect_id):
     var = cg.new_Pvariable(effect_id, config[CONF_NAME])
     cg.add(var.set_move_interval(config[CONF_MOVE_INTERVAL]))
+    cg.add(var.set_scan_width(config[CONF_SCAN_WIDTH]))
     yield var
 
 
