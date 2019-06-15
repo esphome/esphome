3ef56f645edd0ff611a971e97c3e78b18b4e177a
Otto Winter <otto@otto-winter.com>
2019-06-15 18:19:32 +0200

    Rename constants to match their value

--- esphome/components/atm90e32/sensor.py
+++ esphome/components/atm90e32/sensor.py
@@ -9,9 +9,9 @@ CONF_PHASE_A = 'phase_a'
 CONF_PHASE_B = 'phase_b'
 CONF_PHASE_C = 'phase_c'
 
-CONF_LINE_FREQ = 'line_frequency'
+CONF_LINE_FREQUENCY = 'line_frequency'
 CONF_GAIN_PGA = 'gain_pga'
-CONF_GAIN_VOLT = 'gain_voltage'
+CONF_GAIN_VOLTAGE = 'gain_voltage'
 CONF_GAIN_CT = 'gain_ct'
 LINE_FREQS = {
     '50HZ': 50,
@@ -30,7 +30,7 @@ ATM90E32_PHASE_SCHEMA = cv.Schema({
     cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 2),
     cv.Optional(CONF_CURRENT): sensor.sensor_schema(UNIT_AMPERE, ICON_FLASH, 2),
     cv.Optional(CONF_POWER): sensor.sensor_schema(UNIT_WATT, ICON_FLASH, 2),
-    cv.Optional(CONF_GAIN_VOLT, default=41820): cv.uint16_t,
+    cv.Optional(CONF_GAIN_VOLTAGE, default=41820): cv.uint16_t,
     cv.Optional(CONF_GAIN_CT, default=25498): cv.uint16_t,
 })
 
@@ -40,7 +40,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_PHASE_B): ATM90E32_PHASE_SCHEMA,
     cv.Optional(CONF_PHASE_C): ATM90E32_PHASE_SCHEMA,
     cv.Optional(CONF_FREQUENCY): sensor.sensor_schema(UNIT_HZ, ICON_FLASH, 1),
-    cv.Required(CONF_LINE_FREQ): cv.enum(LINE_FREQS, upper=True),
+    cv.Required(CONF_LINE_FREQUENCY): cv.enum(LINE_FREQS, upper=True),
     cv.Optional(CONF_GAIN_PGA, default='2X'): cv.enum(PGA_GAINS, upper=True),
 }).extend(cv.polling_component_schema('60s')).extend(spi.SPI_DEVICE_SCHEMA)
 
@@ -54,7 +54,7 @@ def to_code(config):
         if phase not in config:
             continue
         conf = config[phase]
-        cg.add(var.set_volt_gain(i, conf[CONF_GAIN_VOLT]))
+        cg.add(var.set_volt_gain(i, conf[CONF_GAIN_VOLTAGE]))
         cg.add(var.set_ct_gain(i, conf[CONF_GAIN_CT]))
         if CONF_VOLTAGE in conf:
             sens = yield sensor.new_sensor(conf[CONF_VOLTAGE])
@@ -68,5 +68,5 @@ def to_code(config):
     if CONF_FREQUENCY in config:
         sens = yield sensor.new_sensor(config[CONF_FREQUENCY])
         cg.add(var.set_freq_sensor(sens))
-    cg.add(var.set_line_freq(config[CONF_LINE_FREQ]))
+    cg.add(var.set_line_freq(config[CONF_LINE_FREQUENCY]))
     cg.add(var.set_pga_gain(config[CONF_GAIN_PGA]))
