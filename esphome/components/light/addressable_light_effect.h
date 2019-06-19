075118c96541c91ef2eb5a3e1f56a5ac6ee6abf6
olg <x>
2019-06-19 11:11:52 +0200

    lint

--- esphome/components/light/addressable_light_effect.h
+++ esphome/components/light/addressable_light_effect.h
@@ -50,8 +50,7 @@ class AddressableLightEffect : public LightEffect {
 
 class AddressableLambdaLightEffect : public AddressableLightEffect {
  public:
-  AddressableLambdaLightEffect(const std::string &name,
-                               const std::function<void(AddressableLight &, ESPColor)> &f,
+  AddressableLambdaLightEffect(const std::string &name, const std::function<void(AddressableLight &, ESPColor)> &f,
                                uint32_t update_interval)
       : AddressableLightEffect(name), f_(f), update_interval_(update_interval) {}
   void apply(AddressableLight &it, const ESPColor &current_color) override {
