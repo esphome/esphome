3a05d54affc1fe2753335c426e6043f35a209ef7
Otto Winter <otto@otto-winter.com>
2019-06-25 10:09:03 +0200

    Fix

--- esphome/components/ade7953/sensor.py
+++ esphome/components/ade7953/sensor.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, uart, i2c
+from esphome.components import sensor, i2c
 from esphome.const import CONF_ID, CONF_VOLTAGE, \
     UNIT_VOLT, ICON_FLASH, UNIT_AMPERE, UNIT_WATT
 
@@ -22,13 +22,13 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_CURRENT_B): sensor.sensor_schema(UNIT_AMPERE, ICON_FLASH, 2),
     cv.Optional(CONF_ACTIVE_POWER_A): sensor.sensor_schema(UNIT_WATT, ICON_FLASH, 1),
     cv.Optional(CONF_ACTIVE_POWER_B): sensor.sensor_schema(UNIT_WATT, ICON_FLASH, 1),
-}).extend(cv.polling_component_schema('60s')).extend(uart.UART_DEVICE_SCHEMA)
+}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(0x38))
 
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
-    yield uart.register_uart_device(var, config)
+    yield i2c.register_i2c_device(var, config)
 
     for key in [CONF_VOLTAGE, CONF_CURRENT_A, CONF_CURRENT_B, CONF_ACTIVE_POWER_A,
                 CONF_ACTIVE_POWER_B]:
