13522c8f1944a85766b80a231be2d1b6296e71ca
Otto Winter <otto@otto-winter.com>
2019-07-02 13:03:11 +0200

    WIP: ESP8266 work on connection issues (#648)
    * Erase all flash for USB uploads on ESP8266s
    
    Previously, only erased "write regions".
    
    Downside: Config for other FWs like tasmota could be affected
    
    Upside: Potentially fixes some ESP8266 connection issues
    
    Related: https://github.com/esphome/issues/issues/455#issuecomment-503524479
    
    * Clear WiFi settings for ESP8266
    
    Clears wifi settings from retained storage on ESP8266 (if set).
    
    Unsure if this is the actual issue, but it won't cause problems either.
    
    * Update wifi_component_esp8266.cpp
    
    * Revert erase chip for testing
    
    * Improve wait_time calculation

--- esphome/components/wifi/wifi_component.h
+++ esphome/components/wifi/wifi_component.h
@@ -181,17 +181,18 @@ class WiFiComponent : public Component {
   void print_connect_params_();
 
   bool wifi_mode_(optional<bool> sta, optional<bool> ap);
-  bool wifi_disable_auto_connect_();
+  bool wifi_sta_pre_setup_();
   bool wifi_apply_power_save_();
   bool wifi_sta_ip_config_(optional<ManualIP> manual_ip);
   IPAddress wifi_sta_ip_();
   bool wifi_apply_hostname_();
   bool wifi_sta_connect_(WiFiAP ap);
-  void wifi_register_callbacks_();
+  void wifi_pre_setup_();
   wl_status_t wifi_sta_status_();
   bool wifi_scan_start_();
   bool wifi_ap_ip_config_(optional<ManualIP> manual_ip);
   bool wifi_start_ap_(const WiFiAP &ap);
+  bool wifi_disconnect_();
 
   bool is_captive_portal_active_();
 
