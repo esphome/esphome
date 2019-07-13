f6955a6751887c0808581d5dc28c5cd2a7ecf545
Thomas Eckerstorfer <eckerstorfer.thomas@gmail.com>
2019-07-14 00:11:37 +0200

    support for i2c + spi updated

---
+++ esphome/components/as3935_i2c/as3935_i2c.cpp
@@ -0,0 +1,36 @@
+#include "as3935_i2c.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace as3935_i2c {
+
+static const char *TAG = "as3935_i2c";
+
+void I2CAS3935Component::write_register_(uint8_t reg, uint8_t mask, uint8_t bits, uint8_t start_pos) {
+  uint8_t write_reg;
+  if (!this->read_byte(reg, &write_reg)) {
+    this->mark_failed();
+    ESP_LOGW(TAG, "read_byte failed - increase log level for more details!");
+    return;
+  }
+
+  write_reg &= (~mask);
+  write_reg |= (bits << start_pos);
+
+  if (!this->write_byte(reg, write_reg)) {
+    ESP_LOGW(TAG, "write_byte failed - increase log level for more details!");
+    return;
+  }
+}
+
+uint8_t I2CAS3935Component::read_register_(uint8_t reg) {
+  uint8_t value;
+  if (!this->read_byte(reg, &value, 2)) {
+    ESP_LOGW(TAG, "Read failed!");
+    return 0;
+  }
+  return value;
+}
+
+}  // namespace as3935_i2c
+}  // namespace esphome
---
+++ esphome/components/as3935_i2c/as3935_i2c.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/as3935_base/as3935_base.h"
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+
+namespace esphome {
+namespace as3935_i2c {
+
+class I2CAS3935Component : public as3935_base::AS3935Component, public i2c::I2CDevice {
+ public:
+
+ protected:
+  void write_register_(uint8_t reg, uint8_t mask, uint8_t bits, uint8_t start_position) override;
+  uint8_t read_register_(uint8_t reg) override;
+};
+
+}  // namespace as3935_i2c
+}  // namespace esphome
