584b7fef692f94424b90bdc6b02527b82c23d98e
valordk <valordk@github>
2019-07-21 21:09:49 +0200

    Clean up

--- esphome/components/sts3x/sensor.py
+++ esphome/components/sts3x/sensor.py
@@ -1,8 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import CONF_ID, CONF_TEMPERATURE, \
-    ICON_THERMOMETER, UNIT_CELSIUS
+from esphome.const import CONF_ID, CONF_TEMPERATURE, ICON_THERMOMETER, UNIT_CELSIUS
 
 DEPENDENCIES = ['i2c']
 
@@ -19,7 +18,6 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield i2c.register_i2c_device(var, config)
+    sens = yield sensor.new_sensor(config[CONF_TEMPERATURE])
 
-    if CONF_TEMPERATURE in config:
-        sens = yield sensor.new_sensor(config[CONF_TEMPERATURE])
-        cg.add(var.set_temperature_sensor(sens))
+    cg.add(var.set_temperature_sensor(sens))
