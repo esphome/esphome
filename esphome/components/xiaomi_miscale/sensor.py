9effe68dd92fb46c6a820b6892a280dfcdb9eeb6
tiagofreire-pt <41837236+tiagofreire-pt@users.noreply.github.com>
2019-08-13 21:20:15 +0100

    Update sensor.py
    Removing excessive empty lines.

--- esphome/components/xiaomi_miscale/sensor.py
+++ esphome/components/xiaomi_miscale/sensor.py
@@ -22,14 +22,12 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(UNIT_PERCENT, ICON_BATTERY, 0),
 }).extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
 
-
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield esp32_ble_tracker.register_ble_device(var, config)
 
     cg.add(var.set_address(config[CONF_MAC_ADDRESS].as_hex))
-
 
     if CONF_WEIGHT in config:
         sens = yield sensor.new_sensor(config[CONF_WEIGHT])
