e774d99d7c11bb56a5ebf0157b08cdbb0446e80e
tiagofreire-pt <41837236+tiagofreire-pt@users.noreply.github.com>
2019-08-13 20:43:01 +0100

    Update sensor.py

--- esphome/components/xiaomi_miscale/sensor.py
+++ esphome/components/xiaomi_miscale/sensor.py
@@ -4,7 +4,7 @@ from esphome.components import sensor, esp32_ble_tracker
 from esphome.const import CONF_BATTERY_LEVEL, CONF_MAC_ADDRESS, CONF_TEMPERATURE, \
     UNIT_CELSIUS, ICON_THERMOMETER, UNIT_PERCENT, ICON_WATER_PERCENT, ICON_BATTERY, CONF_ID, \
     CONF_MOISTURE, CONF_ILLUMINANCE, ICON_BRIGHTNESS_5, UNIT_LUX, CONF_CONDUCTIVITY, \
-    UNIT_MICROSIEMENS_PER_CENTIMETER, ICON_FLOWER
+    UNIT_MICROSIEMENS_PER_CENTIMETER, ICON_FLOWER, CONF_WEIGHT, UNIT_KILOGRAMS, ICON_SCALE
 
 DEPENDENCIES = ['esp32_ble_tracker']
 AUTO_LOAD = ['xiaomi_ble']
@@ -13,13 +13,13 @@ CONF_WEIGHT = 'weight'
 ICON_SCALE = 'mdi:scale'
 
 xiaomi_miscale_ns = cg.esphome_ns.namespace('xiaomi_miscale')
-XiaomiMiscale = xiaomi_miscale_ns.class_('XiaomiMiscale', esp32_ble_tracker.ESPBTDeviceListener,
-                                         cg.Component)
+XiaomiMiscale = xiaomi_miscale_ns.class_('XiaomiMiscale', esp32_ble_tracker.ESPBTDeviceListener, cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(XiaomiMiscale),
     cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
-    cv.Optional(CONF_WEIGHT): sensor.sensor_schema(UNIT_GRAMS, ICON_SCALE, 1),
+    cv.Optional(CONF_WEIGHT): sensor.sensor_schema(UNIT_KILOGRAMS, ICON_SCALE, 1),
+    cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(UNIT_PERCENT, ICON_BATTERY, 0),
 }).extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
 
 
@@ -34,3 +34,6 @@ def to_code(config):
     if CONF_WEIGHT in config:
         sens = yield sensor.new_sensor(config[CONF_WEIGHT])
         cg.add(var.set_weight(sens))
+    if CONF_BATTERY_LEVEL in config:
+        sens = yield sensor.new_sensor(config[CONF_BATTERY_LEVEL])
+        cg.add(var.set_battery_level(sens))
