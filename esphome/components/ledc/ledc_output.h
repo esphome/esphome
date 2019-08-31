e465375459c5f2535572012b7537d1c2be3f3b9b
Otto Winter <otto@otto-winter.com>
2019-08-31 19:23:23 +0200

    Add LEDC set_frequency
    Fixes https://github.com/esphome/feature-requests/issues/380

--- esphome/components/ledc/ledc_output.h
+++ esphome/components/ledc/ledc_output.h
@@ -16,11 +16,10 @@ class LEDCOutput : public output::FloatOutput, public Component {
   explicit LEDCOutput(GPIOPin *pin) : pin_(pin) { this->channel_ = next_ledc_channel++; }
 
   void set_channel(uint8_t channel) { this->channel_ = channel; }
-  void set_bit_depth(uint8_t bit_depth) { this->bit_depth_ = bit_depth; }
   void set_frequency(float frequency) { this->frequency_ = frequency; }
+  /// Dynamically change frequency at runtime
+  void apply_frequency(float frequency);
 
-  // ========== INTERNAL METHODS ==========
-  // (In most use cases you won't need these)
   /// Setup LEDC.
   void setup() override;
   void dump_config() override;
@@ -35,6 +34,21 @@ class LEDCOutput : public output::FloatOutput, public Component {
   uint8_t channel_{};
   uint8_t bit_depth_{};
   float frequency_{};
+  float duty_{0.0f};
+};
+
+template<typename... Ts> class SetFrequencyAction : public Action<Ts...> {
+ public:
+  SetFrequencyAction(LEDCOutput *parent) : parent_(parent) {}
+  TEMPLATABLE_VALUE(float, frequency);
+
+  void play(Ts... x) {
+    float freq = this->frequency_.value(x...);
+    this->parent_->apply_frequency(freq);
+  }
+
+ protected:
+  LEDCOutput *parent_;
 };
 
 }  // namespace ledc
