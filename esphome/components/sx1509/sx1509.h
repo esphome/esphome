be0c3bbd12beff8bfda618bcdac2133485276919
Otto Winter <otto@otto-winter.com>
2019-09-01 11:22:54 +0200

    Run clang-format

--- esphome/components/sx1509/sx1509.h
+++ esphome/components/sx1509/sx1509.h
@@ -18,10 +18,10 @@ const uint8_t ANALOG_OUTPUT = 0x03;  // To set a pin mode for PWM output
 
 // PinModes for SX1509 pins
 enum SX1509GPIOMode : uint8_t {
-  SX1509_INPUT = INPUT,                 // 0x00
-  SX1509_INPUT_PULLUP = INPUT_PULLUP,   // 0x02
-  SX1509_ANALOG_OUTPUT = ANALOG_OUTPUT, // 0x03
-  SX1509_OUTPUT = OUTPUT,               // 0x01
+  SX1509_INPUT = INPUT,                  // 0x00
+  SX1509_INPUT_PULLUP = INPUT_PULLUP,    // 0x02
+  SX1509_ANALOG_OUTPUT = ANALOG_OUTPUT,  // 0x03
+  SX1509_OUTPUT = OUTPUT,                // 0x01
 };
 
 const uint8_t REG_I_ON[16] = {REG_I_ON_0,  REG_I_ON_1,  REG_I_ON_2,  REG_I_ON_3, REG_I_ON_4,  REG_I_ON_5,
@@ -30,13 +30,13 @@ const uint8_t REG_I_ON[16] = {REG_I_ON_0,  REG_I_ON_1,  REG_I_ON_2,  REG_I_ON_3,
 
 // for all components that implement the process(uint16_t data )
 class SX1509Processor {
-public:
+ public:
   virtual void process(uint16_t data){};
 };
 
 class SX1509Component : public Component, public i2c::I2CDevice {
-public:
-  SX1509Component() {}
+ public:
+  SX1509Component() = default;
 
   void setup() override;
   void dump_config() override;
@@ -45,9 +45,7 @@ public:
 
   bool digital_read(uint8_t pin);
   uint16_t read_key_data();
-  void set_pin_value(uint8_t pin, uint8_t i_on) {
-    this->write_byte(REG_I_ON[pin], i_on);
-  };
+  void set_pin_value(uint8_t pin, uint8_t i_on) { this->write_byte(REG_I_ON[pin], i_on); };
   void pin_mode(uint8_t pin, uint8_t mode);
   void digital_write(uint8_t pin, bool bit_value);
   u_long get_clock() { return this->clk_x_; };
@@ -58,9 +56,7 @@ public:
   };
   void set_sleep_time(uint16_t sleep_time) { this->sleep_time_ = sleep_time; };
   void set_scan_time(uint8_t scan_time) { this->scan_time_ = scan_time; };
-  void set_debounce_time(uint8_t debounce_time = 1) {
-    this->debounce_time_ = debounce_time;
-  };
+  void set_debounce_time(uint8_t debounce_time = 1) { this->debounce_time_ = debounce_time; };
   void register_keypad_binary_sensor(SX1509Processor *binary_sensor) {
     this->keypad_binary_sensors_.push_back(binary_sensor);
   };
@@ -86,9 +82,8 @@ public:
   void set_debounce_enable_(uint8_t pin);
   void set_debounce_keypad_(uint8_t time, uint8_t num_rows, uint8_t num_cols);
   void setup_led_driver_(uint8_t pin);
-  void clock_(uint8_t osc_source = 2, uint8_t osc_pin_function = 1,
-              uint8_t osc_freq_out = 0, uint8_t osc_divider = 0);
+  void clock_(uint8_t osc_source = 2, uint8_t osc_pin_function = 1, uint8_t osc_freq_out = 0, uint8_t osc_divider = 0);
 };
 
-} // namespace sx1509
-} // namespace esphome
+}  // namespace sx1509
+}  // namespace esphome
