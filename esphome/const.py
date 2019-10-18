1cfc6ac3c6c53c34709739e6cd1c883810942f2a
Otto Winter <otto@otto-winter.com>
2019-10-18 14:46:29 +0200

    Update ESP8266/ESP32 bases (#760)
    * Update ESP8266/ESP32 bases
    
    * Update platformio.ini
    
    * Update boards definitions

--- esphome/const.py
+++ esphome/const.py
@@ -15,11 +15,14 @@ ALLOWED_NAME_CHARS = u'abcdefghijklmnopqrstuvwxyz0123456789_'
 ARDUINO_VERSION_ESP32_DEV = 'https://github.com/platformio/platform-espressif32.git#feature/stage'
 ARDUINO_VERSION_ESP32_1_0_0 = 'espressif32@1.5.0'
 ARDUINO_VERSION_ESP32_1_0_1 = 'espressif32@1.6.0'
+ARDUINO_VERSION_ESP32_1_0_2 = 'espressif32@1.9.0'
+ARDUINO_VERSION_ESP32_1_0_3 = 'espressif32@1.10.0'
+ARDUINO_VERSION_ESP32_1_0_4 = 'espressif32@1.11.0'
 ARDUINO_VERSION_ESP8266_DEV = 'https://github.com/platformio/platform-espressif8266.git#feature' \
                               '/stage'
 ARDUINO_VERSION_ESP8266_2_5_0 = 'espressif8266@2.0.0'
 ARDUINO_VERSION_ESP8266_2_5_1 = 'espressif8266@2.1.0'
-ARDUINO_VERSION_ESP8266_2_5_2 = 'espressif8266@2.2.0'
+ARDUINO_VERSION_ESP8266_2_5_2 = 'espressif8266@2.2.3'
 ARDUINO_VERSION_ESP8266_2_3_0 = 'espressif8266@1.5.0'
 SOURCE_FILE_EXTENSIONS = {'.cpp', '.hpp', '.h', '.c', '.tcc', '.ino'}
 HEADER_FILE_EXTENSIONS = {'.h', '.hpp', '.tcc'}
