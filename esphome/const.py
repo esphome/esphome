16bc60644d6b5668b3fb4f4387e647dd2c9e2b79
Otto Winter <otto@otto-winter.com>
2019-10-19 17:11:22 +0200

    Bump version to v1.15.0-dev

--- esphome/const.py
+++ esphome/const.py
@@ -2,7 +2,7 @@
 """Constants used by esphome."""
 
 MAJOR_VERSION = 1
-MINOR_VERSION = 14
+MINOR_VERSION = 15
 PATCH_VERSION = '0-dev'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
