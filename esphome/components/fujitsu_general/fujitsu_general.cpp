42d14570b114a4463bec40aab27ac1e7c2d8d8d7
Otto Winter <otto@otto-winter.com>
2019-10-24 13:46:00 +0200

    Refactor Fujitsu Climate to climate_ir

--- esphome/components/fujitsu_general/fujitsu_general.cpp
+++ esphome/components/fujitsu_general/fujitsu_general.cpp
@@ -74,58 +74,13 @@ const uint16_t FUJITSU_GENERAL_TRL_SPACE = 8000;
 
 const uint32_t FUJITSU_GENERAL_CARRIER_FREQUENCY = 38000;
 
-climate::ClimateTraits FujitsuGeneralClimate::traits() {
-  auto traits = climate::ClimateTraits();
-  traits.set_supports_current_temperature(this->sensor_ != nullptr);
-  traits.set_supports_auto_mode(true);
-  traits.set_supports_cool_mode(this->supports_cool_);
-  traits.set_supports_heat_mode(this->supports_heat_);
-  traits.set_supports_two_point_target_temperature(false);
-  traits.set_supports_away(false);
-  traits.set_visual_min_temperature(FUJITSU_GENERAL_TEMP_MIN);
-  traits.set_visual_max_temperature(FUJITSU_GENERAL_TEMP_MAX);
-  traits.set_visual_temperature_step(1);
-  return traits;
-}
+FujitsuGeneralClimate::FujitsuGeneralClimate() : ClimateIR(FUJITSU_GENERAL_TEMP_MIN, FUJITSU_GENERAL_TEMP_MAX, 1) {}
 
-void FujitsuGeneralClimate::setup() {
-  if (this->sensor_) {
-    this->sensor_->add_on_state_callback([this](float state) {
-      this->current_temperature = state;
-      // current temperature changed, publish state
-      this->publish_state();
-    });
-    this->current_temperature = this->sensor_->state;
-  } else
-    this->current_temperature = NAN;
-  // restore set points
-  auto restore = this->restore_state_();
-  if (restore.has_value()) {
-    restore->apply(this);
-  } else {
-    // restore from defaults
-    this->mode = climate::CLIMATE_MODE_OFF;
-    this->target_temperature = 24;
-  }
-  // initial state is powered off
-  this->power_ = false;
-}
-
-void FujitsuGeneralClimate::control(const climate::ClimateCall &call) {
-  if (call.get_mode().has_value())
-    this->mode = *call.get_mode();
-  if (call.get_target_temperature().has_value())
-    this->target_temperature = *call.get_target_temperature();
-
+void FujitsuGeneralClimate::transmit_state() {
   if (this->mode == climate::CLIMATE_MODE_OFF) {
     this->transmit_off_();
-  } else {
-    this->transmit_state_();
+    return;
   }
-  this->publish_state();
-}
-
-void FujitsuGeneralClimate::transmit_state_() {
   uint8_t remote_state[FUJITSU_GENERAL_STATE_LENGTH] = {0};
 
   remote_state[0] = FUJITSU_GENERAL_BASE_BYTE0;
@@ -153,7 +108,6 @@ void FujitsuGeneralClimate::transmit_state_() {
 
   // If not powered - set power on flag
   if (!this->power_) {
-    this->power_ = true;
     remote_state[8] = (byte) remote_state[8] | FUJITSU_GENERAL_POWER_ON_MASK_BYTE8;
   }
 
@@ -212,10 +166,7 @@ void FujitsuGeneralClimate::transmit_state_() {
 
   transmit.perform();
 
-  // If was not powered - set powered
-  if (!this->power_) {
-    this->power_ = true;
-  }
+  this->power_ = true;
 }
 
 void FujitsuGeneralClimate::transmit_off_() {
--- esphome/components/fujitsu_general/fujitsu_general.h
+++ esphome/components/fujitsu_general/fujitsu_general.h
@@ -2,43 +2,22 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
-#include "esphome/components/climate/climate.h"
-#include "esphome/components/remote_base/remote_base.h"
-#include "esphome/components/remote_transmitter/remote_transmitter.h"
-#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/climate_ir/climate_ir.h"
 
 namespace esphome {
 namespace fujitsu_general {
 
-class FujitsuGeneralClimate : public climate::Climate, public Component {
+class FujitsuGeneralClimate : public climate_ir::ClimateIR {
  public:
-  void setup() override;
-  void set_transmitter(remote_transmitter::RemoteTransmitterComponent *transmitter) {
-    this->transmitter_ = transmitter;
-  }
-  void set_supports_cool(bool supports_cool) { this->supports_cool_ = supports_cool; }
-  void set_supports_heat(bool supports_heat) { this->supports_heat_ = supports_heat; }
-  void set_sensor(sensor::Sensor *sensor) { this->sensor_ = sensor; }
-  void transmit_off() { this->transmit_off_(); };
-  void transmit_state() { this->transmit_state_(); };
+  FujitsuGeneralClimate();
 
  protected:
-  /// Override control to change settings of the climate device.
-  void control(const climate::ClimateCall &call) override;
-  /// Return the traits of this controller.
-  climate::ClimateTraits traits() override;
-
   /// Transmit via IR the state of this climate controller.
-  void transmit_state_();
+  void transmit_state() override;
   /// Transmit via IR power off command.
   void transmit_off_();
 
-  bool supports_cool_{true};
-  bool supports_heat_{true};
-
-  bool power_;
-  remote_transmitter::RemoteTransmitterComponent *transmitter_;
-  sensor::Sensor *sensor_{nullptr};
+  bool power_{false};
 };
 
 }  // namespace fujitsu_general
