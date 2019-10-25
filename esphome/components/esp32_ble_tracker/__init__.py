52d35e8c2b275072e62279f146dc924e0862c856
Lumpusz <marton.keri@gmail.com>
2019-10-25 16:16:24 +0200

    reformat

--- esphome/components/esp32_ble_tracker/__init__.py
+++ esphome/components/esp32_ble_tracker/__init__.py
@@ -72,13 +72,9 @@ def as_hex(value):
 
 def as_hex_array(value):
     value = value.replace("-", "")
+    cpp_array = ['0x{}'.format(part) for part in [value[i:i+2] for i in range(0, len(value), 2)]]
     return cg.RawExpression(
-        '(uint8_t*)(const uint8_t[16]){{{}}}'.format(
-            ','.join(reversed(
-                ['0x{}'.format(part) for part in [value[i:i+2] for i in range(0, len(value), 2)]])
-                )
-            )
-        )
+        '(uint8_t*)(const uint8_t[16]){{{}}}'.format(','.join(reversed(cpp_array))))
 
 
 CONFIG_SCHEMA = cv.Schema({
