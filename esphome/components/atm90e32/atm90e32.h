5a67e723895297f78bae960cbfd38c787192a85e
John <34163498+CircuitSetup@users.noreply.github.com>
2019-10-27 07:05:13 -0400

    Added more power data to the atm90e32 component (#799)
    * Added more data to atm90e32 component
    
    * ignore
    
    * correction
    
    * Delete 6chan_energy_meter.yaml
    
    * Update sensor.py
    
    fixed indents
    
    * Update atm90e32.h
    
    * Update esphome/components/atm90e32/sensor.py
    
    Co-Authored-By: Otto Winter <otto@otto-winter.com>
    
    * PR request changes
    
    * repository test branch
    
    * Update setup.py
    
    * Update const.py
    
    * backslash
    
    * comma!
    
    * delete test yaml
    
    * corrected chip temp
    
    * change to signed int for get_pf_ functions
    
    * Update atm90e32.h
    
    formatting
    
    * adjusted function & variable names
    
    * Update atm90e32.h
    
    formatting
    
    * Update sensor.py
    
    Import CONF_POWER_FACTOR from const.py
    
    * travis formatting
    
    * Update esphome/components/atm90e32/sensor.py
    
    Co-Authored-By: Otto Winter <otto@otto-winter.com>
    
    * Update esphome/components/atm90e32/atm90e32.h
    
    Co-Authored-By: Otto Winter <otto@otto-winter.com>

--- esphome/components/atm90e32/atm90e32.h
+++ esphome/components/atm90e32/atm90e32.h
@@ -19,10 +19,15 @@ class ATM90E32Component : public PollingComponent,
   void set_voltage_sensor(int phase, sensor::Sensor *obj) { this->phase_[phase].voltage_sensor_ = obj; }
   void set_current_sensor(int phase, sensor::Sensor *obj) { this->phase_[phase].current_sensor_ = obj; }
   void set_power_sensor(int phase, sensor::Sensor *obj) { this->phase_[phase].power_sensor_ = obj; }
+  void set_reactive_power_sensor(int phase, sensor::Sensor *obj) { this->phase_[phase].reactive_power_sensor_ = obj; }
+  void set_power_factor_sensor(int phase, sensor::Sensor *obj) { this->phase_[phase].power_factor_sensor_ = obj; }
   void set_volt_gain(int phase, uint16_t gain) { this->phase_[phase].volt_gain_ = gain; }
   void set_ct_gain(int phase, uint16_t gain) { this->phase_[phase].ct_gain_ = gain; }
 
   void set_freq_sensor(sensor::Sensor *freq_sensor) { freq_sensor_ = freq_sensor; }
+  void set_chip_temperature_sensor(sensor::Sensor *chip_temperature_sensor) {
+    chip_temperature_sensor_ = chip_temperature_sensor;
+  }
   void set_line_freq(int freq) { line_freq_ = freq; }
   void set_pga_gain(uint16_t gain) { pga_gain_ = gain; }
 
@@ -40,7 +45,14 @@ class ATM90E32Component : public PollingComponent,
   float get_active_power_a_();
   float get_active_power_b_();
   float get_active_power_c_();
+  float get_reactive_power_a_();
+  float get_reactive_power_b_();
+  float get_reactive_power_c_();
+  float get_power_factor_a_();
+  float get_power_factor_b_();
+  float get_power_factor_c_();
   float get_frequency_();
+  float get_chip_temperature_();
 
   struct ATM90E32Phase {
     uint16_t volt_gain_{41820};
@@ -48,8 +60,11 @@ class ATM90E32Component : public PollingComponent,
     sensor::Sensor *voltage_sensor_{nullptr};
     sensor::Sensor *current_sensor_{nullptr};
     sensor::Sensor *power_sensor_{nullptr};
+    sensor::Sensor *reactive_power_sensor_{nullptr};
+    sensor::Sensor *power_factor_sensor_{nullptr};
   } phase_[3];
   sensor::Sensor *freq_sensor_{nullptr};
+  sensor::Sensor *chip_temperature_sensor_{nullptr};
   uint16_t pga_gain_{0x15};
   int line_freq_{60};
 };
