fdfe3254c5bc86d9c03f37843fb9481cdd8dfc42
Otto Winter <otto@otto-winter.com>
2019-10-31 19:36:05 +0100

    LEDC song

--- esphome/components/ledc/ledc_output.h
+++ esphome/components/ledc/ledc_output.h
@@ -52,6 +52,96 @@ template<typename... Ts> class SetFrequencyAction : public Action<Ts...> {
   LEDCOutput *parent_;
 };
 
+struct SongActionItem {
+  SongActionItem(float frequency, uint32_t end_at) : frequency(frequency), end_at(end_at) {}
+
+  // Frequency to play, 0 for space
+  float frequency;
+  uint32_t end_at;
+};
+
+template<typename... Ts> class SongAction : public Action<Ts...>, public Component {
+ public:
+  SongAction(LEDCOutput *parent) : parent_(parent) {}
+  void set_active_level(float active_level) {
+    active_level_ = active_level;
+  }
+  void set_items(const std::vector<SongActionItem> &items) {
+    items_ = items;
+  }
+
+  void play(Ts... x) {
+    // empty, complex action
+  }
+
+  void play_complex(Ts... x) override {
+    if (this->is_running_)
+      return;
+
+    // Store loop parameters
+    this->var_ = std::make_tuple(x...);
+    this->is_running_ = true;
+    this->high_freq_loop_.start();
+    this->started_at_ = millis();
+    this->at_ = 0;
+  }
+
+  void loop() override {
+    if (!this->is_running_)
+      return;
+    if (this->at_ >= this->items_.size()) {
+      // stop running
+      this->is_running_ = false;
+      this->high_freq_loop_.stop();
+      this->parent_->set_level(0.0f);
+      this->play_next_tuple(this->var_);
+      return;
+    }
+
+    const uint32_t now = millis();
+    const uint32_t dt = now - this->started_at_;
+
+    // write: whether to write the frequency value to the output
+    //   true at first item and any time we progress to the next item
+    bool write = this->at_ == 0;
+    for (; this->at_ < this->items_.size(); this->at_++) {
+      if (dt < this->items_[this->at_].end_at)
+        break;
+      write = true;
+    }
+    if (this->at_ >= this->items_.size() || !write)
+      // will handle stop in next loop
+      return;
+
+    float freq = this->items_[this->at_].frequency;
+    if (freq <= 0) {
+      this->parent_->set_level(0.0f);
+    } else {
+      this->parent_->apply_frequency(freq);
+      this->parent_->set_level(this->active_level_);
+    }
+  }
+
+  void stop() override {
+    this->is_running_ = false;
+    this->high_freq_loop_.stop();
+  }
+
+  bool is_running() override {
+    return this->is_running_ || this->is_running_next();
+  }
+
+ protected:
+  bool is_running_;
+  uint32_t started_at_;
+  HighFrequencyLoopRequester high_freq_loop_;
+  LEDCOutput *parent_;
+  float active_level_;
+  size_t at_;
+  std::vector<SongActionItem> items_;
+  std::tuple<Ts...> var_{};
+};
+
 }  // namespace ledc
 }  // namespace esphome
 
--- esphome/components/ledc/output.py
+++ esphome/components/ledc/output.py
@@ -1,9 +1,12 @@
+import math
+import re
+
 from esphome import pins, automation
 from esphome.components import output
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.const import CONF_BIT_DEPTH, CONF_CHANNEL, CONF_FREQUENCY, \
-    CONF_ID, CONF_PIN, ESP_PLATFORM_ESP32
+    CONF_ID, CONF_PIN, ESP_PLATFORM_ESP32, CONF_DURATION
 
 ESP_PLATFORMS = [ESP_PLATFORM_ESP32]
 
@@ -33,6 +36,8 @@ def validate_frequency(value):
 ledc_ns = cg.esphome_ns.namespace('ledc')
 LEDCOutput = ledc_ns.class_('LEDCOutput', output.FloatOutput, cg.Component)
 SetFrequencyAction = ledc_ns.class_('SetFrequencyAction', automation.Action)
+SongAction = ledc_ns.class_('SongAction', automation.Action, cg.Component)
+SongActionItem = ledc_ns.class_('SongActionItem')
 
 CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend({
     cv.Required(CONF_ID): cv.declare_id(LEDCOutput),
@@ -64,4 +69,96 @@ def ledc_set_frequency_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg, paren)
     template_ = yield cg.templatable(config[CONF_FREQUENCY], args, float)
     cg.add(var.set_frequency(template_))
+    yield var
+
+
+NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+NOTE_PATTERN = r'^([{}])\s*([0-9]*)$'.format(r'|'.join(NOTES))
+
+
+def validate_note(value):
+    value = cv.string(value).upper()
+    match = re.match(NOTE_PATTERN, value)
+    if match is None:
+        raise cv.Invalid("Invalid note '{}'. Please make sure the format is like "
+                         "'A' (default octave 4), or 'G#4'.")
+    note = NOTES.index(match.group(1))
+    octave = int(match.group(2) or 4)
+    return '{}{}'.format(NOTES[note], octave)
+
+
+def note_to_freq(value):
+    match = re.match(NOTE_PATTERN, value)
+    note = NOTES.index(match.group(1))
+    octave = int(match.group(2))
+    key = note + octave * len(NOTES)
+
+    a4 = 440
+    base = math.pow(2, 1/12)
+    c0 = a4 * math.pow(base, -57)
+
+    # https://pages.mtu.edu/~suits/NoteFreqCalcs.html
+    # f_n = f_0 * a^n
+    # f_0 ... the frequency of the fixed note. Here: C0
+    # n   ... the number of half steps away from the fixed note
+    # f_n ... frequency of the note `n` half steps away
+    # a   ... the twelfth root of 2
+
+    return c0 * math.pow(base, key)
+
+
+CONF_NOTE = 'note'
+CONF_SPACE = 'space'
+CONF_ACTIVE_LEVEL = 'active_level'
+CONF_PATTERN = 'pattern'
+
+
+def validate_pattern_item(value):
+    if not isinstance(value, dict):
+        raise cv.Invalid("Pattern item needs to be a key-value mapping. But this is a {}"
+                         "".format(type(value)))
+    if CONF_FREQUENCY in value:
+        return cv.Schema({
+            cv.Required(CONF_FREQUENCY): cv.frequency,
+            cv.Required(CONF_DURATION): cv.positive_time_period_milliseconds,
+        })(value)
+    elif CONF_NOTE in value:
+        # Convert to frequency representation
+        value = cv.Schema({
+            cv.Required(CONF_NOTE): validate_note,
+            cv.Required(CONF_DURATION): cv.positive_time_period_milliseconds,
+        })(value)
+        return validate_pattern_item({
+            CONF_FREQUENCY: note_to_freq(value[CONF_NOTE]),
+            CONF_DURATION: value[CONF_DURATION]
+        })
+    elif CONF_SPACE in value:
+        # Convert to frequency 0
+        value = cv.Schema({
+            cv.Required(CONF_SPACE): cv.positive_time_period_milliseconds,
+        })(value)
+        return validate_pattern_item({
+            CONF_FREQUENCY: 0,
+            CONF_DURATION: value[CONF_SPACE]
+        })
+
+
+@automation.register_action('output.ledc.song', SongAction, cv.Schema({
+    cv.Required(CONF_ID): cv.use_id(LEDCOutput),
+    cv.Required(CONF_PATTERN): [validate_pattern_item],
+    cv.Optional(CONF_ACTIVE_LEVEL, default='50%'): cv.percentage,
+}).extend(cv.COMPONENT_SCHEMA))
+def ledc_set_frequency_to_code(config, action_id, template_arg, args):
+    paren = yield cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+    yield cg.register_component(var, config)
+    cg.add(var.set_active_level(config[CONF_ACTIVE_LEVEL]))
+
+    items = []
+    end_at = 0
+    for conf in config[CONF_PATTERN]:
+        end_at += conf[CONF_DURATION].total_milliseconds
+        items.append(SongActionItem(conf[CONF_FREQUENCY], end_at))
+    cg.add(var.set_items(items))
+
     yield var
