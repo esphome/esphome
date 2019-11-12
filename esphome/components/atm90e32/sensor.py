9fcbf6cddb7900c6c4cc4af5d98fc9579a7589ed
diramu <github@diramu.net>
2019-11-12 22:06:27 +1100

    correct sensors.yaml reference to set_reactive_power

--- esphome/components/atm90e32/sensor.py
+++ esphome/components/atm90e32/sensor.py
@@ -73,7 +73,7 @@ def to_code(config):
             cg.add(var.set_power_sensor(i, sens))
         if CONF_REACTIVE_POWER in conf:
             sens = yield sensor.new_sensor(conf[CONF_REACTIVE_POWER])
-            cg.add(var.set_react_pow_sensor(i, sens))
+            cg.add(var.set_reactive_power_sensor(i, sens))
         if CONF_POWER_FACTOR in conf:
             sens = yield sensor.new_sensor(conf[CONF_POWER_FACTOR])
             cg.add(var.set_power_factor_sensor(i, sens))
