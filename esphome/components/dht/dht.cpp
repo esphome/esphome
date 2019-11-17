1163fe527326b692c246cd8b3ee93257d1b162b9
Airy André <airy.andre@free.fr>
2019-11-17 10:53:30 +0100

    Handle negative temp for some DHT11  - code from the DHT12 component

--- esphome/components/dht/dht.cpp
+++ esphome/components/dht/dht.cpp
@@ -167,10 +167,16 @@ bool HOT DHT::read_sensor_(float *temperature, float *humidity, bool report_erro
   if (this->model_ == DHT_MODEL_DHT11) {
     // Data format: 8bit integral RH data + 8bit decimal RH data + 8bit integral T data + 8bit decimal T data + 8bit
     // check sum
-    *humidity = data[0] + data[1] * .1f;
-    if (*humidity > 100)
-      *humidity = NAN;
-    *temperature = data[2] + data[3] * .1f;
+    const uint16_t raw_temperature = uint16_t(data[2]) * 10 + (data[3] & 0x7F);
+    *temperature = raw_temperature / 10.0f;
+    if ((data[3] & 0x80) != 0) {
+      // negative
+      *temperature *= -1;
+    }
+
+    const uint16_t raw_humidity = uint16_t(data[0]) * 10 + data[1];
+    *humidity = raw_humidity / 10.0f;
+
   } else {
     uint16_t raw_humidity = (uint16_t(data[0] & 0xFF) << 8) | (data[1] & 0xFF);
     uint16_t raw_temperature = (uint16_t(data[2] & 0xFF) << 8) | (data[3] & 0xFF);
