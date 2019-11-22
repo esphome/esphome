d0698531c13528de7be834044cb84e655860b7b8
Otto Winter <otto@otto-winter.com>
2019-11-22 17:20:10 +0100

    Add AC Dimmer support
    Fixes https://github.com/esphome/feature-requests/issues/278

---
+++ esphome/components/dimmer/dimmer.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/esphal.h"
+#include "esphome/components/output/float_output.h"
+
+namespace esphome {
+namespace dimmer {
+
+struct DimmerDataStore {
+  /// Zero-cross pin
+  ISRInternalGPIOPin *zero_cross_pin;
+  /// Output pin to write to
+  ISRInternalGPIOPin *gate_pin;
+  /// Value of the dimmer - 0 to 255.
+  uint8_t value;
+
+  /// Time between the last two ZC pulses
+  uint32_t cycle_time_us;
+  /// Time (in micros()) of last ZC signal
+  uint32_t crossed_zero_at;
+  /// Time since last ZC pulse to enable gate pin. 0 means not set.
+  uint32_t enable_time_us;
+  /// Time since last ZC pulse to disable gate pin again. 0 means not set.
+  uint32_t disable_time_us;
+
+  uint32_t timer_intr(uint32_t now);
+
+  void gpio_intr();
+  static void s_gpio_intr(DimmerDataStore *arg);
+#ifdef ARDUINO_ARCH_ESP32
+  static void s_timer_intr();
+#endif
+};
+
+class Dimmer : public output::FloatOutput, public Component {
+ public:
+  void setup() override;
+
+  void dump_config() override;
+  void set_gate_pin(GPIOPin *gate_pin) {
+    gate_pin_ = gate_pin;
+  }
+  void set_zero_cross_pin(GPIOPin *zero_cross_pin) {
+    zero_cross_pin_ = zero_cross_pin;
+  }
+
+ protected:
+  void write_state(float state) override;
+
+  GPIOPin *gate_pin_;
+  GPIOPin *zero_cross_pin_;
+  DimmerDataStore store_;
+};
+
+}  // namespace dimmer
+}  // namespace esphome
---
+++ esphome/components/dimmer/output.py
@@ -0,0 +1,29 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import pins
+from esphome.components import output
+from esphome.const import CONF_ID, ESP_PLATFORM_ESP8266
+
+ESP_PLATFORMS = [ESP_PLATFORM_ESP8266]
+
+dimmer_ns = cg.esphome_ns.namespace('dimmer')
+Dimmer = dimmer_ns.class_('Dimmer', output.FloatOutput, cg.Component)
+
+CONF_GATE_PIN = 'gate_pin'
+CONF_ZERO_CROSS_PIN = 'zero_cross_pin'
+CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend({
+    cv.Required(CONF_ID): cv.declare_id(Dimmer),
+    cv.Required(CONF_GATE_PIN): pins.internal_gpio_output_pin_schema,
+    cv.Required(CONF_ZERO_CROSS_PIN): pins.internal_gpio_input_pin_schema,
+}).extend(cv.COMPONENT_SCHEMA)
+
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield output.register_output(var, config)
+
+    pin = yield cg.gpio_pin_expression(config[CONF_GATE_PIN])
+    cg.add(var.set_gate_pin(pin))
+    pin = yield cg.gpio_pin_expression(config[CONF_ZERO_CROSS_PIN])
+    cg.add(var.set_zero_cross_pin(pin))
