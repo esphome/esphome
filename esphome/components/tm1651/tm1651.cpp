d32398e85ce9c40366aea2ed9cb6b0861acaa04d
freekode <efrolov.app@gmail.com>
2019-11-28 15:58:57 +0100

    Add TM1561 support

---
+++ esphome/components/tm1651/tm1651.cpp
@@ -0,0 +1,67 @@
+#include "tm1651.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace tm1651 {
+
+static const char *TAG = "tm1651.display";
+static const uint8_t MAX_LEVEL = 7;
+static const uint8_t MAX_INPUT_LEVEL = 1;
+
+void TM1651Display::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up TM1651...");
+
+  uint8_t clk = clk_pin_->get_pin();
+  uint8_t dio = dio_pin_->get_pin();
+
+  batteryDisplay = new TM1651(clk, dio);
+  batteryDisplay->init();
+  batteryDisplay->clearDisplay();
+}
+
+void TM1651Display::dump_config() {
+  ESP_LOGCONFIG(TAG, "TM1651 Battery Display");
+  LOG_PIN("  CLK: ", clk_pin_);
+  LOG_PIN("  DIO: ", dio_pin_);
+}
+
+void TM1651Display::set_level(float new_level) {
+  this->level = calculate_level(new_level);
+  this->repaint();
+}
+
+void TM1651Display::set_brightness(uint8_t new_brightness) {
+  this->brightness = calculate_brightness(new_brightness);
+  this->repaint();
+}
+
+void TM1651Display::repaint() {
+  batteryDisplay->set(this->brightness);
+  batteryDisplay->displayLevel(this->level);
+}
+
+uint8_t TM1651Display::calculate_level(float level) {
+  if (level < 0) {
+    return 0;
+  } else if (level > MAX_INPUT_LEVEL) {
+    return MAX_INPUT_LEVEL;
+  }
+
+  float calculated_level = MAX_LEVEL / (MAX_INPUT_LEVEL / level);
+  return (uint8_t) roundf(calculated_level);
+}
+
+uint8_t TM1651Display::calculate_brightness(uint8_t new_brightness) {
+  if (new_brightness <= 1) {
+    return 0;
+  } else if (new_brightness == 2) {
+    return 2;
+  } else if (new_brightness >= 3) {
+    return 7;
+  }
+
+  return 0;
+}
+
+}  // namespace tm1651
+}  // namespace esphome
---
+++ esphome/components/tm1651/tm1651.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/esphal.h"
+
+#include <TM1651.h>
+
+namespace esphome {
+namespace tm1651 {
+
+class TM1651Display : public Component {
+
+public:
+  void set_clk_pin(GPIOPin *pin) { clk_pin_ = pin; }
+  void set_dio_pin(GPIOPin *pin) { dio_pin_ = pin; }
+
+  void setup() override;
+  void dump_config() override;
+
+  void set_level(float);
+  void set_brightness(uint8_t);
+
+  uint8_t calculate_level(float);
+  uint8_t calculate_brightness(uint8_t);
+
+  void repaint();
+
+protected:
+  TM1651 *batteryDisplay;
+  GPIOPin *clk_pin_;
+  GPIOPin *dio_pin_;
+
+  uint8_t brightness;
+  uint8_t level;
+};
+
+}  // namespace tm1651
+}  // namespace esphome
