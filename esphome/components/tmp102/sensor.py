507a7b14d740d10e7274a94732c469db1ea2c548
Tim Savage <tim@savage.company>
2019-12-29 17:20:22 +1100

    Moved docstring to component root

--- esphome/components/tmp102/sensor.py
+++ esphome/components/tmp102/sensor.py
@@ -1,12 +1,3 @@
-"""
-The TMP102 is a two-wire, serial output temperature
-sensor available in a tiny SOT563 package. Requiring
-no external components, the TMP102 is capable of
-reading temperatures to a resolution of 0.0625°C.
-
-https://www.sparkfun.com/datasheets/Sensors/Temperature/tmp102.pdf
-
-"""
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
