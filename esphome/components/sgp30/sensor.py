83f2a33d8aa76ded074e98b3ddf83a85e9012672
Panuruj Khambanonda <pk@panurujk.com>
2019-12-30 03:09:57 -0800

    Split the SGP30 baseline into 2 values
    - According to the SGP30 datasheet, each eCO2 and TVOC baseline is a 2-byte value (MSB first)
    - The current implementation ignores the MSB of each of the value
    - Update the schema to allow 2 different baseline values (optional, but both need to be specified for the baseline to apply)

--- esphome/components/sgp30/sensor.py
+++ esphome/components/sgp30/sensor.py
@@ -11,7 +11,8 @@ SGP30Component = sgp30_ns.class_('SGP30Component', cg.PollingComponent, i2c.I2CD
 
 CONF_ECO2 = 'eco2'
 CONF_TVOC = 'tvoc'
-CONF_BASELINE = 'baseline'
+CONF_ECO2_BASELINE = 'eco2_baseline'
+CONF_TVOC_BASELINE = 'tvoc_baseline'
 CONF_UPTIME = 'uptime'
 CONF_COMPENSATION = 'compensation'
 CONF_HUMIDITY_SOURCE = 'humidity_source'
@@ -22,7 +23,8 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_ECO2): sensor.sensor_schema(UNIT_PARTS_PER_MILLION,
                                                  ICON_PERIODIC_TABLE_CO2, 0),
     cv.Required(CONF_TVOC): sensor.sensor_schema(UNIT_PARTS_PER_BILLION, ICON_RADIATOR, 0),
-    cv.Optional(CONF_BASELINE): cv.hex_uint16_t,
+    cv.Optional(CONF_ECO2_BASELINE): cv.hex_uint16_t,
+    cv.Optional(CONF_TVOC_BASELINE): cv.hex_uint16_t,
     cv.Optional(CONF_COMPENSATION): cv.Schema({
         cv.Required(CONF_HUMIDITY_SOURCE): cv.use_id(sensor.Sensor),
         cv.Required(CONF_TEMPERATURE_SOURCE): cv.use_id(sensor.Sensor)
@@ -43,8 +45,11 @@ def to_code(config):
         sens = yield sensor.new_sensor(config[CONF_TVOC])
         cg.add(var.set_tvoc_sensor(sens))
 
-    if CONF_BASELINE in config:
-        cg.add(var.set_baseline(config[CONF_BASELINE]))
+    if CONF_ECO2_BASELINE in config:
+        cg.add(var.set_baseline(config[CONF_ECO2_BASELINE]))
+
+    if CONF_TVOC_BASELINE in config:
+        cg.add(var.set_baseline(config[CONF_TVOC_BASELINE]))
 
     if CONF_COMPENSATION in config:
         compensation_config = config[CONF_COMPENSATION]
