ac33413cf18e3e8c27f77b4e6fdd30c37eba4459
Libor Klepáč <libor.git@klepac.cz>
2020-01-02 19:53:31 +0100

    do not import EsphomeError

--- esphome/components/web_server/__init__.py
+++ esphome/components/web_server/__init__.py
@@ -5,7 +5,7 @@ from esphome.components.web_server_base import CONF_WEB_SERVER_BASE_ID
 from esphome.const import (
     CONF_CSS_INCLUDE, CONF_CSS_URL, CONF_ID, CONF_JS_INCLUDE, CONF_JS_URL, CONF_PORT,
     CONF_AUTH, CONF_USERNAME, CONF_PASSWORD)
-from esphome.core import EsphomeError, coroutine_with_priority
+from esphome.core import coroutine_with_priority
 
 AUTO_LOAD = ['json', 'web_server_base']
 
