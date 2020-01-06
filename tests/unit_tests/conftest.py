e0179c02d4bc4b8397a70d3b725b93e79d936457
Tim Savage <tim@savage.company>
2020-01-07 07:46:11 +1100

    Updated test config so package root is discovered

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,8 +1,24 @@
+"""
+ESPHome Unittests
+~~~~~~~~~~~~~~~~~
+
+Configuration file for unit tests.
+
+If adding unit tests ensure that they are fast. Slower integration tests should
+not be part of a unit test suite.
+
+"""
+import sys
 import pytest
 
 from pathlib import Path
 
+
 here = Path(__file__).parent
+
+# Configure location of package root
+package_root = here.parent.parent
+sys.path.insert(0, package_root.as_posix())
 
 
 @pytest.fixture
@@ -11,3 +27,4 @@ def fixture_path() -> Path:
     Location of all fixture files.
     """
     return here / "fixtures"
+
