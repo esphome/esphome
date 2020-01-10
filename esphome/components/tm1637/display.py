66275ae3bfbbf93f09cc1ddf82d8d737a09a82b2
Guillermo Ruffino <glm.net@gmail.com>
2020-01-09 21:42:25 -0300

    add TM1637 support

--- esphome/components/tm1637/display.py
+++ esphome/components/tm1637/display.py
@@ -2,8 +2,9 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.components import display
-from esphome.const import CONF_DATA_PINS, CONF_ENABLE_PIN, CONF_RS_PIN, CONF_RW_PIN, CONF_ID
+from esphome.const import CONF_CLK_PIN, CONF_DIO_PIN, CONF_ID
 
+CONF_INTENSITY = 'intensity'
 
 tm1637_ns = cg.esphome_ns.namespace('tm1637')
 TM1637Display = tm1637_ns.class_('TM1637Display', cg.PollingComponent)
@@ -12,21 +13,17 @@ CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(TM1637Display),
 
     cv.Optional(CONF_INTENSITY, default=15): cv.All(cv.uint8_t, cv.Range(min=0, max=15)),
-    cv.Required(CONF_RS_PIN): pins.gpio_output_pin_schema,
-    cv.Optional(CONF_RW_PIN): pins.gpio_output_pin_schema,
+    cv.Required(CONF_CLK_PIN): pins.internal_gpio_output_pin_schema,
+    cv.Required(CONF_DIO_PIN): pins.internal_gpio_output_pin_schema,
 }).extend(cv.polling_component_schema('1s'))
 
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
     yield display.register_display(var, config)
 
-    enable = yield cg.gpio_pin_expression(config[CONF_ENABLE_PIN])
-    cg.add(var.set_enable_pin(enable))
-
-    rs = yield cg.gpio_pin_expression(config[CONF_RS_PIN])
-    cg.add(var.set_rs_pin(rs))
-
-    if CONF_RW_PIN in config:
-        rw = yield cg.gpio_pin_expression(config[CONF_RW_PIN])
-        cg.add(var.set_rw_pin(rw))
+    clk = yield cg.gpio_pin_expression(config[CONF_CLK_PIN])
+    cg.add(var.set_clk_pin(clk))
+    dio = yield cg.gpio_pin_expression(config[CONF_DIO_PIN])
+    cg.add(var.set_dio_pin(dio))
--- esphome/components/tm1637/tm1637.h
+++ esphome/components/tm1637/tm1637.h
@@ -2,46 +2,58 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/defines.h"
+#include "esphome/core/esphal.h"
+
+#ifdef USE_TIME
+#include "esphome/components/time/real_time_clock.h"
+#endif
 
 namespace esphome {
 namespace tm1637 {
 
-  //using tm1637_writer_t = std::function<void(TM1637Compontent &)>;
+class TM1637Display;
 
-  class TM1637Display : public PollingComponent {
-  public:
-    TM1637Display(GPIOPin *clk, GPIOPin *dio, uint32_t update_interval = 1000);
-    float get_setup_priority() const override;
-    //void set_writer(tm1637_writer_t &&writer);
+using tm1637_writer_t = std::function<void(TM1637Display &)>;
 
-    void setSegments(const uint8_t segments[], uint8_t length = 4, uint8_t pos = 0);
-    void setBrightness(uint8_t brightness);
-    void clear();
-    void showNumberDec(int num, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0);
-    void showNumberDecEx(int num, uint8_t dots = 0, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0);
-    void showNumberHexEx(uint16_t num, uint8_t dots = 0, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0);
-    uint8_t encodeDigit(uint8_t digit);
+class TM1637Display : public PollingComponent {
+ public:
+  void set_writer(tm1637_writer_t &&writer);
 
-  protected:
-    void setup_pins_();
+  void set_clk_pin(GPIOPin *pin) { clk_pin_ = pin; }
+  void set_dio_pin(GPIOPin *pin) { dio_pin_ = pin; }
 
-    void bitDelay();
+  float get_setup_priority() const override;
 
-    void start();
-    void stop();
+  void setSegments(const uint8_t segments[], uint8_t length = 4, uint8_t pos = 0);
+  void setBrightness(uint8_t brightness);
+  void clear();
+  void showNumberDec(int num, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0);
+  void showNumberDecEx(int num, uint8_t dots = 0, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0);
+  void showNumberHexEx(uint16_t num, uint8_t dots = 0, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0);
+  uint8_t encodeDigit(uint8_t digit);
 
-    bool writeByte(uint8_t b);
+ protected:
+  void setup_pins_();
 
-    void showDots(uint8_t dots, uint8_t* digits);
-    void showNumberBaseEx(int8_t base, uint16_t num, uint8_t dots = 0, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0);
-    
-    GPIOPin *dio_pin_;
-    GPIOPin *clk_pin_;
+  void bitDelay();
 
-  private:
-    uint8_t m_brightness;
-    unsigned int m_bitDelay; 
-  };
+  void start();
+  void stop();
+
+  bool writeByte(uint8_t b);
+
+  void showDots(uint8_t dots, uint8_t *digits);
+  void showNumberBaseEx(int8_t base, uint16_t num, uint8_t dots = 0, bool leading_zero = false, uint8_t length = 4,
+                        uint8_t pos = 0);
+
+  GPIOPin *dio_pin_;
+  GPIOPin *clk_pin_;
+
+ protected:
+  uint8_t m_brightness;
+  unsigned int m_bitDelay;
+  optional<tm1637_writer_t> writer_{};
+};
 
 }  // namespace tm1637
 }  // namespace esphome
