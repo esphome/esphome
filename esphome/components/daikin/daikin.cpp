d2fae972407e0468d300745ce8b8aed4b4699ea4
Héctor Giménez <hector.fwbz@gmail.com>
2020-01-20 22:34:59 +0100

    Daikin ARC43XXX IR remote controller support

---
+++ esphome/components/daikin/daikin.cpp
@@ -0,0 +1,130 @@
+#include "daikin.h"
+#include "esphome/components/remote_base/remote_base.h"
+
+namespace esphome {
+namespace daikin {
+
+static const char *TAG = "daikin.climate";
+
+void DaikinClimate::transmit_state() {
+  uint8_t remote_state[35] = {
+    0x11, 0xDA, 0x27, 0x00, 0xC5, 0x00, 0x00, 0xD7,
+    0x11, 0xDA, 0x27, 0x00, 0x42, 0x49, 0x05, 0xA2,
+    0x11, 0xDA, 0x27, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06, 0x60, 0x00, 0x00, 0xC0, 0x00, 0x00, 0x00
+  };
+
+  remote_state[21] = this->operation_mode_();
+  remote_state[24] = this->fan_speed_();
+  remote_state[22] = this->temperature_();
+
+  // Calculate checksum
+  for (int i=16; i<34; i++) {
+    remote_state[34] += remote_state[i];
+  }
+  
+  auto transmit = this->transmitter_->transmit();
+  auto data = transmit.get_data();
+  data->set_carrier_frequency(DAIKIN_IR_FREQUENCY);
+  
+  data->mark(DAIKIN_HEADER_MARK);
+  data->space(DAIKIN_HEADER_SPACE);
+  for (int i=0; i<8; i++) {
+    for (uint8_t mask = 1; mask > 0; mask <<= 1) {  // iterate through bit mask
+      data->mark(DAIKIN_BIT_MARK);
+      bool bit = remote_state[i] & mask;
+      data->space(bit ? DAIKIN_ONE_SPACE : DAIKIN_ZERO_SPACE);
+    }
+  }
+  data->mark(DAIKIN_BIT_MARK);
+  data->space(DAIKIN_MESSAGE_SPACE);
+  data->mark(DAIKIN_HEADER_MARK);
+  data->space(DAIKIN_HEADER_SPACE);
+
+  for (int i=8; i<16; i++) {
+    for (uint8_t mask = 1; mask > 0; mask <<= 1) {  // iterate through bit mask
+      data->mark(DAIKIN_BIT_MARK);
+      bool bit = remote_state[i] & mask;
+      data->space(bit ? DAIKIN_ONE_SPACE : DAIKIN_ZERO_SPACE);
+    }
+  }
+  data->mark(DAIKIN_BIT_MARK);
+  data->space(DAIKIN_MESSAGE_SPACE);
+  data->mark(DAIKIN_HEADER_MARK);
+  data->space(DAIKIN_HEADER_SPACE);
+
+  for (int i=16; i<35; i++) {
+    for (uint8_t mask = 1; mask > 0; mask <<= 1) {  // iterate through bit mask
+      data->mark(DAIKIN_BIT_MARK);
+      bool bit = remote_state[i] & mask;
+      data->space(bit ? DAIKIN_ONE_SPACE : DAIKIN_ZERO_SPACE);
+    }
+  }
+  data->mark(DAIKIN_BIT_MARK);
+  data->space(0);
+
+  transmit.perform();
+}
+
+uint8_t DaikinClimate::operation_mode_() {
+  uint8_t operating_mode = DAIKIN_MODE_ON;
+  switch (this->mode) {
+    case climate::CLIMATE_MODE_COOL:
+      operating_mode |= DAIKIN_MODE_COOL;
+      break;
+    case climate::CLIMATE_MODE_DRY:
+      operating_mode |= DAIKIN_MODE_DRY;
+      break;
+    case climate::CLIMATE_MODE_HEAT:
+      operating_mode |= DAIKIN_MODE_HEAT;
+      break;
+    case climate::CLIMATE_MODE_AUTO:
+      operating_mode |= DAIKIN_MODE_AUTO;
+      break;
+    case climate::CLIMATE_MODE_FAN_ONLY:
+      operating_mode |= DAIKIN_MODE_FAN;
+      break;
+    case climate::CLIMATE_MODE_OFF:
+    default:
+      operating_mode = DAIKIN_MODE_OFF;
+      break;
+  }
+
+  return operating_mode;
+}
+
+uint8_t DaikinClimate::fan_speed_() {
+  uint8_t fan_speed;
+  switch(this->fan_mode) {
+    case climate::CLIMATE_FAN_LOW:
+      fan_speed = DAIKIN_FAN_1;
+      break;
+    case climate::CLIMATE_FAN_MEDIUM:
+      fan_speed = DAIKIN_FAN_3;
+      break;
+    case climate::CLIMATE_FAN_HIGH:
+      fan_speed = DAIKIN_FAN_5;
+      break;
+    case climate::CLIMATE_FAN_AUTO:
+    default:
+      fan_speed = DAIKIN_FAN_AUTO;
+  }
+  
+  // If swing is enabled switch first 4 bits to 1111
+  return this->swing_mode == climate::CLIMATE_SWING_VERTICAL ? fan_speed | 0xF : fan_speed;
+}
+
+uint8_t DaikinClimate::temperature_() {
+  // Force special temperatures depending on the mode
+  switch (this->mode) {
+    case climate::CLIMATE_MODE_FAN_ONLY:
+      return 25;
+    case climate::CLIMATE_MODE_DRY:
+      return 0xc0;
+    default:
+      uint8_t temperature = this->target_temperature;
+      return temperature << 1;
+  }
+}
+
+}  // namespace daikin
+}  // namespace esphome
