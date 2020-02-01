241c12fd3dc83e3f0c62b982d6385691a9660b50
Martin Manzanelli <m_manzanelli@hotmail.com>
2020-01-31 22:11:04 -0300

    receiver implemented

--- esphome/components/whirlpool/whirlpool.h
+++ esphome/components/whirlpool/whirlpool.h
@@ -6,8 +6,8 @@ namespace esphome {
 namespace whirlpool {
 
 // Temperature
-const float WHIRLPOOL_TEMP_MAX = 30.0;
-const float WHIRLPOOL_TEMP_MIN = 16.0;
+const float WHIRLPOOL_TEMP_MAX = 32.0;
+const float WHIRLPOOL_TEMP_MIN = 18.0;
 
 class WhirlpoolClimate : public climate_ir::ClimateIR {
  public:
@@ -23,14 +23,27 @@ class WhirlpoolClimate : public climate_ir::ClimateIR {
     this->powered_on_assumed_ = this->mode != climate::CLIMATE_MODE_OFF;
   }
 
+  /// Override control to change settings of the climate device.
+  void control(const climate::ClimateCall &call) override {
+    send_swing_cmd_ = call.get_swing_mode().has_value();
+    
+    // swing resets after unit powered off
+/*     if (call.get_mode().has_value() && *call.get_mode() == climate::CLIMATE_MODE_OFF)
+      this->swing_mode = climate::CLIMATE_SWING_OFF; */
+    climate_ir::ClimateIR::control(call);
+  }
+
+
  protected:
   /// Transmit via IR the state of this climate controller.
   void transmit_state() override;
   /// Handle received IR Buffer
-  // bool on_receive(remote_base::RemoteReceiveData data) override;
+  bool on_receive(remote_base::RemoteReceiveData data) override;
 
   // used to track when to send the power toggle command
   bool powered_on_assumed_;
+
+   bool send_swing_cmd_{false};
 };
 
 }  // namespace whirlpool
