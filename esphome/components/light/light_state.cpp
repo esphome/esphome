358b9eca21c8925088462008983ce7b65e27e9e5
Paul Quinn <peq@peq.me.uk>
2020-05-01 15:21:32 +0100

    Update light_state.cpp

--- esphome/components/light/light_state.cpp
+++ esphome/components/light/light_state.cpp
@@ -401,10 +401,10 @@ LightColorValues LightCall::validate_() {
       this->blue_ = optional<float>(1.0f);
     }
     // make white values binary aka 0.0f or 1.0f...this allows brightness to do its job
-  	if (*this->white_ > 0.0f) {
-  		this->white_ = optional<float>(1.0f);
-  	} else {
-  		this->white_ = optional<float>(0.0f);
+    if (*this->white_ > 0.0f) {
+      this->white_ = optional<float>(1.0f);
+    } else {
+      this->white_ = optional<float>(0.0f);
     }
   }
   // White to 0% if (exclusively) setting any RGB value
@@ -418,15 +418,15 @@ LightColorValues LightCall::validate_() {
     // if setting Kelvin from color (i.e. switching to white light), set White to 100%
     auto cv = this->parent_->remote_values;
     bool was_color = cv.get_red() != 1.0f || cv.get_blue() != 1.0f || cv.get_green() != 1.0f;
-  	//bool now_white = *this->red_ == 1.0f && *this->blue_ == 1.0f && *this->green_ == 1.0f;
-  	if(cv.get_white() < 1.0f){
-  		this->white_ = optional<float>(1.0f);    
-  	}
+    //bool now_white = *this->red_ == 1.0f && *this->blue_ == 1.0f && *this->green_ == 1.0f;
+    if(cv.get_white() < 1.0f){
+      this->white_ = optional<float>(1.0f);    
+    }
 
-  	if ( was_color && !this->red_.has_value() && !this->green_.has_value() && !this->blue_.has_value()) {
-    	this->red_ = optional<float>(1.0f);
-    	this->green_ = optional<float>(1.0f);
-      this->blue_ = optional<float>(1.0f);           
+    if ( was_color && !this->red_.has_value() && !this->green_.has_value() && !this->blue_.has_value()) {
+      this->red_ = optional<float>(1.0f);
+      this->green_ = optional<float>(1.0f);
+      this->blue_ = optional<float>(1.0f);     
     }
   }
 
