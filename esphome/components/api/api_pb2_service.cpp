450ec728ac2cec80ba2b279253884bf28c7098ed
Guillermo Ruffino <glm.net@gmail.com>
2020-04-30 21:40:50 -0300

    removed set_nodelay code

--- esphome/components/api/api_pb2_service.cpp
+++ esphome/components/api/api_pb2_service.cpp
@@ -10,69 +10,57 @@ static const char *TAG = "api.service";
 
 bool APIServerConnectionBase::send_hello_response(const HelloResponse &msg) {
   ESP_LOGVV(TAG, "send_hello_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<HelloResponse>(msg, 2);
 }
 bool APIServerConnectionBase::send_connect_response(const ConnectResponse &msg) {
   ESP_LOGVV(TAG, "send_connect_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<ConnectResponse>(msg, 4);
 }
 bool APIServerConnectionBase::send_disconnect_request(const DisconnectRequest &msg) {
   ESP_LOGVV(TAG, "send_disconnect_request: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<DisconnectRequest>(msg, 5);
 }
 bool APIServerConnectionBase::send_disconnect_response(const DisconnectResponse &msg) {
   ESP_LOGVV(TAG, "send_disconnect_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<DisconnectResponse>(msg, 6);
 }
 bool APIServerConnectionBase::send_ping_request(const PingRequest &msg) {
   ESP_LOGVV(TAG, "send_ping_request: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<PingRequest>(msg, 7);
 }
 bool APIServerConnectionBase::send_ping_response(const PingResponse &msg) {
   ESP_LOGVV(TAG, "send_ping_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<PingResponse>(msg, 8);
 }
 bool APIServerConnectionBase::send_device_info_response(const DeviceInfoResponse &msg) {
   ESP_LOGVV(TAG, "send_device_info_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<DeviceInfoResponse>(msg, 10);
 }
 bool APIServerConnectionBase::send_list_entities_done_response(const ListEntitiesDoneResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_done_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<ListEntitiesDoneResponse>(msg, 19);
 }
 #ifdef USE_BINARY_SENSOR
 bool APIServerConnectionBase::send_list_entities_binary_sensor_response(const ListEntitiesBinarySensorResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_binary_sensor_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesBinarySensorResponse>(msg, 12);
 }
 #endif
 #ifdef USE_BINARY_SENSOR
 bool APIServerConnectionBase::send_binary_sensor_state_response(const BinarySensorStateResponse &msg) {
   ESP_LOGVV(TAG, "send_binary_sensor_state_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<BinarySensorStateResponse>(msg, 21);
 }
 #endif
 #ifdef USE_COVER
 bool APIServerConnectionBase::send_list_entities_cover_response(const ListEntitiesCoverResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_cover_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesCoverResponse>(msg, 13);
 }
 #endif
 #ifdef USE_COVER
 bool APIServerConnectionBase::send_cover_state_response(const CoverStateResponse &msg) {
   ESP_LOGVV(TAG, "send_cover_state_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<CoverStateResponse>(msg, 22);
 }
 #endif
@@ -81,14 +69,12 @@ bool APIServerConnectionBase::send_cover_state_response(const CoverStateResponse
 #ifdef USE_FAN
 bool APIServerConnectionBase::send_list_entities_fan_response(const ListEntitiesFanResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_fan_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesFanResponse>(msg, 14);
 }
 #endif
 #ifdef USE_FAN
 bool APIServerConnectionBase::send_fan_state_response(const FanStateResponse &msg) {
   ESP_LOGVV(TAG, "send_fan_state_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<FanStateResponse>(msg, 23);
 }
 #endif
@@ -97,14 +83,12 @@ bool APIServerConnectionBase::send_fan_state_response(const FanStateResponse &ms
 #ifdef USE_LIGHT
 bool APIServerConnectionBase::send_list_entities_light_response(const ListEntitiesLightResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_light_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesLightResponse>(msg, 15);
 }
 #endif
 #ifdef USE_LIGHT
 bool APIServerConnectionBase::send_light_state_response(const LightStateResponse &msg) {
   ESP_LOGVV(TAG, "send_light_state_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<LightStateResponse>(msg, 24);
 }
 #endif
@@ -113,28 +97,24 @@ bool APIServerConnectionBase::send_light_state_response(const LightStateResponse
 #ifdef USE_SENSOR
 bool APIServerConnectionBase::send_list_entities_sensor_response(const ListEntitiesSensorResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_sensor_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesSensorResponse>(msg, 16);
 }
 #endif
 #ifdef USE_SENSOR
 bool APIServerConnectionBase::send_sensor_state_response(const SensorStateResponse &msg) {
   ESP_LOGVV(TAG, "send_sensor_state_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<SensorStateResponse>(msg, 25);
 }
 #endif
 #ifdef USE_SWITCH
 bool APIServerConnectionBase::send_list_entities_switch_response(const ListEntitiesSwitchResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_switch_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesSwitchResponse>(msg, 17);
 }
 #endif
 #ifdef USE_SWITCH
 bool APIServerConnectionBase::send_switch_state_response(const SwitchStateResponse &msg) {
   ESP_LOGVV(TAG, "send_switch_state_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<SwitchStateResponse>(msg, 26);
 }
 #endif
@@ -143,58 +123,48 @@ bool APIServerConnectionBase::send_switch_state_response(const SwitchStateRespon
 #ifdef USE_TEXT_SENSOR
 bool APIServerConnectionBase::send_list_entities_text_sensor_response(const ListEntitiesTextSensorResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_text_sensor_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesTextSensorResponse>(msg, 18);
 }
 #endif
 #ifdef USE_TEXT_SENSOR
 bool APIServerConnectionBase::send_text_sensor_state_response(const TextSensorStateResponse &msg) {
   ESP_LOGVV(TAG, "send_text_sensor_state_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<TextSensorStateResponse>(msg, 27);
 }
 #endif
 bool APIServerConnectionBase::send_subscribe_logs_response(const SubscribeLogsResponse &msg) {
-  this->set_nodelay(false);
   return this->send_message_<SubscribeLogsResponse>(msg, 29);
 }
 bool APIServerConnectionBase::send_homeassistant_service_response(const HomeassistantServiceResponse &msg) {
   ESP_LOGVV(TAG, "send_homeassistant_service_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<HomeassistantServiceResponse>(msg, 35);
 }
 bool APIServerConnectionBase::send_subscribe_home_assistant_state_response(
     const SubscribeHomeAssistantStateResponse &msg) {
   ESP_LOGVV(TAG, "send_subscribe_home_assistant_state_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<SubscribeHomeAssistantStateResponse>(msg, 39);
 }
 bool APIServerConnectionBase::send_get_time_request(const GetTimeRequest &msg) {
   ESP_LOGVV(TAG, "send_get_time_request: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<GetTimeRequest>(msg, 36);
 }
 bool APIServerConnectionBase::send_get_time_response(const GetTimeResponse &msg) {
   ESP_LOGVV(TAG, "send_get_time_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<GetTimeResponse>(msg, 37);
 }
 bool APIServerConnectionBase::send_list_entities_services_response(const ListEntitiesServicesResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_services_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesServicesResponse>(msg, 41);
 }
 #ifdef USE_ESP32_CAMERA
 bool APIServerConnectionBase::send_list_entities_camera_response(const ListEntitiesCameraResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_camera_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesCameraResponse>(msg, 43);
 }
 #endif
 #ifdef USE_ESP32_CAMERA
 bool APIServerConnectionBase::send_camera_image_response(const CameraImageResponse &msg) {
   ESP_LOGVV(TAG, "send_camera_image_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<CameraImageResponse>(msg, 44);
 }
 #endif
@@ -203,14 +173,12 @@ bool APIServerConnectionBase::send_camera_image_response(const CameraImageRespon
 #ifdef USE_CLIMATE
 bool APIServerConnectionBase::send_list_entities_climate_response(const ListEntitiesClimateResponse &msg) {
   ESP_LOGVV(TAG, "send_list_entities_climate_response: %s", msg.dump().c_str());
-  this->set_nodelay(false);
   return this->send_message_<ListEntitiesClimateResponse>(msg, 46);
 }
 #endif
 #ifdef USE_CLIMATE
 bool APIServerConnectionBase::send_climate_state_response(const ClimateStateResponse &msg) {
   ESP_LOGVV(TAG, "send_climate_state_response: %s", msg.dump().c_str());
-  this->set_nodelay(true);
   return this->send_message_<ClimateStateResponse>(msg, 47);
 }
 #endif
