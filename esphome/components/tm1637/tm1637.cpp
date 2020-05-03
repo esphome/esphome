08d1b276bc707d5e2c61557240f7037462f25dd4
nepozs <56661567+nepozs@users.noreply.github.com>
2020-05-03 03:57:26 +0200

    Update tm1637.cpp
    Defined degree symbol.

--- esphome/components/tm1637/tm1637.cpp
+++ esphome/components/tm1637/tm1637.cpp
@@ -115,6 +115,7 @@ const uint8_t TM1637_ASCII_TO_RAW[94] PROGMEM = {
     0b00110001,           // '{', ord 0x7B
     0b00000110,           // '|', ord 0x7C
     0b00000111,           // '}', ord 0x7D
+    0b01100011,           // '~', ord 0x7E (degree symbol)
 };
 void TM1637Display::setup() {
   ESP_LOGCONFIG(TAG, "Setting up TM1637...");
