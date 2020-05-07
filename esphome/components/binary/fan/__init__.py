99e8661ef5560ea9ad9d06ba0258bfc9107724f6
Jim Persson <jim@nurd.se>
2020-05-07 18:40:09 +0200

    Fix python format

--- esphome/components/binary/fan/__init__.py
+++ esphome/components/binary/fan/__init__.py
@@ -1,7 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import fan, output
-from esphome.const import CONF_OSCILLATION_OUTPUT, CONF_OUTPUT, CONF_OUTPUT_ID, CONF_DIRECTION_OUTPUT
+from esphome.const import CONF_OSCILLATION_OUTPUT, CONF_DIRECTION_OUTPUT, \
+    CONF_OUTPUT, CONF_OUTPUT_ID
 from .. import binary_ns
 
 BinaryFan = binary_ns.class_('BinaryFan', cg.Component)
