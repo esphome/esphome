4bb3d559a96f94df65736ea224dc9c3292f319b8
Senex Crenshaw <senexcrenshaw@gmail.com>
2020-05-12 18:06:57 -0400

    Travis Build Fix

--- esphome/components/mcp3008/__init__.py
+++ esphome/components/mcp3008/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import spi, sensor
+from esphome.components import spi
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['spi']
