42161a6047f7359e1c5d173ff83a3c0bdf11bf4d
kbx81 <kbx81x@gmail.com>
2020-05-14 00:29:53 -0500

    Added support for all climate actions and modes

--- esphome/components/bang_bang/bang_bang_climate.h
+++ esphome/components/bang_bang/bang_bang_climate.h
@@ -11,10 +11,11 @@ namespace bang_bang {
 struct BangBangClimateTargetTempConfig {
  public:
   BangBangClimateTargetTempConfig();
-  BangBangClimateTargetTempConfig(float default_temperature_low, float default_temperature_high);
+  BangBangClimateTargetTempConfig(float default_temperature_low, float default_temperature_high, float hysteresis);
 
   float default_temperature_low{NAN};
   float default_temperature_high{NAN};
+  float hysteresis{NAN};
 };
 
 class BangBangClimate : public climate::Climate, public Component {
@@ -25,6 +26,8 @@ class BangBangClimate : public climate::Climate, public Component {
 
   void set_sensor(sensor::Sensor *sensor);
   void set_supports_cool(bool supports_cool);
+  void set_supports_dry(bool supports_dry);
+  void set_supports_fan_only(bool supports_fan_only);
   void set_supports_heat(bool supports_heat);
   void set_supports_fan_mode_on(bool supports_fan_mode_on);
   void set_supports_fan_mode_off(bool supports_fan_mode_off);
@@ -35,12 +38,25 @@ class BangBangClimate : public climate::Climate, public Component {
   void set_supports_fan_mode_middle(bool supports_fan_mode_middle);
   void set_supports_fan_mode_focus(bool supports_fan_mode_focus);
   void set_supports_fan_mode_diffuse(bool supports_fan_mode_diffuse);
+  void set_supports_swing_mode_both(bool supports_swing_mode_both);
+  void set_supports_swing_mode_horizontal(bool supports_swing_mode_horizontal);
+  void set_supports_swing_mode_off(bool supports_swing_mode_off);
+  void set_supports_swing_mode_vertical(bool supports_swing_mode_vertical);
+
   void set_normal_config(const BangBangClimateTargetTempConfig &normal_config);
   void set_away_config(const BangBangClimateTargetTempConfig &away_config);
 
-  Trigger<> *get_idle_trigger() const;
-  Trigger<> *get_cool_trigger() const;
-  Trigger<> *get_heat_trigger() const;
+  Trigger<> *get_cool_action_trigger() const;
+  Trigger<> *get_dry_action_trigger() const;
+  Trigger<> *get_fan_only_action_trigger() const;
+  Trigger<> *get_heat_action_trigger() const;
+  Trigger<> *get_idle_action_trigger() const;
+  Trigger<> *get_auto_mode_trigger() const;
+  Trigger<> *get_cool_mode_trigger() const;
+  Trigger<> *get_dry_mode_trigger() const;
+  Trigger<> *get_fan_only_mode_trigger() const;
+  Trigger<> *get_heat_mode_trigger() const;
+  Trigger<> *get_off_mode_trigger() const;
   Trigger<> *get_fan_mode_on_trigger() const;
   Trigger<> *get_fan_mode_off_trigger() const;
   Trigger<> *get_fan_mode_auto_trigger() const;
@@ -50,6 +66,10 @@ class BangBangClimate : public climate::Climate, public Component {
   Trigger<> *get_fan_mode_middle_trigger() const;
   Trigger<> *get_fan_mode_focus_trigger() const;
   Trigger<> *get_fan_mode_diffuse_trigger() const;
+  Trigger<> *get_swing_mode_both_trigger() const;
+  Trigger<> *get_swing_mode_horizontal_trigger() const;
+  Trigger<> *get_swing_mode_off_trigger() const;
+  Trigger<> *get_swing_mode_vertical_trigger() const;
 
  protected:
   /// Override control to change settings of the climate device.
@@ -59,14 +79,20 @@ class BangBangClimate : public climate::Climate, public Component {
   /// Return the traits of this controller.
   climate::ClimateTraits traits() override;
 
-  /// Re-compute the state of this climate controller.
-  void compute_state_();
+  /// Re-compute the required action of this climate controller.
+  climate::ClimateAction compute_action_();
 
   /// Switch the climate device to the given climate action.
   void switch_to_action_(climate::ClimateAction action);
 
   /// Switch the climate device to the given climate fan mode.
   void switch_to_fan_mode_(climate::ClimateFanMode fan_mode);
+
+  /// Switch the climate device to the given climate mode.
+  void switch_to_mode_(climate::ClimateMode mode);
+
+  /// Switch the climate device to the given climate swing mode.
+  void switch_to_swing_mode_(climate::ClimateSwingMode swing_mode);
 
   /// The sensor used for getting the current temperature
   sensor::Sensor *sensor_{nullptr};
@@ -76,6 +102,8 @@ class BangBangClimate : public climate::Climate, public Component {
    * (for example a thermostat, where only heating and not-heating is possible).
    */
   bool supports_cool_{false};
+  bool supports_dry_{false};
+  bool supports_fan_only_{false};
   bool supports_heat_{false};
   /** Whether the controller supports turning on or off just the fan.
    *
@@ -100,30 +128,61 @@ class BangBangClimate : public climate::Climate, public Component {
   bool supports_fan_mode_middle_{false};
   bool supports_fan_mode_focus_{false};
   bool supports_fan_mode_diffuse_{false};
+  /** Whether the controller supports various swing modes.
+   *
+   * A false value for any given attribute means that the controller has no such swing mode.
+   */
+  bool supports_swing_mode_both_{false};
+  bool supports_swing_mode_off_{false};
+  bool supports_swing_mode_horizontal_{false};
+  bool supports_swing_mode_vertical_{false};
   /** Whether the controller supports an "away" mode
    *
    * A false value means that the controller has no such mode.
    */
   bool supports_away_{false};
-  /** The trigger to call when the controller should switch to idle mode.
-   *
-   * In idle mode, the controller is assumed to have both heating and cooling disabled.
-   */
-  Trigger<> *idle_trigger_{nullptr};
-  /** The trigger to call when the controller should switch to cooling mode.
+  /** The trigger to call when the controller should switch to cooling action/mode.
    *
    * A null value for this attribute means that the controller has no cooling action
    * For example electric heat, where only heating (power on) and not-heating
    * (power off) is possible.
    */
-  Trigger<> *cool_trigger_{nullptr};
-  /** The trigger to call when the controller should switch to heating mode.
+  Trigger<> *cool_action_trigger_{nullptr};
+  Trigger<> *cool_mode_trigger_{nullptr};
+  /** The trigger to call when the controller should switch to dry (dehumidification) mode.
+   *
+   * In dry mode, the controller is assumed to have both heating and cooling disabled,
+   * although the system may use its cooling mechanism to achieve drying.
+   */
+  Trigger<> *dry_action_trigger_{nullptr};
+  Trigger<> *dry_mode_trigger_{nullptr};
+  /** The trigger to call when the controller should switch to heating action/mode.
    *
    * A null value for this attribute means that the controller has no heating action
    * For example window blinds, where only cooling (blinds closed) and not-cooling
    * (blinds open) is possible.
    */
-  Trigger<> *heat_trigger_{nullptr};
+  Trigger<> *heat_action_trigger_{nullptr};
+  Trigger<> *heat_mode_trigger_{nullptr};
+  /** The trigger to call when the controller should switch to auto mode.
+   *
+   * In auto mode, the controller will enable heating/cooling as necessary and switch
+   * to idle when the temperature is within the thresholds/set points.
+   */
+  Trigger<> *auto_mode_trigger_{nullptr};
+  /** The trigger to call when the controller should switch to idle/off action/mode.
+   *
+   * In these actions/modes, the controller is assumed to have both heating and cooling disabled.
+   */
+  Trigger<> *idle_action_trigger_{nullptr};
+  Trigger<> *off_mode_trigger_{nullptr};
+  /** The trigger to call when the controller should switch to fan-only mode.
+   *
+   * In fan-only mode, the controller is assumed to have both heating and cooling disabled.
+   * The system should activate the fan only.
+   */
+  Trigger<> *fan_only_action_trigger_{nullptr};
+  Trigger<> *fan_only_mode_trigger_{nullptr};
   /** The trigger to call when the controller should switch on the fan.
    */
   Trigger<> *fan_mode_on_trigger_{nullptr};
@@ -151,17 +210,36 @@ class BangBangClimate : public climate::Climate, public Component {
   /** The trigger to call when the controller should switch the fan to "diffuse" position.
    */
   Trigger<> *fan_mode_diffuse_trigger_{nullptr};
+  /** The trigger to call when the controller should switch the swing mode to "both".
+   */
+  Trigger<> *swing_mode_both_trigger_{nullptr};
+  /** The trigger to call when the controller should switch the swing mode to "off".
+   */
+  Trigger<> *swing_mode_off_trigger_{nullptr};
+  /** The trigger to call when the controller should switch the swing mode to "horizontal".
+   */
+  Trigger<> *swing_mode_horizontal_trigger_{nullptr};
+  /** The trigger to call when the controller should switch the swing mode to "vertical".
+   */
+  Trigger<> *swing_mode_vertical_trigger_{nullptr};
   /** A reference to the trigger that was previously active.
    *
-   * This is so that the previous trigger can be stopped before enabling a new one.
+   * This is so that the previous trigger can be stopped before enabling a new one
+   * for each climate category (mode, action, fan_mode, swing_mode).
    */
   Trigger<> *prev_action_trigger_{nullptr};
   Trigger<> *prev_fan_mode_trigger_{nullptr};
+  Trigger<> *prev_mode_trigger_{nullptr};
+  Trigger<> *prev_swing_mode_trigger_{nullptr};
 
   climate::ClimateFanMode prev_fan_mode_{climate::CLIMATE_FAN_ON};
+  climate::ClimateMode prev_mode_{climate::CLIMATE_MODE_OFF};
+  climate::ClimateSwingMode prev_swing_mode_{climate::CLIMATE_SWING_OFF};
 
   BangBangClimateTargetTempConfig normal_config_{};
   BangBangClimateTargetTempConfig away_config_{};
+
+  float hysteresis_;
 };
 
 }  // namespace bang_bang
--- esphome/components/bang_bang/climate.py
+++ esphome/components/bang_bang/climate.py
@@ -2,12 +2,14 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import automation
 from esphome.components import climate, sensor
-from esphome.const import CONF_AWAY_CONFIG, CONF_COOL_ACTION, \
-    CONF_DEFAULT_TARGET_TEMPERATURE_HIGH, CONF_DEFAULT_TARGET_TEMPERATURE_LOW, \
-    CONF_FAN_MODE_ON_ACTION, CONF_FAN_MODE_OFF_ACTION, CONF_FAN_MODE_AUTO_ACTION, \
+from esphome.const import CONF_AUTO_MODE, CONF_AWAY_CONFIG, CONF_COOL_ACTION, CONF_COOL_MODE, \
+    CONF_DEFAULT_TARGET_TEMPERATURE_HIGH, CONF_DEFAULT_TARGET_TEMPERATURE_LOW, CONF_DRY_ACTION, \
+    CONF_DRY_MODE, CONF_FAN_MODE_ON_ACTION, CONF_FAN_MODE_OFF_ACTION, CONF_FAN_MODE_AUTO_ACTION, \
     CONF_FAN_MODE_LOW_ACTION, CONF_FAN_MODE_MEDIUM_ACTION, CONF_FAN_MODE_HIGH_ACTION, \
     CONF_FAN_MODE_MIDDLE_ACTION, CONF_FAN_MODE_FOCUS_ACTION, CONF_FAN_MODE_DIFFUSE_ACTION, \
-    CONF_HEAT_ACTION, CONF_ID, CONF_IDLE_ACTION, CONF_SENSOR
+    CONF_FAN_ONLY_ACTION, CONF_FAN_ONLY_MODE, CONF_HEAT_ACTION, CONF_HEAT_MODE, CONF_HYSTERESIS, \
+    CONF_ID, CONF_IDLE_ACTION, CONF_OFF_MODE, CONF_SENSOR, CONF_SWING_BOTH_ACTION, \
+    CONF_SWING_HORIZONTAL_ACTION, CONF_SWING_OFF_ACTION, CONF_SWING_VERTICAL_ACTION
 
 bang_bang_ns = cg.esphome_ns.namespace('bang_bang')
 BangBangClimate = bang_bang_ns.class_('BangBangClimate', climate.Climate, cg.Component)
@@ -20,7 +22,15 @@ CONFIG_SCHEMA = cv.All(climate.CLIMATE_SCHEMA.extend({
     cv.Required(CONF_DEFAULT_TARGET_TEMPERATURE_HIGH): cv.temperature,
     cv.Required(CONF_IDLE_ACTION): automation.validate_automation(single=True),
     cv.Optional(CONF_COOL_ACTION): automation.validate_automation(single=True),
+    cv.Optional(CONF_DRY_ACTION): automation.validate_automation(single=True),
+    cv.Optional(CONF_FAN_ONLY_ACTION): automation.validate_automation(single=True),
     cv.Optional(CONF_HEAT_ACTION): automation.validate_automation(single=True),
+    cv.Optional(CONF_AUTO_MODE): automation.validate_automation(single=True),
+    cv.Optional(CONF_COOL_MODE): automation.validate_automation(single=True),
+    cv.Optional(CONF_DRY_MODE): automation.validate_automation(single=True),
+    cv.Optional(CONF_FAN_ONLY_MODE): automation.validate_automation(single=True),
+    cv.Optional(CONF_HEAT_MODE): automation.validate_automation(single=True),
+    cv.Optional(CONF_OFF_MODE): automation.validate_automation(single=True),
     cv.Optional(CONF_FAN_MODE_ON_ACTION): automation.validate_automation(single=True),
     cv.Optional(CONF_FAN_MODE_OFF_ACTION): automation.validate_automation(single=True),
     cv.Optional(CONF_FAN_MODE_AUTO_ACTION): automation.validate_automation(single=True),
@@ -30,9 +40,15 @@ CONFIG_SCHEMA = cv.All(climate.CLIMATE_SCHEMA.extend({
     cv.Optional(CONF_FAN_MODE_MIDDLE_ACTION): automation.validate_automation(single=True),
     cv.Optional(CONF_FAN_MODE_FOCUS_ACTION): automation.validate_automation(single=True),
     cv.Optional(CONF_FAN_MODE_DIFFUSE_ACTION): automation.validate_automation(single=True),
+    cv.Optional(CONF_SWING_BOTH_ACTION): automation.validate_automation(single=True),
+    cv.Optional(CONF_SWING_HORIZONTAL_ACTION): automation.validate_automation(single=True),
+    cv.Optional(CONF_SWING_OFF_ACTION): automation.validate_automation(single=True),
+    cv.Optional(CONF_SWING_VERTICAL_ACTION): automation.validate_automation(single=True),
+    cv.Optional(CONF_HYSTERESIS, default=0.5): cv.temperature,
     cv.Optional(CONF_AWAY_CONFIG): cv.Schema({
         cv.Required(CONF_DEFAULT_TARGET_TEMPERATURE_LOW): cv.temperature,
         cv.Required(CONF_DEFAULT_TARGET_TEMPERATURE_HIGH): cv.temperature,
+        cv.Optional(CONF_HYSTERESIS, default=0.5): cv.temperature,
     }),
 }).extend(cv.COMPONENT_SCHEMA), cv.has_at_least_one_key(CONF_COOL_ACTION, CONF_HEAT_ACTION))
 
@@ -47,59 +63,109 @@ def to_code(config):
 
     normal_config = BangBangClimateTargetTempConfig(
         config[CONF_DEFAULT_TARGET_TEMPERATURE_LOW],
-        config[CONF_DEFAULT_TARGET_TEMPERATURE_HIGH]
+        config[CONF_DEFAULT_TARGET_TEMPERATURE_HIGH],
+        config[CONF_HYSTERESIS]
     )
     cg.add(var.set_normal_config(normal_config))
 
-    yield automation.build_automation(var.get_idle_trigger(), [], config[CONF_IDLE_ACTION])
+    yield automation.build_automation(var.get_idle_action_trigger(), [], config[CONF_IDLE_ACTION])
 
     if CONF_COOL_ACTION in config:
-        yield automation.build_automation(var.get_cool_trigger(), [], config[CONF_COOL_ACTION])
+        yield automation.build_automation(var.get_cool_action_trigger(), [],
+                                          config[CONF_COOL_ACTION])
         cg.add(var.set_supports_cool(True))
+    if CONF_DRY_ACTION in config:
+        yield automation.build_automation(var.get_dry_action_trigger(), [],
+                                          config[CONF_DRY_ACTION])
+        cg.add(var.set_supports_dry(True))
+    if CONF_FAN_ONLY_ACTION in config:
+        yield automation.build_automation(var.get_fan_only_action_trigger(), [],
+                                          config[CONF_FAN_ONLY_ACTION])
+        cg.add(var.set_supports_fan_only(True))
     if CONF_HEAT_ACTION in config:
-        yield automation.build_automation(var.get_heat_trigger(), [], config[CONF_HEAT_ACTION])
+        yield automation.build_automation(var.get_heat_action_trigger(), [],
+                                          config[CONF_HEAT_ACTION])
         cg.add(var.set_supports_heat(True))
+    if CONF_AUTO_MODE in config:
+        yield automation.build_automation(var.get_auto_mode_trigger(), [],
+                                          config[CONF_AUTO_MODE])
+    if CONF_COOL_MODE in config:
+        yield automation.build_automation(var.get_cool_mode_trigger(), [],
+                                          config[CONF_COOL_MODE])
+        cg.add(var.set_supports_cool(True))
+    if CONF_DRY_MODE in config:
+        yield automation.build_automation(var.get_dry_mode_trigger(), [],
+                                          config[CONF_DRY_MODE])
+        cg.add(var.set_supports_dry(True))
+    if CONF_FAN_ONLY_MODE in config:
+        yield automation.build_automation(var.get_fan_only_mode_trigger(), [],
+                                          config[CONF_FAN_ONLY_MODE])
+        cg.add(var.set_supports_fan_only(True))
+    if CONF_HEAT_MODE in config:
+        yield automation.build_automation(var.get_heat_mode_trigger(), [],
+                                          config[CONF_HEAT_MODE])
+        cg.add(var.set_supports_heat(True))
+    if CONF_OFF_MODE in config:
+        yield automation.build_automation(var.get_off_mode_trigger(), [],
+                                          config[CONF_OFF_MODE])
     if CONF_FAN_MODE_ON_ACTION in config:
-        yield automation.build_automation(var.get_fan_mode_on_trigger(), [], \
-            config[CONF_FAN_MODE_ON_ACTION])
+        yield automation.build_automation(var.get_fan_mode_on_trigger(), [],
+                                          config[CONF_FAN_MODE_ON_ACTION])
         cg.add(var.set_supports_fan_mode_on(True))
     if CONF_FAN_MODE_OFF_ACTION in config:
-        yield automation.build_automation(var.get_fan_mode_off_trigger(), [], \
-            config[CONF_FAN_MODE_OFF_ACTION])
+        yield automation.build_automation(var.get_fan_mode_off_trigger(), [],
+                                          config[CONF_FAN_MODE_OFF_ACTION])
         cg.add(var.set_supports_fan_mode_off(True))
     if CONF_FAN_MODE_AUTO_ACTION in config:
-        yield automation.build_automation(var.get_fan_mode_auto_trigger(), [], \
-            config[CONF_FAN_MODE_AUTO_ACTION])
+        yield automation.build_automation(var.get_fan_mode_auto_trigger(), [],
+                                          config[CONF_FAN_MODE_AUTO_ACTION])
         cg.add(var.set_supports_fan_mode_auto(True))
     if CONF_FAN_MODE_LOW_ACTION in config:
-        yield automation.build_automation(var.get_fan_mode_low_trigger(), [], \
-            config[CONF_FAN_MODE_LOW_ACTION])
+        yield automation.build_automation(var.get_fan_mode_low_trigger(), [],
+                                          config[CONF_FAN_MODE_LOW_ACTION])
         cg.add(var.set_supports_fan_mode_low(True))
     if CONF_FAN_MODE_MEDIUM_ACTION in config:
-        yield automation.build_automation(var.get_fan_mode_medium_trigger(), [], \
-            config[CONF_FAN_MODE_MEDIUM_ACTION])
+        yield automation.build_automation(var.get_fan_mode_medium_trigger(), [],
+                                          config[CONF_FAN_MODE_MEDIUM_ACTION])
         cg.add(var.set_supports_fan_mode_medium(True))
     if CONF_FAN_MODE_HIGH_ACTION in config:
-        yield automation.build_automation(var.get_fan_mode_high_trigger(), [], \
-            config[CONF_FAN_MODE_HIGH_ACTION])
+        yield automation.build_automation(var.get_fan_mode_high_trigger(), [],
+                                          config[CONF_FAN_MODE_HIGH_ACTION])
         cg.add(var.set_supports_fan_mode_high(True))
     if CONF_FAN_MODE_MIDDLE_ACTION in config:
-        yield automation.build_automation(var.get_fan_mode_middle_trigger(), [], \
-            config[CONF_FAN_MODE_MIDDLE_ACTION])
+        yield automation.build_automation(var.get_fan_mode_middle_trigger(), [],
+                                          config[CONF_FAN_MODE_MIDDLE_ACTION])
         cg.add(var.set_supports_fan_mode_middle(True))
     if CONF_FAN_MODE_FOCUS_ACTION in config:
-        yield automation.build_automation(var.get_fan_mode_focus_trigger(), [], \
-            config[CONF_FAN_MODE_FOCUS_ACTION])
+        yield automation.build_automation(var.get_fan_mode_focus_trigger(), [],
+                                          config[CONF_FAN_MODE_FOCUS_ACTION])
         cg.add(var.set_supports_fan_mode_focus(True))
     if CONF_FAN_MODE_DIFFUSE_ACTION in config:
-        yield automation.build_automation(var.get_fan_mode_diffuse_trigger(), [], \
-            config[CONF_FAN_MODE_DIFFUSE_ACTION])
+        yield automation.build_automation(var.get_fan_mode_diffuse_trigger(), [],
+                                          config[CONF_FAN_MODE_DIFFUSE_ACTION])
         cg.add(var.set_supports_fan_mode_diffuse(True))
+    if CONF_SWING_BOTH_ACTION in config:
+        yield automation.build_automation(var.get_swing_mode_both_trigger(), [],
+                                          config[CONF_SWING_BOTH_ACTION])
+        cg.add(var.set_supports_swing_mode_both(True))
+    if CONF_SWING_HORIZONTAL_ACTION in config:
+        yield automation.build_automation(var.get_swing_mode_horizontal_trigger(), [],
+                                          config[CONF_SWING_HORIZONTAL_ACTION])
+        cg.add(var.set_supports_swing_mode_horizontal(True))
+    if CONF_SWING_OFF_ACTION in config:
+        yield automation.build_automation(var.get_swing_mode_off_trigger(), [],
+                                          config[CONF_SWING_OFF_ACTION])
+        cg.add(var.set_supports_swing_mode_off(True))
+    if CONF_SWING_VERTICAL_ACTION in config:
+        yield automation.build_automation(var.get_swing_mode_vertical_trigger(), [],
+                                          config[CONF_SWING_VERTICAL_ACTION])
+        cg.add(var.set_supports_swing_mode_vertical(True))
 
     if CONF_AWAY_CONFIG in config:
         away = config[CONF_AWAY_CONFIG]
         away_config = BangBangClimateTargetTempConfig(
             away[CONF_DEFAULT_TARGET_TEMPERATURE_LOW],
-            away[CONF_DEFAULT_TARGET_TEMPERATURE_HIGH]
+            away[CONF_DEFAULT_TARGET_TEMPERATURE_HIGH],
+            away[CONF_HYSTERESIS]
         )
         cg.add(var.set_away_config(away_config))
