479ccec8ef2d50225e98ce1bb82f6fdb6a6510d2
Senex Crenshaw <senexcrenshaw@gmail.com>
2020-05-18 10:17:12 -0400

    Initial Commit - ST7735

--- esphome/components/st7735/st7735.cpp
+++ esphome/components/st7735/st7735.cpp
@@ -5,47 +5,58 @@
 namespace esphome {
 namespace st7735 {
 
-static const uint8_t ST_CMD_DELAY = 0x80;  // special signifier for command lists
+#define ST_CMD_DELAY 0x80  // special signifier for command lists
 
-static const uint8_t ST77XX_NOP = 0x00;
-static const uint8_t ST77XX_SWRESET = 0x01;
-static const uint8_t ST77XX_RDDID = 0x04;
-static const uint8_t ST77XX_RDDST = 0x09;
+#define ST77XX_NOP 0x00
+#define ST77XX_SWRESET 0x01
+#define ST77XX_RDDID 0x04
+#define ST77XX_RDDST 0x09
 
-static const uint8_t ST77XX_SLPIN = 0x10;
-static const uint8_t ST77XX_SLPOUT = 0x11;
-static const uint8_t ST77XX_PTLON = 0x12;
-static const uint8_t ST77XX_NORON = 0x13;
+#define ST77XX_SLPIN 0x10
+#define ST77XX_SLPOUT 0x11
+#define ST77XX_PTLON 0x12
+#define ST77XX_NORON 0x13
 
-static const uint8_t ST77XX_INVOFF = 0x20;
-static const uint8_t ST77XX_INVON = 0x21;
-static const uint8_t ST77XX_DISPOFF = 0x28;
-static const uint8_t ST77XX_DISPON = 0x29;
+#define ST77XX_INVOFF 0x20
+#define ST77XX_INVON 0x21
+#define ST77XX_DISPOFF 0x28
+#define ST77XX_DISPON 0x29
 #define ST77XX_CASET 0x2A
 #define ST77XX_RASET 0x2B
 #define ST77XX_RAMWR 0x2C
 #define ST77XX_RAMRD 0x2E
 
-static const uint8_t ST77XX_PTLAR = 0x30;
-static const uint8_t ST77XX_TEOFF = 0x34;
-static const uint8_t ST77XX_TEON = 0x35;
-static const uint8_t ST77XX_MADCTL = 0x36;
+#define ST77XX_PTLAR 0x30
+#define ST77XX_TEOFF 0x34
+#define ST77XX_TEON 0x35
+#define ST77XX_MADCTL 0x36
 #define ST77XX_COLMOD 0x3A
 
-static const uint8_t ST77XX_MADCTL_MY = 0x80;
-static const uint8_t ST77XX_MADCTL_MX = 0x40;
-static const uint8_t ST77XX_MADCTL_MV = 0x20;
-static const uint8_t ST77XX_MADCTL_ML = 0x10;
-static const uint8_t ST77XX_MADCTL_RGB = 0x00;
+#define ST77XX_MADCTL_MY 0x80
+#define ST77XX_MADCTL_MX 0x40
+#define ST77XX_MADCTL_MV 0x20
+#define ST77XX_MADCTL_ML 0x10
+#define ST77XX_MADCTL_RGB 0x00
 
 #define ST77XX_RDID1 0xDA
 #define ST77XX_RDID2 0xDB
 #define ST77XX_RDID3 0xDC
 #define ST77XX_RDID4 0xDD
 
+// some flags for initR() :(
+#define INITR_GREENTAB 0x00
+#define INITR_REDTAB 0x01
+#define INITR_BLACKTAB 0x02
+#define INITR_18GREENTAB INITR_GREENTAB
+#define INITR_18REDTAB INITR_REDTAB
+#define INITR_18BLACKTAB INITR_BLACKTAB
+#define INITR_144GREENTAB 0x01
+#define INITR_MINI160x80 0x04
+#define INITR_HALLOWING 0x05
+
 // Some register settings
-static const uint8_t ST7735_MADCTL_BGR = 0x08;
-static const uint8_t ST7735_MADCTL_MH = 0x04;
+#define ST7735_MADCTL_BGR 0x08
+#define ST7735_MADCTL_MH 0x04
 
 #define ST7735_FRMCTR1 0xB1
 #define ST7735_FRMCTR2 0xB2
@@ -67,7 +78,7 @@ static const uint8_t ST7735_MADCTL_MH = 0x04;
 
 // clang-format off
 static const uint8_t PROGMEM
-  BCMD[] = {                        // Init commands for 7735B screens
+  Bcmd[] = {                        // Init commands for 7735B screens
     18,                             // 18 commands in list:
     ST77XX_SWRESET,   ST_CMD_DELAY, //  1: Software reset, no args, w/delay
       50,                           //     50 ms delay
@@ -126,7 +137,7 @@ static const uint8_t PROGMEM
     ST77XX_DISPON,    ST_CMD_DELAY, // 18: Main screen turn on, no args, delay
       255 },                        //     255 = max (500 ms) delay
 
-  RCMD1[] = {                       // 7735R init, part 1 (red or green tab)
+  Rcmd1[] = {                       // 7735R init, part 1 (red or green tab)
     15,                             // 15 commands in list:
     ST77XX_SWRESET,   ST_CMD_DELAY, //  1: Software reset, 0 args, w/delay
       150,                          //     150 ms delay
@@ -163,7 +174,7 @@ static const uint8_t PROGMEM
     ST77XX_COLMOD,  1,              // 15: set color mode, 1 arg, no delay:
       0x05 },                       //     16-bit color
 
-  RCMD2GREEN[] = {                  // 7735R init, part 2 (green tab only)
+  Rcmd2green[] = {                  // 7735R init, part 2 (green tab only)
     2,                              //  2 commands in list:
     ST77XX_CASET,   4,              //  1: Column addr set, 4 args, no delay:
       0x00, 0x02,                   //     XSTART = 0
@@ -172,7 +183,7 @@ static const uint8_t PROGMEM
       0x00, 0x01,                   //     XSTART = 0
       0x00, 0x9F+0x01 },            //     XEND = 159
 
-  RCMD2RED[] = {                    // 7735R init, part 2 (red tab only)
+  Rcmd2red[] = {                    // 7735R init, part 2 (red tab only)
     2,                              //  2 commands in list:
     ST77XX_CASET,   4,              //  1: Column addr set, 4 args, no delay:
       0x00, 0x00,                   //     XSTART = 0
@@ -181,7 +192,7 @@ static const uint8_t PROGMEM
       0x00, 0x00,                   //     XSTART = 0
       0x00, 0x9F },                 //     XEND = 159
 
-  RCMD2GREEN144[] = {               // 7735R init, part 2 (green 1.44 tab)
+  Rcmd2green144[] = {               // 7735R init, part 2 (green 1.44 tab)
     2,                              //  2 commands in list:
     ST77XX_CASET,   4,              //  1: Column addr set, 4 args, no delay:
       0x00, 0x00,                   //     XSTART = 0
@@ -190,7 +201,7 @@ static const uint8_t PROGMEM
       0x00, 0x00,                   //     XSTART = 0
       0x00, 0x7F },                 //     XEND = 127
 
-  RCMD2GREEN160X80[] = {            // 7735R init, part 2 (mini 160x80)
+  Rcmd2green160x80[] = {            // 7735R init, part 2 (mini 160x80)
     2,                              //  2 commands in list:
     ST77XX_CASET,   4,              //  1: Column addr set, 4 args, no delay:
       0x00, 0x00,                   //     XSTART = 0
@@ -199,7 +210,7 @@ static const uint8_t PROGMEM
       0x00, 0x00,                   //     XSTART = 0
       0x00, 0x9F },                 //     XEND = 159
 
-  RCMD3[] = {                       // 7735R init, part 3 (red or green tab)
+  Rcmd3[] = {                       // 7735R init, part 3 (red or green tab)
     4,                              //  4 commands in list:
     ST7735_GMCTRP1, 16      ,       //  1: Gamma Adjustments (pos. polarity), 16 args + delay:
       0x02, 0x1c, 0x07, 0x12,       //     (Not entirely necessary, but provides
@@ -245,38 +256,38 @@ void ST7735::setup() {
   dump_config();
   ESP_LOGD(TAG, "  END");
 
-  display_init_(RCMD1);
+  display_init_(Rcmd1);
 
   if (this->model_ == INITR_GREENTAB) {
-    display_init_(RCMD2GREEN);
+    display_init_(Rcmd2green);
     colstart_ == 0 ? colstart_ = 2 : colstart_;
     rowstart_ == 0 ? rowstart_ = 1 : rowstart_;
   } else if ((this->model_ == INITR_144GREENTAB) || (this->model_ == INITR_HALLOWING)) {
     height_ == 0 ? height_ = ST7735_TFTHEIGHT_128 : height_;
     width_ == 0 ? width_ = ST7735_TFTWIDTH_128 : width_;
-    display_init_(RCMD2GREEN144);
+    display_init_(Rcmd2green144);
     colstart_ == 0 ? colstart_ = 2 : colstart_;
     rowstart_ == 0 ? rowstart_ = 3 : rowstart_;
-  } else if (this->model_ == INITR_MIN_I160X80) {
+  } else if (this->model_ == INITR_MINI160x80) {
     height_ == 0 ? height_ = ST7735_TFTHEIGHT_160 : height_;
     width_ == 0 ? width_ = ST7735_TFTWIDTH_80 : width_;
-    display_init_(RCMD2GREEN160X80);
+    display_init_(Rcmd2green160x80);
     colstart_ = 24;
     rowstart_ = 0;  // For default rotation 0
   } else {
     // colstart, rowstart left at default '0' values
-    display_init_(RCMD2RED);
+    display_init_(Rcmd2red);
   }
-  display_init_(RCMD3);
+  display_init_(Rcmd3);
 
   // Black tab, change MADCTL color filter
-  if ((this->model_ == INITR_BLACKTAB) || (this->model_ == INITR_MIN_I160X80)) {
+  if ((this->model_ == INITR_BLACKTAB) || (this->model_ == INITR_MINI160x80)) {
     uint8_t data = 0xC0;
     sendcommand_(ST77XX_MADCTL, &data, 1);
   }
 
-  this->init_internal_(this->get_buffer_length());
-  memset(this->buffer_, 0x00, this->get_buffer_length());
+  this->init_internal_(this->get_buffer_length_());
+  memset(this->buffer_, 0x00, this->get_buffer_length_());
 }
 
 void ST7735::update() {
@@ -288,11 +299,12 @@ int ST7735::get_height_internal() { return height_; }
 
 int ST7735::get_width_internal() { return width_; }
 
-size_t ST7735::get_buffer_length() {
+size_t ST7735::get_buffer_length_() {
   if (eightbitcolor_) {
     return size_t(this->get_width_internal()) * size_t(this->get_height_internal());
   }
   return size_t(this->get_width_internal()) * size_t(this->get_height_internal()) * 2;
+  
 }
 
 void HOT ST7735::draw_absolute_pixel_internal(int x, int y, int color) {
@@ -305,12 +317,14 @@ void HOT ST7735::draw_absolute_pixel_internal(int x, int y, int color) {
     color = ST7735_BLACK;
   }
 
+  uint16_t pos = this->eightbitcolor_ ? (x + y * this->get_width_internal())  : (x + y * this->get_width_internal()) * 2;
+
   if (this->eightbitcolor_) {
-    uint16_t pos = (x + y * this->get_width_internal());
+     uint16_t pos=(x + y * this->get_width_internal());
     this->buffer_[pos++] = (color >> 8) & 0xff;
     this->buffer_[pos] = color & 0xff;
   } else {
-    uint16_t pos = (x + y * this->get_width_internal()) * 2;
+    uint16_t pos=(x + y * this->get_width_internal()) * 2;
     this->buffer_[pos] = (color >> 8) & 0xff;
   }
 }
@@ -335,7 +349,7 @@ const char *ST7735::model_str_() {
       return "ST7735 REDTAB";
     case INITR_BLACKTAB:
       return "ST7735 BLACKTAB";
-    case INITR_MIN_I160X80:
+    case INITR_MINI160x80:
       return "ST7735 MINI160x80";
     default:
       return "Unknown";
@@ -343,17 +357,17 @@ const char *ST7735::model_str_() {
 }
 
 void ST7735::display_init_(const uint8_t *addr) {
-  uint8_t num_commands, cmd, num_args;
+  uint8_t numCommands, cmd, numArgs;
   uint16_t ms;
 
-  num_commands = pgm_read_byte(addr++);  // Number of commands to follow
-  while (num_commands--) {               // For each command...
-    cmd = pgm_read_byte(addr++);         // Read command
-    num_args = pgm_read_byte(addr++);    // Number of args to follow
-    ms = num_args & ST_CMD_DELAY;        // If hibit set, delay follows args
-    num_args &= ~ST_CMD_DELAY;           // Mask out delay bit
-    this->sendcommand_(cmd, addr, num_args);
-    addr += num_args;
+  numCommands = pgm_read_byte(addr++);  // Number of commands to follow
+  while (numCommands--) {               // For each command...
+    cmd = pgm_read_byte(addr++);        // Read command
+    numArgs = pgm_read_byte(addr++);    // Number of args to follow
+    ms = numArgs & ST_CMD_DELAY;        // If hibit set, delay follows args
+    numArgs &= ~ST_CMD_DELAY;           // Mask out delay bit
+    this->sendcommand_(cmd, addr, numArgs);
+    addr += numArgs;
 
     if (ms) {
       ms = pgm_read_byte(addr++);  // Read post-command delay time (ms)
@@ -370,7 +384,7 @@ void ST7735::dump_config() {
   LOG_PIN("  CS Pin: ", this->cs_);
   LOG_PIN("  DC Pin: ", this->dc_pin_);
   LOG_PIN("  Reset Pin: ", this->reset_pin_);
-  ESP_LOGD(TAG, "  Buffer Size: %zu", this->get_buffer_length());
+  ESP_LOGD(TAG, "  Buffer Size: %zu", this->get_buffer_length_());
   ESP_LOGD(TAG, "  Height: %d", this->height_);
   ESP_LOGD(TAG, "  Width: %d", this->width_);
   ESP_LOGD(TAG, "  ColStart: %d", this->colstart_);
@@ -393,17 +407,17 @@ void HOT ST7735::writedata_(uint8_t value) {
   this->disable();
 }
 
-void HOT ST7735::sendcommand_(uint8_t cmd, const uint8_t *data_bytes, uint8_t num_data_bytes) {
+void HOT ST7735::sendcommand_(uint8_t cmd, const uint8_t *dataBytes, uint8_t numDataBytes) {
   this->writecommand_(cmd);
-  this->senddata_(data_bytes, num_data_bytes);
+  this->senddata_(dataBytes, numDataBytes);
 }
 
-void HOT ST7735::senddata_(const uint8_t *data_bytes, uint8_t num_data_bytes) {
+void HOT ST7735::senddata_(const uint8_t *dataBytes, uint8_t numDataBytes) {
   this->dc_pin_->digital_write(true);  // pull DC high to indicate data
   this->cs_->digital_write(false);
   this->enable();
-  for (uint8_t i = 0; i < num_data_bytes; i++) {
-    this->transfer_byte(pgm_read_byte(data_bytes++));  // write byte - SPI library
+  for (uint8_t i = 0; i < numDataBytes; i++) {
+    this->transfer_byte(pgm_read_byte(dataBytes++));  // write byte - SPI library
   }
   this->cs_->digital_write(true);
   this->disable();
@@ -438,40 +452,40 @@ void HOT ST7735::write_display_data_() {
   this->dc_pin_->digital_write(true);
 
   if (this->eightbitcolor_) {
-    for (int line = 0; line < this->get_buffer_length(); line = line + this->get_width_internal()) {
+    for (int line = 0; line < this->get_buffer_length_(); line = line + this->get_width_internal()) {
       for (int index = 0; index < this->get_width_internal(); ++index) {
         this->write_byte(this->buffer_[index + line]);
-        this->write_byte((this->buffer_[index + line] << 8) + this->buffer_[index + line]);
+        this->write_byte(this->buffer_[index + line] << 8 + this->buffer_[index + line]);
       }
     }
   } else {
-    this->write_array(this->buffer_, this->get_buffer_length());
+    this->write_array(this->buffer_, this->get_buffer_length_());
   }
 
   this->disable();
 }
 
 void ST7735::spi_master_write_addr_(uint16_t addr1, uint16_t addr2) {
-  static uint8_t BYTE[4];
-  BYTE[0] = (addr1 >> 8) & 0xFF;
-  BYTE[1] = addr1 & 0xFF;
-  BYTE[2] = (addr2 >> 8) & 0xFF;
-  BYTE[3] = addr2 & 0xFF;
+  static uint8_t Byte[4];
+  Byte[0] = (addr1 >> 8) & 0xFF;
+  Byte[1] = addr1 & 0xFF;
+  Byte[2] = (addr2 >> 8) & 0xFF;
+  Byte[3] = addr2 & 0xFF;
 
   this->dc_pin_->digital_write(true);
-  this->write_array(BYTE, 4);
+  this->write_array(Byte, 4);
 }
 
 void ST7735::spi_master_write_color_(uint16_t color, uint16_t size) {
-  static uint8_t BYTE[1024];
+  static uint8_t Byte[1024];
   int index = 0;
   for (int i = 0; i < size; i++) {
-    BYTE[index++] = (color >> 8) & 0xFF;
-    BYTE[index++] = color & 0xFF;
+    Byte[index++] = (color >> 8) & 0xFF;
+    Byte[index++] = color & 0xFF;
   }
 
   this->dc_pin_->digital_write(true);
-  return write_array(BYTE, size * 2);
+  return write_array(Byte, size * 2);
 }
 
 }  // namespace st7735
