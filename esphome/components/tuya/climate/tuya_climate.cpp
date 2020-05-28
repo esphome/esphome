2e96eaaadff6722b3336035c5c190d6a04864c11
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2020-05-28 22:46:29 +1200

    Try to display accurate action based on observed behaviour.

--- esphome/components/tuya/climate/tuya_climate.cpp
+++ esphome/components/tuya/climate/tuya_climate.cpp
@@ -112,8 +112,9 @@ void TuyaClimate::compute_state_() {
     this->switch_to_action_(climate::CLIMATE_ACTION_OFF);
     return;
   }
-  const bool too_cold = this->current_temperature < this->target_temperature;
-  const bool too_hot = this->current_temperature > this->target_temperature;
+  const bool too_cold = this->current_temperature < this->target_temperature - 1;
+  const bool too_hot = this->current_temperature > this->target_temperature + 1;
+  const bool on_target = this->current_temperature == this->target_temperature;
 
   climate::ClimateAction target_action;
   if (too_cold) {
@@ -130,8 +131,10 @@ void TuyaClimate::compute_state_() {
     } else {
       target_action = climate::CLIMATE_ACTION_IDLE;
     }
-  } else {
+  } else if(on_target) {
     target_action = climate::CLIMATE_ACTION_IDLE;
+  } else {
+    target_action = this->action;
   }
   this->switch_to_action_(target_action);
 }
