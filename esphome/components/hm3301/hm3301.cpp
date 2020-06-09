a70528170c249902f87f8c205e5987f04f83a5a7
freekode <efrolov.app@gmail.com>
2020-06-09 21:41:09 +0200

    check NP for AQI sensor

--- esphome/components/hm3301/hm3301.cpp
+++ esphome/components/hm3301/hm3301.cpp
@@ -64,7 +64,8 @@ void HM3301Component::update() {
   }
 
   int8_t aqi_value = -1;
-  if (this->aqi_sensor_ != nullptr && pm_2_5_value != -1 && pm_10_0_value != -1) {
+  if (this->aqi_sensor_ != nullptr && this->pm_2_5_sensor_ != nullptr && this->pm_10_0_sensor_ != nullptr &&
+   pm_2_5_value != -1 && pm_10_0_value != -1) {
     AbstractAQICalculator *calculator = this->aqi_calculator_factory_.get_calculator(this->aqi_calc_type_);
     aqi_value = calculator->get_aqi(pm_2_5_value, pm_10_0_value);
   }
