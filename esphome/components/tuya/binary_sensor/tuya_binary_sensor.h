4fde39ccf1e0af4d141e5ff86ecfaa84de7508b5
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2020-06-10 12:48:02 +1200

    Fix copy/paste

--- esphome/components/tuya/binary_sensor/tuya_binary_sensor.h
+++ esphome/components/tuya/binary_sensor/tuya_binary_sensor.h
@@ -2,16 +2,16 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/tuya/tuya.h"
-#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
 namespace tuya {
 
-class TuyaSensor : public sensor::Sensor, public Component {
+class TuyaBinarySensor : public binary_sensor::BinarySensor, public Component {
  public:
   void setup() override;
   void dump_config() override;
-  void set_sensor_id(uint8_t switch_id) { this->sensor_id_ = sensor_id; }
+  void set_sensor_id(uint8_t sensor_id) { this->sensor_id_ = sensor_id; }
 
   void set_tuya_parent(Tuya *parent) { this->parent_ = parent; }
 
