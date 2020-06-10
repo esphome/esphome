b58729667b16dcba1d72f89d6337b9c88840386b
tiagofreire-pt <41837236+tiagofreire-pt@users.noreply.github.com>
2020-06-10 14:45:14 +0100

    Update xiaomi_ble.cpp

--- esphome/components/xiaomi_ble/xiaomi_ble.cpp
+++ esphome/components/xiaomi_ble/xiaomi_ble.cpp
@@ -123,8 +123,8 @@ bool parse_xiaomi_service_data(XiaomiParseResult &result, const esp32_ble_tracke
     ESP_LOGVV(TAG, "Xiaomi no magic bytes");
     return false;
   }
-  # bool success;
-  # XiaomiParseResult result;
+  // bool success;
+  // XiaomiParseResult result;
 
 
   result.type = XiaomiParseResult::TYPE_HHCCJCY01;
