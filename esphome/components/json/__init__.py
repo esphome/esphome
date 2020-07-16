01593c2cea6761ae119d0a4166ab5b0dc6b6803c
Otto Winter <otto@otto-winter.com>
2020-07-16 11:51:00 +0200

    Bump ArduinoJson from 5.13.3 to 6.15.2

--- esphome/components/json/__init__.py
+++ esphome/components/json/__init__.py
@@ -6,6 +6,6 @@ json_ns = cg.esphome_ns.namespace('json')
 
 @coroutine_with_priority(1.0)
 def to_code(config):
-    cg.add_library('ArduinoJson-esphomelib', '5.13.3')
+    cg.add_library('ArduinoJson-esphomelib', '6.15.2')
     cg.add_define('USE_JSON')
     cg.add_global(json_ns.using)
