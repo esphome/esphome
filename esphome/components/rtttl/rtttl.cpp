3750e3d760c2e2f2946a8eda208ff68c149cfa20
Guillermo Ruffino <glm.net@gmail.com>
2020-07-18 13:26:42 -0300

    Cleanup, add action, condition, etc.

--- esphome/components/rtttl/rtttl.cpp
+++ esphome/components/rtttl/rtttl.cpp
@@ -1,14 +1,10 @@
 #include "rtttl.h"
 #include "esphome/core/log.h"
 
-#include "../esp8266_pwm/esp8266_pwm.h"
-
 namespace esphome {
 namespace rtttl {
 
 static const char* TAG = "rtttl";
-
-static const uint8_t OCTAVE_OFFSET = 0;
 
 // These values can also be found as constants in the Tone library (Tone.h)
 static const uint16_t NOTES[] = {0,    262,  277,  294,  311,  330,  349,  370,  392,  415,  440,  466,  494,
@@ -16,7 +12,16 @@ static const uint16_t NOTES[] = {0,    262,  277,  294,  311,  330,  349,  370, 
                                  1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217,
                                  2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951};
 
-#define is_digit(n) (n >= '0' && n <= '9')
+#define SKIP_CHAR_OR_SPACE(p, char) \
+  while (*p == char || *p == ' ') \
+    p++;
+#define GET_INTEGER(p, var) \
+  var = 0; \
+  while (isdigit(*p)) { \
+    var = (var * 10) + (*p++ - '0'); \
+  }
+
+void Rtttl::dump_config() { ESP_LOGCONFIG(TAG, "Rtttl"); }
 
 void Rtttl::play(std::string rtttl) {
   // Absolutely no error checking in here
@@ -26,8 +31,8 @@ void Rtttl::play(std::string rtttl) {
 
   p_ = rtttl_.cbegin();
 
-  this->default_dur_ = 4;
-  this->default_oct_ = 6;
+  this->default_duration_ = 4;
+  this->default_octave_ = 6;
   int bpm = 63;
   int num;
 
@@ -38,52 +43,42 @@ void Rtttl::play(std::string rtttl) {
   while (*p_ != ':')
     p_++;
 
-  // skip colon and spaces
-  while (*p_ == ':' || *p_ == ' ')
-    p_++;
+  SKIP_CHAR_OR_SPACE(p_, ':')
 
   // get default duration
   if (*p_ == 'd') {
     p_++;
     p_++;  // skip "d="
-    num = 0;
-    while (is_digit(*p_)) {
-      num = (num * 10) + (*p_++ - '0');
-    }
+    GET_INTEGER(p_, num)
     if (num > 0)
-      default_dur_ = num;
-    p_++;  // skip comma
+      default_duration_ = num;
+    SKIP_CHAR_OR_SPACE(p_, ',')
   }
 
   // get default octave
   if (*p_ == 'o') {
     p_++;
     p_++;  // skip "o="
-    num = *p_++ - '0';
+    GET_INTEGER(p_, num)
     if (num >= 3 && num <= 7)
-      default_oct_ = num;
-    // skip comma and spaces
-    while (*p_ == ',' || *p_ == ' ')
-      p_++;
+      default_octave_ = num;
+    SKIP_CHAR_OR_SPACE(p_, ',')
   }
 
   // get BPM
   if (*p_ == 'b') {
     p_++;
     p_++;  // skip "b="
-    num = 0;
-    while (is_digit(*p_)) {
-      num = (num * 10) + (*p_++ - '0');
-    }
-    bpm = num;
-    // skip colon and spaces
-    while (*p_ == ':' || *p_ == ' ')
-      p_++;
+    GET_INTEGER(p_, num)
+    if (num != 0)
+      bpm = num;
+    SKIP_CHAR_OR_SPACE(p_, ':')
   }
 
   // BPM usually expresses the number of quarter notes per minute
   this->wholenote_ = (60 * 1000L / bpm) * 4;  // this is the time for whole note (in milliseconds)
 
+  output_freq_ = 0;
   note_playing_ = false;
   next_tone_play_ = millis();
 }
@@ -92,34 +87,27 @@ void Rtttl::loop() {
   if (next_tone_play_ == 0 || millis() < next_tone_play_)
     return;
 
-  if (note_playing_) {
-    // add small silence between notes, but this should be done only when repeating notes.
-    next_tone_play_ = millis() + 10;
-    output_->set_level(0);
-    note_playing_ = false;
-    return;
-  }
-
   if (p_ == rtttl_.cend()) {
     output_->set_level(0.0);
-    ESP_LOGD(TAG, "Play ended");
+    ESP_LOGD(TAG, "Playback finished");
+    this->on_finished_playback_callback_.call();
     next_tone_play_ = 0;
     return;
   }
 
   // first, get note duration, if available
-  auto num = 0;
-  while (is_digit(*p_)) {
-    num = (num * 10) + (*p_++ - '0');
-  }
+  uint8_t num;
+  GET_INTEGER(p_, num)
+
+  uint32_t duration;
 
   if (num)
-    this->duration_ = wholenote_ / num;
+    duration = wholenote_ / num;
   else
-    this->duration_ = wholenote_ / default_dur_;  // we will need to check if we are a dotted note after
+    duration = wholenote_ / default_duration_;  // we will need to check if we are a dotted note after
 
   // now get the note
-  uint8_t note = 0;
+  uint8_t note;
 
   switch (*p_) {
     case 'c':
@@ -144,7 +132,6 @@ void Rtttl::loop() {
       note = 12;
       break;
     case 'p':
-      note = 0;
     default:
       note = 0;
   }
@@ -158,39 +145,40 @@ void Rtttl::loop() {
 
   // now, get optional '.' dotted note
   if (*p_ == '.') {
-    duration_ += duration_ / 2;
+    duration += duration / 2;
     p_++;
   }
 
   // now, get scale
   uint8_t scale;
-  if (isdigit(*p_)) {
-    scale = *p_ - '0';
-    p_++;
-  } else {
-    scale = default_oct_;
-  }
+  GET_INTEGER(p_, scale)
+  if (!scale)
+    scale = default_octave_;
 
-  scale += OCTAVE_OFFSET;
-
-  if (*p_ == ',')
-    p_++;  // skip comma for next note (or we may be at the end)
+  // skip comma for next note (or we may be at the end)
+  SKIP_CHAR_OR_SPACE(p_, ',')
 
   // Now play the note
-
   if (note) {
     auto freq = NOTES[(scale - 4) * 12 + note];
 
-    ESP_LOGVV(TAG, "playing note: %d %d %d", duration_, note);
+    if (note_playing_ && freq == output_freq_) {
+      // Add small silence gap between same note
+      output_->set_level(0.0);
+      delay(10);
+    }
+    output_freq_ = freq;
+
+    ESP_LOGVV(TAG, "playing note: %d for %dms", note, duration);
     output_->update_frequency(freq);
     output_->set_level(0.5);
     note_playing_ = true;
   } else {
-    ESP_LOGVV(TAG, "waiting: %d", duration_);
+    ESP_LOGVV(TAG, "waiting: %dms", duration);
     output_->set_level(0.0);
     note_playing_ = false;
   }
-  next_tone_play_ += duration_;
+  next_tone_play_ += duration;
 }
 }  // namespace rtttl
 }  // namespace esphome
--- esphome/components/rtttl/rtttl.h
+++ esphome/components/rtttl/rtttl.h
@@ -2,7 +2,6 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
-#include "esphome/core/helpers.h"
 #include "esphome/components/output/float_output.h"
 
 namespace esphome {
@@ -14,36 +13,60 @@ class Rtttl : public Component {
  public:
   void set_output(output::FloatOutput *output) { output_ = output; }
   void play(std::string rtttl);
+  void stop() {
+    next_tone_play_ = 0;
+    output_->set_level(0.0);
+  }
+  void dump_config() override;
 
-  void setup() override {}
-  //   void dump_config() override;
-
+  bool is_playing() { return next_tone_play_ != 0; }
   void loop() override;
+
+  void add_on_finished_playback_callback(std::function<void()> callback) {
+    this->on_finished_playback_callback_.add(std::move(callback));
+  }
 
  protected:
   std::string rtttl_;
-  std::string::const_iterator p_{};
-  uint32_t duration_;
+  std::string::const_iterator p_;
   uint32_t wholenote_;
-  uint32_t default_dur_;
-  uint32_t default_oct_;
+  uint32_t default_duration_;
+  uint32_t default_octave_;
 
   uint32_t next_tone_play_{0};
-
+  uint32_t output_freq_;
   bool note_playing_;
-
   output::FloatOutput *output_;
+
+  CallbackManager<void()> on_finished_playback_callback_;
 };
 
-template<typename... Ts> class RtttlPlayAction : public Action<Ts...> {
+template<typename... Ts> class PlayAction : public Action<Ts...> {
  public:
-  RtttlPlayAction(Rtttl *rtttl) : rtttl_(rtttl) {}
+  PlayAction(Rtttl *rtttl) : rtttl_(rtttl) {}
   TEMPLATABLE_VALUE(std::string, value)
 
   void play(Ts... x) override { this->rtttl_->play(this->value_.value(x...)); }
 
  protected:
   Rtttl *rtttl_;
+};
+
+template<typename... Ts> class StopAction : public Action<Ts...>, public Parented<Rtttl> {
+ public:
+  void play(Ts... x) override { this->parent_->stop(); }
+};
+
+template<typename... Ts> class IsPlayingCondition : public Condition<Ts...>, public Parented<Rtttl> {
+ public:
+  bool check(Ts... x) override { return this->parent_->is_playing(); }
+};
+
+class FinishedPlaybackTrigger : public Trigger<> {
+ public:
+  explicit FinishedPlaybackTrigger(Rtttl *parent) {
+    parent->add_on_finished_playback_callback([this]() { this->trigger(); });
+  }
 };
 
 }  // namespace rtttl
