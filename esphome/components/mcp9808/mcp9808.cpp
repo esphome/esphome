131ae9282a0d7b549b37011227a18ecfcadd0191
Harald Nagel <dev@hpn.dev>
2020-07-17 18:59:25 -0700

    Change from component to sensor; code fixes
    - Change from component to sensor
    - Change magic numbers to constants

--- esphome/components/mcp9808/mcp9808.cpp
+++ esphome/components/mcp9808/mcp9808.cpp
@@ -8,34 +8,41 @@ static const uint8_t MCP9808_REG_AMBIENT_TEMP = 0x05;
 static const uint8_t MCP9808_REG_MANUF_ID = 0x06;
 static const uint8_t MCP9808_REG_DEVICE_ID = 0x07;
 
+static const uint16_t MCP9808_MANUF_ID = 0x0054;
+static const uint16_t MCP9808_DEV_ID = 0x0400;
+
+static const uint8_t MCP9808_AMBIENT_CLEAR_FLAGS = 0x1F;
+static const uint8_t MCP9808_AMBIENT_CLEAR_SIGN = 0x0F;
+static const uint8_t MCP9808_AMBIENT_TEMP_NEGATIVE = 0x10;
+
 static const char *TAG = "mcp9808";
 
-void MCP9808Component::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up MCP9808...");
+void MCP9808Sensor::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up %s...", this->name_.c_str());
 
   uint16_t manu;
-  if (!this->read_byte_16(MCP9808_REG_MANUF_ID, &manu, 0) || manu != 0x0054) {
+  if (!this->read_byte_16(MCP9808_REG_MANUF_ID, &manu, 0) || manu != MCP9808_MANUF_ID) {
     this->mark_failed();
-    ESP_LOGCONFIG(TAG, "MCP9808 manufacuturer id failed");
+    ESP_LOGCONFIG(TAG, "%s manufacuturer id failed, device returned %X", this->name_.c_str(), manu);
     return;
   }
   uint16_t dev_id;
-  if (!this->read_byte_16(MCP9808_REG_DEVICE_ID, &dev_id, 0) || dev_id != 0x0400) {
+  if (!this->read_byte_16(MCP9808_REG_DEVICE_ID, &dev_id, 0) || dev_id != MCP9808_DEV_ID) {
     this->mark_failed();
-    ESP_LOGCONFIG(TAG, "MCP9808 device id failed");
+    ESP_LOGCONFIG(TAG, "%s device id failed, device returned %X", this->name_.c_str(), dev_id);
     return;
   }
 }
-void MCP9808Component::dump_config() {
-  ESP_LOGCONFIG(TAG, "MCP9808:");
+void MCP9808Sensor::dump_config() {
+  ESP_LOGCONFIG(TAG, "%s:", this->name_.c_str());
   LOG_I2C_DEVICE(this);
   if (this->is_failed()) {
-    ESP_LOGE(TAG, "Communication with MCP9808 failed!");
+    ESP_LOGE(TAG, "Communication with %s failed!", this->name_.c_str());
   }
   LOG_UPDATE_INTERVAL(this);
-  LOG_SENSOR("  ", "Temperature", this->temperature_);
+  LOG_SENSOR("  ", "Temperature", this);
 }
-void MCP9808Component::update() {
+void MCP9808Sensor::update() {
   uint16_t raw_temp;
   if (!this->read_byte_16(MCP9808_REG_AMBIENT_TEMP, &raw_temp)) {
     this->status_set_warning();
@@ -46,15 +53,14 @@ void MCP9808Component::update() {
     return;
   }
 
-  // http://ww1.microchip.com/downloads/en/DeviceDoc/25095A.pdf
-
   float temp = NAN;
   uint8_t msb = (uint8_t)((raw_temp & 0xff00) >> 8);
   uint8_t lsb = raw_temp & 0x00ff;
 
-  msb = msb & 0x1F;  // clear flags
-  if ((msb & 0x10) == 0x10) {
-    msb = msb & 0x0F;  // clear sign
+  msb = msb & MCP9808_AMBIENT_CLEAR_FLAGS;
+
+  if ((msb & MCP9808_AMBIENT_TEMP_NEGATIVE) == MCP9808_AMBIENT_TEMP_NEGATIVE) {
+    msb = msb & MCP9808_AMBIENT_CLEAR_SIGN;
     temp = (256 - ((uint16_t)(msb) *16 + lsb / 16.0f)) * -1;
   } else {
     temp = (uint16_t)(msb) *16 + lsb / 16.0f;
@@ -65,12 +71,11 @@ void MCP9808Component::update() {
     return;
   }
 
-  this->temperature_->publish_state(temp);
-
-  ESP_LOGD(TAG, "Got temperature=%.1f°C", temp);
+  ESP_LOGD(TAG, "%s: Got temperature=%.1f°C", this->name_.c_str(), temp);
+  this->publish_state(temp);
   this->status_clear_warning();
 }
-float MCP9808Component::get_setup_priority() const { return setup_priority::DATA; }
+float MCP9808Sensor::get_setup_priority() const { return setup_priority::DATA; }
 
 }  // namespace mcp9808
 }  // namespace esphome
--- esphome/components/mcp9808/sensor.py
+++ esphome/components/mcp9808/sensor.py
@@ -1,16 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import CONF_ID, CONF_TEMPERATURE, ICON_THERMOMETER, UNIT_CELSIUS
+from esphome.const import CONF_ID, ICON_THERMOMETER, UNIT_CELSIUS
 
 DEPENDENCIES = ['i2c']
 
 mcp9808_ns = cg.esphome_ns.namespace('mcp9808')
-MCP9808Component = mcp9808_ns.class_('MCP9808Component', cg.PollingComponent, i2c.I2CDevice)
+MCP9808Sensor = mcp9808_ns.class_('MCP9808Sensor', sensor.Sensor, cg.PollingComponent,
+                                  i2c.I2CDevice)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MCP9808Component),
-    cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(UNIT_CELSIUS, ICON_THERMOMETER, 1),
+CONFIG_SCHEMA = sensor.sensor_schema(UNIT_CELSIUS, ICON_THERMOMETER, 1).extend({
+    cv.GenerateID(): cv.declare_id(MCP9808Sensor),
 }).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(0x18))
 
 
@@ -18,7 +18,4 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield i2c.register_i2c_device(var, config)
-
-    if CONF_TEMPERATURE in config:
-        sens = yield sensor.new_sensor(config[CONF_TEMPERATURE])
-        cg.add(var.set_temperature(sens))
+    yield sensor.register_sensor(var, config)
