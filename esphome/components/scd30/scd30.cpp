2d592cec60ab539d72f7fa55c9dd1d9de1f40d4a
Scott Gilliland <zeroping@gmail.com>
2020-08-01 17:40:59 -0400

    Added the ability to do a forced baseline calibration on the SCD30, modeled from how it is done for the MH-Z19.
    The SCD30, like the MH-Z19, can be calibated with a gas with a known CO2 value, but there wasn't a way to trigger that forced calibration. Since you can specifiy the ppm value used, that's also added as a configuration option.

--- esphome/components/scd30/scd30.cpp
+++ esphome/components/scd30/scd30.cpp
@@ -61,11 +61,10 @@ void SCD30Component::setup() {
     }
   }
 
-  if (!this->write_command_(SCD30_CMD_AUTOMATIC_SELF_CALIBRATION, enable_asc_ ? 1 : 0)) {
-    ESP_LOGE(TAG, "Sensor SCD30 error setting automatic self calibration.");
-    this->error_code_ = MEASUREMENT_INIT_FAILED;
-    this->mark_failed();
-    return;
+  if(enable_asc_) {
+    asc_enable();
+  } else {
+    asc_disable();
   }
 }
 
@@ -150,6 +149,46 @@ void SCD30Component::update() {
 
     this->status_clear_warning();
   });
+}
+
+void SCD30Component::forced_recalibration() {
+  if (enable_asc_ == true) {
+    ESP_LOGD(TAG, "SCD30 cannot perform FRC with ASC enabled, disabling");
+    asc_disable();
+  }
+  
+  ESP_LOGD(TAG, "SCD30 starting forced recalibration (FRC) with value %d ppm", frc_baseline_);
+
+  if (!this->write_command_(SCD30_CMD_FORCED_CALIBRATION, frc_baseline_)) {
+    ESP_LOGE(TAG, "Sensor SCD30 error starting continuous measurements.");
+    this->error_code_ = MEASUREMENT_INIT_FAILED;
+    this->mark_failed();
+    return;
+  }
+}
+
+void SCD30Component::asc_enable() {
+  ESP_LOGD(TAG, "SCD30 Enabling automatic calibration (ASC)");
+  
+  if (!this->write_command_(SCD30_CMD_AUTOMATIC_SELF_CALIBRATION, true)) {
+    ESP_LOGE(TAG, "Sensor SCD30 error setting automatic self calibration.");
+    this->error_code_ = MEASUREMENT_INIT_FAILED;
+    this->mark_failed();
+    return;
+  }
+  enable_asc_ = true;
+}
+
+void SCD30Component::asc_disable() {
+  ESP_LOGD(TAG, "SCD30 Disabling automatic calibration (ASC)");
+
+  if (!this->write_command_(SCD30_CMD_AUTOMATIC_SELF_CALIBRATION, false)) {
+    ESP_LOGE(TAG, "Sensor SCD30 error setting automatic self calibration.");
+    this->error_code_ = MEASUREMENT_INIT_FAILED;
+    this->mark_failed();
+    return;
+  }
+  enable_asc_ = false;
 }
 
 bool SCD30Component::write_command_(uint16_t command) {
--- esphome/components/scd30/scd30.h
+++ esphome/components/scd30/scd30.h
@@ -1,21 +1,32 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/core/automation.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace scd30 {
 
+  
+enum SDC30ASCLogic { SCD30_ASC_NONE = 0, SCD30_ASC_ENABLED, SCD30_ASC_DISABLED };
+  
 /// This class implements support for the Sensirion scd30 i2c GAS (VOC and CO2eq) sensors.
 class SCD30Component : public PollingComponent, public i2c::I2CDevice {
  public:
   void set_co2_sensor(sensor::Sensor *co2) { co2_sensor_ = co2; }
   void set_humidity_sensor(sensor::Sensor *humidity) { humidity_sensor_ = humidity; }
   void set_temperature_sensor(sensor::Sensor *temperature) { temperature_sensor_ = temperature; }
-  void set_automatic_self_calibration(bool asc) { enable_asc_ = asc; }
+  //void set_automatic_self_calibration(bool asc) { enable_asc_ = asc; }
   void set_altitude_compensation(uint16_t altitude) { altitude_compensation_ = altitude; }
 
+  void forced_recalibration();
+  void asc_enable();
+  void asc_disable();
+  
+  void set_automatic_self_calibration(bool asc_enabled) { asc_boot_logic_ = asc_enabled ? SCD30_ASC_ENABLED : SCD30_ASC_DISABLED; }
+  void set_frc_baseline(int baseline) { frc_baseline_ = baseline; }
+  
   void setup() override;
   void update() override;
   void dump_config() override;
@@ -39,7 +50,41 @@ class SCD30Component : public PollingComponent, public i2c::I2CDevice {
   sensor::Sensor *co2_sensor_{nullptr};
   sensor::Sensor *humidity_sensor_{nullptr};
   sensor::Sensor *temperature_sensor_{nullptr};
+  
+  SDC30ASCLogic asc_boot_logic_{SCD30_ASC_NONE};
+  int frc_baseline_ = 410;
 };
+
+template<typename... Ts> class SCD30ForcedRecalibrationAction : public Action<Ts...> {
+ public:
+  SCD30ForcedRecalibrationAction(SCD30Component *scd30) : scd30_(scd30) {}
+
+  void play(Ts... x) override { this->scd30_->forced_recalibration(); }
+
+ protected:
+  SCD30Component *scd30_;
+};
+
+template<typename... Ts> class SCD30ASCEnableAction : public Action<Ts...> {
+ public:
+  SCD30ASCEnableAction(SCD30Component *scd30) : scd30_(scd30) {}
+
+  void play(Ts... x) override { this->scd30_->asc_enable(); }
+
+ protected:
+  SCD30Component *scd30_;
+};
+
+template<typename... Ts> class SCD30ASCDisableAction : public Action<Ts...> {
+ public:
+  SCD30ASCDisableAction(SCD30Component *scd30) : scd30_(scd30) {}
+
+  void play(Ts... x) override { this->scd30_->asc_enable(); }
+
+ protected:
+  SCD30Component *scd30_;
+};
+
 
 }  // namespace scd30
 }  // namespace esphome
