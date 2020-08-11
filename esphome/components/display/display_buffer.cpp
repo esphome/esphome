a9d75ca4f44e832638b5087023e606daed16d5b8
Guillermo Ruffino <glm.net@gmail.com>
2020-08-11 10:28:30 -0300

    Image bit dephts (#1241)

--- esphome/components/display/display_buffer.cpp
+++ esphome/components/display/display_buffer.cpp
@@ -204,31 +204,33 @@ void DisplayBuffer::vprintf_(int x, int y, Font *font, Color color, TextAlign al
   if (ret > 0)
     this->print(x, y, font, color, align, buffer);
 }
-void DisplayBuffer::image(int x, int y, Image *image) { this->image(x, y, COLOR_ON, image); }
-void DisplayBuffer::image(int x, int y, Color color, Image *image, bool invert) {
-  if (image->get_type() == BINARY) {
-    for (int img_x = 0; img_x < image->get_width(); img_x++) {
-      for (int img_y = 0; img_y < image->get_height(); img_y++) {
-        if (invert)
-          this->draw_pixel_at(x + img_x, y + img_y, image->get_pixel(img_x, img_y) ? COLOR_OFF : color);
-        else
-          this->draw_pixel_at(x + img_x, y + img_y, image->get_pixel(img_x, img_y) ? color : COLOR_OFF);
+
+void DisplayBuffer::image(int x, int y, Image *image, Color color_on, Color color_off) {
+  switch (image->get_type()) {
+    case IMAGE_TYPE_BINARY:
+      for (int img_x = 0; img_x < image->get_width(); img_x++) {
+        for (int img_y = 0; img_y < image->get_height(); img_y++) {
+          this->draw_pixel_at(x + img_x, y + img_y, image->get_pixel(img_x, img_y) ? color_on : color_off);
+        }
       }
-    }
-  } else if (image->get_type() == GRAYSCALE) {
-    for (int img_x = 0; img_x < image->get_width(); img_x++) {
-      for (int img_y = 0; img_y < image->get_height(); img_y++) {
-        this->draw_pixel_at(x + img_x, y + img_y, image->get_grayscale_pixel(img_x, img_y));
+      break;
+    case IMAGE_TYPE_GRAYSCALE:
+      for (int img_x = 0; img_x < image->get_width(); img_x++) {
+        for (int img_y = 0; img_y < image->get_height(); img_y++) {
+          this->draw_pixel_at(x + img_x, y + img_y, image->get_grayscale_pixel(img_x, img_y));
+        }
       }
-    }
-  } else if (image->get_type() == RGB) {
-    for (int img_x = 0; img_x < image->get_width(); img_x++) {
-      for (int img_y = 0; img_y < image->get_height(); img_y++) {
-        this->draw_pixel_at(x + img_x, y + img_y, image->get_color_pixel(img_x, img_y));
+      break;
+    case IMAGE_TYPE_RGB24:
+      for (int img_x = 0; img_x < image->get_width(); img_x++) {
+        for (int img_y = 0; img_y < image->get_height(); img_y++) {
+          this->draw_pixel_at(x + img_x, y + img_y, image->get_color_pixel(img_x, img_y));
+        }
       }
-    }
+      break;
   }
 }
+
 void DisplayBuffer::get_text_bounds(int x, int y, const char *text, Font *font, TextAlign align, int *x1, int *y1,
                                     int *width, int *height) {
   int x_offset, baseline;
@@ -463,15 +465,14 @@ Color Image::get_grayscale_pixel(int x, int y) const {
   if (x < 0 || x >= this->width_ || y < 0 || y >= this->height_)
     return 0;
   const uint32_t pos = (x + y * this->width_);
-  return Color(pgm_read_byte(this->data_start_ + pos) << 24);
+  const uint8_t gray = pgm_read_byte(this->data_start_ + pos);
+  return Color(gray | gray << 8 | gray << 16 | gray << 24);
 }
 int Image::get_width() const { return this->width_; }
 int Image::get_height() const { return this->height_; }
 ImageType Image::get_type() const { return this->type_; }
-Image::Image(const uint8_t *data_start, int width, int height)
-    : width_(width), height_(height), data_start_(data_start) {}
-Image::Image(const uint8_t *data_start, int width, int height, int type)
-    : width_(width), height_(height), type_((ImageType) type), data_start_(data_start) {}
+Image::Image(const uint8_t *data_start, int width, int height, ImageType type)
+    : width_(width), height_(height), type_(type), data_start_(data_start) {}
 
 DisplayPage::DisplayPage(const display_writer_t &writer) : writer_(writer) {}
 void DisplayPage::show() { this->parent_->show_page(this); }
