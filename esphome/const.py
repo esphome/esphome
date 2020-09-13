52c4dd0e356bf4ffc090874b28b96c6315d59ffa
Guillermo Ruffino <glm.net@gmail.com>
2020-09-13 14:18:32 -0300

    Bump version to v1.15.0

--- esphome/const.py
+++ esphome/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 1
 MINOR_VERSION = 15
-PATCH_VERSION = '0b4'
+PATCH_VERSION = '0'
 __short_version__ = f'{MAJOR_VERSION}.{MINOR_VERSION}'
 __version__ = f'{__short_version__}.{PATCH_VERSION}'
 
