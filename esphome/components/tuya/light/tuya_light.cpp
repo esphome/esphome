f018e3d7df1d85a4b3057c639f436fdd1e4b8e7e
Adam Liddell <git@aliddell.com>
2020-09-19 22:33:12 +0000

    Support dimming to full range and using gamma correction

--- esphome/components/tuya/light/tuya_light.cpp
+++ esphome/components/tuya/light/tuya_light.cpp
@@ -15,16 +15,29 @@ void TuyaLight::setup() {
       auto lower = std::min(this->min_value_, this->max_value_);
       auto upper = std::max(this->min_value_, this->max_value_);
       auto value = std::min(upper, std::max(lower, static_cast<int32_t>(datapoint.value_uint)));
+      float brightness = float(value - this->min_value_) / (this->max_value_ - this->min_value_);  // Don't use lower/upper here to allow inversion
+      brightness = powf(brightness, 1.0 / this->state_->get_gamma_correct()); // Apply inverse gamma correction
 
+      // Handle case where reported value is <= lower bound but not
+      // zero, but we don't want light to appear off by setting
+      // brightness = 0.0.
+      // This can occur when we sent a value near the lower bound
+      // and the returned value is not exactly what we set.
+      if (lower > 0 && brightness == 0.0f) {
+        brightness = 1.0 / (upper - lower);
+      }
+
+      ESP_LOGV(TAG, "Received brightness: %f %d", brightness, value);
       auto call = this->state_->make_call();
-      call.set_brightness(float(value - this->min_value_) / (this->max_value_ - this->min_value_)); // Don't use lower/upper here to allow inversion
+      call.set_brightness(brightness);
       call.perform();
     });
   }
+
   if (switch_id_.has_value()) {
     this->parent_->register_listener(*this->switch_id_, [this](TuyaDatapoint datapoint) {
       this->inhibit_next_send_ = true;
-
+      ESP_LOGV(TAG, "Received switch: %d", datapoint.value_bool);
       auto call = this->state_->make_call();
       call.set_state(datapoint.value_bool);
       call.perform();
@@ -57,22 +70,25 @@ void TuyaLight::write_state(light::LightState *state) {
 
   float brightness;
   state->current_values_as_brightness(&brightness);
-  auto brightness_int = static_cast<uint32_t>(brightness * (this->max_value_ - this->min_value_) + this->min_value_);
-  bool is_off = brightness == 0.0f;
+  bool is_on = brightness != 0.0f;
 
   if (this->dimmer_id_.has_value()) {
+    uint32_t brightness_int = std::ceil(brightness * (this->max_value_ - this->min_value_) + this->min_value_);
+    ESP_LOGV(TAG, "Setting brightness: %f %d", brightness, brightness_int);
+
     TuyaDatapoint datapoint{};
     datapoint.id = *this->dimmer_id_;
     datapoint.type = TuyaDatapointType::INTEGER;
-    datapoint.value_int = is_off ? 0 : brightness_int; // Ensure turn off even when min value is set
+    datapoint.value_int = is_on ? brightness_int : 0; // Ensure turn off even when min value is set
     parent_->set_datapoint_value(datapoint);
   }
 
   if (this->switch_id_.has_value()) {
+    ESP_LOGV(TAG, "Setting switch: %d", is_on);
     TuyaDatapoint datapoint{};
     datapoint.id = *this->switch_id_;
     datapoint.type = TuyaDatapointType::BOOLEAN;
-    datapoint.value_bool = is_off != 0.0f;
+    datapoint.value_bool = is_on;
     parent_->set_datapoint_value(datapoint);
   }
 }
