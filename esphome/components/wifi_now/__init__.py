847fc3041dc2f3e475ffe18527b9f765ace0b5da
Emmo Emminghaus <mo2000@mo2000.de>
2020-10-01 00:31:31 +0200

    Add WIFI_BASIC_COOP define

--- esphome/components/wifi_now/__init__.py
+++ esphome/components/wifi_now/__init__.py
@@ -13,6 +13,7 @@ CONFIG_SCHEMA = component.COMPONENT_SCHEMA
 
 @coroutine
 def to_code(config):
+    cg.add_define('WIFI_BASIC_COOP')
     cg.add_define('USE_WIFINOW')
     if CORE.is_esp8266:
         cg.add_library('ESP8266WiFi', None)
