734594d33aa9da755e94ea9e488c88f2ca89d18b
San <zhujunsan@gmail.com>
2020-10-04 21:58:20 +0800

    [Image] add dither option for image processing

--- esphome/components/image/__init__.py
+++ esphome/components/image/__init__.py
@@ -4,19 +4,25 @@ from esphome import core
 from esphome.components import display, font
 import esphome.config_validation as cv
 import esphome.codegen as cg
-from esphome.const import CONF_FILE, CONF_ID, CONF_TYPE, CONF_RESIZE
+from esphome.const import CONF_FILE, CONF_ID, CONF_TYPE, CONF_RESIZE, CONF_DITHER
 from esphome.core import CORE, HexInt
+from PIL import Image
+
 _LOGGER = logging.getLogger(__name__)
 
 DEPENDENCIES = ['display']
 MULTI_CONF = True
-
 
 ImageType = display.display_ns.enum('ImageType')
 IMAGE_TYPE = {
     'BINARY': ImageType.IMAGE_TYPE_BINARY,
     'GRAYSCALE': ImageType.IMAGE_TYPE_GRAYSCALE,
     'RGB24': ImageType.IMAGE_TYPE_RGB24,
+}
+
+DITHER_MODE = {
+    'NONE': Image.NONE,
+    'FLOYDSTEINBERG': Image.FLOYDSTEINBERG,
 }
 
 Image_ = display.display_ns.class_('Image')
@@ -28,6 +34,7 @@ IMAGE_SCHEMA = cv.Schema({
     cv.Required(CONF_FILE): cv.file_,
     cv.Optional(CONF_RESIZE): cv.dimensions,
     cv.Optional(CONF_TYPE, default='BINARY'): cv.enum(IMAGE_TYPE, upper=True),
+    cv.Optional(CONF_DITHER, default='NONE'): cv.enum(DITHER_MODE, upper=True),
     cv.GenerateID(CONF_RAW_DATA_ID): cv.declare_id(cg.uint8),
 })
 
@@ -35,8 +42,6 @@ CONFIG_SCHEMA = cv.All(font.validate_pillow_installed, IMAGE_SCHEMA)
 
 
 def to_code(config):
-    from PIL import Image
-
     path = CORE.relative_config_path(config[CONF_FILE])
     try:
         image = Image.open(path)
@@ -54,7 +59,7 @@ def to_code(config):
                             " the resize parameter.")
 
     if config[CONF_TYPE] == 'GRAYSCALE':
-        image = image.convert('L', dither=Image.NONE)
+        image = image.convert('L', dither=DITHER_MODE[config[CONF_DITHER]])
         pixels = list(image.getdata())
         data = [0 for _ in range(height * width)]
         pos = 0
@@ -76,7 +81,7 @@ def to_code(config):
             pos += 1
 
     elif config[CONF_TYPE] == 'BINARY':
-        image = image.convert('1', dither=Image.NONE)
+        image = image.convert('1', dither=DITHER_MODE[config[CONF_DITHER]])
         width8 = ((width + 7) // 8) * 8
         data = [0 for _ in range(height * width8 // 8)]
         for y in range(height):
