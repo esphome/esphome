bb83314f3d0cb78b76885fdfbe2cb7f207c9ac69
Jerzy Kołosowski <jerzy@kolosowscy.pl>
2020-10-05 03:08:27 +0200

    Add merge list by items id for packages
    Signed-off-by: Jerzy Kołosowski <jerzy@kolosowscy.pl>

--- esphome/config_helpers.py
+++ esphome/config_helpers.py
@@ -25,6 +25,25 @@ def read_config_file(path: str) -> str:
 
 
 def merge_config(full_old, full_new):
+    def find_and_merge(old_list, new_item):
+        if isinstance(new_item, dict) and "id" in new_item:
+            for n, item in enumerate(old_list):
+                if (
+                    isinstance(item, dict)
+                    and "id" in item
+                    and item["id"] == new_item["id"]
+                ):
+                    old_list[n] = merge(item, new_item)
+                    return True
+        return False
+
+    def merge_list(old, new):
+        res = old.copy()
+        for new_item in new:
+            if not find_and_merge(res, new_item):
+                res.append(new_item)
+        return res
+
     def merge(old, new):
         if isinstance(new, dict):
             if not isinstance(old, dict):
@@ -36,7 +55,7 @@ def merge_config(full_old, full_new):
         if isinstance(new, list):
             if not isinstance(old, list):
                 return new
-            return old + new
+            return merge_list(old, new)
         if new is None:
             return old
 
