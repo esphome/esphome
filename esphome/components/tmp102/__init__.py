d97a9bf8e89689ec948c0cf5c391826efe14865d
Tim Savage <tim@savage.company>
2020-10-27 05:00:43 +1100

    Added tmp102 temperature sensor support (#929)
    * Added tmp102 temperature sensor support
    
    * Added sensor to test3.yaml
    
    * Moved docstring to component root
    
    * Tweak formatting from clang-format script
    
    * Removed extra newline at the end of the file to satisfy pylint
    
    * Update schema to match that of other single-value sensors
    
    In ESPHome, sensors that only expose one value do not put the sensor under another key.
    
    * Add missing import
    
    * Fix test after structural change to component
    
    * removed unused setting
    
    * Update esphome/components/tmp102/tmp102.cpp
    
    Co-authored-by: Otto Winter <otto@otto-winter.com>
    Co-authored-by: Guillermo Ruffino <glm.net@gmail.com>
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

---
+++ esphome/components/tmp102/__init__.py
@@ -0,0 +1,9 @@
+"""
+The TMP102 is a two-wire, serial output temperature
+sensor available in a tiny SOT563 package. Requiring
+no external components, the TMP102 is capable of
+reading temperatures to a resolution of 0.0625°C.
+
+https://www.sparkfun.com/datasheets/Sensors/Temperature/tmp102.pdf
+
+"""
