32d6a0a2609759950eba09ec28a7fd6da167af5b
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2020-10-28 01:03:42 +0400

    Fix option name

--- esphome/components/midea_dongle/__init__.py
+++ esphome/components/midea_dongle/__init__.py
@@ -10,10 +10,10 @@ midea_dongle_ns = cg.esphome_ns.namespace('midea_dongle')
 MideaDongle = midea_dongle_ns.class_('MideaDongle', cg.Component, uart.UARTDevice)
 
 CONF_MIDEA_DONGLE_ID = 'midea_dongle_id'
-CONF_STRATCHED_ICON = 'stratched_icon'
+CONF_STRETCHED_ICON = 'stretched_icon'
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MideaDongle),
-    cv.Optional(CONF_STRATCHED_ICON): cv.boolean,
+    cv.Optional(CONF_STRETCHED_ICON): cv.boolean,
 }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 
@@ -21,5 +21,5 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
-    if CONF_STRATCHED_ICON in config:
-        cg.add(var.use_stretched_icon(config[CONF_STRATCHED_ICON]))
+    if CONF_STRETCHED_ICON in config:
+        cg.add(var.use_stretched_icon(config[CONF_STRETCHED_ICON]))
