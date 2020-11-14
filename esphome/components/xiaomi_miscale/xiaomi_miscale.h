fac8846e2802de7cd0c585d97a0a9a62c6ec587c
dckiller51 <53062806+dckiller51@users.noreply.github.com>
2020-11-14 09:53:34 +0100

    test miscale dir

---
+++ esphome/components/xiaomi_miscale/xiaomi_miscale.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/esp32_ble_tracker/esp32_ble_tracker.h"
+
+#ifdef ARDUINO_ARCH_ESP32
+
+namespace esphome {
+namespace xiaomi_miscale {
+
+struct ParseResult {
+  optional<float> weight;
+  optional<float> impedance;
+  optional<float> battery_level;
+  bool is_duplicate;
+  int raw_offset;
+};
+
+class XiaomiMiscale : public Component, public esp32_ble_tracker::ESPBTDeviceListener {
+ public:
+  void set_address(uint64_t address) { address_ = address; };
+
+  bool parse_device(const esp32_ble_tracker::ESPBTDevice &device) override;
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
+  void set_weight(sensor::Sensor *weight) { weight_ = weight; }
+  void set_battery_level(sensor::Sensor *battery_level) { battery_level_ = battery_level; }
+  void set_impedance(sensor::Sensor *impedance) { impedance_ = impedance; }
+
+ protected:
+  uint64_t address_;
+  sensor::Sensor *weight_{nullptr};
+  sensor::Sensor *battery_level_{nullptr};
+  sensor::Sensor *impedance_{nullptr};
+
+  optional<ParseResult> parse_header(const esp32_ble_tracker::ServiceData &service_data);
+  bool parse_message(const std::vector<uint8_t> &message, ParseResult &result);
+  bool report_results(const optional<ParseResult> &result, const std::string &address);
+};
+
+}  // namespace xiaomi_miscale
+}  // namespace esphome
+
+#endif
