62d8c7998c93ff406785eacbc9ea3920d2d230e4
dckiller51 <53062806+dckiller51@users.noreply.github.com>
2020-11-15 11:07:47 +0100

    Update xiaomi_ble.cpp

--- esphome/components/xiaomi_ble/xiaomi_ble.cpp
+++ esphome/components/xiaomi_ble/xiaomi_ble.cpp
@@ -70,24 +70,26 @@ bool parse_xiaomi_value(uint8_t value_type, const uint8_t *data, uint8_t value_l
   }
   // Miscale weight, 2 bytes, 16-bit  unsigned integer, 1 kg
   else if ((value_type == 0x16) && (value_length == 10)) {
-    const uint16_t weight = uint16_t(data[1]) | (uint16_t(data[2]) << 8);
-    if (data[0] == 0x22 || data[0] == 0xa2)
-      result.weight = weight * 0.01f / 2.0f;
-    else if (data[0] == 0x12 || data[0] == 0xb2)
-      result.weight = weight * 0.01f * 0.6;
-    else if (data[0] == 0x03 || data[0] == 0xb3)
-      result.weight = weight * 0.01f * 0.453592;
+    if (result.type == XiaomiParseResult::TYPE_XMTZC0XHM) {
+      const uint16_t weight = uint16_t(data[1]) | (uint16_t(data[2]) << 8);
+      if (data[0] == 0x22 || data[0] == 0xa2)
+        result.weight = weight * 0.01f / 2.0f;
+      else if (data[0] == 0x12 || data[0] == 0xb2)
+        result.weight = weight * 0.01f * 0.6;
+      else if (data[0] == 0x03 || data[0] == 0xb3)
+        result.weight = weight * 0.01f * 0.453592;
   }
   // Miscale 2 weight, impedence, 2 bytes, 16-bit  unsigned integer, 1 kg
   else if ((value_type == 0x16) && (value_length == 13)) {
-    const uint16_t weight = uint16_t(data[11]) | (uint16_t(data[12]) << 8);
-    const uint16_t impedance = uint16_t(data[9]) | (uint16_t(data[10]) << 8);
-    result.impedance = impedance;
+    if (result.type == XiaomiParseResult::TYPE_XMTZC0XHM) {
+      const uint16_t weight = uint16_t(data[11]) | (uint16_t(data[12]) << 8);
+      const uint16_t impedance = uint16_t(data[9]) | (uint16_t(data[10]) << 8);
+      result.impedance = impedance;
 
-    if (data[0] == 0x02)
-      result.weight = weight * 0.01f / 2.0f;
-    else if (data[0] == 0x03)
-      result.weight = weight * 0.01f * 0.453592;
+      if (data[0] == 0x02)
+        result.weight = weight * 0.01f / 2.0f;
+      else if (data[0] == 0x03)
+        result.weight = weight * 0.01f * 0.453592;
   }
   // idle time since last motion, 4 byte, 32-bit unsigned integer, 1 min
   else if ((value_type == 0x17) && (value_length == 4)) {
