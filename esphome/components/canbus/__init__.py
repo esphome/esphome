5a37f2ee598457ee6b7fa66cabcd3cede54d43b5
danielschramm <daniel.schramm@gmx.de>
2020-11-18 19:13:35 +0100

    Changed binary flag for Extended ID to variable.

--- esphome/components/canbus/__init__.py
+++ esphome/components/canbus/__init__.py
@@ -7,6 +7,7 @@ from esphome.const import CONF_ID, CONF_TRIGGER_ID, CONF_DATA
 IS_PLATFORM_COMPONENT = True
 
 CONF_CAN_ID = 'can_id'
+CONF_CAN_EXT_ID = 'can_ext_id'
 CONF_CANBUS_ID = 'canbus_id'
 CONF_BIT_RATE = 'bit_rate'
 CONF_ON_FRAME = 'on_frame'
@@ -49,11 +50,13 @@ CAN_SPEEDS = {
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CanbusComponent),
-    cv.Required(CONF_CAN_ID): cv.int_range(min=0, max=255),
+    cv.Required(CONF_CAN_ID): cv.int_range(min=0, max=0x1fffffff),
     cv.Optional(CONF_BIT_RATE, default='125KBPS'): cv.enum(CAN_SPEEDS, upper=True),
+    cv.Optional(CONF_CAN_EXT_ID, default=False): cv.boolean,
     cv.Optional(CONF_ON_FRAME): automation.validate_automation({
         cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(CanbusTrigger),
-        cv.GenerateID(CONF_CAN_ID): cv.int_range(min=1, max=4096),
+        cv.GenerateID(CONF_CAN_ID): cv.int_range(min=0, max=0x1fffffff),
+        cv.Optional(CONF_CAN_EXT_ID, default=False): cv.boolean,
     }),
 }).extend(cv.COMPONENT_SCHEMA)
 
@@ -62,10 +65,11 @@ CONFIG_SCHEMA = cv.Schema({
 def setup_canbus_core_(var, config):
     yield cg.register_component(var, config)
     cg.add(var.set_can_id([config[CONF_CAN_ID]]))
+    cg.add(var.set_can_ext_id([config[CONF_CAN_EXT_ID]]))
     cg.add(var.set_bitrate(CAN_SPEEDS[config[CONF_BIT_RATE]]))
 
     for conf in config.get(CONF_ON_FRAME, []):
-        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var, conf[CONF_CAN_ID])
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var, conf[CONF_CAN_ID], conf[CONF_CAN_EXT_ID])
         yield cg.register_component(trigger, conf)
         yield automation.build_automation(trigger, [(cg.std_vector.template(cg.uint8), 'x')], conf)
 
@@ -82,7 +86,8 @@ def register_canbus(var, config):
                             canbus_ns.class_('CanbusSendAction', automation.Action),
                             cv.maybe_simple_value({
                                 cv.GenerateID(CONF_CANBUS_ID): cv.use_id(CanbusComponent),
-                                cv.Optional(CONF_CAN_ID): cv.int_range(min=1, max=4096),
+                                cv.Optional(CONF_CAN_ID): cv.int_range(min=0, max=0x1fffffff),
+                                cv.Optional(CONF_CAN_EXT_ID, default=False): cv.boolean,
                                 cv.Required(CONF_DATA): cv.templatable(validate_raw_data),
                             }, key=CONF_DATA))
 def canbus_action_to_code(config, action_id, template_arg, args):
@@ -92,6 +97,10 @@ def canbus_action_to_code(config, action_id, template_arg, args):
     if CONF_CAN_ID in config:
         can_id = yield cg.templatable(config[CONF_CAN_ID], args, cg.uint16)
         cg.add(var.set_can_id(can_id))
+        
+    if CONF_CAN_EXT_ID in config:
+        can_ext_id = yield cg.templatable(config[CONF_CAN_EXT_ID], args, cg.uint16)
+        cg.add(var.set_can_ext_id(can_ext_id))
 
     data = config[CONF_DATA]
     if isinstance(data, bytes):
