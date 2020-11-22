2e69051c57b5e68ea0ab5ecb61bc2aab2d47e63c
dckiller51 <53062806+dckiller51@users.noreply.github.com>
2020-11-22 01:10:52 +0100

    Update xiaomi_ble.cpp

--- esphome/components/xiaomi_ble/xiaomi_ble.cpp
+++ esphome/components/xiaomi_ble/xiaomi_ble.cpp
@@ -148,9 +148,9 @@ bool parse_xiaomi_message(const std::vector<uint8_t> &message, XiaomiParseResult
   }
   // For MiScale
   const uint8_t *data = message.data();
-  const int data_length = 10;
-  const int data_length1 = data_length + 3;
-  if ((message.size() == data_length1) || (message.size() == data_length)) {
+  const int value_length = 10;
+  const int value_length1 = value_length + 3;
+  if ((message.size() == value_length1) || (message.size() == value_length)) {
     success = true;
   }
 
