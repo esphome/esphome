3890736f2ad90a3d6ec06bf06b162ad8f0392987
dckiller51 <53062806+dckiller51@users.noreply.github.com>
2020-11-22 09:53:54 +0100

    Update xiaomi_ble.cpp

--- esphome/components/xiaomi_ble/xiaomi_ble.cpp
+++ esphome/components/xiaomi_ble/xiaomi_ble.cpp
@@ -110,10 +110,51 @@ bool parse_xiaomi_message(const std::vector<uint8_t> &message, XiaomiParseResult
   result.has_encryption = (message[0] & 0x08) ? true : false;  // update encryption status
   if (result.has_encryption) {
     ESP_LOGVV(TAG, "parse_xiaomi_message(): payload is encrypted, stop reading message.");
-    return {};
+    return false;
   }
 
+  // Data point specs
+  // Byte 0: type
+  // Byte 1: fixed 0x10
+  // Byte 2: length
+  // Byte 3..3+len-1: data point value
+
+  const uint8_t *payload = message.data() + result.raw_offset;
+  uint8_t payload_length = message.size() - result.raw_offset;
+  uint8_t payload_offset = 0;
   bool success = false;
+
+  if (payload_length < 4) {
+    ESP_LOGVV(TAG, "parse_xiaomi_message(): payload has wrong size (%d)!", payload_length);
+    return false;
+  }
+
+  while (payload_length > 0) {
+    if (payload[payload_offset + 1] != 0x10) {
+      ESP_LOGVV(TAG, "parse_xiaomi_message(): fixed byte not found, stop parsing residual data.");
+      break;
+    }
+
+    const uint8_t value_length = payload[payload_offset + 2];
+    if ((value_length < 1) || (value_length > 4) || (payload_length < (3 + value_length))) {
+      ESP_LOGVV(TAG, "parse_xiaomi_message(): value has wrong size (%d)!", value_length);
+      break;
+    }
+
+    const uint8_t value_type = payload[payload_offset + 0];
+    const uint8_t *data = &payload[payload_offset + 3];
+
+    if (parse_xiaomi_value(value_type, data, value_length, result))
+      success = true;
+
+    payload_length -= 3 + value_length;
+    payload_offset += 3 + value_length;
+  }
+  // Hack for MiScale
+  const uint8_t *data = &payload;  // raw data
+  if (parse_xiaomi_value(0x16, data, message.size(), result)) {
+    success = true;
+  }
 
   return success;
 }
