2551bb04e569422e4c71f2f1d460a54a363e8a69
Alex <alexyao2015@gmail.com>
2020-11-23 00:01:33 -0500

    Replace tab

--- esphome/components/ota/__init__.py
+++ esphome/components/ota/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import (
-	CONF_ID, CONF_NUM_ATTEMPTS, CONF_PASSWORD,
+    CONF_ID, CONF_NUM_ATTEMPTS, CONF_PASSWORD,
     CONF_PORT, CONF_REBOOT_TIMEOUT, CONF_SAFE_MODE
 )
 from esphome.core import CORE, coroutine_with_priority
