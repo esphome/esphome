89ab873b0153feecfd47be072e4ff5ef2fcba8e9
stubs12 <43162406+stubs12@users.noreply.github.com>
2020-11-24 19:09:52 -0600

    Fixed whitespace lint issues

--- esphome/components/tuya/__init__.py
+++ esphome/components/tuya/__init__.py
@@ -18,6 +18,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_IGNORE_MCU_UPDATE_ON_DATAPOINTS): cv.ensure_list(cv.uint8_t),
 }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
+
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
@@ -28,4 +29,3 @@ def to_code(config):
     if CONF_IGNORE_MCU_UPDATE_ON_DATAPOINTS in config:
         for dp in config[CONF_IGNORE_MCU_UPDATE_ON_DATAPOINTS]:
             cg.add(var.add_ignore_mcu_update_on_datapoints(dp))
-
