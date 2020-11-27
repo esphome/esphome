e2fd7be6e9420fb632d296b23dc043b8271d69ff
RonenM <micronen@gmail.com>
2020-11-27 23:04:20 +0200

    satisfy github lint

--- esphome/components/substitutions/__init__.py
+++ esphome/components/substitutions/__init__.py
@@ -128,12 +128,12 @@ def do_substitution_pass(config, command_line_substitutions):
 
     # Iterativly substitute substitutions within substitutions
     while True:
-        subs_count = sum(1 for key,val in substitutions.items() if '$' in val)
+        subs_count = sum(1 for key, val in substitutions.items() if '$' in val)
         if subs_count > 0:
             _substitute_item(substitutions, substitutions, [])
-        if subs_count == sum(1 for key,val in substitutions.items() if '$' in val):
+        if subs_count == sum(1 for key, val in substitutions.items() if '$' in val):
             # No substitutions were substituted
             break
-    
+
     config[CONF_SUBSTITUTIONS] = substitutions
     _substitute_item(substitutions, config, [])
