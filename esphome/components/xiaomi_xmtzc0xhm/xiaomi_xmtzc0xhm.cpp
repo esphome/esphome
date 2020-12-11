f70ec945ed902abfa473fb9efb30f08d93b36882
dckiller51 <53062806+dckiller51@users.noreply.github.com>
2020-12-11 12:08:14 +0100

    test

--- esphome/components/xiaomi_xmtzc0xhm/xiaomi_xmtzc0xhm.cpp
+++ esphome/components/xiaomi_xmtzc0xhm/xiaomi_xmtzc0xhm.cpp
@@ -8,56 +8,86 @@ namespace xiaomi_xmtzc0xhm {
 
 static const char *TAG = "xiaomi_xmtzc0xhm";
 
-void XiaomiXMTZC0XHM::dump_config() {
-  ESP_LOGCONFIG(TAG, "Xiaomi XMTZC0XHM");
-  LOG_SENSOR("  ", "Weight", this->weight_);
-  LOG_SENSOR("  ", "Impedance", this->impedance_);
-}
-
-bool XiaomiXMTZC0XHM::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
-  if (device.address_uint64() != this->address_) {
-    ESP_LOGVV(TAG, "parse_device(): unknown MAC address.");
-    return false;
+bool parse_value(uint8_t value_type, const uint8_t *data, uint8_t value_length, ParseResult &result) {
+  // Miscale weight, 2 bytes, 16-bit  unsigned integer, 1 kg
+  else if ((value_type == 0x16) && (value_length == 10)) {
+    const uint16_t weight = uint16_t(data[1]) | (uint16_t(data[2]) << 8);
+    if (data[0] == 0x22 || data[0] == 0xa2)
+      result.weight = weight * 0.01f / 2.0f;
+    else if (data[0] == 0x12 || data[0] == 0xb2)
+      result.weight = weight * 0.01f * 0.6;
+    else if (data[0] == 0x03 || data[0] == 0xb3)
+      result.weight = weight * 0.01f * 0.453592;
   }
-  ESP_LOGVV(TAG, "parse_device(): MAC address %s found.", device.address_str().c_str());
+  // Miscale 2 weight, impedence, 2 bytes, 16-bit  unsigned integer, 1 kg
+  else if ((value_type == 0x16) && (value_length == 13)) {
+    const uint16_t weight = uint16_t(data[11]) | (uint16_t(data[12]) << 8);
+    const uint16_t impedance = uint16_t(data[9]) | (uint16_t(data[10]) << 8);
+    result.impedance = impedance;
 
-  bool success = false;
-  for (auto &service_data : device.get_service_datas()) {
-    auto res = parse_header(service_data);
-    if (res->is_stabilized) {
-      continue;
-    }
-    if (!(parse_message(service_data.data, *res))) {
-      continue;
-    }
-    if (!(report_results(res, device.address_str()))) {
-      continue;
-    }
-    if (res->weight.has_value() && this->weight_ != nullptr)
-      this->weight_->publish_state(*res->weight);
-    if (res->impedance.has_value() && this->impedance_ != nullptr)
-      this->impedance_->publish_state(*res->impedance);
-    success = true;
-  }
-
-  if (!success) {
+    if (data[0] == 0x02)
+      result.weight = weight * 0.01f / 2.0f;
+    else if (data[0] == 0x03)
+      result.weight = weight * 0.01f * 0.453592;
+  } else {
     return false;
   }
 
   return true;
 }
 
-optional<ParseResult> XiaomiXMTZC0XHM::parse_header(const esp32_ble_tracker::ServiceData &service_data) {
+bool parse_message(const std::vector<uint8_t> &message, ParseResult &result) {
+  result.has_encryption = (message[0] & 0x08) ? true : false;  // update encryption status
+  if (result.has_encryption) {
+    ESP_LOGVV(TAG, "parse_message(): payload is encrypted, stop reading message.");
+    return false;
+  }
+
+  // Data point specs
+  // Byte 0: type
+  // Byte 1: fixed 0x10
+  // Byte 2: length
+  // Byte 3..3+len-1: data point value
+
+  const uint8_t *payload = message.data() + result.raw_offset;
+  uint8_t payload_length = message.size() - result.raw_offset;
+  uint8_t payload_offset = 0;
+  bool success = false;
+
+  if (payload_length < 4) {
+    ESP_LOGVV(TAG, "parse_message(): payload has wrong size (%d)!", payload_length);
+    return false;
+  }
+
+  while (payload_length > 0) {
+    if (payload[payload_offset + 1] != 0x10) {
+      ESP_LOGVV(TAG, "parse_message(): fixed byte not found, stop parsing residual data.");
+      break;
+    }
+
+    const uint8_t value_length = payload[payload_offset + 2];
+    if ((value_length < 1) || (value_length > 4) || (payload_length < (3 + value_length))) {
+      ESP_LOGVV(TAG, "parse_message(): value has wrong size (%d)!", value_length);
+      break;
+    }
+
+    const uint8_t value_type = payload[payload_offset + 0];
+    const uint8_t *data = &payload[payload_offset + 3];
+
+    if (parse_value(value_type, data, value_length, result))
+      success = true;
+
+    payload_length -= 3 + value_length;
+    payload_offset += 3 + value_length;
+  }
+
+  return success;
+}
+
+optional<ParseResult> parse_header(const esp32_ble_tracker::ServiceData &service_data) {
   ParseResult result;
   if (!service_data.uuid.contains(0x1D, 0x18) && !service_data.uuid.contains(0x1B, 0x18)) {
     ESP_LOGVV(TAG, "parse_header(): no service data UUID magic bytes.");
-    return {};
-  }
-
-  const auto raw = service_data.data;
-
-  if (raw.size() < 13) {
-    ESP_LOGVV(TAG, "Xiaomi service data too short!");
     return {};
   }
 
@@ -66,80 +96,28 @@ optional<ParseResult> XiaomiXMTZC0XHM::parse_header(const esp32_ble_tracker::Ser
 
   if (!is_xmtzc0xhm && !is_mibfs) {
     ESP_LOGVV(TAG, "Xiaomi no magic bytes");
-    return {};
+    return false;
   }
 
-  bool is_stabilized = false;
-
-  // Hack for MiScale
-  if (is_xmtzc0xhm || is_mibfs) {
-    const uint8_t *datapoint_data = &raw[0];  // raw data
-    if (parse_data_byte(0x16, datapoint_data, raw.size(), result))
-      result.is_stabilized = true;
+  if (is_xmtzc0xhm) {  // Xiaomi Miscale
+    success = true
+  } else if (is_mibfs) {  // Xiaomi Miscale 2
+    success = true
+  } else {
+    ESP_LOGVV(TAG, "parse_header(): unknown device, no magic bytes.");
+    return {};
   }
 
   return result;
 }
 
-bool XiaomiXMTZC0XHM::parse_message(const std::vector<uint8_t> &message, ParseResult &result) {
-  // 2-3 Weight (MISCALE 181D) // 2-3 Years (MISCALE 2 181B)
-  // 4-5 Years (MISCALE 181D)  // 4 month (MISCALE 2 181B)
-  //                           // 5 day (MISCALE 2 181B)
-  // 6 month (MISCALE 181D)    // 6 hour (MISCALE 2 181B)
-  // 7 day (MISCALE 181D)      // 7 minute (MISCALE 2 181B)
-  // 8 hour (MISCALE 181D)     // 8 second (MISCALE 2 181B)
-  // 9 minute (MISCALE 181D)   // 9-10 impedance (MISCALE 2 181B)
-  // 10 second (MISCALE 181D)  //
-  //                           // 11-12 weight (MISCALE 2 181B)
-
-  const uint8_t *data = message.data();
-  const int data_length = 10;
-  const int data_length1 = data_length + 3;
-
-  if (message.size() == data_length1) {
-    // Miscale2 impedance, 2 bytes, 16-bit
-    const int16_t impedance = uint16_t(data[9]) | (uint16_t(data[10]) << 8);
-    result.impedance = impedance;
-
-    // Miscale2 weight, 2 bytes, 16-bit  unsigned integer, 1 kg
-    const int16_t weight = uint16_t(data[11]) | (uint16_t(data[12]) << 8);
-    if (data[0] == 0x02)
-      result.weight = weight * 0.01f / 2.0f;  // unit 'kg'
-    else if (data[0] == 0x03)
-      result.weight = weight * 0.01f * 0.453592;  // unit 'lbs'
-  }
-
-  else if (message.size() == data_length) {
-    int rcvdYear = uint16_t(data[4]) | (uint16_t(data[5]) << 8);
-    static int year = 0;
-    if (year == 0) {
-      year = rcvdYear;
-    } else {
-      if (rcvdYear > year) {
-        // Miscale weight, 2 bytes, 16-bit  unsigned integer, 1 kg
-        const int16_t weight = uint16_t(data[1]) | (uint16_t(data[2]) << 8);
-        if (data[0] == 0x22 || data[0] == 0xa2)
-          result.weight = weight * 0.01f / 2.0f;  // unit 'kg'
-        else if (data[0] == 0x12 || data[0] == 0xb2)
-          result.weight = weight * 0.01f * 0.6;  // unit 'jin'
-        else if (data[0] == 0x03 || data[0] == 0xb3)
-          result.weight = weight * 0.01f * 0.453592;  // unit 'lbs'
-      } else {
-        return false;
-      }
-    }
-  }
-
-  return true;
-}
-
-bool XiaomiXMTZC0XHM::report_results(const optional<ParseResult> &result, const std::string &address) {
+bool report_results(const optional<ParseResult> &result, const std::string &address) {
   if (!result.has_value()) {
     ESP_LOGVV(TAG, "report_results(): no results available.");
     return false;
   }
 
-  ESP_LOGD(TAG, "Got Xiaomi XMTZC0XHM (%s):", address.c_str());
+  ESP_LOGD(TAG, "Got Xiaomi %s (%s):", result->name.c_str(), address.c_str());
 
   if (result->weight.has_value()) {
     ESP_LOGD(TAG, "  Weight: %.2fkg", *result->weight);
@@ -151,7 +129,18 @@ bool XiaomiXMTZC0XHM::report_results(const optional<ParseResult> &result, const 
   return true;
 }
 
-}  // namespace xiaomi_xmtzc0xhm
+bool Listener::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
+  // Previously the message was parsed twice per packet, once by Listener::parse_device()
+  // and then again by the respective device class's parse_device() function. Parsing the header
+  // here and then for each device seems to be unneccessary and complicates the duplicate packet filtering.
+  // Hence I disabled the call to parse_header() here and the message parsing is done entirely
+  // in the respecive device instance. The Listener class is defined in __init__.py and I was not
+  // able to remove it entirely.
+
+  return false;  // with true it's not showing device scans
+}
+
+}  // namespace xiaomi_ble
 }  // namespace esphome
 
 #endif
