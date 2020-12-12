aabc3218490c24d98d80b5f8f94a36de7b7e2c3f
dckiller51 <53062806+dckiller51@users.noreply.github.com>
2020-12-12 02:00:32 +0100

    Update xiaomi_xmtzc0xhm.h

--- esphome/components/xiaomi_xmtzc0xhm/xiaomi_xmtzc0xhm.h
+++ esphome/components/xiaomi_xmtzc0xhm/xiaomi_xmtzc0xhm.h
@@ -32,10 +32,7 @@ class XiaomiXMTZC0XHM : public Component, public esp32_ble_tracker::ESPBTDeviceL
 
 };
 
-bool parse_value(uint8_t value_type, const uint8_t *data, uint8_t value_length, ParseResult &result);
-bool parse_message(const std::vector<uint8_t> &message, ParseResult &result);
-optional<ParseResult> parse_header(const esp32_ble_tracker::ServiceData &service_data);
-bool report_results(const optional<ParseResult> &result, const std::string &address);
+
 
 }  // namespace xiaomi_xmtzc0xhm
 }  // namespace esphome
