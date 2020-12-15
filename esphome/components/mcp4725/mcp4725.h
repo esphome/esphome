7051a025d36332edd91a6bf52657ee85ce09dcbc
Jérémy Jourdin <jjourdin18@gmail.com>
2020-12-15 12:59:40 +0100

    Lint again

--- esphome/components/mcp4725/mcp4725.h
+++ esphome/components/mcp4725/mcp4725.h
@@ -10,12 +10,12 @@ static const uint8_t MCP4725_RES = 12;
 namespace esphome {
 namespace mcp4725 {
 class MCP4725 : public Component, public output::FloatOutput, public i2c::I2CDevice {
-public:
+ public:
   void setup() override;
   void dump_config() override;
   void write_state(float state) override;
 
-protected:
+ protected:
   enum ErrorCode { NONE = 0, COMMUNICATION_FAILED } error_code_{NONE};
 };
 
