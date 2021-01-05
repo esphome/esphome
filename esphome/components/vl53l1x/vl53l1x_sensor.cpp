10bb0f39a3782c9be132b2dd456fc223718d17c5
mampfes <mampfes@users.noreply.github.com>
2021-01-05 16:15:26 +0100

    use clang-format

--- esphome/components/vl53l1x/vl53l1x_sensor.cpp
+++ esphome/components/vl53l1x/vl53l1x_sensor.cpp
@@ -5,11 +5,9 @@
 namespace esphome {
 namespace vl53l1x {
 
-static const char *TAG = "vl53l1x";
+static const char* TAG = "vl53l1x";
 
-VL53L1XSensor::VL53L1XSensor() {
-  vl53l1x_ = new VL53L1X();
-}
+VL53L1XSensor::VL53L1XSensor() { vl53l1x_ = new VL53L1X(); }
 
 void VL53L1XSensor::dump_config() {
   LOG_SENSOR("", "VL53L1X", this);
@@ -23,44 +21,44 @@ void VL53L1XSensor::setup() {
   }
   vl53l1x_->setDistanceMode(static_cast<VL53L1X::DistanceMode>(distance_mode_));
 
-  switch (distance_mode_)
-  {
+  switch (distance_mode_) {
     case DistanceMode::SHORT:
       if (timing_budget_ < 20000)
         timing_budget_ = 20000;
-    break;
+      break;
 
     case DistanceMode::MEDIUM:
     case DistanceMode::LONG:
       if (timing_budget_ < 33000)
         timing_budget_ = 33000;
-    break;
-
+      break;
   }
   vl53l1x_->setMeasurementTimingBudget(timing_budget_);
 }
 
 void VL53L1XSensor::update() {
   // initiate non-blocking single shot measurement
-  (void)vl53l1x_->readSingle(false);
+  (void) vl53l1x_->readSingle(false);
   retry_count_ = retry_budget_;
 }
 
 void VL53L1XSensor::loop() {
   if (vl53l1x_->dataReady()) {
     uint16_t range_mm = vl53l1x_->read(false);  // non-blocking read
-    
+
     // check measurement result
     if (vl53l1x_->ranging_data.range_status == VL53L1X::RangeValid) {
       float range_m = static_cast<float>(range_mm) / 1000.0;
       ESP_LOGD(TAG, "'%s' - Got distance %.3f m", this->name_.c_str(), range_m);
       this->publish_state(range_m);
     } else if (retry_count_ > 0) {
-      ESP_LOGW(TAG, "'%s' - %s --> retrying %d", this->name_.c_str(), VL53L1X::rangeStatusToString(vl53l1x_->ranging_data.range_status), retry_count_);
-      (void)vl53l1x_->readSingle(false);
+      ESP_LOGW(TAG, "'%s' - %s --> retrying %d", this->name_.c_str(),
+               VL53L1X::rangeStatusToString(vl53l1x_->ranging_data.range_status), retry_count_);
+      (void) vl53l1x_->readSingle(false);
       retry_count_--;
     } else {
-      ESP_LOGW(TAG, "'%s' - %s", this->name_.c_str(), VL53L1X::rangeStatusToString(vl53l1x_->ranging_data.range_status));
+      ESP_LOGW(TAG, "'%s' - %s", this->name_.c_str(),
+               VL53L1X::rangeStatusToString(vl53l1x_->ranging_data.range_status));
       this->publish_state(NAN);
     }
   }
@@ -68,8 +66,7 @@ void VL53L1XSensor::loop() {
 
 // Dirty hack which provides access to the TwoWire instance within I2CCompoment.
 // This hack is used to minimize changes in the Pololu VL53L1X library.
-class I2CComponentDummy : public i2c::I2CComponent
-{
+class I2CComponentDummy : public i2c::I2CComponent {
  public:
   TwoWire* get_wire() const { return this->wire_; }
 };
