3208d1dcefeafd7716e37f636175b24aa43566d7
Florian Moesch <florian@moesch.org>
2021-01-11 18:39:01 +0100

    PR esphome/esphome#1457 test

--- esphome/components/ota/__init__.py
+++ esphome/components/ota/__init__.py
@@ -4,7 +4,7 @@ from esphome.const import (
     CONF_ID, CONF_NUM_ATTEMPTS, CONF_PASSWORD,
     CONF_PORT, CONF_REBOOT_TIMEOUT, CONF_SAFE_MODE
 )
-from esphome.core import CORE, coroutine_with_priority
+from esphome.core import CORE, coroutine_with_priority, TimePeriod
 
 CODEOWNERS = ['@esphome/core']
 DEPENDENCIES = ['network']
@@ -17,7 +17,8 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_SAFE_MODE, default=True): cv.boolean,
     cv.SplitDefault(CONF_PORT, esp8266=8266, esp32=3232): cv.port,
     cv.Optional(CONF_PASSWORD, default=''): cv.string,
-    cv.Optional(CONF_REBOOT_TIMEOUT, default='5min'): cv.positive_time_period_milliseconds,
+    cv.Optional(CONF_REBOOT_TIMEOUT, default='5min'):
+      cv.All(cv.positive_time_period_milliseconds, cv.Range(max=TimePeriod(milliseconds=4294967295))),
     cv.Optional(CONF_NUM_ATTEMPTS, default='10'): cv.positive_not_null_int
 }).extend(cv.COMPONENT_SCHEMA)
 
