86229f29e0d220936b4335715165a6df60d09927
dckiller51 <53062806+dckiller51@users.noreply.github.com>
2021-01-18 15:05:09 +0100

    Update sensor.py
    Impedance with 0 decimal

--- esphome/components/xiaomi_miscale2/sensor.py
+++ esphome/components/xiaomi_miscale2/sensor.py
@@ -15,7 +15,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(XiaomiMiscale2),
     cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
     cv.Optional(CONF_WEIGHT): sensor.sensor_schema(UNIT_KILOGRAM, ICON_SCALE_BATHROOM, 2),
-    cv.Optional(CONF_IMPEDANCE): sensor.sensor_schema(UNIT_OHM, ICON_OMEGA, 1),
+    cv.Optional(CONF_IMPEDANCE): sensor.sensor_schema(UNIT_OHM, ICON_OMEGA, 0),
 }).extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
 
 
