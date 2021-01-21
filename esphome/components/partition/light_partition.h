a1848cf1c07cc8fd9cd04ff8752884c76c917fbd
Jim Bauwens <jim.bauwens@vub.be>
2021-01-21 10:12:14 +0100

    Allow segments in a partition to be reversed

--- esphome/components/partition/light_partition.h
+++ esphome/components/partition/light_partition.h
@@ -8,20 +8,22 @@ namespace partition {
 
 class AddressableSegment {
  public:
-  AddressableSegment(light::LightState *src, int32_t src_offset, int32_t size)
-      : src_(static_cast<light::AddressableLight *>(src->get_output())), src_offset_(src_offset), size_(size) {}
+  AddressableSegment(light::LightState *src, int32_t src_offset, int32_t size, bool reversed)
+      : src_(static_cast<light::AddressableLight *>(src->get_output())), src_offset_(src_offset), size_(size), reversed_(reversed) {}
 
   light::AddressableLight *get_src() const { return this->src_; }
   int32_t get_src_offset() const { return this->src_offset_; }
   int32_t get_size() const { return this->size_; }
   int32_t get_dst_offset() const { return this->dst_offset_; }
   void set_dst_offset(int32_t dst_offset) { this->dst_offset_ = dst_offset; }
+  bool is_reversed() const { return this->reversed_ ; }
 
  protected:
   light::AddressableLight *src_;
   int32_t src_offset_;
   int32_t size_;
   int32_t dst_offset_;
+  bool reversed_;
 };
 
 class PartitionLightOutput : public light::AddressableLight {
@@ -72,7 +74,12 @@ class PartitionLightOutput : public light::AddressableLight {
     // offset within the segment
     int32_t seg_off = index - seg.get_dst_offset();
     // offset within the src
-    int32_t src_off = seg.get_src_offset() + seg_off;
+    int32_t src_off;
+    if (seg.is_reversed())
+      src_off = seg.get_src_offset() + seg.get_size() - seg_off - 1;
+    else
+      src_off = seg.get_src_offset() + seg_off;
+
     auto view = (*seg.get_src())[src_off];
     view.raw_set_color_correction(&this->correction_);
     return view;
