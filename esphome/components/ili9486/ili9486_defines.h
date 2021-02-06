48a081c5304392f7c01c517819222604468f3be0
MasterTim17 <pfeffer.tim@gmx.net>
2021-02-06 13:33:05 +0100

    created ili9486

---
+++ esphome/components/ili9486/ili9486_defines.h
@@ -0,0 +1,83 @@
+#pragma once
+
+namespace esphome {
+namespace ili9486 {
+
+// Color definitions
+// clang-format off
+static const uint8_t MADCTL_MY    = 0x80;   ///< Bit 7 Bottom to top
+static const uint8_t MADCTL_MX    = 0x40;   ///< Bit 6 Right to left
+static const uint8_t MADCTL_MV    = 0x20;   ///< Bit 5 Reverse Mode
+static const uint8_t MADCTL_ML    = 0x10;   ///< Bit 4 LCD refresh Bottom to top
+static const uint8_t MADCTL_RGB   = 0x00;  ///< Bit 3 Red-Green-Blue pixel order
+static const uint8_t MADCTL_BGR   = 0x08;  ///< Bit 3 Blue-Green-Red pixel order
+static const uint8_t MADCTL_MH    = 0x04;   ///< Bit 2 LCD refresh right to left
+// clang-format on
+
+static const uint16_t ILI9486_TFTWIDTH = 320;   ///< ILI9486 max TFT width
+static const uint16_t ILI9486_TFTHEIGHT = 240;  ///< ILI9486 max TFT height
+
+// All ILI9486 specific commands some are used by init()
+static const uint8_t ILI9486_NOP = 0x00;
+static const uint8_t ILI9486_SWRESET = 0x01;
+static const uint8_t ILI9486_RDDID = 0x04;
+static const uint8_t ILI9486_RDDST = 0x09;
+
+static const uint8_t ILI9486_SLPIN = 0x10;
+static const uint8_t ILI9486_SLPOUT = 0x11;
+static const uint8_t ILI9486_PTLON = 0x12;
+static const uint8_t ILI9486_NORON = 0x13;
+
+static const uint8_t ILI9486_RDMODE = 0x0A;
+static const uint8_t ILI9486_RDMADCTL = 0x0B;
+static const uint8_t ILI9486_RDPIXFMT = 0x0C;
+static const uint8_t ILI9486_RDIMGFMT = 0x0A;
+static const uint8_t ILI9486_RDSELFDIAG = 0x0F;
+
+static const uint8_t ILI9486_INVOFF = 0x20;
+static const uint8_t ILI9486_INVON = 0x21;
+static const uint8_t ILI9486_GAMMASET = 0x26;
+static const uint8_t ILI9486_DISPOFF = 0x28;
+static const uint8_t ILI9486_DISPON = 0x29;
+
+static const uint8_t ILI9486_CASET = 0x2A;
+static const uint8_t ILI9486_PASET = 0x2B;
+static const uint8_t ILI9486_RAMWR = 0x2C;
+static const uint8_t ILI9486_RAMRD = 0x2E;
+
+static const uint8_t ILI9486_PTLAR = 0x30;
+static const uint8_t ILI9486_VSCRDEF = 0x33;
+static const uint8_t ILI9486_MADCTL = 0x36;
+static const uint8_t ILI9486_VSCRSADD = 0x37;
+static const uint8_t ILI9486_PIXFMT = 0x3A;
+
+static const uint8_t ILI9486_WRDISBV = 0x51;
+static const uint8_t ILI9486_RDDISBV = 0x52;
+static const uint8_t ILI9486_WRCTRLD = 0x53;
+
+static const uint8_t ILI9486_FRMCTR1 = 0xB1;
+static const uint8_t ILI9486_FRMCTR2 = 0xB2;
+static const uint8_t ILI9486_FRMCTR3 = 0xB3;
+static const uint8_t ILI9486_INVCTR = 0xB4;
+static const uint8_t ILI9486_DFUNCTR = 0xB6;
+
+static const uint8_t ILI9486_PWCTR1 = 0xC0;
+static const uint8_t ILI9486_PWCTR2 = 0xC1;
+static const uint8_t ILI9486_PWCTR3 = 0xC2;
+static const uint8_t ILI9486_PWCTR4 = 0xC3;
+static const uint8_t ILI9486_PWCTR5 = 0xC4;
+static const uint8_t ILI9486_VMCTR1 = 0xC5;
+static const uint8_t ILI9486_VMCTR2 = 0xC7;
+
+static const uint8_t ILI9486_RDID4 = 0xD3;
+static const uint8_t ILI9486_RDINDEX = 0xD9;
+static const uint8_t ILI9486_RDID1 = 0xDA;
+static const uint8_t ILI9486_RDID2 = 0xDB;
+static const uint8_t ILI9486_RDID3 = 0xDC;
+static const uint8_t ILI9486_RDIDX = 0xDD;  // TBC
+
+static const uint8_t ILI9486_GMCTRP1 = 0xE0;
+static const uint8_t ILI9486_GMCTRN1 = 0xE1;
+
+}  // namespace ili9486
+}  // namespace esphome
---
+++ esphome/components/ili9486/ili9486_init.h
@@ -0,0 +1,70 @@
+#pragma once
+#include "esphome/core/helpers.h"
+
+namespace esphome {
+namespace ili9486 {
+
+// clang-format off
+static const uint8_t PROGMEM INITCMD_M5STACK[] = {
+  0xEF, 3, 0x03, 0x80, 0x02,
+  0xCF, 3, 0x00, 0xC1, 0x30,
+  0xED, 4, 0x64, 0x03, 0x12, 0x81,
+  0xE8, 3, 0x85, 0x00, 0x78,
+  0xCB, 5, 0x39, 0x2C, 0x00, 0x34, 0x02,
+  0xF7, 1, 0x20,
+  0xEA, 2, 0x00, 0x00,
+  ILI9486_PWCTR1  , 1, 0x23,             // Power control VRH[5:0]
+  ILI9486_PWCTR2  , 1, 0x10,             // Power control SAP[2:0];BT[3:0]
+  ILI9486_VMCTR1  , 2, 0x3e, 0x28,       // VCM control
+  ILI9486_VMCTR2  , 1, 0x86,             // VCM control2
+  ILI9486_MADCTL  , 1, MADCTL_BGR,       // Memory Access Control
+  ILI9486_VSCRSADD, 1, 0x00,             // Vertical scroll zero
+  ILI9486_PIXFMT  , 1, 0x55,
+  ILI9486_FRMCTR1 , 2, 0x00, 0x13,
+  ILI9486_DFUNCTR , 3, 0x08, 0x82, 0x27, // Display Function Control
+  0xF2, 1, 0x00,                         // 3Gamma Function Disable
+  ILI9486_GAMMASET , 1, 0x01,             // Gamma curve selected
+  ILI9486_GMCTRP1 , 15, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, // Set Gamma
+                        0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 
+                        0x0E, 0x09, 0x00,
+  ILI9486_GMCTRN1 , 15, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, // Set Gamma
+                        0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 
+                        0x31, 0x36, 0x0F,
+  ILI9486_SLPOUT  , 0x80,                // Exit Sleep
+  ILI9486_DISPON  , 0x80,                // Display on
+  0x00                                   // End of list
+};
+
+static const uint8_t PROGMEM INITCMD_TFT[] = {
+  0xEF, 3, 0x03, 0x80, 0x02,
+  0xCF, 3, 0x00, 0xC1, 0x30,
+  0xED, 4, 0x64, 0x03, 0x12, 0x81,
+  0xE8, 3, 0x85, 0x00, 0x78,
+  0xCB, 5, 0x39, 0x2C, 0x00, 0x34, 0x02,
+  0xF7, 1, 0x20,
+  0xEA, 2, 0x00, 0x00,
+  ILI9486_PWCTR1  , 1, 0x23,             // Power control VRH[5:0]
+  ILI9486_PWCTR2  , 1, 0x10,             // Power control SAP[2:0];BT[3:0]
+  ILI9486_VMCTR1  , 2, 0x3e, 0x28,       // VCM control
+  ILI9486_VMCTR2  , 1, 0x86,             // VCM control2
+  ILI9486_MADCTL  , 1, 0x48,             // Memory Access Control
+  ILI9486_VSCRSADD, 1, 0x00,             // Vertical scroll zero
+  ILI9486_PIXFMT  , 1, 0x55,
+  ILI9486_FRMCTR1 , 2, 0x00, 0x18,
+  ILI9486_DFUNCTR , 3, 0x08, 0x82, 0x27, // Display Function Control
+  0xF2, 1, 0x00,                         // 3Gamma Function Disable
+  ILI9486_GAMMASET , 1, 0x01,             // Gamma curve selected
+  ILI9486_GMCTRP1 , 15, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, // Set Gamma
+                        0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 
+                        0x0E, 0x09, 0x00,
+  ILI9486_GMCTRN1 , 15, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, // Set Gamma
+                        0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 
+                        0x31, 0x36, 0x0F,
+  ILI9486_SLPOUT  , 0x80,                // Exit Sleep
+  ILI9486_DISPON  , 0x80,                // Display on
+  0x00                                   // End of list
+};
+
+// clang-format on
+}  // namespace ili9486
+}  // namespace esphome
