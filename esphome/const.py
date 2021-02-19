96756042750e366a4a3f92bf2bc08238048fd4e3
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-02-19 21:19:33 +0000

    Update const.py

--- esphome/const.py
+++ esphome/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 1
 MINOR_VERSION = 17
-PATCH_VERSION = '0-dev'
+PATCH_VERSION = '0.2118'
 __short_version__ = f'{MAJOR_VERSION}.{MINOR_VERSION}'
 __version__ = f'{__short_version__}.{PATCH_VERSION}'
 
