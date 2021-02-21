8d8ef57abc43f260281dfe47fe0fcd5aacce31d7
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2021-02-21 12:10:44 +0100

    adc fix: GPIO0 not usable if ADC_VCC is used

--- esphome/components/adc/adc_sensor.cpp
+++ esphome/components/adc/adc_sensor.cpp
@@ -16,7 +16,9 @@ void ADCSensor::set_attenuation(adc_attenuation_t attenuation) { this->attenuati
 
 void ADCSensor::setup() {
   ESP_LOGCONFIG(TAG, "Setting up ADC '%s'...", this->get_name().c_str());
+#if not (defined USE_ADC_SENSOR_VCC and ARDUINO_ARCH_ESP8266)
   GPIOPin(this->pin_, INPUT).setup();
+#endif
 
 #ifdef ARDUINO_ARCH_ESP32
   analogSetPinAttenuation(this->pin_, this->attenuation_);
