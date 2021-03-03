9c851ea58afa600688e94a9d8e4e0af3bd81dbd2
RonenM <micronen@gmail.com>
2021-03-03 21:53:13 +0200

    Add Heap Sensors - free/max block/fragmentation
    Heap sensors can be used to detect memory leakage of heap fragmentation which can lead to ESP resets.

---
+++ esphome/components/heap/heap_sensor.cpp
@@ -0,0 +1,17 @@
+#include "heap_sensor.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace heap {
+
+static const char *TAG = "heap";
+
+void HeapSensor::dump_config() { 
+  ESP_LOGCONFIG(TAG, "Heap:");
+  LOG_SENSOR("  ", "Free", this->free_sensor_);
+  LOG_SENSOR("  ", "Fragmentation", this->fragmentation_sensor_);
+  LOG_SENSOR("  ", "Max Block", this->block_sensor_);
+}
+
+}  // namespace heap
+}  // namespace esphome
---
+++ esphome/components/heap/heap_sensor.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/helpers.h"
+#include "esphome/components/sensor/sensor.h"
+
+namespace esphome {
+namespace heap {
+
+class HeapSensor : public PollingComponent {
+ public:
+  void update() override { 
+    int free = ESP.getFreeHeap();
+    int frag = ESP.getHeapFragmentation();
+    int block = ESP.getMaxFreeBlockSize();
+    if (this->free_sensor_          != nullptr) this->free_sensor_->publish_state(free);
+    if (this->fragmentation_sensor_ != nullptr) this->fragmentation_sensor_->publish_state(frag);
+    if (this->block_sensor_         != nullptr) this->block_sensor_->publish_state(block);
+  }
+
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::HARDWARE; }
+
+  void set_free_sensor(sensor::Sensor *free_sensor) { free_sensor_ = free_sensor; }
+  void set_fragmentation_sensor(sensor::Sensor *fragmentation_sensor) { fragmentation_sensor_ = fragmentation_sensor; }
+  void set_block_sensor(sensor::Sensor *block_sensor) { block_sensor_ = block_sensor; }
+
+ protected:
+  sensor::Sensor *free_sensor_{nullptr};
+  sensor::Sensor *fragmentation_sensor_{nullptr};
+  sensor::Sensor *block_sensor_{nullptr};
+};
+
+}  // namespace heap
+}  // namespace esphome
