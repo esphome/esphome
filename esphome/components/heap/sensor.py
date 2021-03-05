1dee376a85d4e49c2ee6b641f6815c5a53322939
RonenM <micronen@gmail.com>
2021-03-05 15:41:08 +0200

    Lint

--- esphome/components/heap/sensor.py
+++ esphome/components/heap/sensor.py
@@ -1,7 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, ICON_COUNTER, DEVICE_CLASS_EMPTY, CONF_FREE, CONF_FRAGMENTATION, CONF_BLOCK
+from esphome.const import CONF_ID, ICON_COUNTER, DEVICE_CLASS_EMPTY, 
+  CONF_FREE, CONF_FRAGMENTATION, CONF_BLOCK
 
 heap_sensor_ns = cg.esphome_ns.namespace('heap')
 HeapSensor = heap_sensor_ns.class_('HeapSensor', cg.PollingComponent)
@@ -12,6 +13,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_FRAGMENTATION): sensor.sensor_schema('Number', ICON_COUNTER, 0, DEVICE_CLASS_EMPTY),
     cv.Optional(CONF_BLOCK): sensor.sensor_schema('Number', ICON_COUNTER, 0, DEVICE_CLASS_EMPTY),
 }).extend(cv.polling_component_schema('60s'))
+
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
