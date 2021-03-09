87f84d4abc7958e85ad6c204e47728541f50c809
Mike Ryan <falter@gmail.com>
2021-03-09 08:07:05 -0600

    Reformat addressable_light_display/display.py
    - Per code review, reformat `display.py` using black

--- esphome/components/addressable_light_display/display.py
+++ esphome/components/addressable_light_display/display.py
@@ -2,25 +2,41 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import display, light
 from esphome.const import (
-    CONF_ID, CONF_LAMBDA, CONF_PAGES, CONF_ADDRESSABLE_LIGHT_ID, CONF_HEIGHT, CONF_WIDTH,
-    CONF_UPDATE_INTERVAL, CONF_PIXEL_MAPPER
+    CONF_ID,
+    CONF_LAMBDA,
+    CONF_PAGES,
+    CONF_ADDRESSABLE_LIGHT_ID,
+    CONF_HEIGHT,
+    CONF_WIDTH,
+    CONF_UPDATE_INTERVAL,
+    CONF_PIXEL_MAPPER,
 )
+
 CODEOWNERS = ["@justfalter"]
 
-IntRef = cg.int_.operator('ref')
-addressable_light_display_ns = cg.esphome_ns.namespace('addressable_light_display')
-AddressableLightDisplay = addressable_light_display_ns.class_('AddressableLightDisplay',
-                                                              display.DisplayBuffer,
-                                                              cg.PollingComponent)
+IntRef = cg.int_.operator("ref")
+addressable_light_display_ns = cg.esphome_ns.namespace("addressable_light_display")
+AddressableLightDisplay = addressable_light_display_ns.class_(
+    "AddressableLightDisplay", display.DisplayBuffer, cg.PollingComponent
+)
 
-CONFIG_SCHEMA = cv.All(display.FULL_DISPLAY_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(AddressableLightDisplay),
-    cv.Required(CONF_ADDRESSABLE_LIGHT_ID): cv.use_id(light.AddressableLightState),
-    cv.Required(CONF_WIDTH): cv.positive_int,
-    cv.Required(CONF_HEIGHT): cv.positive_int,
-    cv.Optional(CONF_UPDATE_INTERVAL, default='16ms'): cv.positive_time_period_milliseconds,
-    cv.Optional(CONF_PIXEL_MAPPER): cv.returning_lambda,
-}), cv.has_at_most_one_key(CONF_PAGES, CONF_LAMBDA))
+CONFIG_SCHEMA = cv.All(
+    display.FULL_DISPLAY_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(AddressableLightDisplay),
+            cv.Required(CONF_ADDRESSABLE_LIGHT_ID): cv.use_id(
+                light.AddressableLightState
+            ),
+            cv.Required(CONF_WIDTH): cv.positive_int,
+            cv.Required(CONF_HEIGHT): cv.positive_int,
+            cv.Optional(
+                CONF_UPDATE_INTERVAL, default="16ms"
+            ): cv.positive_time_period_milliseconds,
+            cv.Optional(CONF_PIXEL_MAPPER): cv.returning_lambda,
+        }
+    ),
+    cv.has_at_most_one_key(CONF_PAGES, CONF_LAMBDA),
+)
 
 
 def to_code(config):
@@ -34,12 +50,15 @@ def to_code(config):
     yield display.register_display(var, config)
 
     if CONF_PIXEL_MAPPER in config:
-        pixel_mapper_template_ = yield cg.process_lambda(config[CONF_PIXEL_MAPPER],
-                                                         [(IntRef, 'x'), (IntRef, 'y')],
-                                                         return_type=cg.int_)
+        pixel_mapper_template_ = yield cg.process_lambda(
+            config[CONF_PIXEL_MAPPER],
+            [(IntRef, "x"), (IntRef, "y")],
+            return_type=cg.int_,
+        )
         cg.add(var.set_pixel_mapper(pixel_mapper_template_))
 
     if CONF_LAMBDA in config:
-        lambda_ = yield cg.process_lambda(config[CONF_LAMBDA], [(display.DisplayBufferRef, 'it')],
-                                          return_type=cg.void)
+        lambda_ = yield cg.process_lambda(
+            config[CONF_LAMBDA], [(display.DisplayBufferRef, "it")], return_type=cg.void
+        )
         cg.add(var.set_writer(lambda_))
