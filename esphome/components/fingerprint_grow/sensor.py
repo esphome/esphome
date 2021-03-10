8dca5e88aed8056b48f7077e0ffe8d1c1b775223
loongyh <loongyh@users.noreply.github.com>
2021-03-11 06:26:45 +0800

    Add device_class for sensor

--- esphome/components/fingerprint_grow/sensor.py
+++ esphome/components/fingerprint_grow/sensor.py
@@ -10,12 +10,24 @@ DEPENDENCIES = ['fingerprint_grow']
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_FINGERPRINT_GROW_ID): cv.use_id(FingerprintGrowComponent),
-    cv.Optional(CONF_FINGERPRINT_COUNT): sensor.sensor_schema(UNIT_EMPTY, ICON_FINGERPRINT, 0),
-    cv.Optional(CONF_STATUS): sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 0),
-    cv.Optional(CONF_CAPACITY): sensor.sensor_schema(UNIT_EMPTY, ICON_DATABASE, 0),
-    cv.Optional(CONF_SECURITY_LEVEL): sensor.sensor_schema(UNIT_EMPTY, ICON_SECURITY, 0),
-    cv.Optional(CONF_LAST_FINGER_ID): sensor.sensor_schema(UNIT_EMPTY, ICON_ACCOUNT, 0),
-    cv.Optional(CONF_LAST_CONFIDENCE): sensor.sensor_schema(UNIT_EMPTY, ICON_ACCOUNT_CHECK, 0),
+    cv.Optional(CONF_FINGERPRINT_COUNT): sensor.sensor_schema(
+        UNIT_EMPTY, ICON_FINGERPRINT, 0, DEVICE_CLASS_EMPTY
+    ),
+    cv.Optional(CONF_STATUS): sensor.sensor_schema(
+        UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY
+    ),
+    cv.Optional(CONF_CAPACITY): sensor.sensor_schema(
+        UNIT_EMPTY, ICON_DATABASE, 0, DEVICE_CLASS_EMPTY
+    ),
+    cv.Optional(CONF_SECURITY_LEVEL): sensor.sensor_schema(
+        UNIT_EMPTY, ICON_SECURITY, 0, DEVICE_CLASS_EMPTY
+    ),
+    cv.Optional(CONF_LAST_FINGER_ID): sensor.sensor_schema(
+        UNIT_EMPTY, ICON_ACCOUNT, 0, DEVICE_CLASS_EMPTY
+    ),
+    cv.Optional(CONF_LAST_CONFIDENCE): sensor.sensor_schema(
+        UNIT_EMPTY, ICON_ACCOUNT_CHECK, 0, DEVICE_CLASS_EMPTY
+    ),
 })
 
 
