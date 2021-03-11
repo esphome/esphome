53409c2d1d3a8246fa014147c329be1e9a3260ff
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-03-11 23:47:08 +0000

    Update edpbox.cpp

--- esphome/components/edpbox/edpbox.cpp
+++ esphome/components/edpbox/edpbox.cpp
@@ -11,6 +11,7 @@ static const uint8_t EDPBOX_REGISTER_COUNT = 7;  // 7x 16-bit registers
 static const uint8_t EDPBOX_REGISTER_START = 108;  // 006C
 
 void EDPBOX::on_modbus_data(const std::vector<uint8_t> &data) {
+
   if (data.size() < 10) {
     ESP_LOGW(TAG, "Invalid size for EDPBOX!");
     return;
@@ -27,10 +28,12 @@ void EDPBOX::on_modbus_data(const std::vector<uint8_t> &data) {
     return (uint32_t(edpbox_get_16bit(i + 2)) << 16) | (uint32_t(edpbox_get_16bit(i + 0)) << 0);
   };
 
-  uint16_t raw_voltage = edpbox_get_32bit(0);
+  // decode
+
+  uint16_t raw_voltage = edpbox_get_16bit(0);
   float voltage = raw_voltage / 10.0f;  // V
 
-  uint32_t raw_current = edpbox_get_32bit(2);
+  uint32_t raw_current = edpbox_get_16bit(2);
   float current = raw_current / 10.0f;  // A
 
   uint32_t raw_active_power = edpbox_get_32bit(6);
@@ -44,8 +47,8 @@ void EDPBOX::on_modbus_data(const std::vector<uint8_t> &data) {
   uint16_t raw_power_factor = edpbox_get_16bit(16);
   float power_factor = raw_power_factor / 100.0f;
 
-  ESP_LOGD(TAG, "EDPBOX: V=%.1f V, I=%.3f A, P=%.1f W, E=%.1f Wh, F=%.1f Hz, PF=%.2f", voltage, current, active_power,
-           active_energy, frequency, power_factor);
+  ESP_LOGD(TAG, "EDPBOX: reading...");
+
   if (this->voltage_sensor_ != nullptr)
     this->voltage_sensor_->publish_state(voltage);
   if (this->current_sensor_ != nullptr)
@@ -60,9 +63,12 @@ void EDPBOX::on_modbus_data(const std::vector<uint8_t> &data) {
     this->power_factor_sensor_->publish_state(power_factor);
 }
 
-void EDPBOX::update() { this->send(EDPBOX_CMD_READ_IN_REGISTERS, EDPBOX_REGISTER_START, EDPBOX_REGISTER_COUNT); }
+void EDPBOX::update() {
+  this->send(EDPBOX_CMD_READ_IN_REGISTERS, EDPBOX_REGISTER_START, EDPBOX_REGISTER_COUNT);
+}
+
 void EDPBOX::dump_config() {
-  ESP_LOGCONFIG(TAG, "EDPBOX:");
+  ESP_LOGCONFIG(TAG, "EDPBOX: dump...");
   ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
   LOG_SENSOR("", "Voltage", this->voltage_sensor_);
   LOG_SENSOR("", "Current", this->current_sensor_);
