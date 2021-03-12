6ad29f9ba197e185be4f5555c23b7411531af8be
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-03-12 04:44:30 +0000

    Update edpbox.cpp

--- esphome/components/edpbox/edpbox.cpp
+++ esphome/components/edpbox/edpbox.cpp
@@ -14,7 +14,7 @@ void EDPBOX::on_modbus_data(const std::vector<uint8_t> &data) {
     return;
   }
 
-  // 01 04 0e 09 21 00 37 09 32 00 01 09 2f 00 0c 00 45 65 9a 
+  // 01 04 0e 09 21 00 37 09 32 00 01 09 2f 00 0c 00 45 65 9a
   // Id Cc Sz Volt  Curr  Volt  Curr  Volt  Curr  Curtt crc
   //           0     2    4     6     8     10    12
 
@@ -34,9 +34,11 @@ void EDPBOX::on_modbus_data(const std::vector<uint8_t> &data) {
   uint32_t raw_current = edpbox_get_16bit(2);
   float current = raw_current / 10.0f;  // A
 
-  auto raw_index = EDPBOX_INDEX;
+  if (EDPBOX_INDEX == 1)
+    ESP_LOGD(TAG, "EDPBOX: Index A");
 
-  ESP_LOGD(TAG, "EDPBOX: Index ", raw_index);
+  if (EDPBOX_INDEX == 2)
+    ESP_LOGD(TAG, "EDPBOX: Index B");
 
   if (this->voltage_sensor_ != nullptr)
     this->voltage_sensor_->publish_state(voltage);
@@ -49,8 +51,8 @@ void EDPBOX::update() {
   // 6C = 108
   EDPBOX_INDEX = 1;
   this->send(0x04, 108, 7);
-  delay(500);
-  delay(500);
+  delay(500); // NOLINT
+  delay(500); // NOLINT
   EDPBOX_INDEX = 2;
   this->send(0x04, 110, 7);
 }
