2d137f8f8299feab152cd349a553da8aaf62fca0
Huub Eikens <wvhe2011@gmail.com>
2021-03-16 19:06:36 +0100

    Put file through black formatter

--- esphome/components/sgp30/sensor.py
+++ esphome/components/sgp30/sensor.py
@@ -1,39 +1,66 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import CONF_ID, CONF_BASELINE, DEVICE_CLASS_EMPTY, CONF_ECO2, CONF_TVOC, ICON_RADIATOR, UNIT_PARTS_PER_MILLION, \
-    UNIT_PARTS_PER_BILLION, UNIT_EMPTY, ICON_MOLECULE_CO2
+from esphome.const import (
+    CONF_ID,
+    CONF_BASELINE,
+    DEVICE_CLASS_EMPTY,
+    CONF_ECO2,
+    CONF_TVOC,
+    ICON_RADIATOR,
+    UNIT_PARTS_PER_MILLION,
+    UNIT_PARTS_PER_BILLION,
+    UNIT_EMPTY,
+    ICON_MOLECULE_CO2,
+)
 
-DEPENDENCIES = ['i2c']
+DEPENDENCIES = ["i2c"]
 
-sgp30_ns = cg.esphome_ns.namespace('sgp30')
-SGP30Component = sgp30_ns.class_('SGP30Component', cg.PollingComponent, i2c.I2CDevice)
+sgp30_ns = cg.esphome_ns.namespace("sgp30")
+SGP30Component = sgp30_ns.class_("SGP30Component", cg.PollingComponent, i2c.I2CDevice)
 
-CONF_ECO2_BASELINE = 'eco2_baseline'
-CONF_TVOC_BASELINE = 'tvoc_baseline'
-CONF_STORE_BASELINE = 'store_baseline'
-CONF_UPTIME = 'uptime'
-CONF_COMPENSATION = 'compensation'
-CONF_HUMIDITY_SOURCE = 'humidity_source'
-CONF_TEMPERATURE_SOURCE = 'temperature_source'
+CONF_ECO2_BASELINE = "eco2_baseline"
+CONF_TVOC_BASELINE = "tvoc_baseline"
+CONF_STORE_BASELINE = "store_baseline"
+CONF_UPTIME = "uptime"
+CONF_COMPENSATION = "compensation"
+CONF_HUMIDITY_SOURCE = "humidity_source"
+CONF_TEMPERATURE_SOURCE = "temperature_source"
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SGP30Component),
-    cv.Required(CONF_ECO2): sensor.sensor_schema(UNIT_PARTS_PER_MILLION,
-                                                 ICON_MOLECULE_CO2, 0, DEVICE_CLASS_EMPTY),
-    cv.Required(CONF_TVOC): sensor.sensor_schema(UNIT_PARTS_PER_BILLION, ICON_RADIATOR, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_ECO2_BASELINE): sensor.sensor_schema(UNIT_EMPTY, ICON_MOLECULE_CO2, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_TVOC_BASELINE): sensor.sensor_schema(UNIT_EMPTY, ICON_RADIATOR, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_STORE_BASELINE, default=True): cv.boolean,
-    cv.Optional(CONF_BASELINE): cv.Schema({
-        cv.Required(CONF_ECO2_BASELINE): cv.hex_uint16_t,
-        cv.Required(CONF_TVOC_BASELINE): cv.hex_uint16_t,
-    }),
-    cv.Optional(CONF_COMPENSATION): cv.Schema({
-        cv.Required(CONF_HUMIDITY_SOURCE): cv.use_id(sensor.Sensor),
-        cv.Required(CONF_TEMPERATURE_SOURCE): cv.use_id(sensor.Sensor)
-    }),
-}).extend(cv.polling_component_schema('1s')).extend(i2c.i2c_device_schema(0x58))
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(SGP30Component),
+            cv.Required(CONF_ECO2): sensor.sensor_schema(
+                UNIT_PARTS_PER_MILLION, ICON_MOLECULE_CO2, 0, DEVICE_CLASS_EMPTY
+            ),
+            cv.Required(CONF_TVOC): sensor.sensor_schema(
+                UNIT_PARTS_PER_BILLION, ICON_RADIATOR, 0, DEVICE_CLASS_EMPTY
+            ),
+            cv.Optional(CONF_ECO2_BASELINE): sensor.sensor_schema(
+                UNIT_EMPTY, ICON_MOLECULE_CO2, 0, DEVICE_CLASS_EMPTY
+            ),
+            cv.Optional(CONF_TVOC_BASELINE): sensor.sensor_schema(
+                UNIT_EMPTY, ICON_RADIATOR, 0, DEVICE_CLASS_EMPTY
+            ),
+            cv.Optional(CONF_STORE_BASELINE, default=True): cv.boolean,
+            cv.Optional(CONF_BASELINE): cv.Schema(
+                {
+                    cv.Required(CONF_ECO2_BASELINE): cv.hex_uint16_t,
+                    cv.Required(CONF_TVOC_BASELINE): cv.hex_uint16_t,
+                }
+            ),
+            cv.Optional(CONF_COMPENSATION): cv.Schema(
+                {
+                    cv.Required(CONF_HUMIDITY_SOURCE): cv.use_id(sensor.Sensor),
+                    cv.Required(CONF_TEMPERATURE_SOURCE): cv.use_id(sensor.Sensor),
+                }
+            ),
+        }
+    )
+    .extend(cv.polling_component_schema("1s"))
+    .extend(i2c.i2c_device_schema(0x58))
+)
 
 
 def to_code(config):
