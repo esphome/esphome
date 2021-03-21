1c517c62c1c373d2bc848af5015425f98736e7df
Karsten Große <karsten.grosse@sympatron.de>
2021-03-21 15:11:45 +0100

    Fixed some linting issues

--- esphome/components/esp32_can/canbus.py
+++ esphome/components/esp32_can/canbus.py
@@ -2,11 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.components import canbus
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_RX_PIN, CONF_TX_PIN
 from esphome.components.canbus import CanbusComponent
-
-CONF_RX_PIN = 'rx_pin'
-CONF_TX_PIN = 'tx_pin'
 
 esp32_can_ns = cg.esphome_ns.namespace('esp32_can')
 esp32_can = esp32_can_ns.class_('ESP32Can', CanbusComponent)
