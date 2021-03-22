6114d331c9006080c8d3c55a7cb18ee9fd15e824
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-03-22 20:12:48 +1300

    Bump version to v1.17.0b1

--- esphome/const.py
+++ esphome/const.py
@@ -1,8 +1,8 @@
 """Constants used by esphome."""
 
 MAJOR_VERSION = 1
-MINOR_VERSION = 18
-PATCH_VERSION = "0-dev"
+MINOR_VERSION = 17
+PATCH_VERSION = "0b1"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
 
