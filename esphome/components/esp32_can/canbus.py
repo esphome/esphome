7a6eb12b37d6e6b2d8d6a98c06835fec3f5f0e92
Karsten Große <karsten.grosse@sympatron.de>
2021-03-23 00:13:16 +0100

    limited esp32-can to esp32

--- esphome/components/esp32_can/canbus.py
+++ esphome/components/esp32_can/canbus.py
@@ -2,11 +2,13 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.components import canbus
-from esphome.const import CONF_ID, CONF_RX_PIN, CONF_TX_PIN
+from esphome.const import CONF_ID, CONF_RX_PIN, CONF_TX_PIN, ESP_PLATFORM_ESP32
 from esphome.components.canbus import CanbusComponent
 
 esp32_can_ns = cg.esphome_ns.namespace("esp32_can")
 esp32_can = esp32_can_ns.class_("ESP32Can", CanbusComponent)
+
+ESP_PLATFORMS = [ESP_PLATFORM_ESP32]
 
 CONFIG_SCHEMA = canbus.CANBUS_SCHEMA.extend(
     {
