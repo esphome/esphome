0d07b897d5a642aaf6268caa204a4a5dd2d4e2b9
Sympatron <35803463+Sympatron@users.noreply.github.com>
2021-03-23 09:40:26 +0100

    Validate bit rate for esp32-can platform

--- esphome/components/esp32_can/canbus.py
+++ esphome/components/esp32_can/canbus.py
@@ -3,16 +3,27 @@ import esphome.config_validation as cv
 from esphome import pins
 from esphome.components import canbus
 from esphome.const import CONF_ID, CONF_RX_PIN, CONF_TX_PIN, ESP_PLATFORM_ESP32
-from esphome.components.canbus import CanbusComponent
+from esphome.components.canbus import CanbusComponent, CanSpeed, CONF_BIT_RATE
 
 esp32_can_ns = cg.esphome_ns.namespace("esp32_can")
 esp32_can = esp32_can_ns.class_("ESP32Can", CanbusComponent)
 
 ESP_PLATFORMS = [ESP_PLATFORM_ESP32]
 
+# Currently the driver only supports a subset of the bit rates defined in canbus
+CAN_SPEEDS = {
+    "50KBPS": CanSpeed.CAN_50KBPS,
+    "100KBPS": CanSpeed.CAN_100KBPS,
+    "125KBPS": CanSpeed.CAN_125KBPS,
+    "250KBPS": CanSpeed.CAN_250KBPS,
+    "500KBPS": CanSpeed.CAN_500KBPS,
+    "1000KBPS": CanSpeed.CAN_1000KBPS,
+}
+
 CONFIG_SCHEMA = canbus.CANBUS_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(esp32_can),
+        cv.Optional(CONF_BIT_RATE, default="125KBPS"): cv.enum(CAN_SPEEDS, upper=True),
         cv.Required(CONF_RX_PIN): pins.gpio_input_pin_schema,
         cv.Required(CONF_TX_PIN): pins.gpio_output_pin_schema,
     }
