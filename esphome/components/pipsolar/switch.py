16cf89744b1aa62817d2d1a43f44760bd99b92fd
Andreas Hergert <andreashergert1984@googlemail.com>
2021-04-05 11:13:39 +0200

    tidy

--- esphome/components/pipsolar/switch.py
+++ esphome/components/pipsolar/switch.py
@@ -9,7 +9,7 @@ DEPENDENCIES = ['uart']
 CONF_OUTPUT_SOURCE_PRIORITY_UTILITY = 'output_source_priority_utility'
 CONF_OUTPUT_SOURCE_PRIORITY_SOLAR = 'output_source_priority_solar'
 CONF_OUTPUT_SOURCE_PRIORITY_BATTERY = 'output_source_priority_battery'
-CONF_INPUT_VOLTAGE_RANGE = 'input_voltage_range'  #00 for appliance, 01 for UPS
+CONF_INPUT_VOLTAGE_RANGE = 'input_voltage_range'
 CONF_PV_OK_CONDITION_FOR_PARALLEL = 'pv_ok_condition_for_parallel'
 CONF_PV_POWER_BALANCE = 'pv_power_balance'
 
@@ -30,59 +30,59 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_INPUT_VOLTAGE_RANGE): PIPSWITCH_SCHEMA,
     cv.Optional(CONF_PV_OK_CONDITION_FOR_PARALLEL): PIPSWITCH_SCHEMA,
     cv.Optional(CONF_PV_POWER_BALANCE): PIPSWITCH_SCHEMA,
- })
+})
 
 
 def to_code(config):
-    paren = yield cg.get_variable(config[CONF_PIPSOLAR_ID])
-    if CONF_OUTPUT_SOURCE_PRIORITY_UTILITY in config:
-      conf = config[CONF_OUTPUT_SOURCE_PRIORITY_UTILITY]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield cg.register_component(var, conf)
-      yield switch.register_switch(var, conf)
-      cg.add(paren.set_output_source_priority_utility_switch(var))
-      cg.add(var.set_parent(paren))
-      cg.add(var.set_on_command("POP00"))
-    if CONF_OUTPUT_SOURCE_PRIORITY_SOLAR in config:
-      conf = config[CONF_OUTPUT_SOURCE_PRIORITY_SOLAR]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield cg.register_component(var, conf)
-      yield switch.register_switch(var, conf)
-      cg.add(paren.set_output_source_priority_solar_switch(var))
-      cg.add(var.set_parent(paren))
-      cg.add(var.set_on_command("POP01"))
-    if CONF_OUTPUT_SOURCE_PRIORITY_BATTERY in config:
-      conf = config[CONF_OUTPUT_SOURCE_PRIORITY_BATTERY]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield cg.register_component(var, conf)
-      yield switch.register_switch(var, conf)
-      cg.add(paren.set_output_source_priority_battery_switch(var))
-      cg.add(var.set_parent(paren))
-      cg.add(var.set_on_command("POP02"))
-    if CONF_INPUT_VOLTAGE_RANGE in config:
-      conf = config[CONF_INPUT_VOLTAGE_RANGE]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield cg.register_component(var, conf)
-      yield switch.register_switch(var, conf)
-      cg.add(paren.set_input_voltage_range_switch(var))
-      cg.add(var.set_parent(paren))
-      cg.add(var.set_on_command("PGR01"))
-      cg.add(var.set_off_command("PGR00"))
-    if CONF_PV_OK_CONDITION_FOR_PARALLEL in config:
-      conf = config[CONF_PV_OK_CONDITION_FOR_PARALLEL]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield cg.register_component(var, conf)
-      yield switch.register_switch(var, conf)
-      cg.add(paren.set_pv_ok_condition_for_parallel_switch(var))
-      cg.add(var.set_parent(paren))
-      cg.add(var.set_on_command("PPVOKC1"))
-      cg.add(var.set_off_command("PPVOKC0"))
-    if CONF_PV_POWER_BALANCE in config:
-      conf = config[CONF_PV_POWER_BALANCE]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield cg.register_component(var, conf)
-      yield switch.register_switch(var, conf)
-      cg.add(paren.set_pv_power_balance_switch(var))
-      cg.add(var.set_parent(paren))
-      cg.add(var.set_on_command("PSPB1"))
-      cg.add(var.set_off_command("PSPB0"))
+  paren = yield cg.get_variable(config[CONF_PIPSOLAR_ID])
+  if CONF_OUTPUT_SOURCE_PRIORITY_UTILITY in config:
+    conf = config[CONF_OUTPUT_SOURCE_PRIORITY_UTILITY]
+    var = cg.new_Pvariable(conf[CONF_ID])
+    yield cg.register_component(var, conf)
+    yield switch.register_switch(var, conf)
+    cg.add(paren.set_output_source_priority_utility_switch(var))
+    cg.add(var.set_parent(paren))
+    cg.add(var.set_on_command("POP00"))
+  if CONF_OUTPUT_SOURCE_PRIORITY_SOLAR in config:
+    conf = config[CONF_OUTPUT_SOURCE_PRIORITY_SOLAR]
+    var = cg.new_Pvariable(conf[CONF_ID])
+    yield cg.register_component(var, conf)
+    yield switch.register_switch(var, conf)
+    cg.add(paren.set_output_source_priority_solar_switch(var))
+    cg.add(var.set_parent(paren))
+    cg.add(var.set_on_command("POP01"))
+  if CONF_OUTPUT_SOURCE_PRIORITY_BATTERY in config:
+    conf = config[CONF_OUTPUT_SOURCE_PRIORITY_BATTERY]
+    var = cg.new_Pvariable(conf[CONF_ID])
+    yield cg.register_component(var, conf)
+    yield switch.register_switch(var, conf)
+    cg.add(paren.set_output_source_priority_battery_switch(var))
+    cg.add(var.set_parent(paren))
+    cg.add(var.set_on_command("POP02"))
+  if CONF_INPUT_VOLTAGE_RANGE in config:
+    conf = config[CONF_INPUT_VOLTAGE_RANGE]
+    var = cg.new_Pvariable(conf[CONF_ID])
+    yield cg.register_component(var, conf)
+    yield switch.register_switch(var, conf)
+    cg.add(paren.set_input_voltage_range_switch(var))
+    cg.add(var.set_parent(paren))
+    cg.add(var.set_on_command("PGR01"))
+    cg.add(var.set_off_command("PGR00"))
+  if CONF_PV_OK_CONDITION_FOR_PARALLEL in config:
+    conf = config[CONF_PV_OK_CONDITION_FOR_PARALLEL]
+    var = cg.new_Pvariable(conf[CONF_ID])
+    yield cg.register_component(var, conf)
+    yield switch.register_switch(var, conf)
+    cg.add(paren.set_pv_ok_condition_for_parallel_switch(var))
+    cg.add(var.set_parent(paren))
+    cg.add(var.set_on_command("PPVOKC1"))
+    cg.add(var.set_off_command("PPVOKC0"))
+  if CONF_PV_POWER_BALANCE in config:
+    conf = config[CONF_PV_POWER_BALANCE]
+    var = cg.new_Pvariable(conf[CONF_ID])
+    yield cg.register_component(var, conf)
+    yield switch.register_switch(var, conf)
+    cg.add(paren.set_pv_power_balance_switch(var))
+    cg.add(var.set_parent(paren))
+    cg.add(var.set_on_command("PSPB1"))
+    cg.add(var.set_off_command("PSPB0"))
--- esphome/components/pipsolar/text_sensor.py
+++ esphome/components/pipsolar/text_sensor.py
@@ -4,112 +4,115 @@ from esphome.components import text_sensor
 from esphome.const import CONF_ID, CONF_PIPSOLAR_ID
 from . import PipsolarComponent, pipsolar_ns
 
-DEPENDENCIES = ['uart']
+DEPENDENCIES = ["uart"]
 
-CONF_DEVICE_MODE = 'device_mode'
-CONF_LAST_QPIGS = 'last_qpigs'
-CONF_LAST_QPIRI = 'last_qpiri'
-CONF_LAST_QMOD = 'last_qmod'
-CONF_LAST_QFLAG = 'last_qflag'
-CONF_LAST_QPIWS = 'last_qpiws'
-CONF_LAST_QT = 'last_qt'
-CONF_LAST_QMN = 'last_qmn'
+CONF_DEVICE_MODE = "device_mode"
+CONF_LAST_QPIGS = "last_qpigs"
+CONF_LAST_QPIRI = "last_qpiri"
+CONF_LAST_QMOD = "last_qmod"
+CONF_LAST_QFLAG = "last_qflag"
+CONF_LAST_QPIWS = "last_qpiws"
+CONF_LAST_QT = "last_qt"
+CONF_LAST_QMN = "last_qmn"
 
 pipsolar_text_sensor_ns = pipsolar_ns.class_(
     "PipsolarTextSensor", text_sensor.TextSensor, cg.Component
 )
 
-CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
-    cv.Required(CONF_PIPSOLAR_ID): cv.use_id(PipsolarComponent),
-    cv.Optional(CONF_DEVICE_MODE ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
+    {
+        cv.Required(CONF_PIPSOLAR_ID): cv.use_id(PipsolarComponent),
+        cv.Optional(CONF_DEVICE_MODE): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(pipsolar_text_sensor_ns),
             }
         ),
-    cv.Optional(CONF_LAST_QPIGS ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_LAST_QPIGS): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(pipsolar_text_sensor_ns),
             }
         ),
-    cv.Optional(CONF_LAST_QPIRI ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_LAST_QPIRI): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(pipsolar_text_sensor_ns),
             }
         ),
-    cv.Optional(CONF_LAST_QMOD ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_LAST_QMOD): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(pipsolar_text_sensor_ns),
             }
         ),
-    cv.Optional(CONF_LAST_QFLAG ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_LAST_QFLAG): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(pipsolar_text_sensor_ns),
             }
         ),
-    cv.Optional(CONF_LAST_QPIWS ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_LAST_QPIWS): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(pipsolar_text_sensor_ns),
             }
         ),
-    cv.Optional(CONF_LAST_QT ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_LAST_QT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(pipsolar_text_sensor_ns),
             }
         ),
-    cv.Optional(CONF_LAST_QMN ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional(CONF_LAST_QMN): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(pipsolar_text_sensor_ns),
             }
         ),
-    })
+    }
+)
+
 
 def to_code(config):
     paren = yield cg.get_variable(config[CONF_PIPSOLAR_ID])
     if CONF_DEVICE_MODE in config:
-      conf = config[CONF_DEVICE_MODE]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield text_sensor.register_text_sensor(var, conf)
-      yield cg.register_component(var, conf)
-      cg.add(paren.set_device_mode_sensor(var))
+        conf = config[CONF_DEVICE_MODE]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        yield text_sensor.register_text_sensor(var, conf)
+        yield cg.register_component(var, conf)
+        cg.add(paren.set_device_mode_sensor(var))
     if CONF_LAST_QPIGS in config:
-      conf = config[CONF_LAST_QPIGS]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield text_sensor.register_text_sensor(var, conf)
-      yield cg.register_component(var, conf)
-      cg.add(paren.set_last_qpigs_sensor(var))
+        conf = config[CONF_LAST_QPIGS]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        yield text_sensor.register_text_sensor(var, conf)
+        yield cg.register_component(var, conf)
+        cg.add(paren.set_last_qpigs_sensor(var))
     if CONF_LAST_QPIRI in config:
-      conf = config[CONF_LAST_QPIRI]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield text_sensor.register_text_sensor(var, conf)
-      yield cg.register_component(var, conf)
-      cg.add(paren.set_last_qpiri_sensor(var))
+        conf = config[CONF_LAST_QPIRI]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        yield text_sensor.register_text_sensor(var, conf)
+        yield cg.register_component(var, conf)
+        cg.add(paren.set_last_qpiri_sensor(var))
     if CONF_LAST_QMOD in config:
-      conf = config[CONF_LAST_QMOD]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield text_sensor.register_text_sensor(var, conf)
-      yield cg.register_component(var, conf)
-      cg.add(paren.set_last_qmod_sensor(var))
+        conf = config[CONF_LAST_QMOD]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        yield text_sensor.register_text_sensor(var, conf)
+        yield cg.register_component(var, conf)
+        cg.add(paren.set_last_qmod_sensor(var))
     if CONF_LAST_QFLAG in config:
-      conf = config[CONF_LAST_QFLAG]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield text_sensor.register_text_sensor(var, conf)
-      yield cg.register_component(var, conf)
-      cg.add(paren.set_last_qflag_sensor(var))
+        conf = config[CONF_LAST_QFLAG]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        yield text_sensor.register_text_sensor(var, conf)
+        yield cg.register_component(var, conf)
+        cg.add(paren.set_last_qflag_sensor(var))
     if CONF_LAST_QPIWS in config:
-      conf = config[CONF_LAST_QPIWS]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield text_sensor.register_text_sensor(var, conf)
-      yield cg.register_component(var, conf)
-      cg.add(paren.set_last_qpiws_sensor(var))
+        conf = config[CONF_LAST_QPIWS]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        yield text_sensor.register_text_sensor(var, conf)
+        yield cg.register_component(var, conf)
+        cg.add(paren.set_last_qpiws_sensor(var))
     if CONF_LAST_QT in config:
-      conf = config[CONF_LAST_QT]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield text_sensor.register_text_sensor(var, conf)
-      yield cg.register_component(var, conf)
-      cg.add(paren.set_last_qt_sensor(var))
+        conf = config[CONF_LAST_QT]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        yield text_sensor.register_text_sensor(var, conf)
+        yield cg.register_component(var, conf)
+        cg.add(paren.set_last_qt_sensor(var))
     if CONF_LAST_QMN in config:
-      conf = config[CONF_LAST_QMN]
-      var = cg.new_Pvariable(conf[CONF_ID])
-      yield text_sensor.register_text_sensor(var, conf)
-      yield cg.register_component(var, conf)
-      cg.add(paren.set_last_qmn_sensor(var))
+        conf = config[CONF_LAST_QMN]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        yield text_sensor.register_text_sensor(var, conf)
+        yield cg.register_component(var, conf)
+        cg.add(paren.set_last_qmn_sensor(var))
