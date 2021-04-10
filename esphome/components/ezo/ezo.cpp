fffd2a38168207ec6fa9942487cb5382e00bdbc8
Senex Crenshaw <senexcrenshaw@gmail.com>
2021-04-10 13:32:17 +0000

    Added in LED

--- esphome/components/ezo/ezo.cpp
+++ esphome/components/ezo/ezo.cpp
@@ -19,44 +19,47 @@ void EZOSensor::dump_config() {
 }
 
 void EZOSensor::update() {
-  if (this->state_ & EZO_STATE_WAIT) {
-    ESP_LOGE(TAG, "update overrun, still waiting for previous response");
+  if (!this->commands_.empty()) {
+    ESP_LOGE(TAG, "update overrun, still waiting for previous response");  // Not sure if we care
     return;
   }
-  if (!this->commands_.empty()) {
-    this->add_command("R", "", "");
+
+  if (this->commands_.empty()) {  // Read if nothing to do
+    this->get_state();
   }
 
   ezo_command *to_run = this->commands_.front();
 
-  auto data = reinterpret_cast<const uint8_t *>(&to_run[0]);
+  auto data = reinterpret_cast<const uint8_t *>(&to_run->command.c_str()[0]);
+  for (int i = 0; i < to_run->command.length(); i++) {
+    ESP_LOGD(TAG, "Sending command index: %d char: \"%c\" hex: 0x%02X", i, data[i], data[i]);
+  }
+
   this->write_bytes_raw(data, to_run->command.length());
 
-  this->state_ |= EZO_STATE_WAIT;
   this->start_time_ = millis();
-  this->wait_time_ = 900;
+  // this->wait_time_ = to_run->delay_ms;
 }
 
 void EZOSensor::loop() {
-  uint8_t buf[20];
-  if (!(this->state_ & EZO_STATE_WAIT)) {
-    if (this->state_ & EZO_STATE_SEND_TEMP) {
-      int len = sprintf((char *) buf, "T,%0.3f", this->tempcomp_);
-      this->write_bytes_raw(buf, len);
-      this->state_ = EZO_STATE_WAIT | EZO_STATE_WAIT_TEMP;
-      this->start_time_ = millis();
-      this->wait_time_ = 300;
-    }
+  if (this->commands_.empty()) {
     return;
   }
-  if (millis() - this->start_time_ < this->wait_time_)
+
+  if (millis() - this->start_time_ < this->commands_.front()->delay_ms)
     return;
+
+  uint8_t buf[20];
+
   buf[0] = 0;
+
   if (!this->read_bytes_raw(buf, 20)) {
     ESP_LOGE(TAG, "read error");
-    this->state_ = 0;
+    delete this->commands_.front();
+    this->commands_.pop_front();
     return;
   }
+
   switch (buf[0]) {
     case 1:
       break;
@@ -72,21 +75,32 @@ void EZOSensor::loop() {
       ESP_LOGE(TAG, "device returned an unknown response: %d", buf[0]);
       break;
   }
-  if (this->state_ & EZO_STATE_WAIT_TEMP) {
-    this->state_ = 0;
-    return;
-  }
-  this->state_ &= ~EZO_STATE_WAIT;
-  if (buf[0] != 1)
-    return;
+  ezo_command *to_run = this->commands_.front();
 
-  float val = atof((char *) &buf[1]);
-  this->publish_state(val);
+  ESP_LOGD(TAG, "Received buffer \"%s\" for command type %s", buf, EzoCommandTypeStrings[to_run->command_type]);
+  if (buf[0] == 1) {
+    switch (to_run->command_type) {
+      case EZO_COMMAND_TYPE::EZO_READ: {
+        float val = atof((char *) &buf[1]);
+        this->publish_state(val);
+        break;
+      }
+      case EZO_COMMAND_TYPE::EZO_LED: {
+        break;
+      }
+    }
+  }
+
+  delete this->commands_.front();
+  this->commands_.pop_front();
 }
 
+// LED control
+void EZOSensor::set_led_state(bool on) { this->add_command("L," + on ? "1" : "0", EZO_COMMAND_TYPE::EZO_LED); }
+
 void EZOSensor::set_tempcomp_value(float temp) {
-  this->tempcomp_ = temp;
-  this->state_ |= EZO_STATE_SEND_TEMP;
+  // this->tempcomp_ = temp;
+  // this->state_ |= EZO_STATE_SEND_TEMP;
 }
 
 }  // namespace ezo
