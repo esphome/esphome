02fbc6d8fa134a712a03d0ea7d167cb4dbb5da79
Senex Crenshaw <senexcrenshaw@gmail.com>
2021-04-10 12:02:46 +0000

    Initial setup

--- esphome/components/ezo/ezo.cpp
+++ esphome/components/ezo/ezo.cpp
@@ -6,6 +6,10 @@ namespace ezo {
 
 static const char *const TAG = "ezo.sensor";
 
+static const uint16_t EZO_STATE_WAIT = 1;
+static const uint16_t EZO_STATE_SEND_TEMP = 2;
+static const uint16_t EZO_STATE_WAIT_TEMP = 4;
+
 void EZOSensor::dump_config() {
   LOG_SENSOR("", "EZO", this);
   LOG_I2C_DEVICE(this);
@@ -15,75 +19,44 @@ void EZOSensor::dump_config() {
 }
 
 void EZOSensor::update() {
-  // Check if a read is in there already and if not insert on in the second position
-
-  if (!this->commands_.empty() && this->commands_.front()->command_type != EzoCommandType::EZO_READ &&
-      this->commands_.size() > 1) {
-    bool found = false;
-
-    for (auto &i : this->commands_) {
-      if (i->command_type == EzoCommandType::EZO_READ) {
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
-      EzoCommand *ezo_command = new EzoCommand;
-      ezo_command->command = "R";
-      ezo_command->command_type = EzoCommandType::EZO_READ;
-      ezo_command->delay_ms = 900;
-
-      std::deque<EzoCommand *>::iterator it = this->commands_.begin();
-      ++it;
-      this->commands_.insert(it, ezo_command);
-    }
-
+  if (this->state_ & EZO_STATE_WAIT) {
+    ESP_LOGE(TAG, "update overrun, still waiting for previous response");
     return;
   }
+  if (!this->commands_.empty()) {
+    this->add_command("R", "", "");
+  }
 
-  this->get_state();
+  ezo_command *to_run = this->commands_.front();
+
+  auto data = reinterpret_cast<const uint8_t *>(&to_run[0]);
+  this->write_bytes_raw(data, to_run->command.length());
+
+  this->state_ |= EZO_STATE_WAIT;
+  this->start_time_ = millis();
+  this->wait_time_ = 900;
 }
 
 void EZOSensor::loop() {
-  if (this->commands_.empty()) {
-    return;
-  }
-
-  EzoCommand *to_run = this->commands_.front();
-
-  if (!to_run->command_sent) {
-    auto data = reinterpret_cast<const uint8_t *>(&to_run->command.c_str()[0]);
-    ESP_LOGVV(TAG, "Sending command \"%s\"", data);
-
-    this->write_bytes_raw(data, to_run->command.length());
-
-    if (to_run->command_type == EzoCommandType::EZO_SLEEP ||
-        to_run->command_type == EzoCommandType::EZO_I2C) {  // Commands with no return data
-      delete to_run;
-      this->commands_.pop_front();
-      return;
+  uint8_t buf[20];
+  if (!(this->state_ & EZO_STATE_WAIT)) {
+    if (this->state_ & EZO_STATE_SEND_TEMP) {
+      int len = sprintf((char *) buf, "T,%0.3f", this->tempcomp_);
+      this->write_bytes_raw(buf, len);
+      this->state_ = EZO_STATE_WAIT | EZO_STATE_WAIT_TEMP;
+      this->start_time_ = millis();
+      this->wait_time_ = 300;
     }
-
-    this->start_time_ = millis();
-    to_run->command_sent = true;
     return;
   }
-
-  if (millis() - this->start_time_ < to_run->delay_ms)
+  if (millis() - this->start_time_ < this->wait_time_)
     return;
-
-  uint8_t buf[32];
-
   buf[0] = 0;
-
-  if (!this->read_bytes_raw(buf, 32)) {
+  if (!this->read_bytes_raw(buf, 20)) {
     ESP_LOGE(TAG, "read error");
-    delete to_run;
-    this->commands_.pop_front();
+    this->state_ = 0;
     return;
   }
-
   switch (buf[0]) {
     case 1:
       break;
@@ -99,93 +72,22 @@ void EZOSensor::loop() {
       ESP_LOGE(TAG, "device returned an unknown response: %d", buf[0]);
       break;
   }
-
-  ESP_LOGVV(TAG, "Received buffer \"%s\" for command type %s", buf, EZO_COMMAND_TYPE_STRINGS[to_run->command_type]);
-
-  // for (int index = 0; index < 32; ++index) {
-  //   ESP_LOGD(TAG, "Received buffer index: %d char: \"%c\" %d", index, buf[index], buf[index]);
-  // }
-
-  if (buf[0] == 1 || to_run->command_type == EzoCommandType::EZO_CALIBRATION) {  // EZO_CALIBRATION returns 0-3
-    std::string payload = reinterpret_cast<char *>(&buf[1]);
-    if (!payload.empty()) {
-      switch (to_run->command_type) {
-        case EzoCommandType::EZO_READ: {
-          auto val = parse_float(payload);
-          if (!val.has_value()) {
-            ESP_LOGW(TAG, "Can't convert '%s' to number!", payload.c_str());
-          } else {
-            this->publish_state(*val);
-          }
-          break;
-        }
-        case EzoCommandType::EZO_LED: {
-          this->led_callback_.call(payload.back() == '1');
-          break;
-        }
-        case EzoCommandType::EZO_DEVICE_INFORMATION: {
-          int start_location = 0;
-          if ((start_location = payload.find(',')) != std::string::npos) {
-            this->device_infomation_callback_.call(payload.substr(start_location + 1));
-          }
-          break;
-        }
-        case EzoCommandType::EZO_SLOPE: {
-          int start_location = 0;
-          if ((start_location = payload.find(',')) != std::string::npos) {
-            this->slope_callback_.call(payload.substr(start_location + 1));
-          }
-          break;
-        }
-        case EzoCommandType::EZO_CALIBRATION: {
-          int start_location = 0;
-          if ((start_location = payload.find(',')) != std::string::npos) {
-            this->calibration_callback_.call(payload.substr(start_location + 1));
-          }
-          break;
-        }
-        case EzoCommandType::EZO_T: {
-          this->t_callback_.call(payload);
-          break;
-        }
-        case EzoCommandType::EZO_CUSTOM: {
-          this->custom_callback_.call(payload);
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-    }
+  if (this->state_ & EZO_STATE_WAIT_TEMP) {
+    this->state_ = 0;
+    return;
   }
+  this->state_ &= ~EZO_STATE_WAIT;
+  if (buf[0] != 1)
+    return;
 
-  delete to_run;
-  this->commands_.pop_front();
+  float val = atof((char *) &buf[1]);
+  this->publish_state(val);
 }
 
-// T
-void EZOSensor::set_t(const std::string &value) {
-  std::string to_send = "T," + value;
-  this->add_command(to_send, EzoCommandType::EZO_T);
+void EZOSensor::set_tempcomp_value(float temp) {
+  this->tempcomp_ = temp;
+  this->state_ |= EZO_STATE_SEND_TEMP;
 }
-
-// Calibration
-void EZOSensor::clear_calibration() { this->add_command("Cal,clear", EzoCommandType::EZO_CALIBRATION); }
-
-void EZOSensor::set_calibration(const std::string &point, const std::string &value) {
-  std::string to_send = "Cal," + point + "," + value;
-  this->add_command(to_send, EzoCommandType::EZO_CALIBRATION, 900);
-}
-
-// LED control
-void EZOSensor::set_led_state(bool on) {
-  std::string to_send = "L,";
-  to_send += on ? "1" : "0";
-  this->add_command(to_send, EzoCommandType::EZO_LED);
-}
-
-// Custom
-void EZOSensor::set_custom(const std::string &to_send) { this->add_command(to_send, EzoCommandType::EZO_CUSTOM); }
 
 }  // namespace ezo
 }  // namespace esphome
--- esphome/components/ezo/ezo.h
+++ esphome/components/ezo/ezo.h
@@ -8,27 +8,11 @@
 namespace esphome {
 namespace ezo {
 
-enum EzoCommandType : uint8_t {
-  EZO_READ = 0,
-  EZO_LED = 1,
-  EZO_DEVICE_INFORMATION = 2,
-  EZO_SLOPE = 3,
-  EZO_CALIBRATION,
-  EZO_SLEEP = 4,
-  EZO_I2C = 5,
-  EZO_T = 6,
-  EZO_CUSTOM = 7
-};
-static const char *const EZO_COMMAND_TYPE_STRINGS[] = {"EZO_READ",  "EZO_LED",         "EZO_DEVICE_INFORMATION",
-                                                       "EZO_SLOPE", "EZO_CALIBRATION", "EZO_SLEEP",
-                                                       "EZO_I2C",   "EZO_T",           "EZO_CUSTOM"};
-
-class EzoCommand {
+class ezo_command {
  public:
   std::string command;
-  uint16_t delay_ms = 0;
-  bool command_sent = false;
-  EzoCommandType command_type;
+  std::string arguments;
+  std::string return_message;
 };
 
 /// This class implements support for the EZO circuits in i2c mode
@@ -39,71 +23,22 @@ class EZOSensor : public sensor::Sensor, public PollingComponent, public i2c::I2
   void update() override;
   float get_setup_priority() const override { return setup_priority::DATA; };
 
-  void add_command(const std::string &command, EzoCommandType command_type, uint16_t delay_ms = 300) {
-    EzoCommand *ezo_command = new EzoCommand;
-    ezo_command->command = command;
-    ezo_command->command_type = command_type;
-    ezo_command->delay_ms = delay_ms;
-    this->commands_.push_back(ezo_command);
+  void set_tempcomp_value(float temp);
+  void add_command(std::string command, std::string arguments, std::string return_message) {
+    ezo_command *e_command = new ezo_command;
+    e_command->command = command;
+    e_command->arguments = arguments;
+    e_command->return_message = return_message;
+    this->commands_.push_back(e_command);
   };
 
-  // R
-  void get_state(int pos = 0) { this->add_command("R", EzoCommandType::EZO_READ, 900); }
-
-  // I2C
-  void set_i2c() { this->add_command("I2c,100", EzoCommandType::EZO_I2C); }  // NOLINT otherwise we get set_i2_c
-
-  // Sleep
-  void set_sleep() { this->add_command("Sleep", EzoCommandType::EZO_SLEEP); }
-
-  // Calibration
-  void get_calibration() { this->add_command("Cal,?", EzoCommandType::EZO_CALIBRATION); }
-  void set_calibration(const std::string &point, const std::string &value);
-  void clear_calibration();
-  void add_calibration_callback(std::function<void(std::string)> &&callback) {
-    this->calibration_callback_.add(std::move(callback));
-  }
-
-  // Device Information
-  void get_device_information() { this->add_command("i", EzoCommandType::EZO_DEVICE_INFORMATION); }
-  void add_device_infomation_callback(std::function<void(std::string)> &&callback) {
-    this->device_infomation_callback_.add(std::move(callback));
-  }
-
-  // Slope
-  void get_slope() { this->add_command("Slope,?", EzoCommandType::EZO_SLOPE); }
-  void add_slope_callback(std::function<void(std::string)> &&callback) {
-    this->slope_callback_.add(std::move(callback));
-  }
-
-  // LED
-  void set_led_state(bool on);
-  void get_led_state() { this->add_command("L,?", EzoCommandType::EZO_LED); }
-  void add_led_state_callback(std::function<void(bool)> &&callback) { this->led_callback_.add(std::move(callback)); }
-
-  // T
-  void set_tempcomp_value(float temp) { this->set_t(to_string(temp)); }  // For backwards compatibility
-  void get_t() { this->add_command("T,?", EzoCommandType::EZO_T); }
-  void set_t(const std::string &value);
-  void add_t_callback(std::function<void(std::string)> &&callback) { this->t_callback_.add(std::move(callback)); }
-
-  // Custom
-  void set_custom(const std::string &to_send);
-  void add_custom_callback(std::function<void(std::string)> &&callback) {
-    this->custom_callback_.add(std::move(callback));
-  }
-
  protected:
-  std::deque<EzoCommand *> commands_;
+  std::deque<ezo_command *> commands_;
 
-  CallbackManager<void(std::string)> device_infomation_callback_{};
-  CallbackManager<void(std::string)> calibration_callback_{};
-  CallbackManager<void(std::string)> slope_callback_{};
-  CallbackManager<void(std::string)> t_callback_{};
-  CallbackManager<void(std::string)> custom_callback_{};
-  CallbackManager<void(bool)> led_callback_{};
-
-  uint32_t start_time_ = 0;
+  unsigned long start_time_ = 0;
+  unsigned long wait_time_ = 0;
+  uint16_t state_ = 0;
+  float tempcomp_;
 };
 
 }  // namespace ezo
