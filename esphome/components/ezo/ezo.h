50c0150ec74a2fcbb44485c53505f85705306376
Senex Crenshaw <senexcrenshaw@gmail.com>
2021-04-10 13:32:17 +0000

    Added in LED

--- esphome/components/ezo/ezo.h
+++ esphome/components/ezo/ezo.h
@@ -8,11 +8,14 @@
 namespace esphome {
 namespace ezo {
 
+enum EZO_COMMAND_TYPE : uint8_t { EZO_READ = 0, EZO_LED = 1 };
+static const char *EzoCommandTypeStrings[] = {"EZO_READ", "EZO_LED"};
+
 class ezo_command {
  public:
   std::string command;
-  std::string arguments;
-  std::string return_message;
+  uint32_t delay_ms = 300;
+  EZO_COMMAND_TYPE command_type;
 };
 
 /// This class implements support for the EZO circuits in i2c mode
@@ -23,22 +26,30 @@ class EZOSensor : public sensor::Sensor, public PollingComponent, public i2c::I2
   void update() override;
   float get_setup_priority() const override { return setup_priority::DATA; };
 
-  void set_tempcomp_value(float temp);
-  void add_command(std::string command, std::string arguments, std::string return_message) {
+  void add_command(std::string command, EZO_COMMAND_TYPE command_type) {
     ezo_command *e_command = new ezo_command;
     e_command->command = command;
-    e_command->arguments = arguments;
-    e_command->return_message = return_message;
+    e_command->command_type = command_type;
+
     this->commands_.push_back(e_command);
   };
 
+  void set_tempcomp_value(float temp);
+  void get_state() { this->add_command("R", EZO_COMMAND_TYPE::EZO_READ); }
+
+  // LED
+  void set_led_state(bool on);
+  void get_led_state() { this->add_command("L,?", EZO_COMMAND_TYPE::EZO_LED); }
+  void add_led_state_callback(std::function<void(bool)> &&callback) { this->led_callback_.add(std::move(callback)); }
+
  protected:
   std::deque<ezo_command *> commands_;
+  CallbackManager<void(bool)> led_callback_{};
 
   unsigned long start_time_ = 0;
-  unsigned long wait_time_ = 0;
-  uint16_t state_ = 0;
-  float tempcomp_;
+  // unsigned long wait_time_ = 0;
+  // uint16_t state_ = 0;
+  // float tempcomp_;
 };
 
 }  // namespace ezo
