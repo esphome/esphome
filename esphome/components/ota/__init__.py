24a6ed1afaadd58ba48298f5ee08c586b1781bbb
Maurice Makaay <mmakaay1@xs4all.net>
2021-04-23 21:57:05 +0200

    Add automation triggers to ota component.
    The automation triggers added are:
    
    - `on_begin`: triggered right after starting an OTA upgrade
    - `on_progress`: triggered multiple times during upload of the new
      firmware. The upload progress is propagated as a percentage (1% - 100%)
      through trigger variable "x".
    - `on_end`: triggered right after finalizing the OTA upgrade
      successfully, right before the device is rebooted.
    - `on_error`: triggered when the OTA upgrade fails. The error code
      is propagated as an integer through trigger variable "x". Its value
      corresponds to the `error_code` variable in the OTA component code.

--- esphome/components/ota/__init__.py
+++ esphome/components/ota/__init__.py
@@ -1,4 +1,5 @@
 from esphome.cpp_generator import RawExpression
+from esphome import automation
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import (
@@ -14,8 +15,21 @@ from esphome.core import CORE, coroutine_with_priority
 CODEOWNERS = ["@esphome/core"]
 DEPENDENCIES = ["network"]
 
+CONF_ON_BEGIN = "on_begin"
+CONF_ON_PROGRESS = "on_progress"
+CONF_ON_END = "on_end"
+CONF_ON_ERROR = "on_error"
+CONF_ON_BEGIN_TRIGGER_ID = "on_start_trigger_id"
+CONF_ON_PROGRESS_TRIGGER_ID = "on_progres_trigger_id"
+CONF_ON_END_TRIGGER_ID = "on_end_trigger_id"
+CONF_ON_ERROR_TRIGGER_ID = "on_error_trigger_id"
+
 ota_ns = cg.esphome_ns.namespace("ota")
 OTAComponent = ota_ns.class_("OTAComponent", cg.Component)
+OTAStartTrigger = ota_ns.class_("OTAStartTrigger", automation.Trigger.template())
+OTAProgressTrigger = ota_ns.class_("OTAProgressTrigger", automation.Trigger.template())
+OTAEndTrigger = ota_ns.class_("OTAEndTrigger", automation.Trigger.template())
+OTAErrorTrigger = ota_ns.class_("OTAErrorTrigger", automation.Trigger.template())
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -27,6 +41,28 @@ CONFIG_SCHEMA = cv.Schema(
             CONF_REBOOT_TIMEOUT, default="5min"
         ): cv.positive_time_period_milliseconds,
         cv.Optional(CONF_NUM_ATTEMPTS, default="10"): cv.positive_not_null_int,
+        cv.Optional(CONF_ON_BEGIN): automation.validate_automation(
+            {
+                cv.GenerateID(CONF_ON_BEGIN_TRIGGER_ID): cv.declare_id(OTAStartTrigger),
+            }
+        ),
+        cv.Optional(CONF_ON_ERROR): automation.validate_automation(
+            {
+                cv.GenerateID(CONF_ON_ERROR_TRIGGER_ID): cv.declare_id(OTAErrorTrigger),
+            }
+        ),
+        cv.Optional(CONF_ON_PROGRESS): automation.validate_automation(
+            {
+                cv.GenerateID(CONF_ON_PROGRESS_TRIGGER_ID): cv.declare_id(
+                    OTAProgressTrigger
+                ),
+            }
+        ),
+        cv.Optional(CONF_ON_END): automation.validate_automation(
+            {
+                cv.GenerateID(CONF_ON_END_TRIGGER_ID): cv.declare_id(OTAEndTrigger),
+            }
+        ),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -44,6 +80,19 @@ async def to_code(config):
             config[CONF_NUM_ATTEMPTS], config[CONF_REBOOT_TIMEOUT]
         )
         cg.add(RawExpression(f"if ({condition}) return"))
+
+    for conf in config.get(CONF_ON_BEGIN, []):
+        trigger = cg.new_Pvariable(conf[CONF_ON_BEGIN_TRIGGER_ID], var)
+        yield automation.build_automation(trigger, [], conf)
+    for conf in config.get(CONF_ON_PROGRESS, []):
+        trigger = cg.new_Pvariable(conf[CONF_ON_PROGRESS_TRIGGER_ID], var)
+        yield automation.build_automation(trigger, [(float, "x")], conf)
+    for conf in config.get(CONF_ON_END, []):
+        trigger = cg.new_Pvariable(conf[CONF_ON_END_TRIGGER_ID], var)
+        yield automation.build_automation(trigger, [], conf)
+    for conf in config.get(CONF_ON_ERROR, []):
+        trigger = cg.new_Pvariable(conf[CONF_ON_ERROR_TRIGGER_ID], var)
+        yield automation.build_automation(trigger, [(int, "x")], conf)
 
     if CORE.is_esp8266:
         cg.add_library("Update", None)
