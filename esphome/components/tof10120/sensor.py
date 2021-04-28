8bff99a5b31e526096279d1e7bda140128bd9d42
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-04-29 11:34:39 +1200

    Black formatting

--- esphome/components/tof10120/sensor.py
+++ esphome/components/tof10120/sensor.py
@@ -3,15 +3,19 @@ import esphome.config_validation as cv
 from esphome.components import i2c, sensor
 from esphome.const import CONF_ID, UNIT_METER, ICON_ARROW_EXPAND_VERTICAL
 
-DEPENDENCIES = ['i2c']
+DEPENDENCIES = ["i2c"]
 
-tof10120_ns = cg.esphome_ns.namespace('tof10120')
-TOF10120Sensor = tof10120_ns.class_('TOF10120Sensor', sensor.Sensor, cg.PollingComponent,
-                                    i2c.I2CDevice)
+tof10120_ns = cg.esphome_ns.namespace("tof10120")
+TOF10120Sensor = tof10120_ns.class_(
+    "TOF10120Sensor", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice
+)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_METER, ICON_ARROW_EXPAND_VERTICAL, 3).extend({
-    cv.GenerateID(): cv.declare_id(TOF10120Sensor)
-}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(0x52))
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(UNIT_METER, ICON_ARROW_EXPAND_VERTICAL, 3)
+    .extend({cv.GenerateID(): cv.declare_id(TOF10120Sensor)})
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x52))
+)
 
 
 def to_code(config):
