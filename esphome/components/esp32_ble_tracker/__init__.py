12d03d88fb2bb8eb59b5c45e286f7f752060aad0
Ben Buxton <bb@cactii.net>
2021-05-02 22:11:13 +1000

    remove esp_ble_client_schema

--- esphome/components/esp32_ble_tracker/__init__.py
+++ esphome/components/esp32_ble_tracker/__init__.py
@@ -176,13 +176,6 @@ ESP_BLE_DEVICE_SCHEMA = cv.Schema(
     }
 )
 
-ESP_BLE_CLIENT_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_ESP32_BLE_ID): cv.use_id(ESP32BLETracker),
-        cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
-    }
-)
-
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
