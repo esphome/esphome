39e6153e2bb5c2576f3c73173d39a0c7bee5cec6
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-05-12 11:06:54 +1200

    Initial ufire-ise board

---
+++ esphome/components/ufire_ise/__init__.py
@@ -0,0 +1,103 @@
+import esphome.codegen as cg
+from esphome import automation
+import esphome.config_validation as cv
+from esphome.components import i2c, sensor
+from esphome.const import (
+    CONF_ID,
+    CONF_PH,
+    CONF_TEMPERATURE,
+    DEVICE_CLASS_EMPTY,
+    DEVICE_CLASS_TEMPERATURE,
+    ICON_EMPTY,
+    UNIT_CELSIUS,
+    UNIT_PH,
+)
+
+DEPENDENCIES = ["i2c", "sensor"]
+CODEOWNERS = ["@pvizeli"]
+
+CONF_SOLUTION = "solution"
+CONF_TEMPERATURE_SENSOR = "temperature_sensor"
+
+ufire_ise_ns = cg.esphome_ns.namespace("ufire_ise")
+UFireISEComponent = ufire_ise_ns.class_(
+    "UFireISEComponent", cg.PollingComponent, i2c.I2CDevice
+)
+
+# Actions
+UFireISECalibrateProbeLowAction = ufire_ise_ns.class_(
+    "UFireISECalibrateProbeLowAction", automation.Action
+)
+UFireISECalibrateProbeHighAction = ufire_ise_ns.class_(
+    "UFireISECalibrateProbeHighAction", automation.Action
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(UFireISEComponent),
+            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+                UNIT_CELSIUS, ICON_EMPTY, 1, DEVICE_CLASS_TEMPERATURE
+            ),
+            cv.Optional(CONF_PH): sensor.sensor_schema(
+                UNIT_PH, ICON_EMPTY, 1, DEVICE_CLASS_EMPTY
+            ),
+            cv.Optional(CONF_TEMPERATURE_SENSOR): cv.use_id(sensor.Sensor),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x3F))
+)
+
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+
+    if CONF_TEMPERATURE in config:
+        sens = yield sensor.new_sensor(config[CONF_TEMPERATURE])
+        cg.add(var.set_temperature_sensor(sens))
+
+    if CONF_PH in config:
+        sens = yield sensor.new_sensor(config[CONF_PH])
+        cg.add(var.set_ph_sensor(sens))
+
+    if CONF_TEMPERATURE_SENSOR in config:
+        sens = yield cg.get_variable(config[CONF_TEMPERATURE_SENSOR])
+        cg.add(var.set_temperature_sensor_external(sens))
+
+    yield i2c.register_i2c_device(var, config)
+
+
+UFIRE_ISE_CALIBRATE_PROBE_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.use_id(UFireISEComponent),
+        cv.Required(CONF_SOLUTION): cv.templatable(float),
+    }
+)
+
+
+@automation.register_action(
+    "ufire_ise.calibrate_probe_low",
+    UFireISECalibrateProbeLowAction,
+    UFIRE_ISE_CALIBRATE_PROBE_SCHEMA,
+)
+def ufire_ise_calibrate_probe_low_to_code(config, action_id, template_arg, args):
+    paren = yield cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+    template_ = yield cg.templatable(config[CONF_SOLUTION], args, float)
+    cg.add(var.set_solution(template_))
+    yield var
+
+
+@automation.register_action(
+    "ufire_ise.calibrate_probe_high",
+    UFireISECalibrateProbeHighAction,
+    UFIRE_ISE_CALIBRATE_PROBE_SCHEMA,
+)
+def ufire_ise_calibrate_probe_high_to_code(config, action_id, template_arg, args):
+    paren = yield cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+    template_ = yield cg.templatable(config[CONF_SOLUTION], args, float)
+    cg.add(var.set_solution(template_))
+    yield var
---
+++ esphome/components/ufire_ise/ufire_ise.cpp
@@ -0,0 +1,135 @@
+#include "esphome/core/log.h"
+#include "ufire_ise.h"
+
+#include <math.h>
+
+namespace esphome {
+namespace ufire_ise {
+
+static const char *TAG = "ufire_ise";
+
+void UFireISEComponent::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up uFire_ise...");
+
+  uint8_t version;
+  if (!this->read_byte(REGISTER_VERSION, &version, 10) && version != 0xFF) {
+    this->mark_failed();
+    this->status_set_error();
+    return;
+  }
+  ESP_LOGI(TAG, "Found ufire_ise board version 0x%02X", version);
+
+  // Write option for temperature adjustments
+  uint8_t config;
+  this->read_byte(REGISTER_CONFIG, &config, 10);
+  if (this->temperature_sensor_ == nullptr && this->temperature_sensor_external_ == nullptr)
+    config &= ~CONFIG_TEMP_COMPENSATION;
+  else
+    config |= CONFIG_TEMP_COMPENSATION;
+  this->write_byte(REGISTER_CONFIG, config);
+}
+
+void UFireISEComponent::update() {
+  int wait = 0;
+  if (this->temperature_sensor_ != nullptr) {
+    this->write_byte(REGISTER_TASK, COMMAND_MEASURE_TEMP);
+    wait += 750;
+  }
+  if (this->ph_sensor_ != nullptr) {
+    this->write_byte(REGISTER_TASK, COMMAND_MEASURE_MV);
+    wait += 750;
+  }
+
+  // Wait until measurement are taken
+  this->set_timeout("data", wait, [this]() { this->update_internal_(); });
+}
+
+void UFireISEComponent::update_internal_() {
+  float temperature = 0;
+
+  // Read temperature internal and populate it
+  if (this->temperature_sensor_ != nullptr) {
+    temperature = this->measure_temperature_();
+    this->temperature_sensor_->publish_state(temperature);
+  }
+  // Get temperature from external only for adjustments
+  else if (this->temperature_sensor_external_ != nullptr) {
+    temperature = this->temperature_sensor_external_->get_state();
+  }
+
+  if (this->ph_sensor_ != nullptr) {
+    this->ph_sensor_->publish_state(this->measure_ph_(temperature));
+  }
+}
+
+float UFireISEComponent::measure_temperature_() { return this->read_data_(REGISTER_TEMP); }
+
+float UFireISEComponent::measure_mv_() { return this->read_data_(REGISTER_MV); }
+
+float UFireISEComponent::measure_ph_(float temperature) {
+  float mv, ph;
+
+  mv = this->measure_mv_();
+  if (mv == -1)
+    return -1;
+
+  ph = fabs(7.0 - (mv / PROBE_MV_TO_PH));
+
+  // Determine the temperature correction
+  float distance_from_7 = std::abs(7 - roundf(ph));
+  float distance_from_25 = std::floor(std::abs(25 - roundf(temperature)) / 10);
+  float temp_multiplier = (distance_from_25 * distance_from_7) * PROBE_TMP_CORRECTION;
+  if ((ph >= 8.0) && (temperature >= 35))
+    temp_multiplier *= -1;
+  if ((ph <= 6.0) && (temperature <= 15))
+    temp_multiplier *= -1;
+
+  ph += temp_multiplier;
+  if ((ph <= 0.0) || (ph > 14.0))
+    ph = -1;
+  if (isinf(ph))
+    ph = -1;
+  if (isnan(ph))
+    ph = -1;
+
+  return ph;
+}
+
+void UFireISEComponent::set_solution_(float solution) {
+  uint8_t temp[4];
+
+  solution = (7 - solution) * PROBE_MV_TO_PH;
+  memcpy(temp, &solution, sizeof(solution));
+  this->write_bytes(REGISTER_SOLUTION, temp, 4);
+  delay(10);
+}
+
+void UFireISEComponent::calibrate_probe_low(float solution) {
+  this->set_solution_(solution);
+  this->write_byte(REGISTER_TASK, COMMAND_CALIBRATE_LOW);
+}
+
+void UFireISEComponent::calibrate_probe_high(float solution) {
+  this->set_solution_(solution);
+  this->write_byte(REGISTER_TASK, COMMAND_CALIBRATE_HIGH);
+}
+
+float UFireISEComponent::read_data_(uint8_t reg) {
+  float f;
+  uint8_t temp[4];
+
+  this->write_bytes_raw(&reg, 1);
+  delay(10);
+
+  for (uint8_t i = 0; i < 4; i++) {
+    this->read_bytes_raw(temp + i, 1);
+  }
+  memcpy(&f, temp, sizeof(f));
+
+  return f;
+}
+
+void UFireISEComponent::dump_config() {}
+
+}  // namespace ufire_ise
+}  // namespace esphome
