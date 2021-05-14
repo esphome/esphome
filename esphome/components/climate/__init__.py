c0bc48ae2e291a7008a1d84f758431cb937cd430
Lumpusz <marton.keri@gmail.com>
2021-05-14 10:02:59 +0200

    Fix formatting

--- esphome/components/climate/__init__.py
+++ esphome/components/climate/__init__.py
@@ -135,7 +135,9 @@ CLIMATE_CONTROL_ACTION_SCHEMA = cv.Schema(
         cv.Optional(CONF_TARGET_TEMPERATURE_LOW): cv.templatable(cv.temperature),
         cv.Optional(CONF_TARGET_TEMPERATURE_HIGH): cv.templatable(cv.temperature),
         cv.Optional(CONF_AWAY): cv.templatable(cv.boolean),
-        cv.Exclusive(CONF_FAN_MODE, "fan_mode"): cv.templatable(validate_climate_fan_mode),
+        cv.Exclusive(CONF_FAN_MODE, "fan_mode"): cv.templatable(
+            validate_climate_fan_mode
+        ),
         cv.Exclusive(CONF_CUSTOM_FAN_MODE, "fan_mode"): cv.string_strict,
         cv.Exclusive(CONF_PRESET, "preset"): cv.templatable(validate_climate_preset),
         cv.Exclusive(CONF_CUSTOM_PRESET, "preset"): cv.string_strict,
