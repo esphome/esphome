96c000f5f5fbee4cb1b944a68a60b16af05c897e
Franck Nijhof <frenck@frenck.nl>
2021-05-16 11:37:38 +0200

    Update tests/custom.h
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- tests/custom.h
+++ tests/custom.h
@@ -49,7 +49,7 @@ class CustomNativeAPI : public Component, public CustomAPIDevice {
     register_service(&CustomNativeAPI::on_start_dryer, "start_dryer", {"value"});
     register_service(&CustomNativeAPI::on_many_values, "many_values", {"bool", "int", "float", "str1", "str2"});
     subscribe_homeassistant_state(&CustomNativeAPI::on_light_state, "light.my_light");
-    subscribe_homeassistant_state(&CustomNativeAPI::on_light_state, "light.my_light", "brightness");
+    subscribe_homeassistant_state(&CustomNativeAPI::on_brightness_state, "light.my_light", "brightness");
   }
 
   void on_hello_world() { ESP_LOGD("custom_api", "Hello World from native API service!"); }
