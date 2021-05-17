b82ed9a8697f939dda8bbae937e2d6cada32b4d0
Pascal Vizeli <pvizeli@syshack.ch>
2021-05-17 13:08:00 +0000

    Initial ufire-ce board

---
+++ esphome/components/ufire_ec/ufire_ec.cpp
@@ -0,0 +1,116 @@
+#include "esphome/core/log.h"
+#include "ufire_ec.h"
+
+namespace esphome {
+namespace ufire_ec {
+
+static const char *TAG = "ufire_ec";
+
+void UFireECComponent::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up uFire_ec...");
+
+  uint8_t version;
+  if (!this->read_byte(REGISTER_VERSION, &version, 10) && version != 0xFF) {
+    this->mark_failed();
+    this->status_set_error();
+    return;
+  }
+  ESP_LOGI(TAG, "Found ufire_ec board version 0x%02X", version);
+
+  // Write option for temperature adjustments
+  uint8_t config;
+  this->read_byte(REGISTER_CONFIG, &config, 10);
+  if (this->temperature_sensor_ == nullptr && this->temperature_sensor_external_ == nullptr)
+    config &= ~CONFIG_TEMP_COMPENSATION;
+  else
+    config |= CONFIG_TEMP_COMPENSATION;
+  this->write_byte(REGISTER_CONFIG, config);
+
+  // Update temperature compensation
+  this->set_compensation_(this->temperature_compensation_);
+}
+
+void UFireECComponent::update() {
+  if (this->temperature_sensor_ != nullptr) {
+    this->write_byte(REGISTER_TASK, COMMAND_MEASURE_TEMP);
+    this->set_timeout("data_temperature", 750, [this]() { this->update_internal_temperature_(); });
+  } else if (this->temperature_sensor_external_ != nullptr) {
+    float temperature = this->temperature_sensor_external_->get_state();
+    this->set_temperature_(temperature);
+
+    this->write_byte(REGISTER_TASK, COMMAND_MEASURE_EC);
+    this->set_timeout("data_ec", 750, [this]() { this->update_internal_ec_(); });
+  } else if (this->ec_sensor_ != nullptr) {
+    this->write_byte(REGISTER_TASK, COMMAND_MEASURE_EC);
+    this->set_timeout("data_ec", 750, [this]() { this->update_internal_ec_(); });
+  }
+}
+
+void UFireECComponent::update_internal_temperature_() {
+  float temperature = this->measure_temperature_();
+  this->temperature_sensor_->publish_state(temperature);
+  this->set_temperature_(temperature);
+
+  this->write_byte(REGISTER_TASK, COMMAND_MEASURE_EC);
+  this->set_timeout("data_ec", 750, [this]() { this->update_internal_ec_(); });
+}
+
+void UFireECComponent::update_internal_ec_() {
+  if (this->ec_sensor_ != nullptr) {
+    this->ec_sensor_->publish_state(this->measure_ms_());
+  }
+}
+
+float UFireECComponent::measure_temperature_() { return this->read_data_(REGISTER_TEMP); }
+
+float UFireECComponent::measure_ms_() { return this->read_data_(REGISTER_MS); }
+
+void UFireECComponent::set_solution_(float solution) {
+  uint8_t temp[4];
+
+  solution /= (1 - (PROBE_COEFFICIENT * (this->temperature_compensation_ - 25)));
+  memcpy(temp, &solution, sizeof(solution));
+  this->write_bytes(REGISTER_SOLUTION, temp, 4);
+  delay(10);
+}
+
+void UFireECComponent::set_compensation_(float temperature) {
+  uint8_t temp[4];
+
+  memcpy(temp, &temperature, sizeof(temperature));
+  this->write_bytes(REGISTER_COMPENSATION, temp, 4);
+  delay(10);
+}
+
+void UFireECComponent::set_temperature_(float temperature) {
+  uint8_t temp[4];
+
+  memcpy(temp, &temperature, sizeof(temperature));
+  this->write_bytes(REGISTER_TEMP, temp, 4);
+  delay(10);
+}
+
+void UFireECComponent::calibrate_probe(float solution) {
+  this->set_solution_(solution);
+  this->write_byte(REGISTER_TASK, COMMAND_CALIBRATE_PROBE);
+}
+
+float UFireECComponent::read_data_(uint8_t reg) {
+  float f;
+  uint8_t temp[4];
+
+  this->write_bytes_raw(&reg, 1);
+  delay(10);
+
+  for (uint8_t i = 0; i < 4; i++) {
+    this->read_bytes_raw(temp + i, 1);
+  }
+  memcpy(&f, temp, sizeof(f));
+
+  return f;
+}
+
+void UFireECComponent::dump_config() {}
+
+}  // namespace ufire_ec
+}  // namespace esphome
---
+++ esphome/components/ufire_ec/ufire_ec.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/automation.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
+
+namespace esphome {
+namespace ufire_ec {
+
+static const float PROBE_COEFFICIENT = 0.019;
+
+static const uint8_t CONFIG_TEMP_COMPENSATION = 0x02;
+
+static const uint8_t REGISTER_VERSION = 0;
+static const uint8_t REGISTER_MS = 1;
+static const uint8_t REGISTER_TEMP = 5;
+static const uint8_t REGISTER_SOLUTION = 9;
+static const uint8_t REGISTER_COMPENSATION = 45;
+static const uint8_t REGISTER_CONFIG = 54;
+static const uint8_t REGISTER_TASK = 55;
+
+static const uint8_t COMMAND_CALIBRATE_PROBE = 20;
+static const uint8_t COMMAND_MEASURE_TEMP = 40;
+static const uint8_t COMMAND_MEASURE_EC = 80;
+
+class UFireECComponent : public PollingComponent, public i2c::I2CDevice {
+ public:
+  void setup() override;
+  void update() override;
+  void dump_config() override;
+
+  void set_temperature_sensor(sensor::Sensor *temperature_sensor) { this->temperature_sensor_ = temperature_sensor; }
+  void set_temperature_sensor_external(sensor::Sensor *temperature_sensor) {
+    this->temperature_sensor_external_ = temperature_sensor;
+  }
+  void set_ec_sensor(sensor::Sensor *ec_sensor) { this->ec_sensor_ = ec_sensor; }
+  void set_temperature_compensation(float compensation) { this->temperature_compensation_ = compensation; }
+  void calibrate_probe(float solution);
+
+ protected:
+  float measure_temperature_();
+  float measure_ms_();
+  void set_solution_(float solution);
+  void set_compensation_(float temperature);
+  void set_temperature_(float temperature);
+  float read_data_(uint8_t reg);
+  void update_internal_ec_();
+  void update_internal_temperature_();
+
+  sensor::Sensor *temperature_sensor_{nullptr};
+  sensor::Sensor *temperature_sensor_external_{nullptr};
+  sensor::Sensor *ec_sensor_{nullptr};
+  float temperature_compensation_{0.0};
+};
+
+template<typename... Ts> class UFireISECalibrateProbeAction : public Action<Ts...> {
+ public:
+  UFireISECalibrateProbeAction(UFireECComponent *parent) : parent_(parent) {}
+  TEMPLATABLE_VALUE(float, solution)
+
+  void play(Ts... x) override { this->parent_->calibrate_probe(this->solution_.value(x...)); }
+
+ protected:
+  UFireECComponent *parent_;
+};
+
+}  // namespace ufire_ec
+}  // namespace esphome
