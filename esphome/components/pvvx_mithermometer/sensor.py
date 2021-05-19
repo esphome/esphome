cab1fc297f04655ef53e1a88a7cf8444d65aba21
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-05-19 21:36:36 +1200

    Black

--- esphome/components/pvvx_mithermometer/sensor.py
+++ esphome/components/pvvx_mithermometer/sensor.py
@@ -1,27 +1,52 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, esp32_ble_tracker
-from esphome.const import CONF_BATTERY_LEVEL, CONF_BATTERY_VOLTAGE, CONF_MAC_ADDRESS, \
-    CONF_HUMIDITY, CONF_TEMPERATURE, CONF_ID, UNIT_CELSIUS, UNIT_PERCENT, UNIT_VOLT, \
-    ICON_BATTERY, ICON_THERMOMETER, ICON_WATER_PERCENT
-
-CODEOWNERS = ['@pasiz']
-
-DEPENDENCIES = ['esp32_ble_tracker']
-
-pvvx_mithermometer_ns = cg.esphome_ns.namespace('pvvx_mithermometer')
-PVVXMiThermometer = pvvx_mithermometer_ns.class_(
-    'PVVXMiThermometer', esp32_ble_tracker.ESPBTDeviceListener, cg.Component
+from esphome.const import (
+    CONF_BATTERY_LEVEL,
+    CONF_BATTERY_VOLTAGE,
+    CONF_MAC_ADDRESS,
+    CONF_HUMIDITY,
+    CONF_TEMPERATURE,
+    CONF_ID,
+    UNIT_CELSIUS,
+    UNIT_PERCENT,
+    UNIT_VOLT,
+    ICON_BATTERY,
+    ICON_THERMOMETER,
+    ICON_WATER_PERCENT,
 )
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(PVVXMiThermometer),
-    cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
-    cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(UNIT_CELSIUS, ICON_THERMOMETER, 2),
-    cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(UNIT_PERCENT, ICON_WATER_PERCENT, 2),
-    cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(UNIT_PERCENT, ICON_BATTERY, 0),
-    cv.Optional(CONF_BATTERY_VOLTAGE): sensor.sensor_schema(UNIT_VOLT, ICON_BATTERY, 3),
-}).extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+CODEOWNERS = ["@pasiz"]
+
+DEPENDENCIES = ["esp32_ble_tracker"]
+
+pvvx_mithermometer_ns = cg.esphome_ns.namespace("pvvx_mithermometer")
+PVVXMiThermometer = pvvx_mithermometer_ns.class_(
+    "PVVXMiThermometer", esp32_ble_tracker.ESPBTDeviceListener, cg.Component
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(PVVXMiThermometer),
+            cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
+            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+                UNIT_CELSIUS, ICON_THERMOMETER, 2
+            ),
+            cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(
+                UNIT_PERCENT, ICON_WATER_PERCENT, 2
+            ),
+            cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(
+                UNIT_PERCENT, ICON_BATTERY, 0
+            ),
+            cv.Optional(CONF_BATTERY_VOLTAGE): sensor.sensor_schema(
+                UNIT_VOLT, ICON_BATTERY, 3
+            ),
+        }
+    )
+    .extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 
 def to_code(config):
