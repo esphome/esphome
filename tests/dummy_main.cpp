27d9dcc77ef7d7b179d1de1352c97db285e478bf
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-05-25 07:39:20 +1200

    Update tests/dummy_main.cpp
    Co-authored-by: Otto Winter <otto@otto-winter.com>

--- tests/dummy_main.cpp
+++ tests/dummy_main.cpp
@@ -1,7 +1,7 @@
 // Dummy main.cpp file for the PlatformIO project in the git repository.
 // Primarily used to get IDE integration working (so the contents here don't
 // matter at all, as long as it compiles).
-// Note used during runtime nor for CI.
+// Not used during runtime nor for CI.
 
 #include <esphome/core/application.h>
 #include <esphome/components/logger/logger.h>
