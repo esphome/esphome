e71f1beb383e0f2c5636c94d4e1c811fe9b1d778
dentra <dennis.trachuk@gmail.com>
2021-05-26 22:12:35 +0000

    Move log before publish_state

--- esphome/components/tuya/text_sensor/tuya_text_sensor.cpp
+++ esphome/components/tuya/text_sensor/tuya_text_sensor.cpp
@@ -10,15 +10,15 @@ void TuyaTextSensor::setup() {
   this->parent_->register_listener(this->sensor_id_, [this](TuyaDatapoint datapoint) {
     switch (datapoint.type) {
       case TuyaDatapointType::STRING:
-        this->publish_state(datapoint.value_string);
         ESP_LOGD(TAG, "MCU reported text sensor is: %s", datapoint.value_string.c_str());
+        this->publish_state(datapoint.value_string);
         break;
       case TuyaDatapointType::RAW: {
         // wait until #1669 merged to dev branch
         // std::string data = rawencode(datapoint.value_raw.data(), datapoint.value_raw.size());
         std::string data = datapoint.value_string;
-        this->publish_state(data);
         ESP_LOGD(TAG, "MCU reported text sensor is: %s", data.c_str());
+        this->publish_state(data);
         break;
       }
       default:
