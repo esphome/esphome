717f567592d6f650697fc8d9f803ec822d0aa9fe
Pascal Vizeli <pascal.vizeli@syshack.ch>
2021-05-27 10:35:22 +0200

    Address comments from EC PR

--- esphome/components/ufire_ise/ufire_ise.cpp
+++ esphome/components/ufire_ise/ufire_ise.cpp
@@ -144,12 +144,9 @@ void UFireISEComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "uFire-ICE");
   LOG_I2C_DEVICE(this)
   LOG_UPDATE_INTERVAL(this)
-  if (this->ph_sensor_ != nullptr)
-    LOG_SENSOR("  ", "Ph Sensor", this->ph_sensor_)
-  if (this->temperature_sensor_ != nullptr)
-    LOG_SENSOR("  ", "Temperature Sensor", this->temperature_sensor_)
-  if (this->temperature_sensor_external_ != nullptr)
-    LOG_SENSOR("  ", "Temperature Sensor external", this->temperature_sensor_external_)
+  LOG_SENSOR("  ", "Ph Sensor", this->ph_sensor_)
+  LOG_SENSOR("  ", "Temperature Sensor", this->temperature_sensor_)
+  LOG_SENSOR("  ", "Temperature Sensor external", this->temperature_sensor_external_)
 }
 
 }  // namespace ufire_ise
