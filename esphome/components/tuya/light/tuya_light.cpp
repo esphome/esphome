c5c24c198921f207b97a0ee91c2874cee7019697
Trevor North <trevor@freedisc.co.uk>
2021-06-02 10:31:56 +0100

    Tuya improvements (#1491)
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/tuya/light/tuya_light.cpp
+++ esphome/components/tuya/light/tuya_light.cpp
@@ -31,11 +31,7 @@ void TuyaLight::setup() {
     });
   }
   if (min_value_datapoint_id_.has_value()) {
-    TuyaDatapoint datapoint{};
-    datapoint.id = *this->min_value_datapoint_id_;
-    datapoint.type = TuyaDatapointType::INTEGER;
-    datapoint.value_int = this->min_value_;
-    parent_->set_datapoint_value(datapoint);
+    parent_->set_datapoint_value(*this->min_value_datapoint_id_, this->min_value_);
   }
 }
 
@@ -67,49 +63,29 @@ void TuyaLight::write_state(light::LightState *state) {
   if (brightness == 0.0f) {
     // turning off, first try via switch (if exists), then dimmer
     if (switch_id_.has_value()) {
-      TuyaDatapoint datapoint{};
-      datapoint.id = *this->switch_id_;
-      datapoint.type = TuyaDatapointType::BOOLEAN;
-      datapoint.value_bool = false;
-
-      parent_->set_datapoint_value(datapoint);
+      parent_->set_datapoint_value(*this->switch_id_, false);
     } else if (dimmer_id_.has_value()) {
-      TuyaDatapoint datapoint{};
-      datapoint.id = *this->dimmer_id_;
-      datapoint.type = TuyaDatapointType::INTEGER;
-      datapoint.value_int = 0;
-      parent_->set_datapoint_value(datapoint);
+      parent_->set_datapoint_value(*this->dimmer_id_, 0);
     }
     return;
   }
 
   if (this->color_temperature_id_.has_value()) {
-    TuyaDatapoint datapoint{};
-    datapoint.id = *this->color_temperature_id_;
-    datapoint.type = TuyaDatapointType::INTEGER;
-    datapoint.value_int =
+    uint32_t color_temp_int =
         static_cast<uint32_t>(this->color_temperature_max_value_ *
                               (state->current_values.get_color_temperature() - this->cold_white_temperature_) /
                               (this->warm_white_temperature_ - this->cold_white_temperature_));
-    parent_->set_datapoint_value(datapoint);
+    parent_->set_datapoint_value(*this->color_temperature_id_, color_temp_int);
   }
 
   auto brightness_int = static_cast<uint32_t>(brightness * this->max_value_);
   brightness_int = std::max(brightness_int, this->min_value_);
 
   if (this->dimmer_id_.has_value()) {
-    TuyaDatapoint datapoint{};
-    datapoint.id = *this->dimmer_id_;
-    datapoint.type = TuyaDatapointType::INTEGER;
-    datapoint.value_int = brightness_int;
-    parent_->set_datapoint_value(datapoint);
+    parent_->set_datapoint_value(*this->dimmer_id_, brightness_int);
   }
   if (this->switch_id_.has_value()) {
-    TuyaDatapoint datapoint{};
-    datapoint.id = *this->switch_id_;
-    datapoint.type = TuyaDatapointType::BOOLEAN;
-    datapoint.value_bool = true;
-    parent_->set_datapoint_value(datapoint);
+    parent_->set_datapoint_value(*this->switch_id_, true);
   }
 }
 
