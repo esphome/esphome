10ff449ff80a64ca4619ffcec1450c28c8f42916
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-06-04 14:20:59 +1200

    Break on default for ble_service

--- esphome/components/esp32_ble/ble_service.cpp
+++ esphome/components/esp32_ble/ble_service.cpp
@@ -114,6 +114,8 @@ void BLEService::gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t g
       }
       break;
     }
+    default:
+      break;
   }
 
   for (auto *characteristic : this->characteristics_) {
