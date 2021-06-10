dab3585e1897e41b17d8318111780d26ef2d187b
floydheld <alex.falkensteiner@gmail.com>
2021-06-10 22:11:00 +0200

    formatting as per clang

--- esphome/components/multi_button/multi_button.h
+++ esphome/components/multi_button/multi_button.h
@@ -1,27 +1,27 @@
 #pragma once
 
 /** This is a component that allows for receiving simple events from a button connected to a GPIO
-* It supports multiple button presses and/or press&hold and signals them using a numerical code
-* Credits go to @mathertel because his OneButton heavily inspired this work
-*/
+ * It supports multiple button presses and/or press&hold and signals them using a numerical code
+ * Credits go to @mathertel because his OneButton heavily inspired this work
+ */
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace multi_button {
-	
+
 enum class MultiButtonState {
-	RELEASED = 0,
-	MAYBE_PRESSED = 1,
-	PRESSED = 2,
-	MAYBE_RELEASED = 3,
-	PRESS_HOLD = 4,
-	MAYBE_NOT_PRESS_HOLD = 5
+  RELEASED = 0,
+  MAYBE_PRESSED = 1,
+  PRESSED = 2,
+  MAYBE_RELEASED = 3,
+  PRESS_HOLD = 4,
+  MAYBE_NOT_PRESS_HOLD = 5
 };
 
 class MultiButton : public sensor::Sensor, public Component {
-public:
+ public:
   float get_setup_priority() const override { return setup_priority::HARDWARE; }
   void setup() override;
   void dump_config() override;
@@ -29,31 +29,33 @@ public:
 
   void set_pin(GPIOPin *pin) { this->pin_ = pin; }
   void set_debounce(const int millis) { this->debounce_millis_ = millis; }
-	void set_timeout(const int millis) { this->timeout_millis_ = millis; }
+  void set_timeout(const int millis) { this->timeout_millis_ = millis; }
   void set_press_hold_threshold(const int millis) { this->press_hold_threshold_millis_ = millis; }
-  void set_press_hold_update_rate(const float frequency) { this->press_hold_update_interval_millis_ = (int)(1000.0f / frequency); }
-    
-protected:
+  void set_press_hold_update_rate(const float frequency) {
+    this->press_hold_update_interval_millis_ = (int) (1000.0f / frequency);
+  }
 
+ protected:
   GPIOPin *pin_;
-	// The time in [ms] a button state needs to be stable to be recognized and not ignored as bouncing
-	int debounce_millis_;
-	// The timeout in [ms] before an event is recognized and published. A double press must have a smaller pause than this between two presses
-	int timeout_millis_;
+  // The time in [ms] a button state needs to be stable to be recognized and not ignored as bouncing
+  int debounce_millis_;
+  // The timeout in [ms] before an event is recognized and published. A double press must have a smaller pause than this
+  // between two presses
+  int timeout_millis_;
   // The threshold in [ms] where a normal press becomes a press&hold
-	int press_hold_threshold_millis_;
+  int press_hold_threshold_millis_;
   // The interval in [ms] at which updates are being published during a press&hold
-	int press_hold_update_interval_millis_;
+  int press_hold_update_interval_millis_;
 
-	MultiButtonState state_;	
-	unsigned long startTime_;
-	bool level_;
-	int press_count_;
+  MultiButtonState state_;
+  unsigned long startTime_;
+  bool level_;
+  int press_count_;
   int hold_count_;
   bool reset_;
-	
-	void tick_(unsigned long time, bool level);
+
+  void tick_(unsigned long time, bool level);
 };
 
-} //namespace multi_button
-} //namespace esphome
+}  // namespace multi_button
+}  // namespace esphome
--- esphome/components/multi_button/sensor.py
+++ esphome/components/multi_button/sensor.py
@@ -14,19 +14,23 @@ from esphome.const import (
 
 CODEOWNERS = ["@floydheld", "@mathertel"]
 
-multi_button_ns = cg.esphome_ns.namespace('multi_button')
-MultiButton = multi_button_ns.class_('MultiButton', cg.Component)
+multi_button_ns = cg.esphome_ns.namespace("multi_button")
+MultiButton = multi_button_ns.class_("MultiButton", cg.Component)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_EMPTY, "mdi:gesture-tap", 1).extend(
-    {
-        cv.GenerateID(): cv.declare_id(MultiButton),
-        cv.Required(CONF_PIN): pins.gpio_input_pin_schema,
-        cv.Optional(CONF_DEBOUNCE, default=40): cv.int_range(min=1, max=500),
-        cv.Optional(CONF_TIMEOUT, default=300): cv.int_range(min=100, max=5000),
-        cv.Optional(CONF_THRESHOLD, default=600): cv.int_range(min=250, max=5000),
-        cv.Optional(CONF_FREQUENCY, default=1.5): cv.float_range(min=0.1, max=50),
-	}
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(UNIT_EMPTY, "mdi:gesture-tap", 1)
+    .extend(
+        {
+            cv.GenerateID(): cv.declare_id(MultiButton),
+            cv.Required(CONF_PIN): pins.gpio_input_pin_schema,
+            cv.Optional(CONF_DEBOUNCE, default=40): cv.int_range(min=1, max=500),
+            cv.Optional(CONF_TIMEOUT, default=300): cv.int_range(min=100, max=5000),
+            cv.Optional(CONF_THRESHOLD, default=600): cv.int_range(min=250, max=5000),
+            cv.Optional(CONF_FREQUENCY, default=1.5): cv.float_range(min=0.1, max=50),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 
 def to_code(config):
@@ -40,4 +44,3 @@ def to_code(config):
     cg.add(var.set_press_hold_threshold(config[CONF_THRESHOLD]))
     cg.add(var.set_press_hold_update_rate(config[CONF_FREQUENCY]))
     cg.add(var.set_accuracy_decimals(0))
-    
