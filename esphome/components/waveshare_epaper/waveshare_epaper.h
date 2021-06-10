49415718e887b2662d84a7e00bdd66c37034bc9f
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-06-10 13:54:02 +1200

    Update waveshare_epaper.h

--- esphome/components/waveshare_epaper/waveshare_epaper.h
+++ esphome/components/waveshare_epaper/waveshare_epaper.h
@@ -86,7 +86,7 @@ class WaveshareEPaperTypeA : public WaveshareEPaper {
   void display() override;
 
   void deep_sleep() override {
-    if (this->model_ == WAVESHARE_EPAPER_2_9_IN_V2 || this->model_ ==WAVESHARE_EPAPER_1_54_IN_V2) {
+    if (this->model_ == WAVESHARE_EPAPER_2_9_IN_V2 || this->model_ == WAVESHARE_EPAPER_1_54_IN_V2) {
       // COMMAND DEEP SLEEP MODE
       this->command(0x10);
       this->data(0x01);
