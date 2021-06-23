618025e8a5787103855c505947e604e666724fa1
dentra <dennis.trachuk@gmail.com>
2021-06-23 16:00:30 +0000

    Add unexpected datapoint type validation

--- esphome/components/tuya/automation.h
+++ esphome/components/tuya/automation.h
@@ -16,51 +16,37 @@ class TuyaDatapointUpdateTrigger : public Trigger<TuyaDatapoint> {
 
 class TuyaRawDatapointUpdateTrigger : public Trigger<std::vector<uint8_t>> {
  public:
-  explicit TuyaRawDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id) {
-    parent->register_listener(sensor_id, [this](const TuyaDatapoint &dp) { this->trigger(dp.value_raw); });
-  }
+  explicit TuyaRawDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id);
 };
 
 class TuyaBoolDatapointUpdateTrigger : public Trigger<bool> {
  public:
-  explicit TuyaBoolDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id) {
-    parent->register_listener(sensor_id, [this](const TuyaDatapoint &dp) { this->trigger(dp.value_bool); });
-  }
+  explicit TuyaBoolDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id);
 };
 
 class TuyaIntDatapointUpdateTrigger : public Trigger<int> {
  public:
-  explicit TuyaIntDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id) {
-    parent->register_listener(sensor_id, [this](const TuyaDatapoint &dp) { this->trigger(dp.value_int); });
-  }
+  explicit TuyaIntDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id);
 };
 
 class TuyaUIntDatapointUpdateTrigger : public Trigger<uint32_t> {
  public:
-  explicit TuyaUIntDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id) {
-    parent->register_listener(sensor_id, [this](const TuyaDatapoint &dp) { this->trigger(dp.value_uint); });
-  }
+  explicit TuyaUIntDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id);
 };
 
 class TuyaStringDatapointUpdateTrigger : public Trigger<std::string> {
  public:
-  explicit TuyaStringDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id) {
-    parent->register_listener(sensor_id, [this](const TuyaDatapoint &dp) { this->trigger(dp.value_string); });
-  }
+  explicit TuyaStringDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id);
 };
 
 class TuyaEnumDatapointUpdateTrigger : public Trigger<uint8_t> {
  public:
-  explicit TuyaEnumDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id) {
-    parent->register_listener(sensor_id, [this](const TuyaDatapoint &dp) { this->trigger(dp.value_enum); });
-  }
+  explicit TuyaEnumDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id);
 };
 
 class TuyaBitmaskDatapointUpdateTrigger : public Trigger<uint32_t> {
  public:
-  explicit TuyaBitmaskDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id) {
-    parent->register_listener(sensor_id, [this](const TuyaDatapoint &dp) { this->trigger(dp.value_bitmask); });
-  }
+  explicit TuyaBitmaskDatapointUpdateTrigger(Tuya *parent, uint8_t sensor_id);
 };
 
 }  // namespace tuya
