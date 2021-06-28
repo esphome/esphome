ee34458819bc8d8f8efbbcfedf311b4438dc3a1e
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-06-29 10:36:34 +1200

    Move some code into cpp from h

--- esphome/components/number/automation.h
+++ esphome/components/number/automation.h
@@ -36,43 +36,11 @@ class ValueRangeTrigger : public Trigger<float>, public Component {
   template<typename V> void set_min(V min) { this->min_ = min; }
   template<typename V> void set_max(V max) { this->max_ = max; }
 
-  void setup() override {
-    this->rtc_ = global_preferences.make_preference<bool>(this->parent_->get_object_id_hash());
-    bool initial_state;
-    if (this->rtc_.load(&initial_state)) {
-      this->previous_in_range_ = initial_state;
-    }
-
-    this->parent_->add_on_state_callback([this](float state) { this->on_state_(state); });
-  }
-  float get_setup_priority() const override { return setup_priority::HARDWARE; }
+  void setup() override;
+  float get_setup_priority() const override;
 
  protected:
-  void on_state_(float state) {
-    if (isnan(state))
-      return;
-
-    float local_min = this->min_.value(state);
-    float local_max = this->max_.value(state);
-
-    bool in_range;
-    if (isnan(local_min) && isnan(local_max)) {
-      in_range = this->previous_in_range_;
-    } else if (isnan(local_min)) {
-      in_range = state <= local_max;
-    } else if (isnan(local_max)) {
-      in_range = state >= local_min;
-    } else {
-      in_range = local_min <= state && state <= local_max;
-    }
-
-    if (in_range != this->previous_in_range_ && in_range) {
-      this->trigger(state);
-    }
-
-    this->previous_in_range_ = in_range;
-    this->rtc_.save(&in_range);
-  }
+  void on_state_(float state);
 
   Number *parent_;
   ESPPreferenceObject rtc_;
