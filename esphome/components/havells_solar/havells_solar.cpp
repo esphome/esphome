d621c4ffa9ba582d525cc5a55508bc9532d231ad
Sourabh Jaiswal <sourabhjaiswal92@gmail.com>
2021-07-06 16:56:34 +0530

    Added support for havells_solar sensor

---
+++ esphome/components/havells_solar/havells_solar.cpp
@@ -0,0 +1,185 @@
+#include "havells_solar.h"
+#include "havells_solar_registers.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace havells_solar {
+
+static const char *const TAG = "havells_solar";
+
+static const uint8_t MODBUS_CMD_READ_IN_REGISTERS = 0x03;
+static const uint8_t MODBUS_REGISTER_COUNT = 48;  // 48 x 16-bit registers
+
+void HAVELLSSolar::on_modbus_data(const std::vector<uint8_t> &data) {
+  if (data.size() < MODBUS_REGISTER_COUNT * 2) {
+    ESP_LOGW(TAG, "Invalid size for HAVELLSSolar!");
+    return;
+  }
+
+  auto havells_solar_get_2_registers = [&](size_t i,float unit) -> float {
+    uint32_t temp = encode_uint32(data[i], data[i + 1], data[i + 2], data[i + 3]);
+    return temp * unit;
+  };
+
+  auto havells_solar_get_1_register = [&](size_t i,float unit) -> float {
+    uint16_t temp = encode_uint16(data[i], data[i + 1]);
+	return temp * unit ;
+  };
+
+
+  for (uint8_t i = 0; i < 3; i++) {
+    auto phase = this->phases_[i];
+    if (!phase.setup)
+      continue;
+
+    float voltage = havells_solar_get_1_register(HAVELLS_PHASE_1_VOLTAGE * 2 + (i * 4),ONE_DEC_UNIT);
+    float current = havells_solar_get_1_register(HAVELLS_PHASE_1_CURRENT * 2 + (i * 4),TWO_DEC_UNIT);
+
+//    ESP_LOGD(
+//        TAG,
+//        "HAVELLSSolar Phase %c: V=%.3f V, I=%.3f A ",
+//        i + 'A', voltage, current);
+    if (phase.voltage_sensor_ != nullptr)
+      phase.voltage_sensor_->publish_state(voltage);
+    if (phase.current_sensor_ != nullptr)
+      phase.current_sensor_->publish_state(current);
+  }
+
+  for (uint8_t i = 0; i < 2; i++) {
+    auto pv = this->pvs_[i];
+    if (!pv.setup)
+      continue;
+
+    float voltage = havells_solar_get_1_register(HAVELLS_PV_1_VOLTAGE * 2 + (i * 4),ONE_DEC_UNIT);
+    float current = havells_solar_get_1_register(HAVELLS_PV_1_CURRENT * 2 + (i * 4),TWO_DEC_UNIT);
+    float active_power = havells_solar_get_1_register(HAVELLS_PV_1_POWER * 2 + (i * 2),MULTIPLY_TEN_UNIT);
+
+//    ESP_LOGD(
+//        TAG,
+//        "HAVELLSSolar PV %c: V=%.3f V, I=%.3f A ,W=%.3f W",
+//        i + 'A', voltage, current, active_power);
+    if (pv.voltage_sensor_ != nullptr)
+      pv.voltage_sensor_->publish_state(voltage);
+    if (pv.current_sensor_ != nullptr)
+      pv.current_sensor_->publish_state(current);
+    if (pv.active_power_sensor_ != nullptr)
+      pv.active_power_sensor_->publish_state(active_power);
+  }
+
+  float frequency = havells_solar_get_1_register(HAVELLS_GRID_FREQUENCY * 2,TWO_DEC_UNIT);
+  float active_power = havells_solar_get_1_register(HAVELLS_SYSTEM_ACTIVE_POWER * 2,MULTIPLY_TEN_UNIT);
+  float reactive_power = havells_solar_get_1_register(HAVELLS_SYSTEM_REACTIVE_POWER * 2,TWO_DEC_UNIT);
+  float today_production = havells_solar_get_1_register(HAVELLS_TODAY_PRODUCTION * 2,TWO_DEC_UNIT);
+  float total_energy_production = havells_solar_get_2_registers(HAVELLS_TOTAL_ENERGY_PRODUCTION * 2,NO_DEC_UNIT);
+  float total_generation_time = havells_solar_get_2_registers(HAVELLS_TOTAL_GENERATION_TIME * 2,NO_DEC_UNIT);
+  float today_generation_time = havells_solar_get_1_register(HAVELLS_TODAY_GENERATION_TIME * 2,NO_DEC_UNIT);
+  float inverter_module_temp = havells_solar_get_1_register(HAVELLS_INVERTER_MODULE_TEMP * 2,NO_DEC_UNIT);
+  float inverter_inner_temp = havells_solar_get_1_register(HAVELLS_INVERTER_INNER_TEMP * 2,NO_DEC_UNIT);
+  float inverter_bus_voltage = havells_solar_get_1_register(HAVELLS_INVERTER_BUS_VOLTAGE * 2,NO_DEC_UNIT);
+  float pv1_volt_sampled_by_slave_cpu = havells_solar_get_1_register(HAVELLS_PV1_VOLTAGE_SAMPLED_BY_SLAVE_CPU * 2,NO_DEC_UNIT);
+  float pv2_volt_sampled_by_slave_cpu = havells_solar_get_1_register(HAVELLS_PV2_VOLTAGE_SAMPLED_BY_SLAVE_CPU * 2,NO_DEC_UNIT);
+  float insulation_pv1_p_to_ground = havells_solar_get_1_register(HAVELLS_INSULATION_OF_PV1_P_TO_GROUND * 2,NO_DEC_UNIT);
+  float insulation_pv2_p_to_ground = havells_solar_get_1_register(HAVELLS_INSULATION_OF_PV2_P_TO_GROUND * 2,NO_DEC_UNIT);
+  float insulation_pv_n_to_ground = havells_solar_get_1_register(HAVELLS_INSULATION_OF_PV_N_TO_GROUND * 2,NO_DEC_UNIT);
+  float gfci_value = havells_solar_get_1_register(HAVELLS_GFCI_VALUE * 2,NO_DEC_UNIT);
+  float dci_of_r = havells_solar_get_1_register(HAVELLS_DCI_OF_R * 2,NO_DEC_UNIT);
+  float dci_of_s = havells_solar_get_1_register(HAVELLS_DCI_OF_S * 2,NO_DEC_UNIT);
+  float dci_of_t = havells_solar_get_1_register(HAVELLS_DCI_OF_T * 2,NO_DEC_UNIT);
+
+//  ESP_LOGD(TAG, "HAVELLSSolar: F=%.3f Hz, Active P=%.3f W, Reactive P=%.3f VAR, TodayGeneration E=%.3f kWH",
+//           frequency, active_power, reactive_power,today_production);
+//  ESP_LOGD(TAG, "HAVELLSSolar: Total Generation E=%.3f kWh, Total Generation Time H=%.3f hrs, Today Generation Time H=%.3f hrs,",
+//           total_energy_production,total_generation_time,today_generation_time);
+//  ESP_LOGD(TAG, "HAVELLSSolar: Inverter Module Temp C=%.3f Degree, Inverter Inner Temp C=%.3f Degree,",
+//           inverter_module_temp,inverter_inner_temp);
+//  ESP_LOGD(TAG, "HAVELLSSolar: Inverter Bus Voltage V=%.3f V, PV1 Voltage Sampled By Slave CPU V=%.3f V, PV2 Voltage Sampled By Slave CPU V=%.3f V,",
+//           inverter_bus_voltage,pv1_volt_sampled_by_slave_cpu,pv2_volt_sampled_by_slave_cpu);
+//  ESP_LOGD(TAG, "HAVELLSSolar: Insulation Of PV1+ To Ground OHM=%.3f KΩ, Insulation Of PV2+ To Ground OHM=%.3f KΩ,Insulation Of PV- To Ground OHM=%.3f KΩ,",
+//           insulation_pv1_p_to_ground,insulation_pv2_p_to_ground,insulation_pv_n_to_ground);
+//  ESP_LOGD(TAG, "HAVELLSSolar: GFCI Value mA=%.3f mA, DCI Of R mA=%.3f mA, DCI Of S mA=%.3f mA, DCI Of T mA=%.3f mA,",
+//           gfci_value,dci_of_r,dci_of_s,dci_of_t);
+  
+  if (this->frequency_sensor_ != nullptr)
+    this->frequency_sensor_->publish_state(frequency);
+  if (this->active_power_sensor_ != nullptr)
+    this->active_power_sensor_->publish_state(active_power);
+  if (this->reactive_power_sensor_ != nullptr)
+    this->reactive_power_sensor_->publish_state(reactive_power);
+  if (this->today_production_sensor_ != nullptr)
+    this->today_production_sensor_->publish_state(today_production);
+  if (this->total_energy_production_sensor_ != nullptr)
+    this->total_energy_production_sensor_->publish_state(total_energy_production);
+  if (this->total_generation_time_sensor_ != nullptr)
+    this->total_generation_time_sensor_->publish_state(total_generation_time);
+  if (this->today_generation_time_sensor_ != nullptr)
+    this->today_generation_time_sensor_->publish_state(today_generation_time);
+  if (this->inverter_module_temp_sensor_ != nullptr)
+    this->inverter_module_temp_sensor_->publish_state(inverter_module_temp);
+  if (this->inverter_inner_temp_sensor_ != nullptr)
+    this->inverter_inner_temp_sensor_->publish_state(inverter_inner_temp);
+  if (this->inverter_bus_voltage_sensor_ != nullptr)
+    this->inverter_bus_voltage_sensor_->publish_state(inverter_bus_voltage);
+  if (this->pv1_volt_sampled_by_slave_cpu_sensor_ != nullptr)
+    this->pv1_volt_sampled_by_slave_cpu_sensor_->publish_state(pv1_volt_sampled_by_slave_cpu);
+  if (this->pv2_volt_sampled_by_slave_cpu_sensor_ != nullptr)
+    this->pv2_volt_sampled_by_slave_cpu_sensor_->publish_state(pv2_volt_sampled_by_slave_cpu);
+  if (this->insulation_pv1_p_to_ground_sensor_ != nullptr)
+    this->insulation_pv1_p_to_ground_sensor_->publish_state(insulation_pv1_p_to_ground);
+  if (this->insulation_pv2_p_to_ground_sensor_ != nullptr)
+    this->insulation_pv2_p_to_ground_sensor_->publish_state(insulation_pv2_p_to_ground);
+  if (this->insulation_pv_n_to_ground_sensor_ != nullptr)
+    this->insulation_pv_n_to_ground_sensor_->publish_state(insulation_pv_n_to_ground);
+  if (this->gfci_value_sensor_ != nullptr)
+    this->gfci_value_sensor_->publish_state(gfci_value);
+  if (this->dci_of_r_sensor_ != nullptr)
+    this->dci_of_r_sensor_->publish_state(dci_of_r);
+  if (this->dci_of_s_sensor_ != nullptr)
+    this->dci_of_s_sensor_->publish_state(dci_of_s);
+  if (this->dci_of_t_sensor_ != nullptr)
+    this->dci_of_t_sensor_->publish_state(dci_of_t);
+}
+
+void HAVELLSSolar::update() { this->send(MODBUS_CMD_READ_IN_REGISTERS, 0, MODBUS_REGISTER_COUNT); }
+void HAVELLSSolar::dump_config() {
+  ESP_LOGCONFIG(TAG, "HAVELLS Solar:");
+  ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
+  for (uint8_t i = 0; i < 3; i++) {
+    auto phase = this->phases_[i];
+    if (!phase.setup)
+      continue;
+    ESP_LOGCONFIG(TAG, "  Phase %c", i + 'A');
+    LOG_SENSOR("    ", "Voltage", phase.voltage_sensor_);
+    LOG_SENSOR("    ", "Current", phase.current_sensor_);
+  }
+  for (uint8_t i = 0; i < 2; i++) {
+    auto pv = this->pvs_[i];
+    if (!pv.setup)
+      continue;
+    ESP_LOGCONFIG(TAG, "  PV %c", i + 'A');
+    LOG_SENSOR("    ", "Voltage", pv.voltage_sensor_);
+    LOG_SENSOR("    ", "Current", pv.current_sensor_);
+    LOG_SENSOR("    ", "Active Power", pv.active_power_sensor_);
+  }
+  LOG_SENSOR("  ", "Frequency", this->frequency_sensor_);
+  LOG_SENSOR("    ", "Active Power", this->active_power_sensor_);
+  LOG_SENSOR("    ", "Reactive Power", this->reactive_power_sensor_);
+  LOG_SENSOR("    ", "Today Generation", this->today_production_sensor_);
+  LOG_SENSOR("    ", "Total Generation", this->total_energy_production_sensor_);
+  LOG_SENSOR("    ", "Total Generation Time", this->total_generation_time_sensor_);
+  LOG_SENSOR("    ", "Today Generation Time", this->today_generation_time_sensor_);
+  LOG_SENSOR("    ", "Inverter Module Temp", this->inverter_module_temp_sensor_);
+  LOG_SENSOR("    ", "Inverter Inner Temp", this->inverter_inner_temp_sensor_);
+  LOG_SENSOR("    ", "Inverter Bus Voltage", this->inverter_bus_voltage_sensor_);
+  LOG_SENSOR("    ", "PV1 Voltage Sampled By Slave CPU", this->pv1_volt_sampled_by_slave_cpu_sensor_);
+  LOG_SENSOR("    ", "PV2 Voltage Sampled By Slave CPU", this->pv2_volt_sampled_by_slave_cpu_sensor_);
+  LOG_SENSOR("    ", "Insulation Of PV1+ To Ground", this->insulation_pv1_p_to_ground_sensor_);
+  LOG_SENSOR("    ", "Insulation Of PV2+ To Ground", this->insulation_pv2_p_to_ground_sensor_);
+  LOG_SENSOR("    ", "Insulation Of PV- To Ground", this->insulation_pv_n_to_ground_sensor_);
+  LOG_SENSOR("    ", "GFCI Value", this->gfci_value_sensor_);
+  LOG_SENSOR("    ", "DCI Of R", this->dci_of_r_sensor_);
+  LOG_SENSOR("    ", "DCI Of S", this->dci_of_s_sensor_);
+  LOG_SENSOR("    ", "DCI Of T", this->dci_of_t_sensor_);
+}
+
+}  // namespace havells_solar
+}  // namespace esphome
---
+++ esphome/components/havells_solar/sensor.py
@@ -0,0 +1,342 @@
+from esphome.components.atm90e32.sensor import CONF_PHASE_A, CONF_PHASE_B, CONF_PHASE_C
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, modbus
+from esphome.const import (
+    CONF_ACTIVE_POWER,
+    CONF_CURRENT,
+    CONF_FREQUENCY,
+    CONF_ID,
+    CONF_POWER_FACTOR,
+    CONF_REACTIVE_POWER,
+    CONF_VOLTAGE,
+    CONF_ENERGY,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_EMPTY,
+    DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_POWER_FACTOR,
+    DEVICE_CLASS_VOLTAGE,
+    DEVICE_CLASS_TEMPERATURE,
+    ICON_CURRENT_AC,
+    ICON_EMPTY,
+    ICON_FLASH,
+    STATE_CLASS_MEASUREMENT,
+    STATE_CLASS_NONE,
+    UNIT_AMPERE,
+    UNIT_DEGREES,
+    UNIT_EMPTY,
+    UNIT_HERTZ,
+    UNIT_VOLT,
+    UNIT_VOLT_AMPS,
+    UNIT_VOLT_AMPS_REACTIVE,
+    UNIT_VOLT_AMPS_REACTIVE_HOURS,
+    UNIT_WATT,
+    UNIT_WATT_HOURS,
+    UNIT_MINUTE,
+)
+
+CONF_ENERGY_PRODUCTION_DAY              = "energy_production_day"
+CONF_TOTAL_ENERGY_PRODUCTION            = "total_energy_production"
+CONF_TOTAL_GENERAION_TIME               = "total_generation_time"
+CONF_TODAY_GENERAION_TIME               = "today_generation_time"
+CONF_PV1                                = "pv_1"
+CONF_PV2                                = "pv_2"
+UNIT_KILOWATT_HOURS                     = "kWh"
+UNIT_HOURS                              = "hrs"
+UNIT_KOHM                               = "KΩ"
+UNIT_MILIAMPERE                         = "mA"
+
+
+CONF_INVERTER_MODULE_TEMP               = "inverter_module_temp"            
+CONF_INVERTER_INNER_TEMP                = "inverter_inner_temp"             
+CONF_INVERTER_BUS_VOLTAGE               = "inverter_bus_voltage"            
+CONF_PV1_VOLTAGE_SAMPLED_BY_SLAVE_CPU   = "pv1_volt_sampled_by_slave_cpu"
+CONF_PV2_VOLTAGE_SAMPLED_BY_SLAVE_CPU   = "pv2_volt_sampled_by_slave_cpu"                    
+CONF_INSULATION_OF_PV1_P_TO_GROUND      = "insulation_pv1_p_to_ground"   
+CONF_INSULATION_OF_PV2_P_TO_GROUND      = "insulation_pv2_p_to_ground"   
+CONF_INSULATION_OF_PV_N_TO_GROUND       = "insulation_pv_n_to_ground"    
+CONF_GFCI_VALUE                         = "gfci_value"                      
+CONF_DCI_OF_R                           = "dci_of_r"                        
+CONF_DCI_OF_S                           = "dci_of_s"                        
+CONF_DCI_OF_T                           = "dci_of_t"                        
+
+
+AUTO_LOAD = ["modbus"]
+CODEOWNERS = ["@sourabhjaiswal"]
+
+havells_solar_ns = cg.esphome_ns.namespace("havells_solar")
+HAVELLSSolar = havells_solar_ns.class_("HAVELLSSolar", cg.PollingComponent, modbus.ModbusDevice)
+
+PHASE_SENSORS = {
+    CONF_VOLTAGE: sensor.sensor_schema(UNIT_VOLT, ICON_EMPTY, 2, DEVICE_CLASS_VOLTAGE),
+    CONF_CURRENT: sensor.sensor_schema(
+        UNIT_AMPERE, ICON_EMPTY, 2, DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT
+    ),
+}
+PV_SENSORS = {
+    CONF_VOLTAGE: sensor.sensor_schema(UNIT_VOLT, ICON_EMPTY, 2, DEVICE_CLASS_VOLTAGE),
+    CONF_CURRENT: sensor.sensor_schema(
+        UNIT_AMPERE, ICON_EMPTY, 2, DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT
+    ),
+    CONF_ACTIVE_POWER: sensor.sensor_schema(
+        UNIT_WATT, ICON_EMPTY, 0, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+    ),
+}
+
+PHASE_SCHEMA = cv.Schema(
+    {cv.Optional(sensor): schema for sensor, schema in PHASE_SENSORS.items()}
+)
+PV_SCHEMA = cv.Schema(
+    {cv.Optional(sensor): schema for sensor, schema in PV_SENSORS.items()}
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(HAVELLSSolar),
+            cv.Optional(CONF_PHASE_A): PHASE_SCHEMA,
+            cv.Optional(CONF_PHASE_B): PHASE_SCHEMA,
+            cv.Optional(CONF_PHASE_C): PHASE_SCHEMA,
+            cv.Optional(CONF_PV1): PV_SCHEMA,
+            cv.Optional(CONF_PV2): PV_SCHEMA,
+            cv.Optional(CONF_FREQUENCY): sensor.sensor_schema(
+                UNIT_HERTZ,
+                ICON_CURRENT_AC,
+                2,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_ACTIVE_POWER): sensor.sensor_schema(
+                UNIT_WATT,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_POWER,
+                STATE_CLASS_MEASUREMENT
+            ),
+            cv.Optional(CONF_REACTIVE_POWER): sensor.sensor_schema(
+                UNIT_VOLT_AMPS_REACTIVE,
+                ICON_EMPTY,
+                2,
+                DEVICE_CLASS_POWER,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_ENERGY_PRODUCTION_DAY): sensor.sensor_schema(
+                UNIT_KILOWATT_HOURS,
+                ICON_EMPTY,
+                2,
+                DEVICE_CLASS_ENERGY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_TOTAL_ENERGY_PRODUCTION): sensor.sensor_schema(
+                UNIT_KILOWATT_HOURS,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_ENERGY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_TOTAL_GENERAION_TIME): sensor.sensor_schema(
+                UNIT_HOURS,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_TODAY_GENERAION_TIME): sensor.sensor_schema(
+                UNIT_MINUTE,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_INVERTER_MODULE_TEMP): sensor.sensor_schema(
+                UNIT_DEGREES,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_INVERTER_INNER_TEMP): sensor.sensor_schema(
+                UNIT_DEGREES,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_INVERTER_BUS_VOLTAGE): sensor.sensor_schema(
+                UNIT_VOLT,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_PV1_VOLTAGE_SAMPLED_BY_SLAVE_CPU): sensor.sensor_schema(
+                UNIT_VOLT,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_PV2_VOLTAGE_SAMPLED_BY_SLAVE_CPU): sensor.sensor_schema(
+                UNIT_VOLT,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_INSULATION_OF_PV1_P_TO_GROUND): sensor.sensor_schema(
+                UNIT_KOHM,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_INSULATION_OF_PV2_P_TO_GROUND): sensor.sensor_schema(
+                UNIT_KOHM,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_INSULATION_OF_PV_N_TO_GROUND): sensor.sensor_schema(
+                UNIT_KOHM,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_GFCI_VALUE): sensor.sensor_schema(
+                UNIT_MILIAMPERE,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_DCI_OF_R): sensor.sensor_schema(
+                UNIT_MILIAMPERE,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_DCI_OF_S): sensor.sensor_schema(
+                UNIT_MILIAMPERE,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_DCI_OF_T): sensor.sensor_schema(
+                UNIT_MILIAMPERE,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_EMPTY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+        }
+    )
+    .extend(cv.polling_component_schema("10s"))
+    .extend(modbus.modbus_device_schema(0x01))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await modbus.register_modbus_device(var, config)
+
+    if CONF_FREQUENCY in config:
+        sens = await sensor.new_sensor(config[CONF_FREQUENCY])
+        cg.add(var.set_frequency_sensor(sens))
+        
+    if CONF_ACTIVE_POWER in config:
+        sens = await sensor.new_sensor(config[CONF_ACTIVE_POWER])
+        cg.add(var.set_active_power_sensor(sens))
+        
+    if CONF_REACTIVE_POWER in config:
+        sens = await sensor.new_sensor(config[CONF_REACTIVE_POWER])
+        cg.add(var.set_reactive_power_sensor(sens))
+
+    if CONF_ENERGY_PRODUCTION_DAY in config:
+        sens = await sensor.new_sensor(config[CONF_ENERGY_PRODUCTION_DAY])
+        cg.add(var.set_today_production_sensor(sens))
+
+    if CONF_TOTAL_ENERGY_PRODUCTION in config:
+        sens = await sensor.new_sensor(config[CONF_TOTAL_ENERGY_PRODUCTION])
+        cg.add(var.set_total_energy_production_sensor(sens))
+
+    if CONF_TOTAL_GENERAION_TIME in config:
+        sens = await sensor.new_sensor(config[CONF_TOTAL_GENERAION_TIME])
+        cg.add(var.set_total_generation_time_sensor(sens))
+
+    if CONF_TODAY_GENERAION_TIME in config:
+        sens = await sensor.new_sensor(config[CONF_TODAY_GENERAION_TIME])
+        cg.add(var.set_today_generation_time_sensor(sens))
+
+    if CONF_INVERTER_MODULE_TEMP in config:
+        sens = await sensor.new_sensor(config[CONF_INVERTER_MODULE_TEMP])
+        cg.add(var.set_inverter_module_temp_sensor(sens))
+
+    if CONF_INVERTER_INNER_TEMP in config:
+        sens = await sensor.new_sensor(config[CONF_INVERTER_INNER_TEMP])
+        cg.add(var.set_inverter_inner_temp_sensor(sens))
+
+    if CONF_INVERTER_BUS_VOLTAGE in config:
+        sens = await sensor.new_sensor(config[CONF_INVERTER_BUS_VOLTAGE])
+        cg.add(var.set_inverter_bus_voltage_sensor(sens))
+
+    if CONF_PV1_VOLTAGE_SAMPLED_BY_SLAVE_CPU in config:
+        sens = await sensor.new_sensor(config[CONF_PV1_VOLTAGE_SAMPLED_BY_SLAVE_CPU])
+        cg.add(var.set_pv1_volt_sampled_by_slave_cpu_sensor(sens))
+
+    if CONF_PV2_VOLTAGE_SAMPLED_BY_SLAVE_CPU in config:
+        sens = await sensor.new_sensor(config[CONF_PV2_VOLTAGE_SAMPLED_BY_SLAVE_CPU])
+        cg.add(var.set_pv2_volt_sampled_by_slave_cpu_sensor(sens))
+
+    if CONF_INSULATION_OF_PV1_P_TO_GROUND in config:
+        sens = await sensor.new_sensor(config[CONF_INSULATION_OF_PV1_P_TO_GROUND])
+        cg.add(var.set_insulation_pv1_p_to_ground_sensor(sens))
+
+    if CONF_INSULATION_OF_PV2_P_TO_GROUND in config:
+        sens = await sensor.new_sensor(config[CONF_INSULATION_OF_PV2_P_TO_GROUND])
+        cg.add(var.set_insulation_pv2_p_to_ground_sensor(sens))
+
+    if CONF_INSULATION_OF_PV_N_TO_GROUND in config:
+        sens = await sensor.new_sensor(config[CONF_INSULATION_OF_PV_N_TO_GROUND])
+        cg.add(var.set_insulation_pv_n_to_ground_sensor(sens))
+
+    if CONF_GFCI_VALUE in config:
+        sens = await sensor.new_sensor(config[CONF_GFCI_VALUE])
+        cg.add(var.set_gfci_value_sensor(sens))
+
+    if CONF_DCI_OF_R in config:
+        sens = await sensor.new_sensor(config[CONF_DCI_OF_R])
+        cg.add(var.set_dci_of_r_sensor(sens))
+
+    if CONF_DCI_OF_S in config:
+        sens = await sensor.new_sensor(config[CONF_DCI_OF_S])
+        cg.add(var.set_dci_of_s_sensor(sens))
+
+    if CONF_DCI_OF_T in config:
+        sens = await sensor.new_sensor(config[CONF_DCI_OF_T])
+        cg.add(var.set_dci_of_t_sensor(sens))
+
+    for i, phase in enumerate([CONF_PHASE_A, CONF_PHASE_B, CONF_PHASE_C]):
+        if phase not in config:
+            continue
+
+        phase_config = config[phase]
+        for sensor_type in PHASE_SENSORS:
+            if sensor_type in phase_config:
+                sens = await sensor.new_sensor(phase_config[sensor_type])
+                cg.add(getattr(var, f"set_{sensor_type}_sensor")(i, sens))
+                
+    for i, pv in enumerate([CONF_PV1, CONF_PV2]):
+        if pv not in config:
+            continue
+
+        pv_config = config[pv]
+        for sensor_type in pv_config:
+            if sensor_type in pv_config:
+                sens = await sensor.new_sensor(pv_config[sensor_type])
+                cg.add(getattr(var, f"set_{sensor_type}_sensor_pv")(i, sens))
