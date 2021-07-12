e3facd2c667cf32ad07e68df7b3bf49b7f5c9fa6
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-07-12 12:53:01 +1200

    Fix codeowner

--- esphome/components/panasonic/climate.py
+++ esphome/components/panasonic/climate.py
@@ -4,6 +4,7 @@ from esphome.components import climate_ir
 from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
+CODEOWNERS = ["@WeekendWarrior1"]
 
 panasonic_ns = cg.esphome_ns.namespace("panasonic")
 PanasonicClimate = panasonic_ns.class_("PanasonicClimate", climate_ir.ClimateIR)
