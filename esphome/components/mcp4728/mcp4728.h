33e569bb17bd3212de80b3bef4fbe675b2ed67e7
Jarad Olson <brotherdust@gmail.com>
2021-07-12 18:30:12 -0600

    Import MCP4728 support from my config directory

---
+++ esphome/components/mcp4728/mcp4728.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/output/float_output.h"
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/core/log.h"
+#include <Adafruit_MCP4728.h>
+// #include <Wire.h>
+// #include <SPI.h>
+
+namespace esphome
+{
+  namespace mcp4728
+  {
+    Adafruit_MCP4728 mcp;
+    static const char *const TAG = "mcp4728";
+
+    class MCP4728Output;
+
+    class MCP4728Channel : public output::FloatOutput
+    {
+    public:
+      MCP4728Channel(MCP4728OutputComponent *parent, uint8_t channel) : parent_(parent), channel_(channel) {}
+
+    protected:
+      void write_state(float state) override
+      {
+        MCP4728_channel_t channel_enum;
+        uint16_t value = (uint16_t)round(state * (pow(2, 12) - 1));
+        switch (this->channel_)
+        {
+        case 0:
+          channel_enum = MCP4728_CHANNEL_A;
+          break;
+        case 1:
+          channel_enum = MCP4728_CHANNEL_B;
+          break;
+        case 2:
+          channel_enum = MCP4728_CHANNEL_C;
+          break;
+        case 3:
+          channel_enum = MCP4728_CHANNEL_D;
+          break;
+        }
+        this->parent_->set_channel_value_(channel_enum, value);
+      };
+
+      MCP4728OutputComponent *parent_;
+      uint8_t channel_;
+    };
+
+    class MCP4728OutputComponent : public Component, public i2c::I2CDevice
+    {
+    public:
+      MCP4728OutputComponent(float value) : value_(value) {}
+      MCP4728Channel *create_channel(uint8_t channel)
+      {
+        auto *c = new MCP4728Channel(this, channel);
+        return c;
+      }
+
+      void setup() override
+      {
+        ESP_LOGCONFIG(TAG, "Setting up MCP4728OutputComponent...");
+        if (!mcp.begin())
+        {
+          ESP_LOGE(TAG, "Communication with MCP4725 failed!");
+          while (1)
+          {
+            delay(10);
+          }
+        }
+      };
+      float get_setup_priority() const override { return setup_priority::HARDWARE; }
+
+    protected:
+      friend MCP4728Channel;
+      void set_channel_value_(MCP4728_channel_t channel, uint16_t value)
+      {
+        mcp.setChannelValue(
+            channel,
+            value,
+            MCP4728_VREF_VDD,
+            MCP4728_GAIN_2X,
+            MCP4728_PD_MODE_NORMAL,
+            false);
+      }
+      float value_;
+    };
+
+  } // namespace mcp4728
+} // namespace esphome
---
+++ esphome/components/mcp4728/output.py
@@ -0,0 +1,26 @@
+from custom_components import mcp4728
+import esphome.config_validation as cv
+import esphome.codegen as cg
+from esphome.components import output, i2c
+from esphome.const import CONF_ID, CONF_CHANNEL
+from . import MCP4728Output, mcp4728_ns
+
+DEPENDENCIES = ["mcp4728"]
+
+MCP4728Channel = mcp4728_ns.class_("MCP4728Channel", output.FloatOutput)
+CONF_MCP4728_ID = "mcp4728_id"
+
+CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
+    {
+        cv.Required(CONF_ID): cv.declare_id(MCP4728Channel),
+        cv.GenerateID(CONF_MCP4728_ID): cv.use_id(MCP4728Output),
+        cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=3)
+    }
+)
+
+
+async def to_code(config):
+    paren = await cg.get_variable(config[CONF_MCP4728_ID])
+    rhs = paren.create_channel(config[CONF_CHANNEL])
+    var = cg.Pvariable(config[CONF_ID], rhs)
+    await output.register_output(var, config)
