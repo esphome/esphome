f6b4ec07d65b046a2b1aa968487f694458622742
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-07-15 07:36:12 +1200

    Lowercase action

--- esphome/components/pipsolar/__init__.py
+++ esphome/components/pipsolar/__init__.py
@@ -30,7 +30,7 @@ def to_code(config):
 
 
 @automation.register_action(
-    "Pipsolaroutput.set_level",
+    "pipsolaroutput.set_level",
     SetOutputAction,
     cv.Schema(
         {
@@ -39,7 +39,7 @@ def to_code(config):
         }
     ),
 )
-def Pipsolaroutput_set_level_to_code(config, action_id, template_arg, args):
+def pipsolaroutput_set_level_to_code(config, action_id, template_arg, args):
     paren = yield cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
     template_ = yield cg.templatable(config[CONF_VALUE], args, float)
