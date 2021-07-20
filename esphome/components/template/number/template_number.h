99d2db42cd6336f0bc6a5e8ad7425eb2f008473c
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-07-20 15:40:42 +1200

    Add restore_value to template number (#2041)

--- esphome/components/template/number/template_number.h
+++ esphome/components/template/number/template_number.h
@@ -20,11 +20,13 @@ class TemplateNumber : public number::Number, public PollingComponent {
   Trigger<float> *get_set_trigger() const { return set_trigger_; }
   void set_optimistic(bool optimistic) { optimistic_ = optimistic; }
   void set_initial_value(float initial_value) { initial_value_ = initial_value; }
+  void set_restore_value(bool restore_value) { this->restore_value_ = restore_value; }
 
  protected:
   void control(float value) override;
   bool optimistic_{false};
   float initial_value_{NAN};
+  bool restore_value_{false};
   Trigger<float> *set_trigger_ = new Trigger<float>();
   optional<std::function<optional<float>()>> f_;
 
