e52c5eb1a2f53ae98ee7ac948dfd3a9d1d3d016e
Peter van Dijk <peter@7bits.nl>
2021-07-23 19:45:12 +0200

    change icon

--- esphome/components/pm1006/sensor.py
+++ esphome/components/pm1006/sensor.py
@@ -7,7 +7,7 @@ from esphome.const import (
     DEVICE_CLASS_EMPTY,
     STATE_CLASS_MEASUREMENT,
     UNIT_MICROGRAMS_PER_CUBIC_METER,
-    ICON_CHEMICAL_WEAPON,
+    ICON_BLUR,
 )
 
 DEPENDENCIES = ["uart"]
@@ -22,7 +22,7 @@ CONFIG_SCHEMA = cv.All(
             cv.GenerateID(): cv.declare_id(PM1006Component),
             cv.Optional(CONF_PM_2_5): sensor.sensor_schema(
                 UNIT_MICROGRAMS_PER_CUBIC_METER,
-                ICON_CHEMICAL_WEAPON,
+                ICON_BLUR,
                 0,
                 DEVICE_CLASS_EMPTY,
                 STATE_CLASS_MEASUREMENT,
