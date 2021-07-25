3fdb70e056ea643b88b740b8ad5507409ce7082a
Nicholas Peters <npnickster@gmail.com>
2021-07-25 19:01:21 -0400

    Add STATE_CLASS_MEASUREMENT to sdp3x
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/sdp3x/sensor.py
+++ esphome/components/sdp3x/sensor.py
@@ -15,7 +15,13 @@ sdp3x_ns = cg.esphome_ns.namespace("sdp3x")
 SDP3XComponent = sdp3x_ns.class_("SDP3XComponent", cg.PollingComponent, i2c.I2CDevice)
 
 CONFIG_SCHEMA = (
-    sensor.sensor_schema(UNIT_HECTOPASCAL, ICON_EMPTY, 3, DEVICE_CLASS_PRESSURE)
+    sensor.sensor_schema(
+        UNIT_HECTOPASCAL, 
+        ICON_EMPTY, 
+        3, 
+        DEVICE_CLASS_PRESSURE, 
+        STATE_CLASS_MEASUREMENT,
+    )
     .extend(
         {
             cv.GenerateID(): cv.declare_id(SDP3XComponent),
