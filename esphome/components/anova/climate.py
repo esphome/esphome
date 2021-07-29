69a3e22f227763af253efc234b058e1348613d29
Otto winter <otto@otto-winter.com>
2021-07-29 18:03:59 +0200

    Convert more code to async-def syntax

--- esphome/components/anova/climate.py
+++ esphome/components/anova/climate.py
@@ -18,8 +18,8 @@ CONFIG_SCHEMA = (
 )
 
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield climate.register_climate(var, config)
-    yield ble_client.register_ble_node(var, config)
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
+    await ble_client.register_ble_node(var, config)
