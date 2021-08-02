57fe0426e1c83b93d7e60ece502a27a80b11464c
Jarad Olson <brotherdust@gmail.com>
2021-08-02 17:31:59 -0600

    Add initial support for m5stack_4relay
    Signed-off-by: Jarad Olson <brotherdust@gmail.com>

---
+++ esphome/components/m5stack_4relay/__init__.py
@@ -0,0 +1,31 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c
+from esphome.const import (
+    CONF_ID
+)
+
+DEPENDENCIES = ["i2c"]
+MULTI_CONF = True
+AUTO_LOAD = ["switch"]
+
+m5stack_4relay_ns = cg.esphome_ns.namespace("m5stack_4relay")
+M5STACK4RELAYOutput = m5stack_4relay_ns.class_("M5STACK4RELAYSwitchComponent", cg.Component, i2c.I2CDevice)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(M5STACK4RELAYOutput),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(0x26))
+)
+
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield i2c.register_i2c_device(var, config)
+
+    # cg.add_library("adafruit/Adafruit M5STACK4RELAY", "1.0.7")
