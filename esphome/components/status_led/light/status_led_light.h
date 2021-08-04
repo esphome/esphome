95326d6f54cc78fa48090281b36a89677aaff5ef
Adrian Panella <ianchi74@outlook.com>
2021-08-04 15:24:09 -0500

    Split status_led_light into .h and .cpp

--- esphome/components/status_led/light/status_led_light.h
+++ esphome/components/status_led/light/status_led_light.h
@@ -1,14 +1,11 @@
 #pragma once
 
-#include "esphome/core/application.h"
 #include "esphome/core/component.h"
 #include "esphome/core/esphal.h"
 #include "esphome/components/light/light_output.h"
 
 namespace esphome {
 namespace status_led {
-
-static const char *const TAG = "status_led";
 
 class StatusLEDLightOutput : public light::LightOutput, public Component {
  public:
@@ -20,65 +17,16 @@ class StatusLEDLightOutput : public light::LightOutput, public Component {
     return traits;
   }
 
-  void loop() override {
-    uint32_t new_state = App.get_app_state();
+  void loop() override;
 
-    if (new_state != this->last_app_state_) {
-      ESP_LOGD(TAG, "New app state %08X", new_state);
-    }
+  void setup_state(light::LightState *state) override;
 
-    if ((new_state & STATUS_LED_ERROR) != 0u) {
-      this->pin_->digital_write(millis() % 250u < 150u);
-      this->last_app_state_ = new_state;
-    } else if ((new_state & STATUS_LED_WARNING) != 0u) {
-      this->pin_->digital_write(millis() % 1500u < 250u);
-      this->last_app_state_ = new_state;
-    } else if (new_state != this->last_app_state_) {
-      // if no error/warning -> restore light state or turn off
-      bool state = false;
+  void write_state(light::LightState *state) override;
 
-      if (lightstate_)
-        lightstate_->current_values_as_binary(&state);
+  void setup() override;
 
-      this->pin_->digital_write(state);
-      this->last_app_state_ = new_state;
+  void dump_config() override;
 
-      ESP_LOGD(TAG, "Restoring light state %s", ONOFF(state));
-    }
-  }
-
-  void setup_state(light::LightState *state) override {
-    lightstate_ = state;
-    this->pin_->setup();
-    ESP_LOGD(TAG, "'%s': Setting initital state", state->get_name().c_str());
-    this->write_state(state);
-  }
-
-  void write_state(light::LightState *state) override {
-    bool binary;
-    state->current_values_as_binary(&binary);
-
-    // if in warning/error, don't overwrite the status_led
-    // once it is back to OK, the loop will restore the state
-    if ((App.get_app_state() & (STATUS_LED_ERROR | STATUS_LED_WARNING)) == 0u) {
-      this->pin_->digital_write(binary);
-      ESP_LOGD(TAG, "'%s': Setting state %s", state->get_name().c_str(), ONOFF(binary));
-    }
-  }
-
-  void setup() override {
-    ESP_LOGD(TAG, "Setting up Status LED...");
-
-    this->pin_->setup();
-    this->pin_->digital_write(false);
-  }
-
-  void dump_config() override {
-    ESP_LOGCONFIG(TAG, "Status Led Light:");
-    LOG_PIN("  Pin: ", this->pin_);
-    if (this->pin_->is_inverted())
-      ESP_LOGCONFIG(TAG, "  Inverted: YES");
-  }
   float get_setup_priority() const override { return setup_priority::HARDWARE; }
   float get_loop_priority() const override { return 50.0f; }
 
