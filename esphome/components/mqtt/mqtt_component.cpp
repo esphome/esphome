695ac9e51e364eb9471aef8342da1375533dc85a
wifwucite <sv.thiel@web.de>
2021-08-22 12:11:59 +0200

    formatting fixed

--- esphome/components/mqtt/mqtt_component.cpp
+++ esphome/components/mqtt/mqtt_component.cpp
@@ -171,7 +171,8 @@ void MQTTComponent::call_setup() {
 
 void MQTTComponent::dump_config() {
   if (this->is_internal()) {
-    ESP_LOGCONFIG(TAG, "Component %s is internal, it is NOT exposed via MQTT, ignore topics below!", friendly_name().c_str());
+    ESP_LOGCONFIG(TAG, "Component %s is internal, it is NOT exposed via MQTT, ignore topics below!", 
+                  friendly_name().c_str());
   }
 }
 
