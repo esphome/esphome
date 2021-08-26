f3947ece814d48340d26c57e273f34c982efaf4a
WeekendWarrior1 <c.halton.93@gmail.com>
2021-08-26 21:16:48 +1000

    move hbridge light to subfolder

--- esphome/components/hbridge/light/light.py
+++ esphome/components/hbridge/light/light.py
@@ -2,8 +2,9 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light, output
 from esphome.const import CONF_OUTPUT_ID, CONF_PIN_A, CONF_PIN_B
+from .. import hbridge_ns
 
-hbridge_ns = cg.esphome_ns.namespace("hbridge")
+
 HBridgeLightOutput = hbridge_ns.class_(
     "HBridgeLightOutput", cg.PollingComponent, light.LightOutput
 )
