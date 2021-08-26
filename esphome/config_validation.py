7063dc8906a2a5080199a3885bef844bd1574e48
Paul Monigatti <pm@paul.pm>
2021-08-26 14:34:56 +1200

    Check all components in packages rather than just the first entry

--- esphome/config_validation.py
+++ esphome/config_validation.py
@@ -1457,7 +1457,7 @@ class OnlyWith(Optional):
         if self._component in CORE.raw_config or (
             CONF_PACKAGES in CORE.raw_config
             and self._component
-            in {list(x.keys())[0] for x in CORE.raw_config[CONF_PACKAGES].values()}
+            in [k for package in CORE.raw_config[CONF_PACKAGES].values() for k in package.keys()]
         ):
             return self._default
         return vol.UNDEFINED
