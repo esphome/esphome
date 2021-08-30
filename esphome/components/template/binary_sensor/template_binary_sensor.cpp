c81bd6ee2f1cd0b9dd286e94bc27ea391e2a52b1
Chris Nussbaum <chris.nussbaum@protolabs.com>
2021-08-30 11:46:26 -0500

    Template sensors always publish on update interval

--- esphome/components/template/binary_sensor/template_binary_sensor.cpp
+++ esphome/components/template/binary_sensor/template_binary_sensor.cpp
@@ -7,12 +7,13 @@ namespace template_ {
 static const char *const TAG = "template.binary_sensor";
 
 void TemplateBinarySensor::loop() {
-  if (!this->f_.has_value())
-    return;
-
-  auto s = (*this->f_)();
-  if (s.has_value()) {
-    this->publish_state(*s);
+  if (this->f_.has_value()) {
+    auto s = (*this->f_)();
+    if (s.has_value()) {
+      this->publish_state(*s);
+    }
+  } else if (this->has_state()) {
+    this->publish_state(this->state);
   }
 }
 void TemplateBinarySensor::dump_config() { LOG_BINARY_SENSOR("", "Template Binary Sensor", this); }
