746e46c22e527df8aa38e2bdab31de036e50aa27
Chris Nussbaum <chris.nussbaum@protolabs.com>
2021-09-02 07:49:28 -0500

    Revert the update for the binary template sensor

--- esphome/components/template/binary_sensor/template_binary_sensor.cpp
+++ esphome/components/template/binary_sensor/template_binary_sensor.cpp
@@ -7,13 +7,12 @@ namespace template_ {
 static const char *const TAG = "template.binary_sensor";
 
 void TemplateBinarySensor::loop() {
-  if (this->f_.has_value()) {
-    auto s = (*this->f_)();
-    if (s.has_value()) {
-      this->publish_state(*s);
-    }
-  } else if (this->has_state()) {
-    this->publish_state(this->state);
+  if (!this->f_.has_value())
+    return;
+
+  auto s = (*this->f_)();
+  if (s.has_value()) {
+    this->publish_state(*s);
   }
 }
 void TemplateBinarySensor::dump_config() { LOG_BINARY_SENSOR("", "Template Binary Sensor", this); }
