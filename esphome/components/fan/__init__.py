cb18a57debb6dbb3fc415c1eac156580de690180
dgtal1 <27864579+dgtal1@users.noreply.github.com>
2021-09-05 10:56:22 +0200

    Add trigger for fan speed changes
    Added trigger for fan speed changes whenever a new speed is set either externally via Home Assistant, another external channel or internally.
    
    The trigger name is `on_set_speed`.  I updated test1.yaml and will update the docs to reflect the new feature.

--- esphome/components/fan/__init__.py
+++ esphome/components/fan/__init__.py
@@ -15,6 +15,7 @@ from esphome.const import (
     CONF_SPEED_COMMAND_TOPIC,
     CONF_SPEED_STATE_TOPIC,
     CONF_NAME,
+    CONF_ON_SET_SPEED,
     CONF_ON_TURN_OFF,
     CONF_ON_TURN_ON,
     CONF_TRIGGER_ID,
@@ -41,6 +42,7 @@ ToggleAction = fan_ns.class_("ToggleAction", automation.Action)
 
 FanTurnOnTrigger = fan_ns.class_("FanTurnOnTrigger", automation.Trigger.template())
 FanTurnOffTrigger = fan_ns.class_("FanTurnOffTrigger", automation.Trigger.template())
+FanSetSpeedTrigger = fan_ns.class_("FanSetSpeedTrigger", automation.Trigger.template())
 
 FanIsOnCondition = fan_ns.class_("FanIsOnCondition", automation.Condition.template())
 FanIsOffCondition = fan_ns.class_("FanIsOffCondition", automation.Condition.template())
@@ -69,6 +71,11 @@ FAN_SCHEMA = cv.NAMEABLE_SCHEMA.extend(cv.MQTT_COMMAND_COMPONENT_SCHEMA).extend(
         cv.Optional(CONF_ON_TURN_OFF): automation.validate_automation(
             {
                 cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(FanTurnOffTrigger),
+            }
+        ),
+        cv.Optional(CONF_ON_SET_SPEED): automation.validate_automation(
+            {
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(FanSetSpeedTrigger),
             }
         ),
     }
@@ -108,6 +115,9 @@ async def setup_fan_core_(var, config):
         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
         await automation.build_automation(trigger, [], conf)
     for conf in config.get(CONF_ON_TURN_OFF, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [], conf)
+    for conf in config.get(CONF_ON_SET_SPEED, []):
         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
         await automation.build_automation(trigger, [], conf)
 
