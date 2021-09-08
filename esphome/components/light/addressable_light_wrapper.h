96ab5661eda47b54142db0138c7080898fc0feb1
Paul Monigatti <pm@paul.pm>
2021-09-08 14:45:03 +1200

    Set light state via a call rather than directly to the output component

--- esphome/components/light/addressable_light_wrapper.h
+++ esphome/components/light/addressable_light_wrapper.h
@@ -19,16 +19,26 @@ class AddressableLightWrapper : public light::AddressableLight {
   light::LightTraits get_traits() override { return this->light_state_->get_traits(); }
 
   void write_state(light::LightState *state) override {
-    light::LightColorValues *ls = &state->current_values;
-    ls->set_brightness(1.0f);
-    ls->set_color_brightness(1.0f);
-    ls->set_state(1.0f);
-    ls->set_red(this->wrapper_state_[0] / 255.0f);
-    ls->set_green(this->wrapper_state_[1] / 255.0f);
-    ls->set_blue(this->wrapper_state_[2] / 255.0f);
-    ls->set_white(this->wrapper_state_[3] / 255.0f);
+    float gamma = this->light_state_->get_gamma_correct();
+    float r = gamma_uncorrect(this->wrapper_state_[0] / 255.0f, gamma);
+    float g = gamma_uncorrect(this->wrapper_state_[1] / 255.0f, gamma);
+    float b = gamma_uncorrect(this->wrapper_state_[2] / 255.0f, gamma);
+    float w = gamma_uncorrect(this->wrapper_state_[3] / 255.0f, gamma);
+    float brightness = fmaxf(r, fmaxf(g, b));
 
-    this->light_state_->get_output()->write_state(state);
+    auto call = this->light_state_->make_call();
+    call.set_state(true);
+    call.set_brightness_if_supported(brightness);
+    call.set_color_brightness_if_supported(1.0f);
+    call.set_red_if_supported(r);
+    call.set_green_if_supported(g);
+    call.set_blue_if_supported(b);
+    call.set_white_if_supported(w);
+    call.set_transition_length_if_supported(0);
+    call.set_publish(false);
+    call.set_save(false);
+    call.perform();
+
     this->mark_shown_();
   }
 
