292fe0ae11c9dd3b0e8cb8f54b898e25a95ac500
Robert Resch <robert@resch.dev>
2021-09-09 11:59:03 +0200

    fix lint errors

--- esphome/components/xiaomi_miscale2/sensor.py
+++ esphome/components/xiaomi_miscale2/sensor.py
@@ -1,3 +1,5 @@
 import esphome.config_validation as cv
 
-CONFIG_SCHEMA = cv.invalid("This platform has been combined into xiaomi_miscale. Use xiaomi_miscale instead.")
+CONFIG_SCHEMA = cv.invalid(
+    "This platform has been combined into xiaomi_miscale. Use xiaomi_miscale instead."
+)
