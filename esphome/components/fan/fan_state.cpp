a5e5fd25085f88bfd4e8da78e4221fa0134d36ae
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2021-09-11 15:02:46 +0200

    Suppress excessive warnings about deprecated Fan interfaces

--- esphome/components/fan/fan_state.cpp
+++ esphome/components/fan/fan_state.cpp
@@ -67,6 +67,8 @@ void FanStateCall::perform() const {
   this->state_->state_callback_.call();
 }
 
+// This whole method is deprecated, don't warn about usage of deprecated methods inside of it.
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 FanStateCall &FanStateCall::set_speed(const char *legacy_speed) {
   const auto supported_speed_count = this->state_->get_traits().supported_speed_count();
   if (strcasecmp(legacy_speed, "low") == 0) {
