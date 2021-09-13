a23cd53711c9dfa18a26b91c7acdee6359052922
martin <25747549+martgras@users.noreply.github.com>
2021-09-13 15:02:50 +0200

    Lint

--- esphome/components/modbus_controller/number/modbus_number.cpp
+++ esphome/components/modbus_controller/number/modbus_number.cpp
@@ -16,9 +16,7 @@ void ModbusNumber::update() {
 }
 
 /** Write a value to the device
- *
- *
- *
+
  */
 
 void ModbusNumber::control(float value) {
@@ -29,79 +27,83 @@ void ModbusNumber::control(float value) {
 
   std::vector<uint16_t> data;
 
-  float transformed_value = multiply_by_ * value;
+  value = multiply_by_ * value;
   if (this->transform_func_.has_value()) {
     // data is passed by reference
     // the lambda can fill the empty vector directly
     // in that case the return value is ignored
     auto val = (*this->transform_func_)(value, data);
     if (val.has_value())
-      transformed_value = val.value();
+      value = val.value();
+    else
+      value = multiply_by_ * value;
+  } else {
+    value = multiply_by_ * value;
   }
   // lambda didn't set payload
-  if (data.size() == 0) {
+  if (data.empty()) {
+    int32_t val;
     if (connected_sensor_) {
       switch (connected_sensor_->sensor_value_type) {
         case SensorValueType::U_WORD:
         case SensorValueType::S_WORD:
           // cast truncates the float do some rounding here
-          data.push_back(int32_t(transformed_value + 0.5));
+          data.push_back(lroundf(value) & 0xFFFF);
           break;
         case SensorValueType::U_DWORD:
         case SensorValueType::S_DWORD:
-          transformed_value += 0.5;
-          data.push_back((int32_t(transformed_value) && 0xFFFF0000) >> 16);
-          data.push_back(int32_t(transformed_value) && 0xFFFF);
+          val = lroundf(value);
+          data.push_back((val & 0xFFFF0000) >> 16);
+          data.push_back(val & 0xFFFF);
           break;
         case SensorValueType::U_DWORD_R:
         case SensorValueType::S_DWORD_R:
-          transformed_value += 0.5;
-          data.push_back(int32_t(transformed_value) && 0xFFFF);
-          data.push_back((int32_t(transformed_value) && 0xFFFF0000) >> 16);
+          val = lroundf(value);
+          data.push_back(val & 0xFFFF);
+          data.push_back((val & 0xFFFF0000) >> 16);
           break;
         case SensorValueType::FP32:
-          raw_to_float.float_value = transformed_value;
-          data.push_back((raw_to_float.raw && 0xFFFF0000) >> 16);
-          data.push_back(raw_to_float.raw && 0xFFFF);
+          raw_to_float.float_value = value;
+          data.push_back((raw_to_float.raw & 0xFFFF0000) >> 16);
+          data.push_back(raw_to_float.raw & 0xFFFF);
           break;
         case SensorValueType::FP32_R:
-          raw_to_float.float_value = transformed_value;
-          data.push_back(raw_to_float.raw && 0xFFFF);
-          data.push_back((raw_to_float.raw && 0xFFFF0000) >> 16);
+          raw_to_float.float_value = value;
+          data.push_back(raw_to_float.raw & 0xFFFF);
+          data.push_back((raw_to_float.raw & 0xFFFF0000) >> 16);
           break;
         default:
-          ESP_LOGE("TAG", "Invalid data type for modbus number %s", this->get_name());
+          ESP_LOGE("TAG", "Invalid data type for modbus number %s", this->get_name().c_str());
           return;
           break;
       }
     }
   }
-  ESP_LOGD(TAG,
-           "Updating register: connected Sensor=%s start address=0x%X register count=%d new value=%.02f (val=%.02f)",
-           connected_sensor_->get_name().c_str(), connected_sensor_->start_address, connected_sensor_->register_count,
-           value, transformed_value);
   if (connected_sensor_) {
+    ESP_LOGD(TAG,
+             "Updating register: connected Sensor=%s start address=0x%X register count=%d new value=%.02f (val=%.02f)",
+             connected_sensor_->get_name().c_str(), connected_sensor_->start_address, connected_sensor_->register_count,
+             value, value);
+
     // Create and send the write command
     auto write_cmd = ModbusCommandItem::create_write_multiple_command(
         parent_, connected_sensor_->start_address + connected_sensor_->offset, connected_sensor_->register_count, data);
 
     // publish new value
-    write_cmd.on_data_func = [this, write_cmd, transformed_value](ModbusFunctionCode function_code,
-                                                                  uint16_t start_address,
-                                                                  const std::vector<uint8_t> data) {
+    write_cmd.on_data_func = [this, write_cmd, value](ModbusFunctionCode function_code, uint16_t start_address,
+                                                      const std::vector<uint8_t> &data) {
       // gets called when the write command is ack'd from the device
       parent_->on_write_register_response(write_cmd.function_code, start_address, data);
       if (connected_sensor_)
-        connected_sensor_->publish_state(transformed_value);
-      this->publish_state(transformed_value);
+        connected_sensor_->publish_state(value);
+      this->publish_state(value);
     };
     parent_->queue_command(write_cmd);
   }
 }
 void ModbusNumber::dump_config() {
   LOG_NUMBER(TAG, "Modbus Number", this);
-  LOG_UPDATE_INTERVAL(this);
-  ESP_LOGCONFIG(TAG, " Connected Sensor %s", connected_sensor_->get_name());
+  ESP_LOGCONFIG(TAG, " Connected Sensor %s", connected_sensor_->get_name().c_str());
 }
 
 }  // namespace modbus_controller
