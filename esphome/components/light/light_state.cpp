b3866c397e484718781d26a8463659f6033354cd
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2021-09-15 17:07:37 +0200

    Update current_values after transition finishes
    Fixes esphome/issues#2367.

--- esphome/components/light/light_state.cpp
+++ esphome/components/light/light_state.cpp
@@ -121,6 +121,9 @@ void LightState::loop() {
     }
 
     if (this->transformer_->is_finished()) {
+      // if the transition has written directly to the output, current_values is outdated, so update it
+      this->current_values = this->transformer_->get_target_values();
+
       this->transformer_->stop();
       this->transformer_ = nullptr;
       this->target_state_reached_callback_.call();
