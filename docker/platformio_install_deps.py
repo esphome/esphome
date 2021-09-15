aed140d80208032ecb6d3d3358b6c48b7dee95be
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2021-09-15 19:13:30 +0200

    Fix typo

--- docker/platformio_install_deps.py
+++ docker/platformio_install_deps.py
@@ -10,4 +10,4 @@ config = configparser.ConfigParser(inline_comment_prefixes=(';', ))
 config.read(sys.argv[1])
 libs = [x for x in config['common']['lib_deps'].splitlines() if len(x) != 0]
 
-subprocess.check_call(['platformio', 'lib', '-g', 'uninstall', *libs])
+subprocess.check_call(['platformio', 'lib', '-g', 'install', *libs])
