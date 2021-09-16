31879b2ceba7541c7f6694ad91d16d220f9a6a73
Paul Monigatti <pm@paul.pm>
2021-09-16 19:00:10 +1200

    Use single_light_id to specify a non-addressable light segment

--- esphome/components/partition/light.py
+++ esphome/components/partition/light.py
@@ -7,6 +7,7 @@ from esphome.const import (
     CONF_ID,
     CONF_LIGHT_ID,
     CONF_SEGMENTS,
+    CONF_SINGLE_LIGHT_ID,
     CONF_TO,
     CONF_OUTPUT_ID,
     CONF_REVERSED,
@@ -23,7 +24,7 @@ PartitionLightOutput = partitions_ns.class_(
 
 
 def validate_from_to(value):
-    if value[CONF_FROM] > value[CONF_TO]:
+    if CONF_ID in value and value[CONF_FROM] > value[CONF_TO]:
         raise cv.Invalid(
             "From ({}) must not be larger than to ({})"
             "".format(value[CONF_FROM], value[CONF_TO])
@@ -31,35 +32,32 @@ def validate_from_to(value):
     return value
 
 
+ADDRESSABLE_SEGMENT_SCHEMA = cv.Schema(
+    {
+        cv.Required(CONF_ID): cv.use_id(light.AddressableLightState),
+        cv.Required(CONF_FROM): cv.positive_int,
+        cv.Required(CONF_TO): cv.positive_int,
+        cv.Optional(CONF_REVERSED, default=False): cv.boolean,
+    }
+)
+
+NONADDRESSABLE_SEGMENT_SCHEMA = cv.COMPONENT_SCHEMA.extend(
+    {
+        cv.Required(CONF_SINGLE_LIGHT_ID): cv.use_id(light.LightState),
+        cv.GenerateID(CONF_ADDRESSABLE_LIGHT_ID): cv.declare_id(
+            AddressableLightWrapper
+        ),
+        cv.GenerateID(CONF_LIGHT_ID): cv.declare_id(light.types.LightState),
+    }
+)
+
 CONFIG_SCHEMA = light.ADDRESSABLE_LIGHT_SCHEMA.extend(
     {
         cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(PartitionLightOutput),
         cv.Required(CONF_SEGMENTS): cv.All(
             cv.ensure_list(
-                cv.Any(
-                    cv.Schema(
-                        {
-                            cv.Required(CONF_ID): cv.use_id(
-                                light.AddressableLightState
-                            ),
-                            cv.Required(CONF_FROM): cv.positive_int,
-                            cv.Required(CONF_TO): cv.positive_int,
-                            cv.Optional(CONF_REVERSED, default=False): cv.boolean,
-                        },
-                        validate_from_to,
-                    ),
-                    cv.Schema(
-                        {
-                            cv.Required(CONF_ID): cv.use_id(light.LightState),
-                            cv.GenerateID(CONF_ADDRESSABLE_LIGHT_ID): cv.declare_id(
-                                AddressableLightWrapper
-                            ),
-                            cv.GenerateID(CONF_LIGHT_ID): cv.declare_id(
-                                light.types.LightState
-                            ),
-                        },
-                    ),
-                )
+                cv.Any(ADDRESSABLE_SEGMENT_SCHEMA, NONADDRESSABLE_SEGMENT_SCHEMA),
+                validate_from_to,
             ),
             cv.Length(min=1),
         ),
@@ -70,10 +68,11 @@ CONFIG_SCHEMA = light.ADDRESSABLE_LIGHT_SCHEMA.extend(
 async def to_code(config):
     segments = []
     for conf in config[CONF_SEGMENTS]:
-        var = await cg.get_variable(conf[CONF_ID])
-
-        if CONF_ADDRESSABLE_LIGHT_ID in conf and CONF_LIGHT_ID in conf:
-            wrapper = cg.new_Pvariable(conf[CONF_ADDRESSABLE_LIGHT_ID], var)
+        if CONF_SINGLE_LIGHT_ID in conf:
+            wrapper = cg.new_Pvariable(
+                conf[CONF_ADDRESSABLE_LIGHT_ID],
+                await cg.get_variable(conf[CONF_SINGLE_LIGHT_ID]),
+            )
             light_state = cg.new_Pvariable(conf[CONF_LIGHT_ID], "", wrapper)
             await cg.register_component(light_state, conf)
             cg.add(cg.App.register_light(light_state))
@@ -82,7 +81,7 @@ async def to_code(config):
         else:
             segments.append(
                 AddressableSegment(
-                    var,
+                    await cg.get_variable(conf[CONF_ID]),
                     conf[CONF_FROM],
                     conf[CONF_TO] - conf[CONF_FROM] + 1,
                     conf[CONF_REVERSED],
