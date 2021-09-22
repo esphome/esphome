49bbca5ee39d88a22cf11d2227fcd03be32b364d
Otto winter <otto@otto-winter.com>
2021-09-22 19:47:17 +0200

    Switch to builtin RMT_CHANNEL_MAX and change logic for different max meaning

--- esphome/components/remote_base/remote_base.cpp
+++ esphome/components/remote_base/remote_base.cpp
@@ -6,12 +6,6 @@ namespace remote_base {
 
 static const char *const TAG = "remote_base";
 
-#ifdef ARDUINO_ARCH_ESP32
-#if SOC_RMT_CHANNELS_PER_GROUP > 4
-#define RMT_CHANNEL_MAX RMT_CHANNEL_7
-#else
-#define RMT_CHANNEL_MAX RMT_CHANNEL_3
-#endif
 RemoteRMTChannel::RemoteRMTChannel(uint8_t mem_block_num) : mem_block_num_(mem_block_num) {
   static rmt_channel_t next_rmt_channel = RMT_CHANNEL_0;
   this->channel_ = next_rmt_channel;
@@ -19,8 +13,8 @@ RemoteRMTChannel::RemoteRMTChannel(uint8_t mem_block_num) : mem_block_num_(mem_b
 }
 
 void RemoteRMTChannel::config_rmt(rmt_config_t &rmt) {
-  if (rmt_channel_t(int(this->channel_) + this->mem_block_num_) > RMT_CHANNEL_MAX) {
-    this->mem_block_num_ = int(RMT_CHANNEL_MAX) - int(this->channel_) + 1;
+  if (rmt_channel_t(int(this->channel_) + this->mem_block_num_) >= RMT_CHANNEL_MAX) {
+    this->mem_block_num_ = int(RMT_CHANNEL_MAX) - int(this->channel_);
     ESP_LOGW(TAG, "Not enough RMT memory blocks available, reduced to %i blocks.", this->mem_block_num_);
   }
   rmt.channel = this->channel_;
