a790504870c9b2d03d4468fc2a07869a27960cbc
micronen <micronen@gmail.com>
2021-09-24 00:24:29 +0300

    Update esphome/components/debug/debug_component.h
    Co-authored-by: Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>

--- esphome/components/debug/debug_component.h
+++ esphome/components/debug/debug_component.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/core/macros.h"
 #include "esphome/core/helpers.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/sensor/sensor.h"
