a5eb69ec3405f096fc66907ac8d47f27d707526d
Maurice Makaay <mmakaay1@xs4all.net>
2021-09-27 00:40:59 +0200

    Added test and implemented optional correctly.

--- esphome/components/wifi/__init__.py
+++ esphome/components/wifi/__init__.py
@@ -246,7 +246,7 @@ CONFIG_SCHEMA = cv.All(
             cv.SplitDefault(CONF_OUTPUT_POWER, esp8266=20.0): cv.All(
                 cv.decibel, cv.float_range(min=10.0, max=20.5)
             ),
-            cv.Optional(CONF_IGNORE_EFUSE_MAC_CRC, default=False): cv.All(
+            cv.Optional(CONF_IGNORE_EFUSE_MAC_CRC): cv.All(
                 cv.boolean, cv.only_on_esp32, cv.only_with_esp_idf
             ),
             cv.Optional("enable_mdns"): cv.invalid(
@@ -349,7 +349,7 @@ async def to_code(config):
 
     cg.add_define("USE_WIFI")
 
-    if config[CONF_IGNORE_EFUSE_MAC_CRC]:
+    if config.get(CONF_IGNORE_EFUSE_MAC_CRC, False):
         cg.add_define("USE_IGNORE_EFUSE_MAC_CRC")
 
     # Register at end for OTA safe mode
