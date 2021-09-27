6e2a11e7745743c840a455f6d25ef09af05e3fa9
Maurice Makaay <mmakaay1@xs4all.net>
2021-09-27 12:23:12 +0200

    Rename CONF_IGNORE_EFUSE_MAC_CRC to CONF_ESP32_IGNORE_EFUSE_MAC_CRC.

--- esphome/components/wifi/__init__.py
+++ esphome/components/wifi/__init__.py
@@ -30,7 +30,7 @@ from esphome.const import (
     CONF_KEY,
     CONF_USERNAME,
     CONF_EAP,
-    CONF_IGNORE_EFUSE_MAC_CRC,
+    CONF_ESP32_IGNORE_EFUSE_MAC_CRC,
 )
 from esphome.core import CORE, HexInt, coroutine_with_priority
 from esphome.components.network import IPAddress
@@ -247,7 +247,7 @@ CONFIG_SCHEMA = cv.All(
             cv.SplitDefault(CONF_OUTPUT_POWER, esp8266=20.0): cv.All(
                 cv.decibel, cv.float_range(min=10.0, max=20.5)
             ),
-            cv.Optional(CONF_IGNORE_EFUSE_MAC_CRC): cv.All(
+            cv.Optional(CONF_ESP32_IGNORE_EFUSE_MAC_CRC): cv.All(
                 cv.boolean, cv.only_on_esp32, cv.only_with_esp_idf
             ),
             cv.Optional("enable_mdns"): cv.invalid(
@@ -350,7 +350,7 @@ async def to_code(config):
 
     cg.add_define("USE_WIFI")
 
-    if config.get(CONF_IGNORE_EFUSE_MAC_CRC, False):
+    if config.get(CONF_ESP32_IGNORE_EFUSE_MAC_CRC, False):
         cg.add_define("USE_IGNORE_EFUSE_MAC_CRC")
         add_idf_sdkconfig_option("CONFIG_ESP32_PHY_CALIBRATION_AND_DATA_STORAGE", False)
 
