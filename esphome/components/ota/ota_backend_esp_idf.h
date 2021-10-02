932e0469f7a63c3a20326f1e911620744a9c7c11
Maurice Makaay <account+github@makaay.nl>
2021-10-02 16:02:01 +0200

    Fix ESP32 esp-idf OTA updates (#2424)
    * WIP on separating out the OTA backends.
    
    * Split off the individual OTA backends.
    
    * Cleanup the three backends, split into .h and .cpp.
    
    * After successfull flashing, activate the new boot partition.
    
    * Fix linting issues.
    
    * Minor cleanup
    
    Co-authored-by: Maurice Makaay <mmakaay1@xs4all.net>
    Co-authored-by: Otto winter <otto@otto-winter.com>

---
+++ esphome/components/ota/ota_backend_esp_idf.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "esphome/core/defines.h"
+#ifdef USE_ESP_IDF
+
+#include "ota_component.h"
+#include "ota_backend.h"
+#include <esp_ota_ops.h>
+
+namespace esphome {
+namespace ota {
+
+class IDFOTABackend : public OTABackend {
+ public:
+  OTAResponseTypes begin(size_t image_size) override;
+  void set_update_md5(const char *md5) override;
+  OTAResponseTypes write(uint8_t *data, size_t len) override;
+  OTAResponseTypes end() override;
+  void abort() override;
+
+ private:
+  esp_ota_handle_t update_handle_{0};
+  const esp_partition_t *partition_;
+};
+
+}  // namespace ota
+}  // namespace esphome
+#endif
