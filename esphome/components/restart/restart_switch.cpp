5e1219fc9471ecf0f2ed4d7b0d33369e7bad0f00
Paul Monigatti <pm@paul.pm>
2021-10-03 21:53:30 +1300

    Added safe mode option to restart_switch

--- esphome/components/restart/restart_switch.cpp
+++ esphome/components/restart/restart_switch.cpp
@@ -8,11 +8,24 @@ namespace restart {
 
 static const char *const TAG = "restart";
 
+const void RestartSwitch::set_ota(ota::OTAComponent *ota) {
+  this->ota_ = ota;
+}
+
+const void RestartSwitch::set_safe_mode(bool safe_mode) {
+  this->safe_mode_ = safe_mode;
+}
+
 void RestartSwitch::write_state(bool state) {
   // Acknowledge
   this->publish_state(false);
 
   if (state) {
+    // TODO: OTA Safe Mode
+    // if(this->ota_ != nullptr) {
+    //   this->ota_->set_safe_mode_pending(this->safe_mode_);
+    // }
+
     ESP_LOGI(TAG, "Restarting device...");
     // Let MQTT settle a bit
     delay(100);  // NOLINT
--- esphome/components/restart/restart_switch.h
+++ esphome/components/restart/restart_switch.h
@@ -2,6 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/switch/switch.h"
+#include "esphome/components/ota/ota_component.h"
 
 namespace esphome {
 namespace restart {
@@ -10,8 +11,16 @@ class RestartSwitch : public switch_::Switch, public Component {
  public:
   void dump_config() override;
 
+  /// Sets the OTA component to be configured when rebooting
+  const void set_ota(ota::OTAComponent *ota);
+
+  /// Sets whether the restart switch should enter safe mode on reboot
+  const void set_safe_mode(bool safe_mode);
+
  protected:
   void write_state(bool state) override;
+  ota::OTAComponent *ota_{nullptr};
+  bool safe_mode_{false};
 };
 
 }  // namespace restart
