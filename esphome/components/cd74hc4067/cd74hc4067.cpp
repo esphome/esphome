7f4c15bd9d688fd53fc5f52cad8291ba0f24a17b
asoehlke <git@soehlke.de>
2021-10-03 15:45:00 +0200

    replace bitRead

--- esphome/components/cd74hc4067/cd74hc4067.cpp
+++ esphome/components/cd74hc4067/cd74hc4067.cpp
@@ -35,10 +35,28 @@ void CD74HC4067Component::activate_pin(uint8_t pin) {
   if (active_pin_ != pin) {
     ESP_LOGD(TAG, "switch to input %d", pin);
 
-    this->pin_s0_->digital_write(HIGH && bitRead(pin, 0));
-    this->pin_s1_->digital_write(HIGH && bitRead(pin, 1));
-    this->pin_s2_->digital_write(HIGH && bitRead(pin, 2));
-    this->pin_s3_->digital_write(HIGH && bitRead(pin, 3));
+    static int mux_channel[16][4] = {
+        {0, 0, 0, 0},  // channel 0
+        {1, 0, 0, 0},  // channel 1
+        {0, 1, 0, 0},  // channel 2
+        {1, 1, 0, 0},  // channel 3
+        {0, 0, 1, 0},  // channel 4
+        {1, 0, 1, 0},  // channel 5
+        {0, 1, 1, 0},  // channel 6
+        {1, 1, 1, 0},  // channel 7
+        {0, 0, 0, 1},  // channel 8
+        {1, 0, 0, 1},  // channel 9
+        {0, 1, 0, 1},  // channel 10
+        {1, 1, 0, 1},  // channel 11
+        {0, 0, 1, 1},  // channel 12
+        {1, 0, 1, 1},  // channel 13
+        {0, 1, 1, 1},  // channel 14
+        {1, 1, 1, 1}   // channel 15
+    };
+    this->pin_s0_->digital_write(mux_channel[pin][0]);
+    this->pin_s1_->digital_write(mux_channel[pin][1]);
+    this->pin_s2_->digital_write(mux_channel[pin][2]);
+    this->pin_s3_->digital_write(mux_channel[pin][3]);
     // small delay is needed to let the multiplexer switch
     delay(switch_delay_);
     active_pin_ = pin;
