6414aa9ba54de75836b3a04a6f6566312fa12a25
Martin <25747549+martgras@users.noreply.github.com>
2021-10-07 19:57:02 +0200

    Fix i2c byte order

--- esphome/components/i2c/i2c.cpp
+++ esphome/components/i2c/i2c.cpp
@@ -12,7 +12,7 @@ bool I2CDevice::write_bytes_16(uint8_t a_register, const uint16_t *data, uint8_t
   std::unique_ptr<uint16_t[]> temp{new uint16_t[len]};
   for (size_t i = 0; i < len; i++)
     temp[i] = htoi2cs(data[i]);
-  return write_register(a_register, reinterpret_cast<const uint8_t *>(data), len * 2) == ERROR_OK;
+  return write_register(a_register, reinterpret_cast<const uint8_t *>(temp.get(), len * 2) == ERROR_OK;
 }
 
 I2CRegister &I2CRegister::operator=(uint8_t value) {
