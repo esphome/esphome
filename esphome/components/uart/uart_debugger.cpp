94cd296a80113ca12fcb823d3f338038bea6233c
Maurice Makaay <account-github@makaay.nl>
2021-10-12 01:18:21 +0200

    Fix some bad tabs that my editor threw in.

--- esphome/components/uart/uart_debugger.cpp
+++ esphome/components/uart/uart_debugger.cpp
@@ -131,27 +131,27 @@ void UARTDebug::log_string(UARTDirection direction, std::vector<uint8_t> bytes) 
   for (size_t i = 0; i < len; i++) {
     if (bytes[i] == 7) {
       res += "\\a";
-	 	} else if (bytes[i] == 8) {
-			res += "\\b";
-	 	} else if (bytes[i] == 9) {
-			res += "\\t";
-	 	} else if (bytes[i] == 10) {
-			res += "\\n";
-	 	} else if (bytes[i] == 11) {
-			res += "\\v";
-	 	} else if (bytes[i] == 12) {
-			res += "\\f";
-	 	} else if (bytes[i] == 13) {
-			res += "\\r";
-	 	} else if (bytes[i] == 27) {
-			res += "\\e";
-	 	} else if (bytes[i] == 34) {
-			res += "\\\"";
-	 	} else if (bytes[i] == 39) {
-			res += "\\'";
-	 	} else if (bytes[i] == 92) {
-			res += "\\\\";
-	 	} else if (bytes[i] < 32 || bytes[i] > 127) {
+    } else if (bytes[i] == 8) {
+      res += "\\b";
+    } else if (bytes[i] == 9) {
+      res += "\\t";
+    } else if (bytes[i] == 10) {
+      res += "\\n";
+    } else if (bytes[i] == 11) {
+      res += "\\v";
+    } else if (bytes[i] == 12) {
+      res += "\\f";
+    } else if (bytes[i] == 13) {
+      res += "\\r";
+    } else if (bytes[i] == 27) {
+      res += "\\e";
+    } else if (bytes[i] == 34) {
+      res += "\\\"";
+    } else if (bytes[i] == 39) {
+      res += "\\'";
+    } else if (bytes[i] == 92) {
+      res += "\\\\";
+    } else if (bytes[i] < 32 || bytes[i] > 127) {
       sprintf(hexbuf, "\\x%02X", bytes[i]);
       res += hexbuf;
     } else {
@@ -166,14 +166,14 @@ void UARTDebug::log_int(UARTDirection direction, std::vector<uint8_t> bytes, uin
   std::string res;
   size_t len = bytes.size();
   if (direction == UART_DIRECTION_RX) {
-	 	res += "<<< ";
- 	} else {
-	 	res += ">>> ";
- 	}
+    res += "<<< ";
+  } else {
+    res += ">>> ";
+  }
   for (size_t i = 0; i < len; i++) {
     if (i > 0) {
-		 	res += separator;
-	 	}
+      res += separator;
+    }
     res += to_string(bytes[i]);
   }
   ESP_LOGD(TAG, "%s", res.c_str());
