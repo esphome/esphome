4d438b6f197265c2b1dd3957ad98e1cba415af1f
Maurice Makaay <account-github@makaay.nl>
2021-10-14 01:07:00 +0200

    Added public: qualifier (explicit > implicit).

--- esphome/components/ota/ota_backend_arduino_esp32.h
+++ esphome/components/ota/ota_backend_arduino_esp32.h
@@ -9,6 +9,7 @@ namespace esphome {
 namespace ota {
 
 class ArduinoESP32OTABackend : public OTABackend {
+ public:
   OTAResponseTypes begin(size_t image_size) override;
   void set_update_md5(const char *md5) override;
   OTAResponseTypes write(uint8_t *data, size_t len) override;
