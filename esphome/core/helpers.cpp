4883d81c4d9e69e197f28856bd71ff6f6ec3c821
Carlos Garcia Saura <carlosgs@users.noreply.github.com>
2021-10-16 09:42:01 +0200

    Renamed internal function to match clang style

--- esphome/core/helpers.cpp
+++ esphome/core/helpers.cpp
@@ -203,7 +203,7 @@ uint8_t crc8(uint8_t *data, uint8_t len) {
   return crc;
 }
 
-void delayMicroseconds_safe(uint32_t us) {  // prevents CPU locks that could trigger WDT or affect WiFi/BT stability
+void delay_microseconds_safe(uint32_t us) {  // avoids CPU locks that could trigger WDT or affect WiFi/BT stability
   auto start = micros();
   const uint32_t lag = 5000;  // microseconds, specifies the maximum time for a CPU busy-loop.
                               // it must be larger than the worst-case duration of a delay(0) call (hardware tasks)
--- esphome/core/helpers.h
+++ esphome/core/helpers.h
@@ -255,7 +255,7 @@ struct is_callable  // NOLINT
   static constexpr auto value = decltype(test<T>(nullptr))::value;  // NOLINT
 };
 
-void delayMicroseconds_safe(uint32_t us);  // NOLINT(readability-identifier-naming)
+void delay_microseconds_safe(uint32_t us);
 
 template<typename T> class Deduplicator {
  public:
