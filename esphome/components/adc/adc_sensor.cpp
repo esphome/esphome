f8a7afc5c07b3c183d5abc39a7341c477c78f357
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2021-10-17 22:12:30 +0200

    Update adc_sensor.cpp

--- esphome/components/adc/adc_sensor.cpp
+++ esphome/components/adc/adc_sensor.cpp
@@ -16,8 +16,10 @@ namespace adc {
 static const char *const TAG = "adc";
 
 #ifdef USE_ESP32
-void ADCSensor::set_attenuation(adc_atten_t attenuation) { this->attenuation_ = attenuation; }
-
+void ADCSensor::set_attenuation(adc_atten_t attenuation) {
+  this->attenuation_ = attenuation;
+  adc1_config_channel_atten(gpio_to_adc1(pin_->get_pin()), attenuation_);
+}
 inline adc1_channel_t gpio_to_adc1(uint8_t pin) {
 #if CONFIG_IDF_TARGET_ESP32
   switch (pin) {
@@ -66,6 +68,8 @@ void ADCSensor::setup() {
 #endif
 
 #ifdef USE_ESP32
+  if (auto_range_)
+    this->attenuation_ = ADC_ATTEN_DB_11;
   adc1_config_channel_atten(gpio_to_adc1(pin_->get_pin()), attenuation_);
   adc1_config_width(ADC_WIDTH_BIT_12);
 #if !CONFIG_IDF_TARGET_ESP32C3 && !CONFIG_IDF_TARGET_ESP32H2
@@ -84,22 +88,25 @@ void ADCSensor::dump_config() {
 #endif
 #ifdef USE_ESP32
   LOG_PIN("  Pin: ", pin_);
-  switch (this->attenuation_) {
-    case ADC_ATTEN_DB_0:
-      ESP_LOGCONFIG(TAG, " Attenuation: 0db (max 1.1V)");
-      break;
-    case ADC_ATTEN_DB_2_5:
-      ESP_LOGCONFIG(TAG, " Attenuation: 2.5db (max 1.5V)");
-      break;
-    case ADC_ATTEN_DB_6:
-      ESP_LOGCONFIG(TAG, " Attenuation: 6db (max 2.2V)");
-      break;
-    case ADC_ATTEN_DB_11:
-      ESP_LOGCONFIG(TAG, " Attenuation: 11db (max 3.9V)");
-      break;
-    default:  // This is to satisfy the unused ADC_ATTEN_MAX
-      break;
-  }
+  if (auto_range_) 
+    ESP_LOGCONFIG(TAG, " Attenuation: auto-range");
+  else
+    switch (this->attenuation_) {
+      case ADC_ATTEN_DB_0:
+        ESP_LOGCONFIG(TAG, " Attenuation: 0db (max 1.1V)");
+        break;
+      case ADC_ATTEN_DB_2_5:
+        ESP_LOGCONFIG(TAG, " Attenuation: 2.5db (max 1.5V)");
+        break;
+      case ADC_ATTEN_DB_6:
+        ESP_LOGCONFIG(TAG, " Attenuation: 6db (max 2.2V)");
+        break;
+      case ADC_ATTEN_DB_11:
+        ESP_LOGCONFIG(TAG, " Attenuation: 11db (max 3.9V)");
+        break;
+      default:  // This is to satisfy the unused ADC_ATTEN_MAX
+        break;
+    }
 #endif
   LOG_UPDATE_INTERVAL(this);
 }
@@ -109,10 +116,25 @@ void ADCSensor::update() {
   ESP_LOGD(TAG, "'%s': Got voltage=%.2fV", this->get_name().c_str(), value_v);
   this->publish_state(value_v);
 }
-float ADCSensor::sample() {
+int ADCSensor::read_raw() {
+  int raw = 0;
+  #ifdef USE_ESP32
+    raw = adc1_get_raw(gpio_to_adc1(pin_->get_pin()));
+  #endif
+  
+  #ifdef USE_ESP8266
+  #ifdef USE_ADC_SENSOR_VCC
+    raw = ESP.getVcc();  // NOLINT(readability-static-accessed-through-instance)
+  #else
+    raw = analogRead(this->pin_->get_pin());  // NOLINT
+  #endif
+  #endif
+  return raw;
+}
+float ADCSensor::raw_to_voltage(int raw) {
+  float value_v = 0;
 #ifdef USE_ESP32
-  int raw = adc1_get_raw(gpio_to_adc1(pin_->get_pin()));
-  float value_v = raw / 4095.0f;
+  value_v = raw / 4095.0f;
 #if CONFIG_IDF_TARGET_ESP32
   switch (this->attenuation_) {
     case ADC_ATTEN_DB_0:
@@ -147,17 +169,54 @@ float ADCSensor::sample() {
     default:  // This is to satisfy the unused ADC_ATTEN_MAX
       break;
   }
-#endif
-  return value_v;
-#endif
+# endif
+# endif
 
 #ifdef USE_ESP8266
-#ifdef USE_ADC_SENSOR_VCC
-  return ESP.getVcc() / 1024.0f;  // NOLINT(readability-static-accessed-through-instance)
-#else
-  return analogRead(this->pin_->get_pin()) / 1024.0f;  // NOLINT
+  value_v = raw / 1024.0f;
 #endif
+  return value_v;
+}
+float ADCSensor::sample() {
+  int raw = this->read_raw();
+  float value_v = this->raw_to_voltage(raw);
+#ifdef USE_ESP32
+  if (auto_range_) {
+    float v_acc = 0;   // Accumulator for valid ADC samples (>0 && <4095)
+    int nsamples = 0;  // TO-DO: Work only with the linear range limits?
+    if (raw < 4095) {
+      if (raw > 0) {
+        v_acc += value_v;
+        nsamples++;
+      }
+      this->set_attenuation(ADC_ATTEN_DB_6);
+      raw = this->read_raw();
+      if (raw < 4095) {
+        if (raw > 0) {
+          v_acc += this->raw_to_voltage(raw);
+          nsamples++;
+        }
+        this->set_attenuation(ADC_ATTEN_DB_2_5);
+        raw = this->read_raw();
+        if (raw < 4095) {
+          if (raw > 0) {
+            v_acc += this->raw_to_voltage(raw);
+            nsamples++;
+          }
+          this->set_attenuation(ADC_ATTEN_DB_0);
+          raw = this->read_raw();
+          if (raw < 4095) {
+            v_acc += this->raw_to_voltage(raw);
+            nsamples++;
+          }
+        }
+      }
+      value_v = v_acc / (float)nsamples;  // Average the valid samples
+    }
+    this->set_attenuation(ADC_ATTEN_DB_11);
+  }
 #endif
+  return value_v;
 }
 #ifdef USE_ESP8266
 std::string ADCSensor::unique_id() { return get_mac_address() + "-adc"; }
