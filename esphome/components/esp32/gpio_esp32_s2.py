8a4c4c83a25755eaed5134e73d2690ca26604fcd
Martin <25747549+martgras@users.noreply.github.com>
2021-10-22 13:51:36 +0200

    fix gpio validation for esp32 variants

---
+++ esphome/components/esp32/gpio_esp32_s2.py
@@ -0,0 +1,58 @@
+import logging
+
+from esphome.const import (
+    CONF_INPUT,
+    CONF_MODE,
+    CONF_NUMBER,
+    CONF_OPEN_DRAIN,
+    CONF_OUTPUT,
+    CONF_PULLDOWN,
+    CONF_PULLUP,
+)
+
+import esphome.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def esp32_s2_validate_gpio_pin(value):
+    if value < 0 or value > 46:
+        raise cv.Invalid(f"Invalid pin number: {value} (must be 0-46)")
+    return value
+
+
+def esp32_s2_validate_supports(value):
+
+    num = value[CONF_NUMBER]
+    mode = value[CONF_MODE]
+    is_input = mode[CONF_INPUT]
+    is_output = mode[CONF_OUTPUT]
+    is_open_drain = mode[CONF_OPEN_DRAIN]
+    is_pullup = mode[CONF_PULLUP]
+    is_pulldown = mode[CONF_PULLDOWN]
+
+    if num < 0 or num > 46:
+        raise cv.Invalid(f"Invalid pin number: {value} (must be 0-46)")
+
+    if is_input:
+        # All ESP32 pins support input mode
+        pass
+    if is_output and num == 46:
+        raise cv.Invalid(
+            f"GPIO{num} does not support output pin mode.",
+            [CONF_MODE, CONF_OUTPUT],
+        )
+    if is_open_drain and not is_output:
+        raise cv.Invalid(
+            "Open-drain only works with output mode", [CONF_MODE, CONF_OPEN_DRAIN]
+        )
+    if is_pullup and num == 46:
+        raise cv.Invalid(
+            f"GPIO{num} does not support pullups.", [CONF_MODE, CONF_PULLUP]
+        )
+    if is_pulldown and num == 46:
+        raise cv.Invalid(
+            f"GPIO{num} does not support pulldowns.", [CONF_MODE, CONF_PULLDOWN]
+        )
+
+    return value
