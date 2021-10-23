a687b083aed516cbfe93ed25dc51a31837f95fb0
0hax <43876620+0hax@users.noreply.github.com>
2021-10-23 19:01:23 +0200

    Teleinfo ptec (#2599)
    * teleinfo: handle historical mode correctly.
    
    In historical mode, tags like PTEC leads to an issue where we detect a
    timestamp wheras this is not possible in historical mode.
    
    PTEC teleinfo tag looks like:
        PTEC HP..
    Instead of the usual format
        IINST1 001 I
    
    This make our data parsing fails.
    
    While at here, make sure we continue parsing other tags even if parsing
    one of the tag fails.
    
    Signed-off-by: 0hax <0hax@protonmail.com>
    
    * teleinfo: fix compilation with loglevel set to debug.
    
    Signed-off-by: 0hax <0hax@protonmail.com>

--- esphome/components/teleinfo/sensor/teleinfo_sensor.cpp
+++ esphome/components/teleinfo/sensor/teleinfo_sensor.cpp
@@ -9,6 +9,6 @@ void TeleInfoSensor::publish_val(const std::string &val) {
   auto newval = parse_float(val);
   publish_state(*newval);
 }
-void TeleInfoSensor::dump_config() { LOG_SENSOR("  ", tag.c_str(), this); }
+void TeleInfoSensor::dump_config() { LOG_SENSOR("  ", "Teleinfo Sensor", this); }
 }  // namespace teleinfo
 }  // namespace esphome
