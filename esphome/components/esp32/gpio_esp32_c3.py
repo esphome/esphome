368eb88babfd7bc9dd65ece20146f9e9d3e754c0
Martin <25747549+martgras@users.noreply.github.com>
2021-10-23 20:55:02 +0200

    fix esp32-c3 pins

--- esphome/components/esp32/gpio_esp32_c3.py
+++ esphome/components/esp32/gpio_esp32_c3.py
@@ -1,3 +1,5 @@
+import logging
+
 from esphome.const import (
     CONF_INPUT,
     CONF_MODE,
@@ -7,10 +9,30 @@ from esphome.const import (
 )
 import esphome.config_validation as cv
 
+_ESP_32_C3_SPI_PSRAM_PINS = {
+    12: "SPIHD",
+    13: "SPIWP",
+    14: "SPICS0",
+    15: "SPICLK",
+    16: "SPID",
+    17: "SPIQ",
+}
+
+_ESP_32_C3_STRAPPING_PINS = {2, 8, 9}
+
+_LOGGER = logging.getLogger(__name__)
+
 
 def esp32_c3_validate_gpio_pin(value):
     if value < 0 or value > 21:
         raise cv.Invalid(f"Invalid pin number: {value} (must be 0-21)")
+    if value in _ESP_32_C3_SPI_PSRAM_PINS:
+        raise cv.Invalid(
+            f"This pin cannot be used on ESP32-C3s and is already used by the SPI/PSRAM interface (function: {_ESP_32_C3_SPI_PSRAM_PINS[value]})"
+        )
+    if value in _ESP_32_C3_STRAPPING_PINS:
+        _LOGGER.warning("GPIO%d is a Strapping PIN and should be avoided", value)
+
     return value
 
 
