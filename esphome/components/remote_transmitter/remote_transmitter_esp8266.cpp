5a279f417b9d9b25884fab98b5b2123be8e949a7
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2021-10-25 11:26:06 +0200

    Update remote_transmitter_esp8266.cpp

--- esphome/components/remote_transmitter/remote_transmitter_esp8266.cpp
+++ esphome/components/remote_transmitter/remote_transmitter_esp8266.cpp
@@ -34,16 +34,18 @@ void RemoteTransmitterComponent::calculate_on_off_time_(uint32_t carrier_frequen
 }
 
 void RemoteTransmitterComponent::aligned_delay_microseconds_(uint32_t usec) {
-  const uint32_t elapsed = micros() - this->ref_time_;
-  if (usec > elapsed)
-    delayMicroseconds(usec - elapsed);  // aligns delay to "ref_time_"
-  this->ref_time_ += usec;
+  const uint32_t current_time = micros();
+  if (this->ref_time_ == 0)  // initialization
+    this->ref_time_ = current_time;
+  if (ref_time > current_time)
+    delayMicroseconds(ref_time - current_time);  // align delay to "ref_time_"
+  this->ref_time_ += usec;                       // program next delay
 }
 
 void RemoteTransmitterComponent::mark_(uint32_t on_time, uint32_t off_time, uint32_t usec) {
   if (this->carrier_duty_percent_ == 100 || (on_time == 0 && off_time == 0)) {
-    this->pin_->digital_write(true);
     this->aligned_delay_microseconds_(usec);
+    this->pin_->digital_write(true);
     return;
   }
 
@@ -52,13 +54,13 @@ void RemoteTransmitterComponent::mark_(uint32_t on_time, uint32_t off_time, uint
 
   while (current_time - start_time < usec) {  // modulate with carrier frequency
     const uint32_t elapsed = current_time - start_time;
-    this->pin_->digital_write(true);
     this->aligned_delay_microseconds_(std::min(on_time, usec - elapsed));
+    this->pin_->digital_write(true);
     if (elapsed + on_time >= usec)
       break;
 
-    this->pin_->digital_write(false);
     this->aligned_delay_microseconds_(std::min(usec - elapsed - on_time, off_time));
+    this->pin_->digital_write(false);
 
     current_time = micros();
   }
@@ -66,31 +68,28 @@ void RemoteTransmitterComponent::mark_(uint32_t on_time, uint32_t off_time, uint
 }
 
 void RemoteTransmitterComponent::space_(uint32_t usec) {
-  this->pin_->digital_write(false);
   this->aligned_delay_microseconds_(usec);
+  this->pin_->digital_write(false);
 }
 
 void RemoteTransmitterComponent::send_internal(uint32_t send_times, uint32_t send_wait) {
   ESP_LOGD(TAG, "Sending remote code...");
   uint32_t on_time, off_time;
   this->calculate_on_off_time_(this->temp_.get_carrier_frequency(), &on_time, &off_time);
+  this->ref_time_ = 0;  // "ref_time_" will be initialized at first aligned_delay_microseconds_() call
   for (uint32_t i = 0; i < send_times; i++) {
-    {
-      InterruptLock lock;
-      App.feed_wdt();
-      this->ref_time_ = micros();  // "ref_time_" is the timing reference for each pulse train
-      for (int32_t item : this->temp_.get_data()) {
-        if (item > 0) {
-          const auto length = uint32_t(item);
-          this->mark_(on_time, off_time, length);
-        } else {
-          const auto length = uint32_t(-item);
-          this->space_(length);
-        }
-        App.feed_wdt();
+    for (int32_t item : this->temp_.get_data()) {
+      if (item > 0) {
+        const auto length = uint32_t(item);
+        this->mark_(on_time, off_time, length);
+      } else {
+        const auto length = uint32_t(-item);
+        this->space_(length);
       }
-      this->pin_->digital_write(false);
+      App.feed_wdt();
     }
+    this->aligned_delay_microseconds_(0);  // perform the remaining delay
+    this->pin_->digital_write(false);
 
     if (i + 1 < send_times)
       this->aligned_delay_microseconds_(send_wait);
