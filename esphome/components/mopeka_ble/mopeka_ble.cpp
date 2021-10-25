67074d8c8345e93c89e9fd2cd61e03cc1c959d1a
Sean Brogan <spbrogan@live.com>
2021-10-24 18:17:27 -0700

    i really need to get the same clang format working locally

--- esphome/components/mopeka_ble/mopeka_ble.cpp
+++ esphome/components/mopeka_ble/mopeka_ble.cpp
@@ -44,7 +44,6 @@ bool MopekaListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device) 
 
 bool MopekaListener::parse_sync_button_(const std::vector<uint8_t> &message) { return (message[2] & 0x80) != 0; }
 
-
 }  // namespace mopeka_ble
 }  // namespace esphome
 
