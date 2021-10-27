8ffa21878a1eb6cb88ff275c962b520885e72595
Paul Nicholls <paul.n@next.gen.nz>
2021-10-27 17:54:50 +1300

    Tuya Cover: Add support for more features
    Adds support for:
    - Restore mode
    - Control datapoint (for open/close/stop commands)
    - Position report datapoint (if the device reports position and accepts position commands on separate datapoints)
    - Setting device direction-of-travel if the `invert_position` option is enabled and a `direction_datapoint` is configured
    
    Note that the `invert_position` option is no longer used to determine whether to invert the values, as it appears that the Tuya cover position is the inverse of the HA / ESPHome position (percentage open vs percentage closed).

--- esphome/components/tuya/cover/tuya_cover.cpp
+++ esphome/components/tuya/cover/tuya_cover.cpp
@@ -4,16 +4,36 @@
 namespace esphome {
 namespace tuya {
 
+const uint8_t COMMAND_OPEN = 0x00;
+const uint8_t COMMAND_CLOSE = 0x02;
+const uint8_t COMMAND_STOP = 0x01;
+
+using namespace esphome::cover;
+
 static const char *const TAG = "tuya.cover";
 
 void TuyaCover::setup() {
   this->value_range_ = this->max_value_ - this->min_value_;
-  if (this->position_id_.has_value()) {
-    this->parent_->register_listener(*this->position_id_, [this](const TuyaDatapoint &datapoint) {
+
+  uint8_t report_id = 0;
+  bool has_report_id;
+  if (this->position_report_id_.has_value()) {
+    report_id = *this->position_report_id_;
+    has_report_id = true;
+  } else if (this->position_id_.has_value()) {
+    // Fall back to position datapoint if no position report datapoint is set.
+    report_id = *this->position_id_;
+    has_report_id = true;
+  }
+
+  if (has_report_id) {
+    this->parent_->register_listener(report_id, [this](const TuyaDatapoint &datapoint) {
+      if (datapoint.value_int == 123) {
+        ESP_LOGD(TAG, "Ignoring MCU position report - not calibrated");
+        return;
+      }
       auto pos = float(datapoint.value_uint - this->min_value_) / this->value_range_;
-      if (this->invert_position_)
-        pos = 1.0f - pos;
-      this->position = pos;
+      this->position = 1.0f - pos;
       this->publish_state();
     });
   }
@@ -21,31 +41,99 @@ void TuyaCover::setup() {
 
 void TuyaCover::control(const cover::CoverCall &call) {
   if (call.get_stop()) {
-    auto pos = this->position;
-    if (this->invert_position_)
+    if (this->control_id_.has_value()) {
+      this->parent_->force_set_enum_datapoint_value(*this->control_id_, COMMAND_STOP);
+    } else {
+      auto pos = this->position;
       pos = 1.0f - pos;
-    auto position_int = static_cast<uint32_t>(pos * this->value_range_);
-    position_int = position_int + this->min_value_;
+      auto position_int = static_cast<uint32_t>(pos * this->value_range_);
+      position_int = position_int + this->min_value_;
 
-    parent_->set_integer_datapoint_value(*this->position_id_, position_int);
+      parent_->set_integer_datapoint_value(*this->position_id_, position_int);
+    }
   }
   if (call.get_position().has_value()) {
     auto pos = *call.get_position();
-    if (this->invert_position_)
+    if (this->control_id_.has_value() && (pos == COVER_OPEN || pos == COVER_CLOSED)) {
+      if (pos == COVER_OPEN) {
+        this->parent_->force_set_enum_datapoint_value(*this->control_id_, COMMAND_OPEN);
+      } else {
+        this->parent_->force_set_enum_datapoint_value(*this->control_id_, COMMAND_CLOSE);
+      }
+    } else {
       pos = 1.0f - pos;
-    auto position_int = static_cast<uint32_t>(pos * this->value_range_);
-    position_int = position_int + this->min_value_;
+      auto position_int = static_cast<uint32_t>(pos * this->value_range_);
+      position_int = position_int + this->min_value_;
 
-    parent_->set_integer_datapoint_value(*this->position_id_, position_int);
+      parent_->set_integer_datapoint_value(*this->position_id_, position_int);
+    }
   }
 
   this->publish_state();
 }
 
+void TuyaCover::loop() {
+  // Wait until the Tuya component reports state INIT_DONE before attempting
+  // to set the direction of travel or restore the state (if applicable), as
+  // these won't work if the Tuya MCU isn't ready yet.
+  if (!this->init_done_ && this->parent_->get_init_state() == TuyaInitState::INIT_DONE) {
+    this->init_done_ = true;
+
+    // Set the direction (if configured/supported).
+    this->set_direction(this->invert_position_);
+
+    // Handle configured restore mode.
+    switch (this->restore_mode_) {
+      case COVER_NO_RESTORE:
+        break;
+      case COVER_RESTORE: {
+        auto restore = this->restore_state_();
+        if (restore.has_value())
+          restore->apply(this);
+        break;
+      }
+      case COVER_RESTORE_AND_CALL: {
+        auto restore = this->restore_state_();
+        if (restore.has_value()) {
+          restore->to_call(this).perform();
+        }
+        break;
+      }
+    }
+  }
+}
+
+void TuyaCover::set_direction(bool inverted) {
+  if (!this->direction_id_.has_value()) {
+    return;
+  }
+
+  if (inverted) {
+    ESP_LOGD(TAG, "Setting direction: inverted");
+  } else {
+    ESP_LOGD(TAG, "Setting direction: normal");
+  }
+
+  this->parent_->set_boolean_datapoint_value(*this->direction_id_, inverted);
+}
+
 void TuyaCover::dump_config() {
   ESP_LOGCONFIG(TAG, "Tuya Cover:");
+  if (this->invert_position_) {
+    if (this->direction_id_.has_value()) {
+      ESP_LOGCONFIG(TAG, "   Inverted");
+    } else {
+      ESP_LOGCONFIG(TAG, "   Configured as Inverted, but direction_datapoint isn't configured");
+    }
+  }
+  if (this->control_id_.has_value())
+    ESP_LOGCONFIG(TAG, "   Control has datapoint ID %u", *this->control_id_);
+  if (this->direction_id_.has_value())
+    ESP_LOGCONFIG(TAG, "   Direction has datapoint ID %u", *this->direction_id_);
   if (this->position_id_.has_value())
     ESP_LOGCONFIG(TAG, "   Position has datapoint ID %u", *this->position_id_);
+  if (this->position_report_id_.has_value())
+    ESP_LOGCONFIG(TAG, "   Position Report has datapoint ID %u", *this->position_report_id_);
 }
 
 cover::CoverTraits TuyaCover::get_traits() {
