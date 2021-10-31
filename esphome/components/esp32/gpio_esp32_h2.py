ceade9ea2a8c50eae784589b1c1fd9fdb7216eb5
Martin <25747549+martgras@users.noreply.github.com>
2021-10-31 11:08:55 +0100

    Update esphome/components/esp32/gpio_esp32_h2.py
    Co-authored-by: Otto Winter <otto@otto-winter.com>

--- esphome/components/esp32/gpio_esp32_h2.py
+++ esphome/components/esp32/gpio_esp32_h2.py
@@ -7,11 +7,7 @@ import esphome.config_validation as cv
 
 def esp32_h2_validate_gpio_pin(value):
     # ESP32-H2 not yet supported
-    if value > 0:
-        raise cv.Invalid("ESP32-H2 isn't supported yet")
-    if value < 0 or value > 21:
-        raise cv.Invalid(f"Invalid pin number: {value} (must be 0-21)")
-    return value
+    raise cv.Invalid("ESP32-H2 isn't supported yet")
 
 
 def esp32_h2_validate_supports(value):
