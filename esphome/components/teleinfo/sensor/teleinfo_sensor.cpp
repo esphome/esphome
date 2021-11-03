acb32f3d65722f1b3bca7baec0521ca23a48be98
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2021-11-03 22:38:17 +0100

    Fix broken conversion to parse_number()

--- esphome/components/teleinfo/sensor/teleinfo_sensor.cpp
+++ esphome/components/teleinfo/sensor/teleinfo_sensor.cpp
@@ -6,7 +6,7 @@ namespace teleinfo {
 static const char *const TAG = "teleinfo_sensor";
 TeleInfoSensor::TeleInfoSensor(const char *tag) { this->tag = std::string(tag); }
 void TeleInfoSensor::publish_val(const std::string &val) {
-  auto newval = parse_number<float>(val);
+  auto newval = parse_number<float>(val).value_or(0.0f);
   publish_state(*newval);
 }
 void TeleInfoSensor::dump_config() { LOG_SENSOR("  ", "Teleinfo Sensor", this); }
