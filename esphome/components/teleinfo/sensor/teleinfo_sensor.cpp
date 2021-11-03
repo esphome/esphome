1bd1db600fcda621bb031a97006b35000a6065d3
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2021-11-03 22:03:07 +0100

    Convert parse_float() and parse_int() uses to parse_number()

--- esphome/components/teleinfo/sensor/teleinfo_sensor.cpp
+++ esphome/components/teleinfo/sensor/teleinfo_sensor.cpp
@@ -6,7 +6,7 @@ namespace teleinfo {
 static const char *const TAG = "teleinfo_sensor";
 TeleInfoSensor::TeleInfoSensor(const char *tag) { this->tag = std::string(tag); }
 void TeleInfoSensor::publish_val(const std::string &val) {
-  auto newval = parse_float(val);
+  auto newval = parse_number<float>(val);
   publish_state(*newval);
 }
 void TeleInfoSensor::dump_config() { LOG_SENSOR("  ", "Teleinfo Sensor", this); }
