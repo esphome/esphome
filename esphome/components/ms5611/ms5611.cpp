02cc84f1f7df5e8700c0a34386d56a8ae050d301
anatoly-savchenkov <anatoly.savchenkov@gmail.com>
2021-11-05 14:50:40 +0300

    [ms5611] Lowercase vars to meet the coding style

--- esphome/components/ms5611/ms5611.cpp
+++ esphome/components/ms5611/ms5611.cpp
@@ -79,44 +79,44 @@ void MS5611Component::read_pressure_(uint32_t raw_temperature) {
 // Calculations are taken from the datasheet which can be found here:
 // https://www.te.com/commerce/DocumentDelivery/DDEController?Action=showdoc&DocId=Data+Sheet%7FMS5611-01BA03%7FB3%7Fpdf%7FEnglish%7FENG_DS_MS5611-01BA03_B3.pdf%7FCAT-BLPS0036
 // Sections PRESSURE AND TEMPERATURE CALCULATION and SECOND ORDER TEMPERATURE COMPENSATION
-// Variable names below match variable names from the datasheet
+// Variable names below match variable names from the datasheet but lowercased
 void MS5611Component::calculate_values_(uint32_t raw_temperature, uint32_t raw_pressure) {
-  const uint32_t C1 = uint32_t(this->prom_[0]);
-  const uint32_t C2 = uint32_t(this->prom_[1]);
-  const uint16_t C3 = uint16_t(this->prom_[2]);
-  const uint16_t C4 = uint16_t(this->prom_[3]);
-  const int32_t  C5 = int32_t (this->prom_[4]);
-  const uint16_t C6 = uint16_t(this->prom_[5]);
-  const uint32_t D1 = raw_pressure;
-  const int32_t  D2 = raw_temperature;
+  const uint32_t c1 = uint32_t(this->prom_[0]);
+  const uint32_t c2 = uint32_t(this->prom_[1]);
+  const uint16_t c3 = uint16_t(this->prom_[2]);
+  const uint16_t c4 = uint16_t(this->prom_[3]);
+  const int32_t  c5 = int32_t (this->prom_[4]);
+  const uint16_t c6 = uint16_t(this->prom_[5]);
+  const uint32_t d1 = raw_pressure;
+  const int32_t  d2 = raw_temperature;
 
-  // Promote dT to 64 bit here to make the math below cleaner
-  const int64_t dT = D2 - (C5 << 8);
-  int32_t TEMP = (2000 + ((dT * C6) >> 23));
-  
-  int64_t OFF = (C2 << 16) + ((dT * C4) >> 7);
-  int64_t SENS = (C1 << 15) + ((dT * C3) >> 8);
+  // Promote dt to 64 bit here to make the math below cleaner
+  const int64_t dt = d2 - (c5 << 8);
+  int32_t temp = (2000 + ((dt * c6) >> 23));
 
-  if (TEMP < 2000) {
-    const int32_t T2 = (dT * dT) >> 31;
-    int32_t OFF2 = ((5 * (TEMP - 2000) * (TEMP - 2000)) >> 1);
-    int32_t SENS2 = ((5 * (TEMP - 2000) * (TEMP - 2000)) >> 2);
-    if (TEMP < -1500) {
-      OFF2 = (OFF2 + 7 * (TEMP + 1500) * (TEMP + 1500));
-      SENS2 = SENS2 + ((11 * (TEMP + 1500) * (TEMP + 1500)) >> 1);
+  int64_t off = (c2 << 16) + ((dt * c4) >> 7);
+  int64_t sens = (c1 << 15) + ((dt * c3) >> 8);
+
+  if (temp < 2000) {
+    const int32_t t2 = (dt * dt) >> 31;
+    int32_t off2 = ((5 * (temp - 2000) * (temp - 2000)) >> 1);
+    int32_t sens2 = ((5 * (temp - 2000) * (temp - 2000)) >> 2);
+    if (temp < -1500) {
+      off2 = (off2 + 7 * (temp + 1500) * (temp + 1500));
+      sens2 = sens2 + ((11 * (temp + 1500) * (temp + 1500)) >> 1);
     }
-    TEMP = TEMP - T2;
-    OFF = OFF - OFF2;
-    SENS = SENS - SENS2;
+    temp = temp - t2;
+    off = off - off2;
+    sens = sens - sens2;
   }
 
   // Here we multiply unsigned 32-bit by signed 64-bit using signed 64-bit math.
-  // Possible ranges of D1 and SENS from the datasheet guarantee 
+  // Possible ranges of D1 and SENS from the datasheet guarantee
   // that this multiplication does not overflow
-  const int32_t P = ((((D1 * SENS) >> 21) - OFF) >> 15);
+  const int32_t p = ((((d1 * sens) >> 21) - off) >> 15);
 
-  const float temperature = TEMP / 100.0f;
-  const float pressure = P / 100.0f;
+  const float temperature = temp / 100.0f;
+  const float pressure = p / 100.0f;
   ESP_LOGD(TAG, "Got temperature=%0.02f°C pressure=%0.01fhPa", temperature, pressure);
 
   if (this->temperature_sensor_ != nullptr)
