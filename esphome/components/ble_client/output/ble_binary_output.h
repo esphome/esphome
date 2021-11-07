e62f7d465639cd5119b06ad6b2c49019e44c3896
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-11-08 08:47:50 +1300

    Lint

--- esphome/components/ble_client/output/ble_binary_output.h
+++ esphome/components/ble_client/output/ble_binary_output.h
@@ -23,10 +23,10 @@ class BLEBinaryOutput : public output::BinaryOutput, public BLEClientNode, publi
   void set_char_uuid16(uint16_t uuid) { this->char_uuid_ = espbt::ESPBTUUID::from_uint16(uuid); }
   void set_char_uuid32(uint32_t uuid) { this->char_uuid_ = espbt::ESPBTUUID::from_uint32(uuid); }
   void set_char_uuid128(uint8_t *uuid) { this->char_uuid_ = espbt::ESPBTUUID::from_raw(uuid); }
-  void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
+  void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) override;
 
  protected:
-  void write_state(bool state);
+  void write_state(bool state) override;
   espbt::ESPBTUUID service_uuid_;
   espbt::ESPBTUUID char_uuid_;
   espbt::ClientState client_state_;
