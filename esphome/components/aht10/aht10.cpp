7f629899afc156f902ebc8f105c108e1433abc16
krunkel <gert.vankrunkelsven@gmail.com>
2021-11-07 15:10:12 +0100

    Update aht10.cpp to address issue #1635
    The changes in release 2021.10 made measurements unreliable again (issue #1635) removing write to reg 0 solves the issue.

--- esphome/components/aht10/aht10.cpp
+++ esphome/components/aht10/aht10.cpp
@@ -73,13 +73,15 @@ void AHT10Component::update() {
   bool success = false;
   for (int i = 0; i < AHT10_ATTEMPTS; ++i) {
     ESP_LOGVV(TAG, "Attempt %d at %6u", i, millis());
-    delay_microseconds_accurate(4);
-
-    uint8_t reg = 0;
-    if (this->write(&reg, 1) != i2c::ERROR_OK) {
-      ESP_LOGD(TAG, "Communication with AHT10 failed, waiting...");
-      continue;
-    }
+    // delay_microseconds_accurate no longer needed as delay(delay_ms) was added in last release
+    //delay_microseconds_accurate(4);
+    //
+    // the 'this->write(&reg, 1)' makes the aht10 unreliable again. after removing this part measurements are ok.  
+    //uint8_t reg = 0;
+    //if (this->write(&reg, 1) != i2c::ERROR_OK) {
+    //  ESP_LOGD(TAG, "Communication with AHT10 failed, waiting...");
+    //  continue;
+    //}
     delay(delay_ms);
     if (this->read(data, 6) != i2c::ERROR_OK) {
       ESP_LOGD(TAG, "Communication with AHT10 failed, waiting...");
