fdde8dedc81f13574b298de91657155ceb0bffa8
Maurice Makaay <account-github@makaay.nl>
2021-11-07 19:23:15 +0100

    Fix Python lint issue

--- esphome/components/bme680_bsec/__init__.py
+++ esphome/components/bme680_bsec/__init__.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c
 from esphome.const import CONF_ID
-from esphome.core import CORE
 
 CODEOWNERS = ["@trvrnrth"]
 DEPENDENCIES = ["i2c"]
