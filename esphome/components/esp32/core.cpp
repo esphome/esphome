86a3f558ea2330c024c70495d2ece7f1933ff325
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2021-11-10 14:46:01 +0100

    Update core.cpp

--- esphome/components/esp32/core.cpp
+++ esphome/components/esp32/core.cpp
@@ -68,13 +68,13 @@ void loop_task(void *pv_params) {
 }
 
 extern "C" void app_main() {
-  esp32::setup_preferences();
   esp_pm_config_esp32_t pm_config = {
-    .max_freq_mhz = 240,
+    .max_freq_mhz = 160,
     .min_freq_mhz = 80,
     .light_sleep_enable = true
     };
   esp_pm_configure(&pm_config);
+  esp32::setup_preferences();
   xTaskCreate(loop_task, "loopTask", 8192, nullptr, 1, &loop_task_handle);
 }
 #endif  // USE_ESP_IDF
