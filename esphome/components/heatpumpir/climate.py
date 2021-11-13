63b53112f7cee65e1f2dca2e6aa7ab4bcca0ca8e
Clifford Roche <clifford.roche@gmail.com>
2021-11-13 10:50:29 -0500

    Add comment to HeatpumpIR references

--- esphome/components/heatpumpir/climate.py
+++ esphome/components/heatpumpir/climate.py
@@ -112,4 +112,6 @@ def to_code(config):
     cg.add(var.set_max_temperature(config[CONF_MIN_TEMPERATURE]))
     cg.add(var.set_min_temperature(config[CONF_MAX_TEMPERATURE]))
 
+    # PIO isn't updating releases, so referencing the release tag directly. See:
+    # https://github.com/ToniA/arduino-heatpumpir/commit/0948c619d86407a4e50e8db2f3c193e0576c86fd
     cg.add_library("", "", "https://github.com/ToniA/arduino-heatpumpir.git#1.0.18")
