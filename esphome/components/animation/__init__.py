179e76ec986a8e0b9178cc4136e8e788b803b01d
Dave T <17680170+davet2001@users.noreply.github.com>
2021-11-18 22:03:39 +0000

    Raise exception instead of assert

--- esphome/components/animation/__init__.py
+++ esphome/components/animation/__init__.py
@@ -62,7 +62,7 @@ async def to_code(config):
             pixels = list(frame.getdata())
             if len(pixels) != height * width:
                 raise core.EsphomeError(
-                    f"Unexpected number of pixels in frame {frameIndex}: {len(pixels)} != {height*width}"
+                    f"Unexpected number of pixels in frame {frameIndex}: ({len(pixels)} != {height*width}"
                 )
             for pix in pixels:
                 data[pos] = pix
@@ -79,7 +79,7 @@ async def to_code(config):
             pixels = list(frame.getdata())
             if len(pixels) != height * width:
                 raise core.EsphomeError(
-                    f"Unexpected number of pixels in frame {frameIndex}: {len(pixels)} != {height*width}"
+                    f"Unexpected number of pixels in frame {frameIndex}: ({len(pixels)} != {height*width}"
                 )
             for pix in pixels:
                 data[pos] = pix[0]
