03ffcfc2973993452f829baf31ca2e3979fc0741
Adrian Panella <ianchi74@outlook.com>
2021-11-24 20:29:57 -0600

    total_daily_energy: allow to disable restore mode
    Similarly to the "Integration" component, allow to disable the saving of
    intermediate results to device.
    Currently you can set the save interval but not disable it.

--- esphome/components/total_daily_energy/sensor.py
+++ esphome/components/total_daily_energy/sensor.py
@@ -4,6 +4,7 @@ from esphome.components import sensor, time
 from esphome.const import (
     CONF_ICON,
     CONF_ID,
+    CONF_RESTORE,
     CONF_TIME_ID,
     DEVICE_CLASS_ENERGY,
     CONF_METHOD,
@@ -36,6 +37,7 @@ CONFIG_SCHEMA = (
             cv.GenerateID(): cv.declare_id(TotalDailyEnergy),
             cv.GenerateID(CONF_TIME_ID): cv.use_id(time.RealTimeClock),
             cv.Required(CONF_POWER_ID): cv.use_id(sensor.Sensor),
+            cv.Optional(CONF_RESTORE, default=True): cv.boolean,
             cv.Optional(
                 CONF_MIN_SAVE_INTERVAL, default="0s"
             ): cv.positive_time_period_milliseconds,
@@ -70,5 +72,6 @@ async def to_code(config):
     cg.add(var.set_parent(sens))
     time_ = await cg.get_variable(config[CONF_TIME_ID])
     cg.add(var.set_time(time_))
+    cg.add(var.set_restore(config[CONF_RESTORE]))
     cg.add(var.set_min_save_interval(config[CONF_MIN_SAVE_INTERVAL]))
     cg.add(var.set_method(config[CONF_METHOD]))
