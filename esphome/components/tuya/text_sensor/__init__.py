939fb313df622d3fabc14ea837e296c7aa5ec0ec
dentra <dentra@users.noreply.github.com>
2021-11-29 22:08:52 +0300

    Tuya text_sensor and raw data usage (#1812)

--- esphome/components/tuya/text_sensor/__init__.py
+++ esphome/components/tuya/text_sensor/__init__.py
@@ -1,17 +1,17 @@
-from esphome.components import sensor
+from esphome.components import text_sensor
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.const import CONF_ID, CONF_SENSOR_DATAPOINT
 from .. import tuya_ns, CONF_TUYA_ID, Tuya
 
 DEPENDENCIES = ["tuya"]
-CODEOWNERS = ["@jesserockz"]
+CODEOWNERS = ["@dentra"]
 
-TuyaSensor = tuya_ns.class_("TuyaSensor", sensor.Sensor, cg.Component)
+TuyaTextSensor = tuya_ns.class_("TuyaTextSensor", text_sensor.TextSensor, cg.Component)
 
-CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend(
+CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
     {
-        cv.GenerateID(): cv.declare_id(TuyaSensor),
+        cv.GenerateID(): cv.declare_id(TuyaTextSensor),
         cv.GenerateID(CONF_TUYA_ID): cv.use_id(Tuya),
         cv.Required(CONF_SENSOR_DATAPOINT): cv.uint8_t,
     }
@@ -21,7 +21,7 @@ CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
+    await text_sensor.register_text_sensor(var, config)
 
     paren = await cg.get_variable(config[CONF_TUYA_ID])
     cg.add(var.set_tuya_parent(paren))
