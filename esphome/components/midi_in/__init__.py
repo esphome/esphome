7e0d19a30f897be90ba66db6b5f5868b21bcf316
Mikhail Diatchenko <mikhail.diatchenko@gmail.com>
2021-12-02 18:36:10 +1300

    Added enum support for control condition

--- esphome/components/midi_in/__init__.py
+++ esphome/components/midi_in/__init__.py
@@ -15,24 +15,88 @@ CODEOWNERS = ["@muxa"]
 DEPENDENCIES = ["uart"]
 AUTO_LOAD = ["binary_sensor"]
 
-midi_ns = cg.esphome_ns.namespace("midi_in")
+midi_ns = cg.global_ns.namespace("midi")
+midi_in_ns = cg.esphome_ns.namespace("midi_in")
 
-MidiInComponent = midi_ns.class_("MidiInComponent", cg.Component, uart.UARTDevice)
+MidiInComponent = midi_in_ns.class_("MidiInComponent", cg.Component, uart.UARTDevice)
 
-MidiChannelMessage = midi_ns.class_("MidiChannelMessage")
+MidiChannelMessage = midi_in_ns.class_("MidiChannelMessage")
 
-MidiSystemMessage = midi_ns.class_("MidiSystemMessage")
+MidiSystemMessage = midi_in_ns.class_("MidiSystemMessage")
 
-MidiInOnChannelMessageTrigger = midi_ns.class_(
+MidiInOnChannelMessageTrigger = midi_in_ns.class_(
     "MidiInOnChannelMessageTrigger", automation.Trigger.template()
 )
 
-MidiInOnSystemMessageTrigger = midi_ns.class_(
+MidiInOnSystemMessageTrigger = midi_in_ns.class_(
     "MidiInOnSystemMessageTrigger", automation.Trigger.template()
 )
 
-MidiInNoteOnCondition = midi_ns.class_("MidiInNoteOnCondition", automation.Condition)
-MidiInControlInRangeCondition = midi_ns.class_(
+MidiInNoteOnCondition = midi_in_ns.class_("MidiInNoteOnCondition", automation.Condition)
+
+MidiControlChangeNumber = midi_ns.enum("MidiControlChangeNumber")
+MIDI_CONTROL_CHANGES = {
+    "BankSelect": MidiControlChangeNumber.BankSelect,
+    "ModulationWheel": MidiControlChangeNumber.ModulationWheel,
+    "BreathController": MidiControlChangeNumber.BreathController,
+    "FootController": MidiControlChangeNumber.FootController,
+    "PortamentoTime": MidiControlChangeNumber.PortamentoTime,
+    "DataEntryMSB": MidiControlChangeNumber.DataEntryMSB,
+    "ChannelVolume": MidiControlChangeNumber.ChannelVolume,
+    "Balance": MidiControlChangeNumber.Balance,
+    "Pan": MidiControlChangeNumber.Pan,
+    "ExpressionController": MidiControlChangeNumber.ExpressionController,
+    "EffectControl1": MidiControlChangeNumber.EffectControl1,
+    "EffectControl2": MidiControlChangeNumber.EffectControl2,
+    "GeneralPurposeController1": MidiControlChangeNumber.GeneralPurposeController1,
+    "GeneralPurposeController2": MidiControlChangeNumber.GeneralPurposeController2,
+    "GeneralPurposeController3": MidiControlChangeNumber.GeneralPurposeController3,
+    "GeneralPurposeController4": MidiControlChangeNumber.GeneralPurposeController4,
+    "DataEntryLSB": MidiControlChangeNumber.DataEntryLSB,
+    "Sustain": MidiControlChangeNumber.Sustain,
+    "Portamento": MidiControlChangeNumber.Portamento,
+    "Sostenuto": MidiControlChangeNumber.Sostenuto,
+    "SoftPedal": MidiControlChangeNumber.SoftPedal,
+    "Legato": MidiControlChangeNumber.Legato,
+    "Hold": MidiControlChangeNumber.Hold,
+    "SoundController1": MidiControlChangeNumber.SoundController1,
+    "SoundController2": MidiControlChangeNumber.SoundController2,
+    "SoundController3": MidiControlChangeNumber.SoundController3,
+    "SoundController4": MidiControlChangeNumber.SoundController4,
+    "SoundController5": MidiControlChangeNumber.SoundController5,
+    "SoundController6": MidiControlChangeNumber.SoundController6,
+    "SoundController7": MidiControlChangeNumber.SoundController7,
+    "SoundController8": MidiControlChangeNumber.SoundController8,
+    "SoundController9": MidiControlChangeNumber.SoundController9,
+    "SoundController10": MidiControlChangeNumber.SoundController10,
+    "GeneralPurposeController5": MidiControlChangeNumber.GeneralPurposeController5,
+    "GeneralPurposeController6": MidiControlChangeNumber.GeneralPurposeController6,
+    "GeneralPurposeController7": MidiControlChangeNumber.GeneralPurposeController7,
+    "GeneralPurposeController8": MidiControlChangeNumber.GeneralPurposeController8,
+    "PortamentoControl": MidiControlChangeNumber.PortamentoControl,
+    "Effects1": MidiControlChangeNumber.Effects1,
+    "Effects2": MidiControlChangeNumber.Effects2,
+    "Effects3": MidiControlChangeNumber.Effects3,
+    "Effects4": MidiControlChangeNumber.Effects4,
+    "Effects5": MidiControlChangeNumber.Effects5,
+    "DataIncrement": MidiControlChangeNumber.DataIncrement,
+    "DataDecrement": MidiControlChangeNumber.DataDecrement,
+    "NRPNLSB": MidiControlChangeNumber.NRPNLSB,
+    "NRPNMSB": MidiControlChangeNumber.NRPNMSB,
+    "RPNLSB": MidiControlChangeNumber.RPNLSB,
+    "RPNMSB": MidiControlChangeNumber.RPNMSB,
+    "AllSoundOff": MidiControlChangeNumber.AllSoundOff,
+    "ResetAllControllers": MidiControlChangeNumber.ResetAllControllers,
+    "LocalControl": MidiControlChangeNumber.LocalControl,
+    "AllNotesOff": MidiControlChangeNumber.AllNotesOff,
+    "OmniModeOff": MidiControlChangeNumber.OmniModeOff,
+    "OmniModeOn": MidiControlChangeNumber.OmniModeOn,
+    "MonoModeOn": MidiControlChangeNumber.MonoModeOn,
+    "PolyModeOn": MidiControlChangeNumber.PolyModeOn,
+}
+midi_control_changes = cv.enum(MIDI_CONTROL_CHANGES, upper=False)
+
+MidiInControlInRangeCondition = midi_in_ns.class_(
     "MidiInControlInRangeCondition", automation.Condition
 )
 
@@ -92,7 +156,7 @@ FINAL_VALIDATE_SCHEMA = validate_uart
 
 async def to_code(config):
 
-    cg.add_global(midi_ns.using)
+    cg.add_global(midi_in_ns.using)
 
     cg.add_library("MIDI Library", "5.0.2")
 
@@ -160,7 +224,9 @@ async def midi_in_note_on_condition_to_code(config, condition_id, template_arg, 
         {
             cv.GenerateID(): cv.use_id(MidiInComponent),
             cv.Required(CONF_CONTROL): cv.Any(
-                cv.int_range(min=0, max=127), cv.hex_int_range(min=0, max=127)
+                midi_control_changes,
+                cv.int_range(min=0, max=127),
+                cv.hex_int_range(min=0, max=127),
             ),
             cv.Optional(CONF_ABOVE): cv.Any(
                 cv.int_range(min=0, max=127), cv.hex_int_range(min=0, max=127)
