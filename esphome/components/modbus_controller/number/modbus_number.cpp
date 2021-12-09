6f4f132490607eeb1dea6fe8bea73dc28e65165f
Martin <25747549+martgras@users.noreply.github.com>
2021-12-09 11:23:36 +0100

    Allow forcing write_multiple

--- esphome/components/modbus_controller/number/modbus_number.cpp
+++ esphome/components/modbus_controller/number/modbus_number.cpp
@@ -55,7 +55,7 @@ void ModbusNumber::control(float value) {
 
   // Create and send the write command
   ModbusCommandItem write_cmd;
-  if (this->register_count == 1) {
+  if (this->register_count == 1 && !this->use_write_multiple_) {
     write_cmd = ModbusCommandItem::create_write_single_command(parent_, this->start_address + this->offset, data[0]);
   } else {
     write_cmd = ModbusCommandItem::create_write_multiple_command(parent_, this->start_address + this->offset,
