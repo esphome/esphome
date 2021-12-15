f8c93c8f0cbb1f4f8cac16f08e5fd274d7d427bd
Sympatron <35803463+Sympatron@users.noreply.github.com>
2021-12-15 18:12:09 +0100

    Only accept pin numbers for esp32-can

--- esphome/components/esp32_can/canbus.py
+++ esphome/components/esp32_can/canbus.py
@@ -24,8 +24,8 @@ CONFIG_SCHEMA = canbus.CANBUS_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(esp32_can),
         cv.Optional(CONF_BIT_RATE, default="125KBPS"): cv.enum(CAN_SPEEDS, upper=True),
-        cv.Required(CONF_RX_PIN): pins.internal_gpio_input_pin_schema,
-        cv.Required(CONF_TX_PIN): pins.internal_gpio_output_pin_schema,
+        cv.Required(CONF_RX_PIN): pins.internal_gpio_input_pin_number,
+        cv.Required(CONF_TX_PIN): pins.internal_gpio_output_pin_number,
     }
 )
 
