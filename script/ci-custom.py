c57e2f0ac7e69687d5a5e759c53e9a288737d28f
Richard Nauber <richard@nauber.dev>
2021-12-26 00:30:51 +0100

    CI: add ".bin" to ignored file types for script/ci-custom.py

--- script/ci-custom.py
+++ script/ci-custom.py
@@ -80,7 +80,7 @@ file_types = (
     "",
 )
 cpp_include = ("*.h", "*.c", "*.cpp", "*.tcc")
-ignore_types = (".ico", ".woff", ".woff2", "")
+ignore_types = (".ico", ".woff", ".woff2", ".bin", "")
 
 LINT_FILE_CHECKS = []
 LINT_CONTENT_CHECKS = []
