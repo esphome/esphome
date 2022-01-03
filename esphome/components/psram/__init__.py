3499f07063d91f99b705365dcd3ff4c9044eb892
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2022-01-03 20:59:36 +0100

    Log PSRAM availability

--- esphome/components/psram/__init__.py
+++ esphome/components/psram/__init__.py
@@ -2,10 +2,18 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components.esp32 import add_idf_sdkconfig_option
 from esphome.core import CORE
+from esphome.const import (
+    CONF_ID,
+)
 
 CODEOWNERS = ["@esphome/core"]
 
-CONFIG_SCHEMA = cv.All(cv.Schema({}), cv.only_on_esp32)
+psram_ns = cg.esphome_ns.namespace("psram")
+PsramComponent = psram_ns.class_("PsramComponent", cg.Component)
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({cv.GenerateID(): cv.declare_id(PsramComponent)}), cv.only_on_esp32
+)
 
 
 async def to_code(config):
@@ -16,3 +24,6 @@ async def to_code(config):
         add_idf_sdkconfig_option("CONFIG_ESP32_SPIRAM_SUPPORT", True)
         add_idf_sdkconfig_option("CONFIG_SPIRAM_USE_CAPS_ALLOC", True)
         add_idf_sdkconfig_option("CONFIG_SPIRAM_IGNORE_NOTFOUND", True)
+
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
