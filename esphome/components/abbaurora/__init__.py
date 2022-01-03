6dc34c0d69b3c894cee81a6dd6f102cb4931bf8a
Arie Johan van de Werken <ajvandewerken@gmail.com>
2022-01-03 14:52:23 +0100

    Update __init__.py

--- esphome/components/abbaurora/__init__.py
+++ esphome/components/abbaurora/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
-from esphome.components import sensor,uart
+from esphome.components import uart
 from esphome.cpp_helpers import gpio_pin_expression
 from esphome.const import (
     CONF_FLOW_CONTROL_PIN,
@@ -19,7 +19,7 @@ AUTO_LOAD = ["sensor", "text_sensor"]
 CONF_ABBAURORA_ID = "abbaurora_id"
 
 abbaurora_ns = cg.esphome_ns.namespace("esphome::abbaurora")
-ABBAurora = abbaurora_ns.class_("ABBAuroraComponent", uart.UARTDevice, cg.Component )
+ABBAurora = abbaurora_ns.class_("ABBAuroraComponent", uart.UARTDevice, cg.Component)
 
 ABBAURORA_COMPONENT_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
@@ -39,6 +39,7 @@ CONFIG_SCHEMA = (
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 
+
 async def to_code(config):
     cg.add_global(abbaurora_ns.using)
     var = cg.new_Pvariable(config[CONF_ID])
@@ -49,4 +50,3 @@ async def to_code(config):
         cg.add(var.set_flow_control_pin(pin))
     if CONF_ADDRESS in config:
         cg.add(var.set_address(config[CONF_ADDRESS]))
-
