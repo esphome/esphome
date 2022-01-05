b3bdc0d99b8d9b5b288f5ed63021ff0ff9ecc427
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-01-05 13:59:20 +1300

    pylint

--- esphome/components/esp32/post_build.py
+++ esphome/components/esp32/post_build.py
@@ -1,5 +1,6 @@
 # Source https://github.com/letscontrolit/ESPEasy/blob/bb8907ee87af8980f95141caf1415c37fa39c941/tools/pio/post_esp32.py
 
+# pylint: disable=E0602
 Import("env")  # noqa
 
 
@@ -8,19 +9,17 @@ def esp32_create_combined_bin(source, target, env):
     offset = 0x0
     new_file_name = env.subst("$BUILD_DIR/${PROGNAME}-combined.bin")
     sections = env.subst(env.get("FLASH_EXTRA_IMAGES"))
-    new_file = open(new_file_name, "wb")
-    for section in sections:
-        sect_adr, sect_file = section.split(" ", 1)
-        source = open(sect_file, "rb")
-        new_file.seek(int(sect_adr, 0) - offset)
-        new_file.write(source.read())
-        source.close()
+    with open(new_file_name, "wb") as new_file:
+        for section in sections:
+            sect_adr, sect_file = section.split(" ", 1)
+            with open(sect_file, "rb") as extra:
+                new_file.seek(int(sect_adr, 0) - offset)
+                new_file.write(extra.read())
 
-    firmware = open(env.subst("$BUILD_DIR/${PROGNAME}.bin"), "rb")
-    new_file.seek(0x10000 - offset)
-    new_file.write(firmware.read())
-    new_file.close()
-    firmware.close()
+        with open(env.subst("$BUILD_DIR/${PROGNAME}.bin"), "rb") as firmware:
+            new_file.seek(0x10000 - offset)
+            new_file.write(firmware.read())
 
 
+# pylint: disable=E0602
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", esp32_create_combined_bin)  # noqa
