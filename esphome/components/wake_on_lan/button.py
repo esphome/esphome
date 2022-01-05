8d1a4b21278f5734d2df014719d87d9a10c23a8a
William Charlton <will.charlton1@icloud.com>
2022-01-05 13:31:44 +0000

    extend base schema

--- esphome/components/wake_on_lan/button.py
+++ esphome/components/wake_on_lan/button.py
@@ -12,25 +12,23 @@ WakeOnLanButton = wake_on_lan_ns.class_("WakeOnLanButton", button.Button, cg.Com
 DEPENDENCIES = ["wifi"]
 
 CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.Required(CONF_TARGET_MAC_ADDRESS): cv.mac_address,
-            cv.GenerateID(): cv.declare_id(WakeOnLanButton),
-        }
-    )
-    .extend(button.BUTTON_SCHEMA)
+    cv.ENTITY_BASE_SCHEMA.extend(button.BUTTON_SCHEMA)
     .extend(cv.COMPONENT_SCHEMA)
+    .extend(
+        cv.Schema(
+            {
+                cv.Required(CONF_TARGET_MAC_ADDRESS): cv.mac_address,
+                cv.GenerateID(): cv.declare_id(WakeOnLanButton),
+            }
+        )
+    )
 )
 
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    
-    yield cg.add(
-        var.set_macaddr(
-            *config[CONF_TARGET_MAC_ADDRESS].parts
-        )
-    )
+
+    yield cg.add(var.set_macaddr(*config[CONF_TARGET_MAC_ADDRESS].parts))
     yield cg.register_component(var, config)
     yield button.register_button(var, config)
     yield cg.add(var)
