f166bd34576342a91ee1f02c925a080f187a64fa
Synco Reynders <synco@deviceware.co.nz>
2022-01-09 22:37:21 -0500

    Removed #defines

--- esphome/components/mlx90614/mlx90614.cpp
+++ esphome/components/mlx90614/mlx90614.cpp
@@ -9,23 +9,23 @@ static const char *const TAG = "mlx90614.sensor";
 
 // MLX90614 registers from the datasheet
 // RAM
-#define MLX90614_RAWIR1 0x04
-#define MLX90614_RAWIR2 0x05
-#define MLX90614_TA 0x06
-#define MLX90614_TOBJ1 0x07
-#define MLX90614_TOBJ2 0x08
+static const uint8_t MLX90614_RAWIR1 = 0x04;
+static const uint8_t MLX90614_RAWIR2 = 0x05;
+static const uint8_t MLX90614_TA = 0x06;
+static const uint8_t MLX90614_TOBJ1 = 0x07;
+static const uint8_t MLX90614_TOBJ2 = 0x08;
 // EEPROM
-#define MLX90614_TOMAX 0x20
-#define MLX90614_TOMIN 0x21
-#define MLX90614_PWMCTRL 0x22
-#define MLX90614_TARANGE 0x23
-#define MLX90614_EMISS 0x24
-#define MLX90614_CONFIG 0x25
-#define MLX90614_ADDR 0x2E
-#define MLX90614_ID1 0x3C
-#define MLX90614_ID2 0x3D
-#define MLX90614_ID3 0x3E
-#define MLX90614_ID4 0x3F
+static const uint8_t MLX90614_TOMAX = 0x20;
+static const uint8_t MLX90614_TOMIN = 0x21;
+static const uint8_t MLX90614_PWMCTRL = 0x22;
+static const uint8_t MLX90614_TARANGE = 0x23;
+static const uint8_t MLX90614_EMISS = 0x24;
+static const uint8_t MLX90614_CONFIG = 0x25;
+static const uint8_t MLX90614_ADDR = 0x2E;
+static const uint8_t MLX90614_ID1 = 0x3C;
+static const uint8_t MLX90614_ID2 = 0x3D;
+static const uint8_t MLX90614_ID3 = 0x3E;
+static const uint8_t MLX90614_ID4 = 0x3F;
 
 void MLX90614Component::setup() {
   ESP_LOGI(TAG, "Setting up MLX90614 sensor at I2C address 0x%02X", this->address_);
