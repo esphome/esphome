e171ab8419f2951f56d58dc8ccf56c5abc3d3df8
Synco Reynders <synco@deviceware.co.nz>
2022-01-11 11:38:37 -0500

    Lint

--- esphome/components/graph/graph.cpp
+++ esphome/components/graph/graph.cpp
@@ -146,7 +146,7 @@ void Graph::draw(DisplayBuffer *buff, uint16_t x_offset, uint16_t y_offset, Colo
       }
       ESP_LOGW(TAG, "Graphing reducing x-scale to prevent too many gridlines");
     }
-    float xspc = ((float)this->width_ - 1) / this->duration_;
+    float xspc = ((float) this->width_ - 1) / this->duration_;
     for (uint32_t y = 0; y < this->height_; y += 2) {
       for (int i = 0; i <= n; i++) {
         buff->draw_pixel_at(x_offset + this->width_ - i * this->gridspacing_x_ * xspc, y_offset + y, color);
