2cea8f7634ae79de8da79ec7d49b0e9248fe762b
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2022-01-12 09:17:21 +0400

    Changes in Dish protocol

--- esphome/components/remote_base/dish_protocol.cpp
+++ esphome/components/remote_base/dish_protocol.cpp
@@ -16,23 +16,17 @@ static const size_t REMOTE_DATA_SIZE = 2 + 4 * (2 * 6 + 2 * 4 + 2 * 6 + 2);
 void DishProtocol::encode(RemoteTransmitData *dst, const DishData &data) {
   dst->set_carrier_frequency(57600);
   dst->reserve(REMOTE_DATA_SIZE);
-
   // HEADER
   dst->item(HEADER_MARK_US, HEADER_SPACE_US);
-
   //  Typically a DISH device needs to get a command a total of
   //  at least 4 times to accept it.
   for (unsigned i = 0; i < 4; i++) {
     // COMMAND (function, 6 bits, in MSB)
     msb::encode<6, BIT_MARK_US, BIT_ONE_SPACE_US, BIT_ZERO_SPACE_US>(dst, data.command);
-
     // ADDRESS (unit code, 4 bits, in LSB)
     lsb::encode<4, BIT_MARK_US, BIT_ONE_SPACE_US, BIT_ZERO_SPACE_US>(dst, data.address - 1);
-
-    // PADDING (6 zeroes)
-    for (unsigned j = 0; j < 6; j++)
-      dst->item(BIT_MARK_US, BIT_ZERO_SPACE_US);
-
+    // PADDING (6 zero bits)
+    msb::encode<6, BIT_MARK_US, 0, BIT_ZERO_SPACE_US>(dst, 0);
     // FOOTER
     dst->item(HEADER_MARK_US, HEADER_SPACE_US);
   }
@@ -48,17 +42,12 @@ optional<DishData> DishProtocol::decode(RemoteReceiveData src) {
       .address = 0,
       .command = 0,
   };
-
   if (!src.expect_item(HEADER_MARK_US, HEADER_SPACE_US) || !decode_data(src, data))
     return {};
-
-  for (unsigned j = 0; j < 6; j++)
-    if (!src.expect_item(BIT_MARK_US, BIT_ZERO_SPACE_US))
-      return {};
-
+  if (!msb::equal<6, BIT_MARK_US, 0, BIT_ZERO_SPACE_US>(src, 0))
+    return {};
   if (!src.expect_mark(HEADER_MARK_US))
     return {};
-
   data.address++;
   return data;
 }
