e4564d359bc2eae227f0be67b8a651d4bc8e17c6
Tyler Bules <tylerbules@gmail.com>
2022-01-17 14:39:01 -0500

    add CORE import

--- esphome/components/deep_sleep/__init__.py
+++ esphome/components/deep_sleep/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins, automation
+from esphome.core import CORE, coroutine_with_priority
 from esphome.const import (
     CONF_ID,
     CONF_MODE,
