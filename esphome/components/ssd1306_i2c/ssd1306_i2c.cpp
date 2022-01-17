8723018cfeb624d31ba735233f9dc3a18318b5bf
Nikita Kuklev <spontaneouscoder@gmail.com>
2022-01-17 07:52:00 +0000

    Change SH1107 starting column

--- esphome/components/ssd1306_i2c/ssd1306_i2c.cpp
+++ esphome/components/ssd1306_i2c/ssd1306_i2c.cpp
@@ -38,11 +38,16 @@ void I2CSSD1306::dump_config() {
 }
 void I2CSSD1306::command(uint8_t value) { this->write_byte(0x00, value); }
 void HOT I2CSSD1306::write_display_data() {
-  if (this->is_sh1106_()) {
+  if (this->is_sh1106_() | this->is_sh1107_()) {
     uint32_t i = 0;
     for (uint8_t page = 0; page < (uint8_t) this->get_height_internal() / 8; page++) {
       this->command(0xB0 + page);  // row
-      this->command(0x02);         // lower column
+      // Select lower column - 0x02 is historical SH1106 value
+      // Other SH1107 drivers use 0x00, so we do as well
+      if (this->is_sh1106_())
+        this->command(0x02);
+      else
+        this->command(0x00);
       this->command(0x10);         // higher column
 
       for (uint8_t x = 0; x < (uint8_t) this->get_width_internal() / 16; x++) {
