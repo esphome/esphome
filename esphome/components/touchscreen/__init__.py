a572fbd69d1dd137947839e539b956316f7b297b
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-01-22 19:41:17 +1300

    Remove unused validator

--- esphome/components/touchscreen/__init__.py
+++ esphome/components/touchscreen/__init__.py
@@ -18,20 +18,6 @@ TouchListener = touchscreen_ns.class_("TouchListener")
 CONF_DISPLAY = "display"
 CONF_TOUCHSCREEN_ID = "touchscreen_id"
 
-ROTATIONS = {
-    0: TouchRotation.ROTATE_0_DEGREES,
-    90: TouchRotation.ROTATE_90_DEGREES,
-    180: TouchRotation.ROTATE_180_DEGREES,
-    270: TouchRotation.ROTATE_270_DEGREES,
-}
-
-
-def validate_rotation(value):
-    value = cv.string(value)
-    if value.endswith("°"):
-        value = value[:-1]
-    return cv.enum(ROTATIONS, int=True)(value)
-
 
 TOUCHSCREEN_SCHEMA = cv.Schema(
     {
