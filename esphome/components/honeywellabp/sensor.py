7e903761c0a441caca3fe6dbe350a165e1216632
RubyBailey <ruby_bailey11@hotmail.com>
2022-02-04 19:23:46 -0800

    updated

--- esphome/components/honeywellabp/sensor.py
+++ esphome/components/honeywellabp/sensor.py
@@ -19,9 +19,9 @@ CONF_HONEYWELLABP_MAX_PRESSURE = "max_pressure"
 
 honeywellabp_ns = cg.esphome_ns.namespace("honeywellabp")
 HONEYWELLABPSensor = honeywellabp_ns.class_(
-    "HONEYWELLABPSensor", 
-    sensor.Sensor, 
-    cg.PollingComponent, 
+    "HONEYWELLABPSensor",
+    sensor.Sensor,
+    cg.PollingComponent,
     spi.SPIDevice
 )
 
@@ -29,29 +29,30 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(HONEYWELLABPSensor),
-            cv.Optional(CONF_PRESSURE):sensor.sensor_schema(
-                accuracy_decimals = 1,
-                device_class = DEVICE_CLASS_PRESSURE,
-                state_class = STATE_CLASS_MEASUREMENT,
+            cv.Optional(CONF_PRESSURE): sensor.sensor_schema(
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_PRESSURE,
+                state_class=STATE_CLASS_MEASUREMENT,
             ).extend(
                 {
                     cv.Required(CONF_HONEYWELLABP_MIN_PRESSURE): cv.float_,
                     cv.Required(CONF_HONEYWELLABP_MAX_PRESSURE): cv.float_,
                 }
             ),
-            cv.Optional(CONF_TEMPERATURE):sensor.sensor_schema(
-                unit_of_measurement = UNIT_CELSIUS,
-                accuracy_decimals = 1,
-                device_class = DEVICE_CLASS_TEMPERATURE,
-                state_class = STATE_CLASS_MEASUREMENT,
+            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_CELSIUS,
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
             ),
         }
-    ) 
+    )
     .extend(cv.polling_component_schema("60s"))
     .extend(spi.spi_device_schema(cs_pin_required=True))
 )
 
 async def to_code(config):
+
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
@@ -67,5 +68,6 @@ async def to_code(config):
         conf = config[CONF_TEMPERATURE]
         sens = await sensor.new_sensor(conf)
         cg.add(var.set_temperature_sensor(sens))
-        
+
+
           
