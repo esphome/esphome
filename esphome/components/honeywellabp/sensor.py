686f36bb2cfd10ab9a7b4e1689ff706e3177f7b2
RubyBailey <ruby_bailey11@hotmail.com>
2022-02-04 20:51:23 -0800

    updated

--- esphome/components/honeywellabp/sensor.py
+++ esphome/components/honeywellabp/sensor.py
@@ -51,8 +51,8 @@ CONFIG_SCHEMA = (
     .extend(spi.spi_device_schema(cs_pin_required=True))
 )
 
-async def to_code(config):
 
+async def to_code(config):
 
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
