1e5004f495a1bb10d9b70b9ec96f2250bbe59f3e
mknjc <markus.knetschke@gmail.com>
2022-02-08 00:45:27 +0100

    [debug] Refactor debug sensors to use the normal sensor model. (#3162)

---
+++ esphome/components/debug/text_sensor.py
@@ -0,0 +1,29 @@
+from esphome.components import text_sensor
+import esphome.config_validation as cv
+import esphome.codegen as cg
+from esphome.const import (
+    CONF_ID,
+    CONF_DEVICE,
+)
+from . import CONF_DEBUG_ID, DebugComponent
+
+DEPENDENCIES = ["debug"]
+
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_DEBUG_ID): cv.use_id(DebugComponent),
+        cv.Optional(CONF_DEVICE): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
+        ),
+    }
+)
+
+
+async def to_code(config):
+    debug_component = await cg.get_variable(config[CONF_DEBUG_ID])
+
+    if CONF_DEVICE in config:
+        sens = cg.new_Pvariable(config[CONF_DEVICE][CONF_ID])
+        await text_sensor.register_text_sensor(sens, config[CONF_DEVICE])
+        cg.add(debug_component.set_device_info_sensor(sens))
