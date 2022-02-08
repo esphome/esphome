7ca9245735ee04b22fe5fb00ea81d52449b0f7c8
Jonas Bergler <jonas@bergler.name>
2022-02-08 20:27:39 +1300

    wifi_info, reduce polling interval (#3165)
    Co-authored-by: Jonas Bergler <jbergler@meraki.com>
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/wifi_info/text_sensor.py
+++ esphome/components/wifi_info/text_sensor.py
@@ -14,14 +14,16 @@ DEPENDENCIES = ["wifi"]
 
 wifi_info_ns = cg.esphome_ns.namespace("wifi_info")
 IPAddressWiFiInfo = wifi_info_ns.class_(
-    "IPAddressWiFiInfo", text_sensor.TextSensor, cg.Component
+    "IPAddressWiFiInfo", text_sensor.TextSensor, cg.PollingComponent
 )
 ScanResultsWiFiInfo = wifi_info_ns.class_(
     "ScanResultsWiFiInfo", text_sensor.TextSensor, cg.PollingComponent
 )
-SSIDWiFiInfo = wifi_info_ns.class_("SSIDWiFiInfo", text_sensor.TextSensor, cg.Component)
+SSIDWiFiInfo = wifi_info_ns.class_(
+    "SSIDWiFiInfo", text_sensor.TextSensor, cg.PollingComponent
+)
 BSSIDWiFiInfo = wifi_info_ns.class_(
-    "BSSIDWiFiInfo", text_sensor.TextSensor, cg.Component
+    "BSSIDWiFiInfo", text_sensor.TextSensor, cg.PollingComponent
 )
 MacAddressWifiInfo = wifi_info_ns.class_(
     "MacAddressWifiInfo", text_sensor.TextSensor, cg.Component
@@ -31,16 +33,16 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.Optional(CONF_IP_ADDRESS): text_sensor.text_sensor_schema(
             klass=IPAddressWiFiInfo, entity_category=ENTITY_CATEGORY_DIAGNOSTIC
-        ),
+        ).extend(cv.polling_component_schema("1s")),
         cv.Optional(CONF_SCAN_RESULTS): text_sensor.text_sensor_schema(
             klass=ScanResultsWiFiInfo, entity_category=ENTITY_CATEGORY_DIAGNOSTIC
         ).extend(cv.polling_component_schema("60s")),
         cv.Optional(CONF_SSID): text_sensor.text_sensor_schema(
             klass=SSIDWiFiInfo, entity_category=ENTITY_CATEGORY_DIAGNOSTIC
-        ),
+        ).extend(cv.polling_component_schema("1s")),
         cv.Optional(CONF_BSSID): text_sensor.text_sensor_schema(
             klass=BSSIDWiFiInfo, entity_category=ENTITY_CATEGORY_DIAGNOSTIC
-        ),
+        ).extend(cv.polling_component_schema("1s")),
         cv.Optional(CONF_MAC_ADDRESS): text_sensor.text_sensor_schema(
             klass=MacAddressWifiInfo, entity_category=ENTITY_CATEGORY_DIAGNOSTIC
         ),
