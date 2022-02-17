927253ed61b4c67c10cf5292e20cf7e6016dcdcd
Guillem Pagès Gassull <git@guillempages.cat>
2022-02-17 14:51:26 +0100

    Add resize capabilities
    (can only enlarge; shrinking does not work)

--- esphome/components/online_image/__init__.py
+++ esphome/components/online_image/__init__.py
@@ -20,16 +20,9 @@ MULTI_CONF = True
 
 online_image_ns = cg.esphome_ns.namespace("online_image")
 
-ImageType = display.display_ns.enum("ImageType")
-IMAGE_TYPE = {
-    "BINARY": ImageType.IMAGE_TYPE_BINARY,
-    "GRAYSCALE": ImageType.IMAGE_TYPE_GRAYSCALE,
-    "RGB24": ImageType.IMAGE_TYPE_RGB24,
-    "TRANSPARENT_BINARY": ImageType.IMAGE_TYPE_TRANSPARENT_BINARY,
-}
 ImageFormat = online_image_ns.enum("ImageFormat")
 IMAGE_FORMAT = {
-    "JPEG": ImageFormat.JPEG,
+#    "JPEG": ImageFormat.JPEG,
     "PNG": ImageFormat.PNG,
 }
 
@@ -40,11 +33,7 @@ IMAGE_SCHEMA = cv.Schema(
         cv.Required(CONF_ID): cv.declare_id(Image_),
         cv.Required(CONF_URL): cv.string,
         cv.Optional(CONF_RESIZE): cv.dimensions,
-        cv.Optional(CONF_TYPE, default="BINARY"): cv.enum(IMAGE_TYPE, upper=True),
         cv.Optional(CONF_FORMAT, default="PNG"): cv.enum(IMAGE_FORMAT, upper=True),
-        cv.Optional(CONF_DITHER, default="NONE"): cv.one_of(
-            "NONE", "FLOYDSTEINBERG", upper=True
-        ),
         cv.GenerateID(CONF_RAW_DATA_ID): cv.declare_id(cg.uint8),
     }
 )
@@ -53,8 +42,12 @@ CONFIG_SCHEMA = IMAGE_SCHEMA
 
 async def to_code(config):
     url = config[CONF_URL]
+    width, height = 0, 0
+    if config.get(CONF_RESIZE):
+        width, height = config[CONF_RESIZE]
+
     cg.new_Pvariable(
-        config[CONF_ID], url, IMAGE_TYPE[config[CONF_TYPE]]
+        config[CONF_ID], url, width, height
     )
     cg.add_define("USE_ONLINE_IMAGE")
     cg.add_library("pngle", None)
