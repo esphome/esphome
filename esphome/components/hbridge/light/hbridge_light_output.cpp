c883b06e389c87fe64e14cdf3ec17ed3f2cc81d5
FaBjE <5355001+FaBjE@users.noreply.github.com>
2022-02-21 19:11:21 +0100

    Give hbridge_light_output its own .cpp file to allow log output (and fix lint error)

---
+++ esphome/components/hbridge/light/hbridge_light_output.cpp
@@ -0,0 +1,50 @@
+#include "hbridge_light_output.h"
+#include "esphome/components/output/float_output.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace hbridge {
+
+light::LightTraits HBridgeLightOutput::get_traits() {
+  auto traits = light::LightTraits();
+  traits.set_supported_color_modes({light::ColorMode::COLD_WARM_WHITE});
+  traits.set_min_mireds(153);
+  traits.set_max_mireds(500);
+  return traits;
+}
+
+// Component interfacing/overriding from HBridge base class
+void HBridgeLightOutput::setup() {
+  this->direction_a_update_ = false;
+  HBridge::setup();
+
+  // Force "fast" decay mode, lights do not support slow decay
+  HBridge::set_hbridge_decay_mode(CurrentDecayMode::FAST);
+}
+
+void HBridgeLightOutput::loop() {
+  // This method runs every loop. This is a best-effort situation.
+
+  // Alternate updating/flashing each light direction
+  if (!this->direction_a_update_) {  // First LED Direction
+    HBridge::set_output_state_(
+        HBridgeMode::DIRECTION_A,
+        this->light_direction_a_duty_);  // Use protected function to prevent a flood of debug prints
+    this->direction_a_update_ = true;
+  } else {  // Second LED Direction
+    HBridge::set_output_state_(
+        HBridgeMode::DIRECTION_B,
+        this->light_direction_b_duty_);  // Use protected function to prevent a flood of debug prints
+    this->direction_a_update_ = false;
+  }
+
+  HBridge::loop();
+}
+
+void HBridgeLightOutput::write_state(light::LightState *state) {
+  state->current_values_as_cwww(&this->light_direction_a_duty_, &this->light_direction_b_duty_, false);
+  ESP_LOGD("HBridgeLight", "New light state A: %f B: %f", this->light_direction_a_duty_, this->light_direction_b_duty_);
+}
+
+}  // namespace hbridge
+}  // namespace esphome
--- esphome/components/hbridge/light/hbridge_light_output.h
+++ esphome/components/hbridge/light/hbridge_light_output.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/output/float_output.h"
 #include "esphome/components/light/light_output.h"
-#include "esphome/core/log.h"
 #include "esphome/components/hbridge/hbridge.h"
 
 namespace esphome {
@@ -12,47 +10,11 @@ namespace hbridge {
 // Using PollingComponent as the updates are more consistent and reduces flickering
 class HBridgeLightOutput : public HBridge, public light::LightOutput {
  public:
-  light::LightTraits get_traits() override {
-    auto traits = light::LightTraits();
-    traits.set_supported_color_modes({light::ColorMode::COLD_WARM_WHITE});
-    traits.set_min_mireds(153);
-    traits.set_max_mireds(500);
-    return traits;
-  }
+  light::LightTraits get_traits() override;
+  void setup() override;
+  void loop() override;
 
-  // Component interfacing/overriding from HBridge base class
-  void setup() override {
-    this->direction_a_update_ = false;
-    HBridge::setup();
-
-    // Force "fast" decay mode, lights do not support slow decay
-    HBridge::set_hbridge_decay_mode(CurrentDecayMode::FAST);
-  }
-
-  void loop() override {
-    // This method runs every loop. This is a best-effort situation.
-
-    // Alternate updating/flashing each light direction
-    if (!this->direction_a_update_) {  // First LED Direction
-      HBridge::set_output_state_(
-          HBridgeMode::DIRECTION_A,
-          this->light_direction_a_duty_);  // Use protected function to prevent a flood of debug prints
-      this->direction_a_update_ = true;
-    } else {  // Second LED Direction
-      HBridge::set_output_state_(
-          HBridgeMode::DIRECTION_B,
-          this->light_direction_b_duty_);  // Use protected function to prevent a flood of debug prints
-      this->direction_a_update_ = false;
-    }
-
-    HBridge::loop();
-  }
-
-  void write_state(light::LightState *state) override {
-    state->current_values_as_cwww(&this->light_direction_a_duty_, &this->light_direction_b_duty_, false);
-    ESP_LOGD("HBridgeLight", "New light state A: %f B: %f", this->light_direction_a_duty_,
-             this->light_direction_b_duty_);
-  }
+  void write_state(light::LightState *state) override;
 
  protected:
   float light_direction_a_duty_ = 0;
