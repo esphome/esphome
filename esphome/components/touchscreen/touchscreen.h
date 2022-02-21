99eb0638e7af62ef22a68e67fbc9b9ac9a220ae6
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-02-22 07:13:42 +1300

    Remove accidental paste

--- esphome/components/touchscreen/touchscreen.h
+++ esphome/components/touchscreen/touchscreen.h
@@ -44,7 +44,6 @@ class Touchscreen {
   void register_listener(TouchListener *listener) { this->touch_listeners_.push_back(listener); }
 
  protected:
-  void touch(TouchPoint tp);
   /// Call this function to send touch points to the `on_touch` listener and the binary_sensors.
   void send_touch_(TouchPoint tp);
 
