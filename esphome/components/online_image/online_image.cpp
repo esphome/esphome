ae909417f28901f0972abc45707c8a40b8b3fd58
Guillem Pagès Gassull <git@guillempages.cat>
2022-02-27 18:15:39 +0100

    Split decoding into an own class
    Add an abstract decoder, to be able to support different formats

--- esphome/components/online_image/online_image.cpp
+++ esphome/components/online_image/online_image.cpp
@@ -1,5 +1,6 @@
 #include "online_image.h"
 #include "esphome/core/color.h"
+#include "esphome/core/log.h"
 
 #ifdef USE_ESP32
 #include <HTTPClient.h>
@@ -11,91 +12,53 @@
 #endif
 #endif
 
-#include "pngle.h"
+#include <memory>
+
+static const char *const TAG = "online_image";
+
+#ifdef ONLINE_IMAGE_PNG_SUPPORT
+#include "png_image.h"
+#endif
 
 namespace esphome {
 namespace online_image {
 
 using display::DisplayBuffer;
 
-struct Context {
-  int x;
-  int y;
-  uint16_t w;
-  uint16_t h;
-  DisplayBuffer* display;
-};
-
-void drawCallback(pngle_t *pngle, uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint8_t rgba[4]) {
-    Context* context = (Context*)pngle_get_user_data(pngle);
-  Color color(rgba[0], rgba[1], rgba[2], rgba[3]);
-
-  float scale_x = 1;
-  float scale_y = 1;
-  uint32_t width = pngle_get_width(pngle);
-  uint32_t height = pngle_get_height(pngle);
-  if (width && width <= context->w) {
-    scale_x = context->w / width;
-  }
-  if (height && height <= context->h) {
-    scale_y = context->h / height;
-  }
-
-  if (rgba[3]) {
-    x = context->x + ceil(x * scale_x);
-    y = context->y + ceil(y * scale_y);
-    w = ceil(w * scale_x);
-    h = ceil(h * scale_y);
-    context->display->filled_rectangle(x, y, w, h, color);
-  }
-}
 
 void OnlineImage::draw(int x, int y, DisplayBuffer* display) {
   HTTPClient http;
+
+
+  std::unique_ptr<ImageDecoder> decoder;
+
+  #ifdef ONLINE_IMAGE_PNG_SUPPORT
+  if (format == ImageFormat::PNG) {
+    decoder = esphome::make_unique<PngDecoder>(display);
+  }
+  #endif // ONLINE_IMAGE_PNG_SUPPORT
+
+  if (!decoder) {
+    ESP_LOGE(TAG, "Could not instantiate decoder. Image format unsupported.");
+    return;
+  }
 
   http.begin(url);
 
   int httpCode = http.GET();
   if (httpCode != HTTP_CODE_OK) {
     http.end();
-    return ;
+    return;
   }
-
   WiFiClient *stream = http.getStreamPtr();
 
-  pngle_t *pngle = pngle_new();
-  Context context_data {.x=x, .y=y, .w=width, .h=height, .display=display};
-  pngle_set_user_data(pngle, &context_data);
-  pngle_set_draw_callback(pngle, drawCallback);
+  decoder->setOffset(x, y);
 
-  uint8_t buf[2048];
-  int remain = 0;
-  while (http.connected()) {
-    size_t size = stream->available();
-    if (!size) { delay(1); continue; }
+  decoder->prepare(stream);
+  int size = decoder->decode(http, stream);
 
-    if (size > sizeof(buf) - remain) {
-      size = sizeof(buf) - remain;
-    }
-
-    int len = stream->readBytes(buf + remain, size);
-    if (len > 0) {
-      int fed = pngle_feed(pngle, buf, remain + len);
-      if (fed < 0) {
-        break;
-      }
-
-      remain = remain + len - fed;
-      if (remain > 0) memmove(buf, buf + fed, remain);
-    } else {
-      delay(1);
-    }
-  }
-
-  pngle_destroy(pngle);
 
   http.end();
-
 }
 
 } // namespace online_image
