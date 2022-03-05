6fc0cce5da459bad7b104178247440d89d33854b
Quentin Smith <quentin@mit.edu>
2022-03-05 03:04:30 -0500

    Make text templatable

--- esphome/components/display/widgets.h
+++ esphome/components/display/widgets.h
@@ -26,17 +26,36 @@ class WidgetContainer : public Widget {
     children_ = children;
   };
  protected:
-  std::vector<Widget *> children;
+  std::vector<Widget *> children_;
 };
 
 class Horizontal : public WidgetContainer {
+ public:
   virtual int get_width();
 
   virtual void draw(DisplayBuffer* it, int x1, int y1, int width, int height);
 };
 
+class Vertical : public WidgetContainer {
+ public:
+   virtual int get_height();
+
+   virtual void draw(DisplayBuffer* it, int x1, int y1, int width, int height);
+ };
+
  class Text : public Widget {
-   
+ public:
+   TEMPLATABLE_VALUE(const char*, text);
+   void set_font(Font* font);
+
+   virtual int get_width();
+   virtual int get_height();
+
+   virtual void draw(DisplayBuffer* it, int x1, int y1, int width, int height);
+ protected:
+   const char* text_;
+   Font* font_;
+ }
 
 }  // namespace display
 }  // namespace esphome
