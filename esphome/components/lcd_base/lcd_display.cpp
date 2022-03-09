0ab1c01af768ba9afc13c7e9627c6370bf9a76af
Andrew J.Swan <ajs.blackhole@gmail.com>
2022-03-09 11:00:56 +0200

    Added a function to load custom characters (fix clang format)

--- esphome/components/lcd_base/lcd_display.cpp
+++ esphome/components/lcd_base/lcd_display.cpp
@@ -78,7 +78,6 @@ void LCDDisplay::setup() {
 
   this->command_(LCD_DISPLAY_COMMAND_RETURN_HOME);
   delay(2);  // 1.52ms
-
 }
 
 float LCDDisplay::get_setup_priority() const { return setup_priority::PROCESSOR; }
@@ -162,9 +161,9 @@ void LCDDisplay::strftime(uint8_t column, uint8_t row, const char *format, time:
 void LCDDisplay::strftime(const char *format, time::ESPTime time) { this->strftime(0, 0, format, time); }
 #endif
 void LCDDisplay::loadchar(uint8_t location, uint8_t charmap[]) {
-  location &= 0x7; // we only have 8 locations 0-7
+  location &= 0x7;  // we only have 8 locations 0-7
   this->command_(LCD_DISPLAY_COMMAND_SET_CGRAM_ADDR | (location << 3));
-  for (int i=0; i<8; i++) {
+  for (int i = 0; i < 8; i++) {
     this->send(charmap[i], true);
   }
 }
