30f929570650b0414022c3ce6c77588cbef32745
Niclas Larsson <niclas@edgesystems.se>
2022-03-25 10:22:09 +0100

    static variable should be uppercase

--- esphome/components/shelly_dimmer/stm32flash.cpp
+++ esphome/components/shelly_dimmer/stm32flash.cpp
@@ -355,7 +355,7 @@ stm32_err_t stm32_mass_erase(const stm32_t *stm) {
 
 template<typename T> std::unique_ptr<T[], void (*)(T *memory)> malloc_array_raii(size_t size) {
   // Could be constexpr in c++17
-  static const auto deletor = [](T *memory) { free(memory); };
+  static const auto DELETOR = [](T *memory) { free(memory); };
   return std::unique_ptr<T[], decltype(deletor)>{static_cast<T *>(malloc(size)), deletor};
 }
 
