8b0d298e8fe2f31a65d16cf5a6e393b4133b28b6
Stanislav Meduna <stano@meduna.org>
2022-03-25 18:45:00 +0100

    add the touch gui component

---
+++ esphome/components/touch_gui/__init__.py
@@ -0,0 +1,158 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import core, automation
+from esphome.const import CONF_ID, CONF_TRIGGER_ID
+from esphome.components import binary_sensor, display, font
+from esphome.helpers import color
+
+AUTO_LOAD = ["binary_sensor"]
+
+touch_gui_ns = cg.esphome_ns.namespace("touch_gui")
+
+CONF_DISPLAY_ID = "display_id"
+CONF_X = "x"
+CONF_Y = "y"
+CONF_TOUCHED = "touched"
+CONF_BUTTON_COLORS = "button_colors"
+CONF_BACKGROUND = "background"
+CONF_ACTIVE_BACKGROUND = "active_background"
+CONF_FOREGROUND = "foreground"
+CONF_ACTIVE_FOREGROUND = "active_foreground"
+CONF_BORDER = "border"
+CONF_BUTTON_FONT = "button_font"
+CONF_BUTTON = "button"
+CONF_ON_UPDATE = "on_update"
+
+TouchGUIComponent = touch_gui_ns.class_("TouchGUIComponent", cg.PollingComponent)
+TouchGUITouchAction = touch_gui_ns.class_("TouchGUITouchAction", automation.Action)
+TouchGUIActivateButtonAction = touch_gui_ns.class_(
+    "TouchGUIActivateButtonAction", automation.Action
+)
+TouchGuiButton = touch_gui_ns.class_(
+    "TouchGUIButton", binary_sensor.BinarySensor, cg.Component
+)
+TouchGuiUpdateTrigger = touch_gui_ns.class_(
+    "TouchGuiUpdateTrigger", automation.Trigger.template()
+)
+
+MULTI_CONF = True
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(TouchGUIComponent),
+        cv.GenerateID(CONF_DISPLAY_ID): cv.use_id(display.DisplayBuffer),
+        cv.Optional(CONF_BUTTON_COLORS): cv.Schema(
+            {
+                cv.Optional(CONF_BACKGROUND): cv.use_id(color),
+                cv.Optional(CONF_ACTIVE_BACKGROUND): cv.use_id(color),
+                cv.Optional(CONF_FOREGROUND): cv.use_id(color),
+                cv.Optional(CONF_ACTIVE_FOREGROUND): cv.use_id(color),
+                cv.Optional(CONF_BORDER): cv.use_id(color),
+            }
+        ),
+        cv.Optional(CONF_BUTTON_FONT): cv.use_id(font),
+        cv.Optional(CONF_ON_UPDATE): automation.validate_automation(
+            {
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(TouchGuiUpdateTrigger),
+            }
+        ),
+    }
+).extend(cv.polling_component_schema("50ms"))
+
+
+@automation.register_action(
+    "touch_gui.touch",
+    TouchGUITouchAction,
+    cv.Schema(
+        {
+            cv.GenerateID(CONF_ID): cv.use_id(TouchGUIComponent),
+            cv.Optional(CONF_X, default=0): cv.templatable(cv.positive_int),
+            cv.Optional(CONF_Y, default=0): cv.templatable(cv.positive_int),
+            cv.Optional(CONF_TOUCHED, default=True): cv.templatable(cv.boolean),
+        }
+    ),
+)
+def touch_gui_touch_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+
+    gui = yield cg.get_variable(config[CONF_ID])
+    cg.add(var.set_gui(gui))
+
+    if isinstance(config[CONF_X], core.Lambda):
+        template_ = yield cg.templatable(config[CONF_X], args, int)
+        cg.add(var.set_x(template_))
+    else:
+        cg.add(var.set_x(config[CONF_X]))
+
+    if isinstance(config[CONF_Y], core.Lambda):
+        template_ = yield cg.templatable(config[CONF_Y], args, int)
+        cg.add(var.set_y(template_))
+    else:
+        cg.add(var.set_y(config[CONF_Y]))
+
+    if isinstance(config[CONF_TOUCHED], core.Lambda):
+        template_ = yield cg.templatable(config[CONF_TOUCHED], args, bool)
+        cg.add(var.set_touched(template_))
+    else:
+        cg.add(var.set_touched(config[CONF_TOUCHED]))
+
+    yield var
+
+
+@automation.register_action(
+    "touch_gui.activate_button",
+    TouchGUIActivateButtonAction,
+    cv.maybe_simple_value(
+        {
+            cv.GenerateID(CONF_ID): cv.use_id(TouchGUIComponent),
+            cv.Required(CONF_BUTTON): cv.templatable(cv.use_id(TouchGuiButton)),
+        },
+        key=CONF_BUTTON,
+    ),
+)
+def touch_gui_activate_button_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+
+    gui = yield cg.get_variable(config[CONF_ID])
+    cg.add(var.set_gui(gui))
+
+    if isinstance(config[CONF_BUTTON], core.Lambda):
+        template_ = yield cg.templatable(
+            config[CONF_BUTTON], args, TouchGuiButton.operator("ptr")
+        )
+        cg.add(var.set_button(template_))
+    else:
+        btn = yield cg.get_variable(config[CONF_BUTTON])
+        cg.add(var.set_button(btn))
+
+    yield var
+
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    disp = yield cg.get_variable(config[CONF_DISPLAY_ID])
+    cg.add(var.set_display(disp))
+    if CONF_BUTTON_COLORS in config:
+        colors = config[CONF_BUTTON_COLORS]
+        if CONF_BACKGROUND in colors:
+            col = yield cg.get_variable(colors[CONF_BACKGROUND])
+            cg.add(var.set_button_background_color(col))
+        if CONF_ACTIVE_BACKGROUND in colors:
+            col = yield cg.get_variable(colors[CONF_ACTIVE_BACKGROUND])
+            cg.add(var.set_button_active_background_color(col))
+        if CONF_FOREGROUND in colors:
+            col = yield cg.get_variable(colors[CONF_FOREGROUND])
+            cg.add(var.set_button_foreground_color(col))
+        if CONF_ACTIVE_FOREGROUND in colors:
+            col = yield cg.get_variable(colors[CONF_ACTIVE_FOREGROUND])
+            cg.add(var.set_button_active_foreground_color(col))
+        if CONF_BORDER in colors:
+            col = yield cg.get_variable(colors[CONF_BORDER])
+            cg.add(var.set_button_border_color(col))
+    if CONF_BUTTON_FONT in config:
+        fnt = yield cg.get_variable(config[CONF_BUTTON_FONT])
+        cg.add(var.set_button_font(fnt))
+    for conf in config.get(CONF_ON_UPDATE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        yield automation.build_automation(trigger, [], conf)
---
+++ esphome/components/touch_gui/gui.cpp
@@ -0,0 +1,166 @@
+#include "gui.h"
+#include "esphome/core/log.h"
+#include <algorithm>
+
+namespace esphome {
+namespace touch_gui {
+
+static const char *TAG = "touch_gui";
+
+void TouchGUIComponent::dump_config() {
+  ESP_LOGCONFIG(TAG, "Configured");
+  ESP_LOGCONFIG(TAG, "  Buttons: %zu", buttons_.size());
+  ESP_LOGCONFIG(TAG, "  Radio groups: %zu", groups_.size());
+}
+
+void TouchGUIComponent::add_on_update_callback(std::function<void()> &&callback) {
+  this->update_callback_.add(std::move(callback));
+}
+
+void TouchGUIComponent::setup() {
+  // Ensure exactly once activated radio group button in every group
+  RadioGroupMap activated;
+  static const ButtonPtrSet EMPTY_SET;
+
+  // Build a map of groups
+  for (auto *b : this->buttons_) {
+    if (b->get_type() == TOUCH_GUI_BUTTON_TYPE_RADIO) {
+      auto it = this->groups_.insert(std::pair<int, ButtonPtrSet>(b->get_radio_group(), EMPTY_SET)).first;
+      it->second.insert(b);
+      it = activated.insert(std::pair<int, ButtonPtrSet>(b->get_radio_group(), EMPTY_SET)).first;
+      if (b->get_initial())
+        it->second.insert(b);
+    }
+  }
+
+  for (const auto &a : activated) {
+    // If none is active, activate the first one
+    if (a.second.empty()) {
+      auto &g = this->groups_[a.first];
+      if (!g.empty()) {
+        (*g.begin())->set_initial(true);
+      }
+      // If more than one is active, leave the first one
+    } else if (a.second.size() > 1) {
+      auto it = a.second.begin();
+      ++it;
+      for (; it != a.second.end(); ++it)
+        (*it)->set_initial(false);
+    }
+  }
+}
+
+void TouchGUIComponent::update() {
+  bool upd = false;
+  for (auto *b : this->buttons_) {
+    b->update();
+    upd |= b->should_update();
+  }
+
+  if (upd)
+    this->update_callback_.call();
+}
+
+void TouchGUIComponent::draw() {
+  for (auto *b : this->buttons_)
+    if (b->is_visible())
+      b->draw();
+}
+
+void TouchGUIComponent::touch(int x, int y, bool touched) {
+  if (touched) {
+    for (auto *b : this->buttons_)
+      if (b->is_interacted(x, y))
+        activate(b);
+  }
+}
+
+void TouchGUIComponent::activate(TouchGUIButton *button) {
+  switch (button->get_type()) {
+    case TOUCH_GUI_BUTTON_TYPE_TOGGLE:
+      button->publish_state(!button->state);
+      break;
+    case TOUCH_GUI_BUTTON_TYPE_RADIO:
+      if (!button->state) {
+        // Find the radio group, deactivate the previous one, activate the new one
+        auto it = this->groups_.find(button->get_radio_group());
+        if (it != this->groups_.end()) {
+          for (auto *b : it->second) {
+            if (b->state) {
+              b->publish_state(false);
+            }
+          }
+
+          button->publish_state(true);
+        }
+      }
+      break;
+    case TOUCH_GUI_BUTTON_TYPE_MOMENTARY:
+    case TOUCH_GUI_BUTTON_TYPE_AREA:
+      button->publish_state(true);
+      break;
+    default:
+      break;
+  }
+}
+
+void TouchGUIButton::setup() {
+  publish_initial_state(this->initial_);
+
+  // If momentary we need to know of the activation to schedule the release
+  switch (this->type_) {
+    case TOUCH_GUI_BUTTON_TYPE_MOMENTARY:
+    case TOUCH_GUI_BUTTON_TYPE_AREA:
+      add_on_state_callback([this](bool x) {
+        if (x)
+          this->activation_time_ = millis();
+      });
+      break;
+    default:
+      break;
+  }
+
+  // Mark the changed so the display can be asked to update
+  add_on_state_callback([this](bool x) { this->update_requested_ = true; });
+}
+
+void TouchGUIButton::update() {
+  // If this is a momentary button, return it after its touch time expires
+  switch (this->type_) {
+    case TOUCH_GUI_BUTTON_TYPE_MOMENTARY:
+    case TOUCH_GUI_BUTTON_TYPE_AREA:
+      if (this->state && millis() - this->activation_time_ >= this->touch_time_) {
+        publish_state(false);
+        this->update_requested_ = true;
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+void TouchGUIButton::draw() {
+  if (this->writer_.has_value())
+    (this->writer_.value())(*this);
+  else {
+    if (this->type_ != TOUCH_GUI_BUTTON_TYPE_AREA) {
+      get_display()->rectangle(this->x_min_, this->y_min_, this->x_max_ - this->x_min_ + 1,
+                               this->y_max_ - this->y_min_ + 1, get_border_color());
+      get_display()->filled_rectangle(this->x_min_ + 1, this->y_min_ + 1, this->x_max_ - this->x_min_ - 1,
+                                      this->y_max_ - this->y_min_ - 1, get_background_color_to_use());
+      if (get_font() != nullptr && !this->text_.value().empty())
+        get_display()->print(get_x_center(), get_y_center(), get_font(), get_foreground_color_to_use(),
+                             display::TextAlign::CENTER, this->text_.value().c_str());
+    }
+  }
+}
+
+bool TouchGUIButton::is_visible() const {
+  // The button is visible if either the list of the pages is empty (meaning all)
+  // or the displayed page matches
+  return (pages_.empty() ||
+          std::find(pages_.begin(), pages_.end(), parent_->get_display()->get_active_page()) != pages_.end());
+}
+
+}  // namespace touch_gui
+}  // namespace esphome
