050f01f3d12db5ce79a6248652e9ce249416251d
functionpointer <suspendfunction@gmail.com>
2022-04-03 23:38:59 +0200

    hydreon_rgxx: Default update interval 60s
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/hydreon_rgxx/sensor.py
+++ esphome/components/hydreon_rgxx/sensor.py
@@ -94,7 +94,7 @@ CONFIG_SCHEMA = cv.All(
             ),
         }
     )
-    .extend(cv.polling_component_schema("1s"))
+    .extend(cv.polling_component_schema("60s"))
     .extend(uart.UART_DEVICE_SCHEMA),
     _validate,
 )
