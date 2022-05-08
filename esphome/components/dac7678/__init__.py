c5c160be511d25cf86c6fd633507c294792a6148
Nick B <nick.blommen1@gmail.com>
2022-05-08 14:16:33 +0200

    Edited internal reference config name

--- esphome/components/dac7678/__init__.py
+++ esphome/components/dac7678/__init__.py
@@ -9,13 +9,13 @@ MULTI_CONF = True
 
 dac7678_ns = cg.esphome_ns.namespace("dac7678")
 DAC7678Output = dac7678_ns.class_("DAC7678Output", cg.Component, i2c.I2CDevice)
-CONF_DAC7678_INTERNAL_REFERENCE = "dac7678_internal_reference"
+CONF_INTERNAL_REFERENCE = "internal_reference"
 
 CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(DAC7678Output),
-            cv.Optional(CONF_DAC7678_INTERNAL_REFERENCE, default=False): cv.boolean,
+            cv.Optional(CONF_INTERNAL_REFERENCE, default=False): cv.boolean,
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -26,6 +26,6 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cg.add(var.set_internal_reference(config[CONF_DAC7678_INTERNAL_REFERENCE]))
+    cg.add(var.set_internal_reference(config[CONF_INTERNAL_REFERENCE]))
     await i2c.register_i2c_device(var, config)
     return var
