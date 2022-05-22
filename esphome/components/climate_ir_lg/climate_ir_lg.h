3f4d9ae496ded563e120299e390bc6e3bc56e150
Daniel D'Abate <danieldabate@gmail.com>
2022-05-22 18:48:58 +0200

    climate_ir_lg: * Add fan only mode * Add all on modes (cool, heat, AI, fan, dry)

--- esphome/components/climate_ir_lg/climate_ir_lg.h
+++ esphome/components/climate_ir_lg/climate_ir_lg.h
@@ -12,9 +12,9 @@ const uint8_t TEMP_MAX = 30;  // Celsius
 class LgIrClimate : public climate_ir::ClimateIR {
  public:
   LgIrClimate()
-      : climate_ir::ClimateIR(TEMP_MIN, TEMP_MAX, 1.0f, true, false,
-                              {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
-                               climate::CLIMATE_FAN_HIGH},
+      : climate_ir::ClimateIR(TEMP_MIN, TEMP_MAX, 1.0f, true, true,
+                              {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,	
+                               climate::CLIMATE_FAN_HIGH},	
                               {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL}) {}
 
   /// Override control to change settings of the climate device.
