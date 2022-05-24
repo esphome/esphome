a68aa4e709b0291fec3d9593ddef663c2ad35261
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2022-05-24 11:55:30 +0200

    Update adc_sensor.cpp

--- esphome/components/adc/adc_sensor.cpp
+++ esphome/components/adc/adc_sensor.cpp
@@ -18,7 +18,8 @@ static const char *const TAG = "adc";
 
 // 13bit for S2, and 12bit for all other esp32 variants
 #ifdef USE_ESP32
-#ifndef ADC_WIDTH_BIT_DEFAULT
+#include "soc/soc_caps.h"
+/*#ifndef ADC_WIDTH_BIT_DEFAULT
 static const adc_bits_width_t ADC_WIDTH_BIT_DEFAULT = static_cast<adc_bits_width_t>(ADC_WIDTH_MAX - 1);
 #endif
 
@@ -29,7 +30,7 @@ static const int SOC_ADC_MAX_BITWIDTH = 12;
 static const int SOC_ADC_MAX_BITWIDTH = 13;
 #endif
 #endif
-
+*/
 static const int ADC_MAX = (1 << SOC_ADC_MAX_BITWIDTH) - 1;    // 4095 (12 bit) or 8191 (13 bit)
 static const int ADC_HALF = (1 << SOC_ADC_MAX_BITWIDTH) >> 1;  // 2048 (12 bit) or 4096 (13 bit)
 #endif
