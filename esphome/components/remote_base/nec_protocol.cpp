dea37730080d018e2bdb0ba24e98dbab6d278803
Eric van Blokland <mail@ericvanblokland.nl>
2022-05-28 15:33:35 +0200

    The NEC IR protocol uses 8bit addresses and commands
    The other bits are inverses for error checking.
    
    This patch will gracefully accept 16bit addresses and commands for encoding but will emit a warning if you do. The decoder now only sets the proper 8bit address and command.
    
    This patch will break existing implementations which expect to receive 16bit addresses and commands.
    
    This patch will break existing implementations that rely on sending or receiving "made up" commands that fail the error checking.

--- esphome/components/remote_base/nec_protocol.cpp
+++ esphome/components/remote_base/nec_protocol.cpp
@@ -16,8 +16,16 @@ void NECProtocol::encode(RemoteTransmitData *dst, const NECData &data) {
   dst->reserve(68);
   dst->set_carrier_frequency(38000);
 
+  if (data.address > 0xFF || data.command > 0xFF) {
+    ESP_LOGW(TAG,
+             "Your address (0x%04X) or command (0x%04X) is 16bits instead of 8bits, which includes the inverses. "
+             "Your command will be sent, but you might want to fix this, especially your receiver codes "
+             "which are not backwards compatible.",
+             data.address, data.command);
+  }
+
   dst->item(HEADER_HIGH_US, HEADER_LOW_US);
-  for (uint16_t mask = 1; mask; mask <<= 1) {
+  for (uint8_t mask = 1; mask; mask <<= 1) {
     if (data.address & mask) {
       dst->item(BIT_HIGH_US, BIT_ONE_LOW_US);
     } else {
@@ -25,11 +33,27 @@ void NECProtocol::encode(RemoteTransmitData *dst, const NECData &data) {
     }
   }
 
-  for (uint16_t mask = 1; mask; mask <<= 1) {
+  for (uint8_t mask = 1; mask; mask <<= 1) {
+    if (data.address & mask) {
+      dst->item(BIT_HIGH_US, BIT_ZERO_LOW_US);
+    } else {
+      dst->item(BIT_HIGH_US, BIT_ONE_LOW_US);
+    }
+  }
+
+  for (uint8_t mask = 1; mask; mask <<= 1) {
     if (data.command & mask) {
       dst->item(BIT_HIGH_US, BIT_ONE_LOW_US);
     } else {
       dst->item(BIT_HIGH_US, BIT_ZERO_LOW_US);
+    }
+  }
+
+  for (uint8_t mask = 1; mask; mask <<= 1) {
+    if (data.command & mask) {
+      dst->item(BIT_HIGH_US, BIT_ZERO_LOW_US);
+    } else {
+      dst->item(BIT_HIGH_US, BIT_ONE_LOW_US);
     }
   }
 
@@ -40,10 +64,13 @@ optional<NECData> NECProtocol::decode(RemoteReceiveData src) {
       .address = 0,
       .command = 0,
   };
+  uint8_t inverse_address{0};
+  uint8_t inverse_command{0};
+
   if (!src.expect_item(HEADER_HIGH_US, HEADER_LOW_US))
     return {};
 
-  for (uint16_t mask = 1; mask; mask <<= 1) {
+  for (uint8_t mask = 1; mask; mask <<= 1) {
     if (src.expect_item(BIT_HIGH_US, BIT_ONE_LOW_US)) {
       data.address |= mask;
     } else if (src.expect_item(BIT_HIGH_US, BIT_ZERO_LOW_US)) {
@@ -53,7 +80,17 @@ optional<NECData> NECProtocol::decode(RemoteReceiveData src) {
     }
   }
 
-  for (uint16_t mask = 1; mask; mask <<= 1) {
+  for (uint8_t mask = 1; mask; mask <<= 1) {
+    if (src.expect_item(BIT_HIGH_US, BIT_ONE_LOW_US)) {
+      inverse_address |= mask;
+    } else if (src.expect_item(BIT_HIGH_US, BIT_ZERO_LOW_US)) {
+      inverse_address &= ~mask;
+    } else {
+      return {};
+    }
+  }
+
+  for (uint8_t mask = 1; mask; mask <<= 1) {
     if (src.expect_item(BIT_HIGH_US, BIT_ONE_LOW_US)) {
       data.command |= mask;
     } else if (src.expect_item(BIT_HIGH_US, BIT_ZERO_LOW_US)) {
@@ -63,11 +100,26 @@ optional<NECData> NECProtocol::decode(RemoteReceiveData src) {
     }
   }
 
+  for (uint8_t mask = 1; mask; mask <<= 1) {
+    if (src.expect_item(BIT_HIGH_US, BIT_ONE_LOW_US)) {
+      inverse_command |= mask;
+    } else if (src.expect_item(BIT_HIGH_US, BIT_ZERO_LOW_US)) {
+      inverse_command &= ~mask;
+    } else {
+      return {};
+    }
+  }
+
   src.expect_mark(BIT_HIGH_US);
+
+  if ((data.address & inverse_address) != 0 || (data.command & inverse_command) != 0) {
+    return {};
+  }
+
   return data;
 }
 void NECProtocol::dump(const NECData &data) {
-  ESP_LOGD(TAG, "Received NEC: address=0x%04X, command=0x%04X", data.address, data.command);
+  ESP_LOGD(TAG, "Received NEC: address=0x%02X, command=0x%02X", data.address, data.command);
 }
 
 }  // namespace remote_base
