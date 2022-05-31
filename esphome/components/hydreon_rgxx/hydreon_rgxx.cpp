f0fbeae393b8408def076c593790a74e83b63cd5
Wolfgang Tremmel <wtremmel@users.noreply.github.com>
2022-05-31 08:49:44 +0200

    Changed patch to fit with ESPHOME best practice

--- esphome/components/hydreon_rgxx/hydreon_rgxx.cpp
+++ esphome/components/hydreon_rgxx/hydreon_rgxx.cpp
@@ -195,7 +195,7 @@ void HydreonRGxxComponent::process_line_() {
       if (n == std::string::npos) {
         continue;
       }
-      double data = strtof(this->buffer_.substr(n + strlen(PROTOCOL_NAMES[i])).c_str(), nullptr);
+      float data = parse_number<float>(this->buffer_.substr(n + strlen(PROTOCOL_NAMES[i]))).value();
       this->sensors_[i]->publish_state(data);
       ESP_LOGD(TAG, "Received %s: %f", PROTOCOL_NAMES[i], this->sensors_[i]->get_raw_state());
       this->sensors_received_ |= (1 << i);
