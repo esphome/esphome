eb9a4720cd7ac988d47db9696e46c195f7c28174
Zimmermann Zsolt <16361576+cinadr@users.noreply.github.com>
2022-06-07 08:14:41 +0200

    Correcting number ValueRangeTrigger automation key calculation

--- esphome/components/number/automation.cpp
+++ esphome/components/number/automation.cpp
@@ -6,8 +6,8 @@ namespace number {
 
 static const char *const TAG = "number.automation";
 
-void ValueRangeTrigger::setup() {
-  this->rtc_ = global_preferences->make_preference<bool>(this->parent_->get_object_id_hash());
+void ValueRangeTrigger::setup() {                        
+  this->rtc_ = global_preferences->make_preference<bool>(139423563UL ^this->parent_->get_object_id_hash());
   bool initial_state;
   if (this->rtc_.load(&initial_state)) {
     this->previous_in_range_ = initial_state;
