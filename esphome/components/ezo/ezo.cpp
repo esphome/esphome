b2ac21523249b5e5f7a0906813074a2d5081107d
Gilles van den Hoven <gilles@webunity.nl>
2022-06-14 21:30:41 +0200

    Attempt to fix final failing check

--- esphome/components/ezo/ezo.cpp
+++ esphome/components/ezo/ezo.cpp
@@ -30,7 +30,7 @@ void EZOSensor::update() {
     }
 
     if (!found) {
-      EzoCommand *ezo_command = new EzoCommand;
+      gsl::owner<EzoCommand *> ezo_command = new EzoCommand;
       ezo_command->command = "R";
       ezo_command->command_type = EzoCommandType::EZO_READ;
       ezo_command->delay_ms = 900;
