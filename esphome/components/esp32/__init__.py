b87be80e54ca9a6678ed065a0b64f098bd642ab6
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-06-15 11:19:35 +1200

    Dont bump espidf

--- esphome/components/esp32/__init__.py
+++ esphome/components/esp32/__init__.py
@@ -138,11 +138,11 @@ ARDUINO_PLATFORM_VERSION = cv.Version(3, 5, 0)
 # The default/recommended esp-idf framework version
 #  - https://github.com/espressif/esp-idf/releases
 #  - https://api.registry.platformio.org/v3/packages/platformio/tool/framework-espidf
-RECOMMENDED_ESP_IDF_FRAMEWORK_VERSION = cv.Version(4, 4, 1)
+RECOMMENDED_ESP_IDF_FRAMEWORK_VERSION = cv.Version(4, 3, 2)
 # The platformio/espressif32 version to use for esp-idf frameworks
 #  - https://github.com/platformio/platform-espressif32/releases
 #  - https://api.registry.platformio.org/v3/packages/platformio/platform/espressif32
-ESP_IDF_PLATFORM_VERSION = cv.Version(4, 3, 0)
+ESP_IDF_PLATFORM_VERSION = cv.Version(4, 4, 0)
 
 
 def _arduino_check_versions(value):
