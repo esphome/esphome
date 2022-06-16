c974fbbea8b6155c4be47c1117b3aff06c30d196
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-06-16 15:37:14 +1200

    Remove pin stuff

--- esphome/components/es8388/es8388_component.h
+++ esphome/components/es8388/es8388_component.h
@@ -2,7 +2,6 @@
 
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/core/component.h"
-#include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace es8388 {
@@ -10,10 +9,6 @@ namespace es8388 {
 class ES8388Component : public Component, public i2c::I2CDevice {
  public:
   void setup() override;
-  void set_power_pin(GPIOPin *pin) { this->power_pin_ = pin; }
-
- protected:
-  GPIOPin *power_pin_;
 };
 
 }  // namespace es8388
