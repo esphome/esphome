33a843bf619d5a5fbab9b01bfac2f5d64ce979b9
Marcel Hoppe <m.hoppe@newsaktuell.de>
2022-06-16 07:33:11 +0000

    Make clang linter happy

--- esphome/components/wl_134/wl_134.h
+++ esphome/components/wl_134/wl_134.h
@@ -7,57 +7,57 @@
 #include "esphome/components/uart/uart.h"
 
 namespace esphome {
-  namespace wl_134 {
+namespace wl_134 {
 
-    class Wl134Component : public text_sensor::TextSensor, public Component, public uart::UARTDevice {
-      public:
-        enum Rfid134_Error {
-          Rfid134_Error_None,
+class Wl134Component : public text_sensor::TextSensor, public Component, public uart::UARTDevice {
+ public:
+  enum Rfid134Error {
+    RFID134_ERROR_NONE,
 
-          // from library
-          Rfid134_Error_PacketSize = 0x81,
-          Rfid134_Error_PacketEndCodeMissmatch,
-          Rfid134_Error_PacketChecksum,
-          Rfid134_Error_PacketChecksumInvert
-        };
+    // from library
+    RFID134_ERROR_PACKET_SIZE = 0x81,
+    RFID134_ERROR_PACKET_END_CODE_MISSMATCH,
+    RFID134_ERROR_PACKET_CHECKSUM,
+    RFID134_ERROR_PACKET_CHECKSUM_INVERT
+  };
 
-        struct Rfid134Reading {
-          uint16_t country;
-          uint64_t id;
-          bool isData;
-          bool isAnimal;
-          uint16_t reserved0;
-          uint32_t reserved1;
-        };
-        // Nothing really public.
+  struct Rfid134Reading {
+    uint16_t country;
+    uint64_t id;
+    bool isData;
+    bool isAnimal;
+    uint16_t reserved0;
+    uint32_t reserved1;
+  };
+  // Nothing really public.
 
-        // ========== INTERNAL METHODS ==========
-        void setup() override;
-        void loop() override;
-        void dump_config() override;
+  // ========== INTERNAL METHODS ==========
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
 
-        void set_do_reset(bool do_reset) { this->do_reset_ = do_reset; }
+  void set_do_reset(bool do_reset) { this->do_reset_ = do_reset; }
 
-      private:
-        enum DfMp3_Packet {
-          Rfid134_Packet_StartCode,
-          Rfid134_Packet_Id = 1,
-          Rfid134_Packet_Country = 11,
-          Rfid134_Packet_DataFlag = 15,
-          Rfid134_Packet_AnimalFlag = 16,
-          Rfid134_Packet_Reserved0 = 17,
-          Rfid134_Packet_Reserved1 = 21,
-          Rfid134_Packet_CheckSum = 27,
-          Rfid134_Packet_CheckSumInvert = 28,
-          Rfid134_Packet_EndCode = 29,
-          Rfid134_Packet_SIZE
-        };
+ private:
+  enum DfMp3Packet {
+    RFID134_PACKET_START_CODE,
+    RFID134_PACKET_ID = 1,
+    RFID134_PACKET_COUNTRY = 11,
+    RFID134_PACKET_DATA_FLAG = 15,
+    RFID134_PACKET_ANIMAL_FLAG = 16,
+    RFID134_PACKET_RESERVED0 = 17,
+    RFID134_PACKET_RESERVED1 = 21,
+    RFID134_PACKET_CHECKSUM = 27,
+    RFID134_PACKET_CHECKSUM_INVERT = 28,
+    RFID134_PACKET_END_CODE = 29,
+    RFID134_PACKET_SIZE
+  };
 
-        bool do_reset_;
+  bool do_reset_;
 
-        Rfid134_Error read_packet_();
-        uint64_t hex_lsb_ascii_to_uint64_(uint8_t* text, uint8_t textSize);
-    };
+  Rfid134Error read_packet_();
+  uint64_t hex_lsb_ascii_to_uint64_(const uint8_t *text, uint8_t text_size);
+};
 
-  } // namespace hrxl_maxsonar_wr
-} // namespace esphome
+}  // namespace wl_134
+}  // namespace esphome
