ecfc7aeb9b13f9a8fd2885ec03ab1f83ff2872c2
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-06-30 16:09:48 +1200

    Update

--- esphome/components/esp32/__init__.py
+++ esphome/components/esp32/__init__.py
@@ -138,7 +138,7 @@ ARDUINO_PLATFORM_VERSION = cv.Version(3, 5, 0)
 # The default/recommended esp-idf framework version
 #  - https://github.com/espressif/esp-idf/releases
 #  - https://api.registry.platformio.org/v3/packages/platformio/tool/framework-espidf
-RECOMMENDED_ESP_IDF_FRAMEWORK_VERSION = cv.Version(4, 3, 2)
+RECOMMENDED_ESP_IDF_FRAMEWORK_VERSION = cv.Version(4, 4, 1)
 # The platformio/espressif32 version to use for esp-idf frameworks
 #  - https://github.com/platformio/platform-espressif32/releases
 #  - https://api.registry.platformio.org/v3/packages/platformio/platform/espressif32
