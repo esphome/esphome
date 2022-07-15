d40e45a396dddce363b13db842c1ceb57944aadc
Raimond <raimond@home-eisele.de>
2022-07-15 09:03:15 +0200

    Added IR Receiver schema

--- esphome/components/mitsubishi/climate.py
+++ esphome/components/mitsubishi/climate.py
@@ -9,7 +9,7 @@ AUTO_LOAD = ["climate_ir"]
 mitsubishi_ns = cg.esphome_ns.namespace("mitsubishi")
 MitsubishiClimate = mitsubishi_ns.class_("MitsubishiClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend(
+CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(MitsubishiClimate),
     }
