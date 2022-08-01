7b307fd8ef1b799308380fa75b341c1de38b9733
Javier Peletier <jm@epiclabs.io>
2022-08-01 13:29:44 +0200

    fix output switch setup()

--- esphome/components/output/switch/output_switch.cpp
+++ esphome/components/output/switch/output_switch.cpp
@@ -7,7 +7,17 @@ namespace output {
 static const char *const TAG = "output.switch";
 
 void OutputSwitch::dump_config() { LOG_SWITCH("", "Output Switch", this); }
-void OutputSwitch::setup() { Switch::get_initial_state_with_restore_mode(); }
+void OutputSwitch::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up Output Switch '%s'...", this->name_.c_str());
+
+  bool initial_state = Switch::get_initial_state_with_restore_mode();
+
+  if (initial_state) {
+    this->turn_on();
+  } else {
+    this->turn_off();
+  }
+}
 void OutputSwitch::write_state(bool state) {
   if (state) {
     this->output_->turn_on();
