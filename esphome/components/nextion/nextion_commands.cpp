84b1eb13f6fb021713d8c26294952be8fee26758
anatoly-savchenkov <anatoly.savchenkov@gmail.com>
2022-08-07 00:06:27 +0300

    Revert useless chanages

--- esphome/components/nextion/nextion_commands.cpp
+++ esphome/components/nextion/nextion_commands.cpp
@@ -108,9 +108,7 @@ void Nextion::set_component_text_printf(const char *component, const char *forma
 }
 
 // General Nextion
-void Nextion::goto_page(const char *page) {
-  this->add_no_result_to_queue_with_printf_("goto_page", "page %s", page);
-}
+void Nextion::goto_page(const char *page) { this->add_no_result_to_queue_with_printf_("goto_page", "page %s", page); }
 
 void Nextion::set_backlight_brightness(float brightness) {
   if (brightness < 0 || brightness > 1.0) {
