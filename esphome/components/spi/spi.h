8efcf3fdd28635a60ab650f0367d065be675e46a
NP v/d Spek <nielsnl68@gmail.com>
2022-08-08 08:35:09 +0200

    restore test changes.

--- esphome/components/spi/spi.h
+++ esphome/components/spi/spi.h
@@ -16,15 +16,15 @@ namespace esphome {
 namespace spi {
 
 #ifdef USE_ESP32
-//#if !defined(VSPI)
-//#ifdef VSPI_HOST
-//#define VSPI VSPI_HOST
-//#elif defined(SPI2_HOST)
-//#define VSPI SPI2_HOST
-//#else
+#if !defined(VSPI)
+#ifdef VSPI_HOST
+#define VSPI VSPI_HOST
+#elif defined(SPI3_HOST)
+#define VSPI SPI3_HOST
+#else
 #define VSPI 3
-//#endif
-//#endif
+#endif
+#endif
 #endif
 
 /// The bit-order for SPI devices. This defines how the data read from and written to the device is interpreted.
