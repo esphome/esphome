c8bf3d3f54ed531106c26b877c0980fd4eb41a42
Samuel Sieb <samuel@sieb.net>
2022-08-11 18:38:27 -0700

    add units

--- esphome/components/apds9960/__init__.py
+++ esphome/components/apds9960/__init__.py
@@ -15,11 +15,20 @@ CONF_GESTURE_LED_DRIVE = "gesture_led_drive"
 CONF_GESTURE_GAIN = "gesture_gain"
 CONF_GESTURE_WAIT_TIME = "gesture_wait_time"
 
-DRIVE_LEVELS = {100: 0, 50: 1, 25: 2, 12.5: 3}
-PROXIMITY_LEVELS = {1: 0, 2: 1, 4: 2, 8: 3}
-AMBIENT_LEVELS = {1: 0, 4: 1, 16: 2, 64: 3}
-GESTURE_LEVELS = {1: 0, 2: 1, 4: 2, 8: 3}
-GESTURE_WAIT_TIMES = {0: 0, 2.8: 1, 5.6: 2, 8.4: 3, 14: 4, 22.4: 5, 30.8: 6, 39.2: 7}
+DRIVE_LEVELS = {"100ma": 0, "50ma": 1, "25ma": 2, "12.5ma": 3}
+PROXIMITY_LEVELS = {"1x": 0, "2x": 1, "4x": 2, "8x": 3}
+AMBIENT_LEVELS = {"1x": 0, "4x": 1, "16x": 2, "64x": 3}
+GESTURE_LEVELS = {"1x": 0, "2x": 1, "4x": 2, "8x": 3}
+GESTURE_WAIT_TIMES = {
+    "0ms": 0,
+    "2.8ms": 1,
+    "5.6ms": 2,
+    "8.4ms": 3,
+    "14ms": 4,
+    "22.4ms": 5,
+    "30.8ms": 6,
+    "39.2ms": 7,
+}
 
 apds9960_nds = cg.esphome_ns.namespace("apds9960")
 APDS9960 = apds9960_nds.class_("APDS9960", cg.PollingComponent, i2c.I2CDevice)
@@ -28,12 +37,20 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(APDS9960),
-            cv.Optional(CONF_LED_DRIVE, 100): cv.enum(DRIVE_LEVELS),
-            cv.Optional(CONF_PROXIMITY_GAIN, 4): cv.enum(PROXIMITY_LEVELS),
-            cv.Optional(CONF_AMBIENT_LIGHT_GAIN, 4): cv.enum(AMBIENT_LEVELS),
-            cv.Optional(CONF_GESTURE_LED_DRIVE, 100): cv.enum(DRIVE_LEVELS),
-            cv.Optional(CONF_GESTURE_GAIN, 4): cv.enum(GESTURE_LEVELS),
-            cv.Optional(CONF_GESTURE_WAIT_TIME, 2.8): cv.enum(GESTURE_WAIT_TIMES),
+            cv.Optional(CONF_LED_DRIVE, "100ma"): cv.enum(DRIVE_LEVELS, lower=True),
+            cv.Optional(CONF_PROXIMITY_GAIN, "4x"): cv.enum(
+                PROXIMITY_LEVELS, lower=True
+            ),
+            cv.Optional(CONF_AMBIENT_LIGHT_GAIN, "4x"): cv.enum(
+                AMBIENT_LEVELS, lower=True
+            ),
+            cv.Optional(CONF_GESTURE_LED_DRIVE, "100ma"): cv.enum(
+                DRIVE_LEVELS, lower=True
+            ),
+            cv.Optional(CONF_GESTURE_GAIN, "4x"): cv.enum(GESTURE_LEVELS, lower=True),
+            cv.Optional(CONF_GESTURE_WAIT_TIME, "2.8ms"): cv.enum(
+                GESTURE_WAIT_TIMES, lower=True
+            ),
         }
     )
     .extend(cv.polling_component_schema("60s"))
