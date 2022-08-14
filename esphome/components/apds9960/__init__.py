0f2de33129a0ae33695098f74b7e54ec74ee68bc
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-08-15 08:43:27 +1200

    Apply suggestions from code review

--- esphome/components/apds9960/__init__.py
+++ esphome/components/apds9960/__init__.py
@@ -15,7 +15,7 @@ CONF_GESTURE_LED_DRIVE = "gesture_led_drive"
 CONF_GESTURE_GAIN = "gesture_gain"
 CONF_GESTURE_WAIT_TIME = "gesture_wait_time"
 
-DRIVE_LEVELS = {"100ma": 0, "50ma": 1, "25ma": 2, "12.5ma": 3}
+DRIVE_LEVELS = {"100mA": 0, "50mA": 1, "25mA": 2, "12.5mA": 3}
 PROXIMITY_LEVELS = {"1x": 0, "2x": 1, "4x": 2, "8x": 3}
 AMBIENT_LEVELS = {"1x": 0, "4x": 1, "16x": 2, "64x": 3}
 GESTURE_LEVELS = {"1x": 0, "2x": 1, "4x": 2, "8x": 3}
@@ -37,14 +37,14 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(APDS9960),
-            cv.Optional(CONF_LED_DRIVE, "100ma"): cv.enum(DRIVE_LEVELS, lower=True),
+            cv.Optional(CONF_LED_DRIVE, "100mA"): cv.enum(DRIVE_LEVELS, lower=True),
             cv.Optional(CONF_PROXIMITY_GAIN, "4x"): cv.enum(
                 PROXIMITY_LEVELS, lower=True
             ),
             cv.Optional(CONF_AMBIENT_LIGHT_GAIN, "4x"): cv.enum(
                 AMBIENT_LEVELS, lower=True
             ),
-            cv.Optional(CONF_GESTURE_LED_DRIVE, "100ma"): cv.enum(
+            cv.Optional(CONF_GESTURE_LED_DRIVE, "100mA"): cv.enum(
                 DRIVE_LEVELS, lower=True
             ),
             cv.Optional(CONF_GESTURE_GAIN, "4x"): cv.enum(GESTURE_LEVELS, lower=True),
