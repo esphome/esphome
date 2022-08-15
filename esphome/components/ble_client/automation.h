8df27d4c3fa027a19bb18ca28a862646da4f0767
buxtronix <bbuxton@gmail.com>
2022-08-15 13:06:05 +1000

    Only trigger ble_client on_connect after discovering services (#3710)

--- esphome/components/ble_client/automation.h
+++ esphome/components/ble_client/automation.h
@@ -15,10 +15,10 @@ class BLEClientConnectTrigger : public Trigger<>, public BLEClientNode {
   void loop() override {}
   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
                            esp_ble_gattc_cb_param_t *param) override {
-    if (event == ESP_GATTC_OPEN_EVT && param->open.status == ESP_GATT_OK)
-      this->trigger();
-    if (event == ESP_GATTC_SEARCH_CMPL_EVT)
+    if (event == ESP_GATTC_SEARCH_CMPL_EVT) {
       this->node_state = espbt::ClientState::ESTABLISHED;
+      this->trigger();
+    }
   }
 };
 
