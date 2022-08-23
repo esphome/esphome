8a215a9e8efa9543d56995c6c49cb8aae6b579ef
Mathias Stock <89242257+Stock-M@users.noreply.github.com>
2022-08-23 10:39:41 +0200

    Update tee501.cpp

--- esphome/components/tee501/tee501.cpp
+++ esphome/components/tee501/tee501.cpp
@@ -1,7 +1,6 @@
 #include "tee501.h"
 #include "esphome/core/log.h"
 
-
 namespace esphome {
 namespace tee501 {
 
@@ -18,8 +17,10 @@ void TEE501Component::setup() {
     this->mark_failed();
     return;
   }
-  uint32_t serialNumber1= (identification[0] << 24) + (identification[1] << 16) + (identification[2] << 8) + identification[3];
-  uint32_t serialNumber2= (identification[4] << 24) + (identification[5] << 16) + (identification[6] << 8) + identification[7];
+  uint32_t serialNumber1 =
+      (identification[0] << 24) + (identification[1] << 16) + (identification[2] << 8) + identification[3];
+  uint32_t serialNumber2 =
+      (identification[4] << 24) + (identification[5] << 16) + (identification[6] << 8) + identification[7];
   ESP_LOGV(TAG, "    Serial Number: 0x%08X%08X", serialNumber1, serialNumber2);
 }
 
@@ -42,7 +43,6 @@ void TEE501Component::dump_config() {
   LOG_SENSOR("  ", "TEE501", this);
 }
 
-
 float TEE501Component::get_setup_priority() const { return setup_priority::DATA; }
 void TEE501Component::update() {
   uint8_t address_1[] = {0x2C, 0x1B};
@@ -50,32 +50,28 @@ void TEE501Component::update() {
   this->set_timeout(50, [this]() {
     uint8_t i2cResponse[3];
     this->read(i2cResponse, 3);
-    if(i2cResponse[2] != calcCrc8(i2cResponse, 0, 1)) {
+    if (i2cResponse[2] != calcCrc8(i2cResponse, 0, 1)) {
       this->error_code_ = CRC_CHECK_FAILED;
       this->status_set_warning();
       return;
     }
-    float temperature = ((float)(i2cResponse[0]) * 256 + i2cResponse[1]) / 100;
+    float temperature = ((float) (i2cResponse[0]) * 256 + i2cResponse[1]) / 100;
     ESP_LOGD(TAG, "Got temperature=%.2f°C", temperature);
     this->publish_state(temperature);
     this->status_clear_warning();
   });
 }
 
-
-
-
-
-unsigned char TEE501Component::calcCrc8 (unsigned char buf[], unsigned char from, unsigned char to) {
-  unsigned char crcVal = CRC8_ONEWIRE_START;
+unsigned char TEE501Component::calcCrc8(unsigned char buf[], unsigned char from, unsigned char to) {
+  unsigned char crcVal = 0xFF;
   unsigned char i = 0;
   unsigned char j = 0;
   for (i = from; i <= to; i++) {
     int curVal = buf[i];
     for (j = 0; j < 8; j++) {
-      if (((crcVal ^ curVal) & 0x80) != 0)  // If MSBs are not equal 
+      if (((crcVal ^ curVal) & 0x80) != 0)  // If MSBs are not equal
       {
-        crcVal = ((crcVal << 1) ^ CRC8_ONEWIRE_POLY);
+        crcVal = ((crcVal << 1) ^ 0x31);
       } else {
         crcVal = (crcVal << 1);
       }
@@ -84,8 +80,6 @@ unsigned char TEE501Component::calcCrc8 (unsigned char buf[], unsigned char from
   }
   return crcVal;
 }
-
-
 
 }  // namespace tee501
 }  // namespace esphome
