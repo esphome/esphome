0cd3d5733dd9d88f079c772af0e8559043607b78
Mathias Stock <89242257+Stock-M@users.noreply.github.com>
2022-08-23 09:30:42 +0200

    Update hte501.cpp

--- esphome/components/hte501/hte501.cpp
+++ esphome/components/hte501/hte501.cpp
@@ -8,17 +8,19 @@ static const char *const TAG = "hte501";
 
 void HTE501Component::setup() {
   ESP_LOGCONFIG(TAG, "Setting up HTE501...");
-  uint8_t address[] = {0x70,0x29};
-  this->write(address,2,false);
+  uint8_t address[] = {0x70, 0x29};
+  this->write(address, 2, false);
   uint8_t identification[9];
   this->read(identification, 9);
-  if(identification[8] != calcCrc8(identification, 0, 7)) {
+  if (identification[8] != calcCrc8(identification, 0, 7)) {
     this->error_code_ = CRC_CHECK_FAILED;
     this->mark_failed();
     return;
   }
-  uint32_t serialNumber1= (identification[0] << 24) + (identification[1] << 16) + (identification[2] << 8) + identification[3];
-  uint32_t serialNumber2= (identification[4] << 24) + (identification[5] << 16) + (identification[6] << 8) + identification[7];
+  uint32_t serialNumber1 =
+      (identification[0] << 24) + (identification[1] << 16) + (identification[2] << 8) + identification[3];
+  uint32_t serialNumber2 =
+      (identification[4] << 24) + (identification[5] << 16) + (identification[6] << 8) + identification[7];
   ESP_LOGV(TAG, "    Serial Number: 0x%08X%08X", serialNumber1, serialNumber2);
 }
 
@@ -45,20 +47,20 @@ void HTE501Component::dump_config() {
 
 float HTE501Component::get_setup_priority() const { return setup_priority::DATA; }
 void HTE501Component::update() {
-  uint8_t address_1[] = {0x2C,0x1B};
+  uint8_t address_1[] = {0x2C, 0x1B};
   this->write(address_1, 2, true);
   this->set_timeout(50, [this]() {
-  uint8_t i2cResponse[6];
-  this->read(i2cResponse, 6);
-  if(i2cResponse[2] != calcCrc8(i2cResponse, 0, 1) && i2cResponse[5] != calcCrc8(i2cResponse, 3, 4)) {
-    this->error_code_ = CRC_CHECK_FAILED;
-    this->status_set_warning();
-    return;
-  }
-  float temperature = ((float)(i2cResponse[0]) * 256 + i2cResponse[1]) / 100;
-  float humidity = ((float)(i2cResponse[3]) * 256 + i2cResponse[4]) / 100;
+    uint8_t i2cResponse[6];
+    this->read(i2cResponse, 6);
+    if(i2cResponse[2] != calcCrc8(i2cResponse, 0, 1) && i2cResponse[5] != calcCrc8(i2cResponse, 3, 4)) {
+      this->error_code_ = CRC_CHECK_FAILED;
+      this->status_set_warning();
+      return;
+    }
+    float temperature = ((float) (i2cResponse[0]) * 256 + i2cResponse[1]) / 100;
+    float humidity = ((float) (i2cResponse[3]) * 256 + i2cResponse[4]) / 100;
 
-ESP_LOGD(TAG, "Got temperature=%.2f°C humidity=%.2f%%", temperature, humidity);
+    ESP_LOGD(TAG, "Got temperature=%.2f°C humidity=%.2f%%", temperature, humidity);
     if (this->temperature_sensor_ != nullptr)
       this->temperature_sensor_->publish_state(temperature);
     if (this->humidity_sensor_ != nullptr)
@@ -68,21 +70,17 @@ ESP_LOGD(TAG, "Got temperature=%.2f°C humidity=%.2f%%", temperature, humidity);
 }
 
 
-unsigned char HTE501Component::calcCrc8 (unsigned char buf[], unsigned char from, unsigned char to)
-{
+unsigned char HTE501Component::calcCrc8(unsigned char buf[], unsigned char from, unsigned char to) {
   unsigned char crcVal = CRC8_ONEWIRE_START;
   unsigned char i = 0;
   unsigned char j = 0;
-  for (i = from; i <= to; i++)
-  {
+  for (i = from; i <= to; i++) {
     int curVal = buf[i];
-    for (j = 0; j < 8; j++)
-    {
+    for (j = 0; j < 8; j++) {
       if (((crcVal ^ curVal) & 0x80) != 0)        //If MSBs are not equal
       {
         crcVal = ((crcVal << 1) ^ CRC8_ONEWIRE_POLY);
-      }
-      else {
+      } else {
         crcVal = (crcVal << 1);
       }
       curVal = curVal << 1;
