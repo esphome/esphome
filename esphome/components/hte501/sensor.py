88b92c9fdb0d7437712d801c2b4abc64dd3eecdb
Mathias Stock <89242257+Stock-M@users.noreply.github.com>
2022-08-23 09:24:38 +0200

    Update sensor.py

--- esphome/components/hte501/sensor.py
+++ esphome/components/hte501/sensor.py
@@ -41,11 +41,11 @@ CONFIG_SCHEMA = (
     .extend(i2c.i2c_device_schema(0x40))
 )
 
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
-
 
     if CONF_TEMPERATURE in config:
         sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
