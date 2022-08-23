e95b06859a8a1ff7f5aa95c2974934fcf24c08ac
Mathias Stock <89242257+Stock-M@users.noreply.github.com>
2022-08-23 09:00:06 +0200

    Update tee501.h

--- esphome/components/tee501/tee501.h
+++ esphome/components/tee501/tee501.h
@@ -4,8 +4,8 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
 
-static const unisgned char CRC8_ONEWIRE_POLY 0x31;
-static const unisgned char CRC8_ONEWIRE_START 0xFF;
+static const unsigned char CRC8_ONEWIRE_POLY = 0x31;
+static const unsigned char CRC8_ONEWIRE_START = 0xFF;
 
 namespace esphome {
 namespace tee501 {
@@ -18,15 +18,11 @@ class TEE501Component : public sensor::Sensor, public PollingComponent, public i
   float get_setup_priority() const override;
   void update() override;
 
-  protected:
-  unsigned char calcCrc8 (unsigned char buf[], unsigned char from, unsigned char to);
+ protected:
+  unsigned char calcCrc8(unsigned char buf[], unsigned char from, unsigned char to);
 
-  enum ErrorCode {
-  NONE = 0,
-  COMMUNICATION_FAILED,
-  CRC_CHECK_FAILED
-  } error_code_{NONE};
-};
+  enum ErrorCode { NONE = 0, COMMUNICATION_FAILED, CRC_CHECK_FAILED } error_code_{NONE};
+ };
 
 }  // namespace tee501
 }  // namespace esphome
