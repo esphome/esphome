20adb55447773f7a7134292af30689d6ac36fc27
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-08-25 11:33:07 +0200

    libretuya: apply changes from esp32 preferences

--- esphome/components/libretuya/preferences.cpp
+++ esphome/components/libretuya/preferences.cpp
@@ -38,6 +38,7 @@ class LibreTuyaPreferenceBackend : public ESPPreferenceBackend {
     save.key = key;
     save.data.assign(data, data + len);
     s_pending_save.emplace_back(save);
+    ESP_LOGVV(TAG, "s_pending_save: key: %s, len: %d", key.c_str(), len);
     return true;
   }
 
@@ -59,6 +60,8 @@ class LibreTuyaPreferenceBackend : public ESPPreferenceBackend {
     if (actual_len != len) {
       ESP_LOGVV(TAG, "NVS length does not match (%u!=%u)", actual_len, len);
       return false;
+    } else {
+      ESP_LOGVV(TAG, "fdb_kv_get_blob: key: %s, len: %d", key.c_str(), len);
     }
     return true;
   }
@@ -68,7 +71,6 @@ class LibreTuyaPreferences : public ESPPreferences {
  public:
   struct fdb_kvdb db;
   struct fdb_blob blob;
-  uint32_t current_offset = 0;
 
   void open() {
     //
@@ -88,12 +90,9 @@ class LibreTuyaPreferences : public ESPPreferences {
     auto *pref = new LibreTuyaPreferenceBackend();  // NOLINT(cppcoreguidelines-owning-memory)
     pref->db = &db;
     pref->blob = &blob;
-    current_offset += length;
 
-    uint32_t keyval = current_offset ^ type;
-    char keybuf[16];
-    snprintf(keybuf, sizeof(keybuf), "%d", keyval);
-    pref->key = keybuf;  // copied to std::string
+    uint32_t keyval = type;
+    pref->key = str_sprintf("%u", keyval);
 
     return ESPPreferenceObject(pref);
   }
@@ -102,24 +101,60 @@ class LibreTuyaPreferences : public ESPPreferences {
     if (s_pending_save.empty())
       return true;
 
-    ESP_LOGD(TAG, "Saving preferences to flash...");
+    ESP_LOGD(TAG, "Saving %d preferences to flash...", s_pending_save.size());
     // goal try write all pending saves even if one fails
-    bool any_failed = false;
+    int cached = 0, written = 0, failed = 0;
+    fdb_err_t last_err = FDB_NO_ERR;
+    std::string last_key{};
 
     // go through vector from back to front (makes erase easier/more efficient)
     for (ssize_t i = s_pending_save.size() - 1; i >= 0; i--) {
       const auto &save = s_pending_save[i];
-      fdb_blob_make(&blob, save.data.data(), save.data.size());
-      fdb_err_t err = fdb_kv_set_blob(&db, save.key.c_str(), &blob);
-      if (err != FDB_NO_ERR) {
-        ESP_LOGV(TAG, "fdb_kv_set_blob('%s', len=%u) failed: %d", save.key.c_str(), save.data.size(), err);
-        any_failed = true;
-        continue;
+      ESP_LOGVV(TAG, "Checking if FDB data %s has changed", save.key.c_str());
+      if (is_changed(&db, save)) {
+        ESP_LOGV(TAG, "sync: key: %s, len: %d", save.key.c_str(), save.data.size());
+        fdb_blob_make(&blob, save.data.data(), save.data.size());
+        fdb_err_t err = fdb_kv_set_blob(&db, save.key.c_str(), &blob);
+        if (err != FDB_NO_ERR) {
+          ESP_LOGV(TAG, "fdb_kv_set_blob('%s', len=%u) failed: %d", save.key.c_str(), save.data.size(), err);
+          failed++;
+          last_err = err;
+          last_key = save.key;
+          continue;
+        }
+        written++;
+      } else {
+        ESP_LOGD(TAG, "FDB data not changed; skipping %s  len=%u", save.key.c_str(), save.data.size());
+        cached++;
       }
       s_pending_save.erase(s_pending_save.begin() + i);
     }
+    ESP_LOGD(TAG, "Saving %d preferences to flash: %d cached, %d written, %d failed", cached + written + failed, cached,
+             written, failed);
+    if (failed > 0) {
+      ESP_LOGE(TAG, "Error saving %d preferences to flash. Last error=%d for key=%s", failed, last_err,
+               last_key.c_str());
+    }
 
-    return !any_failed;
+    return failed == 0;
+  }
+
+  bool is_changed(const fdb_kvdb_t db, const NVSData &to_save) {
+    NVSData stored_data{};
+    struct fdb_kv kv;
+    fdb_kv_t kvp = fdb_kv_get_obj(db, to_save.key.c_str(), &kv);
+    if (kvp == nullptr) {
+      ESP_LOGV(TAG, "fdb_kv_get_obj('%s'): nullptr - the key might not be set yet", to_save.key.c_str());
+      return true;
+    }
+    stored_data.data.reserve(kv.value_len);
+    fdb_blob_make(&blob, stored_data.data.data(), kv.value_len);
+    size_t actual_len = fdb_kv_get_blob(db, to_save.key.c_str(), &blob);
+    if (actual_len != kv.value_len) {
+      ESP_LOGV(TAG, "fdb_kv_get_blob('%s') len mismatch: %u != %u", to_save.key.c_str(), actual_len, kv.value_len);
+      return true;
+    }
+    return to_save.data != stored_data.data;
   }
 };
 
