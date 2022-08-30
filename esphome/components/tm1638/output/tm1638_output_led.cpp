9691cafa7bea490538f33ae91e8d80c99005172e
Skykingjwc <skykingjwc@yahoo.com>
2022-08-29 21:32:40 -0400

    Added switches back to TM1638_Component

--- esphome/components/tm1638/output/tm1638_output_led.cpp
+++ esphome/components/tm1638/output/tm1638_output_led.cpp
@@ -1,4 +1,4 @@
-#include "tm1638_led.h"
+#include "tm1638_output_led.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
@@ -6,11 +6,9 @@ namespace tm1638 {
 
 static const char *const TAG = "tm1638.led";
 
-void TM1638Led::write_state(bool state) { tm1638_->set_led(led_, state); }
+void TM1638OutputLed::write_state(bool state) { tm1638_->set_led(led_, state); }
 
-void TM1638Led::dump_config() {
-  //LOG_SWITCH("", "TM1638 LED", this);
-  //ASK SSIEB about this
+void TM1638OutputLed::dump_config() {
   ESP_LOGCONFIG(TAG, "  LED: %d", led_);
 }
 
--- esphome/components/tm1638/switch/__init__.py
+++ esphome/components/tm1638/switch/__init__.py
@@ -1,15 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import output
+from esphome.components import switch
 from esphome.const import CONF_ID, CONF_LED
 from ..display import tm1638_ns, TM1638Component, CONF_TM1638_ID
 
-TM1638OutputLed = tm1638_ns.class_("TM1638OutputLed", output.BinaryOutput)
+TM1638SwitchLed = tm1638_ns.class_("TM1638SwitchLed", switch.Switch)
 
 
-CONFIG_SCHEMA = output.BINARY_OUTPUT_SCHEMA.extend(
+CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend(
     {
-        cv.GenerateID(): cv.declare_id(TM1638OutputLed),
+        cv.GenerateID(): cv.declare_id(TM1638SwitchLed),
         cv.GenerateID(CONF_TM1638_ID): cv.use_id(TM1638Component),
         cv.Required(CONF_LED): cv.int_range(min=0, max=7),
     }
@@ -18,7 +18,7 @@ CONFIG_SCHEMA = output.BINARY_OUTPUT_SCHEMA.extend(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await output.register_output(var, config)
+    await switch.register_switch(var, config)
     cg.add(var.set_lednum(config[CONF_LED]))
     hub = await cg.get_variable(config[CONF_TM1638_ID])
     cg.add(var.set_tm1638(hub))
