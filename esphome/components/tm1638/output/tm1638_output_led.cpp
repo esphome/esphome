73207c7c643f70cae847aa0bb58d1722f7cc10ee
Skykingjwc <skykingjwc@yahoo.com>
2022-08-30 20:50:51 -0400

    Clang, LOG_BINARY_OUTPUT too many arguments

--- esphome/components/tm1638/output/tm1638_output_led.cpp
+++ esphome/components/tm1638/output/tm1638_output_led.cpp
@@ -9,8 +9,8 @@ static const char *const TAG = "tm1638.led";
 void TM1638OutputLed::write_state(bool state) { tm1638_->set_led(led_, state); }
 
 void TM1638OutputLed::dump_config() {
-    LOG_BINARY_OUTPUT("", "TM1638 LED", this);
-    ESP_LOGCONFIG(TAG, "  LED: %d", led_);
+  LOG_BINARY_OUTPUT(this);
+  ESP_LOGCONFIG(TAG, "  LED: %d", led_);
 }
 
 }  // namespace tm1638
