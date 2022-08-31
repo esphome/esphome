d160c450c439812a71f778a1b3f0ce9705b8f361
NielsNl68 <nielsnl68@gmail.com>
2022-08-31 14:00:15 +0200

    other warning i overlooked

--- esphome/components/xpt2046/touchscreen.py
+++ esphome/components/xpt2046/touchscreen.py
@@ -73,7 +73,7 @@ CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend(
             ),
             cv.Optional(CONF_THRESHOLD, default=400): cv.int_range(min=0, max=4095),
             cv.Optional(CONF_REPORT_INTERVAL, default="never"): report_interval,
-            # cv.Optional(CONF_SWAP_X_Y, default=False): cv.boolean,
+            cv.Optional(CONF_SWAP_X_Y, default=False): cv.boolean,
         }
     )
     # .extend(cv.polling_component_schema("50ms"))
