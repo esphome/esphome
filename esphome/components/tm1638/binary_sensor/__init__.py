5ec8a2169b0f7a07b3b5a24e3e8bcef93490d68b
Skykingjwc <skykingjwc@yahoo.com>
2022-09-04 18:41:26 -0400

    Removed unecessary import

--- esphome/components/tm1638/binary_sensor/__init__.py
+++ esphome/components/tm1638/binary_sensor/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID, CONF_KEY
+from esphome.const import CONF_KEY
 from ..display import tm1638_ns, TM1638Component, CONF_TM1638_ID
 
 TM1638Key = tm1638_ns.class_("TM1638Key", binary_sensor.BinarySensor)
