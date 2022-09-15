9a55b84bf136858ce26e4e2136d6d3ee9784bce1
Jadson Santos <jadson44.santos@gmail.com>
2022-09-15 02:47:20 +0000

    Improvments

--- esphome/components/ethernet_info/ethernet_info_text_sensor.h
+++ esphome/components/ethernet_info/ethernet_info_text_sensor.h
@@ -7,19 +7,18 @@
 namespace esphome {
 namespace ethernet_info {
 
-class IPAddressWiFiInfo : public PollingComponent, public text_sensor::TextSensor {
+class IPAddressEthernetInfo : public PollingComponent, public text_sensor::TextSensor {
  public:
   void update() override {
-    
-      tcpip_adapter_ip_info_t tcpip;
 
-      tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_ETH, &tcpip);
-    
-      ip = IPAddress(tcpip.ip.addr);
+    tcpip_adapter_ip_info_t tcpip;
+    tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_ETH, &tcpip);
+
+    auto ip = tcpip.ip.addr;
 
     if (ip != this->last_ip_) {
       this->last_ip_ = ip;
-      this->publish_state(ip.str());
+      this->publish_state(network::IPAddress(ip).str().c_str());
     }
 
   }
