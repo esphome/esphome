008584d20c0fecbd3608896d424c77c816d3aa83
lorki97 <lorenz.markus97@gmail.com>
2022-09-22 05:31:29 +0200

    added write_packet helper method

--- esphome/components/mr24d11c10/mr24d11c10.cpp
+++ esphome/components/mr24d11c10/mr24d11c10.cpp
@@ -1,13 +1,21 @@
 #include "mr24d11c10.h"
 #include "esphome/core/log.h"
 #include "crc.h"
-
+#include <sstream>
 namespace esphome {
 namespace mr24d11c10 {
 
 static const char *const TAG = "mr24d11c10";
 
-static const int MESSAGE_HEAD = 0x55;
+static const int PACKET_START = 0x55;
+
+// Read Commands
+static const int READ_COMMAND = 0x01;
+static const int RC_MARKING_SEARCH = 0x01;
+static const int RC_MS_DEVICE_ID = 0x01;
+static const int RC_MS_SOFTWARE_VERSION = 0x02;
+static const int RC_MS_HARDWARE_VERSION = 0x03;
+static const int RC_MS_PROTOCOL_VERSION = 0x04;
 
 void MR24D11C10Sensor::setup() { ESP_LOGCONFIG(TAG, "Setting up MR24D11C10"); }
 
@@ -15,6 +23,11 @@ void MR24D11C10Sensor::dump_config() {
   LOG_BINARY_SENSOR("", "MR24D11C10", this);
 
   this->check_uart_settings(9600);
+
+  this->write_packet(READ_COMMAND, RC_MARKING_SEARCH, RC_MS_DEVICE_ID);
+  // this->write_packet(READ_COMMAND, RC_MARKING_SEARCH, RC_MS_SOFTWARE_VERSION);
+  // this->write_packet(READ_COMMAND, RC_MARKING_SEARCH, RC_MS_HARDWARE_VERSION);
+  // this->write_packet(READ_COMMAND, RC_MARKING_SEARCH, RC_MS_PROTOCOL_VERSION);
 
   if (this->is_failed()) {
     ESP_LOGE(TAG, "Communication with MR24D11C10 failed!");
@@ -34,8 +47,8 @@ void MR24D11C10Sensor::loop() {
   // ESP_LOGD(TAG, "vector size: %d", this->packet.size());
 
   // detect malformed message start
-  if (!this->reading && byte != MESSAGE_HEAD) {
-    ESP_LOGW(TAG, "Malformed message, did not receive MESSAGE_HEAD, received: 0x%x", byte);
+  if (!this->reading && byte != PACKET_START) {
+    ESP_LOGW(TAG, "Malformed message, did not receive PACKET_START, received: 0x%x", byte);
     return;
   }
 
@@ -57,7 +70,7 @@ void MR24D11C10Sensor::loop() {
     uint16_t length_low = this->packet.at(1);
     uint16_t length_high = this->packet.at(2);
 
-    this->expected_length = (length_high << 8 | length_low) + 1;  // include MESSAGE_HEAD in length
+    this->expected_length = (length_high << 8 | length_low) + 1;  // include PACKET_START in length
     ESP_LOGD(TAG, "Expected packet length (including MSG_HEAD): %d", this->expected_length);
   }
 
@@ -83,6 +96,48 @@ void MR24D11C10Sensor::loop() {
       return;
     }
   }
+}
+
+void MR24D11C10Sensor::write_packet(uint8_t function_code, uint8_t address_code_1, uint8_t address_code_2,
+                                    std::vector<uint8_t> &data) {
+  uint16_t packet_size = data.size() + 8;
+  std::vector<uint8_t> packet;
+  packet.reserve(packet_size);
+
+  packet.push_back(PACKET_START);
+  packet.push_back(packet_size & 0x00FF);
+  packet.push_back(packet_size >> 8);
+  packet.push_back(function_code);
+  packet.push_back(address_code_1);
+  packet.push_back(address_code_2);
+  packet.insert(std::end(packet), std::begin(data), std::end(data));
+
+  uint16_t crc = us_CalculateCrc16(packet.data(), packet_size - 2);
+
+  packet.push_back(crc >> 8);
+  packet.push_back(crc & 0x00FF);
+
+  this->write_array(packet);
+
+  this->log_packet(packet);
+}
+
+void MR24D11C10Sensor::write_packet(uint8_t function_code, uint8_t address_code_1, uint8_t address_code_2) {
+  std::vector<uint8_t> empty_data;
+  MR24D11C10Sensor::write_packet(function_code, address_code_1, address_code_2, empty_data);
+}
+
+void MR24D11C10Sensor::log_packet(std::vector<uint8_t> &packet) {
+  std::stringstream ss;
+
+  ss << "Packet:";
+  for (uint8_t &byte : packet) {
+    ss << " " << std::hex << static_cast<int>(byte);
+    ESP_LOGD(TAG, "%x", byte);
+  }
+
+  ss.flush();
+  ESP_LOGD(TAG, ss.str().c_str());
 }
 
 }  // namespace mr24d11c10
--- esphome/components/mr24d11c10/mr24d11c10.h
+++ esphome/components/mr24d11c10/mr24d11c10.h
@@ -18,6 +18,11 @@ class MR24D11C10Sensor : public binary_sensor::BinarySensor, public Component, p
   void loop() override;
 
  protected:
+  void write_packet(uint8_t function_code, uint8_t address_code_1, uint8_t address_code_2, std::vector<uint8_t> &data);
+  void write_packet(uint8_t function_code, uint8_t address_code_1, uint8_t address_code_2);
+
+  void log_packet(std::vector<uint8_t> &packet);
+
   std::vector<uint8_t> packet;
   bool reading = false;
   uint16_t expected_length = 0;
