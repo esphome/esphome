125c6c3b3d354d4eff0b7c63f7c98a89d22b25ea
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2022-10-02 20:23:12 -0700

    Python code to read and generate code

---
+++ esphome/components/ft6336u/touchscreen.py
@@ -0,0 +1,48 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+from esphome import pins
+from esphome.components import i2c, touchscreen
+from esphome.const import CONF_ID
+
+CODEOWNERS = ["@gpambrozio"]
+DEPENDENCIES = ["i2c"]
+
+ft6336u_ns = cg.esphome_ns.namespace("ft6336u")
+FT6336UTouchscreen = ft6336u_ns.class_(
+    "FT6336UTouchscreen",
+    touchscreen.Touchscreen,
+    cg.Component,
+    i2c.I2CDevice,
+)
+
+CONF_FT6336U_ID = "ft6336u_id"
+CONF_INTERRUPT_PIN = "interrupt_pin"
+CONF_RTS_PIN = "rts_pin"
+
+
+CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend(
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(FT6336UTouchscreen),
+            cv.Required(CONF_INTERRUPT_PIN): cv.All(
+                pins.internal_gpio_input_pin_schema
+            ),
+            cv.Required(CONF_RTS_PIN): pins.gpio_output_pin_schema,
+        }
+    )
+    .extend(i2c.i2c_device_schema(0x38))
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
+    await touchscreen.register_touchscreen(var, config)
+
+    interrupt_pin = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
+    cg.add(var.set_interrupt_pin(interrupt_pin))
+    rts_pin = await cg.gpio_pin_expression(config[CONF_RTS_PIN])
+    cg.add(var.set_rts_pin(rts_pin))
