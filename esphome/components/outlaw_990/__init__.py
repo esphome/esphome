397af81833a071f58f89464c388e9002c8b0b8b8
Steve Richardson <steve.richardson@makeitlabs.com>
2022-10-05 15:28:56 -0400

    add codeowners

--- esphome/components/outlaw_990/__init__.py
+++ esphome/components/outlaw_990/__init__.py
@@ -3,6 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
 
+CODEOWNERS = ["@tangentaudio"]
 DEPENDENCIES = ['uart']
 AUTO_LOAD = ["binary_sensor"]
 MULTI_CONF = True
