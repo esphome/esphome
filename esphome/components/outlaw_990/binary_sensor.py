8a23535c481c54385b14daf1a278311881e7c8c2
Steve Richardson <steve.richardson@makeitlabs.com>
2022-10-05 16:08:39 -0400

    remove unused icon import

--- esphome/components/outlaw_990/binary_sensor.py
+++ esphome/components/outlaw_990/binary_sensor.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ICON, ICON_POWER
 from . import CONF_OUTLAW_990_ID, Outlaw990
 
 DEPENDENCIES = ["outlaw_990"]
