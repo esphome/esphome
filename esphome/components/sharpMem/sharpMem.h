e907eb1b5a1195ea0de86f6a0731cb042fc078df
Dominik Skalník <skaldo91@gmail.com>
2022-10-08 20:23:19 +0200

    fix type of private class member cs_

--- esphome/components/sharpMem/sharpMem.h
+++ esphome/components/sharpMem/sharpMem.h
@@ -49,7 +49,7 @@ class SharpMem : public PollingComponent,
   void end_transaction_();
 
   int16_t width_ = 400, height_ = 240;
-  InternalGPIOPin *cs_;
+  GPIOPin *cs_;
   optional<sharpMem_writer_t> writer_local_{};
 
   uint8_t _sharpmem_vcom;
