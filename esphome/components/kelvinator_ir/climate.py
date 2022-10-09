5fb5f3fd4572054966f3dc8b8216bca0ed214491
Michael Horne <3130139+michael-horne@users.noreply.github.com>
2022-10-09 09:29:09 +0000

    Fix name.

--- esphome/components/kelvinator_ir/climate.py
+++ esphome/components/kelvinator_ir/climate.py
@@ -6,7 +6,7 @@ from esphome.const import CONF_ID
 AUTO_LOAD = ["climate_ir"]
 CODEOWNERS = ["@michael-horne"]
 
-CONF_HEADER_LIGHT = "light"
+CONF_LIGHT = "light"
 
 kelvinator_ir_ns = cg.esphome_ns.namespace("kelvinator_ir")
 KelvinatorIR = kelvinator_ir_ns.class_("KelvinatorIR", climate_ir.ClimateIR)
@@ -14,7 +14,7 @@ KelvinatorIR = kelvinator_ir_ns.class_("KelvinatorIR", climate_ir.ClimateIR)
 CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(KelvinatorIR),
-        cv.Optional(CONF_HEADER_LIGHT, default=True): cv.boolean,
+        cv.Optional(CONF_LIGHT, default=True): cv.boolean,
     }
 )
 
@@ -22,4 +22,4 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await climate_ir.register_climate_ir(var, config)
-    cg.add(var.set_light(config[CONF_HEADER_LIGHT]))
+    cg.add(var.set_light(config[CONF_LIGHT]))
