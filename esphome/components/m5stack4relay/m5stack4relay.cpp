e09e2176399f0d0bab4557ba394968058298704b
NP v/d Spek <nielsnl68@gmail.com>
2022-10-16 12:17:38 +0200

    fix crashing issue

--- esphome/components/m5stack4relay/m5stack4relay.cpp
+++ esphome/components/m5stack4relay/m5stack4relay.cpp
@@ -20,17 +20,13 @@ void M5Relay4Control::setup() {
 }
 
 uint8_t M5Relay4Control::get_relay_states_() {
-  uint8_t * result;
-  this->read_byte(UNIT_4RELAY_RELAY_REG, result);
-  return *result;
+  uint8_t result;
+  this->read_byte(UNIT_4RELAY_RELAY_REG, &result);
+  return result;
 }
 
 bool M5Relay4Control::set_relay(RelayBit bit, bool state) {
   uint8_t org = get_relay_states_();
-
-  ESP_LOGD(TAG, "Current status 0x%02X", org);
-  ESP_LOGD(TAG, "Desired channel: %1u", bit);
-  ESP_LOGD(TAG, "Desired state: %1u", state);
 
   if (state) {
     org |= (1u << (uint8_t) bit);
@@ -38,17 +34,15 @@ bool M5Relay4Control::set_relay(RelayBit bit, bool state) {
     org &= ~(1u << (uint8_t) bit);
   }
 
-  ESP_LOGD(TAG, "New status 0x%02X", org);
-
   return write_byte(UNIT_4RELAY_RELAY_REG, org);
 }
 
-void M5Relay4Control::set_sync_mode (bool mode) {
-    this->sync_mode_ = mode;
-    if (has_been_setup_) {
-      write_byte(UNIT_4RELAY_CONFIG_REG, this->sync_mode_);
-    }
+void M5Relay4Control::set_sync_mode(bool mode) {
+  this->sync_mode_ = mode;
+  if (has_been_setup_) {
+    write_byte(UNIT_4RELAY_CONFIG_REG, this->sync_mode_);
   }
+}
 
 void M5Relay4Switch::write_state(bool state) {
   if (!this->parent_->set_relay(this->bit_, state)) {
