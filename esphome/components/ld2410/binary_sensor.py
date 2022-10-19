6e0e00ce6c2522710424d280dcd665c7b6901aa2
sebcaps <sebastien.carecolin@gmail.com>
2022-10-19 21:23:18 +0000

    fix lint

--- esphome/components/ld2410/binary_sensor.py
+++ esphome/components/ld2410/binary_sensor.py
@@ -9,18 +9,18 @@ from . import CONF_LD2410_ID, LD2410Component
 
 DEPENDENCIES = ["ld2410"]
 CONF_HAS_TARGET = "has_target"
-CONF_HASMOVING_TARGET = "has_moving_target"
-CONF_HASSTILL_TARGET = "has_still_target"
+CONF_HAS_MOVING_TARGET = "has_moving_target"
+CONF_HAS_STILL_TARGET = "has_still_target"
 
 CONFIG_SCHEMA = {
     cv.GenerateID(CONF_LD2410_ID): cv.use_id(LD2410Component),
     cv.Optional(CONF_HAS_TARGET): binary_sensor.binary_sensor_schema(
         device_class=DEVICE_CLASS_PRESENCE
     ),
-    cv.Optional(CONF_HASMOVING_TARGET): binary_sensor.binary_sensor_schema(
+    cv.Optional(CONF_HAS_MOVING_TARGET): binary_sensor.binary_sensor_schema(
         device_class=DEVICE_CLASS_MOTION
     ),
-    cv.Optional(CONF_HASSTILL_TARGET): binary_sensor.binary_sensor_schema(
+    cv.Optional(CONF_HAS_STILL_TARGET): binary_sensor.binary_sensor_schema(
         device_class=DEVICE_CLASS_PRESENCE
     ),
 }
@@ -31,9 +31,9 @@ async def to_code(config):
     if CONF_HAS_TARGET in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_HAS_TARGET])
         cg.add(ld2410_component.settargetsensor(sens))
-    if CONF_HASMOVING_TARGET in config:
-        sens = await binary_sensor.new_binary_sensor(config[CONF_HASMOVING_TARGET])
+    if CONF_HAS_MOVING_TARGET in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_HAS_MOVING_TARGET])
         cg.add(ld2410_component.setmovingtargetsensor(sens))
-    if CONF_HASSTILL_TARGET in config:
-        sens = await binary_sensor.new_binary_sensor(config[CONF_HASSTILL_TARGET])
+    if CONF_HAS_STILL_TARGET in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_HAS_STILL_TARGET])
         cg.add(ld2410_component.setstilltargetsensor(sens))
