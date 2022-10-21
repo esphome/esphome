e67d7328fdd4f7ce0b31359e18eab8f4bb004c65
Cossid <cossid@cossid.com>
2022-10-20 19:15:38 -0500

    SM2335 - Fix missing log argument usage.

--- esphome/components/sm2335/sm2335.cpp
+++ esphome/components/sm2335/sm2335.cpp
@@ -46,8 +46,8 @@ void SM2335::dump_config() {
   ESP_LOGCONFIG(TAG, "SM2335:");
   LOG_PIN("  Data Pin: ", this->data_pin_);
   LOG_PIN("  Clock Pin: ", this->clock_pin_);
-  ESP_LOGCONFIG(TAG, "  Color Channels Max Power: ", this->max_power_color_channels_);
-  ESP_LOGCONFIG(TAG, "  White Channels Max Power: ", this->max_power_white_channels_);
+  ESP_LOGCONFIG(TAG, "  Color Channels Max Power: %u", this->max_power_color_channels_);
+  ESP_LOGCONFIG(TAG, "  White Channels Max Power: %u", this->max_power_white_channels_);
 }
 
 void SM2335::loop() {
