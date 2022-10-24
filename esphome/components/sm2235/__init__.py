18e604ba4d37615135872c4ce0d3ed122aaaff4e
Cossid <cossid@cossid.com>
2022-10-23 22:09:23 -0500

    SM_1024BIT_BASE - Refactor SM2335 out to SM_1024BIT_BASE to back SM2235 and SM2335

--- esphome/components/sm2235/__init__.py
+++ esphome/components/sm2235/__init__.py
@@ -6,14 +6,14 @@ AUTO_LOAD = ["sm_1024bit_base", "output"]
 CODEOWNERS = ["@Cossid"]
 MULTI_CONF = True
 
-sm2335_ns = cg.esphome_ns.namespace("sm2335")
+sm2235_ns = cg.esphome_ns.namespace("sm2235")
 
-SM2335 = sm2335_ns.class_("SM2335", sm_1024bit_base.SM_1024BIT_Base)
+SM2235 = sm2235_ns.class_("SM2235", sm_1024bit_base.SM_1024BIT_Base)
 
 CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.GenerateID(): cv.declare_id(SM2335),
+            cv.GenerateID(): cv.declare_id(SM2235),
         }
     )
     .extend(sm_1024bit_base.SM_1024BIT_BASE_CONFIG_SCHEMA)
@@ -21,4 +21,4 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     var = await sm_1024bit_base.register_sm_1024bit_base(config)
-    cg.add(var.set_model(0xC0))
+    cg.add(var.set_model(0xD0))
--- esphome/components/sm2235/sm2235.cpp
+++ esphome/components/sm2235/sm2235.cpp
@@ -1,13 +1,13 @@
-#include "sm2335.h"
+#include "sm2235.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
-namespace sm2335 {
+namespace sm2235 {
 
-static const char *const TAG = "sm2335";
+static const char *const TAG = "sm2235";
 
-void SM2335::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up sm2335 Output Component...");
+void SM2235::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up sm2235 Output Component...");
   this->data_pin_->setup();
   this->data_pin_->digital_write(true);
   this->clock_pin_->setup();
@@ -15,13 +15,13 @@ void SM2335::setup() {
   this->pwm_amounts_.resize(5, 0);
 }
 
-void SM2335::dump_config() {
-  ESP_LOGCONFIG(TAG, "sm2335:");
+void SM2235::dump_config() {
+  ESP_LOGCONFIG(TAG, "sm2235:");
   LOG_PIN("  Data Pin: ", this->data_pin_);
   LOG_PIN("  Clock Pin: ", this->clock_pin_);
   ESP_LOGCONFIG(TAG, "  Color Channels Max Power: %u", this->max_power_color_channels_);
   ESP_LOGCONFIG(TAG, "  White Channels Max Power: %u", this->max_power_white_channels_);
 }
 
-}  // namespace sm2335
+}  // namespace sm2235
 }  // namespace esphome
