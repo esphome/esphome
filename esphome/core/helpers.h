9ff939b3935c8cea8ba01b2f4ef46c6cac4cce21
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-11-07 20:40:23 +1300

    Ignore const lint on mac

--- esphome/core/helpers.h
+++ esphome/core/helpers.h
@@ -568,7 +568,7 @@ class HighFrequencyLoopRequester {
 };
 
 /// Get the device MAC address as raw bytes, written into the provided byte array (6 bytes).
-void get_mac_address_raw(uint8_t *mac);
+void get_mac_address_raw(uint8_t *mac);  // NOLINT(readability-non-const-parameter)
 
 /// Get the device MAC address as a string, in lowercase hex notation.
 std::string get_mac_address();
