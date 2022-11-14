8bc71d1d27816b933d216106ef9ce837c99068c6
Artur Sikorski <2mikrobi@gmail.com>
2022-11-14 19:37:13 +0100

    Formatted code.

--- esphome/components/qmc5883l/sensor.py
+++ esphome/components/qmc5883l/sensor.py
@@ -103,11 +103,11 @@ CONFIG_SCHEMA = (
 
 
 def auto_data_rate(config):
-    interval_sec=0
+    interval_sec = 0
     if config[CONF_UPDATE_INTERVAL].seconds is not None:
-      interval_sec=config[CONF_UPDATE_INTERVAL].seconds
-    if config[CONF_UPDATE_INTERVAL].milliseconds is not None: 
-      interval_sec+=config[CONF_UPDATE_INTERVAL].milliseconds/1000
+        interval_sec = config[CONF_UPDATE_INTERVAL].seconds
+    if config[CONF_UPDATE_INTERVAL].milliseconds is not None:
+        interval_sec += config[CONF_UPDATE_INTERVAL].milliseconds / 1000
     interval_hz = 1.0 / interval_sec
     for datarate in sorted(QMC5883LDatarates.keys()):
         if float(datarate) >= interval_hz:
