64aa45331c237b4b3c44480a46d6d775ce6dcca4
J. Nick Koston <nick@koston.org>
2022-11-28 11:35:07 -1000

    tidy

--- esphome/components/esp32_ble_client/ble_client_base.cpp
+++ esphome/components/esp32_ble_client/ble_client_base.cpp
@@ -187,7 +187,8 @@ bool BLEClientBase::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_
       break;
     }
     case ESP_GATTC_REG_FOR_NOTIFY_EVT: {
-      if (this->connection_type_ == espbt::ConnectionType::V2_WITH_CACHE || this->connection_type_ == espbt::ConnectionType::V2_WITHOUT_CACHE) {
+      if (this->connection_type_ == espbt::ConnectionType::V2_WITH_CACHE ||
+          this->connection_type_ == espbt::ConnectionType::V2_WITHOUT_CACHE) {
         // Client is responsible for flipping the descriptor value
         // when using the cache
         break;
