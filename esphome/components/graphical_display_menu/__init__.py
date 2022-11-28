902fa5d929eaadf292dc857209271272ce6eee46
Michael Davidson <mr.m.davidson@gmail.com>
2022-11-28 22:26:59 +1100

    Apply formatting

--- esphome/components/graphical_display_menu/__init__.py
+++ esphome/components/graphical_display_menu/__init__.py
@@ -13,7 +13,6 @@ CONF_DISPLAY = "display"
 CONF_DISPLAY_UPDATER = "display_updater"
 CONF_FONT = "font"
 
-
 graphical_display_menu = cg.esphome_ns.namespace("graphical_display_menu")
 GraphicalDisplayMenu = graphical_display_menu.class_(
     "GraphicalDisplayMenu", DisplayMenuComponent
@@ -28,11 +27,14 @@ CONFIG_SCHEMA = DISPLAY_MENU_BASE_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(GraphicalDisplayMenu),
             cv.GenerateID(CONF_DISPLAY): cv.use_id(display.DisplayBuffer),
-            cv.GenerateID(CONF_DISPLAY_UPDATER): cv.Optional(cv.use_id(cg.PollingComponent)),
+            cv.GenerateID(CONF_DISPLAY_UPDATER): cv.Optional(
+                cv.use_id(cg.PollingComponent)
+            ),
             cv.GenerateID(CONF_FONT): cv.use_id(font.Font),
         }
     )
 )
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
