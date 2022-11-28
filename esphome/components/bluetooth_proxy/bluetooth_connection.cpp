0c99b0d7715a51e9fee0dda0a8dfc27156acbaf9
J. Nick Koston <nick@koston.org>
2022-11-27 15:34:47 -1000

    adjust

--- esphome/components/bluetooth_proxy/bluetooth_connection.cpp
+++ esphome/components/bluetooth_proxy/bluetooth_connection.cpp
@@ -215,7 +215,7 @@ esp_err_t BluetoothConnection::read_descriptor(uint16_t handle) {
   return ESP_OK;
 }
 
-esp_err_t BluetoothConnection::write_descriptor(uint16_t handle, const std::string &data) {
+esp_err_t BluetoothConnection::write_descriptor(uint16_t handle, const std::string &data, bool response) {
   if (!this->connected()) {
     ESP_LOGW(TAG, "[%d] [%s] Cannot write GATT descriptor, not connected.", this->connection_index_,
              this->address_str_.c_str());
@@ -223,10 +223,9 @@ esp_err_t BluetoothConnection::write_descriptor(uint16_t handle, const std::stri
   }
   ESP_LOGV(TAG, "[%d] [%s] Writing GATT descriptor handle %d", this->connection_index_, this->address_str_.c_str(),
            handle);
-
   esp_err_t err =
       esp_ble_gattc_write_char_descr(this->gattc_if_, this->conn_id_, handle, data.size(), (uint8_t *) data.data(),
-                                     ESP_GATT_WRITE_TYPE_NO_RSP, ESP_GATT_AUTH_REQ_NONE);
+                                     response ? ESP_GATT_WRITE_TYPE_RSP : ESP_GATT_WRITE_TYPE_NO_RSP, ESP_GATT_AUTH_REQ_NONE);
   if (err != ERR_OK) {
     ESP_LOGW(TAG, "[%d] [%s] esp_ble_gattc_write_char_descr error, err=%d", this->connection_index_,
              this->address_str_.c_str(), err);
