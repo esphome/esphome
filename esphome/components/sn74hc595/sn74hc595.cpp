53281ada0634dc0b60b3c7a08745e526cde3a5ca
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-11-30 16:14:30 +1300

    typo

--- esphome/components/sn74hc595/sn74hc595.cpp
+++ esphome/components/sn74hc595/sn74hc595.cpp
@@ -30,7 +30,7 @@ void SN74HC595Component::dump_config() { ESP_LOGCONFIG(TAG, "SN74HC595:"); }
 
 void SN74HC595Component::digital_write_(uint16_t pin, bool value) {
   if (pin >= this->sr_count_ * 8) {
-    ESP_LOGE(TAG, "Pin %u is out of range! Maximum pin number with %u cihps in series is %u", pin, this->sr_count_,
+    ESP_LOGE(TAG, "Pin %u is out of range! Maximum pin number with %u chips in series is %u", pin, this->sr_count_,
              (this->sr_count_ * 8) - 1);
     return;
   }
