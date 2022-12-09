52a9673fcf2a46b344cb7b6170d24213e26bcbdd
dentra <dennis.trachuk@gmail.com>
2022-12-09 17:38:43 +0000

    parse multipart requests only when ota enabled

--- esphome/components/web_server_idf/__init__.py
+++ esphome/components/web_server_idf/__init__.py
@@ -1,6 +1,8 @@
+import esphome.codegen as cg
 import esphome.config_validation as cv
-
+from esphome.core import CORE
 from esphome.components.esp32 import add_idf_sdkconfig_option
+from esphome.const import CONF_WEB_SERVER, CONF_OTA
 
 CODEOWNERS = ["@dentra"]
 
@@ -13,3 +15,6 @@ CONFIG_SCHEMA = cv.All(
 async def to_code(config):
     # Increase the maximum supported size of headers section in HTTP request packet to be processed by the server
     add_idf_sdkconfig_option("CONFIG_HTTPD_MAX_REQ_HDR_LEN", 1024)
+
+    if CORE.config[CONF_WEB_SERVER][CONF_OTA]:
+        cg.add_define("USE_WEBSERVER_IDF_MULTIPART")
