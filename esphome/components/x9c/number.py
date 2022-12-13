095235b5b0b180ded86c9c13b296ab8cfa5b88ad
Etienne Descoteaux <etienne.md@me.com>
2022-12-13 14:38:19 -0500

    Add X9C Potentiometer component

---
+++ esphome/components/x9c/number.py
@@ -0,0 +1,48 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import pins
+from esphome.components import number
+from esphome.const import (
+    CONF_ID,
+    CONF_CS_PIN,
+    CONF_INC_PIN,
+    CONF_UD_PIN,
+    CONF_INITIAL_VALUE,
+)
+
+x9c_ns = cg.esphome_ns.namespace("x9c")
+
+X9cNumber = x9c_ns.class_("X9cNumber", number.Number, cg.Component)
+
+CONFIG_SCHEMA = cv.All(
+    number.NUMBER_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(X9cNumber),
+            cv.Required(CONF_CS_PIN): pins.internal_gpio_output_pin_schema,
+            cv.Required(CONF_INC_PIN): pins.internal_gpio_output_pin_schema,
+            cv.Required(CONF_UD_PIN): pins.internal_gpio_output_pin_schema,
+            cv.Optional(CONF_INITIAL_VALUE, default=100): cv.int_range(min=1, max=100),
+        }
+    )
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await number.register_number(
+        var,
+        config,
+        min_value=1,
+        max_value=100,
+        step=1,
+    )
+
+    cs_pin = await cg.gpio_pin_expression(config[CONF_CS_PIN])
+    cg.add(var.set_cs_pin(cs_pin))
+    inc_pin = await cg.gpio_pin_expression(config[CONF_INC_PIN])
+    cg.add(var.set_inc_pin(inc_pin))
+    ud_pin = await cg.gpio_pin_expression(config[CONF_UD_PIN])
+    cg.add(var.set_ud_pin(ud_pin))
+
+    cg.add(var.set_initial_value(config[CONF_INITIAL_VALUE]))
---
+++ esphome/components/x9c/x9c.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/number/number.h"
+
+namespace esphome {
+namespace x9c {
+
+class X9cNumber : public number::Number, public Component {
+ public:
+  void set_cs_pin(InternalGPIOPin *pin) { cs_pin_ = pin; }
+  void set_inc_pin(InternalGPIOPin *pin) { inc_pin_ = pin; }
+  void set_ud_pin(InternalGPIOPin *pin) { ud_pin_ = pin; }
+  void set_initial_value(int initial_value) { initial_value_ = initial_value; }
+
+  void setup() override;
+  void dump_config() override;
+
+  void trim_value(int change_amount);
+
+protected:
+  void control(float value) override;
+  InternalGPIOPin *cs_pin_;
+  InternalGPIOPin *inc_pin_;
+  InternalGPIOPin *ud_pin_;
+  int initial_value_;
+};
+
+}  // namespace x9c
+}  // namespace esphome
