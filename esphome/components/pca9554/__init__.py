0ca8cee920e6e82b9ce74db909871f461f14abf0
hwstar <steve@rodgers619.com>
2022-12-14 18:03:36 -0800

    Format changes to __init__.py

--- esphome/components/pca9554/__init__.py
+++ esphome/components/pca9554/__init__.py
@@ -10,6 +10,7 @@ from esphome.const import (
     CONF_INVERTED,
     CONF_OUTPUT,
 )
+
 CODEOWNERS = ["@hwstar"]
 DEPENDENCIES = ["i2c"]
 MULTI_CONF = False
@@ -22,14 +23,13 @@ PCA9554GPIOPin = pca9554_ns.class_(
 
 CONF_PCA9554 = "pca9554"
 CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.Required(CONF_ID): cv.declare_id(PCA9554Component)
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(i2c.i2c_device_schema(0x20)) #Note: 0x20 for the non-A part. The PCA9554A parts start at addess 0x38
+  cv.Schema({cv.Required(CONF_ID): cv.declare_id(PCA9554Component)})
+  .extend(cv.COMPONENT_SCHEMA)
+  .extend( 
+  i2c.i2c_device_schema(0x20)
+  )  # Note: 0x20 for the non-A part. The PCA9554A parts start at addess 0x38
 )
+
 
 
 async def to_code(config):
