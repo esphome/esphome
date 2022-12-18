cbbc8c7179a0b41723984705c69e511c7446cf2f
Joakim Plate <elupus@ecce.se>
2022-12-18 15:06:03 +0100

    More lint fixes

--- esphome/components/mdns/__init__.py
+++ esphome/components/mdns/__init__.py
@@ -1,4 +1,10 @@
-from esphome.const import CONF_ID, CONF_PORT, CONF_NAME, CONF_PROTOCOL, CONF_SERVICES, CONF_SERVICE
+from esphome.const import (
+    CONF_ID,
+    CONF_PORT,
+    CONF_PROTOCOL,
+    CONF_SERVICES,
+    CONF_SERVICE,
+)
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.core import CORE, coroutine_with_priority
