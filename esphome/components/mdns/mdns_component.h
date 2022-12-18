7bb190fb468d7405e3b5e4a02e28d03c84e8f717
Joakim Plate <elupus@ecce.se>
2022-12-18 21:06:02 +0100

    Use rvalues to avoid copy

--- esphome/components/mdns/mdns_component.h
+++ esphome/components/mdns/mdns_component.h
@@ -33,7 +33,7 @@ class MDNSComponent : public Component {
 #endif
   float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
 
-  void add_extra_service(const MDNSService &service) { services_extra_.push_back(service); }
+  void add_extra_service(MDNSService service) { services_extra_.push_back(std::move(service)); }
 
  protected:
   std::vector<MDNSService> services_extra_{};
