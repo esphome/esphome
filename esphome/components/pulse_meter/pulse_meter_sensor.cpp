4739e8a3d5ecc18e4edf9f9f222de9ac4e2cc8e9
Trent Houliston <trent@houliston.me>
2022-12-28 23:55:44 +1100

    Refactor pulse_meter to better handle higher frequencies

--- esphome/components/pulse_meter/pulse_meter_sensor.cpp
+++ esphome/components/pulse_meter/pulse_meter_sensor.cpp
@@ -1,4 +1,5 @@
 #include "pulse_meter_sensor.h"
+#include <utility>
 #include "esphome/core/log.h"
 
 namespace esphome {
@@ -6,69 +7,86 @@ namespace pulse_meter {
 
 static const char *const TAG = "pulse_meter";
 
+void PulseMeterSensor::set_pin(InternalGPIOPin *pin) { this->pin_ = pin; }
+void PulseMeterSensor::set_filter_us(uint32_t filter) { this->filter_us_ = filter; }
+void PulseMeterSensor::set_timeout_us(uint32_t timeout) { this->timeout_us_ = timeout; }
+void PulseMeterSensor::set_total_sensor(sensor::Sensor *sensor) { this->total_sensor_ = sensor; }
+void PulseMeterSensor::set_filter_mode(InternalFilterMode mode) { this->filter_mode_ = mode; }
+
+void PulseMeterSensor::set_total_pulses(uint32_t pulses) { this->total_pulses_ = pulses; }
+
 void PulseMeterSensor::setup() {
   this->pin_->setup();
   this->isr_pin_ = pin_->to_isr();
-  this->pin_->attach_interrupt(PulseMeterSensor::gpio_intr, this, gpio::INTERRUPT_ANY_EDGE);
 
-  this->last_detected_edge_us_ = 0;
-  this->last_valid_edge_us_ = 0;
-  this->pulse_width_us_ = 0;
-  this->sensor_is_high_ = this->isr_pin_.digital_read();
-  this->has_valid_edge_ = false;
-  this->pending_state_change_ = NONE;
+  last_edge_candidate_us_ = 0;
+  in_edge_ = false;
+  this->last_pin_val_ = this->pin_->digital_read();
+
+  this->initialized_ = false;
+
+  this->get_->count_ = 0;
+  this->get_->last_detected_edge_us_ = 0;
+  this->set_->count_ = 0;
+  this->set_->last_detected_edge_us_ = 0;
+
+  this->get_ = this->state_;
+  this->set_ = this->state_ + 1;
+
+  if (this->filter_mode_ == FILTER_EDGE) {
+    this->pin_->attach_interrupt(PulseMeterSensor::edge_intr, this, gpio::INTERRUPT_RISING_EDGE);
+  } else if (this->filter_mode_ == FILTER_PULSE) {
+    this->pin_->attach_interrupt(PulseMeterSensor::pulse_intr, this, gpio::INTERRUPT_ANY_EDGE);
+  }
 }
-// In PULSE mode we set a flag (pending_state_change_) for every interrupt
-// that constitutes a state change. In the loop() method we check if a time
-// interval greater than the internal_filter time has passed without any
-// interrupts.
 
 void PulseMeterSensor::loop() {
-  // Get a snapshot of the needed volatile sensor values, to make sure they are not
-  // modified by the ISR while we are in the loop() method. If they are changed
-  // after we the variable "now" has been set, overflow will occur in the
-  // subsequent arithmetic
-  const bool has_valid_edge = this->has_valid_edge_;
-  const uint32_t last_detected_edge_us = this->last_detected_edge_us_;
-  const uint32_t last_valid_edge_us = this->last_valid_edge_us_;
-  // Get the current time after the snapshot of saved times
-  const uint32_t now = micros();
+  // Swap out set and get to make sure the ISR always keeps a consistent state
+  // last_update from get was the last_update from set the last time loop was run
+  uint32_t last_update = this->get_->last_detected_edge_us_;
+  this->get_->count_ = 0;
 
-  this->handle_state_change_(now, last_detected_edge_us, last_valid_edge_us, has_valid_edge);
+  // Swap pointers
+  auto *temp = this->set_;
+  this->set_ = this->get_;
+  this->get_ = temp;
 
-  // If we've exceeded our timeout interval without receiving any pulses, assume 0 pulses/min until
-  // we get at least two valid pulses.
-  const uint32_t time_since_valid_edge_us = now - last_detected_edge_us;
-  if ((has_valid_edge) && (time_since_valid_edge_us > this->timeout_us_)) {
-    ESP_LOGD(TAG, "No pulse detected for %us, assuming 0 pulses/min", time_since_valid_edge_us / 1000000);
+  // Add the count from the last loop to the total
+  this->total_pulses_ += this->get_->count_;
 
-    this->last_valid_edge_us_ = 0;
-    this->pulse_width_us_ = 0;
-    this->has_valid_edge_ = false;
-    this->last_detected_edge_us_ = 0;
-  }
+  // If nothing was detected on the most recent loop, move the old timestamp along
+  this->get_->last_detected_edge_us_ = this->get_->count_ == 0 ? last_update : this->get_->last_detected_edge_us_;
 
-  // We quantize our pulse widths to 1 ms to avoid unnecessary jitter
-  const uint32_t pulse_width_ms = this->pulse_width_us_ / 1000;
-  if (this->pulse_width_dedupe_.next(pulse_width_ms)) {
-    if (pulse_width_ms == 0) {
-      // Treat 0 pulse width as 0 pulses/min (normally because we've not detected any pulses for a while)
-      this->publish_state(0);
-    } else {
-      // Calculate pulses/min from the pulse width in ms
-      this->publish_state((60.0f * 1000.0f) / pulse_width_ms);
+  // We need to detect at least two edges to have a valid pulse width
+  if (!this->initialized_) {
+    if (this->get_->count_ > 0) {
+      this->initialized_ = true;
     }
+    return;
   }
 
-  if (this->total_sensor_ != nullptr) {
-    const uint32_t total = this->total_pulses_;
-    if (this->total_dedupe_.next(total)) {
+  // If the last_update values are the same there have not been any pulses since the last loop
+  if (last_update == this->get_->last_detected_edge_us_) {
+    const uint32_t now = micros();
+    const uint32_t time_since_valid_edge_us = now - this->get_->last_detected_edge_us_;
+    if (time_since_valid_edge_us > this->timeout_us_) {
+      ESP_LOGD(TAG, "No pulse detected for %us, assuming 0 pulses/min", time_since_valid_edge_us / 1000000);
+      this->initialized_ = false;
+      this->publish_state(0.0f);
+    }
+  } else {
+    uint32_t delta_us = this->get_->last_detected_edge_us_ - last_update;
+    float pulse_width_us = delta_us / float(this->get_->count_);
+    this->publish_state((60.0f * 1000000.0f) / pulse_width_us);
+
+    if (this->total_sensor_ != nullptr) {
+      const uint32_t total = this->total_pulses_;
       this->total_sensor_->publish_state(total);
     }
   }
 }
 
-void PulseMeterSensor::set_total_pulses(uint32_t pulses) { this->total_pulses_ = pulses; }
+float PulseMeterSensor::get_setup_priority() const { return setup_priority::DATA; }
 
 void PulseMeterSensor::dump_config() {
   LOG_SENSOR("", "Pulse Meter", this);
@@ -81,97 +99,39 @@ void PulseMeterSensor::dump_config() {
   ESP_LOGCONFIG(TAG, "  Assuming 0 pulses/min after not receiving a pulse for %us", this->timeout_us_ / 1000000);
 }
 
-void IRAM_ATTR PulseMeterSensor::gpio_intr(PulseMeterSensor *sensor) {
+void IRAM_ATTR PulseMeterSensor::edge_intr(PulseMeterSensor *sensor) {
+  // This is an interrupt handler - we can't call any virtual method from this method
+  // Get the current time before we do anything else so the measurements are consistent
+  const uint32_t now = micros();
+
+  if ((now - sensor->set_->last_detected_edge_us_) >= sensor->filter_us_) {
+    sensor->set_->last_detected_edge_us_ = now;
+    sensor->set_->count_++;
+  }
+}
+
+void IRAM_ATTR PulseMeterSensor::pulse_intr(PulseMeterSensor *sensor) {
   // This is an interrupt handler - we can't call any virtual method from this method
   // Get the current time before we do anything else so the measurements are consistent
   const uint32_t now = micros();
   const bool pin_val = sensor->isr_pin_.digital_read();
 
-  if (sensor->filter_mode_ == FILTER_EDGE) {
-    // We only look at rising edges
-    if (!pin_val) {
-      return;
-    }
-    // Check to see if we should filter this edge out
-    if ((now - sensor->last_detected_edge_us_) >= sensor->filter_us_) {
-      // Don't measure the first valid pulse (we need at least two pulses to measure the width)
-      if (sensor->has_valid_edge_) {
-        sensor->pulse_width_us_ = (now - sensor->last_valid_edge_us_);
-      }
-      sensor->total_pulses_++;
-      sensor->last_valid_edge_us_ = now;
-      sensor->has_valid_edge_ = true;
-    }
-    sensor->last_detected_edge_us_ = now;
-  } else {
-    // Filter Mode is PULSE
-    const uint32_t delta_t_us = now - sensor->last_detected_edge_us_;
-    // We need to check if we have missed to handle a state change in the
-    // loop() function. This can happen when the filter_us value is less than
-    // the loop() interval, which is ~50-60ms
-    // The section below is essentially a modified repeat of the
-    // handle_state_change method. Ideally i would refactor and call the
-    // method here as well. However functions called in ISRs need to meet
-    // strict criteria and I don't think the methos would meet them.
-    if (sensor->pending_state_change_ != NONE && (delta_t_us > sensor->filter_us_)) {
-      // We have missed to handle a state change in the loop function.
-      sensor->sensor_is_high_ = sensor->pending_state_change_ == TO_HIGH;
-      if (sensor->sensor_is_high_) {
-        // We need to handle a pulse that would have been missed by the loop function
-        sensor->total_pulses_++;
-        if (sensor->has_valid_edge_) {
-          sensor->pulse_width_us_ = sensor->last_detected_edge_us_ - sensor->last_valid_edge_us_;
-          sensor->has_valid_edge_ = true;
-          sensor->last_valid_edge_us_ = sensor->last_detected_edge_us_;
-        }
-      }
-    }  // End of checking for and handling of change in state
-
-    // Ignore false edges that may be caused by bouncing and exit the ISR ASAP
-    if (pin_val == sensor->sensor_is_high_) {
-      sensor->pending_state_change_ = NONE;
-      return;
-    }
-    sensor->pending_state_change_ = pin_val ? TO_HIGH : TO_LOW;
-    sensor->last_detected_edge_us_ = now;
+  // Rising edge
+  if (!sensor->last_pin_val_ && pin_val) {
+    sensor->last_edge_candidate_us_ = now;
+    sensor->in_edge_ = true;
   }
-}
-
-void PulseMeterSensor::handle_state_change_(uint32_t now, uint32_t last_detected_edge_us, uint32_t last_valid_edge_us,
-                                            bool has_valid_edge) {
-  if (this->pending_state_change_ == NONE) {
-    return;
-  }
-
-  const bool pin_val = this->isr_pin_.digital_read();
-  if (pin_val == this->sensor_is_high_) {
-    // Most likely caused by high frequency bouncing. Theoretically we should
-    // expect interrupts of alternating state. Here we are registering an
-    // interrupt with no change in state. Another interrupt will likely trigger
-    // just after this one and have an alternate state.
-    this->pending_state_change_ = NONE;
-    return;
-  }
-
-  if ((now - last_detected_edge_us) > this->filter_us_) {
-    this->sensor_is_high_ = pin_val;
-    ESP_LOGVV(TAG, "State is now %s", pin_val ? "high" : "low");
-
-    // Increment with valid rising edges only
-    if (pin_val) {
-      this->total_pulses_++;
-      ESP_LOGVV(TAG, "Incremented pulses to %u", this->total_pulses_);
-
-      if (has_valid_edge) {
-        this->pulse_width_us_ = last_detected_edge_us - last_valid_edge_us;
-        ESP_LOGVV(TAG, "Set pulse width to %u", this->pulse_width_us_);
-      }
-      this->has_valid_edge_ = true;
-      this->last_valid_edge_us_ = last_detected_edge_us;
-      ESP_LOGVV(TAG, "last_valid_edge_us_ is now %u", this->last_valid_edge_us_);
+  // Falling edge
+  else if (sensor->last_pin_val_ && !pin_val) {
+    // The last edge candidate must be after the last rising edge otherwise we already processed that pulse
+    // Then we check that the length of this pulse is greater than the filter time
+    if (sensor->in_edge_ && now - sensor->last_edge_candidate_us_ > sensor->filter_us_) {
+      sensor->set_->last_detected_edge_us_ = sensor->last_edge_candidate_us_;
+      sensor->set_->count_++;
     }
-    this->pending_state_change_ = NONE;
+    sensor->in_edge_ = false;
   }
+  sensor->last_pin_val_ = pin_val;
 }
 
 }  // namespace pulse_meter
--- esphome/components/pulse_meter/pulse_meter_sensor.h
+++ esphome/components/pulse_meter/pulse_meter_sensor.h
@@ -15,43 +15,47 @@ class PulseMeterSensor : public sensor::Sensor, public Component {
     FILTER_PULSE,
   };
 
-  void set_pin(InternalGPIOPin *pin) { this->pin_ = pin; }
-  void set_filter_us(uint32_t filter) { this->filter_us_ = filter; }
-  void set_filter_mode(InternalFilterMode mode) { this->filter_mode_ = mode; }
-  void set_timeout_us(uint32_t timeout) { this->timeout_us_ = timeout; }
-  void set_total_sensor(sensor::Sensor *sensor) { this->total_sensor_ = sensor; }
+  void set_pin(InternalGPIOPin *pin);
+  void set_filter_us(uint32_t filter);
+  void set_timeout_us(uint32_t timeout);
+  void set_total_sensor(sensor::Sensor *sensor);
+  void set_filter_mode(InternalFilterMode mode);
 
   void set_total_pulses(uint32_t pulses);
 
   void setup() override;
   void loop() override;
-  float get_setup_priority() const override { return setup_priority::DATA; }
+  float get_setup_priority() const override;
   void dump_config() override;
 
  protected:
-  enum StateChange { TO_LOW = 0, TO_HIGH, NONE };
-
-  static void gpio_intr(PulseMeterSensor *sensor);
-  void handle_state_change_(uint32_t now, uint32_t last_detected_edge_us, uint32_t last_valid_edge_us,
-                            bool has_valid_edge);
+  static void edge_intr(PulseMeterSensor *sensor);
+  static void pulse_intr(PulseMeterSensor *sensor);
 
   InternalGPIOPin *pin_{nullptr};
-  ISRInternalGPIOPin isr_pin_;
   uint32_t filter_us_ = 0;
   uint32_t timeout_us_ = 1000000UL * 60UL * 5UL;
   sensor::Sensor *total_sensor_{nullptr};
   InternalFilterMode filter_mode_{FILTER_EDGE};
 
-  Deduplicator<uint32_t> pulse_width_dedupe_;
-  Deduplicator<uint32_t> total_dedupe_;
+  struct State {
+    uint32_t last_detected_edge_us_ = 0;
+    uint32_t count_ = 0;
+  };
 
-  volatile uint32_t last_detected_edge_us_ = 0;
-  volatile uint32_t last_valid_edge_us_ = 0;
-  volatile uint32_t pulse_width_us_ = 0;
-  volatile uint32_t total_pulses_ = 0;
-  volatile bool sensor_is_high_ = false;
-  volatile bool has_valid_edge_ = false;
-  volatile StateChange pending_state_change_{NONE};
+  State state_[2];
+
+  volatile State *set_ = state_;
+  volatile State *get_ = state_ + 1;
+
+  bool initialized_ = false;
+  uint32_t total_pulses_ = 0;
+
+  // Only use these variables in the ISR
+  ISRInternalGPIOPin isr_pin_;
+  uint32_t last_edge_candidate_us_ = 0;
+  bool in_edge_ = false;
+  bool last_pin_val_ = false;
 };
 
 }  // namespace pulse_meter
