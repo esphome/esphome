b1af78fb94ebbdbc9495290c6fdc77658a6ced2d
Vincent Schmandt <vincentscode@gmail.com>
2022-12-30 16:00:17 +0100

    Update code style

--- esphome/components/ens160/ens160.cpp
+++ esphome/components/ens160/ens160.cpp
@@ -14,79 +14,52 @@ namespace ens160 {
 
 static const char *const TAG = "ens160";
 
-#define ENS160_PARTID				0x6001
-#define ENS160_BOOTING				10
+static const uint16_t ENS160_PARTID = 0x6001;
+static const uint8_t ENS160_BOOTING = 10;
 
-// ENS160 registers for version V0
-#define ENS160_REG_PART_ID          0x00 		// 2 byte register
-#define ENS160_REG_OPMODE			0x10
-#define ENS160_REG_CONFIG			0x11
-#define ENS160_REG_COMMAND			0x12
-#define ENS160_REG_TEMP_IN			0x13
-#define ENS160_REG_RH_IN			0x15
-#define ENS160_REG_DATA_STATUS		0x20
-#define ENS160_REG_DATA_AQI			0x21
-#define ENS160_REG_DATA_TVOC		0x22
-#define ENS160_REG_DATA_ECO2		0x24			
-#define ENS160_REG_DATA_BL			0x28
-#define ENS160_REG_DATA_T			0x30
-#define ENS160_REG_DATA_RH			0x32
-#define ENS160_REG_DATA_MISR		0x38
-#define ENS160_REG_GPR_WRITE_0		0x40
-#define ENS160_REG_GPR_WRITE_1		ENS160_REG_GPR_WRITE_0 + 1
-#define ENS160_REG_GPR_WRITE_2		ENS160_REG_GPR_WRITE_0 + 2
-#define ENS160_REG_GPR_WRITE_3		ENS160_REG_GPR_WRITE_0 + 3
-#define ENS160_REG_GPR_WRITE_4		ENS160_REG_GPR_WRITE_0 + 4
-#define ENS160_REG_GPR_WRITE_5		ENS160_REG_GPR_WRITE_0 + 5
-#define ENS160_REG_GPR_WRITE_6		ENS160_REG_GPR_WRITE_0 + 6
-#define ENS160_REG_GPR_WRITE_7		ENS160_REG_GPR_WRITE_0 + 7
-#define ENS160_REG_GPR_READ_0		0x48
-#define ENS160_REG_GPR_READ_4		ENS160_REG_GPR_READ_0 + 4
-#define ENS160_REG_GPR_READ_6		ENS160_REG_GPR_READ_0 + 6
-#define ENS160_REG_GPR_READ_7		ENS160_REG_GPR_READ_0 + 7
+static const uint8_t ENS160_REG_PART_ID = 0x00;
+static const uint8_t ENS160_REG_OPMODE = 0x10;
+static const uint8_t ENS160_REG_CONFIG = 0x11;
+static const uint8_t ENS160_REG_COMMAND = 0x12;
+static const uint8_t ENS160_REG_TEMP_IN = 0x13;
+static const uint8_t ENS160_REG_RH_IN = 0x15;
+static const uint8_t ENS160_REG_DATA_STATUS = 0x20;
+static const uint8_t ENS160_REG_DATA_AQI = 0x21;
+static const uint8_t ENS160_REG_DATA_TVOC = 0x22;
+static const uint8_t ENS160_REG_DATA_ECO2 = 0x24;
+static const uint8_t ENS160_REG_DATA_BL = 0x28;
+static const uint8_t ENS160_REG_DATA_T = 0x30;
+static const uint8_t ENS160_REG_DATA_RH = 0x32;
+static const uint8_t ENS160_REG_DATA_MISR = 0x38;
+static const uint8_t ENS160_REG_GPR_WRITE_0 = 0x40;
+static const uint8_t ENS160_REG_GPR_WRITE_1 = ENS160_REG_GPR_WRITE_0 + 1;
+static const uint8_t ENS160_REG_GPR_WRITE_2 = ENS160_REG_GPR_WRITE_0 + 2;
+static const uint8_t ENS160_REG_GPR_WRITE_3 = ENS160_REG_GPR_WRITE_0 + 3;
+static const uint8_t ENS160_REG_GPR_WRITE_4 = ENS160_REG_GPR_WRITE_0 + 4;
+static const uint8_t ENS160_REG_GPR_WRITE_5 = ENS160_REG_GPR_WRITE_0 + 5;
+static const uint8_t ENS160_REG_GPR_WRITE_6 = ENS160_REG_GPR_WRITE_0 + 6;
+static const uint8_t ENS160_REG_GPR_WRITE_7 = ENS160_REG_GPR_WRITE_0 + 7;
+static const uint8_t ENS160_REG_GPR_READ_0 = 0x48;
+static const uint8_t ENS160_REG_GPR_READ_4 = ENS160_REG_GPR_READ_0 + 4;
+static const uint8_t ENS160_REG_GPR_READ_6 = ENS160_REG_GPR_READ_0 + 6;
+static const uint8_t ENS160_REG_GPR_READ_7 = ENS160_REG_GPR_READ_0 + 7;
 
-//ENS160 data register fields
-#define ENS160_COMMAND_NOP			0x00
-#define ENS160_COMMAND_CLRGPR		0xCC
-#define ENS160_COMMAND_GET_APPVER	0x0E 
-#define ENS160_COMMAND_SETTH		0x02
-#define ENS160_COMMAND_SETSEQ		0xC2
+static const uint8_t ENS160_COMMAND_NOP = 0x00;
+static const uint8_t ENS160_COMMAND_CLRGPR = 0xCC;
+static const uint8_t ENS160_COMMAND_GET_APPVER = 0x0E;
 
-#define ENS160_OPMODE_RESET			0xF0
-#define ENS160_OPMODE_DEP_SLEEP		0x00
-#define ENS160_OPMODE_IDLE			0x01
-#define ENS160_OPMODE_STD			0x02
-#define ENS160_OPMODE_INTERMEDIATE	0x03	
-#define ENS160_OPMODE_CUSTOM		0xC0
-#define ENS160_OPMODE_D0			0xD0
-#define ENS160_OPMODE_D1			0xD1
-#define ENS160_OPMODE_BOOTLOADER	0xB0
+static const uint8_t ENS160_OPMODE_RESET = 0xF0;
+static const uint8_t ENS160_OPMODE_IDLE = 0x01;
+static const uint8_t ENS160_OPMODE_STD = 0x02;
 
-#define ENS160_BL_CMD_START			0x02
-#define ENS160_BL_CMD_ERASE_APP		0x04
-#define ENS160_BL_CMD_ERASE_BLINE	0x06
-#define ENS160_BL_CMD_WRITE			0x08
-#define ENS160_BL_CMD_VERIFY		0x0A
-#define ENS160_BL_CMD_GET_BLVER		0x0C
-#define ENS160_BL_CMD_GET_APPVER	0x0E
-#define ENS160_BL_CMD_EXITBL		0x12
+static const uint8_t ENS160_DATA_STATUS_STATER = 0x07;
+static const uint8_t ENS160_DATA_STATUS_NEWDAT = 0x02;
+static const uint8_t ENS160_DATA_STATUS_NEWGPR = 0x01;
 
-#define ENS160_SEQ_ACK_NOTCOMPLETE	0x80
-#define ENS160_SEQ_ACK_COMPLETE		0xC0
-
-#define IS_ENS160_SEQ_ACK_NOT_COMPLETE(x) 	(ENS160_SEQ_ACK_NOTCOMPLETE == (ENS160_SEQ_ACK_NOTCOMPLETE & (x)))
-#define IS_ENS160_SEQ_ACK_COMPLETE(x) 		(ENS160_SEQ_ACK_COMPLETE == (ENS160_SEQ_ACK_COMPLETE & (x)))
-
-#define ENS160_DATA_STATUS_STATER	0x07
-#define ENS160_DATA_STATUS_NEWDAT	0x02
-#define ENS160_DATA_STATUS_NEWGPR	0x01
-
+#define ENS160_CONCAT_BYTES(msb, lsb)   (((uint16_t)msb << 8) | (uint16_t)lsb)
 #define IS_NEWDAT(x) 			(ENS160_DATA_STATUS_NEWDAT == (ENS160_DATA_STATUS_NEWDAT & (x)))
 #define IS_NEWGPR(x) 			(ENS160_DATA_STATUS_NEWGPR == (ENS160_DATA_STATUS_NEWGPR & (x)))
 #define IS_NEW_DATA_AVAILABLE(x) 	(0 != ((ENS160_DATA_STATUS_NEWDAT | ENS160_DATA_STATUS_NEWGPR ) & (x)))
-
-#define CONVERT_RS_RAW2OHMS_I(x) 	(1 << ((x) >> 11))
-#define CONVERT_RS_RAW2OHMS_F(x) 	(pow (2, (float)(x) / 2048))
 
 #define CHECK_TRUE(f, error_code) \
   if (!(f)) { \
@@ -166,11 +139,16 @@ void ENS160Component::update() {
     return;
   }
 
-	uint8_t i2cbuf[8];
-  this->read_bytes(ENS160_REG_DATA_AQI, i2cbuf, 7);
-  uint8_t _data_tvoc = i2cbuf[1] | ((uint16_t)i2cbuf[2] << 8);
-  uint8_t _data_eco2 = i2cbuf[3] | ((uint16_t)i2cbuf[4] << 8);
-  uint8_t _data_aqi = i2cbuf[0];
+  uint8_t _buf_eco2[2];
+  uint8_t _buf_tvoc[2];
+  uint8_t _data_aqi = 0;
+  this->read_register(ENS160_REG_DATA_ECO2, _buf_eco2, sizeof(_buf_eco2));
+  this->read_register(ENS160_REG_DATA_TVOC, _buf_tvoc, sizeof(_buf_tvoc));
+  this->read_register(ENS160_REG_DATA_AQI, &_data_aqi, sizeof(_data_aqi));
+
+  uint16_t _data_eco2 = ENS160_CONCAT_BYTES(_buf_eco2[1], _buf_eco2[0]);
+  uint16_t _data_tvoc = ENS160_CONCAT_BYTES(_buf_tvoc[1], _buf_tvoc[0]);
+
   if (this->co2_ != nullptr) this->co2_->publish_state(_data_eco2);
   if (this->tvoc_ != nullptr) this->tvoc_->publish_state(_data_tvoc);
   if (this->aqi_ != nullptr) this->aqi_->publish_state(_data_aqi);
