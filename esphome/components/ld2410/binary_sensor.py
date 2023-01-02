5848b076094680bc508f0bb7d7e41d7cd81965bc
seb cc <sebastien.carecolin@gmail.com>
2023-01-02 17:05:05 +0000

    update binaty sensor device class

--- esphome/components/ld2410/binary_sensor.py
+++ esphome/components/ld2410/binary_sensor.py
@@ -1,10 +1,7 @@
 import esphome.codegen as cg
 from esphome.components import binary_sensor
 import esphome.config_validation as cv
-from esphome.const import (
-    DEVICE_CLASS_MOTION,
-    DEVICE_CLASS_PRESENCE,
-)
+from esphome.const import DEVICE_CLASS_MOTION, DEVICE_CLASS_OCCUPANCY
 from . import CONF_LD2410_ID, LD2410Component
 
 DEPENDENCIES = ["ld2410"]
@@ -15,13 +12,13 @@ CONF_HAS_STILL_TARGET = "has_still_target"
 CONFIG_SCHEMA = {
     cv.GenerateID(CONF_LD2410_ID): cv.use_id(LD2410Component),
     cv.Optional(CONF_HAS_TARGET): binary_sensor.binary_sensor_schema(
-        device_class=DEVICE_CLASS_PRESENCE
+        device_class=DEVICE_CLASS_OCCUPANCY
     ),
     cv.Optional(CONF_HAS_MOVING_TARGET): binary_sensor.binary_sensor_schema(
         device_class=DEVICE_CLASS_MOTION
     ),
     cv.Optional(CONF_HAS_STILL_TARGET): binary_sensor.binary_sensor_schema(
-        device_class=DEVICE_CLASS_PRESENCE
+        device_class=DEVICE_CLASS_OCCUPANCY
     ),
 }
 
