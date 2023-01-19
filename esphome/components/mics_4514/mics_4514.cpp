c139842c404c6d2080e7b2e99f5bf02863c6dcde
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-01-20 08:51:42 +1300

    Update logging

--- esphome/components/mics_4514/mics_4514.cpp
+++ esphome/components/mics_4514/mics_4514.cpp
@@ -12,20 +12,36 @@ static const uint8_t SENSOR_REGISTER = 0x04;
 static const uint8_t POWER_MODE_REGISTER = 0x0a;
 
 void MICS4514Component::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up MICS 4514...");
   uint8_t power_mode;
   this->read_register(POWER_MODE_REGISTER, &power_mode, 1);
   if (power_mode == 0x00) {
-    ESP_LOGD(TAG, "Waking up MICS 4514");
+    ESP_LOGCONFIG(TAG, "Waking up MICS 4514");
     power_mode = 0x01;
     this->write_register(POWER_MODE_REGISTER, &power_mode, 1);
     delay(100);  // NOLINT
-    this->set_timeout("warmup", 3 * 60 * 1000, [this]() { this->warmed_up_ = true; });
+    this->set_timeout("warmup", 3 * 60 * 1000, [this]() {
+      this->warmed_up_ = true;
+      ESP_LOGCONFIG(TAG, "MICS 4514 setup complete.");
+    });
     this->status_set_warning();
     return;
   }
+  ESP_LOGCONFIG(TAG, "Device already awake.");
   this->warmed_up_ = true;
+  ESP_LOGCONFIG(TAG, "MICS 4514 setup complete.");
 }
-void MICS4514Component::dump_config() {}
+void MICS4514Component::dump_config() {
+  ESP_LOGCONFIG(TAG, "MICS 4514:");
+  LOG_I2C_DEVICE(this);
+  LOG_UPDATE_INTERVAL(this);
+  LOG_SENSOR("  ", "Nitrogen Dioxide", this->nitrogen_dioxide_sensor_);
+  LOG_SENSOR("  ", "Carbon Monoxide", this->carbon_monoxide_sensor_);
+  LOG_SENSOR("  ", "Methane", this->methane_sensor_);
+  LOG_SENSOR("  ", "Ethanol", this->ethanol_sensor_);
+  LOG_SENSOR("  ", "Hydrogen", this->hydrogen_sensor_);
+  LOG_SENSOR("  ", "Ammonia", this->ammonia_sensor_);
+}
 float MICS4514Component::get_setup_priority() const { return setup_priority::DATA; }
 void MICS4514Component::update() {
   if (!this->warmed_up_) {
@@ -37,7 +53,7 @@ void MICS4514Component::update() {
     return;
   }
   this->status_clear_warning();
-  ESP_LOGD(TAG, "Got data: %02X %02X %02X %02X %02X %02X", data[0], data[1], data[2], data[3], data[4], data[5]);
+  ESP_LOGV(TAG, "Got data: %02X %02X %02X %02X %02X %02X", data[0], data[1], data[2], data[3], data[4], data[5]);
   uint16_t ox = encode_uint16(data[0], data[1]);
   uint16_t red = encode_uint16(data[2], data[3]);
   uint16_t power = encode_uint16(data[4], data[5]);
