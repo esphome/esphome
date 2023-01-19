cb500620cdb6e74eb7d6cacb21a69e50d86c71d6
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2023-01-19 19:02:01 +0100

    libretuya: enable MQTT compilation

--- esphome/components/mqtt/mqtt_client.cpp
+++ esphome/components/mqtt/mqtt_client.cpp
@@ -91,7 +91,7 @@ void MQTTClientComponent::start_dnslookup_() {
   this->dns_resolve_error_ = false;
   this->dns_resolved_ = false;
   ip_addr_t addr;
-#ifdef USE_ESP32
+#if defined(USE_ESP32) || defined(USE_LIBRETUYA)
   err_t err = dns_gethostbyname_addrtype(this->credentials_.address.c_str(), &addr,
                                          MQTTClientComponent::dns_found_callback, this, LWIP_DNS_ADDRTYPE_IPV4);
 #endif
@@ -103,10 +103,9 @@ void MQTTClientComponent::start_dnslookup_() {
     case ERR_OK: {
       // Got IP immediately
       this->dns_resolved_ = true;
-#ifdef USE_ESP32
+#if LWIP_IPV4 && LWIP_IPV6
       this->ip_ = addr.u_addr.ip4.addr;
-#endif
-#ifdef USE_ESP8266
+#else
       this->ip_ = addr.addr;
 #endif
       this->start_connect_();
@@ -159,10 +158,9 @@ void MQTTClientComponent::dns_found_callback(const char *name, const ip_addr_t *
   if (ipaddr == nullptr) {
     a_this->dns_resolve_error_ = true;
   } else {
-#ifdef USE_ESP32
+#if LWIP_IPV4 && LWIP_IPV6
     a_this->ip_ = ipaddr->u_addr.ip4.addr;
-#endif
-#ifdef USE_ESP8266
+#else
     a_this->ip_ = ipaddr->addr;
 #endif
     a_this->dns_resolved_ = true;
