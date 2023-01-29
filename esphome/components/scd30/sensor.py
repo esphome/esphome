066160dfcbe63291798b2a155e283089589b87c2
Florian Trück <ftrueck@gmail.com>
2023-01-29 14:23:19 +0100

    Update sensor.py
    py-lint...

--- esphome/components/scd30/sensor.py
+++ esphome/components/scd30/sensor.py
@@ -114,7 +114,7 @@ async def to_code(config):
         sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
         cg.add(var.set_temperature_sensor(sens))
 
-        
+
 @automation.register_action(
     "scd30.force_recalibration_with_reference",
     ForceRecalibrationWithReference,
@@ -127,7 +127,6 @@ async def to_code(config):
         }
     ),
 )
-
 async def scd30_force_recalibration_with_reference_to_code(
     config, action_id, template_arg, args
 ):
