aced9ec97e6d4102755170aa34336bcdb54a85be
Tr4um4z <82873922+Tr4um4z@users.noreply.github.com>
2023-02-01 23:12:03 +0100

    Update pid_climate.cpp
    Some fixes

--- esphome/components/pid/pid_climate.cpp
+++ esphome/components/pid/pid_climate.cpp
@@ -94,16 +94,12 @@ void PIDClimate::write_output_(float value) {
 
   // Update action variable for user feedback what's happening
   climate::ClimateAction new_action;
-  if (this->mode == 2 && value < 0) {
+  if (this->supports_cool_() && value < 0 && (this->mode == 2 || this->mode == 1)) {
     new_action = climate::CLIMATE_ACTION_COOLING;
-  } else if (this->mode == 3 && value > 0) {
+  } else if (this->supports_heat_() && value > 0 && (this->mode == 3 || this->mode == 1)) {
     new_action = climate::CLIMATE_ACTION_HEATING;
   } else if (this->mode == climate::CLIMATE_MODE_OFF) {
     new_action = climate::CLIMATE_ACTION_OFF;
-  } else if (this->mode == 1 && value > 0) {
-    new_action = climate::CLIMATE_ACTION_HEATING;
-  } else if (this->mode == 1 && value < 0) {
-    new_action = climate::CLIMATE_ACTION_COOLING;
   } else {
     new_action = climate::CLIMATE_ACTION_IDLE;
   }
