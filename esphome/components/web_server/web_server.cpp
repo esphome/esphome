9eca875b0447067e52ab84592a7f531e0e0ac5c1
dentra <dennis.trachuk@gmail.com>
2023-02-03 17:48:28 +0000

    temp web_server fix to be compat with 2022.12

--- esphome/components/web_server/web_server.cpp
+++ esphome/components/web_server/web_server.cpp
@@ -6,6 +6,7 @@
 #include "esphome/core/entity_base.h"
 #include "esphome/core/log.h"
 #include "esphome/core/util.h"
+#include "esphome/core/version.h"
 
 #ifdef USE_ARDUINO
 #include "StreamString.h"
