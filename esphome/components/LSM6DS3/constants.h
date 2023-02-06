801d3b44b263df142cbf27f705edb7d4bb1e0b65
Nate Lust <natelust@linux.com>
2023-02-06 17:30:01 -0500

    More formatting changes

--- esphome/components/LSM6DS3/constants.h
+++ esphome/components/LSM6DS3/constants.h
@@ -4,50 +4,49 @@
 namespace esphome {
 namespace LSM6DS3 {
 
-static const uint8_t LSM6DS3_ACC_GYRO_CTRL1_XL =             0X10;
-static const uint8_t LSM6DS3_ACC_GYRO_CTRL4_C =              0X13;
-static const uint8_t LSM6DS3_ACC_GYRO_BW_SCAL_ODR_ENABLED =  0x80;
-static const uint8_t LSM6DS3_PERF_CTRL6_C =                  0x15;
-static const uint8_t LSM6DS3_HIGH_PEF_DISABLE =              0x10;
+static const uint8_t LSM6DS3_ACC_GYRO_CTRL1_XL = 0X10;
+static const uint8_t LSM6DS3_ACC_GYRO_CTRL4_C = 0X13;
+static const uint8_t LSM6DS3_ACC_GYRO_BW_SCAL_ODR_ENABLED = 0x80;
+static const uint8_t LSM6DS3_PERF_CTRL6_C = 0x15;
+static const uint8_t LSM6DS3_HIGH_PEF_DISABLE = 0x10;
 
-static const uint8_t LSM6DS3_ACC_GYRO_BW_XL_100Hz =  0x02;
-static const uint8_t LSM6DS3_ACC_GYRO_FS_XL_16g =    0x04;
+static const uint8_t LSM6DS3_ACC_GYRO_BW_XL_100Hz = 0x02;
+static const uint8_t LSM6DS3_ACC_GYRO_FS_XL_16g = 0x04;
 static const uint8_t LSM6DS3_ACC_GYRO_FS_G_2000dps = 0x0C;
-static const uint8_t LSM6DS3_ACC_GYRO_CTRL2_G =      0X11;
+static const uint8_t LSM6DS3_ACC_GYRO_CTRL2_G = 0X11;
 static const uint8_t LSM6DS3_ACC_GYRO_WHO_AM_I_REG = 0X0F;
-static const uint8_t LSM6DS3_ACC_GYRO_WHO_AM_I =     0X69;
-static const uint8_t LSM6DS3_C_ACC_GYRO_WHO_AM_I =   0x6A;
-static const uint8_t LSM6DS3_ACC_GYRO_OUTX_L_XL =    0X28;
-static const uint8_t LSM6DS3_ACC_GYRO_OUTY_L_XL =    0X2A;
-static const uint8_t LSM6DS3_ACC_GYRO_OUTZ_L_XL =    0X2C;
-static const uint8_t LSM6DS3_ACC_GYRO_OUTX_L_G =     0X22;
-static const uint8_t LSM6DS3_ACC_GYRO_OUTY_L_G =     0X24;
-static const uint8_t LSM6DS3_ACC_GYRO_OUTZ_L_G =     0X26;
-static const uint8_t LSM6DS3_ACC_GYRO_OUT_TEMP_L =   0X20;
+static const uint8_t LSM6DS3_ACC_GYRO_WHO_AM_I = 0X69;
+static const uint8_t LSM6DS3_C_ACC_GYRO_WHO_AM_I = 0x6A;
+static const uint8_t LSM6DS3_ACC_GYRO_OUTX_L_XL = 0X28;
+static const uint8_t LSM6DS3_ACC_GYRO_OUTY_L_XL = 0X2A;
+static const uint8_t LSM6DS3_ACC_GYRO_OUTZ_L_XL = 0X2C;
+static const uint8_t LSM6DS3_ACC_GYRO_OUTX_L_G = 0X22;
+static const uint8_t LSM6DS3_ACC_GYRO_OUTY_L_G = 0X24;
+static const uint8_t LSM6DS3_ACC_GYRO_OUTZ_L_G = 0X26;
+static const uint8_t LSM6DS3_ACC_GYRO_OUT_TEMP_L = 0X20;
 
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_POWER_DOWN = 0x00;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_13Hz = 0x10;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_26Hz = 0x20;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_52Hz = 0x30;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_104Hz = 0x40;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_208Hz = 0x50;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_416Hz = 0x60;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_833Hz = 0x70;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_1660Hz = 0x80;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_3330Hz = 0x90;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_6660Hz = 0xA0;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_13330Hz = 0xB0;
 
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_POWER_DOWN =    0x00;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_13Hz =          0x10;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_26Hz =          0x20;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_52Hz =          0x30;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_104Hz =         0x40;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_208Hz =         0x50;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_416Hz =         0x60;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_833Hz =         0x70;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_1660Hz =        0x80;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_3330Hz =        0x90;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_6660Hz =        0xA0;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_XL_13330Hz =       0xB0;
-
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_POWER_DOWN =     0x00;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_13Hz =           0x10;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_26Hz =           0x20;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_52Hz =           0x30;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_104Hz =          0x40;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_208Hz =          0x50;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_416Hz =          0x60;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_833Hz =          0x70;
-static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_1660Hz =         0x80;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_POWER_DOWN = 0x00;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_13Hz = 0x10;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_26Hz = 0x20;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_52Hz = 0x30;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_104Hz = 0x40;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_208Hz = 0x50;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_416Hz = 0x60;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_833Hz = 0x70;
+static const uint8_t LSM6DS3_ACC_GYRO_ODR_G_1660Hz = 0x80;
 
 }  // namespace LSM6DS3
 }  // namespace esphome
--- esphome/components/LSM6DS3/lsm6ds3.h
+++ esphome/components/LSM6DS3/lsm6ds3.h
@@ -7,52 +7,52 @@
 namespace esphome {
 namespace LSM6DS3 {
 
-enum SensorType {accel, gyro, temp};
+enum SensorType { accel, gyro, temp };
 
 class LSM6DS3Component : public PollingComponent, public i2c::I2CDevice {
-  public:
-    void setup() override;
-    void dump_config() override;
+ public:
+  void setup() override;
+  void dump_config() override;
 
-    void update() override;
+  void update() override;
 
-    float get_setup_priority() const override;
+  float get_setup_priority() const override;
 
-    void set_accel_x_sensor(sensor::Sensor *accel_x_sensor) { accel_x_sensor_ = accel_x_sensor; }
-    void set_accel_y_sensor(sensor::Sensor *accel_y_sensor) { accel_y_sensor_ = accel_y_sensor; }
-    void set_accel_z_sensor(sensor::Sensor *accel_z_sensor) { accel_z_sensor_ = accel_z_sensor; }
-    void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
-    void set_gyro_x_sensor(sensor::Sensor *gyro_x_sensor) { gyro_x_sensor_ = gyro_x_sensor; }
-    void set_gyro_y_sensor(sensor::Sensor *gyro_y_sensor) { gyro_y_sensor_ = gyro_y_sensor; }
-    void set_gyro_z_sensor(sensor::Sensor *gyro_z_sensor) { gyro_z_sensor_ = gyro_z_sensor; }
+  void set_accel_x_sensor(sensor::Sensor *accel_x_sensor) { accel_x_sensor_ = accel_x_sensor; }
+  void set_accel_y_sensor(sensor::Sensor *accel_y_sensor) { accel_y_sensor_ = accel_y_sensor; }
+  void set_accel_z_sensor(sensor::Sensor *accel_z_sensor) { accel_z_sensor_ = accel_z_sensor; }
+  void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
+  void set_gyro_x_sensor(sensor::Sensor *gyro_x_sensor) { gyro_x_sensor_ = gyro_x_sensor; }
+  void set_gyro_y_sensor(sensor::Sensor *gyro_y_sensor) { gyro_y_sensor_ = gyro_y_sensor; }
+  void set_gyro_z_sensor(sensor::Sensor *gyro_z_sensor) { gyro_z_sensor_ = gyro_z_sensor; }
 
-    void set_high_perf(bool high_perf) {this->_high_perf = high_perf;}
-    void set_sample_accl_rate(uint16_t sample_rate) {this->_sample_accl_rate = sample_rate;}
-    void set_sample_gyro_rate(uint16_t sample_rate) {this->_sample_gyro_rate = sample_rate;}
-    void set_power_save(bool power_save) {this->_power_save = power_save;}
+  void set_high_perf(bool high_perf) { this->_high_perf = high_perf; }
+  void set_sample_accl_rate(uint16_t sample_rate) { this->_sample_accl_rate = sample_rate; }
+  void set_sample_gyro_rate(uint16_t sample_rate) { this->_sample_gyro_rate = sample_rate; }
+  void set_power_save(bool power_save) { this->_power_save = power_save; }
 
-  protected:
-    sensor::Sensor *accel_x_sensor_{nullptr};
-    sensor::Sensor *accel_y_sensor_{nullptr};
-    sensor::Sensor *accel_z_sensor_{nullptr};
-    sensor::Sensor *temperature_sensor_{nullptr};
-    sensor::Sensor *gyro_x_sensor_{nullptr};
-    sensor::Sensor *gyro_y_sensor_{nullptr};
-    sensor::Sensor *gyro_z_sensor_{nullptr};
+ protected:
+  sensor::Sensor *accel_x_sensor_{nullptr};
+  sensor::Sensor *accel_y_sensor_{nullptr};
+  sensor::Sensor *accel_z_sensor_{nullptr};
+  sensor::Sensor *temperature_sensor_{nullptr};
+  sensor::Sensor *gyro_x_sensor_{nullptr};
+  sensor::Sensor *gyro_y_sensor_{nullptr};
+  sensor::Sensor *gyro_z_sensor_{nullptr};
 
-    uint16_t temp_sensitivity = 16;
-    bool _high_perf = 0;
-    uint16_t _sample_gyro_rate = 0;
-    uint16_t _sample_accl_rate = 0;
-    bool _power_save = 0;
-    bool _do_sleep = 0;
-    bool _has_accl_temp = 0;
-    bool _has_gyro = 0;
-    bool _is_sleeping = 0;
-    uint8_t accl_conf = 0;
-    uint8_t gyro_conf = 0;
+  uint16_t temp_sensitivity = 16;
+  bool _high_perf = 0;
+  uint16_t _sample_gyro_rate = 0;
+  uint16_t _sample_accl_rate = 0;
+  bool _power_save = 0;
+  bool _do_sleep = 0;
+  bool _has_accl_temp = 0;
+  bool _has_gyro = 0;
+  bool _is_sleeping = 0;
+  uint8_t accl_conf = 0;
+  uint8_t gyro_conf = 0;
 
-    void _read_sensor(uint8_t reg, sensor::Sensor * sensor, SensorType type, bool do_publish);
+  void _read_sensor(uint8_t reg, sensor::Sensor *sensor, SensorType type, bool do_publish);
 };
 ;
 
