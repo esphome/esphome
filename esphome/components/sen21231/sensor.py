0c73d4f3c67485cd862c8bdf1f26fbef2fb22f1f
Shreyas Karnik <karnik.shreyas@gmail.com>
2023-02-16 19:47:55 -0800

    remove unused import

--- esphome/components/sen21231/sensor.py
+++ esphome/components/sen21231/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import CONF_ID, ICON_MOTION_SENSOR, UNIT_EMPTY
+from esphome.const import ICON_MOTION_SENSOR, UNIT_EMPTY
 
 CODEOWNERS = ["@shreyaskarnik"]
 DEPENDENCIES = ["i2c"]
