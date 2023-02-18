f991f06915fb575a46ca8d9205ececb73b4f0ab2
Orestes Mersinias <orestis.mers@gmail.com>
2023-02-18 15:03:53 +0000

    Format climate.py

--- esphome/components/gree/climate.py
+++ esphome/components/gree/climate.py
@@ -1,10 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import (
-    CONF_ID,
-    CONF_MODEL
-)
+from esphome.const import CONF_ID, CONF_MODEL
 
 CODEOWNERS = ["@orestismers"]
 
