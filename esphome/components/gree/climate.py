4161499b4e429a282a2e5863a0eb120204ae560e
orestismers <33354671+orestismers@users.noreply.github.com>
2023-02-18 15:20:29 +0200

    More fixes

--- esphome/components/gree/climate.py
+++ esphome/components/gree/climate.py
@@ -4,7 +4,7 @@ from esphome.components import climate_ir
 from esphome.const import (
     CONF_ID,
     CONF_MODEL,
-    CONF_TEMPERATURE_SOURCE
+    CONF_REFERENCE_TEMPERATURE
 )
 
 AUTO_LOAD = ["climate_ir"]
@@ -24,7 +24,7 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(GreeClimate),
         cv.Required(CONF_MODEL): cv.enum(MODELS),
-        cv.Optional(CONF_TEMPERATURE_SOURCE): cv.boolean()
+        cv.Optional(CONF_REFERENCE_TEMPERATURE): cv.boolean()
     }
 )
 
@@ -32,10 +32,10 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
 
-    if CONF_TEMPERATURE_SOURCE not in config:
-        config[CONF_TEMPERATURE_SOURCE] = False
+    if CONF_REFERENCE_TEMPERATURE not in config:
+        config[CONF_REFERENCE_TEMPERATURE] = False
 
     cg.add(var.set_model(config[CONF_MODEL]))
-    cg.add(var.set_iFeel(config[CONF_TEMPERATURE_SOURCE]))
+    cg.add(var.set_iFeel(config[CONF_REFERENCE_TEMPERATURE]))
 
     await climate_ir.register_climate_ir(var, config)
