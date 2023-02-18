528273127e1da7015d618bcc9da8e4abe03c6c55
orestismers <33354671+orestismers@users.noreply.github.com>
2023-02-18 15:22:10 +0200

    More fixes

--- esphome/components/gree/climate.py
+++ esphome/components/gree/climate.py
@@ -4,9 +4,8 @@ from esphome.components import climate_ir
 from esphome.const import (
     CONF_ID,
     CONF_MODEL,
+    CONF_REFERENCE_TEMPERATURE
 )
-
-CONF_IFEEL = "ifeel"
 
 AUTO_LOAD = ["climate_ir"]
 
@@ -25,7 +24,7 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(GreeClimate),
         cv.Required(CONF_MODEL): cv.enum(MODELS),
-        cv.Optional(CONF_IFEEL): cv.boolean(False)
+        cv.Optional(CONF_REFERENCE_TEMPERATURE): cv.boolean(False)
     }
 )
 
@@ -33,7 +32,10 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
 
+    if CONF_REFERENCE_TEMPERATURE not in config:
+        config[CONF_REFERENCE_TEMPERATURE] = False
+
     cg.add(var.set_model(config[CONF_MODEL]))
-    cg.add(var.set_iFeel(config[CONF_IFEEL]))
+    cg.add(var.set_iFeel(config[CONF_REFERENCE_TEMPERATURE]))
 
     await climate_ir.register_climate_ir(var, config)
