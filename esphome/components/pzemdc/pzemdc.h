4a0ac57e407ecaa1bc40b63ff7dd2fe82dcad7da
Kai Gerken <kai.gerken@custom-coding.net>
2023-02-23 19:37:31 +0100

    add reset energy action for pzemdc

--- esphome/components/pzemdc/pzemdc.h
+++ esphome/components/pzemdc/pzemdc.h
@@ -9,6 +9,8 @@
 namespace esphome {
 namespace pzemdc {
 
+template<typename... Ts> class ResetEnergyAction;
+
 class PZEMDC : public PollingComponent, public modbus::ModbusDevice {
  public:
   void set_voltage_sensor(sensor::Sensor *voltage_sensor) { voltage_sensor_ = voltage_sensor; }
@@ -27,6 +29,18 @@ class PZEMDC : public PollingComponent, public modbus::ModbusDevice {
   sensor::Sensor *current_sensor_{nullptr};
   sensor::Sensor *power_sensor_{nullptr};
   sensor::Sensor *energy_sensor_{nullptr};
+
+  void reset_energy_();
+};
+
+template<typename... Ts> class ResetEnergyAction : public Action<Ts...> {
+ public:
+  ResetEnergyAction(PZEMDC *pzemdc) : pzemdc_(pzemdc) {}
+
+  void play(Ts... x) override { this->pzemdc_->reset_energy_(); }
+
+ protected:
+  PZEMDC *pzemdc_;
 };
 
 }  // namespace pzemdc
