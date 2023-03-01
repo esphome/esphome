a3672f9f7f5fd1cffbf4fdb3cdc6d8d4b61cfae2
regevbr <regevbr@gmail.com>
2023-03-01 19:23:10 +0200

    fix CR comments

---
+++ esphome/components/ld2410/button/ld2410_button.cpp
@@ -0,0 +1,11 @@
+#include "ld2410_button.h"
+
+namespace esphome {
+namespace ld2410 {
+
+LD2410Button::LD2410Button() {}
+
+void LD2410Button::press_action() {}
+
+}  // namespace ld2410
+}  // namespace esphome
--- esphome/components/ld2410/switch/__init__.py
+++ esphome/components/ld2410/switch/__init__.py
@@ -4,14 +4,14 @@ import esphome.config_validation as cv
 from esphome.const import DEVICE_CLASS_SWITCH
 from .. import CONF_LD2410_ID, LD2410Component, ld2410_ns
 
-VirtualSwitch = ld2410_ns.class_("VirtualSwitch", switch.Switch)
+LD2410Switch = ld2410_ns.class_("LD2410Switch", switch.Switch)
 
 CONF_ENGINEERING_MODE = "engineering_mode"
 
 CONFIG_SCHEMA = {
     cv.GenerateID(CONF_LD2410_ID): cv.use_id(LD2410Component),
     cv.Optional(CONF_ENGINEERING_MODE): switch.switch_schema(
-        VirtualSwitch,
+        LD2410Switch,
         device_class=DEVICE_CLASS_SWITCH,
         icon="mdi:wrench",
     ),
