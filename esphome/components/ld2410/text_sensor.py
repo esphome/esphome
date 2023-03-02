6b68adb0a7ab510c6b7b13064107507e4ee0e5b6
regevbr <regevbr@gmail.com>
2023-03-02 18:26:45 +0200

    add bluetooth related entities

--- esphome/components/ld2410/text_sensor.py
+++ esphome/components/ld2410/text_sensor.py
@@ -1,7 +1,12 @@
 import esphome.codegen as cg
 from esphome.components import text_sensor
 import esphome.config_validation as cv
-from esphome.const import ENTITY_CATEGORY_DIAGNOSTIC, CONF_VERSION
+from esphome.const import (
+    ENTITY_CATEGORY_DIAGNOSTIC,
+    CONF_VERSION,
+    CONF_MAC_ADDRESS,
+    ICON_BLUETOOTH,
+)
 from . import CONF_LD2410_ID, LD2410Component
 
 DEPENDENCIES = ["ld2410"]
@@ -11,6 +16,9 @@ CONFIG_SCHEMA = {
     cv.Optional(CONF_VERSION): text_sensor.text_sensor_schema(
         entity_category=ENTITY_CATEGORY_DIAGNOSTIC, icon="mdi:label"
     ),
+    cv.Optional(CONF_MAC_ADDRESS): text_sensor.text_sensor_schema(
+        entity_category=ENTITY_CATEGORY_DIAGNOSTIC, icon=ICON_BLUETOOTH
+    ),
 }
 
 
@@ -19,3 +27,6 @@ async def to_code(config):
     if CONF_VERSION in config:
         sens = await text_sensor.new_text_sensor(config[CONF_VERSION])
         cg.add(ld2410_component.set_version_text_sensor(sens))
+    if CONF_MAC_ADDRESS in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_MAC_ADDRESS])
+        cg.add(ld2410_component.set_mac_text_sensor(sens))
