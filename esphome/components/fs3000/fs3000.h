26fe1595002a203b671328d0ee6e267b98562d11
Kevin Ahrendt <kahrendt@gmail.com>
2023-03-02 18:21:58 -0500

    Clean up code with clang.

--- esphome/components/fs3000/fs3000.h
+++ esphome/components/fs3000/fs3000.h
@@ -10,24 +10,24 @@ namespace fs3000 {
 // FS3000 has two models, 1005 and 1015
 //  1005 has a max speed detection of 7.23 m/s
 //  1015 has a max speed detection of 15 m/s
-enum FS3000Model {FIVE, FIFTEEN};
+enum FS3000Model { FIVE, FIFTEEN };
 
 class FS3000Component : public PollingComponent, public i2c::I2CDevice, public sensor::Sensor {
-    public:
-        void update() override;
+ public:
+  void update() override;
 
-        void dump_config() override;
-        float get_setup_priority() const override { return setup_priority::DATA; }
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
-        void set_model(FS3000Model model);
+  void set_model(FS3000Model model);
 
-    protected:
-        FS3000Model model_{FIVE};
+ protected:
+  FS3000Model model_{FIVE};
 
-        uint16_t raw_data_points_[13];
-        float mps_data_points_[13];
+  uint16_t raw_data_points_[13];
+  float mps_data_points_[13];
 
-        float fit_raw_(uint16_t raw_value);
+  float fit_raw_(uint16_t raw_value);
 };
 
 }  // namespace fs3000
