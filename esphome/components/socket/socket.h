ff88f3469b1dcf8a4b56e780e1facd2cd67b9819
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-03-17 16:44:14 +1300

    Wrap ipv6 code a bit more for when ipv6 support should not be compiled in

--- esphome/components/socket/socket.h
+++ esphome/components/socket/socket.h
@@ -44,7 +44,10 @@ std::unique_ptr<Socket> socket(int domain, int type, int protocol);
 /// Create a socket in the newest available IP domain (IPv6 or IPv4) of the given type and protocol.
 std::unique_ptr<Socket> socket_ip(int type, int protocol);
 
-/// Set a sockaddr to the any address for the IP version used by socket_ip().
+/// Set a sockaddr to the specified address and port for the IP version used by socket_ip().
+socklen_t set_sockaddr(struct sockaddr *addr, socklen_t addrlen, const char *ip_address, uint16_t port);
+
+/// Set a sockaddr to the any address and specified port for the IP version used by socket_ip().
 socklen_t set_sockaddr_any(struct sockaddr *addr, socklen_t addrlen, uint16_t port);
 
 }  // namespace socket
