0a05da10ec59a81dfdc0ea7109d65c2143a9f0d5
NP v/d Spek <github_mail@lumensoft.nl>
2023-03-23 19:14:43 +0100

    Add define USE_DISPLAY when a display is used.

--- esphome/components/display/__init__.py
+++ esphome/components/display/__init__.py
@@ -86,6 +86,7 @@ FULL_DISPLAY_SCHEMA = BASIC_DISPLAY_SCHEMA.extend(
 
 
 async def setup_display_core_(var, config):
+    cg.add_define("USE_DISPLAY")
     if CONF_ROTATION in config:
         cg.add(var.set_rotation(DISPLAY_ROTATIONS[config[CONF_ROTATION]]))
 
