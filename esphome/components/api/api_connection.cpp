9ee661c1e4efb2bcf11f3490942332d0a5519535
J. Nick Koston <nick@koston.org>
2023-03-26 11:48:56 -1000

    Add ability to clear the gatt cache (#4621)
    * Add ability to clear the gatt cache
    
    With BlueZ we can fully clear the cache when something goes wrong with the services, however since this is also a cache on the ESP32 we need to be able to clear the on device cache as well for the proxies since if something goes wrong with the service resolution it can cache the bad resolution on NVS forever.
    
    Our current client implementation is limited to clearing the memory cache in Home Assistant https://github.com/home-assistant/core/blob/89355e087952417a6824507fd3b197f9d0520e19/homeassistant/components/esphome/bluetooth/client.py#L512
    
    related issue https://github.com/esphome/issues/issues/4156
    
    https://github.com/esphome/aioesphomeapi/pull/410
    
    * naming
    
    * lint
    
    * lint
    
    * naming
    
    * naming
    
    * naming
    
    * 88 now that 87 is taken
    
    * make const
    
    * Update esphome/components/api/api_frame_helper.cpp

--- esphome/components/api/api_connection.cpp
+++ esphome/components/api/api_connection.cpp
@@ -953,7 +953,9 @@ DeviceInfoResponse APIConnection::device_info(const DeviceInfoRequest &msg) {
   resp.webserver_port = USE_WEBSERVER_PORT;
 #endif
 #ifdef USE_BLUETOOTH_PROXY
-  resp.bluetooth_proxy_version = bluetooth_proxy::global_bluetooth_proxy->has_active() ? 4 : 1;
+  resp.bluetooth_proxy_version = bluetooth_proxy::global_bluetooth_proxy->has_active()
+                                     ? bluetooth_proxy::ACTIVE_CONNECTIONS_VERSION
+                                     : bluetooth_proxy::PASSIVE_ONLY_VERSION;
 #endif
   return resp;
 }
