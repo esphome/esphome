7a7ea05e1d6c91cb8176ee01c99fcc6e2e7f46d8
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-03-26 23:10:36 +1300

    Reorganise into component hub and output platforms

--- esphome/components/gp8403/gp8403.h
+++ esphome/components/gp8403/gp8403.h
@@ -1,23 +1,26 @@
 #pragma once
 
 #include "esphome/components/i2c/i2c.h"
-#include "esphome/components/output/float_output.h"
 #include "esphome/core/component.h"
 
 namespace esphome {
 namespace gp8403 {
 
-class GP8403Output : public Component, public i2c::I2CDevice, public output::FloatOutput {
+enum GP8403Voltage {
+  GP8403_VOLTAGE_5V = 0x00,
+  GP8403_VOLTAGE_10V = 0x11,
+};
+
+class GP8403 : public Component, public i2c::I2CDevice {
  public:
+  void setup() override;
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  void set_channel(uint8_t channel) { this->channel_ = channel; }
-
-  void write_state(float state) override;
+  void set_voltage(gp8403::GP8403Voltage voltage) { this->voltage_ = voltage; }
 
  protected:
-  uint8_t channel_;
+  GP8403Voltage voltage_;
 };
 
 }  // namespace gp8403
--- esphome/components/gp8403/output/gp8403_output.cpp
+++ esphome/components/gp8403/output/gp8403_output.cpp
@@ -7,17 +7,16 @@ namespace gp8403 {
 
 static const char *const TAG = "gp8403.output";
 
-static const uint8_t REGISTER = 0x02;
+static const uint8_t OUTPUT_REGISTER = 0x02;
 
 void GP8403Output::dump_config() {
   ESP_LOGCONFIG(TAG, "GP8403 Output:");
   ESP_LOGCONFIG(TAG, "  Channel: %u", this->channel_);
-  LOG_I2C_DEVICE(this);
 }
 
 void GP8403Output::write_state(float state) {
-  uint16_t value = ((uint16_t)(state * 4095)) << 4;
-  i2c::ErrorCode err = this->write_register(REGISTER + (2 * this->channel_), (uint8_t *) &value, 2);
+  uint16_t value = ((uint16_t) (state * 4095)) << 4;
+  i2c::ErrorCode err = this->parent_->write_register(OUTPUT_REGISTER + (2 * this->channel_), (uint8_t *) &value, 2);
   if (err != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "Error writing to GP8403, code %d", err);
   }
