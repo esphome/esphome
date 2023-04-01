c3384a152b17e463ddcb584d3f458d4c9f1bb107
Mat931 <49403702+Mat931@users.noreply.github.com>
2023-04-01 11:42:52 +0200

    Add more Config Validation

--- esphome/components/remote_receiver/__init__.py
+++ esphome/components/remote_receiver/__init__.py
@@ -52,8 +52,8 @@ CONFIG_SCHEMA = remote_base.validate_triggers(
             ),
             cv.Optional(
                 CONF_IDLE, default="10ms"
-            ): cv.positive_time_period_microseconds,
-            cv.Optional(CONF_MEMORY_BLOCKS, default=3): cv.Range(min=1, max=8),
+            ): cv.All(cv.positive_time_period_microseconds, cv.Range(max=TimePeriod(microseconds=4294967295))),
+            cv.SplitDefault(CONF_MEMORY_BLOCKS, esp32=3): cv.All(cv.only_on_esp32, cv.Range(min=1, max=8)),
             cv.SplitDefault(CONF_CLOCK_DIVIDER, esp32=80): cv.All(cv.only_on_esp32, cv.Range(min=1, max=255)),
         }
     ).extend(cv.COMPONENT_SCHEMA)
