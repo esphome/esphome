4f916465b6f4b9cb575ec50a11315ca99248c4a0
David K <neffs1@gmail.com>
2023-04-01 16:06:32 +0200

    Update library dependencies
    We'll stick with 1.4.2200 for now. 1.4.2200 is not on platform.io registry, use tag instead.
    
    Update to 1.5.2400 needs some work due to multi instance support.

--- esphome/components/bme68x_bsec/__init__.py
+++ esphome/components/bme68x_bsec/__init__.py
@@ -67,10 +67,9 @@ async def to_code(config):
     cg.add_library(
         "BME68x Sensor library",
         "1.1.40407",
-        "https://github.com/BoschSensortec/Bosch-BME68x-Library.git",
     )
     cg.add_library(
         "BSEC2 Software Library",
-        "1.4.2200",
-        "https://github.com/BoschSensortec/Bosch-BSEC2-Library.git",
+        None,
+        "https://github.com/boschsensortec/Bosch-BSEC2-Library.git#v1.4.2200",
     )
