f5792bcf7abcadce2d3c8dbc54b0100185d32f5b
Tomasz Duda <tomaszduda23@gmail.com>
2023-04-06 20:42:22 +0200

    add task if logger is called from not main thread

--- esphome/components/logger/logger.cpp
+++ esphome/components/logger/logger.cpp
@@ -45,7 +45,12 @@ void Logger::write_header_(int level, const char *tag, int line) {
 
   const char *color = LOG_LEVEL_COLORS[level];
   const char *letter = LOG_LEVEL_LETTERS[level];
-  this->printf_to_buffer_("%s[%s][%s:%03u]: ", color, letter, tag, line);
+  void *current_task = xTaskGetCurrentTaskHandle();
+  if (current_task == main_task) {
+    this->printf_to_buffer_("%s[%s][%s:%03u]: ", color, letter, tag, line);
+  } else {
+    this->printf_to_buffer_("%s[%s][%s:%03u][%s]: ", color, letter, tag, line, pcTaskGetName(current_task));
+  }
 }
 
 void HOT Logger::log_vprintf_(int level, const char *tag, int line, const char *format, va_list args) {  // NOLINT
@@ -151,6 +156,7 @@ void HOT Logger::log_message_(int level, const char *tag, int offset) {
 Logger::Logger(uint32_t baud_rate, size_t tx_buffer_size) : baud_rate_(baud_rate), tx_buffer_size_(tx_buffer_size) {
   // add 1 to buffer size for null terminator
   this->tx_buffer_ = new char[this->tx_buffer_size_ + 1];  // NOLINT
+  this->main_task = xTaskGetCurrentTaskHandle();
 }
 
 void Logger::pre_setup() {
