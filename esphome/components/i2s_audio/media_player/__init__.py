1f22a34bdc60f61d317502573a3a02f25ea1141a
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-04-12 11:45:10 +1200

    Add push to talk voice assistant (#4648)
    * Add push to talk voice assistant
    
    * Refactor most code into voice_assistant
    
    * Make voice_assistant the component and remove push_to_talk (can be done in yaml)
    
    * Fix component setup
    
    * Always AF_INET to match serverside
    
    * Fix microphone and media player co-existence
    
    * Format
    
    * Update codeowners
    
    * Update test file
    
    * Fix endifs
    
    * nullptr not NULL
    
    * clang-tidy
    
    * Format
    
    * fixup: Add VA event data
    
    * Generate proto
    
    * Parse and log events
    
    * Add default to switch
    
    * Fix
    
    * Add mic/va to test5

--- esphome/components/i2s_audio/media_player/__init__.py
+++ esphome/components/i2s_audio/media_player/__init__.py
@@ -5,22 +5,25 @@ import esphome.config_validation as cv
 from esphome import pins
 
 from esphome.const import CONF_ID, CONF_MODE
-from esphome.core import CORE
+
+from .. import (
+    i2s_audio_ns,
+    I2SAudioComponent,
+    I2SAudioOut,
+    CONF_I2S_AUDIO_ID,
+    CONF_I2S_DOUT_PIN,
+)
 
 CODEOWNERS = ["@jesserockz"]
-DEPENDENCIES = ["esp32"]
-
-i2s_audio_ns = cg.esphome_ns.namespace("i2s_audio")
+DEPENDENCIES = ["i2s_audio"]
 
 I2SAudioMediaPlayer = i2s_audio_ns.class_(
-    "I2SAudioMediaPlayer", cg.Component, media_player.MediaPlayer
+    "I2SAudioMediaPlayer", cg.Component, media_player.MediaPlayer, I2SAudioOut
 )
 
 i2s_dac_mode_t = cg.global_ns.enum("i2s_dac_mode_t")
 
-CONF_I2S_DOUT_PIN = "i2s_dout_pin"
-CONF_I2S_BCLK_PIN = "i2s_bclk_pin"
-CONF_I2S_LRCLK_PIN = "i2s_lrclk_pin"
+
 CONF_MUTE_PIN = "mute_pin"
 CONF_AUDIO_ID = "audio_id"
 CONF_DAC_TYPE = "dac_type"
@@ -48,34 +51,26 @@ def validate_esp32_variant(config):
 CONFIG_SCHEMA = cv.All(
     cv.typed_schema(
         {
-            "internal": cv.Schema(
+            "internal": media_player.MEDIA_PLAYER_SCHEMA.extend(
                 {
                     cv.GenerateID(): cv.declare_id(I2SAudioMediaPlayer),
+                    cv.GenerateID(CONF_I2S_AUDIO_ID): cv.use_id(I2SAudioComponent),
                     cv.Required(CONF_MODE): cv.enum(INTERNAL_DAC_OPTIONS, lower=True),
                 }
-            )
-            .extend(media_player.MEDIA_PLAYER_SCHEMA)
-            .extend(cv.COMPONENT_SCHEMA),
-            "external": cv.Schema(
+            ).extend(cv.COMPONENT_SCHEMA),
+            "external": media_player.MEDIA_PLAYER_SCHEMA.extend(
                 {
                     cv.GenerateID(): cv.declare_id(I2SAudioMediaPlayer),
+                    cv.GenerateID(CONF_I2S_AUDIO_ID): cv.use_id(I2SAudioComponent),
                     cv.Required(
                         CONF_I2S_DOUT_PIN
-                    ): pins.internal_gpio_output_pin_number,
-                    cv.Required(
-                        CONF_I2S_BCLK_PIN
-                    ): pins.internal_gpio_output_pin_number,
-                    cv.Required(
-                        CONF_I2S_LRCLK_PIN
                     ): pins.internal_gpio_output_pin_number,
                     cv.Optional(CONF_MUTE_PIN): pins.gpio_output_pin_schema,
                     cv.Optional(CONF_MODE, default="mono"): cv.one_of(
                         *EXTERNAL_DAC_OPTIONS, lower=True
                     ),
                 }
-            )
-            .extend(media_player.MEDIA_PLAYER_SCHEMA)
-            .extend(cv.COMPONENT_SCHEMA),
+            ).extend(cv.COMPONENT_SCHEMA),
         },
         key=CONF_DAC_TYPE,
     ),
@@ -89,19 +84,19 @@ async def to_code(config):
     await cg.register_component(var, config)
     await media_player.register_media_player(var, config)
 
+    parent = await cg.get_variable(config[CONF_I2S_AUDIO_ID])
+    cg.add(parent.register_audio_out(var))
+
     if config[CONF_DAC_TYPE] == "internal":
         cg.add(var.set_internal_dac_mode(config[CONF_MODE]))
     else:
         cg.add(var.set_dout_pin(config[CONF_I2S_DOUT_PIN]))
-        cg.add(var.set_bclk_pin(config[CONF_I2S_BCLK_PIN]))
-        cg.add(var.set_lrclk_pin(config[CONF_I2S_LRCLK_PIN]))
         if CONF_MUTE_PIN in config:
             pin = await cg.gpio_pin_expression(config[CONF_MUTE_PIN])
             cg.add(var.set_mute_pin(pin))
         cg.add(var.set_external_dac_channels(2 if config[CONF_MODE] == "stereo" else 1))
 
-    if CORE.is_esp32:
-        cg.add_library("WiFiClientSecure", None)
-        cg.add_library("HTTPClient", None)
-        cg.add_library("esphome/ESP32-audioI2S", "2.0.6")
-        cg.add_build_flag("-DAUDIO_NO_SD_FS")
+    cg.add_library("WiFiClientSecure", None)
+    cg.add_library("HTTPClient", None)
+    cg.add_library("esphome/ESP32-audioI2S", "2.0.6")
+    cg.add_build_flag("-DAUDIO_NO_SD_FS")
