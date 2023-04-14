02c57281c85f5460618de931cdd76adef589b50d
Koen Breeman <koenbreeman@kpnplanet.nl>
2023-04-14 10:25:33 +0200

    corrected errors and added test

--- esphome/components/m5stack4relay/switch/__init__.py
+++ esphome/components/m5stack4relay/switch/__init__.py
@@ -1,11 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, switch
-from esphome.const import (
-    CONF_CHANNEL, 
-    CONF_INTERLOCK, 
-    CONF_INTERLOCK_WAIT_TIME
-)
+from esphome.const import CONF_CHANNEL, CONF_INTERLOCK, CONF_INTERLOCK_WAIT_TIME
 
 from .. import m5stack4relay_ns, M5Stack4Relay, CONF_M5STACK4RELAY_ID
 
