842c9f5ad51411e8ae2fa2e574c74c27e3a3feaa
snetsplit@gmail.com <snetsplit@gmail.com>
2023-04-15 12:27:22 -0500

    Fix out of memory boot loop when js includes or css includes are too large, and implant a custom index page option

--- esphome/components/web_server/web_server.h
+++ esphome/components/web_server/web_server.h
@@ -66,7 +66,7 @@ class WebServer : public Controller, public Component, public AsyncWebHandler {
    */
   void set_js_include(const uint8_t *js_include);
   
-  /** Set local index page 
+  /** Set local index page
    *
    * @param local_page_include Set a pointer to the byte array that holds the gzip compressed index.html file
    */
