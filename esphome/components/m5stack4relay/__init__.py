d0165a791504bf8af52f0382304db80d0304550d
Koen Breeman <koenbreeman@kpnplanet.nl>
2023-04-18 22:04:50 +0200

    Revert "removed m5stack4realy"
    This reverts commit 36913f05befd6e2475d9af4ec5ec40cf0653ef45.

---
+++ esphome/components/m5stack4relay/__init__.py
@@ -0,0 +1,32 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c
+from esphome.const import CONF_ID
+
+CODEOWNERS = ["@KoenBreeman"]
+DEPENDENCIES = ["i2c"]
+
+MULTI_CONF = True
+
+CONF_I2C_ADDR = 0x26
+
+CONF_M5STACK4RELAY_ID = "m5stack4relay_id"
+
+m5stack4relay_ns = cg.esphome_ns.namespace("m5stack4relay")
+M5Stack4Relay = m5stack4relay_ns.class_("M5Stack4Relay", cg.Component, i2c.I2CDevice)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(M5Stack4Relay),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
