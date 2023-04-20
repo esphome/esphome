eb939e487f5a97862a668988fa2039bded3333ce
esnet146 <64173457+esnet146@users.noreply.github.com>
2023-04-20 23:04:58 +0300

    Update sm2135.cpp

--- esphome/components/sm2135/sm2135.cpp
+++ esphome/components/sm2135/sm2135.cpp
@@ -19,19 +19,7 @@ static const uint8_t SM2135_ADDR_W = 0xC6;   // Warm
 static const uint8_t SM2135_RGB = 0x00;  // RGB channel
 static const uint8_t SM2135_CW = 0x80;   // CW channel (Chip default)
 
-static const uint8_t SM2135_10MA = 0x00;
-static const uint8_t SM2135_15MA = 0x01;
-static const uint8_t SM2135_20MA = 0x02;  // RGB max current (Chip default)
-static const uint8_t SM2135_25MA = 0x03;
-static const uint8_t SM2135_30MA = 0x04;  // CW max current (Chip default)
-static const uint8_t SM2135_35MA = 0x05;
-static const uint8_t SM2135_40MA = 0x06;
-static const uint8_t SM2135_45MA = 0x07;  // Max value for RGB
-static const uint8_t SM2135_50MA = 0x08;
-static const uint8_t SM2135_55MA = 0x09;
-static const uint8_t SM2135_60MA = 0x0A;
 
-static const uint8_t SM2135_CURRENT = (SM2135_20MA << 4) | SM2135_10MA;
 
 void SM2135::setup() {
   ESP_LOGCONFIG(TAG, "Setting up SM2135OutputComponent...");
@@ -40,22 +28,26 @@ void SM2135::setup() {
   this->clock_pin_->setup();
   this->clock_pin_->digital_write(true);
   this->pwm_amounts_.resize(5, 0);
+  update_=true;
 }
 void SM2135::dump_config() {
   ESP_LOGCONFIG(TAG, "SM2135:");
   LOG_PIN("  Data Pin: ", this->data_pin_);
   LOG_PIN("  Clock Pin: ", this->clock_pin_);
+  ESP_LOGCONFIG(TAG, "  Color Channels Max Power: %u", this->max_power_color_channels_);
+  ESP_LOGCONFIG(TAG, "  White Channels Max Power: %u", this->max_power_white_channels_);
+
 }
 
 void SM2135::loop() {
-  if (!this->update_)
+    if (!this->update_)
     return;
 
   uint8_t data[6];
   if (this->update_channel_ == 3 || this->update_channel_ == 4) {
     // No color so must be Cold/Warm
     data[0] = SM2135_ADDR_MC;
-    data[1] = SM2135_CURRENT;
+    data[1] = this->max_power_color_channels_ << 4 | this->max_power_white_channels_;
     data[2] = SM2135_CW;
     this->write_buffer_(data, 3);
     delay(1);
@@ -66,7 +58,7 @@ void SM2135::loop() {
   } else {
     // Color
     data[0] = SM2135_ADDR_MC;
-    data[1] = SM2135_CURRENT;
+    data[1] = this->max_power_color_channels_ << 4 | this->max_power_white_channels_;
     data[2] = SM2135_RGB;
     data[3] = this->pwm_amounts_[1];  // Green
     data[4] = this->pwm_amounts_[0];  // Red
