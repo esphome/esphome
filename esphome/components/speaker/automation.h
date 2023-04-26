90d6377f2d1df347b9a3a27e7f3e0804a3878665
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-04-26 19:56:08 +1200

    Speaker support

---
+++ esphome/components/speaker/automation.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "esphome/core/automation.h"
+#include "speaker.h"
+
+#include <vector>
+
+namespace esphome {
+namespace speaker {
+
+template<typename... Ts> class PlayAction : public Action<Ts...>, public Parented<Speaker> {
+ public:
+  void set_data_template(std::function<std::vector<uint8_t>(Ts...)> func) {
+    this->data_func_ = func;
+    this->static_ = false;
+  }
+  void set_data_static(const std::vector<uint8_t> &data) {
+    this->data_static_ = data;
+    this->static_ = true;
+  }
+
+  void play(Ts... x) override {
+    if (this->static_) {
+      this->parent_->play(this->data_static_);
+    } else {
+      auto val = this->data_func_(x...);
+      this->parent_->play(val);
+    }
+  }
+
+ protected:
+  bool static_{false};
+  std::function<std::vector<uint8_t>(Ts...)> data_func_{};
+  std::vector<uint8_t> data_static_{};
+};
+
+template<typename... Ts> class StopAction : public Action<Ts...>, public Parented<Speaker> {
+ public:
+  void play(Ts... x) override { this->parent_->stop(); }
+};
+
+}  // namespace speaker
+}  // namespace esphome
--- esphome/components/voice_assistant/voice_assistant.h
+++ esphome/components/voice_assistant/voice_assistant.h
@@ -2,23 +2,44 @@
 
 #include "esphome/core/automation.h"
 #include "esphome/core/component.h"
+#include "esphome/core/defines.h"
 #include "esphome/core/helpers.h"
 
 #include "esphome/components/api/api_pb2.h"
 #include "esphome/components/api/api_server.h"
 #include "esphome/components/microphone/microphone.h"
+#ifdef USE_SPEAKER
+#include "esphome/components/speaker/speaker.h"
+#endif
 #include "esphome/components/socket/socket.h"
 
 namespace esphome {
 namespace voice_assistant {
 
+// Version 1: Initial version
+// Version 2: Adds raw speaker support
+static const uint32_t INITIAL_VERSION = 1;
+static const uint32_t SPEAKER_SUPPORT = 2;
+
 class VoiceAssistant : public Component {
  public:
   void setup() override;
+  void loop() override;
   float get_setup_priority() const override;
   void start(struct sockaddr_storage *addr, uint16_t port);
 
   void set_microphone(microphone::Microphone *mic) { this->mic_ = mic; }
+#ifdef USE_SPEAKER
+  void set_speaker(speaker::Speaker *speaker) { this->speaker_ = speaker; }
+#endif
+
+  uint32_t get_version() const {
+#ifdef USE_SPEAKER
+    if (this->speaker_ != nullptr)
+      return SPEAKER_SUPPORT;
+#endif
+    return INITIAL_VERSION;
+  }
 
   void request_start();
   void signal_stop();
@@ -44,6 +65,9 @@ class VoiceAssistant : public Component {
   Trigger<std::string, std::string> *error_trigger_ = new Trigger<std::string, std::string>();
 
   microphone::Microphone *mic_{nullptr};
+#ifdef USE_SPEAKER
+  speaker::Speaker *speaker_{nullptr};
+#endif
 
   bool running_{false};
 };
