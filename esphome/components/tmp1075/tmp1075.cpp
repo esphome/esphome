871cd6d47dcfa61a61e1788b11dcb41343eecd90
Sybren A. Stüvel <sybren@stuvel.eu>
2023-05-03 14:03:53 +0200

    Add support for TMP1075 temperature sensor
    TMP1075 is a temperature sensor with I2C interface in industry standard
    LM75 form factor and pinout.
    
    https://www.ti.com/product/TMP1075
    
    Example YAML:
    
    ```yaml
    sensor:
      - platform: tmp1075
        name: TMP1075 Temperature
        id: radiator_temp
        update_interval: 10s
        i2c_id: i2c_bus_1
        conversion_rate: 27.5ms
        alert:
          limit_low: 50
          limit_high: 75
          fault_count: 1
          polarity: active_high
    ```

---
+++ esphome/components/tmp1075/tmp1075.cpp
@@ -0,0 +1,147 @@
+#include "esphome/core/log.h"
+#include "esphome/components/tmp1075/tmp1075.h"
+
+namespace esphome {
+namespace tmp1075 {
+
+static const char *const TAG = "tmp1075";
+
+constexpr uint8_t REG_TEMP = 0x0;   // Temperature result
+constexpr uint8_t REG_CFGR = 0x1;   // Configuration
+constexpr uint8_t REG_LLIM = 0x2;   // Low limit
+constexpr uint8_t REG_HLIM = 0x3;   // High limit
+constexpr uint8_t REG_DIEID = 0xF;  // Device ID
+
+constexpr uint16_t EXPECT_DIEID = 0x0075;  // Expected Device ID.
+
+static uint16_t temp2regvalue(const float temp);
+static float regvalue2temp(const uint16_t regvalue);
+
+void TMP1075Sensor::setup() {
+  uint8_t dieID;
+  if (!this->read_byte(REG_DIEID, &dieID)) {
+    ESP_LOGW(TAG, "'%s' - unable to read ID", this->name_.c_str());
+    this->mark_failed();
+    return;
+  }
+  if (dieID != EXPECT_DIEID) {
+    ESP_LOGW(TAG, "'%s' - unexpected ID 0x%x found, expected 0x%x", this->name_.c_str(), dieID, EXPECT_DIEID);
+    this->mark_failed();
+    return;
+  }
+
+  this->write_config();
+}
+
+void TMP1075Sensor::update() {
+  uint16_t regvalue;
+  if (!read_byte_16(REG_TEMP, &regvalue)) {
+    ESP_LOGW(TAG, "'%s' - unable to read temperature register", this->name_.c_str());
+    this->status_set_warning();
+    return;
+  }
+
+  const float temp = regvalue2temp(regvalue);
+  this->publish_state(temp);
+}
+
+void TMP1075Sensor::dump_config() {
+  LOG_SENSOR("", "TMP1075 Sensor", this);
+  if (this->is_failed()) {
+    ESP_LOGE(TAG, "  Communication with TMP1075 failed!");
+    return;
+  }
+  ESP_LOGCONFIG(TAG, "  limit low  : %d", alert_limit_low_);
+  ESP_LOGCONFIG(TAG, "  limit high : %d", alert_limit_high_);
+  ESP_LOGCONFIG(TAG, "  oneshot    : %d", config_.fields.oneshot);
+  ESP_LOGCONFIG(TAG, "  rate       : %d", config_.fields.rate);
+  ESP_LOGCONFIG(TAG, "  fault_count: %d", config_.fields.faults);
+  ESP_LOGCONFIG(TAG, "  polarity   : %d", config_.fields.polarity);
+  ESP_LOGCONFIG(TAG, "  alert_mode : %d", config_.fields.alert_mode);
+  ESP_LOGCONFIG(TAG, "  shutdown   : %d", config_.fields.shutdown);
+}
+
+void TMP1075Sensor::set_alert_limit_low(const float temp) { this->alert_limit_low_ = temp; }
+void TMP1075Sensor::set_alert_limit_high(const float temp) { this->alert_limit_high_ = temp; }
+void TMP1075Sensor::set_oneshot(const bool oneshot) { config_.fields.oneshot = oneshot; }
+void TMP1075Sensor::set_conversion_rate(const enum eConversionRate rate) { config_.fields.rate = rate; }
+void TMP1075Sensor::set_fault_count(const int faults) {
+  if (faults < 1) {
+    ESP_LOGE(TAG, "'%s' - fault_count too low: %d", this->name_.c_str(), faults);
+    return;
+  }
+  if (faults > 4) {
+    ESP_LOGE(TAG, "'%s' - fault_count too high: %d", this->name_.c_str(), faults);
+    return;
+  }
+  config_.fields.faults = faults - 1;
+}
+void TMP1075Sensor::set_alert_polarity(const bool polarity) { config_.fields.polarity = polarity; }
+void TMP1075Sensor::set_alert_mode(const bool alert_mode) { config_.fields.alert_mode = alert_mode; }
+void TMP1075Sensor::set_shutdown(const bool shutdown) { config_.fields.shutdown = shutdown; }
+
+void TMP1075Sensor::load_config() {
+  uint16_t regvalue;
+  if (!read_byte_16(REG_CFGR, &regvalue)) {
+    ESP_LOGW(TAG, "'%s' - unable to read configuration register", this->name_.c_str());
+    return;
+  }
+  config_.regvalue = regvalue;
+  ESP_LOGD(TAG, "'%s' - loaded config register %04x", this->name_.c_str(), config_.regvalue);
+  logd_config();
+}
+
+void TMP1075Sensor::logd_config() {
+  ESP_LOGD(TAG, "  oneshot   : %d", config_.fields.oneshot);
+  ESP_LOGD(TAG, "  rate      : %d", config_.fields.rate);
+  ESP_LOGD(TAG, "  faults    : %d", config_.fields.faults);
+  ESP_LOGD(TAG, "  polarity  : %d", config_.fields.polarity);
+  ESP_LOGD(TAG, "  alert_mode: %d", config_.fields.alert_mode);
+  ESP_LOGD(TAG, "  shutdown  : %d", config_.fields.shutdown);
+}
+
+void TMP1075Sensor::write_config() {
+  send_alert_limit_low();
+  send_alert_limit_high();
+  send_config();
+}
+
+void TMP1075Sensor::send_config() {
+  ESP_LOGD(TAG, "'%s' - sending configuration %04x", this->name_.c_str(), config_.regvalue);
+  logd_config();
+  if (!this->write_byte_16(REG_CFGR, config_.regvalue)) {
+    ESP_LOGW(TAG, "'%s' - unable to write configuration register", this->name_.c_str());
+    return;
+  }
+}
+
+void TMP1075Sensor::send_alert_limit_low() {
+  ESP_LOGD(TAG, "'%s' - sending alert limit low %.3f °C", this->name_.c_str(), alert_limit_low_);
+  const uint16_t regvalue = temp2regvalue(alert_limit_low_);
+  if (!this->write_byte_16(REG_LLIM, regvalue)) {
+    ESP_LOGW(TAG, "'%s' - unable to write low limit register", this->name_.c_str());
+    return;
+  }
+}
+
+void TMP1075Sensor::send_alert_limit_high() {
+  ESP_LOGD(TAG, "'%s' - sending alert limit high %.3f °C", this->name_.c_str(), alert_limit_high_);
+  const uint16_t regvalue = temp2regvalue(alert_limit_high_);
+  if (!this->write_byte_16(REG_HLIM, regvalue)) {
+    ESP_LOGW(TAG, "'%s' - unable to write high limit register", this->name_.c_str());
+    return;
+  }
+}
+
+static uint16_t temp2regvalue(const float temp) {
+  const uint16_t regvalue = temp / 0.0625f;
+  return regvalue << 4;
+}
+
+static float regvalue2temp(const uint16_t regvalue) {
+  const int16_t signed_value = regvalue;
+  return (signed_value >> 4) * 0.0625f;
+}
+
+}  // namespace tmp1075
+}  // namespace esphome
---
+++ esphome/components/tmp1075/tmp1075.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace tmp1075 {
+
+struct TMP1075Config {
+  union {
+    struct {
+      uint8_t oneshot : 1;  // One-shot conversion mode. Writing 1, starts a single temperature
+                            // conversion. Read returns 0.
+
+      uint8_t rate : 2;  // Conversion rate setting when device is in continuous conversion mode.
+                         // 00: 27.5 ms conversion rate
+                         // 01: 55 ms conversion rate
+                         // 10: 110 ms conversion rate
+                         // 11: 220 ms conversion rate (35 ms TMP1075N)
+
+      uint8_t faults : 2;  // Consecutive fault measurements to trigger the alert function.
+                           // 00: 1 fault
+                           // 01: 2 faults
+                           // 10: 3 faults (4 faults TMP1075N)
+                           // 11: 4 faults (6 faults TMP1075N)
+
+      uint8_t polarity : 1;  // Polarity of the output pin.
+                             // 0: Active low ALERT pin
+                             // 1: Active high ALERT pin
+
+      uint8_t alert_mode : 1;  // Selects the function of the ALERT pin.
+                               // 0: ALERT pin functions in comparator mode
+                               // 1: ALERT pin functions in interrupt mode
+
+      uint8_t shutdown : 1;  // Sets the device in shutdown mode to conserve power.
+                             // 0: Device is in continuous conversion
+                             // 1: Device is in shutdown mode
+      uint8_t unused : 8;
+    } fields;
+    uint16_t regvalue;
+  };
+};
+
+enum eConversionRate {
+  CONV_RATE_27_5_MS,
+  CONV_RATE_55_MS,
+  CONV_RATE_110_MS,
+  CONV_RATE_220_MS,
+};
+
+class TMP1075Sensor : public PollingComponent, public sensor::Sensor, public i2c::I2CDevice {
+ public:
+  void setup() override;
+  void update() override;
+
+  float get_setup_priority() const override
+  {
+    return setup_priority::DATA;
+  }
+
+  void dump_config() override;
+
+  // Call write_config() after calling any of these to send the new config to
+  // the IC. The setup() function also does this.
+  void set_alert_limit_low(float temp);
+  void set_alert_limit_high(float temp);
+  void set_oneshot(bool oneshot);
+  void set_conversion_rate(enum eConversionRate rate);
+  void set_fault_count(int faults);
+  void set_alert_polarity(bool polarity);
+  void set_alert_mode(bool alert_mode);
+  void set_shutdown(bool shutdown);
+
+  void write_config();
+
+ protected:
+  TMP1075Config config_ = {0};
+
+  // Disable the alert pin by default.
+  float alert_limit_low_ = -128.0f;
+  float alert_limit_high_ = 127.9375f;
+
+  void load_config();
+  void send_alert_limit_low();
+  void send_alert_limit_high();
+  void send_config();
+  void logd_config();
+};
+
+}  // namespace tmp1075
+}  // namespace esphome
