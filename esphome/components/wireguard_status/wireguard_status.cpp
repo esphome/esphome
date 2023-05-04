43c9d448656091d0d7390001a8cb5bd344f004e2
Simone Rossetto <simros85@gmail.com>
2023-05-04 06:46:59 +0200

    Add wireguard binary sensor to check if remote peer is up

---
+++ esphome/components/wireguard_status/wireguard_status.cpp
@@ -0,0 +1,36 @@
+#include "wireguard_status.h"
+
+#include "esphome/core/log.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace wireguard {
+namespace sensors {
+
+static const char * const TAG = "wireguard_status.binary_sensor";
+
+void WireguardStatus::update() {
+  if(this->wireguard_ == nullptr) {
+    ESP_LOGW(TAG, "wireguard component not available");
+    return;
+  }
+
+  if(this->wireguard_->is_failed()) {
+    ESP_LOGE(TAG, "wireguard component is failed");
+    this->mark_failed();
+    return;
+  }
+
+  publish_state(this->wireguard_->is_peer_up());
+}
+
+void WireguardStatus::dump_config() {
+  LOG_BINARY_SENSOR("", "WireGuard Status", this);
+  LOG_UPDATE_INTERVAL(this);
+}
+
+}  // namespace sensors
+}  // namespace wireguard
+}  // namespace esphome
+
+// vim: tabstop=2 shiftwidth=2 expandtab
