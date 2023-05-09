820f1da381bcdb865939380a20eabe76f26f8747
TheRealTom <neportegal@seznam.cz>
2023-05-09 17:11:38 +0200

    Resolving bugs from installing the component

--- esphome/components/mr24d11c10/mr24d11c10.h
+++ esphome/components/mr24d11c10/mr24d11c10.h
@@ -65,7 +65,7 @@ class MR24D11C10Component : public Component, public uart::UARTDevice {
   size_t msg_len;
 
   void set_human_presence_binary_sensor(binary_sensor::BinarySensor *sens) { this->target_present_ = sens; };
-  void set_body_movement_sensor(Sensor::Sensor *sens) { this->body_movement_ = sens; };
+  void set_body_movement_sensor(sensor::Sensor *sens) { this->body_movement_ = sens; };
   void send_new_scene_settings(uint8_t id);
   void send_new_threshold(uint8_t id);
   void get_radar_device_id();
