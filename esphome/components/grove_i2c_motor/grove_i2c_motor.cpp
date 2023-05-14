6b185be739cf49e5a36ff2ee59d27e0f38f1845f
christian bianchini <max246@gmail.com>
2023-05-14 19:47:10 +0100

    fixes

--- esphome/components/grove_i2c_motor/grove_i2c_motor.cpp
+++ esphome/components/grove_i2c_motor/grove_i2c_motor.cpp
@@ -26,42 +26,46 @@ void GroveMotorDriveTB6612FNG::set_i2c_addr(uint8_t addr) { motor->setI2cAddr(ad
 
 void GroveMotorDriveTB6612FNG::dc_motor_run(uint8_t chl, int16_t speed) {
   motor_channel_type_t channel;
-  if (chl == 0)
+  if (chl == 0) {
     channel = MOTOR_CHA;
-  else
+  } else {
     channel = MOTOR_CHB;
+  }
   motor->dcMotorRun(channel, speed);
 }
 
 void GroveMotorDriveTB6612FNG::dc_motor_brake(uint8_t chl) {
   motor_channel_type_t channel;
-  if (chl == 0)
+  if (chl == 0) {
     channel = MOTOR_CHA;
-  else
+  } else {
     channel = MOTOR_CHB;
+  }
   motor->dcMotorBrake(channel);
 }
 
 void GroveMotorDriveTB6612FNG::dc_motor_stop(uint8_t chl) {
   motor_channel_type_t channel;
-  if (chl == 0)
+  if (chl == 0) {
     channel = MOTOR_CHA;
-  else
+  } else {
     channel = MOTOR_CHB;
+  }
   motor->dcMotorStop(channel);
 }
 
 void GroveMotorDriveTB6612FNG::stepper_run(int mode, int16_t steps, uint16_t rpm) {
   stepper_mode_type_t mode_type;
 
-  if (mode == 0)
+  if (mode == 0) {
     mode_type = FULL_STEP;
-  else if (mode == 1)
+  } else if (mode == 1) {
     mode_type = WAVE_DRIVE;
-  else if (mode == 2)
+  } else if (mode == 2) {
     mode_type = HALF_STEP;
-  else
+  } else {
     mode_type = MICRO_STEPPING;
+  }
   motor->stepperRun(mode_type, steps, rpm);
 }
 
@@ -70,14 +74,15 @@ void GroveMotorDriveTB6612FNG::stepper_stop() { motor->stepperStop(); }
 void GroveMotorDriveTB6612FNG::stepper_keep_run(int mode, uint16_t rpm, bool is_cw) {
   stepper_mode_type_t mode_type;
 
-  if (mode == 0)
+  if (mode == 0) {
     mode_type = FULL_STEP;
-  else if (mode == 1)
+  } else if (mode == 1) {
     mode_type = WAVE_DRIVE;
-  else if (mode == 2)
+  } else if (mode == 2) {
     mode_type = HALF_STEP;
-  else
+  } else {
     mode_type = MICRO_STEPPING;
+  }
   motor->stepperKeepRun(mode_type, rpm, is_cw);
 }
 
