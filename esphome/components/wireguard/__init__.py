7208401d371099525c8e6680fda2e63a4b013bec
Simone Rossetto <simros85@gmail.com>
2023-05-17 20:15:36 +0200

    Make validation stricter for wireguard address and netmask

--- esphome/components/wireguard/__init__.py
+++ esphome/components/wireguard/__init__.py
@@ -21,8 +21,8 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(Wireguard),
         cv.GenerateID(CONF_TIME_ID): cv.use_id(time.RealTimeClock),
-        cv.Required(CONF_ADDRESS): cv.string,
-        cv.Optional(CONF_NETMASK, default="255.255.255.255"): cv.string,
+        cv.Required(CONF_ADDRESS): cv.ipv4,
+        cv.Optional(CONF_NETMASK, default="255.255.255.255"): cv.ipv4,
         cv.Required(CONF_PRIVATE_KEY): cv.string,
         cv.Required(CONF_PEER_ENDPOINT): cv.string,
         cv.Required(CONF_PEER_PUBLIC_KEY): cv.string,
@@ -36,8 +36,8 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
 
-    cg.add(var.set_address(config[CONF_ADDRESS]))
-    cg.add(var.set_netmask(config[CONF_NETMASK]))
+    cg.add(var.set_address(str(config[CONF_ADDRESS])))
+    cg.add(var.set_netmask(str(config[CONF_NETMASK])))
     cg.add(var.set_private_key(config[CONF_PRIVATE_KEY]))
     cg.add(var.set_peer_endpoint(config[CONF_PEER_ENDPOINT]))
     cg.add(var.set_peer_public_key(config[CONF_PEER_PUBLIC_KEY]))
