9461fb6c6714a932ca3573a7e4d6cbe1e3b7b898
kroimon <home-assistant@kroimon.de>
2023-05-19 15:12:50 +0000

    Add CODEOWNERS

--- esphome/components/es8311/__init__.py
+++ esphome/components/es8311/__init__.py
@@ -4,9 +4,10 @@ import esphome.config_validation as cv
 from esphome.components import i2c
 from esphome.const import CONF_ID
 
+CODEOWNERS = ["@kroimon"]
+
 es8311_ns = cg.esphome_ns.namespace("es8311")
 ES8311Component = es8311_ns.class_("ES8311Component", cg.Component, i2c.I2CDevice)
-
 
 CONFIG_SCHEMA = (
     cv.Schema({cv.GenerateID(): cv.declare_id(ES8311Component)})
