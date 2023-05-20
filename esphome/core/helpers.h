897fb142ea8c6225630f7726c73f20ffab40a8b8
Evgeny <iam@freekode.org>
2023-05-20 16:44:35 +0200

    added base64 helper

--- esphome/core/helpers.h
+++ esphome/core/helpers.h
@@ -429,6 +429,12 @@ std::string value_accuracy_to_string(float value, int8_t accuracy_decimals);
 /// Derive accuracy in decimals from an increment step.
 int8_t step_to_accuracy_decimals(float step);
 
+static const std::string BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                        "abcdefghijklmnopqrstuvwxyz"
+                                        "0123456789+/";
+std::string base64_encode(const char *buf, unsigned int buf_len);
+std::vector<char> base64_decode(std::string const &);
+
 ///@}
 
 /// @name Colors
