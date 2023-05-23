9ea253e55913cd470d1a44bb9a3e2112857806f7
Your Name <you@example.com>
2023-05-23 12:22:51 +0200

    Remove cpp & h changes to combine with PR #4658

--- esphome/components/logger/logger.cpp
+++ esphome/components/logger/logger.cpp
@@ -270,13 +270,7 @@ void Logger::pre_setup() {
         this->hw_serial_ = &Serial;
         Serial.begin(this->baud_rate_);
         break;
-#endif  // USE_RP2040
-#if defined(USE_ESP32) && (defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3))
-      case UART_SELECTION_USB_CDC:
-        this->hw_serial_ = &USBSerial;
-        USBSerial.begin(this->baud_rate_);
-        break;
-#endif  // USE_ESP32 && (USE_ESP32_VARIANT_ESP32S2 || USE_ESP32_VARIANT_ESP32S3)
+#endif
     }
 #endif  // USE_ARDUINO
 #ifdef USE_ESP_IDF
@@ -403,7 +397,6 @@ const char *const UART_SELECTIONS[] = {
 #if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3)
     "USB_CDC",
 #endif  // USE_ESP32_VARIANT_ESP32S2 || USE_ESP32_VARIANT_ESP32S3
-#if defined(USE_ESP_IDF)
 #if defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32S3)
     "USB_SERIAL_JTAG",
 #endif  // USE_ESP32_VARIANT_ESP32C3 || USE_ESP32_VARIANT_ESP32S3
