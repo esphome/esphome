a15b4eae59ebf1e28aee76af419feba4c48384dc
Aleksandar Dimitrov <admin@sharkydog.info>
2023-05-23 23:11:50 +0300

    formatting

--- esphome/components/fram_pref/FRAM_PREF.h
+++ esphome/components/fram_pref/FRAM_PREF.h
@@ -28,35 +28,35 @@ struct PREF_STRUCT {
 class FRAM_PREF : public Component, public ESPPreferences {
   public:
     FRAM_PREF(fram::FRAM * fram);
-    
+
     void set_pool(uint16_t pool_size, uint16_t pool_start);
     void set_static_pref(std::string key, uint16_t addr, uint16_t size, std::function<uint32_t()> && fn, bool persist_key);
-    
+
     void setup() override;
     void dump_config() override;
     float get_setup_priority() const override { return setup_priority::BUS; }
-    
+
     ESPPreferenceObject make_preference(size_t length, uint32_t type, bool in_flash) override;
     ESPPreferenceObject make_preference(size_t length, uint32_t type) override;
     bool sync() override;
     bool reset() override;
-  
+
   protected:
     friend class FRAMPreferenceBackend;
-    
+
     bool _check();
     void _clear();
-    
+
     fram::FRAM * fram_;
     uint16_t pool_size_{0};
     uint16_t pool_start_{0};
     uint16_t pool_next_{0};
     bool pool_cleared_{false};
-    
+
     std::vector<PREF_STRUCT> prefs_;
     std::vector<std::function<uint32_t()>> prefs_static_cb_;
     std::map<uint32_t,uint8_t> prefs_static_map_;
-    
+
     ESPPreferences * pref_prev_;
 };
 
