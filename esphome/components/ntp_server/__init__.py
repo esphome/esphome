25b5016ae45ec9429df1eed96879bb1dce3ab724
RobertJN64 <robertjnies@gmail.com>
2023-05-28 12:54:56 -0400

    trying to fix format issues

--- esphome/components/ntp_server/__init__.py
+++ esphome/components/ntp_server/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_TIME
+from esphome.const import CONF_ID
 
 CODEOWNERS = ["@RobertJN64"]
 DEPENDENCIES = ["time"]
@@ -17,7 +17,7 @@ CONFIG_SCHEMA = cv.All(
     )
 )
 
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
