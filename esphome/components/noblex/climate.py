aae550510af1023d1b22d280d077639cea08c85e
AGalfra <alegalfrascoli@gmail.com>
2023-06-02 20:10:58 -0300

    check black

--- esphome/components/noblex/climate.py
+++ esphome/components/noblex/climate.py
@@ -14,6 +14,7 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     }
 )
 
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await climate_ir.register_climate_ir(var, config)
