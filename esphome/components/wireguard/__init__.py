9d456555aeaa3fa9dec5193fcfde0b1c5e2f112f
Simone Rossetto <simros85@gmail.com>
2023-06-04 06:17:55 +0200

    Update wireguard codeowners

--- esphome/components/wireguard/__init__.py
+++ esphome/components/wireguard/__init__.py
@@ -19,7 +19,7 @@ CONF_PEER_ALLOWED_IPS = "peer_allowed_ips"
 CONF_PEER_PERSISTENT_KEEPALIVE = "peer_persistent_keepalive"
 
 DEPENDENCIES = ["time", "esp32"]
-CODEOWNERS = ["@lhoracek", "@droscy"]
+CODEOWNERS = ["@lhoracek", "@droscy", "@thomas0bernard"]
 
 wireguard_ns = cg.esphome_ns.namespace("wireguard")
 Wireguard = wireguard_ns.class_("Wireguard", cg.Component, cg.PollingComponent)
