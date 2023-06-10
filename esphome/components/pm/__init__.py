19775c6d5921a336444562239a314ebc50d4a672
silverchris <silverchris@gmail.com>
2023-06-10 19:59:00 -0400

    Revert "Temporaraly set parameter names in __init__.py instead of const.py"
    This reverts commit a96eaa999201d7377b448dc2ad026297deb19d0a.

--- esphome/components/pm/__init__.py
+++ esphome/components/pm/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_MIN_FREQ_MHZ, CONF_MAX_FREQ_MHZ, CONF_TICKLESS
 from esphome.components.esp32 import add_idf_sdkconfig_option
 from esphome.core import CORE
 
@@ -26,8 +26,8 @@ def to_code(config):
         add_idf_sdkconfig_option("CONFIG_FREERTOS_IDLE_TIME_BEFORE_SLEEP", 3)
     var = cg.new_Pvariable(config[CONF_ID])
 
-    cg.add(var.set_freq(config["min_freq_mhz"], config["max_freq_mhz"]))
-    cg.add(var.set_tickless(config["tickless"]))
+    cg.add(var.set_freq(config[CONF_MIN_FREQ_MHZ], config[CONF_MAX_FREQ_MHZ]))
+    cg.add(var.set_tickless(config[CONF_TICKLESS]))
 
     yield cg.register_component(var, config)
 
