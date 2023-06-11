f33d832cf8e397fcb5e8bfd98936f2231085e90a
TheRealTom <neportegal@seznam.cz>
2023-06-12 00:20:50 +0200

    Fix cLang format

--- esphome/components/mr24d11c10/radar.h
+++ esphome/components/mr24d11c10/radar.h
@@ -6,23 +6,20 @@
  * License: MIT
  */
 
-
 namespace esphome {
 namespace mr24d11c10 {
 
 #ifndef _RADAR_H__
 #define _RADAR_H__
 
-
-class radar
-{
-    private:
-    public:
-        int Bodysign_val(int ad1, int ad2, int ad3, int ad4, int ad5);
-        int Situation_judgment(int ad1, int ad2, int ad3, int ad4, int ad5);
-        int Fall_judgment(int ad1, int ad2, int ad3, int ad4);
-        char CRC(char ad1, char ad2, char ad3, char ad4, char ad5, char ad6, char ad7);
-        unsigned short int us_CalculateCrc16(unsigned char *lpuc_Frame, unsigned short int lus_Len);
+class radar {
+ private:
+ public:
+  int Bodysign_val(int ad1, int ad2, int ad3, int ad4, int ad5);
+  int Situation_judgment(int ad1, int ad2, int ad3, int ad4, int ad5);
+  int Fall_judgment(int ad1, int ad2, int ad3, int ad4);
+  char CRC(char ad1, char ad2, char ad3, char ad4, char ad5, char ad6, char ad7);
+  unsigned short int us_CalculateCrc16(unsigned char *lpuc_Frame, unsigned short int lus_Len);
 };
 #endif
 
