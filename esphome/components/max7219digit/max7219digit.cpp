07e498742fc06cea5c66c426df3c3310f0bbf307
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-06-12 09:44:03 +1200

    Update max7219digit.cpp

--- esphome/components/max7219digit/max7219digit.cpp
+++ esphome/components/max7219digit/max7219digit.cpp
@@ -279,7 +279,7 @@ void MAX7219Component::send64pixels(uint8_t chip, const uint8_t pixels[8]) {
       }
     } else {
       for (uint8_t i = 0; i < 8; i++) {
-          b |= ((pixels[7 - col] >> i) & 1) << (7 - i);
+        b |= ((pixels[7 - col] >> i) & 1) << (7 - i);
       }
     }
     // send this byte to display at selected chip
