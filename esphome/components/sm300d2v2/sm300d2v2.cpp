8b2f2b05e1db09bdd68c091c3a6a00dcc07f7799
Duker4444 <32688504+Duker4444@users.noreply.github.com>
2023-06-16 13:53:06 -0400

    Update sm300d2v2.cpp
    Taking out Checksum Re-validating Output.

--- esphome/components/sm300d2v2/sm300d2v2.cpp
+++ esphome/components/sm300d2v2/sm300d2v2.cpp
@@ -14,11 +14,7 @@ void SM300D2Sensor::update() {
   while (this->available() > 0 && this->peek_byte(&peeked) && peeked != 0x01)
     this->read();
 
-// Trim 3 characters from the beginning of the response array
   bool read_success = read_array(response, SM300D2_RESPONSE_LENGTH);
-
-  string response = SM300D2_RESPONSE_LENGTH.Substring(3);
-  Console.WriteLine(response);
 
   if (!read_success) {
     ESP_LOGW(TAG, "Reading data from SM300D2 failed!");
@@ -48,16 +44,16 @@ void SM300D2Sensor::update() {
 
   ESP_LOGD(TAG, "Successfully read SM300D2 data");
 
-  const uint16_t co2 = (response[1] * 256) + response[2];
-  const uint16_t formaldehyde = (response[3] * 256) + response[4];
-  const uint16_t tvoc = (response[5] * 256) + response[6];
-  const uint16_t pm_2_5 = (response[7] * 256) + response[8];
-  const uint16_t pm_10_0 = (response[9] * 256) + response[10];
+  const uint16_t co2 = (response[4] * 256) + response[5];
+  const uint16_t formaldehyde = (response[6] * 256) + response[7];
+  const uint16_t tvoc = (response[8] * 256) + response[9];
+  const uint16_t pm_2_5 = (response[10] * 256) + response[11];
+  const uint16_t pm_10_0 = (response[12] * 256) + response[13];
   // A negative value is indicated by adding 0x80 (128) to the temperature value
-  const float temperature = ((response[11] + (response[12] * 0.1f)) > 128)
-                                ? (((response[11] + (response[12] * 0.1f)) - 128) * -1)
-                                : response[11] + (response[12] * 0.1f);
-  const float humidity = response[13] + (response[14] * 0.1f);
+  const float temperature = ((response[14] + (response[15] * 0.1f)) > 128)
+                                ? (((response[14] + (response[15] * 0.1f)) - 128) * -1)
+                                : response[14] + (response[15] * 0.1f);
+  const float humidity = response[16] + (response[17] * 0.1f);
 
   ESP_LOGD(TAG, "Received CO₂: %u ppm", co2);
   if (this->co2_sensor_ != nullptr)
