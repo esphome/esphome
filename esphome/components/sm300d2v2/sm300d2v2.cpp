9e15fad42005ddcd9780798c0f5887a20a31dd1b
Duker4444 <32688504+Duker4444@users.noreply.github.com>
2023-06-16 10:42:44 -0400

    Update sm300d2v2.cpp

--- esphome/components/sm300d2v2/sm300d2v2.cpp
+++ esphome/components/sm300d2v2/sm300d2v2.cpp
@@ -5,13 +5,13 @@ namespace esphome {
 namespace sm300d2v2 {
 
 static const char *const TAG = "sm300d2v2";
-static const uint8_t SM300D2_RESPONSE_LENGTH = 17;
+static const uint8_t SM300D2_RESPONSE_LENGTH = 19;
 
 void SM300D2Sensor::update() {
   uint8_t response[SM300D2_RESPONSE_LENGTH];
   uint8_t peeked;
 
-  while (this->available() > 0 && this->peek_byte(&peeked) && peeked != 0x3C)
+  while (this->available() > 0 && this->peek_byte(&peeked) && peeked != 0x01)
     this->read();
 
   bool read_success = read_array(response, SM300D2_RESPONSE_LENGTH);
@@ -22,38 +22,38 @@ void SM300D2Sensor::update() {
     return;
   }
 
-  if (response[0] != 0x3C || response[1] != 0x02) {
-    ESP_LOGW(TAG, "Invalid preamble for SM300D2 response!");
-    this->status_set_warning();
-    return;
-  }
+  //if (response[0] != 0x3C || response[1] != 0x02) {
+  //  ESP_LOGW(TAG, "Invalid preamble for SM300D2 response!");
+  //  this->status_set_warning();
+  //  return;
+  //}
 
-  uint16_t calculated_checksum = this->sm300d2_checksum_(response);
+  //uint16_t calculated_checksum = this->sm300d2_checksum_(response);
   // Occasionally the checksum has a +/- 0x80 offset. Negative temperatures are
   // responsible for some of these. The rest are unknown/undocumented.
-  if ((calculated_checksum != response[SM300D2_RESPONSE_LENGTH - 1]) &&
-      (calculated_checksum - 0x80 != response[SM300D2_RESPONSE_LENGTH - 1]) &&
-      (calculated_checksum + 0x80 != response[SM300D2_RESPONSE_LENGTH - 1])) {
-    ESP_LOGW(TAG, "SM300D2 Checksum doesn't match: 0x%02X!=0x%02X", response[SM300D2_RESPONSE_LENGTH - 1],
-             calculated_checksum);
-    this->status_set_warning();
-    return;
-  }
+  //if ((calculated_checksum != response[SM300D2_RESPONSE_LENGTH - 1]) &&
+  //    (calculated_checksum - 0x80 != response[SM300D2_RESPONSE_LENGTH - 1]) &&
+  //    (calculated_checksum + 0x80 != response[SM300D2_RESPONSE_LENGTH - 1])) {
+  //  ESP_LOGW(TAG, "SM300D2 Checksum doesn't match: 0x%02X!=0x%02X", response[SM300D2_RESPONSE_LENGTH - 1],
+  //           calculated_checksum);
+  //  this->status_set_warning();
+  //  return;
+  //}
 
   this->status_clear_warning();
 
   ESP_LOGD(TAG, "Successfully read SM300D2 data");
 
-  const uint16_t co2 = (response[2] * 256) + response[3];
-  const uint16_t formaldehyde = (response[4] * 256) + response[5];
-  const uint16_t tvoc = (response[6] * 256) + response[7];
-  const uint16_t pm_2_5 = (response[8] * 256) + response[9];
-  const uint16_t pm_10_0 = (response[10] * 256) + response[11];
+  const uint16_t co2 = (response[4] * 256) + response[5];
+  const uint16_t formaldehyde = (response[6] * 256) + response[7];
+  const uint16_t tvoc = (response[8] * 256) + response[9];
+  const uint16_t pm_2_5 = (response[10] * 256) + response[11];
+  const uint16_t pm_10_0 = (response[12] * 256) + response[13];
   // A negative value is indicated by adding 0x80 (128) to the temperature value
-  const float temperature = ((response[12] + (response[13] * 0.1f)) > 128)
-                                ? (((response[12] + (response[13] * 0.1f)) - 128) * -1)
-                                : response[12] + (response[13] * 0.1f);
-  const float humidity = response[14] + (response[15] * 0.1f);
+  const float temperature = ((response[14] + (response[15] * 0.1f)) > 128)
+                                ? (((response[14] + (response[15] * 0.1f)) - 128) * -1)
+                                : response[14] + (response[15] * 0.1f);
+  const float humidity = response[16] + (response[17] * 0.1f);
 
   ESP_LOGD(TAG, "Received CO₂: %u ppm", co2);
   if (this->co2_sensor_ != nullptr)
