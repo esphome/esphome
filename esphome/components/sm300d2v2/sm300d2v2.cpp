9b574568373cd366dabf32b51606ab7916736abc
Duker4444 <32688504+Duker4444@users.noreply.github.com>
2023-06-20 10:49:13 -0400

    Update sm300d2v2.cpp
    Working to satisfy clang formatting.

--- esphome/components/sm300d2v2/sm300d2v2.cpp
+++ esphome/components/sm300d2v2/sm300d2v2.cpp
@@ -11,14 +11,14 @@ void SM300D2Sensor::update() {
   uint8_t response[SM300D2_RESPONSE_LENGTH];
   uint8_t peeked;
   uint8_t previousByte = 0;
-   //adding timer for trigger between datasets.
+  //adding timer for trigger between datasets.
   unsigned long previousTime = 0;
   unsigned long startTime = millis();
 
   while (this->available() > 0) {
     this->peek_byte(&peeked);
 
-   // Check if approx half-second delay has elapsed since the previous byte
+    // Check if approx half-second delay has elapsed since the previous byte
     if (millis() - previousTime >= 300 && peeked == 0x01) {
       break; // Exit the loop if the desired byte and delay are satisfied
     }
@@ -26,28 +26,24 @@ void SM300D2Sensor::update() {
     previousByte = peeked;
     previousTime = millis();
     this->read();
-// Reset the start time whenever a byte is registered
+    // Reset the start time whenever a byte is registered
     startTime = millis();
   }
-//reading in the 19 bytes into memory
+  //reading in the 19 bytes into memory
   bool read_success = read_array(response, SM300D2_RESPONSE_LENGTH);
-
-
-// If no data recieved within 1 sec. Throw error 
+  // If no data recieved within 1 sec. Throw error
   if (!read_success) {
     ESP_LOGW(TAG, "Reading data from SM300D2 failed!");
     status_set_warning();
     return;
   }
-
   this->status_clear_warning();
-
   ESP_LOGD(TAG, "Successfully read SM300D2 data %u",response);
-  //Comms Address of device
+  // Comms Address of device
   const uint16_t addr = (response[0]);
-  //Vendor Function Type
+  // Vendor Function Type
   const uint16_t function = (response[1]) + response[2];
-  //Sensor Data
+  // Sensor Data
   const uint16_t co2 = (response[3] * 256) + response[4];
   const uint16_t formaldehyde = (response[5] * 256) + response[6];
   const uint16_t tvoc = (response[7] * 256) + response[8];
@@ -59,52 +55,35 @@ void SM300D2Sensor::update() {
                                 : response[13] + (response[14] * 0.1f);
   const float humidity = response[15] + (response[14] * 0.1f);
 
-  //Report out to log and publish to HA
+  // Report out to log and publish to HA
   ESP_LOGD(TAG, "Received Addr: %u", addr);
   if (this->addr_sensor_ != nullptr)
     this->addr_sensor_->publish_state(addr);
-  
   ESP_LOGD(TAG, "Received Function Type: %u" , function);
   if (this->function_sensor_ != nullptr)
     this->function_sensor_->publish_state(function);
-
   ESP_LOGD(TAG, "Received CO₂: %u ppm", co2);
   if (this->co2_sensor_ != nullptr)
     this->co2_sensor_->publish_state(co2);
-
   ESP_LOGD(TAG, "Received Formaldehyde: %u µg/m³", formaldehyde);
   if (this->formaldehyde_sensor_ != nullptr)
     this->formaldehyde_sensor_->publish_state(formaldehyde);
-
   ESP_LOGD(TAG, "Received TVOC: %u µg/m³", tvoc);
   if (this->tvoc_sensor_ != nullptr)
     this->tvoc_sensor_->publish_state(tvoc);
-
   ESP_LOGD(TAG, "Received PM2.5: %u µg/m³", pm_2_5);
   if (this->pm_2_5_sensor_ != nullptr)
     this->pm_2_5_sensor_->publish_state(pm_2_5);
-
   ESP_LOGD(TAG, "Received PM10: %u µg/m³", pm_10_0);
   if (this->pm_10_0_sensor_ != nullptr)
     this->pm_10_0_sensor_->publish_state(pm_10_0);
-
   ESP_LOGD(TAG, "Received Temperature: %.2f °C", temperature);
   if (this->temperature_sensor_ != nullptr)
     this->temperature_sensor_->publish_state(temperature);
-
   ESP_LOGD(TAG, "Received Humidity: %.2f percent", humidity);
   if (this->humidity_sensor_ != nullptr)
     this->humidity_sensor_->publish_state(humidity);
 }
-
-//Checksum validation is broken "work in progress"
-//uint16_t SM300D2Sensor::sm300d2_checksum_(uint8_t *ptr) {
-//  uint8_t sum = 0;
-//  for (int i = 0; i < (SM300D2_RESPONSE_LENGTH - 1); i++) {
-//    sum += *ptr++;
-//  }
-//  return sum;
-//}
 
 void SM300D2Sensor::dump_config() {
   ESP_LOGCONFIG(TAG, "SM300D2:");
@@ -118,5 +97,5 @@ void SM300D2Sensor::dump_config() {
   this->check_uart_settings(9600);
 }
 
-}  // namespace sm300d2
+}  // namespace sm300d2v2
 }  // namespace esphome
