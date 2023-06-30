fde0941ed7b1b4dd4640f9c3632a98663daa2bf7
NP v/d Spek <github_mail@lumensoft.nl>
2023-06-30 11:34:14 +0200

    fixed version number

--- esphome/components/fastled/light.py
+++ esphome/components/fastled/light.py
@@ -131,7 +131,7 @@ async def new_fastled_light(config):
         cg.add(var.set_max_refresh_rate(config[CONF_MAX_REFRESH_RATE]))
 
     await light.register_light(var, config)
-    cg.add_library("FastLED", "3.5.0")
+    cg.add_library("fastled/FastLED", "3.6.0")
     return var
 
 
