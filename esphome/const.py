a77cf1beec9454b21568cc6ef4413ebafb04e5f9
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-07-10 11:24:49 +1200

    Bump version to 2023.6.5

--- esphome/const.py
+++ esphome/const.py
@@ -1,6 +1,6 @@
 """Constants used by esphome."""
 
-__version__ = "2023.6.4"
+__version__ = "2023.6.5"
 
 ALLOWED_NAME_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789-_"
 VALID_SUBSTITUTIONS_CHARACTERS = (
