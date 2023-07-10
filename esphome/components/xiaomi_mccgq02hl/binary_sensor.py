edd78240389dd4fc43b804906aba501eac467c38
JordyH66 <emailhuangmingming@163.com>
2023-07-11 00:08:00 +0800

    Increase battery parameter acquisition, and test attributes through HA platform are valid

--- esphome/components/xiaomi_mccgq02hl/binary_sensor.py
+++ esphome/components/xiaomi_mccgq02hl/binary_sensor.py
@@ -1,13 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import binary_sensor, esp32_ble_tracker
+from esphome.components import sensor, binary_sensor, esp32_ble_tracker
 from esphome.const import (
     CONF_MAC_ADDRESS,
     CONF_BINDKEY,
     CONF_LIGHT,
     DEVICE_CLASS_LIGHT,
     DEVICE_CLASS_DOOR,
-    CONF_ON_OPEN,
+    CONF_OPEN,
+    CONF_BATTERY_LEVEL,
+    UNIT_PERCENT,
+    DEVICE_CLASS_BATTERY,
+    ENTITY_CATEGORY_DIAGNOSTIC,
 )
 
 DEPENDENCIES = ["esp32_ble_tracker"]
@@ -22,16 +26,22 @@ XiaomiMCCGQ02HL = xiaomi_mccgq02hl_ns.class_(
 )
 
 CONFIG_SCHEMA = cv.All(
-    binary_sensor.binary_sensor_schema(XiaomiMCCGQ02HL, device_class=DEVICE_CLASS_DOOR)
+    binary_sensor.binary_sensor_schema(XiaomiMCCGQ02HL)
     .extend(
         {
             cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
             cv.Required(CONF_BINDKEY): cv.bind_key,
-            cv.Optional(CONF_ON_OPEN, default=True): binary_sensor.binary_sensor_schema(
+            cv.Optional(CONF_OPEN, default=True): binary_sensor.binary_sensor_schema(
                 device_class=DEVICE_CLASS_DOOR
             ),
             cv.Optional(CONF_LIGHT): binary_sensor.binary_sensor_schema(
                 device_class=DEVICE_CLASS_LIGHT
+            ),
+            cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(
+                unit_of_measurement=UNIT_PERCENT,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_BATTERY,
+                entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
             ),
         }
     )
@@ -48,9 +58,12 @@ async def to_code(config):
     cg.add(var.set_address(config[CONF_MAC_ADDRESS].as_hex))
     cg.add(var.set_bindkey(config[CONF_BINDKEY]))
 
-    if CONF_ON_OPEN in config:
-        sens = await binary_sensor.new_binary_sensor(config[CONF_ON_OPEN])
+    if CONF_OPEN in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_OPEN])
         cg.add(var.set_open(sens))
     if CONF_LIGHT in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_LIGHT])
         cg.add(var.set_light(sens))
+    if CONF_BATTERY_LEVEL in config:
+        sens = await sensor.new_sensor(config[CONF_BATTERY_LEVEL])
+        cg.add(var.set_battery_level(sens))
