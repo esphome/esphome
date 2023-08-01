453ae728a320dd84dc71f1b4bdc050a3bd3fd900
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-08-02 07:50:23 +1200

    Add device class

--- esphome/components/a01nyub/sensor.py
+++ esphome/components/a01nyub/sensor.py
@@ -4,6 +4,7 @@ from esphome.const import (
     STATE_CLASS_MEASUREMENT,
     UNIT_METER,
     ICON_ARROW_EXPAND_VERTICAL,
+    DEVICE_CLASS_DISTANCE,
 )
 
 CODEOWNERS = ["@MrSuicideParrot"]
@@ -20,6 +21,7 @@ CONFIG_SCHEMA = sensor.sensor_schema(
     icon=ICON_ARROW_EXPAND_VERTICAL,
     accuracy_decimals=3,
     state_class=STATE_CLASS_MEASUREMENT,
+    device_class=DEVICE_CLASS_DISTANCE,
 ).extend(uart.UART_DEVICE_SCHEMA)
 
 FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
