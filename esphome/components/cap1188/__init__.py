f81c556b63fe8949ffbe5cd8e3896ec5e8040fe3
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-08-02 16:25:26 +1200

    Update components "if x in config" (#5181)

--- esphome/components/cap1188/__init__.py
+++ esphome/components/cap1188/__init__.py
@@ -37,8 +37,8 @@ async def to_code(config):
     cg.add(var.set_touch_threshold(config[CONF_TOUCH_THRESHOLD]))
     cg.add(var.set_allow_multiple_touches(config[CONF_ALLOW_MULTIPLE_TOUCHES]))
 
-    if CONF_RESET_PIN in config:
-        pin = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
+    if reset_pin_config := config.get(CONF_RESET_PIN):
+        pin = await cg.gpio_pin_expression(reset_pin_config)
         cg.add(var.set_reset_pin(pin))
 
     await cg.register_component(var, config)
