fe8cfe48704bf1354ad07aac135fe8f9d662868b
Dominik Skalník <skaldo91@gmail.com>
2023-08-18 21:57:20 +0200

    Remove unused CONF_CS_PIN import

--- esphome/components/sharp_memory_lcd/display.py
+++ esphome/components/sharp_memory_lcd/display.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.components import display, spi
-from esphome.const import CONF_ID, CONF_LAMBDA, CONF_WIDTH, CONF_HEIGHT, CONF_CS_PIN
+from esphome.const import CONF_ID, CONF_LAMBDA, CONF_WIDTH, CONF_HEIGHT
 
 CODEOWNERS = ["@skaldo"]
 DEPENDENCIES = ["spi"]
