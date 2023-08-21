62675ae963e137401eb06bb569023efa35286d3a
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2023-08-21 15:57:29 +1000

    Clang tidy

--- esphome/components/spi_rgb_led/spi_rgb_led.h
+++ esphome/components/spi_rgb_led/spi_rgb_led.h
@@ -14,8 +14,8 @@ namespace spi_rgb_led {
 
 static const char *const TAG = "spi_rgb_led";
 class SpiRgbLed : public light::AddressableLight,
-               public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_HIGH, spi::CLOCK_PHASE_TRAILING,
-                                     SPIRGBLED_DATA_RATE> {
+                  public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_HIGH, spi::CLOCK_PHASE_TRAILING,
+                                        SPIRGBLED_DATA_RATE> {
  public:
   void setup() {}
 
