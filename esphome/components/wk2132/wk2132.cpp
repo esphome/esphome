a8f38139955bf1f4a2b4d62b8002e0e96716e707
DrCoolZic <DrCoolZic@hotmail.com>
2023-08-21 18:25:19 +0200

    sniffing only when initialized

--- esphome/components/wk2132/wk2132.cpp
+++ esphome/components/wk2132/wk2132.cpp
@@ -267,7 +267,7 @@ bool WK2132Channel::read_data_(uint8_t *buffer, size_t len) {
   auto error = parent_->read(buffer, len);
   if (error == i2c::ERROR_OK) {
     parent_->status_clear_warning();
-    if (parent_->test_mode_.test(1))  // test sniff (bit 1)
+    if (parent_->test_mode_.test(1) && parent_->initialized_)  // test sniff (bit 1)
       ESP_LOGI(TAG, "sniff reading %d chars %02X... on UART @%02X channel %d", len, *buffer, parent_->base_address_,
                channel_);
     ESP_LOGV(TAG, "read_data(ch=%d buffer[0]=%02X [%s], len=%d): I2C code %d", channel_, *buffer, i2c(*buffer), len,
@@ -290,7 +290,7 @@ bool WK2132Channel::write_data_(const uint8_t *buffer, size_t len) {
   auto error = parent_->write(buffer, len);
   if (error == i2c::ERROR_OK) {
     parent_->status_clear_warning();
-    if (parent_->test_mode_.test(1))  // test sniff (bit 1)
+    if (parent_->test_mode_.test(1) && parent_->initialized_)  // test sniff (bit 1)
       ESP_LOGI(TAG, "sniff sending %d chars %02X... on UART @%02X channel %d", len, *buffer, parent_->base_address_,
                channel_);
     ESP_LOGV(TAG, "write_data(ch=%d buffer[0]=%02X [%s], len=%d): I2C code %d", channel_, *buffer, i2c(*buffer), len,
