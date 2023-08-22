dbcac8a810eed04968659e51aeba5030fdc0c859
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2023-08-22 10:45:28 +1000

    Update other uses of CONF_STRAPPING

--- esphome/pins.py
+++ esphome/pins.py
@@ -9,7 +9,7 @@ from esphome.const import (
     CONF_OUTPUT,
     CONF_PULLDOWN,
     CONF_PULLUP,
-    CONF_STRAPPING,
+    CONF_IGNORE_STRAPPING_WARNING,
 )
 from esphome.util import SimpleRegistry
 from esphome.core import CORE
@@ -152,12 +152,12 @@ def check_strapping_pin(conf, strapping_pin_list, logger):
     import esphome.config_validation as cv
 
     num = conf[CONF_NUMBER]
-    if num in strapping_pin_list and not conf.get(CONF_STRAPPING):
+    if num in strapping_pin_list and not conf.get(CONF_IGNORE_STRAPPING_WARNING):
         logger.warning(
             f"GPIO{num} is a strapping PIN and should only be used for I/O with care.\n"
             "Attaching external pullup/down resistors to strapping pins can cause unexpected failures.\n"
             "See https://esphome.io/guides/faq.html#why-am-i-getting-a-warning-about-strapping-pins",
         )
     # mitigate undisciplined use of strapping:
-    if num not in strapping_pin_list and conf.get(CONF_STRAPPING):
+    if num not in strapping_pin_list and conf.get(CONF_IGNORE_STRAPPING_WARNING):
         raise cv.Invalid(f"GPIO{num} is not a strapping pin")
