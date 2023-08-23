esphome/esphome

commit 98bf4276000e39f22f4b2977fe7ba4d05609a25c

Add standardized CRC helper functions (#4798)
Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

esphome/components/pipsolar/pipsolar.cpp
@@ -1,5 +1,6 @@
 #include "pipsolar.h"
 #include "esphome/core/log.h"
+#include "esphome/core/helpers.h"
 
 namespace esphome {
 namespace pipsolar {
@@ -768,7 +769,7 @@ uint8_t Pipsolar::check_incoming_length_(uint8_t length) {
 
 uint8_t Pipsolar::check_incoming_crc_() {
   uint16_t crc16;
-  crc16 = cal_crc_half_(read_buffer_, read_pos_ - 3);
+  crc16 = crc16be(read_buffer_, read_pos_ - 3);
   ESP_LOGD(TAG, "checking crc on incoming message");
   if (((uint8_t) ((crc16) >> 8)) == read_buffer_[read_pos_ - 3] &&
       ((uint8_t) ((crc16) &0xff)) == read_buffer_[read_pos_ - 2]) {
@@ -797,7 +798,7 @@ uint8_t Pipsolar::send_next_command_() {
     this->command_start_millis_ = millis();
     this->empty_uart_buffer_();
     this->read_pos_ = 0;
-    crc16 = cal_crc_half_(byte_command, length);
+    crc16 = crc16be(byte_command, length);
     this->write_str(command);
     // checksum
     this->write(((uint8_t) ((crc16) >> 8)));   // highbyte
@@ -824,8 +825,8 @@ void Pipsolar::send_next_poll_() {
   this->command_start_millis_ = millis();
   this->empty_uart_buffer_();
   this->read_pos_ = 0;
-  crc16 = cal_crc_half_(this->used_polling_commands_[this->last_polling_command_].command,
-                        this->used_polling_commands_[this->last_polling_command_].length);
+  crc16 = crc16be(this->used_polling_commands_[this->last_polling_command_].command,
+                  this->used_polling_commands_[this->last_polling_command_].length);
   this->write_array(this->used_polling_commands_[this->last_polling_command_].command,
                     this->used_polling_commands_[this->last_polling_command_].length);
   // checksum
@@ -890,43 +891,6 @@ void Pipsolar::add_polling_command_(const char *command, ENUMPollingCommand poll
       return;
     }
   }
-}
-
-uint16_t Pipsolar::cal_crc_half_(uint8_t *msg, uint8_t len) {
-  uint16_t crc;
-
-  uint8_t da;
-  uint8_t *ptr;
-  uint8_t b_crc_hign;
-  uint8_t b_crc_low;
-
-  uint16_t crc_ta[16] = {0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
-                         0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef};
-
-  ptr = msg;
-  crc = 0;
-
-  while (len-- != 0) {
-    da = ((uint8_t) (crc >> 8)) >> 4;
-    crc <<= 4;
-    crc ^= crc_ta[da ^ (*ptr >> 4)];
-    da = ((uint8_t) (crc >> 8)) >> 4;
-    crc <<= 4;
-    crc ^= crc_ta[da ^ (*ptr & 0x0f)];
-    ptr++;
-  }
-
-  b_crc_low = crc;
-  b_crc_hign = (uint8_t) (crc >> 8);
-
-  if (b_crc_low == 0x28 || b_crc_low == 0x0d || b_crc_low == 0x0a)
-    b_crc_low++;
-  if (b_crc_hign == 0x28 || b_crc_hign == 0x0d || b_crc_hign == 0x0a)
-    b_crc_hign++;
-
-  crc = ((uint16_t) b_crc_hign) << 8;
-  crc += b_crc_low;
-  return (crc);
 }
 
 }  // namespace pipsolar

esphome/components/sml/constants.h
@@ -17,32 +17,9 @@ enum SmlType : uint8_t {
 
 enum SmlMessageType : uint16_t { SML_PUBLIC_OPEN_RES = 0x0101, SML_GET_LIST_RES = 0x701 };
 
-enum Crc16CheckResult : uint8_t { CHECK_CRC16_FAILED, CHECK_CRC16_X25_SUCCESS, CHECK_CRC16_KERMIT_SUCCESS };
-
 // masks with two-bit mapping 0x1b -> 0b01; 0x01 -> 0b10; 0x1a -> 0b11
 const uint16_t START_MASK = 0x55aa;  // 0x1b 1b 1b 1b 1b 01 01 01 01
 const uint16_t END_MASK = 0x0157;    // 0x1b 1b 1b 1b 1a
-
-const uint16_t CRC16_X25_TABLE[256] = {
-    0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf, 0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5,
-    0xe97e, 0xf8f7, 0x1081, 0x0108, 0x3393, 0x221a, 0x56a5, 0x472c, 0x75b7, 0x643e, 0x9cc9, 0x8d40, 0xbfdb, 0xae52,
-    0xdaed, 0xcb64, 0xf9ff, 0xe876, 0x2102, 0x308b, 0x0210, 0x1399, 0x6726, 0x76af, 0x4434, 0x55bd, 0xad4a, 0xbcc3,
-    0x8e58, 0x9fd1, 0xeb6e, 0xfae7, 0xc87c, 0xd9f5, 0x3183, 0x200a, 0x1291, 0x0318, 0x77a7, 0x662e, 0x54b5, 0x453c,
-    0xbdcb, 0xac42, 0x9ed9, 0x8f50, 0xfbef, 0xea66, 0xd8fd, 0xc974, 0x4204, 0x538d, 0x6116, 0x709f, 0x0420, 0x15a9,
-    0x2732, 0x36bb, 0xce4c, 0xdfc5, 0xed5e, 0xfcd7, 0x8868, 0x99e1, 0xab7a, 0xbaf3, 0x5285, 0x430c, 0x7197, 0x601e,
-    0x14a1, 0x0528, 0x37b3, 0x263a, 0xdecd, 0xcf44, 0xfddf, 0xec56, 0x98e9, 0x8960, 0xbbfb, 0xaa72, 0x6306, 0x728f,
-    0x4014, 0x519d, 0x2522, 0x34ab, 0x0630, 0x17b9, 0xef4e, 0xfec7, 0xcc5c, 0xddd5, 0xa96a, 0xb8e3, 0x8a78, 0x9bf1,
-    0x7387, 0x620e, 0x5095, 0x411c, 0x35a3, 0x242a, 0x16b1, 0x0738, 0xffcf, 0xee46, 0xdcdd, 0xcd54, 0xb9eb, 0xa862,
-    0x9af9, 0x8b70, 0x8408, 0x9581, 0xa71a, 0xb693, 0xc22c, 0xd3a5, 0xe13e, 0xf0b7, 0x0840, 0x19c9, 0x2b52, 0x3adb,
-    0x4e64, 0x5fed, 0x6d76, 0x7cff, 0x9489, 0x8500, 0xb79b, 0xa612, 0xd2ad, 0xc324, 0xf1bf, 0xe036, 0x18c1, 0x0948,
-    0x3bd3, 0x2a5a, 0x5ee5, 0x4f6c, 0x7df7, 0x6c7e, 0xa50a, 0xb483, 0x8618, 0x9791, 0xe32e, 0xf2a7, 0xc03c, 0xd1b5,
-    0x2942, 0x38cb, 0x0a50, 0x1bd9, 0x6f66, 0x7eef, 0x4c74, 0x5dfd, 0xb58b, 0xa402, 0x9699, 0x8710, 0xf3af, 0xe226,
-    0xd0bd, 0xc134, 0x39c3, 0x284a, 0x1ad1, 0x0b58, 0x7fe7, 0x6e6e, 0x5cf5, 0x4d7c, 0xc60c, 0xd785, 0xe51e, 0xf497,
-    0x8028, 0x91a1, 0xa33a, 0xb2b3, 0x4a44, 0x5bcd, 0x6956, 0x78df, 0x0c60, 0x1de9, 0x2f72, 0x3efb, 0xd68d, 0xc704,
-    0xf59f, 0xe416, 0x90a9, 0x8120, 0xb3bb, 0xa232, 0x5ac5, 0x4b4c, 0x79d7, 0x685e, 0x1ce1, 0x0d68, 0x3ff3, 0x2e7a,
-    0xe70e, 0xf687, 0xc41c, 0xd595, 0xa12a, 0xb0a3, 0x8238, 0x93b1, 0x6b46, 0x7acf, 0x4854, 0x59dd, 0x2d62, 0x3ceb,
-    0x0e70, 0x1ff9, 0xf78f, 0xe606, 0xd49d, 0xc514, 0xb1ab, 0xa022, 0x92b9, 0x8330, 0x7bc7, 0x6a4e, 0x58d5, 0x495c,
-    0x3de3, 0x2c6a, 0x1ef1, 0x0f78};
 
 }  // namespace sml
 }  // namespace esphome

esphome/components/sml/sml.cpp
@@ -1,5 +1,6 @@
 #include "sml.h"
 #include "esphome/core/log.h"
+#include "esphome/core/helpers.h"
 #include "sml_parser.h"
 
 namespace esphome {
@@ -99,34 +100,21 @@ bool check_sml_data(const bytes &buffer) {
   }
 
   uint16_t crc_received = (buffer.at(buffer.size() - 2) << 8) | buffer.at(buffer.size() - 1);
-  if (crc_received == calc_crc16_x25(buffer.begin(), buffer.end() - 2, 0x6e23)) {
+  uint16_t crc_calculated = crc16(buffer.data(), buffer.size(), 0x6e23, 0x8408, true, true);
+  crc_calculated = (crc_calculated >> 8) | (crc_calculated << 8);
+  if (crc_received == crc_calculated) {
     ESP_LOGV(TAG, "Checksum verification successful with CRC16/X25.");
     return true;
   }
 
-  if (crc_received == calc_crc16_kermit(buffer.begin(), buffer.end() - 2, 0xed50)) {
+  crc_calculated = crc16(buffer.data(), buffer.size(), 0xed50, 0x8408);
+  if (crc_received == crc_calculated) {
     ESP_LOGV(TAG, "Checksum verification successful with CRC16/KERMIT.");
     return true;
   }
 
   ESP_LOGW(TAG, "Checksum error in received SML data.");
   return false;
-}
-
-uint16_t calc_crc16_p1021(bytes::const_iterator begin, bytes::const_iterator end, uint16_t crcsum) {
-  for (auto it = begin; it != end; it++) {
-    crcsum = (crcsum >> 8) ^ CRC16_X25_TABLE[(crcsum & 0xff) ^ *it];
-  }
-  return crcsum;
-}
-
-uint16_t calc_crc16_x25(bytes::const_iterator begin, bytes::const_iterator end, uint16_t crcsum = 0) {
-  crcsum = calc_crc16_p1021(begin, end, crcsum ^ 0xffff) ^ 0xffff;
-  return (crcsum >> 8) | ((crcsum & 0xff) << 8);
-}
-
-uint16_t calc_crc16_kermit(bytes::const_iterator begin, bytes::const_iterator end, uint16_t crcsum = 0) {
-  return calc_crc16_p1021(begin, end, crcsum);
 }
 
 uint8_t get_code(uint8_t byte) {

esphome/components/sml/sml.h
@@ -38,9 +38,6 @@ class Sml : public Component, public uart::UARTDevice {
 };
 
 bool check_sml_data(const bytes &buffer);
-uint16_t calc_crc16_p1021(bytes::const_iterator begin, bytes::const_iterator end, uint16_t crcsum);
-uint16_t calc_crc16_x25(bytes::const_iterator begin, bytes::const_iterator end, uint16_t crcsum);
-uint16_t calc_crc16_kermit(bytes::const_iterator begin, bytes::const_iterator end, uint16_t crcsum);
 
 uint8_t get_code(uint8_t byte);
 }  // namespace sml

esphome/core/helpers.cpp
@@ -34,6 +34,9 @@
 #include <limits>
 #include <random>
 #endif
+#ifdef USE_ESP32
+#include "esp32/rom/crc.h"
+#endif
 
 #ifdef USE_ESP32_IGNORE_EFUSE_MAC_CRC
 #include "esp_efuse.h"
@@ -43,6 +46,23 @@
 namespace esphome {
 
 static const char *const TAG = "helpers";
+
+static const uint16_t CRC16_A001_LE_LUT_L[] = {0x0000, 0xc0c1, 0xc181, 0x0140, 0xc301, 0x03c0, 0x0280, 0xc241,
+                                               0xc601, 0x06c0, 0x0780, 0xc741, 0x0500, 0xc5c1, 0xc481, 0x0440};
+static const uint16_t CRC16_A001_LE_LUT_H[] = {0x0000, 0xcc01, 0xd801, 0x1400, 0xf001, 0x3c00, 0x2800, 0xe401,
+                                               0xa001, 0x6c00, 0x7800, 0xb401, 0x5000, 0x9c01, 0x8801, 0x4400};
+
+#ifndef USE_ESP32
+static const uint16_t CRC16_8408_LE_LUT_L[] = {0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf,
+                                               0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7};
+static const uint16_t CRC16_8408_LE_LUT_H[] = {0x0000, 0x1081, 0x2102, 0x3183, 0x4204, 0x5285, 0x6306, 0x7387,
+                                               0x8408, 0x9489, 0xa50a, 0xb58b, 0xc60c, 0xd68d, 0xe70e, 0xf78f};
+
+static const uint16_t CRC16_1021_BE_LUT_L[] = {0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+                                               0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef};
+static const uint16_t CRC16_1021_BE_LUT_H[] = {0x0000, 0x1231, 0x2462, 0x3653, 0x48c4, 0x5af5, 0x6ca6, 0x7e97,
+                                               0x9188, 0x83b9, 0xb5ea, 0xa7db, 0xd94c, 0xcb7d, 0xfd2e, 0xef1f};
+#endif
 
 // STL backports
 
@@ -76,21 +96,79 @@ uint8_t crc8(uint8_t *data, uint8_t len) {
   }
   return crc;
 }
-uint16_t crc16(const uint8_t *data, uint8_t len) {
-  uint16_t crc = 0xFFFF;
-  while (len--) {
-    crc ^= *data++;
-    for (uint8_t i = 0; i < 8; i++) {
-      if ((crc & 0x01) != 0) {
-        crc >>= 1;
-        crc ^= 0xA001;
-      } else {
-        crc >>= 1;
+
+uint16_t crc16(const uint8_t *data, uint16_t len, uint16_t crc, uint16_t reverse_poly, bool refin, bool refout) {
+#ifdef USE_ESP32
+  if (reverse_poly == 0x8408) {
+    crc = crc16_le(refin ? crc : (crc ^ 0xffff), data, len);
+    return refout ? crc : (crc ^ 0xffff);
+  }
+#endif
+  if (refin) {
+    crc ^= 0xffff;
+  }
+#ifndef USE_ESP32
+  if (reverse_poly == 0x8408) {
+    while (len--) {
+      uint8_t combo = crc ^ (uint8_t) *data++;
+      crc = (crc >> 8) ^ CRC16_8408_LE_LUT_L[combo & 0x0F] ^ CRC16_8408_LE_LUT_H[combo >> 4];
+    }
+  } else
+#endif
+      if (reverse_poly == 0xa001) {
+    while (len--) {
+      uint8_t combo = crc ^ (uint8_t) *data++;
+      crc = (crc >> 8) ^ CRC16_A001_LE_LUT_L[combo & 0x0F] ^ CRC16_A001_LE_LUT_H[combo >> 4];
+    }
+  } else {
+    while (len--) {
+      crc ^= *data++;
+      for (uint8_t i = 0; i < 8; i++) {
+        if (crc & 0x0001) {
+          crc = (crc >> 1) ^ reverse_poly;
+        } else {
+          crc >>= 1;
+        }
       }
     }
   }
-  return crc;
+  return refout ? (crc ^ 0xffff) : crc;
 }
+
+uint16_t crc16be(const uint8_t *data, uint16_t len, uint16_t crc, uint16_t poly, bool refin, bool refout) {
+#ifdef USE_ESP32
+  if (poly == 0x1021) {
+    crc = crc16_be(refin ? crc : (crc ^ 0xffff), data, len);
+    return refout ? crc : (crc ^ 0xffff);
+  }
+#endif
+  if (refin) {
+    crc ^= 0xffff;
+  }
+#ifndef USE_ESP32
+  if (poly == 0x1021) {
+    while (len--) {
+      uint8_t combo = (crc >> 8) ^ *data++;
+      crc = (crc << 8) ^ CRC16_1021_BE_LUT_L[combo & 0x0F] ^ CRC16_1021_BE_LUT_H[combo >> 4];
+    }
+  } else {
+#endif
+    while (len--) {
+      crc ^= (((uint16_t) *data++) << 8);
+      for (uint8_t i = 0; i < 8; i++) {
+        if (crc & 0x8000) {
+          crc = (crc << 1) ^ poly;
+        } else {
+          crc <<= 1;
+        }
+      }
+    }
+#ifndef USE_ESP32
+  }
+#endif
+  return refout ? (crc ^ 0xffff) : crc;
+}
+
 uint32_t fnv1_hash(const std::string &str) {
   uint32_t hash = 2166136261UL;
   for (char c : str) {

esphome/core/helpers.h
@@ -155,7 +155,10 @@ template<typename T, typename U> T remap(U value, U min, U max, T min_out, T max
 uint8_t crc8(uint8_t *data, uint8_t len);
 
 /// Calculate a CRC-16 checksum of \p data with size \p len.
-uint16_t crc16(const uint8_t *data, uint8_t len);
+uint16_t crc16(const uint8_t *data, uint16_t len, uint16_t crc = 0xffff, uint16_t reverse_poly = 0xa001,
+               bool refin = false, bool refout = false);
+uint16_t crc16be(const uint8_t *data, uint16_t len, uint16_t crc = 0, uint16_t poly = 0x1021, bool refin = false,
+                 bool refout = false);
 
 /// Calculate a FNV-1 hash of \p str.
 uint32_t fnv1_hash(const std::string &str);

commit 0ac56e0799720ddcc9200408b4b400d08335d2e2

Fix pipsolar CRC calculation
Fixes bug introduced in 98bf427.

esphome/components/pipsolar/pipsolar.cpp
@@ -770,16 +770,25 @@ uint8_t Pipsolar::check_incoming_length_(uint8_t length) {
 uint8_t Pipsolar::check_incoming_crc_() {
   uint16_t crc16;
   crc16 = crc16be(read_buffer_, read_pos_ - 3);
+
+  /* voltronic/pipsolar inverters block certain characters from CRC */
+  uint8_t b_crc_high = ((crc16) >> 8);
+  uint8_t b_crc_low  = ((crc16) & 0xff);
+  if (b_crc_high == 0x28 || b_crc_high == 0x0d || b_crc_high == 0x0a)
+    b_crc_high++;
+  if (b_crc_low == 0x28 || b_crc_low == 0x0d || b_crc_low == 0x0a)
+    b_crc_low++;
+
   ESP_LOGD(TAG, "checking crc on incoming message");
-  if (((uint8_t) ((crc16) >> 8)) == read_buffer_[read_pos_ - 3] &&
-      ((uint8_t) ((crc16) &0xff)) == read_buffer_[read_pos_ - 2]) {
+  if ((b_crc_high == read_buffer_[read_pos_ - 3]) &&
+      (b_crc_low  == read_buffer_[read_pos_ - 2])) {
     ESP_LOGD(TAG, "CRC OK");
     read_buffer_[read_pos_ - 1] = 0;
     read_buffer_[read_pos_ - 2] = 0;
     read_buffer_[read_pos_ - 3] = 0;
     return 1;
   }
-  ESP_LOGD(TAG, "CRC NOK expected: %X %X but got: %X %X", ((uint8_t) ((crc16) >> 8)), ((uint8_t) ((crc16) &0xff)),
+  ESP_LOGD(TAG, "CRC NOK expected: %X %X but got: %X %X", b_crc_high, b_crc_low,
            read_buffer_[read_pos_ - 3], read_buffer_[read_pos_ - 2]);
   return 0;
 }
@@ -800,9 +809,19 @@ uint8_t Pipsolar::send_next_command_() {
     this->read_pos_ = 0;
     crc16 = crc16be(byte_command, length);
     this->write_str(command);
+
+    /* voltronic/pipsolar inverters block certain characters from CRC */
+    uint8_t b_crc_high = ((crc16) >> 8);
+    uint8_t b_crc_low  = ((crc16) & 0xff);
+    if (b_crc_high == 0x28 || b_crc_high == 0x0d || b_crc_high == 0x0a)
+      b_crc_high++;
+    if (b_crc_low == 0x28 || b_crc_low == 0x0d || b_crc_low == 0x0a)
+      b_crc_low++;
+
     // checksum
-    this->write(((uint8_t) ((crc16) >> 8)));   // highbyte
-    this->write(((uint8_t) ((crc16) &0xff)));  // lowbyte
+    this->write(b_crc_high);   // highbyte
+    this->write(b_crc_low);    // lowbyte
+
     // end Byte
     this->write(0x0D);
     ESP_LOGD(TAG, "Sending command from queue: %s with length %d", command, length);
@@ -829,9 +848,18 @@ void Pipsolar::send_next_poll_() {
                   this->used_polling_commands_[this->last_polling_command_].length);
   this->write_array(this->used_polling_commands_[this->last_polling_command_].command,
                     this->used_polling_commands_[this->last_polling_command_].length);
+    /* voltronic/pipsolar inverters block certain characters from CRC */
+  uint8_t b_crc_high = ((crc16) >> 8);
+  uint8_t b_crc_low  = ((crc16) & 0xff);
+  if (b_crc_high == 0x28 || b_crc_high == 0x0d || b_crc_high == 0x0a)
+    b_crc_high++;
+  if (b_crc_low == 0x28 || b_crc_low == 0x0d || b_crc_low == 0x0a)
+    b_crc_low++;
+
   // checksum
-  this->write(((uint8_t) ((crc16) >> 8)));   // highbyte
-  this->write(((uint8_t) ((crc16) &0xff)));  // lowbyte
+  this->write(b_crc_high);   // highbyte
+  this->write(b_crc_low);    // lowbyte
+
   // end Byte
   this->write(0x0D);
   ESP_LOGD(TAG, "Sending polling command : %s with length %d",