49a64f16c7ac266f1cf7237237a63395c1a26d41
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2023-08-24 19:56:00 +1000

    Checkpoint

--- esphome/components/spi/spi.cpp
+++ esphome/components/spi/spi.cpp
@@ -2,14 +2,15 @@
 #include "esphome/core/log.h"
 #include "esphome/core/application.h"
 
+
 namespace esphome {
 namespace spi {
 
+static uint8_t spi_bus_num = 0;
 const char *const TAG = "spi";
 
-namespace {
-uint8_t spi_bus_num = 0;
-}
+SPIDelegateDummy SPIDelegate::null_delegate;
+GPIOPin *NullPin::null_pin = new NullPin();
 
 SPIDelegate *SPIComponent::register_device(SPIClient *device,
                                            SPIMode mode,
@@ -18,17 +19,18 @@ SPIDelegate *SPIComponent::register_device(SPIClient *device,
                                            GPIOPin *cs_pin) {
 
   if (this->devices_.count(device) != 0) {
-    esph_log_e(TAG, "SPI device already registered");
+    ESP_LOGE(TAG, "SPI device already registered");
     return this->devices_[device];
   }
+  SPIDelegate *delegate;
   if (this->spi_channel_ == nullptr) {
     // NOLINT
-    SPIDelegate *delegate = new SPIDelegateBitBash(data_rate, bit_order, SPIComponent::get_phase(mode),
-                                                   SPIComponent::get_polarity(mode), cs_pin,
-                                                   this->clk_pin_, this->sdo_pin_, this->sdi_pin_);
-    this->devices_[device] = delegate;
-    return delegate;
-  }
+    delegate = new SPIDelegateBitBash(data_rate, bit_order, mode, cs_pin, this->clk_pin_, this->sdo_pin_,
+                                                   this->sdi_pin_);
+  } else
+    delegate = new SPIHWDelegate(this->spi_channel_, data_rate, bit_order, mode, cs_pin); //NOLINT
+  this->devices_[device] = delegate;
+  return delegate;
 }
 
 void SPIComponent::unregister_device(SPIClient *device) {
@@ -40,35 +42,8 @@ void SPIComponent::unregister_device(SPIClient *device) {
   this->devices_.erase(device);
 }
 
-SPIMode SPIComponent::get_mode(SPIClockPolarity polarity, SPIClockPhase phase) {
-  if (polarity == CLOCK_POLARITY_HIGH) {
-    return phase == CLOCK_PHASE_LEADING ? MODE2 : MODE3;
-  }
-  return phase == CLOCK_PHASE_LEADING ? MODE0 : MODE1;
-}
-
-SPIClockPhase SPIComponent::get_phase(SPIMode mode) {
-  switch (mode) {
-    case MODE0:
-    case MODE2:
-      return CLOCK_PHASE_LEADING;
-    default:
-      return CLOCK_PHASE_TRAILING;
-  }
-}
-
-SPIClockPolarity SPIComponent::get_polarity(SPIMode mode) {
-  switch (mode) {
-    case MODE0:
-    case MODE1:
-      return CLOCK_POLARITY_LOW;
-    default:
-      return CLOCK_POLARITY_HIGH;
-  }
-}
-
 void SPIComponent::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up SPI bus...");
+  ESP_LOGD(TAG, "Setting up SPI bus...");
   this->clk_pin_->setup();
   this->clk_pin_->digital_write(true);
 
@@ -101,19 +76,21 @@ void SPIComponent::setup() {
       mosi_pin = has_mosi ? mosi_internal->get_pin() : -1;
     }
   }
+
 #ifdef USE_ESP8266
   if (!(clk_pin == 6 && miso_pin == 7 && mosi_pin == 8) &&
       !(clk_pin == 14 && (!has_miso || miso_pin == 12) && (!has_mosi || mosi_pin == 13)))
     use_hw_spi = false;
 
   if (use_hw_spi) {
-    this->hw_spi_ = &SPI;
-    this->hw_spi_->pins(clk_pin, miso_pin, mosi_pin, 0);
-    this->hw_spi_->begin();
+    this->spi_channel_ = &SPI;
+    this->spi_channel_->pins(clk_pin, miso_pin, mosi_pin, 0);
+    this->spi_channel_->begin();
     return;
   }
 #endif  // USE_ESP8266
 #ifdef USE_ESP32
+
   if (spi_bus_num >= 2) {
     use_hw_spi = false;
   }
@@ -178,23 +155,17 @@ void SPIComponent::dump_config() {
 
 float SPIComponent::get_setup_priority() const { return setup_priority::BUS; }
 
+void SPIDelegateDummy::begin_transaction() {
+  ESP_LOGE(TAG, "SPIDevice not initialised - did you call spi_setup()?");
+}
+
 void SPIClient::enable() {
-  if (this->delegate_ == nullptr) {
-    ESP_LOGE(TAG, "Must call spi_setup() before enable()");
-    return;
-  }
   this->delegate_->begin_transaction();
 }
 
-void SPIClient::disable() { this->delegate_->end_transaction(); }
-
-// NOLINTNEXTLINE
-#ifndef CLANG_TIDY
-#pragma GCC optimize("unroll-loops")
-// NOLINTNEXTLINE
-#pragma GCC optimize("O2")
-#endif  // CLANG_TIDY
-
+void SPIClient::disable() {
+  this->delegate_->end_transaction();
+}
 
 }  // namespace spi
 }  // namespace esphome
