de1df2887928bf6a397f8e5645fb5857603588b7
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2023-08-28 13:42:41 +1000

    Clang-format

--- esphome/components/spi/spi_arduino.cpp
+++ esphome/components/spi/spi_arduino.cpp
@@ -12,9 +12,9 @@ static std::vector<std::function<SPIClass *()>> bus_list = {
 #ifdef USE_ESP32
 #if defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3) || \
     defined(USE_ESP32_VARIANT_ESP32C2) || defined(USE_ESP32_VARIANT_ESP32C6)
-    [] { return new SPIClass(FSPI); }, // NOLINT(cppcoreguidelines-owning-memory,-warnings-as-errors)
+    [] { return new SPIClass(FSPI); },  // NOLINT(cppcoreguidelines-owning-memory,-warnings-as-errors)
 #else
-    [] { return new SPIClass(HSPI); }, // NOLINT(cppcoreguidelines-owning-memory,-warnings-as-errors)
+    [] { return new SPIClass(HSPI); },  // NOLINT(cppcoreguidelines-owning-memory,-warnings-as-errors)
 #endif  // USE_ESP32_VARIANT
 #endif
     [] { return &SPI; }};
