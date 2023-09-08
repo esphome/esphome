6cdcbf84c0ebfeab9f6f150766652b29c3bb7421
xuzhenxing <xrnmail@163.com>
2023-09-08 15:23:10 +0800

    esp32: Update copy_files() for rm components

--- esphome/components/esp32/__init__.py
+++ esphome/components/esp32/__init__.py
@@ -583,9 +583,9 @@ def copy_files():
 
         import shutil
 
-        shutil.rmtree(CORE.relative_build_path("components"), ignore_errors=True)
-
         if CORE.data[KEY_ESP32][KEY_COMPONENTS]:
+            shutil.rmtree(CORE.relative_build_path("components"), ignore_errors=True)
+
             components: dict = CORE.data[KEY_ESP32][KEY_COMPONENTS]
 
             for name, component in components.items():
