f8deb6b7b8f047b9db2ad98b7485f709131b0d09
Péter Sárközi <xmisterhu@gmail.com>
2023-09-09 21:42:59 +0200

    Add TCP stack init when WiFi is disabled

--- esphome/components/wifi/wifi_component.cpp
+++ esphome/components/wifi/wifi_component.cpp
@@ -40,6 +40,9 @@ void WiFiComponent::setup() {
   if (this->enable_on_boot_) {
     this->start();
   } else {
+#if defined(USE_ESP32) || defined(USE_ESP8266)
+    esp_netif_init();
+#endif
     this->state_ = WIFI_COMPONENT_STATE_DISABLED;
   }
 }
