d1ac33ee780bb06ae127ca2768e0952e65ced03c
fsiek <si@cypheros.de>
2023-10-02 00:34:45 +0200

    Fixed formating
    Signed-off-by: fsiek <si@cypheros.de>

--- esphome/components/ili9xxx/ili9xxx_display.cpp
+++ esphome/components/ili9xxx/ili9xxx_display.cpp
@@ -23,10 +23,8 @@ void ILI9XXXDisplay::setup() {
   uint32_t max_mem = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT);
   if (this->buffer_color_mode_ == BITS_16) {
     if (this->get_buffer_length_() * 2 > max_mem) {
-      //Reduce size
-      downsize_resolution(2);
-      //fill display without using the buffer to clear border around buffered screen
-      this->fill_spi(this->x_high_, this->y_high_, this->x_low_, this->y_low_, 0x0000);
+      downsize_resolution(2);//Reduce size.
+      this->fill_spi(this->x_high_, this->y_high_, this->x_low_, this->y_low_, 0x0000);//fill display without using the buffer to clear border around buffered screen
     }
     this->init_internal_(this->get_buffer_length_() * 2);
     if (this->buffer_ != nullptr) {
@@ -35,10 +33,8 @@ void ILI9XXXDisplay::setup() {
     this->buffer_color_mode_ = BITS_8;
   }
   if (this->get_buffer_length_() > max_mem) {
-    //Reduce size
-    downsize_resolution(1);
-    //fill display without using the buffer to clear border around buffered screen
-    this->fill_spi(this->x_high_, this->y_high_, this->x_low_, this->y_low_, 0x0000);
+    downsize_resolution(1);//Reduce size
+    this->fill_spi(this->x_high_, this->y_high_, this->x_low_, this->y_low_, 0x0000);//fill display without using the buffer to clear border around buffered screen
   }
   this->init_internal_(this->get_buffer_length_());
   if (this->buffer_ == nullptr) {
