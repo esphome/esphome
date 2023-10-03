a401a48602700992f2d80c746e3f065b602b446d
RubyBailey <60991881+RubyBailey@users.noreply.github.com>
2023-10-02 22:15:29 -0700

    Update climate.py
    updates fan, vane directions and optional horizontal and vertical vane settings

--- esphome/components/mitsubishi/climate.py
+++ esphome/components/mitsubishi/climate.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_LOW, CONF_MEDIUM, CONF_HIGH
 
 CODEOWNERS = ["@RubyBailey"]
 AUTO_LOAD = ["climate_ir"]
@@ -9,18 +9,15 @@ AUTO_LOAD = ["climate_ir"]
 mitsubishi_ns = cg.esphome_ns.namespace("mitsubishi")
 MitsubishiClimate = mitsubishi_ns.class_("MitsubishiClimate", climate_ir.ClimateIR)
 
-CONF_SET_FAN_SPEEDS = "set_fan_speeds"
-CONF_FAN_LOW = "fan_low"
-CONF_FAN_MEDIUM = "fan_medium"
-CONF_FAN_HI = "fan_hi"
+CONF_SETFANSPEEDS = "set_fan_speeds"
 
-SetFanSpeeds = mitsubishi_ns.enum("SetFanSpeeds")
+Setfanspeeds = mitsubishi_ns.enum("Setfanspeeds")
 SETFANSPEEDS = {
-    "fan1": SetFanSpeeds.MITSUBISHI_FAN_1,
-    "fan2": SetFanSpeeds.MITSUBISHI_FAN_2,
-    "fan3": SetFanSpeeds.MITSUBISHI_FAN_3,
-    "fan4": SetFanSpeeds.MITSUBISHI_FAN_4,
-    "fan5": SetFanSpeeds.MITSUBISHI_FAN_5,
+    "fan1": Setfanspeeds.MITSUBISHI_FAN_1,
+    "fan2": Setfanspeeds.MITSUBISHI_FAN_2,
+    "fan3": Setfanspeeds.MITSUBISHI_FAN_3,
+    "fan4": Setfanspeeds.MITSUBISHI_FAN_4,
+    "fan5": Setfanspeeds.MITSUBISHI_FAN_5,
 }
 
 
@@ -40,9 +37,9 @@ VerticalDirections = mitsubishi_ns.enum("VerticalDirections")
 VERTICAL_DIRECTIONS = {
     "auto": VerticalDirections.VERTICAL_DIRECTION_AUTO,
     "up": VerticalDirections.VERTICAL_DIRECTION_UP,
-    "mup": VerticalDirections.VERTICAL_DIRECTION_MUP,
+    "middle-up": VerticalDirections.VERTICAL_DIRECTION_MIDDLE_UP,
     "middle": VerticalDirections.VERTICAL_DIRECTION_MIDDLE,
-    "mdown": VerticalDirections.VERTICAL_DIRECTION_MDOWN,
+    "middle-down": VerticalDirections.VERTICAL_DIRECTION_MIDDLE_DOWN,
     "down": VerticalDirections.VERTICAL_DIRECTION_DOWN,
 }
 
@@ -51,18 +48,18 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(MitsubishiClimate),
         cv.Optional(
-            CONF_SET_FAN_SPEEDS,
+            CONF_SETFANSPEEDS,
             default={
-                CONF_FAN_LOW: "fan1",
-                CONF_FAN_MEDIUM: "fan2",
-                CONF_FAN_HI: "fan3",
+                CONF_LOW: "fan1",
+                CONF_MEDIUM: "fan2",
+                CONF_HIGH: "fan3",
             },
         ): cv.All(
             cv.Schema(
                 {
-                    cv.Required(CONF_FAN_LOW): cv.enum(SETFANSPEEDS),
-                    cv.Required(CONF_FAN_MEDIUM): cv.enum(SETFANSPEEDS),
-                    cv.Required(CONF_FAN_HI): cv.enum(SETFANSPEEDS),
+                    cv.Required(CONF_LOW): cv.enum(SETFANSPEEDS),
+                    cv.Required(CONF_MEDIUM): cv.enum(SETFANSPEEDS),
+                    cv.Required(CONF_HIGH): cv.enum(SETFANSPEEDS),
                 }
             ),
         ),
@@ -80,10 +77,14 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await climate_ir.register_climate_ir(var, config)
 
-    fan = config[CONF_SET_FAN_SPEEDS]
-    cg.add(var.set_fan_low(fan[CONF_FAN_LOW]))
-    cg.add(var.set_fan_medium(fan[CONF_FAN_MEDIUM]))
-    cg.add(var.set_fan_hi(fan[CONF_FAN_HI]))
+    if CONF_SETFANSPEEDS in config:
+        fan = config[CONF_SETFANSPEEDS]
+        cg.add(var.set_fan_low(fan[CONF_LOW]))
+        cg.add(var.set_fan_medium(fan[CONF_MEDIUM]))
+        cg.add(var.set_fan_hi(fan[CONF_HIGH]))
 
-    cg.add(var.set_horizontal_default(config[CONF_HORIZONTAL_DEFAULT]))
-    cg.add(var.set_vertical_default(config[CONF_VERTICAL_DEFAULT]))
+    if CONF_HORIZONTAL_DEFAULT in config:
+        cg.add(var.set_horizontal_default(config[CONF_HORIZONTAL_DEFAULT]))
+
+    if CONF_VERTICAL_DEFAULT in config:
+        cg.add(var.set_vertical_default(config[CONF_VERTICAL_DEFAULT]))
