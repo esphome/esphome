a94509d08bcc889665f281f13ebcd0570f4812d4
RubyBailey <60991881+RubyBailey@users.noreply.github.com>
2023-10-02 22:17:42 -0700

    Update mitsubishi.h
    updated fan, vertical and horizontal settings and made vertical and horizontal optional

--- esphome/components/mitsubishi/mitsubishi.h
+++ esphome/components/mitsubishi/mitsubishi.h
@@ -10,7 +10,7 @@ const uint8_t MITSUBISHI_TEMP_MIN = 16;  // Celsius
 const uint8_t MITSUBISHI_TEMP_MAX = 31;  // Celsius
 
 // Fan speeds
-enum SetFanSpeed {
+enum Setfanspeed {
   MITSUBISHI_FAN_1 = 0x01,
   MITSUBISHI_FAN_2 = 0x02,
   MITSUBISHI_FAN_3 = 0x03,
@@ -32,26 +32,28 @@ enum HorizontalDirection {
 enum VerticalDirection {
   VERTICAL_DIRECTION_AUTO = 0x00,
   VERTICAL_DIRECTION_UP = 0x08,
-  VERTICAL_DIRECTION_MUP = 0x10,
+  VERTICAL_DIRECTION_MIDDLE_UP = 0x10,
   VERTICAL_DIRECTION_MIDDLE = 0x18,
-  VERTICAL_DIRECTION_MDOWN = 0x20,
+  VERTICAL_DIRECTION_MIDDLE_DOWN = 0x20,
   VERTICAL_DIRECTION_DOWN = 0x28,
 };
 
 class MitsubishiClimate : public climate_ir::ClimateIR {
  public:
   MitsubishiClimate()
-      : climate_ir::ClimateIR(MITSUBISHI_TEMP_MIN, MITSUBISHI_TEMP_MAX, 1.0f, true, false,
-                              {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
-                               climate::CLIMATE_FAN_HIGH},
-                              {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_BOTH, climate::CLIMATE_SWING_VERTICAL,
-                               climate::CLIMATE_SWING_HORIZONTAL}) {}
+      : climate_ir::ClimateIR(
+        MITSUBISHI_TEMP_MIN, MITSUBISHI_TEMP_MAX, 1.0f, true, false,{
+          climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, 
+          climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH
+        },
+        {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_BOTH, 
+          climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL
+        }
+      ) {}
 
-  void set_fan_low(SetFanSpeed fan_low) { this->fan_low_ = fan_low; }
-
-  void set_fan_medium(SetFanSpeed fan_medium) { this->fan_medium_ = fan_medium; }
-
-  void set_fan_hi(SetFanSpeed fan_hi) { this->fan_hi_ = fan_hi; }
+  void set_fan_low(Setfanspeed low) { this->fan_low_ = low; }
+  void set_fan_medium(Setfanspeed medium) { this->fan_medium_ = medium; }
+  void set_fan_hi(Setfanspeed high) { this->fan_hi_ = high; }
 
   void set_horizontal_default(HorizontalDirection horizontal_direction) {
     this->default_horizontal_direction_ = horizontal_direction;
@@ -67,12 +69,11 @@ class MitsubishiClimate : public climate_ir::ClimateIR {
   bool on_receive(remote_base::RemoteReceiveData data) override;
   bool parse_state_frame_(const uint8_t frame[]);
 
-  // SetFanSpeed
-  SetFanSpeed fan_low_;
-  SetFanSpeed fan_medium_;
-  SetFanSpeed fan_hi_;
+//  Setfanspeeds setfanspeeds_;
+  Setfanspeed fan_low_;
+  Setfanspeed fan_medium_;
+  Setfanspeed fan_hi_;
 
-  // Horizontal and Vertical
   HorizontalDirection default_horizontal_direction_;
   VerticalDirection default_vertical_direction_;
 };
