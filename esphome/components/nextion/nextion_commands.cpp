04b0429ea84ac2297aacea37ff266262e8e196e7
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2023-10-05 11:02:46 +0200

    Fix indentation for clang-format test

--- esphome/components/nextion/nextion_commands.cpp
+++ esphome/components/nextion/nextion_commands.cpp
@@ -40,7 +40,7 @@ void Nextion::sleep(bool sleep) {
 void Nextion::set_protocol_reparse_mode(bool active_mode) {
   if (active_mode) {  // Sets active protocol reparse mode
     this->send_command_("recmod=1");
-    } else {                                          // Sets passive protocol reparse mode
+  } else {                                            // Sets passive protocol reparse mode
     this->send_command_("DRAKJHSUYDGBNCJHGJKSHBDN");  // To exit active reparse mode this sequence must be sent
     this->send_command_("recmod=0");                  // Sending recmode=0 twice is recommended
     this->send_command_("recmod=0");
