e55b17c3111616fba61ad7d9d61dccbbc3155880
angelnu <git@angelnu.com>
2023-10-09 09:22:57 +0000

    move register operations to cpp

--- esphome/components/ade7953_i2c/ade7953_i2c.cpp
+++ esphome/components/ade7953_i2c/ade7953_i2c.cpp
@@ -11,6 +11,75 @@ void AdE7953I2c::dump_config() {
   LOG_I2C_DEVICE(this);
   ade7953_base::ADE7953::dump_config();
 }
+bool AdE7953I2c::ade_write_8(uint16_t reg, uint8_t value) {
+  std::vector<uint8_t> data;
+  data.push_back(reg >> 8);
+  data.push_back(reg >> 0);
+  data.push_back(value);
+  return write(data.data(), data.size()) != i2c::ERROR_OK;
+}
+bool AdE7953I2c::ade_write_16(uint16_t reg, uint16_t value) {
+  std::vector<uint8_t> data;
+  data.push_back(reg >> 8);
+  data.push_back(reg >> 0);
+  data.push_back(value >> 8);
+  data.push_back(value >> 0);
+  return write(data.data(), data.size()) != i2c::ERROR_OK;
+}
+bool AdE7953I2c::ade_write_32(uint16_t reg, uint32_t value) {
+  std::vector<uint8_t> data;
+  data.push_back(reg >> 8);
+  data.push_back(reg >> 0);
+  data.push_back(value >> 24);
+  data.push_back(value >> 16);
+  data.push_back(value >> 8);
+  data.push_back(value >> 0);
+  return write(data.data(), data.size()) != i2c::ERROR_OK;
+}
+bool AdE7953I2c::ade_read_8(uint16_t reg, uint8_t *value) {
+  uint8_t reg_data[2];
+  reg_data[0] = reg >> 8;
+  reg_data[1] = reg >> 0;
+  i2c::ErrorCode err = write(reg_data, 2);
+  if (err != i2c::ERROR_OK)
+    return true;
+  err = read(value, 1);
+  return (err != i2c::ERROR_OK);
+}
+bool AdE7953I2c::ade_read_16(uint16_t reg, uint16_t *value) {
+  uint8_t reg_data[2];
+  reg_data[0] = reg >> 8;
+  reg_data[1] = reg >> 0;
+  i2c::ErrorCode err = write(reg_data, 2);
+  if (err != i2c::ERROR_OK)
+    return true;
+  uint8_t recv[2];
+  err = read(recv, 2);
+  if (err != i2c::ERROR_OK)
+    return true;
+  *value = 0;
+  *value |= ((uint32_t) recv[0]) << 8;
+  *value |= ((uint32_t) recv[1]);
+  return false;
+}
+bool AdE7953I2c::ade_read_32(uint16_t reg, uint32_t *value) {
+  uint8_t reg_data[2];
+  reg_data[0] = reg >> 8;
+  reg_data[1] = reg >> 0;
+  i2c::ErrorCode err = write(reg_data, 2);
+  if (err != i2c::ERROR_OK)
+    return true;
+  uint8_t recv[4];
+  err = read(recv, 4);
+  if (err != i2c::ERROR_OK)
+    return true;
+  *value = 0;
+  *value |= ((uint32_t) recv[0]) << 24;
+  *value |= ((uint32_t) recv[1]) << 16;
+  *value |= ((uint32_t) recv[2]) << 8;
+  *value |= ((uint32_t) recv[3]);
+  return false;
+}
 
 }  // namespace ade7953_i2c
 }  // namespace esphome
--- esphome/components/ade7953_spi/ade7953_spi.h
+++ esphome/components/ade7953_spi/ade7953_spi.h
@@ -23,61 +23,12 @@ class AdE7953Spi : public ade7953_base::ADE7953,
   void dump_config() override;
 
  protected:
-  bool ade_write_8(uint16_t reg, uint8_t value) override {
-    this->enable();
-    this->write_byte16(reg);
-    this->transfer_byte(0);
-    this->transfer_byte(value);
-    this->disable();
-    return false;
-  }
-  bool ade_write_16(uint16_t reg, uint16_t value) override {
-    this->enable();
-    this->write_byte16(reg);
-    this->transfer_byte(0);
-    this->write_byte16(value);
-    this->disable();
-    return false;
-  }
-  bool ade_write_32(uint16_t reg, uint32_t value) override {
-    this->enable();
-    this->write_byte16(reg);
-    this->transfer_byte(0);
-    this->write_byte16(value >> 16);
-    this->write_byte16(value & 0xFFFF);
-    this->disable();
-    return false;
-  }
-  bool ade_read_8(uint16_t reg, uint8_t *value) override {
-    this->enable();
-    this->write_byte16(reg);
-    this->transfer_byte(0x80);
-    *value = this->read_byte();
-    this->disable();
-    return false;
-  }
-  bool ade_read_16(uint16_t reg, uint16_t *value) override {
-    this->enable();
-    this->write_byte16(reg);
-    this->transfer_byte(0x80);
-    *value = 0;
-    *value |= this->read_byte() << 8;
-    *value |= this->read_byte();
-    this->disable();
-    return false;
-  }
-  bool ade_read_32(uint16_t reg, uint32_t *value) override {
-    this->enable();
-    this->write_byte16(reg);
-    this->transfer_byte(0x80);
-    *value = 0;
-    *value |= this->read_byte() << 24;
-    *value |= this->read_byte() << 16;
-    *value |= this->read_byte() << 8;
-    *value |= this->read_byte();
-    this->disable();
-    return false;
-  }
+  bool ade_write_8(uint16_t reg, uint8_t value) override;
+  bool ade_write_16(uint16_t reg, uint16_t value) override;
+  bool ade_write_32(uint16_t reg, uint32_t value) override;
+  bool ade_read_8(uint16_t reg, uint8_t *value) override;
+  bool ade_read_16(uint16_t reg, uint16_t *value) override;
+  bool ade_read_32(uint16_t reg, uint32_t *value) override;
 };
 
 }  // namespace ade7953_spi
