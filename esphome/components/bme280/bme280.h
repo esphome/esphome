9f3bc0f7af4461ddbecaa7bf707f027aff6b720c
andreibodrov <ap.bodrov@gmail.com>
2023-10-16 06:00:51 +0300

    typos

--- esphome/components/bme280/bme280.h
+++ esphome/components/bme280/bme280.h
@@ -1,10 +1,12 @@
+#pragma once
 
 #include "esphome/components/bme280_base/bme280_base.h"
 #include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace bme280 {
-#pragma once
+
+static const char *const TAG = "bme280.sensor";
 
 class BME280I2CComponent : public esphome::bme280_base::BME280Component, public i2c::I2CDevice {
   bool read_byte(uint8_t a_register, uint8_t *data) override;
