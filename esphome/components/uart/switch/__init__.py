ae0d8b61f57f4569d1b9fdafce7448401be72097
Samuel Sieb <samuel@sieb.net>
2023-10-17 01:16:09 -0700

    change config

--- esphome/components/uart/switch/__init__.py
+++ esphome/components/uart/switch/__init__.py
@@ -9,34 +9,27 @@ DEPENDENCIES = ["uart"]
 
 UARTSwitch = uart_ns.class_("UARTSwitch", switch.Switch, uart.UARTDevice, cg.Component)
 
-CONF_DATA_ON = "data_on"
-CONF_DATA_OFF = "data_off"
+CONF_TURN_OFF = "turn_off"
+CONF_TURN_ON = "turn_on"
 
-
-def validate_data(config):
-    if (CONF_DATA_ON in config or CONF_DATA_OFF in config) and (
-        CONF_DATA in config or CONF_SEND_EVERY in config
-    ):
-        raise cv.Invalid(
-            '"data" or "send_every" can\'t be used with "data_on" or "data_off"'
-        )
-    return config
-
-
-CONFIG_SCHEMA = cv.All(
+CONFIG_SCHEMA = (
     switch.switch_schema(UARTSwitch, block_inverted=True)
     .extend(
         {
-            cv.Optional(CONF_DATA): validate_raw_data,
-            cv.Optional(CONF_DATA_OFF): validate_raw_data,
-            cv.Optional(CONF_DATA_ON): validate_raw_data,
+            cv.Required(CONF_DATA): cv.Any(
+                validate_raw_data,
+                cv.Schema(
+                    {
+                        cv.Optional(CONF_TURN_OFF): validate_raw_data,
+                        cv.Optional(CONF_TURN_ON): validate_raw_data,
+                    }
+                ),
+            ),
             cv.Optional(CONF_SEND_EVERY): cv.positive_time_period_milliseconds,
         },
     )
     .extend(uart.UART_DEVICE_SCHEMA)
-    .extend(cv.COMPONENT_SCHEMA),
-    cv.has_at_least_one_key(CONF_DATA, CONF_DATA_ON, CONF_DATA_OFF),
-    validate_data,
+    .extend(cv.COMPONENT_SCHEMA)
 )
 
 
@@ -45,20 +38,21 @@ async def to_code(config):
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    if data := config.get(CONF_DATA):
-        if isinstance(data, bytes):
-            data = [HexInt(x) for x in data]
-        cg.add(var.set_data(data))
-
-    if data := config.get(CONF_DATA_ON):
+    data = config[CONF_DATA]
+    if isinstance(data, dict):
+        if data_on := data.get(CONF_TURN_ON):
+            if isinstance(data_on, bytes):
+                data_on = [HexInt(x) for x in data_on]
+            cg.add(var.set_data_on(data_on))
+        if data_off := data.get(CONF_TURN_OFF):
+            if isinstance(data_off, bytes):
+                data_off = [HexInt(x) for x in data_off]
+            cg.add(var.set_data_off(data_off))
+    else:
+        data = config[CONF_DATA]
         if isinstance(data, bytes):
             data = [HexInt(x) for x in data]
         cg.add(var.set_data_on(data))
-
-    if data := config.get(CONF_DATA_OFF):
-        if isinstance(data, bytes):
-            data = [HexInt(x) for x in data]
-        cg.add(var.set_data_off(data))
-
+        cg.add(var.set_single_state(True))
     if CONF_SEND_EVERY in config:
         cg.add(var.set_send_every(config[CONF_SEND_EVERY]))
--- esphome/components/uart/switch/uart_switch.cpp
+++ esphome/components/uart/switch/uart_switch.cpp
@@ -7,18 +7,13 @@ namespace uart {
 static const char *const TAG = "uart.switch";
 
 void UARTSwitch::loop() {
-  if (this->state && this->send_every_) {
+  if (this->send_every_) {
     const uint32_t now = millis();
     if (now - this->last_transmission_ > this->send_every_) {
-      this->write_command_();
+      this->write_command_(this->state);
       this->last_transmission_ = now;
     }
   }
-}
-
-void UARTSwitch::write_command_() {
-  ESP_LOGD(TAG, "'%s': Sending data...", this->get_name().c_str());
-  this->write_array(this->data_.data(), this->data_.size());
 }
 
 void UARTSwitch::write_command_(bool state) {
@@ -36,6 +31,7 @@ void UARTSwitch::write_state(bool state) {
   if (!this->single_state_) {
     this->publish_state(state);
     this->write_command_(state);
+    this->last_transmission_ = millis();
     return;
   }
 
@@ -45,7 +41,7 @@ void UARTSwitch::write_state(bool state) {
   }
 
   this->publish_state(true);
-  this->write_command_();
+  this->write_command_(true);
 
   if (this->send_every_ == 0) {
     this->publish_state(false);
