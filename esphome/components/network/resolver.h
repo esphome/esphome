b858a01fd0ae9fd035f810e7678dad75839503d1
Jimmy Hedman <jimmy.hedman@gmail.com>
2023-10-19 20:57:25 +0200

    Resolves mdns and regular dns

--- esphome/components/network/resolver.h
+++ esphome/components/network/resolver.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include "ip_address.h"
+#include "esphome/components/mdns/mdns_component.h"
 namespace esphome {
 namespace network {
 
@@ -14,7 +15,11 @@ class Resolver {
   network::IPAddress resolve(std::string);
 
 protected:
+  static void dns_found_callback(const char *, const ip_addr_t *, void *);
   std::map<std::string, network::IPAddress> hosts_;
+  network::IPAddress ip_;
+  bool dns_resolved_{false};
+  bool dns_resolve_error_{false};
 };
 
 extern Resolver *global_resolver;
