aca563336bb1059a8f55da408f2cd95df3510a0d
Jimmy Hedman <jimmy.hedman@gmail.com>
2023-10-22 22:56:59 +0200

    Make linter happier

--- esphome/components/network/ip_address.h
+++ esphome/components/network/ip_address.h
@@ -34,7 +34,10 @@ struct IPAddress {
   }
   IPAddress(const ip_addr_t *other_ip) { ip_addr_copy(ip_addr_, *other_ip); }
   IPAddress(const std::string &in_address) { ipaddr_aton(in_address.c_str(), &ip_addr_); }
-  IPAddress(ip4_addr_t *other_ip) { memcpy((void *) &ip_addr_, (void *) other_ip, sizeof(ip4_addr_t)); ip_addr_.type = IPADDR_TYPE_V4; }
+  IPAddress(ip4_addr_t *other_ip) {
+    memcpy((void *) &ip_addr_, (void *) other_ip, sizeof(ip4_addr_t));
+    ip_addr_.type = IPADDR_TYPE_V4;
+  }
 #if USE_ARDUINO
   IPAddress(const arduino_ns::IPAddress &other_ip) { ip_addr_set_ip4_u32(&ip_addr_, other_ip); }
 #endif
