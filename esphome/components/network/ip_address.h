b4589254a4391a226723b9be4a7abfb7cc7e3910
Keith Burzinski <kbx81x@gmail.com>
2023-10-23 18:43:54 -0500

    Set IP address `type` only when IPv4 and IPv6 are both enabled (#5595)

--- esphome/components/network/ip_address.h
+++ esphome/components/network/ip_address.h
@@ -36,7 +36,7 @@ struct IPAddress {
   IPAddress(const std::string &in_address) { ipaddr_aton(in_address.c_str(), &ip_addr_); }
   IPAddress(ip4_addr_t *other_ip) {
     memcpy((void *) &ip_addr_, (void *) other_ip, sizeof(ip4_addr_t));
-#if USE_ESP32
+#if USE_ESP32 && LWIP_IPV6
     ip_addr_.type = IPADDR_TYPE_V4;
 #endif
   }
