d65c707634b1c065211903a3bd59b2aa92d9fe68
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-10-26 15:59:03 +1300

    fix variable name

--- esphome/components/dfrobot_sen0395/switch/dfrobot_sen0395_switch.cpp
+++ esphome/components/dfrobot_sen0395/switch/dfrobot_sen0395_switch.cpp
@@ -6,40 +6,40 @@ namespace dfrobot_sen0395 {
 void Sen0395PowerSwitch::write_state(bool state) { this->parent_->enqueue(make_unique<PowerCommand>(state)); }
 
 void Sen0395LedSwitch::write_state(bool state) {
-  bool wasActive = false;
+  bool was_active = false;
   if (this->parent_->is_active()) {
-    wasActive = true;
+    was_active = true;
     this->parent_->enqueue(make_unique<PowerCommand>(false));
   }
   this->parent_->enqueue(make_unique<LedModeCommand>(state));
   this->parent_->enqueue(make_unique<SaveCfgCommand>());
-  if (wasActive) {
+  if (was_active) {
     this->parent_->enqueue(make_unique<PowerCommand>(true));
   }
 }
 
 void Sen0395UartPresenceSwitch::write_state(bool state) {
-  bool wasActive = false;
+  bool was_active = false;
   if (this->parent_->is_active()) {
-    wasActive = true;
+    was_active = true;
     this->parent_->enqueue(make_unique<PowerCommand>(false));
   }
   this->parent_->enqueue(make_unique<UartOutputCommand>(state));
   this->parent_->enqueue(make_unique<SaveCfgCommand>());
-  if (wasActive) {
+  if (was_active) {
     this->parent_->enqueue(make_unique<PowerCommand>(true));
   }
 }
 
 void Sen0395StartAfterBootSwitch::write_state(bool state) {
-  bool wasActive = false;
+  bool was_active = false;
   if (this->parent_->is_active()) {
-    wasActive = true;
+    was_active = true;
     this->parent_->enqueue(make_unique<PowerCommand>(false));
   }
   this->parent_->enqueue(make_unique<SensorCfgStartCommand>(state));
   this->parent_->enqueue(make_unique<SaveCfgCommand>());
-  if (wasActive) {
+  if (was_active) {
     this->parent_->enqueue(make_unique<PowerCommand>(true));
   }
 }
