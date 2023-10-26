12ab7b6d16f137f3322ab5e2e273537a6e632458
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-10-26 15:15:06 +1300

    Rename component to dfrobot_sen0395 Organise switch code into sub-folder and make one class per switch type. Move commands and automation code into own files Fix consts

--- esphome/components/dfrobot_sen0395/__init__.py
+++ esphome/components/dfrobot_sen0395/__init__.py
@@ -12,38 +12,38 @@ CODEOWNERS = ["@niklasweber"]
 DEPENDENCIES = ["uart"]
 MULTI_CONF = True
 
-dfrobot_mmwave_radar_ns = cg.esphome_ns.namespace("dfrobot_mmwave_radar")
-DfrobotMmwaveRadarComponent = dfrobot_mmwave_radar_ns.class_(
-    "DfrobotMmwaveRadarComponent", cg.Component
+dfrobot_sen0395_ns = cg.esphome_ns.namespace("dfrobot_sen0395")
+DfrobotSen0395Component = dfrobot_sen0395_ns.class_(
+    "DfrobotSen0395Component", cg.Component
 )
 
 # Actions
-DfrobotMmwaveRadarPowerAction = dfrobot_mmwave_radar_ns.class_(
-    "DfrobotMmwaveRadarPowerAction", automation.Action
+DfrobotSen0395PowerAction = dfrobot_sen0395_ns.class_(
+    "DfrobotSen0395PowerAction", automation.Action
 )
-DfrobotMmwaveRadarResetAction = dfrobot_mmwave_radar_ns.class_(
-    "DfrobotMmwaveRadarResetAction", automation.Action
+DfrobotSen0395ResetAction = dfrobot_sen0395_ns.class_(
+    "DfrobotSen0395ResetAction", automation.Action
 )
-DfrobotMmwaveRadarSettingsAction = dfrobot_mmwave_radar_ns.class_(
-    "DfrobotMmwaveRadarSettingsAction", automation.Action
+DfrobotSen0395SettingsAction = dfrobot_sen0395_ns.class_(
+    "DfrobotSen0395SettingsAction", automation.Action
 )
 
-DFROBOT_MMWAVE_RADAR_ID = "dfrobot_mmwave_radar_id"
+CONF_DFROBOT_SEN0395_ID = "dfrobot_sen0395_id"
 
-DELAY_AFTER_DETECT = "delay_after_detect"
-DELAY_AFTER_DISAPPEAR = "delay_after_disappear"
-DETECTION_SEGMENTS = "detection_segments"
-OUTPUT_LATENCY = "output_latency"
-START_AFTER_POWER_ON = "start_after_power_on"
-TURN_ON_LED = "turn_on_led"
-FACTORY_RESET = "factory_reset"
-PRESENCE_VIA_UART = "presence_via_uart"
-SENSITIVITY = "sensitivity"
+CONF_DELAY_AFTER_DETECT = "delay_after_detect"
+CONF_DELAY_AFTER_DISAPPEAR = "delay_after_disappear"
+CONF_DETECTION_SEGMENTS = "detection_segments"
+CONF_OUTPUT_LATENCY = "output_latency"
+CONF_START_AFTER_POWER_ON = "start_after_power_on"
+CONF_TURN_ON_LED = "turn_on_led"
+CONF_FACTORY_RESET = "factory_reset"
+CONF_PRESENCE_VIA_UART = "presence_via_uart"
+CONF_SENSITIVITY = "sensitivity"
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
-            cv.GenerateID(): cv.declare_id(DfrobotMmwaveRadarComponent),
+            cv.GenerateID(): cv.declare_id(DfrobotSen0395Component),
         }
     ).extend(uart.UART_DEVICE_SCHEMA)
 )
@@ -56,47 +56,15 @@ async def to_code(config):
 
 
 @automation.register_action(
-    "dfrobot_mmwave_radar.start",
-    DfrobotMmwaveRadarPowerAction,
+    "dfrobot_sen0395.reset",
+    DfrobotSen0395ResetAction,
     maybe_simple_id(
         {
-            cv.GenerateID(): cv.use_id(DfrobotMmwaveRadarComponent),
+            cv.GenerateID(): cv.use_id(DfrobotSen0395Component),
         }
     ),
 )
-async def dfrobot_mmwave_radar_start_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
-    cg.add(var.set_power(True))
-    return var
-
-
-@automation.register_action(
-    "dfrobot_mmwave_radar.stop",
-    DfrobotMmwaveRadarPowerAction,
-    maybe_simple_id(
-        {
-            cv.GenerateID(): cv.use_id(DfrobotMmwaveRadarComponent),
-        }
-    ),
-)
-async def dfrobot_mmwave_radar_stop_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
-    cg.add(var.set_power(False))
-    return var
-
-
-@automation.register_action(
-    "dfrobot_mmwave_radar.reset",
-    DfrobotMmwaveRadarResetAction,
-    maybe_simple_id(
-        {
-            cv.GenerateID(): cv.use_id(DfrobotMmwaveRadarComponent),
-        }
-    ),
-)
-async def dfrobot_mmwave_radar_reset_to_code(config, action_id, template_arg, args):
+async def dfrobot_sen0395_reset_to_code(config, action_id, template_arg, args):
     parent = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, parent)
 
@@ -160,75 +128,54 @@ def range_segment_list(input):
     return input
 
 
-def at_least_one_settings_option(config):
-    """Make sure at least one option is defined
-
-    All settings are optional, but at least one setting needs to be specified.
-    Otherwise there is nothing to to and the action would be useless.
-    """
-    has_at_least_one_option = False
-
-    if DETECTION_SEGMENTS in config:
-        has_at_least_one_option = True
-    if OUTPUT_LATENCY in config:
-        has_at_least_one_option = True
-    if START_AFTER_POWER_ON in config:
-        has_at_least_one_option = True
-    if TURN_ON_LED in config:
-        has_at_least_one_option = True
-    if FACTORY_RESET in config:
-        has_at_least_one_option = True
-    if PRESENCE_VIA_UART in config:
-        has_at_least_one_option = True
-    if SENSITIVITY in config:
-        has_at_least_one_option = True
-
-    if not has_at_least_one_option:
-        raise cv.Invalid("At least one settings option is required")
-    return config
-
-
 MMWAVE_SETTINGS_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.use_id(DfrobotMmwaveRadarComponent),
-        cv.Optional(FACTORY_RESET): cv.templatable(cv.boolean),
-        cv.Optional(DETECTION_SEGMENTS): range_segment_list,
-        cv.Optional(OUTPUT_LATENCY): {
-            cv.Required(DELAY_AFTER_DETECT): cv.templatable(
+        cv.GenerateID(): cv.use_id(DfrobotSen0395Component),
+        cv.Optional(CONF_FACTORY_RESET): cv.templatable(cv.boolean),
+        cv.Optional(CONF_DETECTION_SEGMENTS): range_segment_list,
+        cv.Optional(CONF_OUTPUT_LATENCY): {
+            cv.Required(CONF_DELAY_AFTER_DETECT): cv.templatable(
                 cv.All(
                     cv.positive_time_period,
                     cv.Range(max=core.TimePeriod(seconds=1638.375)),
                 )
             ),
-            cv.Required(DELAY_AFTER_DISAPPEAR): cv.templatable(
+            cv.Required(CONF_DELAY_AFTER_DISAPPEAR): cv.templatable(
                 cv.All(
                     cv.positive_time_period,
                     cv.Range(max=core.TimePeriod(seconds=1638.375)),
                 )
             ),
         },
-        cv.Optional(START_AFTER_POWER_ON): cv.templatable(cv.boolean),
-        cv.Optional(TURN_ON_LED): cv.templatable(cv.boolean),
-        cv.Optional(PRESENCE_VIA_UART): cv.templatable(cv.boolean),
-        cv.Optional(SENSITIVITY): cv.templatable(cv.int_range(min=0, max=9)),
+        cv.Optional(CONF_SENSITIVITY): cv.templatable(cv.int_range(min=0, max=9)),
     }
-).add_extra(at_least_one_settings_option)
+).add_extra(
+    cv.has_at_least_one_key(
+        [
+            CONF_FACTORY_RESET,
+            CONF_DETECTION_SEGMENTS,
+            CONF_OUTPUT_LATENCY,
+            CONF_SENSITIVITY,
+        ]
+    )
+)
 
 
 @automation.register_action(
-    "dfrobot_mmwave_radar.settings",
-    DfrobotMmwaveRadarSettingsAction,
+    "dfrobot_sen0395.settings",
+    DfrobotSen0395SettingsAction,
     MMWAVE_SETTINGS_SCHEMA,
 )
-async def dfrobot_mmwave_radar_settings_to_code(config, action_id, template_arg, args):
+async def dfrobot_sen0395_settings_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
 
-    if FACTORY_RESET in config:
-        template_ = await cg.templatable(config[FACTORY_RESET], args, int)
+    if factory_reset_config := config.get(CONF_FACTORY_RESET):
+        template_ = await cg.templatable(factory_reset_config, args, int)
         cg.add(var.set_factory_reset(template_))
-    if DETECTION_SEGMENTS in config:
-        segments = config[DETECTION_SEGMENTS]
+
+    if CONF_DETECTION_SEGMENTS in config:
+        segments = config[CONF_DETECTION_SEGMENTS]
 
         if len(segments) >= 2:
             template_ = await cg.templatable(segments[0], args, float)
@@ -250,31 +197,22 @@ async def dfrobot_mmwave_radar_settings_to_code(config, action_id, template_arg,
             cg.add(var.set_det_min4(template_))
             template_ = await cg.templatable(segments[7], args, float)
             cg.add(var.set_det_max4(template_))
-    if OUTPUT_LATENCY in config:
+    if CONF_OUTPUT_LATENCY in config:
         template_ = await cg.templatable(
-            config[OUTPUT_LATENCY][DELAY_AFTER_DETECT], args, float
+            config[CONF_OUTPUT_LATENCY][CONF_DELAY_AFTER_DETECT], args, float
         )
         if isinstance(template_, cv.TimePeriod):
             template_ = template_.total_milliseconds / 1000
         cg.add(var.set_delay_after_detect(template_))
 
         template_ = await cg.templatable(
-            config[OUTPUT_LATENCY][DELAY_AFTER_DISAPPEAR], args, float
+            config[CONF_OUTPUT_LATENCY][CONF_DELAY_AFTER_DISAPPEAR], args, float
         )
         if isinstance(template_, cv.TimePeriod):
             template_ = template_.total_milliseconds / 1000
         cg.add(var.set_delay_after_disappear(template_))
-    if START_AFTER_POWER_ON in config:
-        template_ = await cg.templatable(config[START_AFTER_POWER_ON], args, int)
-        cg.add(var.set_start_after_power_on(template_))
-    if TURN_ON_LED in config:
-        template_ = await cg.templatable(config[TURN_ON_LED], args, int)
-        cg.add(var.set_turn_on_led(template_))
-    if PRESENCE_VIA_UART in config:
-        template_ = await cg.templatable(config[PRESENCE_VIA_UART], args, int)
-        cg.add(var.set_presence_via_uart(template_))
-    if SENSITIVITY in config:
-        template_ = await cg.templatable(config[SENSITIVITY], args, int)
+    if CONF_SENSITIVITY in config:
+        template_ = await cg.templatable(config[CONF_SENSITIVITY], args, int)
         cg.add(var.set_sensitivity(template_))
 
     return var
--- esphome/components/dfrobot_sen0395/binary_sensor.py
+++ esphome/components/dfrobot_sen0395/binary_sensor.py
@@ -2,21 +2,21 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import DEVICE_CLASS_MOTION
-from . import DFROBOT_MMWAVE_RADAR_ID, DfrobotMmwaveRadarComponent
+from . import CONF_DFROBOT_SEN0395_ID, DfrobotSen0395Component
 
-DEPENDENCIES = ["dfrobot_mmwave_radar"]
+DEPENDENCIES = ["dfrobot_sen0395"]
 
 CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(
     device_class=DEVICE_CLASS_MOTION
 ).extend(
     {
-        cv.GenerateID(DFROBOT_MMWAVE_RADAR_ID): cv.use_id(DfrobotMmwaveRadarComponent),
+        cv.GenerateID(CONF_DFROBOT_SEN0395_ID): cv.use_id(DfrobotSen0395Component),
     }
 )
 
 
 async def to_code(config):
-    dfr_mmwave_component = await cg.get_variable(config[DFROBOT_MMWAVE_RADAR_ID])
+    parent = await cg.get_variable(config[CONF_DFROBOT_SEN0395_ID])
     binary_sens = await binary_sensor.new_binary_sensor(config)
 
-    cg.add(dfr_mmwave_component.set_detected_binary_sensor(binary_sens))
+    cg.add(parent.set_detected_binary_sensor(binary_sens))
