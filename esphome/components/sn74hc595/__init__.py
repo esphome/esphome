aaf784f31930a40fb298c238362e72bfd4080588
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-10-27 14:02:22 +1300

    Split the class implementations for GPIO vs SPI

--- esphome/components/sn74hc595/__init__.py
+++ esphome/components/sn74hc595/__init__.py
@@ -12,6 +12,7 @@ from esphome.const import (
     CONF_CLOCK_PIN,
     CONF_OUTPUT,
 )
+from esphome.core import EsphomeError
 
 DEPENDENCIES = []
 AUTO_LOAD = []
@@ -19,9 +20,14 @@ MULTI_CONF = True
 
 sn74hc595_ns = cg.esphome_ns.namespace("sn74hc595")
 
-SN74HC595Component = sn74hc595_ns.class_(
-    "SN74HC595Component", cg.Component, spi.SPIDevice
+SN74HC595Component = sn74hc595_ns.class_("SN74HC595Component", cg.Component)
+SN74HC595GPIOComponent = sn74hc595_ns.class_(
+    "SN74HC595GPIOComponent", SN74HC595Component
 )
+SN74HC595SPIComponent = sn74hc595_ns.class_(
+    "SN74HC595SPIComponent", SN74HC595Component, spi.SPIDevice
+)
+
 SN74HC595GPIOPin = sn74hc595_ns.class_(
     "SN74HC595GPIOPin", cg.GPIOPin, cg.Parented.template(SN74HC595Component)
 )
@@ -32,48 +38,49 @@ CONF_OE_PIN = "oe_pin"
 CONF_SR_COUNT = "sr_count"
 
 
-def _validate_config(config):
-    if CONF_DATA_PIN in config or CONF_CLOCK_PIN in config:
-        # Not using SPI
-        del config[CONF_SPI_ID]
-
-        if CONF_DATA_PIN not in config:
-            raise cv.Invalid(f"{CONF_DATA_PIN} missing but {CONF_CLOCK_PIN} specified")
-
-        if CONF_CLOCK_PIN not in config:
-            raise cv.Invalid(f"{CONF_CLOCK_PIN} missing but {CONF_DATA_PIN} specified")
-
-    return config
-
-
-CONFIG_SCHEMA = cv.All(
+CONFIG_SCHEMA = cv.Any(
     cv.Schema(
         {
-            cv.Required(CONF_ID): cv.declare_id(SN74HC595Component),
-            cv.Optional(CONF_DATA_PIN): pins.gpio_output_pin_schema,
-            cv.Optional(CONF_CLOCK_PIN): pins.gpio_output_pin_schema,
+            cv.Required(CONF_ID): cv.declare_id(SN74HC595GPIOComponent),
+            cv.Required(CONF_DATA_PIN): pins.gpio_output_pin_schema,
+            cv.Required(CONF_CLOCK_PIN): pins.gpio_output_pin_schema,
+            cv.Required(CONF_LATCH_PIN): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_OE_PIN): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_SR_COUNT, default=1): cv.int_range(min=1, max=256),
+        }
+    ).extend(cv.COMPONENT_SCHEMA),
+    cv.Schema(
+        {
+            cv.Required(CONF_ID): cv.declare_id(SN74HC595SPIComponent),
             cv.Required(CONF_LATCH_PIN): pins.gpio_output_pin_schema,
             cv.Optional(CONF_OE_PIN): pins.gpio_output_pin_schema,
             cv.Optional(CONF_SR_COUNT, default=1): cv.int_range(min=1, max=256),
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
-    .extend(spi.spi_device_schema(cs_pin_required=False)),
-    _validate_config,
+    .extend(spi.spi_device_schema(cs_pin_required=False))
+    .extend(
+        {
+            cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
+        }
+    ),
 )
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    if CONF_DATA_PIN in config and CONF_CLOCK_PIN in config:
+    if CONF_DATA_PIN in config:
         data_pin = await cg.gpio_pin_expression(config[CONF_DATA_PIN])
         cg.add(var.set_data_pin(data_pin))
         clock_pin = await cg.gpio_pin_expression(config[CONF_CLOCK_PIN])
         cg.add(var.set_clock_pin(clock_pin))
-    else:
+    elif CONF_SPI_ID in config:
         await spi.register_spi_device(var, config)
         cg.add(var.use_spi())
+    else:
+        raise EsphomeError("Not supported")
+
     latch_pin = await cg.gpio_pin_expression(config[CONF_LATCH_PIN])
     cg.add(var.set_latch_pin(latch_pin))
     if CONF_OE_PIN in config:
--- esphome/components/sn74hc595/sn74hc595.cpp
+++ esphome/components/sn74hc595/sn74hc595.cpp
@@ -6,31 +6,38 @@ namespace sn74hc595 {
 
 static const char *const TAG = "sn74hc595";
 
-void SN74HC595Component::setup() {
+void SN74HC595Component::pre_setup_() {
   ESP_LOGCONFIG(TAG, "Setting up SN74HC595...");
 
   if (this->have_oe_pin_) {  // disable output
     this->oe_pin_->setup();
     this->oe_pin_->digital_write(true);
   }
-
-  // initialize output pins
-  if (this->use_spi_) {
-#ifdef USE_SPI
-    this->spi_setup();
-#endif
-  } else {
-    this->clock_pin_->setup();
-    this->data_pin_->setup();
-    this->clock_pin_->digital_write(false);
-    this->data_pin_->digital_write(false);
-  }
+}
+void SN74HC595Component::post_setup_() {
   this->latch_pin_->setup();
   this->latch_pin_->digital_write(false);
 
   // send state to shift register
   this->write_gpio_();
 }
+
+void SN74HC595GPIOComponent::setup() {
+  this->pre_setup_();
+  this->clock_pin_->setup();
+  this->data_pin_->setup();
+  this->clock_pin_->digital_write(false);
+  this->data_pin_->digital_write(false);
+  this->post_setup_();
+}
+
+#ifdef USE_SPI
+void SN74HC595SPIComponent::setup() {
+  this->pre_setup_();
+  this->spi_setup();
+  this->post_setup_();
+}
+#endif
 
 void SN74HC595Component::dump_config() { ESP_LOGCONFIG(TAG, "SN74HC595:"); }
 
@@ -48,24 +55,30 @@ void SN74HC595Component::digital_write_(uint16_t pin, bool value) {
   this->write_gpio_();
 }
 
-void SN74HC595Component::write_gpio_() {
+void SN74HC595GPIOComponent::write_gpio_() {
   for (auto byte = this->output_bytes_.rbegin(); byte != this->output_bytes_.rend(); byte++) {
-    if (this->use_spi_) {
-#ifdef USE_SPI
-      this->enable();
-      this->transfer_byte(*byte);
-      this->disable();
-#endif
-    } else {
-      for (int8_t i = 7; i >= 0; i--) {
-        bool bit = (*byte >> i) & 1;
-        this->data_pin_->digital_write(bit);
-        this->clock_pin_->digital_write(true);
-        this->clock_pin_->digital_write(false);
-      }
+    for (int8_t i = 7; i >= 0; i--) {
+      bool bit = (*byte >> i) & 1;
+      this->data_pin_->digital_write(bit);
+      this->clock_pin_->digital_write(true);
+      this->clock_pin_->digital_write(false);
     }
   }
+  SN74HC595Component::write_gpio_();
+}
 
+#ifdef USE_SPI
+void SN74HC595SPIComponent::write_gpio_() {
+  for (auto byte = this->output_bytes_.rbegin(); byte != this->output_bytes_.rend(); byte++) {
+    this->enable();
+    this->transfer_byte(*byte);
+    this->disable();
+  }
+  SN74HC595Component::write_gpio_();
+}
+#endif
+
+void SN74HC595Component::write_gpio_() {
   // pulse latch to activate new values
   this->latch_pin_->digital_write(true);
   this->latch_pin_->digital_write(false);
@@ -81,11 +94,7 @@ float SN74HC595Component::get_setup_priority() const { return setup_priority::IO
 void SN74HC595GPIOPin::digital_write(bool value) {
   this->parent_->digital_write_(this->pin_, value != this->inverted_);
 }
-std::string SN74HC595GPIOPin::dump_summary() const {
-  char buffer[32];
-  snprintf(buffer, sizeof(buffer), "%u via SN74HC595", pin_);
-  return buffer;
-}
+std::string SN74HC595GPIOPin::dump_summary() const { return str_snprintf("%u via SN74HC595", 18, pin_); }
 
 }  // namespace sn74hc595
 }  // namespace esphome
