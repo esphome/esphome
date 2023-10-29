934b34a83e6f9f625d69438d0bcade8b7eb1f874
Dewet Diener <dewet22@users.noreply.github.com>
2023-10-29 12:03:25 +0000

    Fix black complaint

--- esphome/components/font/__init__.py
+++ esphome/components/font/__init__.py
@@ -137,7 +137,9 @@ def validate_weight_name(value):
 
 
 def download_gfonts(value):
-    name = f"{value[CONF_FAMILY]}:ital,wght@{int(value[CONF_ITALIC])},{value[CONF_WEIGHT]}"
+    name = (
+        f"{value[CONF_FAMILY]}:ital,wght@{int(value[CONF_ITALIC])},{value[CONF_WEIGHT]}"
+    )
     url = f"https://fonts.googleapis.com/css2?family={name}"
 
     path = _compute_gfonts_local_path(value)
