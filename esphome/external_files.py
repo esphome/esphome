fbf8d53f5ca22d3ee72e44d031915be52ff32da1
landon rohatensky <landonroha@gmail.com>
2023-10-30 05:29:11 -0700

    add check etag equality

--- esphome/external_files.py
+++ esphome/external_files.py
@@ -2,9 +2,30 @@ from pathlib import Path
 import os
 from urllib.parse import urlparse, unquote
 from datetime import datetime
+import requests
 from esphome.core import CORE, TimePeriodSeconds
 
 CODEOWNERS = ["@landonr"]
+
+NETWORK_TIMEOUT = 30
+ETAG = "ETag"
+
+
+def check_etag_equality(url, local_file_path):
+    # Check if the local file exists
+    if os.path.exists(local_file_path):
+        # Send an HTTP HEAD request to the URL to get the remote file's ETag
+        response = requests.head(url, timeout=NETWORK_TIMEOUT)
+        remote_etag = response.headers.get(ETAG)
+
+        # Get the local file's ETag if available
+        with open(local_file_path, "rb") as local_file:
+            local_etag = local_file.headers.get(ETAG)
+
+        # Compare ETags
+        if remote_etag is not None and local_etag is not None:
+            return remote_etag == local_etag
+    return False
 
 
 def is_file_recent(file_path: str, refresh: TimePeriodSeconds) -> bool:
