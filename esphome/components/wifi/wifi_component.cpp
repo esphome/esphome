ad743594b56c4f368484af62a77f03a2f0ef3db0
Kevin P. Fleming <kevin@km6g.us>
2023-10-31 06:36:11 -0400

    Ensure that all uses of strncpy in wifi component are safe.

--- esphome/components/wifi/wifi_component.cpp
+++ esphome/components/wifi/wifi_component.cpp
@@ -261,8 +261,8 @@ void WiFiComponent::set_sta(const WiFiAP &ap) {
 void WiFiComponent::clear_sta() { this->sta_.clear(); }
 void WiFiComponent::save_wifi_sta(const std::string &ssid, const std::string &password) {
   SavedWifiSettings save{};
-  strncpy(save.ssid, ssid.c_str(), sizeof(save.ssid));
-  strncpy(save.password, password.c_str(), sizeof(save.password));
+  strncpy(save.ssid, ssid.c_str(), sizeof(save.ssid) - 1);
+  strncpy(save.password, password.c_str(), sizeof(save.password) - 1);
   this->pref_.save(&save);
   // ensure it's written immediately
   global_preferences->sync();
