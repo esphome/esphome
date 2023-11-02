e37fbe6b2ec6817db31297e0ceb4423a2fd2fed7
descipher <mike.laspina@shaw.ca>
2023-11-02 09:04:05 -0500

    Inverted logic on gate select

--- esphome/components/ld2420/number/gate_config_number.cpp
+++ esphome/components/ld2420/number/gate_config_number.cpp
@@ -15,22 +15,21 @@ void LD2420TimeoutNumber::control(float timeout) {
 void LD2420MinDistanceNumber::control(float min_gate) {
   if ((uint16_t) min_gate > this->parent_->new_config.max_gate) {
     min_gate = this->parent_->get_min_gate_distance_value();
-    this->publish_state(min_gate);
   } else {
     this->parent_->new_config.min_gate = (uint16_t) min_gate;
-    this->publish_state(min_gate);
   }
+  this->publish_state(min_gate);
 }
 
 void LD2420MaxDistanceNumber::control(float max_gate) {
   if ((uint16_t) max_gate < this->parent_->new_config.min_gate) {
     max_gate = this->parent_->get_max_gate_distance_value();
-    this->publish_state(max_gate);
   } else {
     this->parent_->new_config.max_gate = (uint16_t) max_gate;
-    this->publish_state(max_gate);
   }
+  this->publish_state(max_gate);
 }
+
 
 void LD2420GateSelectNumber::control(float gate_select) {
   const uint8_t gate = (uint8_t) gate_select;
@@ -64,7 +63,7 @@ LD2420StillThresholdNumbers::LD2420StillThresholdNumbers(uint8_t gate) : gate_(g
 
 void LD2420StillThresholdNumbers::control(float still_threshold) {
   this->publish_state(still_threshold);
-  if (this->parent_->is_gate_select()) {
+  if (!this->parent_->is_gate_select()) {
     this->parent_->new_config.still_thresh[this->gate_] = still_threshold;
   } else {
     this->parent_->new_config.still_thresh[this->parent_->get_gate_select_value()] = still_threshold;
