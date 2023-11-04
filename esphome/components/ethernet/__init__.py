280f4618f237dcf7b8a32ae6e7ff6dc9d3078539
Jeroen van Oort <jvanoort@simplexis.nl>
2023-11-04 19:22:22 +0100

    Added support for SPI based W5500 ethernet

--- esphome/components/ethernet/__init__.py
+++ esphome/components/ethernet/__init__.py
@@ -1,6 +1,7 @@
 from esphome import pins
 import esphome.config_validation as cv
 import esphome.codegen as cg
+from esphome.components.esp32 import add_idf_sdkconfig_option
 from esphome.const import (
     CONF_DOMAIN,
     CONF_ID,
@@ -12,6 +13,12 @@ from esphome.const import (
     CONF_SUBNET,
     CONF_DNS1,
     CONF_DNS2,
+    CONF_CLK_PIN,
+    CONF_MISO_PIN,
+    CONF_MOSI_PIN,
+    CONF_CS_PIN,
+    CONF_INTERRUPT_PIN,
+    CONF_RESET_PIN,
 )
 from esphome.core import CORE, coroutine_with_priority
 from esphome.components.network import IPAddress
@@ -27,6 +34,8 @@ CONF_MDIO_PIN = "mdio_pin"
 CONF_CLK_MODE = "clk_mode"
 CONF_POWER_PIN = "power_pin"
 
+CONF_CLOCK_SPEED = "clock_speed"
+
 EthernetType = ethernet_ns.enum("EthernetType")
 ETHERNET_TYPES = {
     "LAN8720": EthernetType.ETHERNET_TYPE_LAN8720,
@@ -36,6 +45,7 @@ ETHERNET_TYPES = {
     "JL1101": EthernetType.ETHERNET_TYPE_JL1101,
     "KSZ8081": EthernetType.ETHERNET_TYPE_KSZ8081,
     "KSZ8081RNA": EthernetType.ETHERNET_TYPE_KSZ8081RNA,
+    "W5500": EthernetType.ETHERNET_TYPE_W5500,
 }
 
 emac_rmii_clock_mode_t = cg.global_ns.enum("emac_rmii_clock_mode_t")
@@ -84,11 +94,22 @@ def _validate(config):
     return config
 
 
-CONFIG_SCHEMA = cv.All(
+BASE_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(EthernetComponent),
+        cv.Optional(CONF_MANUAL_IP): MANUAL_IP_SCHEMA,
+        cv.Optional(CONF_DOMAIN, default=".local"): cv.domain_name,
+        cv.Optional(CONF_USE_ADDRESS): cv.string_strict,
+        cv.Optional("enable_mdns"): cv.invalid(
+            "This option has been removed. Please use the [disabled] option under the "
+            "new mdns component instead."
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+RMII_SCHEMA = BASE_SCHEMA.extend(
     cv.Schema(
         {
-            cv.GenerateID(): cv.declare_id(EthernetComponent),
-            cv.Required(CONF_TYPE): cv.enum(ETHERNET_TYPES, upper=True),
             cv.Required(CONF_MDC_PIN): pins.internal_gpio_output_pin_number,
             cv.Required(CONF_MDIO_PIN): pins.internal_gpio_output_pin_number,
             cv.Optional(CONF_CLK_MODE, default="GPIO0_IN"): cv.enum(
@@ -96,15 +117,36 @@ CONFIG_SCHEMA = cv.All(
             ),
             cv.Optional(CONF_PHY_ADDR, default=0): cv.int_range(min=0, max=31),
             cv.Optional(CONF_POWER_PIN): pins.internal_gpio_output_pin_number,
-            cv.Optional(CONF_MANUAL_IP): MANUAL_IP_SCHEMA,
-            cv.Optional(CONF_DOMAIN, default=".local"): cv.domain_name,
-            cv.Optional(CONF_USE_ADDRESS): cv.string_strict,
-            cv.Optional("enable_mdns"): cv.invalid(
-                "This option has been removed. Please use the [disabled] option under the "
-                "new mdns component instead."
-            ),
         }
-    ).extend(cv.COMPONENT_SCHEMA),
+    )
+)
+
+SPI_SCHEMA = BASE_SCHEMA.extend(
+    cv.Schema(
+        {
+            cv.Required(CONF_CLK_PIN): pins.internal_gpio_output_pin_number,
+            cv.Required(CONF_MISO_PIN): pins.internal_gpio_input_pin_number,
+            cv.Required(CONF_MOSI_PIN): pins.internal_gpio_output_pin_number,
+            cv.Required(CONF_CS_PIN): pins.internal_gpio_output_pin_number,
+            cv.Optional(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_number,
+            cv.Optional(CONF_RESET_PIN): pins.internal_gpio_output_pin_number,
+            cv.Optional(CONF_CLOCK_SPEED, default=30): cv.int_range(1, 80),  # type: ignore[arg-type]
+        }
+    ),
+)
+
+CONFIG_SCHEMA = cv.All(
+    cv.typed_schema(
+        {
+            "LAN8720": RMII_SCHEMA,
+            "RTL8201": RMII_SCHEMA,
+            "DP83848": RMII_SCHEMA,
+            "IP101": RMII_SCHEMA,
+            "JL1101": RMII_SCHEMA,
+            "W5500": SPI_SCHEMA,
+        },
+        upper=True,
+    ),
     _validate,
 )
 
@@ -125,15 +167,31 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_phy_addr(config[CONF_PHY_ADDR]))
-    cg.add(var.set_mdc_pin(config[CONF_MDC_PIN]))
-    cg.add(var.set_mdio_pin(config[CONF_MDIO_PIN]))
-    cg.add(var.set_type(config[CONF_TYPE]))
-    cg.add(var.set_clk_mode(*CLK_MODES[config[CONF_CLK_MODE]]))
-    cg.add(var.set_use_address(config[CONF_USE_ADDRESS]))
+    if config[CONF_TYPE] == "W5500":
+        cg.add(var.set_clk_pin(config[CONF_CLK_PIN]))
+        cg.add(var.set_miso_pin(config[CONF_MISO_PIN]))
+        cg.add(var.set_mosi_pin(config[CONF_MOSI_PIN]))
+        cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
+        if CONF_INTERRUPT_PIN in config:
+            cg.add(var.set_interrupt_pin(config[CONF_INTERRUPT_PIN]))
+        if CONF_RESET_PIN in config:
+            cg.add(var.set_reset_pin(config[CONF_RESET_PIN]))
+        cg.add(var.set_clock_speed(config[CONF_CLOCK_SPEED]))
 
-    if CONF_POWER_PIN in config:
-        cg.add(var.set_power_pin(config[CONF_POWER_PIN]))
+        cg.add_define("USE_ETHERNET_SPI")
+        if CORE.using_esp_idf:
+            add_idf_sdkconfig_option("CONFIG_ETH_USE_SPI_ETHERNET", True)
+            add_idf_sdkconfig_option("CONFIG_ETH_SPI_ETHERNET_W5500", True)
+    else:
+        cg.add(var.set_phy_addr(config[CONF_PHY_ADDR]))
+        cg.add(var.set_mdc_pin(config[CONF_MDC_PIN]))
+        cg.add(var.set_mdio_pin(config[CONF_MDIO_PIN]))
+        cg.add(var.set_clk_mode(*CLK_MODES[config[CONF_CLK_MODE]]))
+        if CONF_POWER_PIN in config:
+            cg.add(var.set_power_pin(config[CONF_POWER_PIN]))
+
+    cg.add(var.set_type(ETHERNET_TYPES[config[CONF_TYPE]]))
+    cg.add(var.set_use_address(config[CONF_USE_ADDRESS]))
 
     if CONF_MANUAL_IP in config:
         cg.add(var.set_manual_ip(manual_ip(config[CONF_MANUAL_IP])))
