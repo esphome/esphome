ce71e613b32f8942acef0c5d28ede5d8dd65ccfa
angelnu <git@angelnu.com>
2023-11-06 22:48:59 +0000

    change alias to deprecation

--- esphome/components/ade7953/sensor.py
+++ esphome/components/ade7953/sensor.py
@@ -1,13 +1,5 @@
-from ..ade7953_i2c import sensor as ade7953_i2c
+import esphome.config_validation as cv
 
-# ade7953 is an alias of ade7953_i2c. ade7953 was used before
-# support for spi was added
-
-DEPENDENCIES = ade7953_i2c.DEPENDENCIES
-AUTO_LOAD = ade7953_i2c.AUTO_LOAD + ["ade7953_i2c"]
-
-CONFIG_SCHEMA = ade7953_i2c.CONFIG_SCHEMA
-
-
-async def to_code(config):
-    await ade7953_i2c.to_code(config)
+CONFIG_SCHEMA = CONFIG_SCHEMA = cv.invalid(
+    "The ade7953 sensor component has been renamed to ade7953_i2c."
+)
