d14176df2ed8f748a6a9c0c5c51e377323a14291
Stéphane Lepin <stephane.lepin@gmail.com>
2023-11-08 23:11:49 +0100

    better frame logging

--- components/hdmi_cec/hdmi_cec.cpp
+++ components/hdmi_cec/hdmi_cec.cpp
@@ -29,7 +29,11 @@ void HDMICEC::loop() {
 
   auto frame = this->recv_queue_.front();
   this->recv_queue_.pop();
-  ESP_LOGD(TAG, "got frame with %d bytes. first byte is %02x", frame.size(), frame[0]);
+
+  ESP_LOGD(TAG, "got frame (%d bytes):", frame.size());
+  for (int i = 0; i < frame.size(); i++) {
+    ESP_LOGD(TAG, "   [%d] = 0x%02X", i, frame[i]);
+  }
 }
 
 void IRAM_ATTR HDMICEC::gpio_intr(HDMICEC *self) {
