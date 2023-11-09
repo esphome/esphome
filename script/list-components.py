2d2d1570cd2a434cddffed9cd1f53acf6f1c326b
Your Name <you@example.com>
2023-11-09 12:17:44 +0100

    iterate on pipeline stage `list-components`.

--- script/list-components.py
+++ script/list-components.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from helpers import git_ls_files, changed_files
+import argparse
 
 
 def filter_component_files(str):
@@ -11,7 +12,7 @@ def extract_component_names_array_from_files_array(files):
     components = []
     for file in files:
         file_parts = file.split("/")
-        if len(file_parts) >= 3:
+        if len(file_parts) >= 4:
             component_name = file_parts[2]
             if component_name not in components:
                 components.append(component_name)
@@ -19,12 +20,22 @@ def extract_component_names_array_from_files_array(files):
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", "--changed", action="store_true", help="Only run on changed files"
+    )
+    args = parser.parse_args()
+
     files = git_ls_files()
     files = filter(filter_component_files, files)
-    changed = changed_files()
-    files = [f for f in files if f in changed]
-    files.sort()
+
+    if args.changed:
+        changed = changed_files()
+        files = [f for f in files if f in changed]
+
+    files = sorted(files)
     components = extract_component_names_array_from_files_array(files)
+
     for c in components:
         print(c)
 
