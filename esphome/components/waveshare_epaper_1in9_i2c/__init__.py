adec1eb72a6100d5e665f942f1d67a9826cea3c6
Giovanni Condello <giovanni.condello@coderit.it>
2023-11-12 11:15:43 +0100

    Add test case

--- esphome/components/waveshare_epaper_1in9_i2c/__init__.py
+++ esphome/components/waveshare_epaper_1in9_i2c/__init__.py
@@ -4,6 +4,7 @@ from esphome import pins
 from esphome.components import i2c
 from esphome.const import (
     CONF_BUSY_PIN,
+    CONF_I2C_ID,
     CONF_ID,
     CONF_RESET_PIN,
 )
@@ -17,22 +18,20 @@ WaveShareEPaper1in9I2C = waveshare_epaper_1in9_i2c_ns.class_(
 )
 
 
-CONF_I2C_BUS = "i2c_bus"
-CONF_I2C_COMMAND_ADDRESS = "i2c_command_address"
-CONF_I2C_DATA_ADDRESS = "i2c_data_address"
+CONF_COMMAND_ADDRESS = "command_address"
+CONF_DATA_ADDRESS = "data_address"
+MULTI_CONF = True
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(WaveShareEPaper1in9I2C),
         cv.Required(CONF_RESET_PIN): pins.gpio_output_pin_schema,
         cv.Required(CONF_BUSY_PIN): pins.gpio_input_pin_schema,
-        cv.Required(CONF_I2C_BUS): cv.use_id(i2c.I2CBus),
-        cv.Optional(CONF_I2C_COMMAND_ADDRESS, default=0x3C): cv.int_range(
-            min=0, max=0xFF
-        ),
-        cv.Optional(CONF_I2C_DATA_ADDRESS, default=0x3D): cv.int_range(min=0, max=0xFF),
+        cv.GenerateID(CONF_I2C_ID): cv.use_id(i2c.I2CBus),
+        cv.Optional(CONF_COMMAND_ADDRESS, default=0x3C): cv.int_range(min=0, max=0xFF),
+        cv.Optional(CONF_DATA_ADDRESS, default=0x3D): cv.int_range(min=0, max=0xFF),
     }
-).extend(cv.polling_component_schema("180s"))
+).extend(cv.polling_component_schema("1s"))
 
 
 async def to_code(config):
@@ -43,10 +42,10 @@ async def to_code(config):
     busy_pin = await cg.gpio_pin_expression(config[CONF_BUSY_PIN])
     cg.add(var.set_busy_pin(busy_pin))
 
-    i2c_bus = await cg.get_variable(config[CONF_I2C_BUS])
+    i2c_bus = await cg.get_variable(config[CONF_I2C_ID])
 
-    command_address = config[CONF_I2C_COMMAND_ADDRESS]
+    command_address = config[CONF_COMMAND_ADDRESS]
     cg.add(var.create_command_device(i2c_bus, command_address))
 
-    data_address = config[CONF_I2C_DATA_ADDRESS]
+    data_address = config[CONF_DATA_ADDRESS]
     cg.add(var.create_data_device(i2c_bus, data_address))
