93c749f324508839495ba5cd9c082a2c712c48d0
J. Nick Koston <nick@koston.org>
2023-11-15 22:25:36 -0600

    remove typing_extensions

--- esphome/dashboard/enum.py
+++ esphome/dashboard/enum.py
@@ -4,13 +4,11 @@ from __future__ import annotations
 from enum import Enum
 from typing import Any
 
-from typing_extensions import Self
-
 
 class StrEnum(str, Enum):
     """Partial backport of Python 3.11's StrEnum for our basic use cases."""
 
-    def __new__(cls, value: str, *args: Any, **kwargs: Any) -> Self:
+    def __new__(cls, value: str, *args: Any, **kwargs: Any) -> StrEnum:
         """Create a new StrEnum instance."""
         if not isinstance(value, str):
             raise TypeError(f"{value!r} is not a string")
