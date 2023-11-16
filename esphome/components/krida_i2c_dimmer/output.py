a81961502f9699662e882a5cec84d9460b60e99b
Dana Simmons <dana@simmons-audio.com>
2023-11-16 09:00:45 -0500

    Apply style changes and add codeowner

--- esphome/components/krida_i2c_dimmer/output.py
+++ esphome/components/krida_i2c_dimmer/output.py
@@ -6,13 +6,15 @@ from esphome.const import CONF_ID, CONF_CHANNEL
 DEPENDENCIES = ["i2c"]
 
 krida_dimmer = cg.esphome_ns.namespace("krida_i2c_dimmer")
-KridaDimmer = krida_dimmer.class_("KridaI2CDimmer", output.FloatOutput, cg.Component, i2c.I2CDevice)
+KridaDimmer = krida_dimmer.class_(
+    "KridaI2CDimmer", output.FloatOutput, cg.Component, i2c.I2CDevice
+)
 
 CONFIG_SCHEMA = (
     output.FLOAT_OUTPUT_SCHEMA.extend(
         {
             cv.Required(CONF_ID): cv.declare_id(KridaDimmer),
-            cv.Required(CONF_CHANNEL): cv.hex_int_range(0x80, 0x85)
+            cv.Required(CONF_CHANNEL): cv.hex_int_range(0x80, 0x85),
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
