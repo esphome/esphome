b605d63786542493d1ab489c17d3f3b532b30557
Jimmy Hedman <jimmy.hedman@gmail.com>
2023-11-20 22:42:38 +0100

    Add ip address subsensors to ethernet_info

--- esphome/components/ethernet_info/ethernet_info_text_sensor.h
+++ esphome/components/ethernet_info/ethernet_info_text_sensor.h
@@ -16,23 +16,27 @@ class IPAddressEthernetInfo : public PollingComponent, public text_sensor::TextS
     auto ips = ethernet::global_eth_component->get_ip_addresses();
     if (ips != this->last_ips_) {
       this->last_ips_ = ips;
+      this->publish_state(ips[0].str());
+      uint8_t sensor = 0;
       for (auto &ip : ips) {
         if (ip.is_set()) {
-          address_results += ip.is_ip4() ? "IPv4: " : "IPv6: ";
-          address_results += ip.str();
-          address_results += "\n";
+          if (ip_sensors_[sensor])  {
+            this->ip_sensors_[sensor]->publish_state(ip.str());
+            sensor++;
+          }
         }
       }
-      this->publish_state(address_results);
     }
   }
 
   float get_setup_priority() const override { return setup_priority::ETHERNET; }
   std::string unique_id() override { return get_mac_address() + "-ethernetinfo"; }
   void dump_config() override;
+  void add_ip_sensors(uint8_t index, text_sensor::TextSensor *s) { ip_sensors_[index] = s; }
 
  protected:
   network::IPAddresses last_ips_;
+  std::array<text_sensor::TextSensor *, 5> ip_sensors_;
 };
 
 }  // namespace ethernet_info
