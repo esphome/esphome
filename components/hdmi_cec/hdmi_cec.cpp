f3ef588a528c824377942a3b5ef62f1b36a54a9a
Stéphane Lepin <stephane.lepin@gmail.com>
2023-11-20 06:44:56 +0100

    send: wait for "bus free" condition before every attempt

--- components/hdmi_cec/hdmi_cec.cpp
+++ components/hdmi_cec/hdmi_cec.cpp
@@ -104,18 +104,19 @@ bool HDMICEC::send(uint8_t source, uint8_t destination, const std::vector<uint8_
   {
     LockGuard send_lock(send_mutex_);
 
-    ESP_LOGV(TAG, "HDMICEC::send(): waiting for the bus to be free...");
-    while((micros() - last_falling_edge_us_) < MIN_SIGNAL_FREE_TIME) {
-      delay_microseconds_safe(TOTAL_BIT_US); // wait for one bit period
-    }
-    ESP_LOGV(TAG, "HDMICEC::send(): bus available, sending frame...");
-
     for (size_t i = 0; i < MAX_ATTEMPTS; i++) {
+      ESP_LOGV(TAG, "HDMICEC::send(): waiting for the bus to be free...");
+      while((micros() - last_falling_edge_us_) < MIN_SIGNAL_FREE_TIME) {
+        delay_microseconds_safe(TOTAL_BIT_US); // wait for one bit period
+      }
+      ESP_LOGV(TAG, "HDMICEC::send(): bus available, sending frame...");
+
       bool success = send_frame_(frame, is_broadcast);
       if (success) {
         ESP_LOGV(TAG, "HDMICEC::send(): frame sent and acknowledged");
         return true;
       }
+
       delay_microseconds_safe(TOTAL_BIT_US);
     }
   }
