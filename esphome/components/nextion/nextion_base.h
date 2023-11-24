3a482ff1592e3519cebc51aab1d1f7cac9c15ce7
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2023-11-24 12:15:15 +0100

    Expose `is_connected()`

--- esphome/components/nextion/nextion_base.h
+++ esphome/components/nextion/nextion_base.h
@@ -48,6 +48,7 @@ class NextionBase {
 
   bool is_sleeping() { return this->is_sleeping_; }
   bool is_setup() { return this->is_setup_; }
+  bool is_connected() { return this->is_connected_; }
   bool is_detected() { return this->is_detected_; }
 
  protected:
