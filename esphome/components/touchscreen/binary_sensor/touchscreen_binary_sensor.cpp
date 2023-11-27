5ff58f672543172df84c03b8d14732b74417bc55
NP v/d Spek <github_mail@lumensoft.nl>
2023-11-27 05:08:58 +0100

    impl Jesse's suggestions #2

--- esphome/components/touchscreen/binary_sensor/touchscreen_binary_sensor.cpp
+++ esphome/components/touchscreen/binary_sensor/touchscreen_binary_sensor.cpp
@@ -10,15 +10,13 @@ void TouchscreenBinarySensor::setup() {
 
 void TouchscreenBinarySensor::touch(TouchPoint tp) {
   bool touched = (tp.x >= this->x_min_ && tp.x <= this->x_max_ && tp.y >= this->y_min_ && tp.y <= this->y_max_);
-#ifdef USE_DISPLAY
   if ((this->page_ != nullptr) && (this->parent_->get_display() != nullptr)) {
     touched &= this->page_ == this->parent_->get_display()->get_active_page();
   }
-#endif
   if (touched) {
     this->publish_state(true);
   } else {
-    release();
+    this->release();
   }
 }
 
