bb4d290636ce6f4986de1c0e28d831ed52c3c928
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2023-12-01 14:47:33 +0100

    ci-suggest-changes

--- esphome/components/nextion/nextion.h
+++ esphome/components/nextion/nextion.h
@@ -956,7 +956,9 @@ class Nextion : public NextionBase, public PollingComponent, public uart::UARTDe
   void set_wake_up_page_internal(uint8_t wake_up_page) { this->wake_up_page_ = wake_up_page; }
   void set_start_up_page_internal(uint8_t start_up_page) { this->start_up_page_ = start_up_page; }
   void set_auto_wake_on_touch_internal(bool auto_wake_on_touch) { this->auto_wake_on_touch_ = auto_wake_on_touch; }
-  void set_exit_reparse_on_start_internal(bool exit_reparse_on_start) { this->exit_reparse_on_start_ = exit_reparse_on_start; }
+  void set_exit_reparse_on_start_internal(bool exit_reparse_on_start) {
+    this->exit_reparse_on_start_ = exit_reparse_on_start;
+  }
 
  protected:
   std::deque<NextionQueue *> nextion_queue_;
