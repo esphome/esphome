24fc646c94c96847f81141c23215cab0bbf211f2
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2023-12-02 14:55:06 +1100

    Working version.

--- esphome/components/ft5x06/touchscreen/__init__.py
+++ esphome/components/ft5x06/touchscreen/__init__.py
@@ -1,9 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
-from esphome import pins
 from esphome.components import i2c, touchscreen
-from esphome.const import CONF_INTERRUPT_PIN, CONF_ID, CONF_ROTATION
+from esphome.const import CONF_ID, CONF_ROTATION
 from .. import ft5x06_ns
 
 FT5x06ButtonListener = ft5x06_ns.class_("FT5x06ButtonListener")
@@ -25,7 +24,6 @@ CONFIG_SCHEMA = (
         {
             cv.GenerateID(): cv.declare_id(FT5x06Touchscreen),
             cv.Optional(CONF_ROTATION): cv.enum(ROTATIONS),
-            cv.Required(CONF_INTERRUPT_PIN): pins.gpio_input_pin_schema,
         }
     )
     .extend(i2c.i2c_device_schema(0x48))
@@ -38,8 +36,5 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
     await touchscreen.register_touchscreen(var, config)
-
-    interrupt_pin = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
-    cg.add(var.set_interrupt_pin(interrupt_pin))
     if rotation := config.get(CONF_ROTATION):
         cg.add(var.set_rotation(ROTATIONS[rotation]))
--- esphome/components/ft5x06/touchscreen/ft5x06_touchscreen.h
+++ esphome/components/ft5x06/touchscreen/ft5x06_touchscreen.h
@@ -40,14 +40,11 @@ class FT5x06ButtonListener {
 class FT5x06Touchscreen : public touchscreen::Touchscreen, public Component, public i2c::I2CDevice {
  public:
   void set_rotation(touchscreen::TouchRotation rotation) { this->rotation_ = rotation; }
-  void set_interrupt_pin(GPIOPin *pin) { this->interrupt_pin_ = pin; }
   void register_button_listener(FT5x06ButtonListener *listener) { this->button_listeners_.push_back(listener); }
 
   void setup() override {
     i2c::ErrorCode err;
     ESP_LOGCONFIG(TAG, "Setting up FT5x06 Touchscreen...");
-    this->interrupt_pin_->pin_mode(gpio::FLAG_INPUT);
-    this->interrupt_pin_->setup();
     // wait 200ms after reset.
     this->set_timeout(200, [this] { this->continue_setup_(); });
   }
@@ -66,22 +63,13 @@ class FT5x06Touchscreen : public touchscreen::Touchscreen, public Component, pub
       case FT5X06_ID_2:
       case FT5X06_ID_3:
         this->vendor_id_ = (VendorId) data;
-        esph_log_d(TAG, "Read vendor ID 0x%X", data);
+        esph_log_v(TAG, "Read vendor ID 0x%X", data);
         break;
 
       default:
         esph_log_e(TAG, "Unknown vendor ID 0x%X", data);
         this->mark_failed();
         return;
-    }
-
-    if (this->interrupt_pin_->is_internal()) {
-      this->is_int_internal_ = true;
-      static_cast<InternalGPIOPin *>(this->interrupt_pin_)
-          ->attach_interrupt(Store::gpio_intr, &this->store_, gpio::INTERRUPT_FALLING_EDGE);
-      this->write_byte(FT5X06_I_MODE, 1);
-    } else {
-      this->write_byte(FT5X06_I_MODE, 0);
     }
     ESP_LOGCONFIG(TAG, "FT5x06 Touchscreen setup complete");
   }
@@ -91,30 +79,22 @@ class FT5x06Touchscreen : public touchscreen::Touchscreen, public Component, pub
     uint8_t touch_cnt;
     uint8_t data[MAX_TOUCHES][6];  // 8 bytes each for each point, plus extra space for the key byte
 
-    if (this->is_int_internal_) {
-      if (!this->store_.available)
-        return;
-      this->store_.available = false;
-    } else if (!this->was_down_) {
-      if (this->interrupt_pin_->digital_read())
-        return;
-    }
     if (!this->read_byte(FT5X06_TD_STATUS, &touch_cnt) || touch_cnt > MAX_TOUCHES) {
       esph_log_w(TAG, "Failed to read status");
       return;
     }
     if (touch_cnt == 0) {
-      this->send_release_();
-      this->was_down_ = false;
-      esph_log_d(TAG, "Send release");
+      if (this->was_down_) {
+        this->send_release_();
+        this->was_down_ = false;
+        esph_log_v(TAG, "Send release");
+      }
       return;
     }
     if (!this->read_bytes(FT5X06_TOUCH_DATA, (uint8_t *) data, touch_cnt * 6)) {
       esph_log_w(TAG, "Failed to read touch data");
       return;
     }
-    this->was_down_ = true;
-
     for (uint8_t i = 0; i != touch_cnt; i++) {
       uint8_t status = data[i][0] >> 6;
       tp.id = data[i][2] >> 3;
@@ -139,8 +119,9 @@ class FT5x06Touchscreen : public touchscreen::Touchscreen, public Component, pub
           tp.y = x;
           break;
       }
-      esph_log_d(TAG, "Read %X status, id: %d, pos %d/%d", status, tp.id, tp.x, tp.y);
+      esph_log_v(TAG, "Read %X status, id: %d, pos %d/%d", status, tp.id, tp.x, tp.y);
       if (status == 0 || status == 2) {
+        this->was_down_ = true;
         this->defer([this, tp]() { this->send_touch_(tp); });
       }
     }
@@ -149,14 +130,11 @@ class FT5x06Touchscreen : public touchscreen::Touchscreen, public Component, pub
   void dump_config() override {
     ESP_LOGCONFIG(TAG, "FT5x06 Touchscreen:");
     LOG_I2C_DEVICE(this);
-    LOG_PIN("  Interrupt Pin: ", this->interrupt_pin_);
     ESP_LOGCONFIG(TAG, "  Rotation: %d", (int) this->rotation_);
     ESP_LOGCONFIG(TAG, "  Vendor ID: 0x%X", (int) this->vendor_id_);
   }
 
  protected:
-  GPIOPin *interrupt_pin_;
-  bool is_int_internal_{};
   bool was_down_{};
   Store store_;
   std::vector<FT5x06ButtonListener *> button_listeners_;
