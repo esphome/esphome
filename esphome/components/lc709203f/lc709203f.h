4c4177b29c528bda727f40e1788dd037e41399e5
J.G.Aguado <jon-garcia@hotmail.com>
2023-12-07 23:32:36 +0100

    Fixed formatting issues

--- esphome/components/lc709203f/lc709203f.h
+++ esphome/components/lc709203f/lc709203f.h
@@ -2,7 +2,7 @@
  * File:          lc709203f.h
  * Addapted by:   J.G. Aguado
  * Date:          30/11/2023
- * 
+ *
  * Description:
  * This component adapts (into ESPHome) the I2C Driver for the LC709203F Battery Monitor IC,
  * based on the library from Daniel deBeer (EzSBC) available at:
@@ -12,7 +12,6 @@
 #ifndef _LC709203F_H
 #define _LC709203F_H
 
-
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
@@ -20,59 +19,62 @@
 namespace esphome {
 namespace lc709203f {
 
-#define LC709203F_I2C_ADDR              0x0B    // LC709203F default i2c address
+#define LC709203F_I2C_ADDR 0x0B  // LC709203F default i2c address
 
 // Registers per datasheet Table 6
 
-#define LC709203F_WO_BEFORE_RSOC      0x04    // Executes RSOC initialization with sampled maximum voltage when 0xAA55 is set
-#define LC709203F_RW_THERMISTORB      0x06    // Sets B−constant of the thermistor to be measured.
-#define LC709203F_WO_INITRSOC 	      0x07    // Executes RSOC initialization when 0xAA55 is set.
-#define LC709203F_RW_CELLTEMPERATURE  0x08    // Read or Write Cell Temperature  For ESP32-Bar-Rev2 must write temperature, no thermistor on board
-#define LC709203F_RO_CELLVOLTAGE      0x09    // Read cell voltage
-#define LC709203F_RW_CURRENTDIRECTION 0x0A    // Selects Auto/Charge/Discharge mode 0x0000: Auto mode, 0x0001: Charge mode, 0xFFFF: Discharge mode
-#define LC709203F_RW_APA 	      0x0B    // APA Register, Table 7
-#define LC709203F_RW_APTHERMISTOR     0x0C    // Sets a value to adjust temperature measurement delay timing. Defaults to0x001E
-#define LC709203F_RW_RSOC             0x0D    // Read cell indicator to empty in 1% steps
-#define LC709203F_RO_ITE     	      0x0F    // Read cell indicator to empty in 0.1% steps
-#define LC709203F_RO_ICVERSION 	      0x11    // Contains the ID number of the IC
-#define LC709203F_RW_PROFILE	      0x12    // Adjusts the battery profile for nominal and fully charged voltages, see Table 8
-#define LC709203F_RW_ALARMRSOC 	      0x13    // Alarm on percent threshold
-#define LC709203F_RW_ALARMVOLT 	      0x14    // Alarm on voltage threshold
-#define LC709203F_RW_POWERMODE 	      0x15    // Sets sleep/power mode 0x0001: Operational mode 0x0002: Sleep mode
-#define LC709203F_RW_STATUSBIT 	      0x16    // Temperature method, 0x0000: I2C mode, 0x0001: Thermistor mode 
-#define LC709203F_RO_CELLPROFILE      0x1A    // Displays battery profile code
+#define LC709203F_WO_BEFORE_RSOC 0x04  // Executes RSOC initialization with sampled maximum voltage when 0xAA55 is set
+#define LC709203F_RW_THERMISTORB 0x06  // Sets B−constant of the thermistor to be measured.
+#define LC709203F_WO_INITRSOC 0x07     // Executes RSOC initialization when 0xAA55 is set.
+#define LC709203F_RW_CELLTEMPERATURE \
+  0x08  // Read or Write Cell Temperature  For ESP32-Bar-Rev2 must write temperature, no thermistor on board
+#define LC709203F_RO_CELLVOLTAGE 0x09  // Read cell voltage
+#define LC709203F_RW_CURRENTDIRECTION \
+  0x0A  // Selects Auto/Charge/Discharge mode 0x0000: Auto mode, 0x0001: Charge mode, 0xFFFF: Discharge mode
+#define LC709203F_RW_APA 0x0B  // APA Register, Table 7
+#define LC709203F_RW_APTHERMISTOR \
+  0x0C                               // Sets a value to adjust temperature measurement delay timing. Defaults to0x001E
+#define LC709203F_RW_RSOC 0x0D       // Read cell indicator to empty in 1% steps
+#define LC709203F_RO_ITE 0x0F        // Read cell indicator to empty in 0.1% steps
+#define LC709203F_RO_ICVERSION 0x11  // Contains the ID number of the IC
+#define LC709203F_RW_PROFILE 0x12    // Adjusts the battery profile for nominal and fully charged voltages, see Table 8
+#define LC709203F_RW_ALARMRSOC 0x13  // Alarm on percent threshold
+#define LC709203F_RW_ALARMVOLT 0x14  // Alarm on voltage threshold
+#define LC709203F_RW_POWERMODE 0x15  // Sets sleep/power mode 0x0001: Operational mode 0x0002: Sleep mode
+#define LC709203F_RW_STATUSBIT 0x16  // Temperature method, 0x0000: I2C mode, 0x0001: Thermistor mode
+#define LC709203F_RO_CELLPROFILE 0x1A  // Displays battery profile code
 
 // to remove warning static uint8_t crc8(uint8_t *data, int len);
 
 /*!  Approx cell capacity Table 7 */
 typedef enum {
-  LC709203F_APA_100MAH              = 0x08,
-  LC709203F_APA_200MAH              = 0x0B,
-  LC709203F_APA_500MAH              = 0x10,
-  LC709203F_APA_1000MAH             = 0x19,
-  LC709203F_APA_2000MAH             = 0x2D,
-  LC709203F_APA_3000MAH             = 0x36,
+  LC709203F_APA_100MAH = 0x08,
+  LC709203F_APA_200MAH = 0x0B,
+  LC709203F_APA_500MAH = 0x10,
+  LC709203F_APA_1000MAH = 0x19,
+  LC709203F_APA_2000MAH = 0x2D,
+  LC709203F_APA_3000MAH = 0x36,
 } lc709203_adjustment_t;
 
 /*!  Cell profile */
 typedef enum {
-  LC709203_NOM3p7_Charge4p2         = 1,
-  LC709203_NOM3p8_Charge4p35        = 3,
+  LC709203_NOM3p7_Charge4p2 = 1,
+  LC709203_NOM3p8_Charge4p35 = 3,
   LC709203_NOM3p8_Charge4p35_Less500mAh = 6,
-  LC709203_ICR18650_SAMSUNG         = 5,
-  LC709203_ICR18650_PANASONIC       = 4
-}lc709203_cell_profile_t ;
+  LC709203_ICR18650_SAMSUNG = 5,
+  LC709203_ICR18650_PANASONIC = 4
+} lc709203_cell_profile_t;
 
 /*!  Cell temperature source */
 typedef enum {
-  LC709203F_TEMPERATURE_I2C         = 0x0000,
-  LC709203F_TEMPERATURE_THERMISTOR  = 0x0001,
+  LC709203F_TEMPERATURE_I2C = 0x0000,
+  LC709203F_TEMPERATURE_THERMISTOR = 0x0001,
 } lc709203_tempmode_t;
 
 /*!  Chip power state */
 typedef enum {
-  LC709203F_POWER_OPERATE           = 0x0001,
-  LC709203F_POWER_SLEEP             = 0x0002,
+  LC709203F_POWER_OPERATE = 0x0001,
+  LC709203F_POWER_SLEEP = 0x0002,
 } lc709203_powermode_t;
 
 /*!
@@ -81,20 +83,18 @@ typedef enum {
  */
 // class LC709203F {
 class LC709203FComponent : public PollingComponent, public i2c::I2CDevice {
-public:
-
-  bool begin( void );
+ public:
+  bool begin(void);
   void initRSOC(void);
 
   void setPowerMode(lc709203_powermode_t t);
   void setCellCapacity(lc709203_adjustment_t apa);
   void setCellProfile(lc709203_cell_profile_t t);
-  
+
   uint16_t getICversion(void);
   uint16_t cellVoltage_mV(void);
-  uint16_t cellRemainingPercent10(void);	// Remaining capacity in increments of 0.1% as integer
-  uint16_t cellStateOfCharge(void);		// In increments of 1% as integer
-	
+  uint16_t cellRemainingPercent10(void);  // Remaining capacity in increments of 0.1% as integer
+  uint16_t cellStateOfCharge(void);       // In increments of 1% as integer
 
   uint16_t getThermistorBeta(void);
   void setThermistorB(uint16_t beta);
@@ -104,18 +104,17 @@ public:
   void setAlarmRSOC(uint8_t percent);
   void setAlarmVoltage(float voltage);
 
-
   //  added to align with esphome
-  void set_cellVoltage_sensor( sensor::Sensor *cellVoltage)       { cellVoltage_    = cellVoltage; }
-  void set_cellRemPercent_sensor( sensor::Sensor *cellRemPercent) { cellRemPercent_ = cellRemPercent; }
-  void set_icversion_sensor( sensor::Sensor *icversion)           { icversion_      = icversion; }
-  void set_cellCharge_sensor( sensor::Sensor *cellCharge)         { cellCharge_     = cellCharge; }
+  void set_cellVoltage_sensor(sensor::Sensor *cellVoltage) { cellVoltage_ = cellVoltage; }
+  void set_cellRemPercent_sensor(sensor::Sensor *cellRemPercent) { cellRemPercent_ = cellRemPercent; }
+  void set_icversion_sensor(sensor::Sensor *icversion) { icversion_ = icversion; }
+  void set_cellCharge_sensor(sensor::Sensor *cellCharge) { cellCharge_ = cellCharge; }
   void setup() override;
   void update() override;
   void dump_config() override;
 
-protected:
-  void write16( uint8_t regAddress, uint16_t data);
+ protected:
+  void write16(uint8_t regAddress, uint16_t data);
   int16_t read16(uint8_t regAddress);
 
   // added for esphome
@@ -123,10 +122,9 @@ protected:
   sensor::Sensor *cellRemPercent_;
   sensor::Sensor *icversion_;
   sensor::Sensor *cellCharge_;
-
 };
 
-}   // namespace lc709203f
-}   // namespace esphome
+}  // namespace lc709203f
+}  // namespace esphome
 
 #endif
--- esphome/components/lc709203f/sensor.py
+++ esphome/components/lc709203f/sensor.py
@@ -1,34 +1,48 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import CONF_ID,\
-    CONF_BATTERY_LEVEL, CONF_BATTERY_VOLTAGE, UNIT_VOLT, ICON_EMPTY, UNIT_PERCENT, UNIT_EMPTY
+from esphome.const import (
+    CONF_ID,
+    CONF_BATTERY_LEVEL,
+    CONF_BATTERY_VOLTAGE,
+    UNIT_VOLT,
+    ICON_EMPTY,
+    UNIT_PERCENT,
+    UNIT_EMPTY,
+)
 
-DEPENDENCIES = ['i2c']
+DEPENDENCIES = ["i2c"]
 
 # locally defined constant
 
-lc709203f_ns = cg.esphome_ns.namespace('lc709203f')
+lc709203f_ns = cg.esphome_ns.namespace("lc709203f")
 
-LC709203FComponent = lc709203f_ns.class_('LC709203FComponent', cg.PollingComponent, i2c.I2CDevice)
+LC709203FComponent = lc709203f_ns.class_(
+    "LC709203FComponent", cg.PollingComponent, i2c.I2CDevice
+)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(LC709203FComponent),
-    cv.Optional(CONF_BATTERY_VOLTAGE):
-        sensor.sensor_schema(unit_of_measurement=UNIT_VOLT, icon=ICON_EMPTY, accuracy_decimals=2).extend({
-        }),
-    cv.Optional(CONF_BATTERY_LEVEL):
-        sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, icon=ICON_EMPTY, accuracy_decimals=1).extend({
-        }),
-    cv.Optional('cell_charge'):
-        sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, icon=ICON_EMPTY, accuracy_decimals=0).extend({
-        }),
-    #cv.Optional('icversion'): cv.uint16_t,
-    cv.Optional('icversion'):
-        sensor.sensor_schema(unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=0).extend({
-        }),
-
-}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(0x77))
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(LC709203FComponent),
+            cv.Optional(CONF_BATTERY_VOLTAGE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_VOLT, icon=ICON_EMPTY, accuracy_decimals=2
+            ).extend({}),
+            cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(
+                unit_of_measurement=UNIT_PERCENT, icon=ICON_EMPTY, accuracy_decimals=1
+            ).extend({}),
+            cv.Optional("cell_charge"): sensor.sensor_schema(
+                unit_of_measurement=UNIT_PERCENT, icon=ICON_EMPTY, accuracy_decimals=0
+            ).extend({}),
+            # cv.Optional('icversion'): cv.uint16_t,
+            cv.Optional("icversion"): sensor.sensor_schema(
+                unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=0
+            ).extend({}),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x77))
+)
 
 
 def to_code(config):
@@ -44,20 +58,20 @@ def to_code(config):
         sens = yield sensor.new_sensor(config[CONF_BATTERY_VOLTAGE])
         cg.add(var.set_cellVoltage_sensor(sens))
 
-    if 'cell_charge' in config:
-        sens = yield sensor.new_sensor(config['cell_charge'])
+    if "cell_charge" in config:
+        sens = yield sensor.new_sensor(config["cell_charge"])
         cg.add(var.set_cellCharge_sensor(sens))
 
-    if 'icversion'  in config:
-        sens = yield sensor.new_sensor(config['icversion'])
+    if "icversion" in config:
+        sens = yield sensor.new_sensor(config["icversion"])
         cg.add(var.set_icversion_sensor(sens))
 
 
-#/ read-only values
-#/ implement sensors for cellVoltage_mV, cellRemainingPercent10, cellStateOfCharge, getICversion
+# / read-only values
+# / implement sensors for cellVoltage_mV, cellRemainingPercent10, cellStateOfCharge, getICversion
 
-#/ write-only values
-#/ implement config for setPowerMode, setCellCapacity, setCellProfile, setAlarmRSOC, setAlarmVoltage
+# / write-only values
+# / implement config for setPowerMode, setCellCapacity, setCellProfile, setAlarmRSOC, setAlarmVoltage
 
-#/ uninstalled ( exsbc source )
-#/ getThermistorBeta, setThermistorB, setTemperatureMode, getCellTemperature, initRSOC
+# / uninstalled ( exsbc source )
+# / getThermistorBeta, setThermistorB, setTemperatureMode, getCellTemperature, initRSOC
