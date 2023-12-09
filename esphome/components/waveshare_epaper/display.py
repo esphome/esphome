02316a7d9f5deb238f4ad4a35f45b10eebe4fca2
Fabian Pflug <fabian.pflug@gumulka.eu>
2023-12-09 15:09:09 +0100

    Add support for waveshare 2.9in B V3 version
    The V3 version of the 2.9in B Veriant has a new interface and therefor
    needs a new init routine and also handles the red channel differently
    from the previous version.

--- esphome/components/waveshare_epaper/display.py
+++ esphome/components/waveshare_epaper/display.py
@@ -29,6 +29,9 @@ WaveshareEPaper2P7In = waveshare_epaper_ns.class_(
 WaveshareEPaper2P9InB = waveshare_epaper_ns.class_(
     "WaveshareEPaper2P9InB", WaveshareEPaper
 )
+WaveshareEPaper2P9InBV3 = waveshare_epaper_ns.class_(
+    "WaveshareEPaper2P9InBV3", WaveshareEPaper
+)
 GDEY029T94 = waveshare_epaper_ns.class_("GDEY029T94", WaveshareEPaper)
 WaveshareEPaper4P2In = waveshare_epaper_ns.class_(
     "WaveshareEPaper4P2In", WaveshareEPaper
@@ -84,6 +87,7 @@ MODELS = {
     "gdey029t94": ("c", GDEY029T94),
     "2.70in": ("b", WaveshareEPaper2P7In),
     "2.90in-b": ("b", WaveshareEPaper2P9InB),
+    "2.90in-bv3": ("b", WaveshareEPaper2P9InBV3),
     "4.20in": ("b", WaveshareEPaper4P2In),
     "4.20in-bv2": ("b", WaveshareEPaper4P2InBV2),
     "5.83in": ("b", WaveshareEPaper5P8In),
