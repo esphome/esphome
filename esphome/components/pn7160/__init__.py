10b963bbb510cc516b9c90528fc4f50d8a08ecd0
Keith Burzinski <kbx81x@gmail.com>
2023-12-12 01:08:27 -0600

    Implement optimizations based on review

--- esphome/components/pn7160/__init__.py
+++ esphome/components/pn7160/__init__.py
@@ -124,26 +124,14 @@ PN7160_SCHEMA = cv.Schema(
     SetEmulationMessageAction,
     SET_MESSAGE_ACTION_SCHEMA,
 )
-async def pn7160_set_emulation_message_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
-    template_ = await cg.templatable(config[CONF_MESSAGE], args, cg.std_string)
-    cg.add(var.set_message(template_))
-    template_ = await cg.templatable(
-        config[CONF_INCLUDE_ANDROID_APP_RECORD], args, cg.bool_
-    )
-    cg.add(var.set_include_android_app_record(template_))
-    return var
-
-
 @automation.register_action(
     "tag.set_write_message",
     SetWriteMessageAction,
     SET_MESSAGE_ACTION_SCHEMA,
 )
-async def pn7160_set_write_message_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
+async def pn7160_set_message_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
     template_ = await cg.templatable(config[CONF_MESSAGE], args, cg.std_string)
     cg.add(var.set_message(template_))
     template_ = await cg.templatable(
@@ -172,15 +160,16 @@ async def pn7160_set_write_message_to_code(config, action_id, template_arg, args
     "tag.set_write_mode", SetWriteModeAction, SIMPLE_ACTION_SCHEMA
 )
 async def pn7160_simple_action_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    return cg.new_Pvariable(action_id, template_arg, paren)
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    return var
 
 
 async def setup_pn7160(var, config):
     await cg.register_component(var, config)
 
-    if CONF_DWL_REQ_PIN in config:
-        pin = await cg.gpio_pin_expression(config[CONF_DWL_REQ_PIN])
+    if dwl_req_pin_config := config.get(CONF_DWL_REQ_PIN):
+        pin = await cg.gpio_pin_expression(dwl_req_pin_config)
         cg.add(var.set_dwl_req_pin(pin))
 
     pin = await cg.gpio_pin_expression(config[CONF_IRQ_PIN])
@@ -189,12 +178,12 @@ async def setup_pn7160(var, config):
     pin = await cg.gpio_pin_expression(config[CONF_VEN_PIN])
     cg.add(var.set_ven_pin(pin))
 
-    if CONF_WKUP_REQ_PIN in config:
-        pin = await cg.gpio_pin_expression(config[CONF_WKUP_REQ_PIN])
+    if wakeup_req_pin_config := config.get(CONF_WKUP_REQ_PIN):
+        pin = await cg.gpio_pin_expression(wakeup_req_pin_config)
         cg.add(var.set_wkup_req_pin(pin))
 
-    if CONF_EMULATION_MESSAGE in config:
-        cg.add(var.set_tag_emulation_message(config[CONF_EMULATION_MESSAGE]))
+    if emulation_message_config := config.get(CONF_EMULATION_MESSAGE):
+        cg.add(var.set_tag_emulation_message(emulation_message_config))
         cg.add(var.set_tag_emulation_on())
 
     if CONF_TAG_TTL in config:
