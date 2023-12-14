777cdb1c21ec3b467f47cf71ffdc53ee9112b69a
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-12-14 13:24:16 +0900

    Allow use of CDC/JTAG loggers on esp32 variants with Arduino (#4658)
    * Allow use of CDC/JTAG loggers on esp32 variants with Arduino
    
    * Only on s2/s3
    
    * Separate C3 from S2/S3
    
    * C code builds & runs correctly, still needs work though
    
    * Works on S2
    
    * It works!
    
    * Remove unnecessary header
    
    ---------
    
    Co-authored-by: Keith Burzinski <kbx81x@gmail.com>

--- esphome/components/logger/__init__.py
+++ esphome/components/logger/__init__.py
@@ -124,8 +124,6 @@ is_log_level = cv.one_of(*LOG_LEVELS, upper=True)
 
 def uart_selection(value):
     if CORE.is_esp32:
-        if value.upper() in ESP_IDF_UARTS and not CORE.using_esp_idf:
-            raise cv.Invalid(f"Only esp-idf framework supports {value}.")
         variant = get_esp32_variant()
         if variant in UART_SELECTION_ESP32:
             return cv.one_of(*UART_SELECTION_ESP32[variant], upper=True)(value)
