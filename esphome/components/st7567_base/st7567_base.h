0b2129c9f6378feb0e8c12e4c9b4eb2e2ce7b261
Anton Viktorov <anton.viktorov@live.com>
2023-12-17 21:55:35 +0000

    ST7567 clang ident fix

--- esphome/components/st7567_base/st7567_base.h
+++ esphome/components/st7567_base/st7567_base.h
@@ -34,59 +34,59 @@ static const uint8_t ST7567_SET_EV_PARAM = 0x00;
 static const uint8_t ST7567_RESISTOR_RATIO = 0x20;
 
 class ST7567 : public display::DisplayBuffer {
-   public:
-    void setup() override;
+ public:
+  void setup() override;
 
-    void update() override;
+  void update() override;
 
-    void set_reset_pin(GPIOPin *reset_pin) { this->reset_pin_ = reset_pin; }
-    void init_flip_x(bool flip_x) { this->flip_x_ = flip_x; }
-    void init_flip_y(bool flip_y) { this->flip_y_ = flip_y; }
-    void init_invert(bool invert) { this->invert_ = invert; }
-    void set_invert(bool invert);
+  void set_reset_pin(GPIOPin *reset_pin) { this->reset_pin_ = reset_pin; }
+  void init_flip_x(bool flip_x) { this->flip_x_ = flip_x; }
+  void init_flip_y(bool flip_y) { this->flip_y_ = flip_y; }
+  void init_invert(bool invert) { this->invert_ = invert; }
+  void set_invert(bool invert);
 
-    void set_contrast(uint8_t val);    // 0..63, 27-30 normal
-    void set_brightness(uint8_t val);  // 0..7, 5 normal
-    void set_all_pixels_on(bool enable);
-    void set_scroll(uint8_t line);
+  void set_contrast(uint8_t val);    // 0..63, 27-30 normal
+  void set_brightness(uint8_t val);  // 0..7, 5 normal
+  void set_all_pixels_on(bool enable);
+  void set_scroll(uint8_t line);
 
-    bool is_on();
-    void turn_on();
-    void turn_off();
+  bool is_on();
+  void turn_on();
+  void turn_off();
 
-    float get_setup_priority() const override { return setup_priority::PROCESSOR; }
-    void fill(Color color) override;
+  float get_setup_priority() const override { return setup_priority::PROCESSOR; }
+  void fill(Color color) override;
 
-    display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_BINARY; }
+  display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_BINARY; }
 
-   protected:
-    virtual void command(uint8_t value) = 0;
-    virtual void write_display_data() = 0;
+ protected:
+  virtual void command(uint8_t value) = 0;
+  virtual void write_display_data() = 0;
 
-    void init_reset_();
-    void display_init_();
+  void init_reset_();
+  void display_init_();
 
-    void draw_absolute_pixel_internal(int x, int y, Color color) override;
+  void draw_absolute_pixel_internal(int x, int y, Color color) override;
 
-    int get_height_internal() override;
-    int get_width_internal() override;
-    size_t get_buffer_length_();
+  int get_height_internal() override;
+  int get_width_internal() override;
+  size_t get_buffer_length_();
 
-    int get_offset_x() { return flip_x_ ? 4 : 0; };
+  int get_offset_x() { return flip_x_ ? 4 : 0; };
 
-    const char *model_str_();
+  const char *model_str_();
 
-    GPIOPin *reset_pin_{nullptr};
-    bool is_on_{false};
-    // float contrast_{1.0};
-    // float brightness_{1.0};
-    uint8_t contrast_{27};
-    uint8_t brightness_{5};
-    bool flip_x_{true};
-    bool flip_y_{true};
-    bool invert_{false};
-    bool all_pixels_on_{false};
-    uint8_t start_line_{0};
+  GPIOPin *reset_pin_{nullptr};
+  bool is_on_{false};
+  // float contrast_{1.0};
+  // float brightness_{1.0};
+  uint8_t contrast_{27};
+  uint8_t brightness_{5};
+  bool flip_x_{true};
+  bool flip_y_{true};
+  bool invert_{false};
+  bool all_pixels_on_{false};
+  uint8_t start_line_{0};
 };
 
 }  // namespace st7567_base
--- esphome/components/st7567_i2c/st7567_i2c.h
+++ esphome/components/st7567_i2c/st7567_i2c.h
@@ -8,15 +8,15 @@ namespace esphome {
 namespace st7567_i2c {
 
 class I2CST7567 : public st7567_base::ST7567, public i2c::I2CDevice {
-   public:
-    void setup() override;
-    void dump_config() override;
+ public:
+  void setup() override;
+  void dump_config() override;
 
-   protected:
-    void command(uint8_t value) override;
-    void write_display_data() override;
+ protected:
+  void command(uint8_t value) override;
+  void write_display_data() override;
 
-    enum ErrorCode { NONE = 0, COMMUNICATION_FAILED } error_code_{NONE};
+  enum ErrorCode { NONE = 0, COMMUNICATION_FAILED } error_code_{NONE};
 };
 
 }  // namespace st7567_i2c
