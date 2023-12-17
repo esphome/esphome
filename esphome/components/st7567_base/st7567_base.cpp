2d69b0f41ca303c122cd6cf05c16ceec879a8f3b
Anton Viktorov <anton.viktorov@live.com>
2023-12-17 21:55:35 +0000

    ST7567 clang ident fix

--- esphome/components/st7567_base/st7567_base.cpp
+++ esphome/components/st7567_base/st7567_base.cpp
@@ -8,94 +8,94 @@ namespace st7567_base {
 static const char *const TAG = "st7567";
 
 void ST7567::setup() {
-    this->init_internal_(this->get_buffer_length_());
-    this->display_init_();
+  this->init_internal_(this->get_buffer_length_());
+  this->display_init_();
 }
 
 void ST7567::display_init_() {
-    ESP_LOGD(TAG, "Initializing ST7567 display...");
+  ESP_LOGD(TAG, "Initializing ST7567 display...");
 
-    this->command(ST7567_BIAS_9);
-    this->command(this->flip_x_ ? ST7567_SEG_REVERSE : ST7567_SEG_NORMAL);
-    this->command(this->flip_y_ ? ST7567_COM_NORMAL : ST7567_COM_REMAP);
-    this->command(ST7567_POWER_CTL | 0x4);
-    this->command(ST7567_POWER_CTL | 0x6);
-    this->command(ST7567_POWER_CTL | 0x7);
+  this->command(ST7567_BIAS_9);
+  this->command(this->flip_x_ ? ST7567_SEG_REVERSE : ST7567_SEG_NORMAL);
+  this->command(this->flip_y_ ? ST7567_COM_NORMAL : ST7567_COM_REMAP);
+  this->command(ST7567_POWER_CTL | 0x4);
+  this->command(ST7567_POWER_CTL | 0x6);
+  this->command(ST7567_POWER_CTL | 0x7);
 
-    //********Adjust display brightness********
-    // this->command(0x25);  // 0x20-0x27 is the internal Rb/Ra resistance
-    //                 // adjustment setting of V5 voltage RR=4.5V
-    this->set_brightness(this->brightness_);
-    this->set_contrast(this->contrast_);
+  //********Adjust display brightness********
+  // this->command(0x25);  // 0x20-0x27 is the internal Rb/Ra resistance
+  //                 // adjustment setting of V5 voltage RR=4.5V
+  this->set_brightness(this->brightness_);
+  this->set_contrast(this->contrast_);
 
-    this->command(ST7567_INVERT_OFF | this->invert_);
+  this->command(ST7567_INVERT_OFF | this->invert_);
 
-    this->command(ST7567_BOOSTER_ON);
-    this->command(ST7567_REGULATOR_ON);
-    this->command(ST7567_POWER_ON);
+  this->command(ST7567_BOOSTER_ON);
+  this->command(ST7567_REGULATOR_ON);
+  this->command(ST7567_POWER_ON);
 
-    this->command(ST7567_SCAN_START_LINE);
-    this->command(ST7567_DISPLAY_NORMAL);
+  this->command(ST7567_SCAN_START_LINE);
+  this->command(ST7567_DISPLAY_NORMAL);
 
-    this->clear();
-    this->write_display_data();
+  this->clear();
+  this->write_display_data();
 
-    this->command(ST7567_DISPLAY_ON);
+  this->command(ST7567_DISPLAY_ON);
 }
 
 void ST7567::update() {
-    this->do_update_();
-    this->write_display_data();
+  this->do_update_();
+  this->write_display_data();
 }
 
 void ST7567::set_all_pixels_on(bool enable) {
-    this->all_pixels_on_ = enable;
-    this->command(ST7567_DISPLAY_NORMAL | this->all_pixels_on_);
+  this->all_pixels_on_ = enable;
+  this->command(ST7567_DISPLAY_NORMAL | this->all_pixels_on_);
 }
 
 void ST7567::set_invert(bool invert) {
-    this->invert_ = invert;
-    this->command(ST7567_INVERT_OFF | this->invert_);
+  this->invert_ = invert;
+  this->command(ST7567_INVERT_OFF | this->invert_);
 }
 
 void ST7567::set_contrast(uint8_t val) {
-    this->contrast_ = val & 0b111111;
-    // 0..63, 26 is normal
+  this->contrast_ = val & 0b111111;
+  // 0..63, 26 is normal
 
-    // two byte command
-    // first byte 0x81
-    // second byte 0-63
+  // two byte command
+  // first byte 0x81
+  // second byte 0-63
 
-    this->command(ST7567_SET_EV_CMD);
-    this->command(this->contrast_);
+  this->command(ST7567_SET_EV_CMD);
+  this->command(this->contrast_);
 }
 
 void ST7567::set_brightness(uint8_t val) {
-    this->brightness_ = val & 0b111;
-    // 0..7, 5 normal
+  this->brightness_ = val & 0b111;
+  // 0..7, 5 normal
 
-    //********Adjust display brightness********
-    // 0x20-0x27 is the internal Rb/Ra resistance
-    // adjustment setting of V5 voltage RR=4.5V
+  //********Adjust display brightness********
+  // 0x20-0x27 is the internal Rb/Ra resistance
+  // adjustment setting of V5 voltage RR=4.5V
 
-    this->command(ST7567_RESISTOR_RATIO | this->brightness_);
+  this->command(ST7567_RESISTOR_RATIO | this->brightness_);
 }
 
 bool ST7567::is_on() { return this->is_on_; }
 
 void ST7567::turn_on() {
-    this->command(ST7567_DISPLAY_ON);
-    this->is_on_ = true;
+  this->command(ST7567_DISPLAY_ON);
+  this->is_on_ = true;
 }
 
 void ST7567::turn_off() {
-    this->command(ST7567_DISPLAY_OFF);
-    this->is_on_ = false;
+  this->command(ST7567_DISPLAY_OFF);
+  this->is_on_ = false;
 }
 
 void ST7567::set_scroll(uint8_t line) {
-    this->start_line_ = line % this->get_height_internal();
-    this->command(esphome::st7567_base::ST7567_SET_START_LINE + this->start_line_);
+  this->start_line_ = line % this->get_height_internal();
+  this->command(esphome::st7567_base::ST7567_SET_START_LINE + this->start_line_);
 }
 
 // 128x64, but memory size 132x64, line starts from 0, but if flipped then from 131, not 127
@@ -104,43 +104,43 @@ int ST7567::get_width_internal() { return 128 + 4; }
 int ST7567::get_height_internal() { return 64; }
 
 size_t ST7567::get_buffer_length_() {
-    return size_t(this->get_width_internal()) * size_t(this->get_height_internal()) / 8u;
+  return size_t(this->get_width_internal()) * size_t(this->get_height_internal()) / 8u;
 }
 
 void HOT ST7567::draw_absolute_pixel_internal(int x, int y, Color color) {
-    // ST7567A has built-in RAM with 132x65 bit capacity which stores the display data.
-    // but only first 128 pixels from each line are shown on screen
-    // if screen got flipped horizontally then it shows last 128 pixels,
-    // so we need to write x coordinate starting from column 4, not column 0
-    x += this->get_offset_x();
+  // ST7567A has built-in RAM with 132x65 bit capacity which stores the display data.
+  // but only first 128 pixels from each line are shown on screen
+  // if screen got flipped horizontally then it shows last 128 pixels,
+  // so we need to write x coordinate starting from column 4, not column 0
+  x += this->get_offset_x();
 
-    if (x >= this->get_width_internal() || x < 0 || y >= this->get_height_internal() || y < 0) {
-        ESP_LOGW(TAG, "Position out of area: %dx%d", x, y);
-        return;
-    }
+  if (x >= this->get_width_internal() || x < 0 || y >= this->get_height_internal() || y < 0) {
+    ESP_LOGW(TAG, "Position out of area: %dx%d", x, y);
+    return;
+  }
 
-    uint16_t pos = x + (y / 8) * this->get_width_internal();
-    uint8_t subpos = y & 0x07;
-    if (color.is_on()) {
-        this->buffer_[pos] |= (1 << subpos);
-    } else {
-        this->buffer_[pos] &= ~(1 << subpos);
-    }
+  uint16_t pos = x + (y / 8) * this->get_width_internal();
+  uint8_t subpos = y & 0x07;
+  if (color.is_on()) {
+    this->buffer_[pos] |= (1 << subpos);
+  } else {
+    this->buffer_[pos] &= ~(1 << subpos);
+  }
 }
 
 void ST7567::fill(Color color) { memset(buffer_, color.is_on() ? 0xFF : 0x00, this->get_buffer_length_()); }
 
 void ST7567::init_reset_() {
-    if (this->reset_pin_ != nullptr) {
-        this->reset_pin_->setup();
-        this->reset_pin_->digital_write(true);
-        delay(1);
-        // Trigger Reset
-        this->reset_pin_->digital_write(false);
-        delay(10);
-        // Wake up
-        this->reset_pin_->digital_write(true);
-    }
+  if (this->reset_pin_ != nullptr) {
+    this->reset_pin_->setup();
+    this->reset_pin_->digital_write(true);
+    delay(1);
+    // Trigger Reset
+    this->reset_pin_->digital_write(false);
+    delay(10);
+    // Wake up
+    this->reset_pin_->digital_write(true);
+  }
 }
 const char *ST7567::model_str_() { return "ST7567 128x64"; }
 
--- esphome/components/st7567_spi/st7567_spi.cpp
+++ esphome/components/st7567_spi/st7567_spi.cpp
@@ -7,57 +7,57 @@ namespace st7567_spi {
 static const char *const TAG = "st7567_spi";
 
 void SPIST7567::setup() {
-    ESP_LOGCONFIG(TAG, "Setting up SPI ST7567 display...");
-    this->spi_setup();
-    this->dc_pin_->setup();
-    if (this->cs_)
-        this->cs_->setup();
+  ESP_LOGCONFIG(TAG, "Setting up SPI ST7567 display...");
+  this->spi_setup();
+  this->dc_pin_->setup();
+  if (this->cs_)
+    this->cs_->setup();
 
-    this->init_reset_();
-    ST7567::setup();
+  this->init_reset_();
+  ST7567::setup();
 }
 
 void SPIST7567::dump_config() {
-    LOG_DISPLAY("", "SPI ST7567", this);
-    ESP_LOGCONFIG(TAG, "  Model: %s", this->model_str_());
-    LOG_PIN("  CS Pin: ", this->cs_);
-    LOG_PIN("  DC Pin: ", this->dc_pin_);
-    LOG_PIN("  Reset Pin: ", this->reset_pin_);
-    ESP_LOGCONFIG(TAG, "  Flip X: %s", YESNO(this->flip_x_));
-    ESP_LOGCONFIG(TAG, "  Flip Y: %s", YESNO(this->flip_y_));
-    ESP_LOGCONFIG(TAG, "  Inverted Color: %s", YESNO(this->invert_));
-    LOG_UPDATE_INTERVAL(this);
+  LOG_DISPLAY("", "SPI ST7567", this);
+  ESP_LOGCONFIG(TAG, "  Model: %s", this->model_str_());
+  LOG_PIN("  CS Pin: ", this->cs_);
+  LOG_PIN("  DC Pin: ", this->dc_pin_);
+  LOG_PIN("  Reset Pin: ", this->reset_pin_);
+  ESP_LOGCONFIG(TAG, "  Flip X: %s", YESNO(this->flip_x_));
+  ESP_LOGCONFIG(TAG, "  Flip Y: %s", YESNO(this->flip_y_));
+  ESP_LOGCONFIG(TAG, "  Inverted Color: %s", YESNO(this->invert_));
+  LOG_UPDATE_INTERVAL(this);
 }
 
 void SPIST7567::command(uint8_t value) {
-    if (this->cs_)
-        this->cs_->digital_write(true);
-    this->dc_pin_->digital_write(false);
-    delay(1);
-    this->enable();
-    if (this->cs_)
-        this->cs_->digital_write(false);
-    this->write_byte(value);
-    if (this->cs_)
-        this->cs_->digital_write(true);
-    this->disable();
+  if (this->cs_)
+    this->cs_->digital_write(true);
+  this->dc_pin_->digital_write(false);
+  delay(1);
+  this->enable();
+  if (this->cs_)
+    this->cs_->digital_write(false);
+  this->write_byte(value);
+  if (this->cs_)
+    this->cs_->digital_write(true);
+  this->disable();
 }
 
 void HOT SPIST7567::write_display_data() {
-    ESP_LOGD(TAG, "write_display_data()");
+  ESP_LOGD(TAG, "write_display_data()");
 
-    this->command(esphome::st7567_base::ST7567_SET_START_LINE);
-    for (uint8_t y = 0; y < (uint8_t) this->get_height_internal() / 8; y++) {
-        this->dc_pin_->digital_write(false);
-        this->command(esphome::st7567_base::ST7567_PAGE_ADDR + y);  // Set Page
-        this->command(esphome::st7567_base::ST7567_COL_ADDR_H);     // Set MSB Column address
-        this->command(esphome::st7567_base::ST7567_COL_ADDR_L);     // Set LSB Column address
-        this->dc_pin_->digital_write(true);
+  this->command(esphome::st7567_base::ST7567_SET_START_LINE);
+  for (uint8_t y = 0; y < (uint8_t) this->get_height_internal() / 8; y++) {
+    this->dc_pin_->digital_write(false);
+    this->command(esphome::st7567_base::ST7567_PAGE_ADDR + y);  // Set Page
+    this->command(esphome::st7567_base::ST7567_COL_ADDR_H);     // Set MSB Column address
+    this->command(esphome::st7567_base::ST7567_COL_ADDR_L);     // Set LSB Column address
+    this->dc_pin_->digital_write(true);
 
-        this->enable();
-        this->write_array(&this->buffer_[y * this->get_width_internal()], this->get_width_internal());
-        this->disable();
-    }
+    this->enable();
+    this->write_array(&this->buffer_[y * this->get_width_internal()], this->get_width_internal());
+    this->disable();
+  }
 }
 
 }  // namespace st7567_spi
