b84741e7b87b50b0200cd844aea4e5a5a0844096
SGE <siemon.geeroms@globis-software.com>
2023-12-18 15:30:32 +0100

    only min max if they are specified

--- esphome/components/touchscreen/touchscreen.cpp
+++ esphome/components/touchscreen/touchscreen.cpp
@@ -118,19 +118,27 @@ void Touchscreen::send_touches_() {
 }
 
 int16_t Touchscreen::normalize_(int16_t val, int16_t min_val, int16_t max_val, bool inverted) {
+
   int16_t ret;
 
+//only normalize when min and max value are specified
+if(min_val && max_val)
+{
   if (val <= min_val) {
     ret = 0;
   } else if (val >= max_val) {
     ret = 0xfff;
   } else {
-    ret = (int16_t) ((int) 0xfff * (val - min_val) / (max_val - min_val));
+      ret = (int16_t) ((int) 0xfff * (val - min_val) / (max_val - min_val));
+    }
   }
+  else
+  {
+    ret = val;
+  }
+    ret = (inverted) ? 0xfff - ret : ret;
 
-  ret = (inverted) ? 0xfff - ret : ret;
-
-  return ret;
+    return ret;
 }
 
 }  // namespace touchscreen
