a10b48a8a400a98bbac7d1b19cb7c4cdd6b44680
SGE <siemon.geeroms@globis-software.com>
2023-12-18 11:45:18 +0100

    debug info touch positions

--- esphome/components/touchscreen/touchscreen.cpp
+++ esphome/components/touchscreen/touchscreen.cpp
@@ -80,6 +80,8 @@ void Touchscreen::set_raw_touch_position_(uint8_t id, int16_t x_raw, int16_t y_r
     tp.y_org = tp.y;
   }
 
+  ESP_LOGD(TAG, "Touch info, x_raw: %d, y_raw: %d, p: %d, x: %d, y: %d", x_raw, y_raw, z_raw, x, y);
+
   this->touches_[id] = tp;
 
   this->is_touched_ = true;
