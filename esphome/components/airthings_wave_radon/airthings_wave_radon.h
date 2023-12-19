e756e0a3033f69098f8b90006a25e50e02dd6867
Will Haines <will@haines.cc>
2023-12-18 21:45:30 -0700

    Refactor to make use of airthings_wave_base class

--- esphome/components/airthings_wave_radon/airthings_wave_radon.h
+++ esphome/components/airthings_wave_radon/airthings_wave_radon.h
@@ -2,50 +2,33 @@
 
 #ifdef USE_ESP32
 
-#include <esp_gattc_api.h>
-#include <algorithm>
-#include <iterator>
-#include "esphome/components/ble_client/ble_client.h"
-#include "esphome/components/esp32_ble_tracker/esp32_ble_tracker.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/core/component.h"
-#include "esphome/core/log.h"
+#include "esphome/components/airthings_wave_base/airthings_wave_base.h"
 
 namespace esphome {
 namespace airthings_wave_radon {
 
+namespace espbt = esphome::esp32_ble_tracker;
+
 static const char *const SERVICE_UUID = "b42e4a8e-ade7-11e4-89d3-123b93f75cba";
 static const char *const CHARACTERISTIC_UUID = "b42e4dcc-ade7-11e4-89d3-123b93f75cba";
+static const char *const ACCESS_CONTROL_POINT_CHARACTERISTIC_UUID = "b42e4dcc-ade7-11e4-89d3-123b93f75cba";
 
-class AirthingsWaveRadon : public PollingComponent, public ble_client::BLEClientNode {
+class AirthingsWaveRadon : public airthings_wave_base::AirthingsWaveBase {
  public:
   AirthingsWaveRadon();
 
   void dump_config() override;
-  void update() override;
 
-  void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
-                           esp_ble_gattc_cb_param_t *param) override;
-
-  void set_temperature(sensor::Sensor *temperature) { temperature_sensor_ = temperature; }
   void set_radon(sensor::Sensor *radon) { radon_sensor_ = radon; }
   void set_radon_long_term(sensor::Sensor *radon_long_term) { radon_long_term_sensor_ = radon_long_term; }
-  void set_humidity(sensor::Sensor *humidity) { humidity_sensor_ = humidity; }
 
  protected:
   bool is_valid_radon_value_(uint16_t radon);
 
-  void read_sensors_(uint8_t *value, uint16_t value_len);
-  void request_read_values_();
+  void read_sensors(uint8_t *raw_value, uint16_t value_len) override;
 
-  sensor::Sensor *temperature_sensor_{nullptr};
   sensor::Sensor *radon_sensor_{nullptr};
   sensor::Sensor *radon_long_term_sensor_{nullptr};
-  sensor::Sensor *humidity_sensor_{nullptr};
-
-  uint16_t handle_;
-  esp32_ble_tracker::ESPBTUUID service_uuid_;
-  esp32_ble_tracker::ESPBTUUID sensors_data_characteristic_uuid_;
 
   struct WaveRadonReadings {
     uint8_t version;
@@ -55,6 +38,7 @@ class AirthingsWaveRadon : public PollingComponent, public ble_client::BLEClient
     uint16_t radon;
     uint16_t radon_lt;
     uint16_t temperature;
+    uint16_t pressure;
   };
 };
 
