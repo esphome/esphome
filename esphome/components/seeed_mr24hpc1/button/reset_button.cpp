2c3f060374a7d93b2e8db7771e791629c19b4ec7
limengdu <37475446+limengdu@users.noreply.github.com>
2023-12-20 15:07:25 +0800

    del files

--- esphome/components/seeed_mr24hpc1/button/reset_button.cpp
+++
@@ -1,9 +0,0 @@
-#include "reset_button.h"
-
-namespace esphome {
-namespace seeed_mr24hpc1 {
-
-void ResetButton::press_action() { this->parent_->set_reset(); }
-
-}  // namespace seeed_mr24hpc1
-}  // namespace esphome
--- esphome/components/seeed_mr24hpc1/number/motion2rest_time_number.cpp
+++
@@ -1,9 +0,0 @@
-#include "motion2rest_time_number.h"
-
-namespace esphome {
-namespace seeed_mr24hpc1 {
-
-void Motion2RestTimeNumber::control(float value) { this->parent_->set_motion_to_rest_time(value); }
-
-}  // namespace seeed_mr24hpc1
-}  // namespace esphome
