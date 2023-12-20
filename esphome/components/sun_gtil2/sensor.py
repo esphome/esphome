fc9f1bd32943a660c737ebf3cc16f60c6e2115f0
Mat931 <49403702+Mat931@users.noreply.github.com>
2023-12-20 21:49:59 +0100

    Remove unused imports

--- esphome/components/sun_gtil2/sensor.py
+++ esphome/components/sun_gtil2/sensor.py
@@ -2,8 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    CONF_ID,
-    CONF_STATE,
     DEVICE_CLASS_VOLTAGE,
     DEVICE_CLASS_POWER,
     DEVICE_CLASS_TEMPERATURE,
