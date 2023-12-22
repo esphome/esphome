d2476f85ecd7f1adf8906ac3fb8c5c7fc9e877d6
Christian Glombek <lorbus@fedoraproject.org>
2023-12-22 20:29:19 +0100

    web_server: Add support for RP2040

--- esphome/components/web_server_base/__init__.py
+++ esphome/components/web_server_base/__init__.py
@@ -36,5 +36,6 @@ async def to_code(config):
             cg.add_library("WiFi", None)
             cg.add_library("FS", None)
             cg.add_library("Update", None)
-        # https://github.com/esphome/ESPAsyncWebServer/blob/master/library.json
-        cg.add_library("esphome/ESPAsyncWebServer-esphome", "3.1.0")
+        # Fork of https://github.com/esphome/ESPAsyncWebServer/blob/master/library.json
+        # that includes https://github.com/esphome/ESPAsyncWebServer/pull/22
+        cg.add_library(None, None, "https://github.com/skilau/ESPAsyncWebServer.git")
