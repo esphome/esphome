0d85a43f9dc96dbe9a8c0a166b3ec8a1e0b27c07
NP v/d Spek <github_mail@lumensoft.nl>
2023-12-25 04:07:38 +0100

    Update duration count

--- esphome/components/rtttl/rtttl.cpp
+++ esphome/components/rtttl/rtttl.cpp
@@ -278,7 +278,6 @@ void Rtttl::loop() {
 #endif
 #ifdef USE_SPEAKER
   if (this->speaker_ != nullptr) {
-
     this->samples_sent_ = 0;
     this->samples_gap_ = 0;
     this->samples_per_wave_ = 0;
@@ -290,9 +289,11 @@ void Rtttl::loop() {
       this->samples_per_wave_ = (this->sample_rate_ << 10) / this->output_freq_;
 
       // make sure there is enough samples to add a full last sinus.
-      uint16_t devision = this->samples_count_ / this->samples_per_wave_, x = this->samples_count_;
-      this->samples_count_ = (devision) * this->samples_per_wave_;
-      ESP_LOGD(TAG, "play time old: %d div: %d new: ", x, devision, this->samples_count_);
+      uint16_t devision = floor((this->samples_count_<<10) / this->samples_per_wave_)+1,
+      uint16_t x = this->samples_count_;
+      this->samples_count_ = (devision * this->samples_per_wave_);
+      ESP_LOGD(TAG, "play time old: %d div: %d new: %d %d", x, devision, this->samples_count_, this->samples_count_ >> 10);
+      this->samples_count_ = this->samples_count_ >> 10;
     }
     // Convert from frequency in Hz to high and low samples in fixed point
   }
