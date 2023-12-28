9876516ce1871bc6c36d4535fbb8ea3818ef209d
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2023-12-28 01:37:07 +0100

    Remove defaults

--- esphome/components/nextion/nextion_commands.cpp
+++ esphome/components/nextion/nextion_commands.cpp
@@ -294,14 +294,14 @@ void Nextion::filled_circle(int center_x, int center_y, int radius, Color color)
                                             display::ColorUtil::color_to_565(color));
 }
 
-void Nextion::qrcode(int x1, int y1, const char *content, int size = 200, uint16_t background_color = 65535, uint16_t foreground_color = 0,
-            int logo_pic = -1, uint8_t border_width = 8) {
+void Nextion::qrcode(int x1, int y1, const char *content, int size, uint16_t background_color, uint16_t foreground_color,
+            int logo_pic, uint8_t border_width) {
   this->add_no_result_to_queue_with_printf_("qrcode", "qrcode %d,%d,%d,%d,%d,%d,%d,\"%s\"", x1, y1, size, background_color,
                                             foreground_color, logo_pic, border_width, content);
 }
 
-void Nextion::qrcode(int x1, int y1, const char *content, int size = 200, Color background_color = Color(255, 255, 255),
-            Color foreground_color = Color(0, 0, 0), int logo_pic = -1, uint8_t border_width = 8) {
+void Nextion::qrcode(int x1, int y1, const char *content, int size, Color background_color,
+            Color foreground_color, int logo_pic, uint8_t border_width) {
   this->add_no_result_to_queue_with_printf_("qrcode", "qrcode %d,%d,%d,%d,%d,%d,%d,\"%s\"", x1, y1, size, display::ColorUtil::color_to_565(background_color),
                                             display::ColorUtil::color_to_565(foreground_color), logo_pic, border_width, content);
 }
