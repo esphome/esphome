6f00c9351cc7018596591a099769417dbe8ee4d1
Tomasz Duda <tomaszduda23@gmail.com>
2024-01-11 11:05:48 +0100

    fix clang

--- esphome/components/ota/ota_component.cpp
+++ esphome/components/ota/ota_component.cpp
@@ -314,8 +314,8 @@ void OTAComponent::handle_() {
       goto error;  // NOLINT(cppcoreguidelines-avoid-goto)
     }
     total += read;
-    for (;size_acknowledged + OTA_BLOCK_SIZE <= total ||
-      (total == ota_size && size_acknowledged < ota_size); size_acknowledged += OTA_BLOCK_SIZE) {
+    for (; size_acknowledged + OTA_BLOCK_SIZE <= total || (total == ota_size && size_acknowledged < ota_size);
+         size_acknowledged += OTA_BLOCK_SIZE) {
       buf[0] = OTA_RESPONSE_CHUNK_OK;
       this->writeall_(buf, 1);
     }
