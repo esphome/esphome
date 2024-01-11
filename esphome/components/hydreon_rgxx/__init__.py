e963f5aea08990e4d292489a8bdc69c6c64a1dcd
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2024-01-11 21:33:59 +1000

    add resolution option

--- esphome/components/hydreon_rgxx/__init__.py
+++ esphome/components/hydreon_rgxx/__init__.py
@@ -6,6 +6,7 @@ DEPENDENCIES = ["uart"]
 
 hydreon_rgxx_ns = cg.esphome_ns.namespace("hydreon_rgxx")
 RGModel = hydreon_rgxx_ns.enum("RGModel")
+RG15Resolution = hydreon_rgxx_ns.enum("RG15Resolution")
 HydreonRGxxComponent = hydreon_rgxx_ns.class_(
     "HydreonRGxxComponent", cg.PollingComponent, uart.UARTDevice
 )
