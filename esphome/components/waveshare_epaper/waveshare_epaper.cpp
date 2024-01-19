b0b3f25e6f0d6f4e5786f6f4978700e287af5a80
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-01-19 13:27:46 +1100

    Remove delay.

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -2277,12 +2277,10 @@ void HOT WaveshareEPaper2P13InDKE::display() {
     this->wait_until_idle_();
 
     // data must be sent again on partial update
-    delay(300);  // NOLINT
     this->command(0x24);
     this->start_data_();
     this->write_array(this->buffer_, this->get_buffer_length_());
     this->end_data_();
-    // delay(300);  // NOLINT
   }
 
   ESP_LOGI(TAG, "Completed e-paper update.");
