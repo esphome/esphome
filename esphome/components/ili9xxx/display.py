c1872f5344c15f05f71af05dc188737d02054f8f
NP v/d Spek <github_mail@lumensoft.nl>
2024-01-20 18:57:32 +0100

    oepsie

--- esphome/components/ili9xxx/display.py
+++ esphome/components/ili9xxx/display.py
@@ -66,7 +66,7 @@ MODELS = {
     "ST7789V": ili9xxx_ns.class_("ILI9XXXST7789V", ILI9XXXDisplay),
     "S3BOX": ili9xxx_ns.class_("ILI9XXXS3Box", ILI9XXXDisplay),
     "S3BOX_LITE": ili9xxx_ns.class_("ILI9XXXS3BoxLite", ILI9XXXDisplay),
-    "CYD_ILI9342": ili9xxx_ns.class_("ILI9XXXCYD_ILI9342", ILI9XXXDisplay),
+    "CYD_ILI9342": ili9xxx_ns.class_("ILI9XXXCYDILI9342", ILI9XXXDisplay),
 }
 
 COLOR_ORDERS = {
