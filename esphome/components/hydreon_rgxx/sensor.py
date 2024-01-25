000b23e342654d24059b93bdd882ef59ba872925
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2024-01-25 10:20:14 +1000

    Update esphome/components/hydreon_rgxx/sensor.py
    Co-authored-by: functionpointer <suspendfunction@gmail.com>

--- esphome/components/hydreon_rgxx/sensor.py
+++ esphome/components/hydreon_rgxx/sensor.py
@@ -83,7 +83,7 @@ CONFIG_SCHEMA = cv.All(
                 upper=True,
                 space="_",
             ),
-            cv.Optional(CONF_RESOLUTION, default="high"): cv.enum(
+            cv.Optional(CONF_RESOLUTION): cv.enum(
                 RG15_RESOLUTION, upper=False
             ),
             cv.Optional(CONF_ACC): sensor.sensor_schema(
