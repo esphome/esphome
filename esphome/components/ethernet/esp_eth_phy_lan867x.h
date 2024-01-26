094b354bbd500786f16bcdaddfa5d23df309841f
Viacheslav Bocharov <adeep@lexina.in>
2024-01-26 15:52:00 +0300

    components/ethernet/esp_eth_phy_lan867x apply clang-format rules

--- esphome/components/ethernet/esp_eth_phy_lan867x.h
+++ esphome/components/ethernet/esp_eth_phy_lan867x.h
@@ -20,23 +20,23 @@ extern "C" {
 #endif
 
 typedef enum {
-    LAN867X_ETH_CMD_S_PLCA = ETH_CMD_CUSTOM_PHY_CMDS,   /*!< Enable or disable PLCA */
-    LAN867X_ETH_CMD_S_PLCA_NCNT,                        /*!< Set PLCA node count */
-    LAN867X_ETH_CMD_G_PLCA_NCNT,                        /*!< Get PLCA node count */
-    LAN867X_ETH_CMD_S_PLCA_ID,                          /*!< Set PLCA ID */
-    LAN867X_ETH_CMD_G_PLCA_ID,                          /*!< Get PLCA ID */
-    LAN768X_ETH_CMD_PLCA_RST                            /*!< Reset PLCA*/
+  LAN867X_ETH_CMD_S_PLCA = ETH_CMD_CUSTOM_PHY_CMDS, /*!< Enable or disable PLCA */
+  LAN867X_ETH_CMD_S_PLCA_NCNT,                      /*!< Set PLCA node count */
+  LAN867X_ETH_CMD_G_PLCA_NCNT,                      /*!< Get PLCA node count */
+  LAN867X_ETH_CMD_S_PLCA_ID,                        /*!< Set PLCA ID */
+  LAN867X_ETH_CMD_G_PLCA_ID,                        /*!< Get PLCA ID */
+  LAN768X_ETH_CMD_PLCA_RST                          /*!< Reset PLCA*/
 } phy_lan867x_custom_io_cmd_t;
 
 /**
-* @brief Create a PHY instance of LAN867x
-*
-* @param[in] config: configuration of PHY
-*
-* @return
-*      - instance: create PHY instance successfully
-*      - NULL: create PHY instance failed because some error occurred
-*/
+ * @brief Create a PHY instance of LAN867x
+ *
+ * @param[in] config: configuration of PHY
+ *
+ * @return
+ *      - instance: create PHY instance successfully
+ *      - NULL: create PHY instance failed because some error occurred
+ */
 esp_eth_phy_t *esp_eth_phy_new_lan867x(const eth_phy_config_t *config);
 
 #ifdef __cplusplus
