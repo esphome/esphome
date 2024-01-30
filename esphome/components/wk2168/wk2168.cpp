5f863cd494af4dafdbd1b44b1057f13dbe5feff3
DrCoolZic <DrCoolZic@hotmail.com>
2024-01-30 18:06:47 +0100

    fix badly placed parenthesis in loop()

--- esphome/components/wk2168/wk2168.cpp
+++ esphome/components/wk2168/wk2168.cpp
@@ -70,6 +70,7 @@ void WK2168Component::test_gpio_output_() {
 void WK2168Component::loop() {
   wk_base::WKBaseComponent::loop();  // we call the loop() method from base class
   if (((this->component_state_ & COMPONENT_STATE_MASK) != COMPONENT_STATE_LOOP) && !test_mode_)
+
     return;
 
 #ifdef TEST_COMPONENT
