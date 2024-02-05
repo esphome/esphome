80c8c0130fe8a6dac7be94e3a631215ee218caed
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2024-02-04 22:05:33 -0800

    Update wled_light_effect.cpp

--- esphome/components/wled/wled_light_effect.cpp
+++ esphome/components/wled/wled_light_effect.cpp
@@ -157,9 +157,9 @@ bool WLEDLightEffect::parse_frame_(light::AddressableLight &it, const uint8_t *p
 bool WLEDLightEffect::parse_notifier_frame_(light::AddressableLight &it, const uint8_t *payload, uint16_t size) {
   
   
-  uint8_t r = payload[3];
-  uint8_t g = payload[4];
-  uint8_t b = payload[5];
+  uint8_t r = payload[1];
+  uint8_t g = payload[2];
+  uint8_t b = payload[3];
   
   it[0].set(Color(r, g, b));
   
