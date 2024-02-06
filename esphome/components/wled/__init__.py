9a3e3365c34d9e2797faa0282881097a6fd3f3c1
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2024-02-05 17:13:22 -0800

    Update __init__.py

--- esphome/components/wled/__init__.py
+++ esphome/components/wled/__init__.py
@@ -8,6 +8,7 @@ wled_ns = cg.esphome_ns.namespace("wled")
 WLEDLightEffect = wled_ns.class_("WLEDLightEffect", AddressableLightEffect)
 
 CONFIG_SCHEMA = cv.All(cv.Schema({}), cv.only_with_arduino)
+CONF_SYNC_GROUP_MASK = "sync_group_mask"
 
 @register_addressable_effect(
     "wled",
@@ -15,7 +16,7 @@ CONFIG_SCHEMA = cv.All(cv.Schema({}), cv.only_with_arduino)
     "WLED",
     {
         cv.Optional(CONF_PORT, default=21324): cv.port,
-        cv.Optional("sync_group_mask", default=0): cv.int_range(min=0, max=255) # 0 matches all, mask sync groups 1-8 with 1 2 4 8 16 32 64 128
+        cv.Optional(CONF_SYNC_GROUP_MASK, default=0): cv.int_range(min=0, max=255) # 0 matches all, mask sync groups 1-8 with 1 2 4 8 16 32 64 128
     },
 )
 async def wled_light_effect_to_code(config, effect_id):
