b9035885ea8b5e655f8660c425cf9c8a3b4b714b
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2024-02-05 18:31:56 -0800

    formatting fix for __init__.py

--- esphome/components/wled/__init__.py
+++ esphome/components/wled/__init__.py
@@ -10,17 +10,18 @@ WLEDLightEffect = wled_ns.class_("WLEDLightEffect", AddressableLightEffect)
 CONFIG_SCHEMA = cv.All(cv.Schema({}), cv.only_with_arduino)
 CONF_SYNC_GROUP_MASK = "sync_group_mask"
 
+
 @register_addressable_effect(
     "wled",
     WLEDLightEffect,
     "WLED",
     {
         cv.Optional(CONF_PORT, default=21324): cv.port,
-        cv.Optional(CONF_SYNC_GROUP_MASK, default=0): cv.int_range(min=0, max=255) # 0 matches all, mask sync groups 1-8 with 1 2 4 8 16 32 64 128
+        cv.Optional(CONF_SYNC_GROUP_MASK, default=0): cv.int_range(min=0, max=255)  # 0 matches all, mask sync groups 1-8 with 1 2 4 8 16 32 64 128
     },
 )
 async def wled_light_effect_to_code(config, effect_id):
     effect = cg.new_Pvariable(effect_id, config[CONF_NAME])
     cg.add(effect.set_port(config[CONF_PORT]))
-    cg.add(effect.set_sync_group_mask(config[CONF_SYNC_GROUP_MASK]));
+    cg.add(effect.set_sync_group_mask(config[CONF_SYNC_GROUP_MASK]))
     return effect
