649c423ed3ca5244610cbfdaadc2d2b2ed9c0c46
descipher <mike.laspina@shaw.ca>
2024-02-10 12:18:38 -0600

    Set delay on cmd reply

--- esphome/components/ld2420/ld2420.cpp
+++ esphome/components/ld2420/ld2420.cpp
@@ -211,10 +211,11 @@ void LD2420Component::factory_reset_action() {
 void LD2420Component::restart_module_action() {
   ESP_LOGCONFIG(TAG, "Restarting LD2420 module...");
   this->send_module_restart();
-  delay_microseconds_safe(45000);
-  this->set_config_mode(true);
-  this->set_system_mode(system_mode_);
-  this->set_config_mode(false);
+  this->set_timeout(250, [this]() {
+    this->set_config_mode(true);
+    this->set_system_mode(system_mode_);
+    this->set_config_mode(false);
+  });
   ESP_LOGCONFIG(TAG, "LD2420 Restarted.");
 }
 
@@ -424,10 +425,6 @@ void LD2420Component::handle_simple_mode_(const uint8_t *inbuf, int len) {
 }
 
 void LD2420Component::handle_ack_data_(uint8_t *buffer, int len) {
-  // This is a workaround to a firmware bug that started in v1.5.4 if we send write registers updates
-  // faster than 22 per second the modules will not respond correctly and will ignore further commands
-  // The symptoms are constant detection at the 35cm range.
-  delay_microseconds_safe(70000);  // Throttle down to no more than 22 LD2420 register writes per second
   this->cmd_reply_.command = buffer[CMD_FRAME_COMMAND];
   this->cmd_reply_.length = buffer[CMD_FRAME_DATA_LENGTH];
   uint8_t reg_element = 0;
@@ -531,18 +528,16 @@ int LD2420Component::send_cmd_from_array(CmdFrameT frame) {
       this->write_byte(cmd_buffer[index]);
     }
 
-    delay_microseconds_safe(500);  // give the module a moment to process it
     error = 0;
     if (frame.command == CMD_RESTART) {
-      delay_microseconds_safe(25000);  // Wait for the restart
-      return 0;                        // restart does not reply exit now
+      return 0;  // restart does not reply exit now
     }
 
     while (!this->cmd_reply_.ack) {
       while (available()) {
         this->readline_(read(), ack_buffer, sizeof(ack_buffer));
       }
-      delay_microseconds_safe(250);
+      delay_microseconds_safe(1450);
       if (loop_count <= 0) {
         error = LD2420_ERROR_TIMEOUT;
         retry--;
