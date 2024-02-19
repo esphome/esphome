7183945ffa4518628c9e80090e66726427f0529b
Alex Boyd <alex@opengroove.org>
2024-02-18 19:34:29 -0700

    [custom_i2c] Initial custom_i2c implementation

---
+++ esphome/components/custom_i2c/constants.py
@@ -0,0 +1,28 @@
+CONF_DEVICE_ID = "device_id"
+CONF_REGISTER_ID = "register_id"
+CONF_DEVICES = "devices"
+CONF_REGISTERS = "registers"
+CONF_READ_DELAY = "read_delay"
+CONF_MESSAGE = "message"
+CONF_ON_SETUP_ID = "on_setup_id"
+CONF_ON_SETUP = "on_setup"
+CONF_BYTES = "bytes"
+CONF_VALUE = "value"
+CONF_TYPE = "type"
+CONF_MESSAGE_BUILDER_ID = "message_builder_id"
+CONF_MESSAGE_BUILDER_KEY = "message_builder_key"
+CONF_CACHE = "cache"
+CONF_PIN_BANKS = "pin_banks"
+# TODO: consider changing to custom_i2c_pin_bank_id since this globally pollutes the `pin: ...` configuration option on
+# any components that use GPIO pins.
+# (That's annoyingly long, though. Alternative: leave as is, and if/when someone else comes along and wants to make a
+# component that follows this "pin/pin bank" model, turn it into a registry so that anyone can register a pin bank type
+# and refer to pins in that pin bank using the `pin_bank_id` config key. Actually I think I prefer this option.)
+# TODO: Also decide if we want to rename this to `pin_bank` to follow the pattern that pin keys don't include `_id` in
+# their names (which per https://discord.com/channels/429907082951524364/1204312566231334992/1204545049908477972 is
+# inconsistent with the rest of esphome, but seems to be a consistent inconsistency across components that provide GPIO
+# pins).
+CONF_PIN_BANK_ID = "pin_bank_id"
+
+KEY_CUSTOM_I2C = "custom_i2c"
+KEY_PIN_BANK_BYTE_COUNTS = "pin_bank_byte_counts"
---
+++ esphome/components/custom_i2c/output.py
@@ -0,0 +1,27 @@
+from esphome.components import output
+import esphome.config_validation as cv
+import esphome.codegen as cg
+from esphome.const import CONF_ID
+
+from .. import custom_i2c
+from .constants import CONF_REGISTER_ID, CONF_BYTES
+
+CustomI2COutput = custom_i2c.custom_i2c_ns.class_(
+    "CustomI2COutput", output.FloatOutput, cg.Component
+)
+
+CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
+    {
+        cv.Required(CONF_ID): cv.declare_id(CustomI2COutput),
+        cv.Required(CONF_REGISTER_ID): cv.use_id(custom_i2c.CustomI2CRegister),
+        cv.Optional(CONF_BYTES, 1): cv.int_range(1, 4),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID], cg.TemplateArguments(config[CONF_BYTES]))
+    await cg.register_component(var, config)
+    await output.register_output(var, config)
+
+    cg.add(var.set_register(await cg.get_variable(config[CONF_REGISTER_ID])))
