7e9590ec1e20a37804c1be24d2b3a8a455d18847
DrCoolZic <DrCoolZic@hotmail.com>
2024-02-29 23:06:49 +0100

    wk2204 wk2212 spi/i2c
    also added owner in init for weika i2c et spi
    On branch wk2132-split1
    Your branch is up to date with 'origin/wk2132-split1'.
    
    Changes to be committed:
    	modified:   CODEOWNERS
    	modified:   esphome/components/weikai_i2c/__init__.py
    	modified:   esphome/components/weikai_spi/__init__.py
    	modified:   esphome/components/wk2204_i2c/__init__.py
    	modified:   esphome/components/wk2204_spi/__init__.py
    	modified:   esphome/components/wk2212_i2c/__init__.py
    	modified:   esphome/components/wk2212_spi/__init__.py

--- esphome/components/wk2204_i2c/__init__.py
+++ esphome/components/wk2204_i2c/__init__.py
@@ -1,15 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, weikai
+from esphome.components import i2c, weikai, weikai_i2c
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@DrCoolZic"]
 DEPENDENCIES = ["i2c"]
-AUTO_LOAD = ["weikai"]
+AUTO_LOAD = ["weikai", "weikai_i2c"]
 MULTI_CONF = True
 
-weikai_ns = cg.esphome_ns.namespace("weikai")
-WeikaiComponentI2C = weikai_ns.class_(
+weikai_i2c_ns = cg.esphome_ns.namespace("weikai_i2c")
+WeikaiComponentI2C = weikai_i2c_ns.class_(
     "WeikaiComponentI2C", weikai.WeikaiComponent, i2c.I2CDevice
 )
 
@@ -25,9 +25,6 @@ CONFIG_SCHEMA = cv.All(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    cg.add_build_flag("-DI2C_BUFFER_LENGTH=255")
-    cg.add_build_flag("-DUSE_I2C_BUS")
-    cg.add_build_flag("-DHAS_GPIO_PIN")
     cg.add(var.set_name(str(config[CONF_ID])))
     await weikai.register_weikai(var, config)
     await i2c.register_i2c_device(var, config)
