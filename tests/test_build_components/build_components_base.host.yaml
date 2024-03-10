esphome/esphome

commit de2d5a65b5bf30a8f665af7c06a28596d57afc0a

Separate logger implementations for each hardware platform into different files (#6167)
Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>
Co-authored-by: Keith Burzinski <kbx81x@gmail.com>

esphome/components/logger/__init__.py
@@ -127,6 +127,10 @@ def uart_selection(value):
         if CORE.using_arduino and value.upper() in ESP_ARDUINO_UNSUPPORTED_USB_UARTS:
             raise cv.Invalid(f"Arduino framework does not support {value}.")
         variant = get_esp32_variant()
+        if CORE.using_esp_idf and variant == VARIANT_ESP32C3 and value == USB_CDC:
+            raise cv.Invalid(
+                f"{value} is not supported for variant {variant} when using ESP-IDF."
+            )
         if variant in UART_SELECTION_ESP32:
             return cv.one_of(*UART_SELECTION_ESP32[variant], upper=True)(value)
     if CORE.is_esp8266:
@@ -274,6 +278,16 @@ async def to_code(config):
             add_idf_sdkconfig_option("CONFIG_ESP_CONSOLE_USB_CDC", True)
         elif config[CONF_HARDWARE_UART] == USB_SERIAL_JTAG:
             add_idf_sdkconfig_option("CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG", True)
+    try:
+        uart_selection(USB_SERIAL_JTAG)
+        cg.add_define("USE_LOGGER_USB_SERIAL_JTAG")
+    except cv.Invalid:
+        pass
+    try:
+        uart_selection(USB_CDC)
+        cg.add_define("USE_LOGGER_USB_CDC")
+    except cv.Invalid:
+        pass
 
     # Register at end for safe mode
     await cg.register_component(log, config)

esphome/components/logger/logger.cpp
@@ -1,28 +1,6 @@
 #include "logger.h"
 #include <cinttypes>
 
-#ifdef USE_ESP_IDF
-#include <driver/uart.h>
-
-#if defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32C6) || defined(USE_ESP32_VARIANT_ESP32S3) || \
-    defined(USE_ESP32_VARIANT_ESP32H2)
-#include <driver/usb_serial_jtag.h>
-#include <esp_vfs_dev.h>
-#include <esp_vfs_usb_serial_jtag.h>
-#endif
-
-#include "freertos/FreeRTOS.h"
-#include "esp_idf_version.h"
-
-#include <cstdint>
-#include <cstdio>
-#include <fcntl.h>
-
-#endif  // USE_ESP_IDF
-
-#if defined(USE_ESP32_FRAMEWORK_ARDUINO) || defined(USE_ESP_IDF)
-#include <esp_log.h>
-#endif  // USE_ESP32_FRAMEWORK_ARDUINO || USE_ESP_IDF
 #include "esphome/core/hal.h"
 #include "esphome/core/log.h"
 
@@ -106,58 +84,6 @@ void Logger::log_vprintf_(int level, const char *tag, int line, const __FlashStr
 }
 #endif
 
-#ifdef USE_ESP_IDF
-void Logger::init_uart_() {
-  uart_config_t uart_config{};
-  uart_config.baud_rate = (int) baud_rate_;
-  uart_config.data_bits = UART_DATA_8_BITS;
-  uart_config.parity = UART_PARITY_DISABLE;
-  uart_config.stop_bits = UART_STOP_BITS_1;
-  uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
-  uart_config.source_clk = UART_SCLK_DEFAULT;
-#endif
-  uart_param_config(this->uart_num_, &uart_config);
-  const int uart_buffer_size = tx_buffer_size_;
-  // Install UART driver using an event queue here
-  uart_driver_install(this->uart_num_, uart_buffer_size, uart_buffer_size, 10, nullptr, 0);
-}
-
-#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3)
-void Logger::init_usb_cdc_() {}
-#endif
-
-#if defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32C6) || defined(USE_ESP32_VARIANT_ESP32S3) || \
-    defined(USE_ESP32_VARIANT_ESP32H2)
-void Logger::init_usb_serial_jtag_() {
-  setvbuf(stdin, NULL, _IONBF, 0);  // Disable buffering on stdin
-
-  // Minicom, screen, idf_monitor send CR when ENTER key is pressed
-  esp_vfs_dev_usb_serial_jtag_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
-  // Move the caret to the beginning of the next line on '\n'
-  esp_vfs_dev_usb_serial_jtag_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
-
-  // Enable non-blocking mode on stdin and stdout
-  fcntl(fileno(stdout), F_SETFL, 0);
-  fcntl(fileno(stdin), F_SETFL, 0);
-
-  usb_serial_jtag_driver_config_t usb_serial_jtag_config{};
-  usb_serial_jtag_config.rx_buffer_size = 512;
-  usb_serial_jtag_config.tx_buffer_size = 512;
-
-  esp_err_t ret = ESP_OK;
-  // Install USB-SERIAL-JTAG driver for interrupt-driven reads and writes
-  ret = usb_serial_jtag_driver_install(&usb_serial_jtag_config);
-  if (ret != ESP_OK) {
-    return;
-  }
-
-  // Tell vfs to use usb-serial-jtag driver
-  esp_vfs_usb_serial_jtag_use_driver();
-}
-#endif
-#endif
-
 int HOT Logger::level_for(const char *tag) {
   // Uses std::vector<> for low memory footprint, though the vector
   // could be sorted to minimize lookup times. This feature isn't used that
@@ -169,6 +95,7 @@ int HOT Logger::level_for(const char *tag) {
   }
   return ESPHOME_LOG_LEVEL;
 }
+
 void HOT Logger::log_message_(int level, const char *tag, int offset) {
   // remove trailing newline
   if (this->tx_buffer_[this->tx_buffer_at_ - 1] == '\n') {
@@ -178,28 +105,9 @@ void HOT Logger::log_message_(int level, const char *tag, int offset) {
   this->set_null_terminator_();
 
   const char *msg = this->tx_buffer_ + offset;
+
   if (this->baud_rate_ > 0) {
-#ifdef USE_ARDUINO
-    this->hw_serial_->println(msg);
-#endif  // USE_ARDUINO
-#ifdef USE_ESP_IDF
-    if (
-#if defined(USE_ESP32_VARIANT_ESP32S2)
-        this->uart_ == UART_SELECTION_USB_CDC
-#elif defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32C6) || defined(USE_ESP32_VARIANT_ESP32H2)
-        this->uart_ == UART_SELECTION_USB_SERIAL_JTAG
-#elif defined(USE_ESP32_VARIANT_ESP32S3)
-        this->uart_ == UART_SELECTION_USB_CDC || this->uart_ == UART_SELECTION_USB_SERIAL_JTAG
-#else
-        /* DISABLES CODE */ (false)  // NOLINT
-#endif
-    ) {
-      puts(msg);
-    } else {
-      uart_write_bytes(this->uart_num_, msg, strlen(msg));
-      uart_write_bytes(this->uart_num_, "\n", 1);
-    }
-#endif
+    this->write_msg_(msg);
   }
 
 #ifdef USE_ESP32
@@ -211,17 +119,6 @@ void HOT Logger::log_message_(int level, const char *tag, int offset) {
   if (xPortGetFreeHeapSize() < 2048)
     return;
 #endif
-#ifdef USE_HOST
-  time_t rawtime;
-  struct tm *timeinfo;
-  char buffer[80];
-
-  time(&rawtime);
-  timeinfo = localtime(&rawtime);
-  strftime(buffer, sizeof buffer, "[%H:%M:%S]", timeinfo);
-  fputs(buffer, stdout);
-  puts(msg);
-#endif
 
   this->log_callback_.call(level, tag, msg);
 }
@@ -230,179 +127,6 @@ Logger::Logger(uint32_t baud_rate, size_t tx_buffer_size) : baud_rate_(baud_rate
   // add 1 to buffer size for null terminator
   this->tx_buffer_ = new char[this->tx_buffer_size_ + 1];  // NOLINT
 }
-
-#ifndef USE_LIBRETINY
-void Logger::pre_setup() {
-  if (this->baud_rate_ > 0) {
-#ifdef USE_ARDUINO
-    switch (this->uart_) {
-      case UART_SELECTION_UART0:
-#ifdef USE_ESP8266
-      case UART_SELECTION_UART0_SWAP:
-#endif
-#ifdef USE_RP2040
-        this->hw_serial_ = &Serial1;
-        Serial1.begin(this->baud_rate_);
-#else
-#if ARDUINO_USB_CDC_ON_BOOT
-        this->hw_serial_ = &Serial0;
-        Serial0.begin(this->baud_rate_);
-#else
-        this->hw_serial_ = &Serial;
-        Serial.begin(this->baud_rate_);
-#endif
-#endif
-#ifdef USE_ESP8266
-        if (this->uart_ == UART_SELECTION_UART0_SWAP) {
-          Serial.swap();
-        }
-        Serial.setDebugOutput(ESPHOME_LOG_LEVEL >= ESPHOME_LOG_LEVEL_VERBOSE);
-#endif
-        break;
-      case UART_SELECTION_UART1:
-#ifdef USE_RP2040
-        this->hw_serial_ = &Serial2;
-        Serial2.begin(this->baud_rate_);
-#else
-        this->hw_serial_ = &Serial1;
-        Serial1.begin(this->baud_rate_);
-#endif
-#ifdef USE_ESP8266
-        Serial1.setDebugOutput(ESPHOME_LOG_LEVEL >= ESPHOME_LOG_LEVEL_VERBOSE);
-#endif
-        break;
-#if defined(USE_ESP32) && !defined(USE_ESP32_VARIANT_ESP32C3) && !defined(USE_ESP32_VARIANT_ESP32C6) && \
-    !defined(USE_ESP32_VARIANT_ESP32S2) && !defined(USE_ESP32_VARIANT_ESP32S3)
-      case UART_SELECTION_UART2:
-        this->hw_serial_ = &Serial2;
-        Serial2.begin(this->baud_rate_);
-        break;
-#endif
-#if defined(USE_ESP32) && \
-    (defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3) || defined(USE_ESP32_VARIANT_ESP32C3))
-#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3) || defined(USE_ESP32_VARIANT_ESP32C3)
-      case UART_SELECTION_USB_CDC:
-#endif  // USE_ESP32_VARIANT_ESP32S2 || USE_ESP32_VARIANT_ESP32S3 || USE_ESP32_VARIANT_ESP32C3
-#if defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32S3)
-      case UART_SELECTION_USB_SERIAL_JTAG:
-#endif  // USE_ESP32_VARIANT_ESP32C3 || USE_ESP32_VARIANT_ESP32S3
-#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3) || defined(USE_ESP32_VARIANT_ESP32C3)
-#if ARDUINO_USB_CDC_ON_BOOT
-        this->hw_serial_ = &Serial;
-        Serial.setTxTimeoutMs(0);  // workaround for 2.0.9 crash when there's no data connection
-        Serial.begin(this->baud_rate_);
-#else
-        this->hw_serial_ = &Serial;
-        Serial.begin(this->baud_rate_);
-#endif  // ARDUINO_USB_CDC_ON_BOOT
-#endif  // USE_ESP32_VARIANT_ESP32S2 || USE_ESP32_VARIANT_ESP32S3 || USE_ESP32_VARIANT_ESP32C3
-        break;
-#endif  // USE_ESP32 && (USE_ESP32_VARIANT_ESP32S2 || USE_ESP32_VARIANT_ESP32S3 || USE_ESP32_VARIANT_ESP32C3)
-#ifdef USE_RP2040
-      case UART_SELECTION_USB_CDC:
-        this->hw_serial_ = &Serial;
-        Serial.begin(this->baud_rate_);
-        break;
-#endif  // USE_RP2040
-    }
-#endif  // USE_ARDUINO
-#ifdef USE_ESP_IDF
-    this->uart_num_ = UART_NUM_0;
-    switch (this->uart_) {
-      case UART_SELECTION_UART0:
-        this->uart_num_ = UART_NUM_0;
-        break;
-      case UART_SELECTION_UART1:
-        this->uart_num_ = UART_NUM_1;
-        break;
-#if !defined(USE_ESP32_VARIANT_ESP32C3) && !defined(USE_ESP32_VARIANT_ESP32C6) && \
-    !defined(USE_ESP32_VARIANT_ESP32S2) && !defined(USE_ESP32_VARIANT_ESP32S3) && !defined(USE_ESP32_VARIANT_ESP32H2)
-      case UART_SELECTION_UART2:
-        this->uart_num_ = UART_NUM_2;
-        break;
-#endif  // !USE_ESP32_VARIANT_ESP32C3 && !USE_ESP32_VARIANT_ESP32S2 && !USE_ESP32_VARIANT_ESP32S3 &&
-        // !USE_ESP32_VARIANT_ESP32H2
-#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3)
-      case UART_SELECTION_USB_CDC:
-        this->uart_num_ = -1;
-        this->init_usb_cdc_();
-        break;
-#endif  // USE_ESP32_VARIANT_ESP32S2 || USE_ESP32_VARIANT_ESP32S3
-#if defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32C6) || defined(USE_ESP32_VARIANT_ESP32S3) || \
-    defined(USE_ESP32_VARIANT_ESP32H2)
-      case UART_SELECTION_USB_SERIAL_JTAG:
-        this->uart_num_ = -1;
-        this->init_usb_serial_jtag_();
-        break;
-#endif  // USE_ESP32_VARIANT_ESP32C3 || USE_ESP32_VARIANT_ESP32C6 || USE_ESP32_VARIANT_ESP32S3 ||
-        // USE_ESP32_VARIANT_ESP32H2
-    }
-    if (this->uart_num_ >= 0) {
-      this->init_uart_();
-    }
-#endif  // USE_ESP_IDF
-  }
-#ifdef USE_ESP8266
-  else {
-    uart_set_debug(UART_NO);
-  }
-#endif  // USE_ESP8266
-
-  global_logger = this;
-#if defined(USE_ESP_IDF) || defined(USE_ESP32_FRAMEWORK_ARDUINO)
-  esp_log_set_vprintf(esp_idf_log_vprintf_);
-  if (ESPHOME_LOG_LEVEL >= ESPHOME_LOG_LEVEL_VERBOSE) {
-    esp_log_level_set("*", ESP_LOG_VERBOSE);
-  }
-#endif  // USE_ESP_IDF || USE_ESP32_FRAMEWORK_ARDUINO
-
-  ESP_LOGI(TAG, "Log initialized");
-}
-#else  // USE_LIBRETINY
-void Logger::pre_setup() {
-  if (this->baud_rate_ > 0) {
-    switch (this->uart_) {
-#if LT_HW_UART0
-      case UART_SELECTION_UART0:
-        this->hw_serial_ = &Serial0;
-        Serial0.begin(this->baud_rate_);
-        break;
-#endif
-#if LT_HW_UART1
-      case UART_SELECTION_UART1:
-        this->hw_serial_ = &Serial1;
-        Serial1.begin(this->baud_rate_);
-        break;
-#endif
-#if LT_HW_UART2
-      case UART_SELECTION_UART2:
-        this->hw_serial_ = &Serial2;
-        Serial2.begin(this->baud_rate_);
-        break;
-#endif
-      default:
-        this->hw_serial_ = &Serial;
-        Serial.begin(this->baud_rate_);
-        if (this->uart_ != UART_SELECTION_DEFAULT) {
-          ESP_LOGW(TAG, "  The chosen logger UART port is not available on this board."
-                        "The default port was used instead.");
-        }
-        break;
-    }
-
-    // change lt_log() port to match default Serial
-    if (this->uart_ == UART_SELECTION_DEFAULT) {
-      this->uart_ = (UARTSelection) (LT_UART_DEFAULT_SERIAL + 1);
-      lt_log_set_port(LT_UART_DEFAULT_SERIAL);
-    } else {
-      lt_log_set_port(this->uart_ - 1);
-    }
-  }
-
-  global_logger = this;
-  ESP_LOGI(TAG, "Log initialized");
-}
-#endif  // USE_LIBRETINY
 
 void Logger::set_baud_rate(uint32_t baud_rate) { this->baud_rate_ = baud_rate; }
 void Logger::set_log_level(const std::string &tag, int log_level) {
@@ -418,38 +142,12 @@ void Logger::add_on_log_callback(std::function<void(int, const char *, const cha
 }
 float Logger::get_setup_priority() const { return setup_priority::BUS + 500.0f; }
 const char *const LOG_LEVELS[] = {"NONE", "ERROR", "WARN", "INFO", "CONFIG", "DEBUG", "VERBOSE", "VERY_VERBOSE"};
-#ifdef USE_ESP32
-const char *const UART_SELECTIONS[] = {
-    "UART0",           "UART1",
-#if !defined(USE_ESP32_VARIANT_ESP32C3) && !defined(USE_ESP32_VARIANT_ESP32C6) && \
-    !defined(USE_ESP32_VARIANT_ESP32S2) && !defined(USE_ESP32_VARIANT_ESP32S3) && !defined(USE_ESP32_VARIANT_ESP32H2)
-    "UART2",
-#endif  // !USE_ESP32_VARIANT_ESP32C3 && !USE_ESP32_VARINT_ESP32C6 && !USE_ESP32_VARIANT_ESP32S2 &&
-        // !USE_ESP32_VARIANT_ESP32S3 && !USE_ESP32_VARIANT_ESP32H2
-#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3)
-    "USB_CDC",
-#endif  // USE_ESP32_VARIANT_ESP32S2 || USE_ESP32_VARIANT_ESP32S3
-#if defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32S3)
-    "USB_SERIAL_JTAG",
-#endif  // USE_ESP32_VARIANT_ESP32C3 || USE_ESP32_VARIANT_ESP32S3
-};
-#endif  // USE_ESP32
-#ifdef USE_ESP8266
-const char *const UART_SELECTIONS[] = {"UART0", "UART1", "UART0_SWAP"};
-#endif  // USE_ESP8266
-#ifdef USE_RP2040
-const char *const UART_SELECTIONS[] = {"UART0", "UART1", "USB_CDC"};
-#endif  // USE_RP2040
-#ifdef USE_LIBRETINY
-const char *const UART_SELECTIONS[] = {"DEFAULT", "UART0", "UART1", "UART2"};
-#endif  // USE_LIBRETINY
+
 void Logger::dump_config() {
   ESP_LOGCONFIG(TAG, "Logger:");
   ESP_LOGCONFIG(TAG, "  Level: %s", LOG_LEVELS[ESPHOME_LOG_LEVEL]);
   ESP_LOGCONFIG(TAG, "  Log Baud Rate: %" PRIu32, this->baud_rate_);
-#if defined(USE_ESP32) || defined(USE_ESP8266) || defined(USE_RP2040) || defined(USE_LIBRETINY)
-  ESP_LOGCONFIG(TAG, "  Hardware UART: %s", UART_SELECTIONS[this->uart_]);
-#endif
+  ESP_LOGCONFIG(TAG, "  Hardware UART: %s", get_uart_selection_());
 
   for (auto &it : this->log_levels_) {
     ESP_LOGCONFIG(TAG, "  Level for '%s': %s", it.tag.c_str(), LOG_LEVELS[it.level]);

esphome/components/logger/logger.h
@@ -34,34 +34,22 @@ enum UARTSelection {
 #ifdef USE_LIBRETINY
   UART_SELECTION_DEFAULT = 0,
   UART_SELECTION_UART0,
-  UART_SELECTION_UART1,
-  UART_SELECTION_UART2,
 #else
   UART_SELECTION_UART0 = 0,
+#endif
   UART_SELECTION_UART1,
-#if defined(USE_ESP32)
-#if !defined(USE_ESP32_VARIANT_ESP32C3) && !defined(USE_ESP32_VARIANT_ESP32C6) && \
-    !defined(USE_ESP32_VARIANT_ESP32S2) && !defined(USE_ESP32_VARIANT_ESP32S3) && !defined(USE_ESP32_VARIANT_ESP32H2)
+#if defined(USE_LIBRETINY) || defined(USE_ESP32_VARIANT_ESP32)
   UART_SELECTION_UART2,
-#endif  // !USE_ESP32_VARIANT_ESP32C3 && !USE_ESP32_VARIANT_ESP32C6 && !USE_ESP32_VARIANT_ESP32S2 &&
-        // !USE_ESP32_VARIANT_ESP32S3 && !USE_ESP32_VARIANT_ESP32H2
-#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3) || \
-    (defined(USE_ESP32_VARIANT_ESP32C3) && defined(USE_ARDUINO))
+#endif
+#ifdef USE_LOGGER_USB_CDC
   UART_SELECTION_USB_CDC,
-#endif  // USE_ESP32_VARIANT_ESP32S2 || USE_ESP32_VARIANT_ESP32S3 || USE_ESP32_VARIANT_ESP32C3
-#if defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32C6) || defined(USE_ESP32_VARIANT_ESP32S3) || \
-    defined(USE_ESP32_VARIANT_ESP32H2)
+#endif
+#ifdef USE_LOGGER_USB_SERIAL_JTAG
   UART_SELECTION_USB_SERIAL_JTAG,
-#endif  // USE_ESP32_VARIANT_ESP32C3 || USE_ESP32_VARIANT_ESP32C6 || USE_ESP32_VARIANT_ESP32S3 ||
-        // USE_ESP32_VARIANT_ESP32H2
-#endif  // USE_ESP32
+#endif
 #ifdef USE_ESP8266
   UART_SELECTION_UART0_SWAP,
 #endif  // USE_ESP8266
-#ifdef USE_RP2040
-  UART_SELECTION_USB_CDC,
-#endif  // USE_RP2040
-#endif  // USE_LIBRETINY
 };
 #endif  // USE_ESP32 || USE_ESP8266 || USE_RP2040 || USE_LIBRETINY
 
@@ -106,19 +94,10 @@ class Logger : public Component {
 #endif
 
  protected:
-#ifdef USE_ESP_IDF
-  void init_uart_();
-#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3)
-  void init_usb_cdc_();
-#endif
-#if defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32C6) || defined(USE_ESP32_VARIANT_ESP32S3) || \
-    defined(USE_ESP32_VARIANT_ESP32H2)
-  void init_usb_serial_jtag_();
-#endif
-#endif
   void write_header_(int level, const char *tag, int line);
   void write_footer_();
   void log_message_(int level, const char *tag, int offset = 0);
+  void write_msg_(const char *msg);
 
   inline bool is_buffer_full_() const { return this->tx_buffer_at_ >= this->tx_buffer_size_; }
   inline int buffer_remaining_capacity_() const { return this->tx_buffer_size_ - this->tx_buffer_at_; }
@@ -157,6 +136,8 @@ class Logger : public Component {
     this->vprintf_to_buffer_(format, arg);
     va_end(arg);
   }
+
+  const char *get_uart_selection_();
 
   uint32_t baud_rate_;
   char *tx_buffer_{nullptr};

esphome/components/logger/logger_host.cpp
@@ -1,0 +1,22 @@
+#if defined(USE_HOST)
+#include "logger.h"
+
+namespace esphome {
+namespace logger {
+
+void HOT Logger::write_msg_(const char *msg) {
+  time_t rawtime;
+  struct tm *timeinfo;
+  char buffer[80];
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  strftime(buffer, sizeof buffer, "[%H:%M:%S]", timeinfo);
+  fputs(buffer, stdout);
+  puts(msg);
+}
+
+}  // namespace logger
+}  // namespace esphome
+
+#endif

esphome/components/logger/logger_esp32.cpp
@@ -1,0 +1,201 @@
+#ifdef USE_ESP32
+#include "logger.h"
+
+#if defined(USE_ESP32_FRAMEWORK_ARDUINO) || defined(USE_ESP_IDF)
+#include <esp_log.h>
+#endif  // USE_ESP32_FRAMEWORK_ARDUINO || USE_ESP_IDF
+
+#ifdef USE_ESP_IDF
+#include <driver/uart.h>
+
+#ifdef USE_LOGGER_USB_SERIAL_JTAG
+#include <driver/usb_serial_jtag.h>
+#include <esp_vfs_dev.h>
+#include <esp_vfs_usb_serial_jtag.h>
+#endif
+
+#include "freertos/FreeRTOS.h"
+#include "esp_idf_version.h"
+
+#include <cstdint>
+#include <cstdio>
+#include <fcntl.h>
+
+#endif  // USE_ESP_IDF
+
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace logger {
+
+static const char *const TAG = "logger";
+
+#ifdef USE_ESP_IDF
+
+#ifdef USE_LOGGER_USB_SERIAL_JTAG
+static void init_usb_serial_jtag_() {
+  setvbuf(stdin, NULL, _IONBF, 0);  // Disable buffering on stdin
+
+  // Minicom, screen, idf_monitor send CR when ENTER key is pressed
+  esp_vfs_dev_usb_serial_jtag_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+  // Move the caret to the beginning of the next line on '\n'
+  esp_vfs_dev_usb_serial_jtag_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+
+  // Enable non-blocking mode on stdin and stdout
+  fcntl(fileno(stdout), F_SETFL, 0);
+  fcntl(fileno(stdin), F_SETFL, 0);
+
+  usb_serial_jtag_driver_config_t usb_serial_jtag_config{};
+  usb_serial_jtag_config.rx_buffer_size = 512;
+  usb_serial_jtag_config.tx_buffer_size = 512;
+
+  esp_err_t ret = ESP_OK;
+  // Install USB-SERIAL-JTAG driver for interrupt-driven reads and writes
+  ret = usb_serial_jtag_driver_install(&usb_serial_jtag_config);
+  if (ret != ESP_OK) {
+    return;
+  }
+
+  // Tell vfs to use usb-serial-jtag driver
+  esp_vfs_usb_serial_jtag_use_driver();
+}
+#endif
+
+void init_uart(uart_port_t uart_num, uint32_t baud_rate, int tx_buffer_size) {
+  uart_config_t uart_config{};
+  uart_config.baud_rate = (int) baud_rate;
+  uart_config.data_bits = UART_DATA_8_BITS;
+  uart_config.parity = UART_PARITY_DISABLE;
+  uart_config.stop_bits = UART_STOP_BITS_1;
+  uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+  uart_config.source_clk = UART_SCLK_DEFAULT;
+#endif
+  uart_param_config(uart_num, &uart_config);
+  const int uart_buffer_size = tx_buffer_size;
+  // Install UART driver using an event queue here
+  uart_driver_install(uart_num, uart_buffer_size, uart_buffer_size, 10, nullptr, 0);
+}
+
+#endif  // USE_ESP_IDF
+
+void Logger::pre_setup() {
+  if (this->baud_rate_ > 0) {
+#ifdef USE_ARDUINO
+    switch (this->uart_) {
+      case UART_SELECTION_UART0:
+#if ARDUINO_USB_CDC_ON_BOOT
+        this->hw_serial_ = &Serial0;
+        Serial0.begin(this->baud_rate_);
+#else
+        this->hw_serial_ = &Serial;
+        Serial.begin(this->baud_rate_);
+#endif
+        break;
+      case UART_SELECTION_UART1:
+        this->hw_serial_ = &Serial1;
+        Serial1.begin(this->baud_rate_);
+        break;
+#ifdef USE_ESP32_VARIANT_ESP32
+      case UART_SELECTION_UART2:
+        this->hw_serial_ = &Serial2;
+        Serial2.begin(this->baud_rate_);
+        break;
+#endif
+
+#ifdef USE_LOGGER_USB_CDC
+      case UART_SELECTION_USB_CDC:
+        this->hw_serial_ = &Serial;
+#if ARDUINO_USB_CDC_ON_BOOT
+        Serial.setTxTimeoutMs(0);  // workaround for 2.0.9 crash when there's no data connection
+#endif
+        Serial.begin(this->baud_rate_);
+        break;
+#endif
+    }
+#endif  // USE_ARDUINO
+
+#ifdef USE_ESP_IDF
+    this->uart_num_ = UART_NUM_0;
+    switch (this->uart_) {
+      case UART_SELECTION_UART0:
+        this->uart_num_ = UART_NUM_0;
+        break;
+      case UART_SELECTION_UART1:
+        this->uart_num_ = UART_NUM_1;
+        break;
+#ifdef USE_ESP32_VARIANT_ESP32
+      case UART_SELECTION_UART2:
+        this->uart_num_ = UART_NUM_2;
+        break;
+#endif
+#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3)
+      case UART_SELECTION_USB_CDC:
+        this->uart_num_ = -1;
+        break;
+#endif
+#ifdef USE_LOGGER_USB_SERIAL_JTAG
+      case UART_SELECTION_USB_SERIAL_JTAG:
+        this->uart_num_ = -1;
+        init_usb_serial_jtag_();
+        break;
+#endif
+    }
+    if (this->uart_num_ >= 0) {
+      init_uart(this->uart_num_, baud_rate_, tx_buffer_size_);
+    }
+#endif  // USE_ESP_IDF
+  }
+
+  global_logger = this;
+#if defined(USE_ESP_IDF) || defined(USE_ESP32_FRAMEWORK_ARDUINO)
+  esp_log_set_vprintf(esp_idf_log_vprintf_);
+  if (ESPHOME_LOG_LEVEL >= ESPHOME_LOG_LEVEL_VERBOSE) {
+    esp_log_level_set("*", ESP_LOG_VERBOSE);
+  }
+#endif  // USE_ESP_IDF || USE_ESP32_FRAMEWORK_ARDUINO
+
+  ESP_LOGI(TAG, "Log initialized");
+}
+
+#ifdef USE_ESP_IDF
+void HOT Logger::write_msg_(const char *msg) {
+  if (
+#if defined(USE_ESP32_VARIANT_ESP32S2)
+      this->uart_ == UART_SELECTION_USB_CDC
+#elif defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32C6) || defined(USE_ESP32_VARIANT_ESP32H2)
+      this->uart_ == UART_SELECTION_USB_SERIAL_JTAG
+#elif defined(USE_ESP32_VARIANT_ESP32S3)
+      this->uart_ == UART_SELECTION_USB_CDC || this->uart_ == UART_SELECTION_USB_SERIAL_JTAG
+#else
+      /* DISABLES CODE */ (false)  // NOLINT
+#endif
+  ) {
+    puts(msg);
+  } else {
+    uart_write_bytes(this->uart_num_, msg, strlen(msg));
+    uart_write_bytes(this->uart_num_, "\n", 1);
+  }
+}
+#else
+void HOT Logger::write_msg_(const char *msg) { this->hw_serial_->println(msg); }
+#endif
+
+const char *const UART_SELECTIONS[] = {
+    "UART0",           "UART1",
+#ifdef USE_ESP32_VARIANT_ESP32
+    "UART2",
+#endif
+#ifdef USE_LOGGER_USB_CDC
+    "USB_CDC",
+#endif
+#ifdef USE_LOGGER_USB_SERIAL_JTAG
+    "USB_SERIAL_JTAG",
+#endif
+};
+
+const char *Logger::get_uart_selection_() { return UART_SELECTIONS[this->uart_]; }
+
+}  // namespace logger
+}  // namespace esphome
+#endif

esphome/components/logger/logger_esp8266.cpp
@@ -1,0 +1,45 @@
+#ifdef USE_ESP8266
+#include "logger.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace logger {
+
+static const char *const TAG = "logger";
+
+void Logger::pre_setup() {
+  if (this->baud_rate_ > 0) {
+    switch (this->uart_) {
+      case UART_SELECTION_UART0:
+      case UART_SELECTION_UART0_SWAP:
+        this->hw_serial_ = &Serial;
+        Serial.begin(this->baud_rate_);
+        if (this->uart_ == UART_SELECTION_UART0_SWAP) {
+          Serial.swap();
+        }
+        Serial.setDebugOutput(ESPHOME_LOG_LEVEL >= ESPHOME_LOG_LEVEL_VERBOSE);
+        break;
+      case UART_SELECTION_UART1:
+        this->hw_serial_ = &Serial1;
+        Serial1.begin(this->baud_rate_);
+        Serial1.setDebugOutput(ESPHOME_LOG_LEVEL >= ESPHOME_LOG_LEVEL_VERBOSE);
+        break;
+    }
+  } else {
+    uart_set_debug(UART_NO);
+  }
+
+  global_logger = this;
+
+  ESP_LOGI(TAG, "Log initialized");
+}
+
+void HOT Logger::write_msg_(const char *msg) { this->hw_serial_->println(msg); }
+
+const char *const UART_SELECTIONS[] = {"UART0", "UART1", "UART0_SWAP"};
+
+const char *Logger::get_uart_selection_() { return UART_SELECTIONS[this->uart_]; }
+
+}  // namespace logger
+}  // namespace esphome
+#endif

commit 0bc645ded7f73715dfc85b325c79310015d493dd

Fix build failures on host platform caused by #6167 (#6338)
* Fix build failures for logger component on host platform

* Add climits header

* Restore logger functionality on host

* Install libsodium in ci

.github/workflows/ci.yml
@@ -433,6 +433,9 @@ jobs:
       matrix:
         file: ${{ fromJson(needs.list-components.outputs.matrix) }}
     steps:
+      - name: Install libsodium
+        run: sudo apt-get install libsodium-dev
+
       - name: Check out code from GitHub
         uses: actions/checkout@v4.1.1
       - name: Restore Python

esphome/components/debug/debug_component.cpp
@@ -6,6 +6,7 @@
 #include "esphome/core/helpers.h"
 #include "esphome/core/version.h"
 #include <cinttypes>
+#include <climits>
 
 #ifdef USE_ESP32
 
@@ -49,6 +50,8 @@ static uint32_t get_free_heap() {
   return rp2040.getFreeHeap();
 #elif defined(USE_LIBRETINY)
   return lt_heap_get_free();
+#elif defined(USE_HOST)
+  return INT_MAX;
 #endif
 }
 

esphome/components/logger/__init__.py
@@ -144,6 +144,8 @@ def uart_selection(value):
         component = get_libretiny_component()
         if component in UART_SELECTION_LIBRETINY:
             return cv.one_of(*UART_SELECTION_LIBRETINY[component], upper=True)(value)
+    if CORE.is_host:
+        raise cv.Invalid("Uart selection not valid for host platform")
     raise NotImplementedError
 
 

esphome/components/logger/logger.cpp
@@ -146,8 +146,10 @@ const char *const LOG_LEVELS[] = {"NONE", "ERROR", "WARN", "INFO", "CONFIG", "DE
 void Logger::dump_config() {
   ESP_LOGCONFIG(TAG, "Logger:");
   ESP_LOGCONFIG(TAG, "  Level: %s", LOG_LEVELS[ESPHOME_LOG_LEVEL]);
+#ifndef USE_HOST
   ESP_LOGCONFIG(TAG, "  Log Baud Rate: %" PRIu32, this->baud_rate_);
   ESP_LOGCONFIG(TAG, "  Hardware UART: %s", get_uart_selection_());
+#endif
 
   for (auto &it : this->log_levels_) {
     ESP_LOGCONFIG(TAG, "  Level for '%s': %s", it.tag.c_str(), LOG_LEVELS[it.level]);

esphome/components/logger/logger.h
@@ -137,7 +137,9 @@ class Logger : public Component {
     va_end(arg);
   }
 
+#ifndef USE_HOST
   const char *get_uart_selection_();
+#endif
 
   uint32_t baud_rate_;
   char *tx_buffer_{nullptr};

esphome/components/logger/logger_host.cpp
@@ -16,6 +16,8 @@ void HOT Logger::write_msg_(const char *msg) {
   puts(msg);
 }
 
+void Logger::pre_setup() { global_logger = this; }
+
 }  // namespace logger
 }  // namespace esphome
 

tests/components/debug/test.host.yaml
@@ -1,0 +1,1 @@
+debug:

tests/test_build_components/build_components_base.host.yaml
@@ -1,0 +1,18 @@
+esphome:
+  name: componenttesthost
+  friendly_name: $component_name
+
+host:
+  mac_address: "62:23:45:AF:B3:DD"
+
+logger:
+  level: VERY_VERBOSE
+
+packages:
+  component_under_test: !include
+    file: $component_test_file
+    vars:
+      component_name: $component_name
+      test_name: $test_name
+      target_platform: $target_platform
+      component_test_file: $component_test_file