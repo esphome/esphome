c559ccbb831d1afd19863151fd3fa42a489dda48
Clyde Stubbs <2366188+clydebarrow@users.noreply.github.com>
2024-03-11 16:52:05 +1100

    ILI9XXX: Lazily allocate buffer (#6352)

--- esphome/components/ili9xxx/ili9xxx_display.h
+++ esphome/components/ili9xxx/ili9xxx_display.h
@@ -86,6 +86,14 @@ class ILI9XXXDisplay : public display::DisplayBuffer,
                       display::ColorBitness bitness, bool big_endian, int x_offset, int y_offset, int x_pad) override;
 
  protected:
+  inline bool check_buffer_() {
+    if (this->buffer_ == nullptr) {
+      this->alloc_buffer_();
+      return !this->is_failed();
+    }
+    return true;
+  }
+
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
   void setup_pins_();
 
@@ -116,6 +124,7 @@ class ILI9XXXDisplay : public display::DisplayBuffer,
   void end_command_();
   void start_data_();
   void end_data_();
+  void alloc_buffer_();
 
   GPIOPin *reset_pin_{nullptr};
   GPIOPin *dc_pin_{nullptr};
