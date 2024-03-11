38dead0c65645b551cc3e951bcca952ff3551ee8
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-03-11 14:30:57 +1100

    Add tests; Rename constants

--- esphome/components/cst816/touchscreen/cst816_touchscreen.h
+++ esphome/components/cst816/touchscreen/cst816_touchscreen.h
@@ -11,18 +11,18 @@ namespace cst816 {
 
 static const char *const TAG = "cst816.touchscreen";
 
-static const uint8_t CST816_REG_STATUS = 0x00;
-static const uint8_t CST816_REG_TOUCH_NUM = 0x02;
-static const uint8_t CST816_REG_XPOS_HIGH = 0x03;
-static const uint8_t CST816_REG_XPOS_LOW = 0x04;
-static const uint8_t CST816_REG_YPOS_HIGH = 0x05;
-static const uint8_t CST816_REG_YPOS_LOW = 0x06;
-static const uint8_t CST816_REG_DIS_AUTOSLEEP = 0xFE;
-static const uint8_t CST816_REG_CHIP_ID = 0xA7;
-static const uint8_t CST816_REG_FW_VERSION = 0xA9;
-static const uint8_t CST816_REG_SLEEP = 0xE5;
-static const uint8_t CST816_REG_IRQ_CTL = 0xFA;
-static const uint8_t CST816_IRQ_EN_MOTION = 0x70;
+static const uint8_t REG_STATUS = 0x00;
+static const uint8_t REG_TOUCH_NUM = 0x02;
+static const uint8_t REG_XPOS_HIGH = 0x03;
+static const uint8_t REG_XPOS_LOW = 0x04;
+static const uint8_t REG_YPOS_HIGH = 0x05;
+static const uint8_t REG_YPOS_LOW = 0x06;
+static const uint8_t REG_DIS_AUTOSLEEP = 0xFE;
+static const uint8_t REG_CHIP_ID = 0xA7;
+static const uint8_t REG_FW_VERSION = 0xA9;
+static const uint8_t REG_SLEEP = 0xE5;
+static const uint8_t REG_IRQ_CTL = 0xFA;
+static const uint8_t IRQ_EN_MOTION = 0x70;
 
 static const uint8_t CST820_CHIP_ID = 0xB7;
 static const uint8_t CST816S_CHIP_ID = 0xB4;
@@ -54,18 +54,18 @@ class CST816Touchscreen : public touchscreen::Touchscreen, public i2c::I2CDevice
 
   void update_touches() override {
     uint8_t data[13];
-    if (!this->read_bytes(CST816_REG_STATUS, data, sizeof data)) {
+    if (!this->read_bytes(REG_STATUS, data, sizeof data)) {
       this->status_set_warning();
       return;
     }
-    uint8_t num_of_touches = data[CST816_REG_TOUCH_NUM] & 3;
+    uint8_t num_of_touches = data[REG_TOUCH_NUM] & 3;
     if (num_of_touches == 0) {
       this->update_button_state_(false);
       return;
     }
 
-    uint16_t x = encode_uint16(data[CST816_REG_XPOS_HIGH] & 0xF, data[CST816_REG_XPOS_LOW]);
-    uint16_t y = encode_uint16(data[CST816_REG_YPOS_HIGH] & 0xF, data[CST816_REG_YPOS_LOW]);
+    uint16_t x = encode_uint16(data[REG_XPOS_HIGH] & 0xF, data[REG_XPOS_LOW]);
+    uint16_t y = encode_uint16(data[REG_YPOS_HIGH] & 0xF, data[REG_YPOS_LOW]);
     esph_log_v(TAG, "Read touch %d/%d", x, y);
     if (x >= this->x_raw_max_) {
       this->update_button_state_(true);
@@ -86,7 +86,7 @@ class CST816Touchscreen : public touchscreen::Touchscreen, public i2c::I2CDevice
       this->interrupt_pin_->setup();
       this->attach_interrupt_(this->interrupt_pin_, gpio::INTERRUPT_FALLING_EDGE);
     }
-    if (!this->read_byte(CST816_REG_CHIP_ID, &this->chip_id_)) {
+    if (!this->read_byte(REG_CHIP_ID, &this->chip_id_)) {
       this->mark_failed();
       esph_log_e(TAG, "Failed to read chip id");
       return;
@@ -103,7 +103,7 @@ class CST816Touchscreen : public touchscreen::Touchscreen, public i2c::I2CDevice
         esph_log_e(TAG, "Unknown chip ID 0x%02X", this->chip_id_);
         return;
     }
-    this->write_byte(CST816_REG_IRQ_CTL, CST816_IRQ_EN_MOTION);
+    this->write_byte(REG_IRQ_CTL, IRQ_EN_MOTION);
     if (this->x_raw_max_ == this->x_raw_min_) {
       this->x_raw_max_ = this->display_->get_native_width();
     }
