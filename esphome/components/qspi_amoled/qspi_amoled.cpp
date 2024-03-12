62e4b71ea8ec96dc5f4780bdb787642506dbe496
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-03-12 15:41:42 +1100

    Remove fossil code.

--- esphome/components/qspi_amoled/qspi_amoled.cpp
+++ esphome/components/qspi_amoled/qspi_amoled.cpp
@@ -15,9 +15,6 @@ void QspiAmoLed::dump_config() {
   LOG_PIN("  CS Pin: ", this->cs_);
   LOG_PIN("  Reset Pin: ", this->reset_pin_);
   ESP_LOGCONFIG(TAG, "  SPI Data rate: %dMHz", (unsigned) (this->data_rate_ / 1000000));
-#ifdef USE_POWER_SUPPLY
-  ESP_LOGCONFIG(TAG, "  Power Supply Configured: yes");
-#endif
 }
 
 #endif
