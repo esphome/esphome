0ebb6efee691a4330050073ce74d60a8d63d65a4
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-03-13 16:33:42 +1300

    Bump version to 2024.3.0b1

--- esphome/const.py
+++ esphome/const.py
@@ -1,6 +1,6 @@
 """Constants used by esphome."""
 
-__version__ = "2024.3.0-dev"
+__version__ = "2024.3.0b1"
 
 ALLOWED_NAME_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789-_"
 VALID_SUBSTITUTIONS_CHARACTERS = (
