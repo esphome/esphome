a71011270f09fda8250591696c89536df13fef2e
X-Ryl669 <boite.pour.spam@gmail.com>
2024-03-15 06:48:45 +0100

    Fix for single binary sensor and switch

--- esphome/components/at581x/binary_sensor.py
+++ esphome/components/at581x/binary_sensor.py
@@ -10,19 +10,19 @@ from . import CONF_AT581X_ID, AT581XComponent
 
 DEPENDENCIES = ["at581x"]
 
-CONFIG_SCHEMA = (
-    binary_sensor.binary_sensor_schema(
-        device_class=DEVICE_CLASS_MOTION,
-        icon=ICON_MOTION_SENSOR,
-    ).extend(cv.Schema(
+CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(
+    device_class=DEVICE_CLASS_MOTION,
+    icon=ICON_MOTION_SENSOR,
+).extend(
+    cv.Schema(
         {
             cv.GenerateID(CONF_AT581X_ID): cv.use_id(AT581XComponent),
         }
     )
 )
 
+
 async def to_code(config):
     at581x_component = await cg.get_variable(config[CONF_AT581X_ID])
-    if has_target_config := config.get(CONF_HAS_TARGET):
-        sens = await binary_sensor.new_binary_sensor(has_target_config)
-        cg.add(at581x_component.set_motion_binary_sensor(sens))
+    sens = await binary_sensor.new_binary_sensor(config)
+    cg.add(at581x_component.set_motion_binary_sensor(sens))
