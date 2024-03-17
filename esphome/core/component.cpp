5dec75790b836ec97f558c488113dd1169da8182
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2024-03-17 13:25:52 +0100

    Fix printf type
    To be compatible with all frameworks

--- esphome/core/component.cpp
+++ esphome/core/component.cpp
@@ -5,6 +5,7 @@
 #include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
 #include <utility>
+#include <inttypes.h>
 
 namespace esphome {
 
@@ -211,7 +212,7 @@ WarnIfComponentBlockingGuard::~WarnIfComponentBlockingGuard() {
   uint32_t now = millis();
   if (now - started_ > 50) {
     const char *src = component_ == nullptr ? "<null>" : component_->get_component_source();
-    ESP_LOGW(TAG, "Component %s took a long time for an operation (%u ms).", src, (now - started_));
+    ESP_LOGW(TAG, "Component %s took a long time for an operation (%" PRIu32 " ms).", src, (now - started_));
     ESP_LOGW(TAG, "Components should block for at most 30 ms.");
     ;
   }
