esphome/esphome

commit 0298adb1d836d195c8080073b81ec2b327bfdec1

aht10: Added new CMD and renamed existing CMD to match datasheet (#6303)

esphome/components/aht10/aht10.cpp
@@ -21,30 +21,39 @@ namespace esphome {
 namespace aht10 {
 
 static const char *const TAG = "aht10";
-static const size_t SIZE_CALIBRATE_CMD = 3;
-static const uint8_t AHT10_CALIBRATE_CMD[] = {0xE1, 0x08, 0x00};
-static const uint8_t AHT20_CALIBRATE_CMD[] = {0xBE, 0x08, 0x00};
+static const uint8_t AHT10_INITIALIZE_CMD[] = {0xE1, 0x08, 0x00};
+static const uint8_t AHT20_INITIALIZE_CMD[] = {0xBE, 0x08, 0x00};
 static const uint8_t AHT10_MEASURE_CMD[] = {0xAC, 0x33, 0x00};
-static const uint8_t AHT10_DEFAULT_DELAY = 5;    // ms, for calibration and temperature measurement
-static const uint8_t AHT10_HUMIDITY_DELAY = 30;  // ms
-static const uint8_t AHT10_ATTEMPTS = 3;         // safety margin, normally 3 attempts are enough: 3*30=90ms
-static const uint8_t AHT10_CAL_ATTEMPTS = 10;
+static const uint8_t AHT10_SOFTRESET_CMD[] = {0xBA};
+
+static const uint8_t AHT10_DEFAULT_DELAY = 5;     // ms, for initialization and temperature measurement
+static const uint8_t AHT10_HUMIDITY_DELAY = 30;   // ms
+static const uint8_t AHT10_SOFTRESET_DELAY = 30;  // ms
+
+static const uint8_t AHT10_ATTEMPTS = 3;  // safety margin, normally 3 attempts are enough: 3*30=90ms
+static const uint8_t AHT10_INIT_ATTEMPTS = 10;
+
 static const uint8_t AHT10_STATUS_BUSY = 0x80;
 
 void AHT10Component::setup() {
-  const uint8_t *calibrate_cmd;
+  if (this->write(AHT10_SOFTRESET_CMD, sizeof(AHT10_SOFTRESET_CMD)) != i2c::ERROR_OK) {
+    ESP_LOGE(TAG, "Reset AHT10 failed!");
+  }
+  delay(AHT10_SOFTRESET_DELAY);
+
+  const uint8_t *init_cmd;
   switch (this->variant_) {
     case AHT10Variant::AHT20:
-      calibrate_cmd = AHT20_CALIBRATE_CMD;
+      init_cmd = AHT20_INITIALIZE_CMD;
       ESP_LOGCONFIG(TAG, "Setting up AHT20");
       break;
     case AHT10Variant::AHT10:
     default:
-      calibrate_cmd = AHT10_CALIBRATE_CMD;
+      init_cmd = AHT10_INITIALIZE_CMD;
       ESP_LOGCONFIG(TAG, "Setting up AHT10");
   }
 
-  if (this->write(calibrate_cmd, SIZE_CALIBRATE_CMD) != i2c::ERROR_OK) {
+  if (this->write(init_cmd, sizeof(init_cmd)) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "Communication with AHT10 failed!");
     this->mark_failed();
     return;
@@ -59,19 +68,19 @@ void AHT10Component::setup() {
       return;
     }
     ++cal_attempts;
-    if (cal_attempts > AHT10_CAL_ATTEMPTS) {
-      ESP_LOGE(TAG, "AHT10 calibration timed out!");
+    if (cal_attempts > AHT10_INIT_ATTEMPTS) {
+      ESP_LOGE(TAG, "AHT10 initialization timed out!");
       this->mark_failed();
       return;
     }
   }
   if ((data & 0x68) != 0x08) {  // Bit[6:5] = 0b00, NORMAL mode and Bit[3] = 0b1, CALIBRATED
-    ESP_LOGE(TAG, "AHT10 calibration failed!");
+    ESP_LOGE(TAG, "AHT10 initialization failed!");
     this->mark_failed();
     return;
   }
 
-  ESP_LOGV(TAG, "AHT10 calibrated");
+  ESP_LOGV(TAG, "AHT10 initialization");
 }
 
 void AHT10Component::update() {

commit 860719defa3cb1ead1b91a2bde59a3be572d4cdc

Fix bug introduced in #6303

esphome/components/aht10/aht10.cpp
@@ -54,7 +54,7 @@ void AHT10Component::setup() {
       ESP_LOGCONFIG(TAG, "Setting up AHT10");
   }
 
-  if (this->write(init_cmd, sizeof(init_cmd)) != i2c::ERROR_OK) {
+  if (this->write(init_cmd, sizeof(AHT10_INITIALIZE_CMD)) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "Communication with AHT10 failed!");
     this->mark_failed();
     return;