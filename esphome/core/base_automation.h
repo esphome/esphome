9fabb78881ce70b63856822b1448d01fcf0212cf
H. Árkosi Róbert <robreg@zsurob.hu>
2024-03-22 21:00:30 +0100

    Increase length of `ESPHOME_PROJECT_VERSION`
    30 chars get used very quickly when the user uses a few special characters in the version string. 
    
    Eg. `Gázkazán 1-2-4 PID vezérlés` requires double amount of chars: `G\303\241zkaz\303\241n 1-2-4 PID vez\303\251rl\303\251s`.

--- esphome/core/base_automation.h
+++ esphome/core/base_automation.h
@@ -132,9 +132,9 @@ class ProjectUpdateTrigger : public Trigger<std::string>, public Component {
  public:
   void setup() override {
     uint32_t hash = fnv1_hash(ESPHOME_PROJECT_NAME);
-    ESPPreferenceObject pref = global_preferences->make_preference<char[30]>(hash, true);
-    char previous_version[30];
-    char current_version[30] = ESPHOME_PROJECT_VERSION;
+    ESPPreferenceObject pref = global_preferences->make_preference<char[60]>(hash, true);
+    char previous_version[60];
+    char current_version[60] = ESPHOME_PROJECT_VERSION;
     if (pref.load(&previous_version)) {
       int cmp = strcmp(previous_version, current_version);
       if (cmp < 0) {
