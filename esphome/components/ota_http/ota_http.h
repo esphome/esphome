f15a92b6bf72cd1dd27d77ed2637a6aaf619e649
oarcher <olivier.archer@gmail.com>
2024-03-24 20:00:51 +0000

    lint

--- esphome/components/ota_http/ota_http.h
+++ esphome/components/ota_http/ota_http.h
@@ -59,9 +59,9 @@ class OtaHttpComponent : public Component {
   size_t body_length_ = 0;
   size_t bytes_read_ = 0;
   uint64_t timeout_;
-  const uint16_t http_recv_buffer_ = 500;       // the firmware GET chunk size
-  const uint16_t max_http_recv_buffer_ = 512;   // internal max http buffer size must be > HTTP_RECV_BUFFER_ (TLS
-                                                // overhead) and must be a power of two from 512 to 4096
+  const uint16_t http_recv_buffer_ = 500;      // the firmware GET chunk size
+  const uint16_t max_http_recv_buffer_ = 512;  // internal max http buffer size must be > HTTP_RECV_BUFFER_ (TLS
+                                              // overhead) and must be a power of two from 512 to 4096
   bool update_started_ = false;
   static const std::unique_ptr<ota::OTABackend> BACKEND;
   void cleanup_();
