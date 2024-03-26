46a042f8a2591263c8786633d4129cb5824ece9c
MagicBear <magicbearmo@gmail.com>
2024-03-26 15:04:41 +0900

    fix for ci-suggest-changes

--- esphome/components/daikin_arc/climate.py
+++ esphome/components/daikin_arc/climate.py
@@ -9,9 +9,7 @@ daikin_arc_ns = cg.esphome_ns.namespace("daikin_arc")
 DaikinArcClimate = daikin_arc_ns.class_("DaikinArcClimate", climate_ir.ClimateIR)
 
 CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(DaikinArcClimate)
-    }
+    {cv.GenerateID(): cv.declare_id(DaikinArcClimate)}
 )
 
 
