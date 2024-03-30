df7936e6eb8718838b9cf2878dbda543bd5f409a
Scott Le <sle@atlassian.com>
2024-03-31 00:21:48 +1100

    fix missing config

--- esphome/components/litter_robot_presence_detector/__init__.py
+++ esphome/components/litter_robot_presence_detector/__init__.py
@@ -16,4 +16,4 @@ CONFIG_SCHEMA = cv.Schema(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var)
+    await cg.register_component(var, config)
