e1aad9a8ce8aef0eb0c502057272b9fc0f6aba47
David Friedland <david.friedland@gmail.com>
2024-03-30 13:51:39 -0700

    Change event types assignment to use copy instead of move semantics.

--- esphome/components/event/event.cpp
+++ esphome/components/event/event.cpp
@@ -20,7 +20,7 @@ bool Event::has_event_type(const std::string &event_type) const {
   return std::find(types_.begin(), types_.end(), event_type) != types_.end();
 }
 
-void Event::set_event_types(const std::vector<std::string> &types) { this->types_ = std::move(types); }
+void Event::set_event_types(const std::vector<std::string> &types) { this->types_ = types; }
 
 void Event::add_on_event_fired_callback(std::function<void(const std::string &event_type)> &&callback) {
   this->event_fired_callback_.add(std::move(callback));
