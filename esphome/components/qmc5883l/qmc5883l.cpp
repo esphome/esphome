938923dc830ba8cffbbb15d55d7372aa9fec82ab
tronikos <tronikos@users.noreply.github.com>
2024-04-01 00:03:12 -0700

    Add temperature for QMC5883L

--- esphome/components/qmc5883l/qmc5883l.cpp
+++ esphome/components/qmc5883l/qmc5883l.cpp
@@ -72,16 +72,18 @@ void QMC5883LComponent::dump_config() {
   LOG_SENSOR("  ", "Y Axis", this->y_sensor_);
   LOG_SENSOR("  ", "Z Axis", this->z_sensor_);
   LOG_SENSOR("  ", "Heading", this->heading_sensor_);
+  LOG_SENSOR("  ", "Temperature", this->temperature_sensor_);
 }
 float QMC5883LComponent::get_setup_priority() const { return setup_priority::DATA; }
 void QMC5883LComponent::update() {
   uint8_t status = false;
   this->read_byte(QMC5883L_REGISTER_STATUS, &status);
 
-  uint16_t raw_x, raw_y, raw_z;
+  uint16_t raw_x, raw_y, raw_z, raw_temp;
   if (!this->read_byte_16_(QMC5883L_REGISTER_DATA_X_LSB, &raw_x) ||
       !this->read_byte_16_(QMC5883L_REGISTER_DATA_Y_LSB, &raw_y) ||
-      !this->read_byte_16_(QMC5883L_REGISTER_DATA_Z_LSB, &raw_z)) {
+      !this->read_byte_16_(QMC5883L_REGISTER_DATA_Z_LSB, &raw_z) ||
+      !this->read_byte_16_(QMC5883L_REGISTER_TEMPERATURE_LSB, &raw_temp)) {
     this->status_set_warning();
     return;
   }
@@ -102,9 +104,10 @@ void QMC5883LComponent::update() {
   const float x = int16_t(raw_x) * mg_per_bit * 0.1f;
   const float y = int16_t(raw_y) * mg_per_bit * 0.1f;
   const float z = int16_t(raw_z) * mg_per_bit * 0.1f;
+  const float temp = int16_t(raw_temp) * 0.01f;
 
   float heading = atan2f(0.0f - x, y) * 180.0f / M_PI;
-  ESP_LOGD(TAG, "Got x=%0.02fµT y=%0.02fµT z=%0.02fµT heading=%0.01f° status=%u", x, y, z, heading, status);
+  ESP_LOGD(TAG, "Got x=%0.02fµT y=%0.02fµT z=%0.02fµT heading=%0.01f° temperature=%0.01f°C status=%u", x, y, z, heading, temp, status);
 
   if (this->x_sensor_ != nullptr)
     this->x_sensor_->publish_state(x);
@@ -114,6 +117,8 @@ void QMC5883LComponent::update() {
     this->z_sensor_->publish_state(z);
   if (this->heading_sensor_ != nullptr)
     this->heading_sensor_->publish_state(heading);
+  if (this->temperature_sensor_ != nullptr)
+    this->temperature_sensor_->publish_state(temp);
 }
 
 bool QMC5883LComponent::read_byte_16_(uint8_t a_register, uint16_t *data) {
