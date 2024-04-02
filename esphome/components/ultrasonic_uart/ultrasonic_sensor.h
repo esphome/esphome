e24c2332d6f70e9c357e6f008339bccb391f7673
Masterz69 <igors.micko@gmail.com>
2024-04-02 18:08:16 +0300

    Create ultrasonic_sensor.h

---
+++ esphome/components/ultrasonic_uart/ultrasonic_sensor.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/uart/uart.h"
+
+namespace esphome {
+namespace ultrasonic_uart {
+
+class UltrasonicSensorComponent_UART : public sensor::Sensor, public PollingComponent, public uart::UARTDevice {
+ public:
+  void setup() override;
+  void dump_config() override;
+
+  void update() override;
+
+  float get_setup_priority() const override;
+};
+
+}  // namespace ultrasonic_uart
+}  // namespace esphome
