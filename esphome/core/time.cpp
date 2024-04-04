f15e8f68a84198974a22db83356d6a40c2c02182
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-04-04 13:00:14 +1300

    Fix

--- esphome/core/time.cpp
+++ esphome/core/time.cpp
@@ -73,9 +73,10 @@ bool ESPTime::strptime(const std::string &time_to_parse, ESPTime &esp_time) {
   uint8_t second;
   int num;
 
-  if (sscanf(time_to_parse.c_str(), "%04u-%02u-%02u %02u:%02u:%02u %n", &year, &month, &day, &hour, &minute,  // NOLINT
-             &second,                                                                                         // NOLINT
-             &num) == 6 &&                                                                                    // NOLINT
+  if (sscanf(time_to_parse.c_str(), "%04hu-%02hhu-%02hhu %02hhu:%02hhu:%02hhu %n", &year, &month, &day,  // NOLINT
+             &hour,                                                                                      // NOLINT
+             &minute,                                                                                    // NOLINT
+             &second, &num) == 6 &&                                                                      // NOLINT
       num == time_to_parse.size()) {
     esp_time.year = year;
     esp_time.month = month;
@@ -83,17 +84,17 @@ bool ESPTime::strptime(const std::string &time_to_parse, ESPTime &esp_time) {
     esp_time.hour = hour;
     esp_time.minute = minute;
     esp_time.second = second;
-  } else if (sscanf(time_to_parse.c_str(), "%02u:%02u:%02u %n", &hour, &minute, &second, &num) == 3 &&  // NOLINT
+  } else if (sscanf(time_to_parse.c_str(), "%02hhu:%02hhu:%02hhu %n", &hour, &minute, &second, &num) == 3 &&  // NOLINT
              num == time_to_parse.size()) {
     esp_time.hour = hour;
     esp_time.minute = minute;
     esp_time.second = second;
-  } else if (sscanf(time_to_parse.c_str(), "%02u:%02u %n", &hour, &minute, &num) == 2 &&  // NOLINT
+  } else if (sscanf(time_to_parse.c_str(), "%02hhu:%02hhu %n", &hour, &minute, &num) == 2 &&  // NOLINT
              num == time_to_parse.size()) {
     esp_time.hour = hour;
     esp_time.minute = minute;
     esp_time.second = 0;
-  } else if (sscanf(time_to_parse.c_str(), "%04u-%02u-%02u %n", &year, &month, &day, &num) == 3 &&  // NOLINT
+  } else if (sscanf(time_to_parse.c_str(), "%04hu-%02hhu-%02hhu %n", &year, &month, &day, &num) == 3 &&  // NOLINT
              num == time_to_parse.size()) {
     esp_time.year = year;
     esp_time.month = month;
