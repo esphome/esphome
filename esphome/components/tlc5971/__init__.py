acf1a7f1923ba829f893cd11654ced68138a748e
IJssel Koster <ij.koster@student.tudelft.nl>
2024-04-08 01:38:35 +0200

    Reformated python code

--- esphome/components/tlc5971/__init__.py
+++ esphome/components/tlc5971/__init__.py
@@ -5,10 +5,10 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.const import (
-  CONF_CLOCK_PIN,
-  CONF_DATA_PIN,
-  CONF_ID,
-  CONF_NUM_CHIPS,
+    CONF_CLOCK_PIN,
+    CONF_DATA_PIN,
+    CONF_ID,
+    CONF_NUM_CHIPS,
 )
 
 
@@ -20,22 +20,22 @@ TLC5971 = tlc5971_ns.class_("TLC5971", cg.Component)
 
 MULTI_CONF = True
 CONFIG_SCHEMA = cv.Schema(
-  {
-    cv.GenerateID(): cv.declare_id(TLC5971),
-    cv.Required(CONF_DATA_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_CLOCK_PIN): pins.gpio_output_pin_schema,
-    cv.Optional(CONF_NUM_CHIPS, default=1): cv.int_range(min=1, max=85),
-  }
+    {
+        cv.GenerateID(): cv.declare_id(TLC5971),
+        cv.Required(CONF_DATA_PIN): pins.gpio_output_pin_schema,
+        cv.Required(CONF_CLOCK_PIN): pins.gpio_output_pin_schema,
+        cv.Optional(CONF_NUM_CHIPS, default=1): cv.int_range(min=1, max=85),
+    }
 ).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
-  var = cg.new_Pvariable(config[CONF_ID])
-  await cg.register_component(var, config)
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
 
-  data = await cg.gpio_pin_expression(config[CONF_DATA_PIN])
-  cg.add(var.set_data_pin(data))
-  clock = await cg.gpio_pin_expression(config[CONF_CLOCK_PIN])
-  cg.add(var.set_clock_pin(clock))
+    data = await cg.gpio_pin_expression(config[CONF_DATA_PIN])
+    cg.add(var.set_data_pin(data))
+    clock = await cg.gpio_pin_expression(config[CONF_CLOCK_PIN])
+    cg.add(var.set_clock_pin(clock))
 
-  cg.add(var.set_num_chips(config[CONF_NUM_CHIPS]))
+    cg.add(var.set_num_chips(config[CONF_NUM_CHIPS]))
--- esphome/components/tlc5971/output.py
+++ esphome/components/tlc5971/output.py
@@ -11,18 +11,18 @@ Channel = TLC5971.class_("Channel", output.FloatOutput)
 
 CONF_TLC5971_ID = "tlc5971_id"
 CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
-  {
-    cv.GenerateID(CONF_TLC5971_ID): cv.use_id(TLC5971),
-    cv.Required(CONF_ID): cv.declare_id(Channel),
-    cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=65535),
-  }
+    {
+        cv.GenerateID(CONF_TLC5971_ID): cv.use_id(TLC5971),
+        cv.Required(CONF_ID): cv.declare_id(Channel),
+        cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=65535),
+    }
 ).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
-  var = cg.new_Pvariable(config[CONF_ID])
-  await output.register_output(var, config)
+    var = cg.new_Pvariable(config[CONF_ID])
+    await output.register_output(var, config)
 
-  parent = await cg.get_variable(config[CONF_TLC5971_ID])
-  cg.add(var.set_parent(parent))
-  cg.add(var.set_channel(config[CONF_CHANNEL]))
+    parent = await cg.get_variable(config[CONF_TLC5971_ID])
+    cg.add(var.set_parent(parent))
+    cg.add(var.set_channel(config[CONF_CHANNEL]))
