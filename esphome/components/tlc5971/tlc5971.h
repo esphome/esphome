c84a7a09f4f37ffca4c3c0c4695efc9e90e70855
IJssel Koster <ij.koster@student.tudelft.nl>
2024-04-09 14:24:44 +0200

    fixed indentation for clang

--- esphome/components/tlc5971/tlc5971.h
+++ esphome/components/tlc5971/tlc5971.h
@@ -11,33 +11,33 @@ namespace esphome {
 namespace tlc5971 {
 
 class TLC5971 : public Component {
-  public:
-    const uint8_t N_CHANNELS_PER_CHIP = 12;
+ public:
+  const uint8_t N_CHANNELS_PER_CHIP = 12;
 
-    void set_data_pin(GPIOPin *data_pin) { data_pin_ = data_pin; }
-    void set_clock_pin(GPIOPin *clock_pin) { clock_pin_ = clock_pin; }
-    void set_num_chips(uint8_t num_chips) { num_chips_ = num_chips; }
+  void set_data_pin(GPIOPin *data_pin) { data_pin_ = data_pin; }
+  void set_clock_pin(GPIOPin *clock_pin) { clock_pin_ = clock_pin; }
+  void set_num_chips(uint8_t num_chips) { num_chips_ = num_chips; }
 
-    void setup() override;
+  void setup() override;
 
-    void dump_config() override;
+  void dump_config() override;
 
-    float get_setup_priority() const override { return setup_priority::HARDWARE; }
+  float get_setup_priority() const override { return setup_priority::HARDWARE; }
 
-    /// Send new values if they were updated.
-    void loop() override;
+  /// Send new values if they were updated.
+  void loop() override;
 
-    void set_channel_value(uint16_t channel, uint16_t value);
+  void set_channel_value(uint16_t channel, uint16_t value);
 
-  protected:
-    void transfer_(uint8_t send);
+ protected:
+  void transfer_(uint8_t send);
 
-    GPIOPin *data_pin_;
-    GPIOPin *clock_pin_;
-    uint8_t num_chips_;
+  GPIOPin *data_pin_;
+  GPIOPin *clock_pin_;
+  uint8_t num_chips_;
 
-    std::vector<uint16_t> pwm_amounts_;
-    bool update_{true};
+  std::vector<uint16_t> pwm_amounts_;
+  bool update_{true};
 };
 }  // namespace tlc5971
 }  // namespace esphome
