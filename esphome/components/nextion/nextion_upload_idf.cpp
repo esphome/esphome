78dddee59ee5788748ca9b0960b0f9c299297f68
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2024-04-14 01:09:54 +0200

    Report detailed free heap

--- esphome/components/nextion/nextion_upload_idf.cpp
+++ esphome/components/nextion/nextion_upload_idf.cpp
@@ -86,9 +86,15 @@ Nextion::TFTUploadResult Nextion::upload_by_chunks_(esp_http_client_handle_t htt
       App.feed_wdt();
       this->recv_ret_string_(recv_string, upload_first_chunk_sent_ ? 500 : 5000, true);
       this->content_length_ -= read_len;
+#ifdef USE_PSRAM
+      ESP_LOGD(TAG, "Uploaded %0.2f %%, remaining %d bytes, free heap (dram/psram): %" PRIu32 "/%" PRIu32 " bytes",
+               100.0 * (this->tft_size_ - this->content_length_) / this->tft_size_, this->content_length_,
+               heap_caps_get_free_size(MALLOC_CAP_INTERNAL), esp_get_free_heap_size(MALLOC_CAP_SPIRAM));
+#else
       ESP_LOGD(TAG, "Uploaded %0.2f %%, remaining %d bytes, free heap: %" PRIu32 " bytes",
                100.0 * (this->tft_size_ - this->content_length_) / this->tft_size_, this->content_length_,
                esp_get_free_heap_size());
+#endif
       upload_first_chunk_sent_ = true;
       if (recv_string[0] == 0x08 && recv_string.size() == 5) {  // handle partial upload request
         ESP_LOGD(TAG, "recv_string [%s]",
