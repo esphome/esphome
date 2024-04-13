3f09d0af5b2f1b002d2cf1a5c921d1c63020d161
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2024-04-14 01:12:45 +0200

    Use heap_caps_get_free_size for free heap

--- esphome/components/nextion/nextion_upload_idf.cpp
+++ esphome/components/nextion/nextion_upload_idf.cpp
@@ -89,7 +89,7 @@ Nextion::TFTUploadResult Nextion::upload_by_chunks_(esp_http_client_handle_t htt
 #ifdef USE_PSRAM
       ESP_LOGD(TAG, "Uploaded %0.2f %%, remaining %d bytes, free heap (dram+psram): %" PRIu32 "+%" PRIu32 " bytes",
                100.0 * (this->tft_size_ - this->content_length_) / this->tft_size_, this->content_length_,
-               heap_caps_get_free_size(MALLOC_CAP_INTERNAL), esp_get_free_heap_size(MALLOC_CAP_SPIRAM));
+               heap_caps_get_free_size(MALLOC_CAP_INTERNAL), heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
 #else
       ESP_LOGD(TAG, "Uploaded %0.2f %%, remaining %d bytes, free heap: %" PRIu32 " bytes",
                100.0 * (this->tft_size_ - this->content_length_) / this->tft_size_, this->content_length_,
