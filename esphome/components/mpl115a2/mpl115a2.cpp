85e8a6477b668b7e3c11403d5decc36e977e6f13
unic8s <erik.lembke@unic8.com>
2024-04-19 00:03:08 +0200

    Renamed in/out variables

--- esphome/components/mpl115a2/mpl115a2.cpp
+++ esphome/components/mpl115a2/mpl115a2.cpp
@@ -56,7 +56,7 @@ void MPL115A2Component::dump_config() {
 }
 
 void MPL115A2Component::update() {
-  uint16_t pressureRaw, tempRaw;
+  uint16_t pressureIn, tempIn;
   float pressureComp;
 
   uint8_t cmd[2] = {MPL115A2_REGISTER_STARTCONVERSION, 0};
@@ -71,22 +71,22 @@ void MPL115A2Component::update() {
   this->write(cmd, 1);
   this->read(buffer, 4);
 
-  pressureRaw = (((uint16_t) buffer[0] << 8) | buffer[1]) >> 6;
-  tempRaw = (((uint16_t) buffer[2] << 8) | buffer[3]) >> 6;
+  pressureIn = (((uint16_t) buffer[0] << 8) | buffer[1]) >> 6;
+  tempIn = (((uint16_t) buffer[2] << 8) | buffer[3]) >> 6;
 
   // See datasheet p.6 for evaluation sequence
-  pressureComp = _mpl115a2_a0 + (_mpl115a2_b1 + _mpl115a2_c12 * tempRaw) * pressureRaw + _mpl115a2_b2 * tempRaw;
+  pressureComp = _mpl115a2_a0 + (_mpl115a2_b1 + _mpl115a2_c12 * tempIn) * pressureIn + _mpl115a2_b2 * tempIn;
 
-  float pressure = ((65.0F / 1023.0F) * pressureComp) + 50.0F;
+  float pressureOut = ((65.0F / 1023.0F) * pressureComp) + 50.0F;
   if (this->pressure_ != nullptr)
-    this->pressure_->publish_state(pressure);
+    this->pressure_->publish_state(pressureOut);
   int16_t t = encode_uint16(buffer[3], buffer[4]);
 
-  float temperature = ((float) tempRaw - 498.0F) / -5.35F + 25.0F;
+  float temperatureOut = ((float) tempIn - 498.0F) / -5.35F + 25.0F;
   if (this->temperature_ != nullptr)
-    this->temperature_->publish_state(temperature);
+    this->temperature_->publish_state(temperatureOut);
 
-  ESP_LOGD(TAG, "Got Temperature=%.1f°C Pressure=%.1f", temperature, pressure);
+  ESP_LOGD(TAG, "Got Temperature=%.1f°C Pressure=%.1f", temperatureOut, pressureOut);
 
   this->status_clear_warning();
 }
