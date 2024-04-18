a0f3a2f0c67264ad1c6595e06680f935db8cad87
Jonathan Swoboda <jonathan.swoboda>
2024-04-18 09:32:59 -0400

    Pass value to all or filters but only return the first value

--- esphome/components/sensor/filter.cpp
+++ esphome/components/sensor/filter.cpp
@@ -359,18 +359,17 @@ OrFilter::OrFilter(std::vector<Filter *> filters) : filters_(std::move(filters))
 OrFilter::PhiNode::PhiNode(OrFilter *or_parent) : or_parent_(or_parent) {}
 
 optional<float> OrFilter::PhiNode::new_value(float value) {
-  this->or_parent_->output(value);
-  this->or_parent_->has_value_ = true;
+  if (!this->or_parent_->has_value_) {
+    this->or_parent_->output(value);
+    this->or_parent_->has_value_ = true;
+  }
+
   return {};
 }
 optional<float> OrFilter::new_value(float value) {
   this->has_value_ = false;
-  for (Filter *filter : this->filters_) {
+  for (Filter *filter : this->filters_)
     filter->input(value);
-    if (this->has_value_) {
-      break;
-    }
-  }
 
   return {};
 }
