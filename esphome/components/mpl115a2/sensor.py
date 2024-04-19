107d79418f5c5bc6be5c46d26bd9b428aef324ef
unic8s <erik.lembke@unic8.com>
2024-04-19 11:26:00 +0200

    Fix black formatting

--- esphome/components/mpl115a2/sensor.py
+++ esphome/components/mpl115a2/sensor.py
@@ -9,7 +9,7 @@ from esphome.const import (
     DEVICE_CLASS_TEMPERATURE,
     STATE_CLASS_MEASUREMENT,
     UNIT_CELSIUS,
-    UNIT_HECTOPASCAL
+    UNIT_HECTOPASCAL,
 )
 
 CODEOWNERS = ["@unic8s"]
