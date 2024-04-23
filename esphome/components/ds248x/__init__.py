74cd8acc0d94f95ab2cc1de67a7ce3e020b02700
Iron Man <astark@medialsace.fr>
2024-04-23 17:33:43 +0200

    Update __init__.py
    Import CONF_TYPE from const.py

--- esphome/components/ds248x/__init__.py
+++ esphome/components/ds248x/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
-from esphome.const import CONF_ID, CONF_SLEEP_PIN
+from esphome.const import CONF_ID, CONF_SLEEP_PIN, CONF_TYPE
 from esphome.components import i2c, sensor
 
 MULTI_CONF = True
