81f76bf1df45ee934665ecbd524b1295488fd1fd
Keith Burzinski <kbx81x@gmail.com>
2024-04-24 04:39:44 -0500

    Add `text_sensor`, too

--- script/sync-device_class.py
+++ script/sync-device_class.py
@@ -10,6 +10,7 @@ from homeassistant.components.event import EventDeviceClass
 from homeassistant.components.number import NumberDeviceClass
 from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.components.switch import SwitchDeviceClass
+from homeassistant.components.text_sensor import TextSensorDeviceClass
 from homeassistant.components.valve import ValveDeviceClass
 
 # pylint: enable=import-error
@@ -27,6 +28,7 @@ DOMAINS = {
     "number": NumberDeviceClass,
     "sensor": SensorDeviceClass,
     "switch": SwitchDeviceClass,
+    "text_sensor": TextSensorDeviceClass,
     "valve": ValveDeviceClass,
 }
 
