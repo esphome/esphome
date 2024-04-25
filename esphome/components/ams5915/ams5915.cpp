0c70595bd1512e89a779ae7e9c68456333cb1d87
unsplorer <mitchell_way@outlook.com>
2024-04-25 16:03:39 -0500

    wrapped arduino lib

--- esphome/components/ams5915/ams5915.cpp
+++ esphome/components/ams5915/ams5915.cpp
@@ -5,10 +5,173 @@
 namespace esphome {
 namespace ams5915 {
 static const char *const TAG = "ams5915";
-AMS5915 sPress(Wire,0x28,AMS5915::AMS5915_1000_D_B);
+Ams5915 sPress(Wire,0x28,Ams5915::AMS5915_1000_D_B);
+/* constructor, I2C bus, sensor address, and transducer type */
+Ams5915::Ams5915(TwoWire &bus,uint8_t address,Transducer type){
+  // I2C bus
+  _bus = &bus; 
+  // I2C address
+  _address = address; 
+  // transducer type
+  _type = type; 
+}
+
+/* starts the I2C communication and sets the pressure and temperature ranges using getTransducer */
+int Ams5915::begin(){
+  // starting the I2C bus
+	_bus->begin();
+  // setting the I2C clock
+  _bus->setClock(_i2cRate);
+	// setting the min and max pressure based on the chip
+	getTransducer();
+  // checking to see if we can talk with the sensor
+  for (size_t i=0; i < _maxAttempts; i++) {
+    _status = readBytes(&_pressureCounts,&_temperatureCounts);
+    if (_status > 0) {break;}
+    delay(10);
+  }
+  return _status;
+}
+
+/* reads data from the sensor */
+int Ams5915::readSensor(){
+  // get pressure and temperature counts off transducer
+  _status = readBytes(&_pressureCounts,&_temperatureCounts);
+  // convert counts to pressure, PA
+  _data.Pressure_Pa = (((float)(_pressureCounts - _digOutPmin))/(((float)(_digOutPmax - _digOutPmin))/((float)(_pMax - _pMin)))+(float)_pMin)*_mBar2Pa;
+  // convert counts to temperature, C
+  _data.Temp_C = (float)((_temperatureCounts*200))/2048.0f-50.0f;
+  return _status;
+}
+
+/* returns the pressure value, PA */
+float Ams5915::getPressure_Pa(){
+  return _data.Pressure_Pa;
+}
+
+/* returns the temperature value, C */
+float Ams5915::getTemperature_C(){
+  return _data.Temp_C;
+}
+
+/* sets the pressure range based on the chip */
+void Ams5915::getTransducer(){
+  // setting the min and max pressures based on which transducer it is
+  switch(_type) {
+    case AMS5915_0005_D:
+      _pMin = AMS5915_0005_D_P_MIN;
+      _pMax = AMS5915_0005_D_P_MAX;
+      break;
+    case AMS5915_0010_D:
+      _pMin = AMS5915_0010_D_P_MIN;
+      _pMax = AMS5915_0010_D_P_MAX;
+      break;
+    case AMS5915_0005_D_B:
+      _pMin = AMS5915_0005_D_B_P_MIN;
+      _pMax = AMS5915_0005_D_B_P_MAX;
+      break;
+    case AMS5915_0010_D_B:
+      _pMin = AMS5915_0010_D_B_P_MIN;
+      _pMax = AMS5915_0010_D_B_P_MAX;
+      break;
+    case AMS5915_0020_D:
+      _pMin = AMS5915_0020_D_P_MIN;
+      _pMax = AMS5915_0020_D_P_MAX;
+      break;
+    case AMS5915_0050_D:
+      _pMin = AMS5915_0050_D_P_MIN;
+      _pMax = AMS5915_0050_D_P_MAX;
+      break;
+    case AMS5915_0100_D:
+      _pMin = AMS5915_0100_D_P_MIN;
+      _pMax = AMS5915_0100_D_P_MAX;
+      break;
+    case AMS5915_0020_D_B:
+      _pMin = AMS5915_0020_D_B_P_MIN;
+      _pMax = AMS5915_0020_D_B_P_MAX;
+      break;
+    case AMS5915_0050_D_B:
+      _pMin = AMS5915_0050_D_B_P_MIN;
+      _pMax = AMS5915_0050_D_B_P_MAX;
+      break;
+    case AMS5915_0100_D_B:
+      _pMin = AMS5915_0100_D_B_P_MIN;
+      _pMax = AMS5915_0100_D_B_P_MAX;
+      break;
+    case AMS5915_0200_D:
+      _pMin = AMS5915_0200_D_P_MIN;
+      _pMax = AMS5915_0200_D_P_MAX;
+      break;
+    case AMS5915_0350_D:
+      _pMin = AMS5915_0350_D_P_MIN;
+      _pMax = AMS5915_0350_D_P_MAX;
+      break;
+    case AMS5915_1000_D:
+      _pMin = AMS5915_1000_D_P_MIN;
+      _pMax = AMS5915_1000_D_P_MAX;
+      break;
+    case AMS5915_2000_D:
+      _pMin = AMS5915_2000_D_P_MIN;
+      _pMax = AMS5915_2000_D_P_MAX;
+      break;
+    case AMS5915_4000_D:
+      _pMin = AMS5915_4000_D_P_MIN;
+      _pMax = AMS5915_4000_D_P_MAX;
+      break;
+    case AMS5915_7000_D:
+      _pMin = AMS5915_7000_D_P_MIN;
+      _pMax = AMS5915_7000_D_P_MAX;
+      break;
+    case AMS5915_10000_D:
+      _pMin = AMS5915_10000_D_P_MIN;
+      _pMax = AMS5915_10000_D_P_MAX;
+      break;
+    case AMS5915_0200_D_B:
+      _pMin = AMS5915_0200_D_B_P_MIN;
+      _pMax = AMS5915_0200_D_B_P_MAX;
+      break;
+    case AMS5915_0350_D_B:
+      _pMin = AMS5915_0350_D_B_P_MIN;
+      _pMax = AMS5915_0350_D_B_P_MAX;
+      break;
+    case AMS5915_1000_D_B:
+      _pMin = AMS5915_1000_D_B_P_MIN;
+      _pMax = AMS5915_1000_D_B_P_MAX;
+      break;
+    case AMS5915_1000_A:
+      _pMin = AMS5915_1000_A_P_MIN;
+      _pMax = AMS5915_1000_A_P_MAX;
+      break;
+    case AMS5915_1200_B:
+      _pMin = AMS5915_1200_B_P_MIN;
+      _pMax = AMS5915_1200_B_P_MAX;
+      break;
+  }
+}
+
+/* reads pressure and temperature and returns values in counts */
+int Ams5915::readBytes(uint16_t* pressureCounts,uint16_t* temperatureCounts){
+  // read from sensor
+  _numBytes = _bus->requestFrom(_address,sizeof(_buffer));
+  // put the data in buffer
+  if (_numBytes == sizeof(_buffer)) {
+    _buffer[0] = _bus->read(); 
+    _buffer[1] = _bus->read();
+    _buffer[2] = _bus->read();
+    _buffer[3] = _bus->read();
+    // assemble into a uint16_t
+    *pressureCounts = (((uint16_t) (_buffer[0]&0x3F)) <<8) + (((uint16_t) _buffer[1]));
+    *temperatureCounts = (((uint16_t) (_buffer[2])) <<3) + (((uint16_t) _buffer[3]&0xE0)>>5);
+    _status = 1;
+  } else {
+    _status = -1;
+  }
+  return _status;
+}
+
 void Ams5915::setup() {
   if (sPress.begin() < 0) {
-    ESP_LOGE(TAG, "Failed to read pressure from AMS5915");
+    ESP_LOGE(TAG, "Failed to read pressure from Ams5915");
     this->mark_failed();
   }
 }
@@ -28,10 +191,10 @@ void Ams5915::update() {
 
 
 void Ams5915::dump_config() {
-  ESP_LOGCONFIG(TAG, "ams5915:");
+  ESP_LOGCONFIG(TAG, "Ams5915:");
   LOG_I2C_DEVICE(this);
 }
 
 
-}  // namespace ams5915
+}  // namespace Ams5915
 }  // namespace esphome
--- esphome/components/ams5915/ams5915.h
+++ esphome/components/ams5915/ams5915.h
@@ -4,7 +4,8 @@
 #include "esphome/core/hal.h"
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/sensor/sensor.h"
-#include "AMS5915.h"
+#include "Wire.h"
+#include "Arduino.h"
 
 namespace esphome {
 namespace ams5915 {
@@ -17,10 +18,123 @@ class Ams5915 : public PollingComponent, public sensor::Sensor, public i2c::I2CD
   void update() override;
   void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
   void set_pressure_sensor(sensor::Sensor *pressure_sensor) { pressure_sensor_ = pressure_sensor; }
+  enum Transducer
+  {
+      AMS5915_0005_D,
+      AMS5915_0010_D,
+      AMS5915_0005_D_B,
+      AMS5915_0010_D_B,
+      AMS5915_0020_D,
+      AMS5915_0050_D,
+      AMS5915_0100_D,
+      AMS5915_0020_D_B,
+      AMS5915_0050_D_B,
+      AMS5915_0100_D_B,
+      AMS5915_0200_D,
+      AMS5915_0350_D,
+      AMS5915_1000_D,
+      AMS5915_2000_D,
+      AMS5915_4000_D,
+      AMS5915_7000_D,
+      AMS5915_10000_D,
+      AMS5915_0200_D_B,
+      AMS5915_0350_D_B,
+      AMS5915_1000_D_B,
+      AMS5915_1000_A,
+      AMS5915_1200_B
+  };
+  Ams5915(TwoWire &bus,uint8_t address,Transducer type);
+  int begin();
+  int readSensor();
+  float getPressure_Pa();
+  float getTemperature_C();
 
- protected:
-    sensor::Sensor *temperature_sensor_{nullptr};
-    sensor::Sensor *pressure_sensor_{nullptr};
+  private:
+  // struct to hold sensor data
+    struct Data {
+      float Pressure_Pa;
+      float Temp_C;
+    };
+    Data _data;
+    // I2C bus
+    TwoWire *_bus;
+    // sensor address
+    uint8_t _address;
+    // transducer type
+    Transducer _type;
+    // buffer for I2C data
+    uint8_t _buffer[4];
+    // number of bytes received from I2C
+    size_t _numBytes;
+    // maximum number of attempts to talk to sensor
+    const size_t _maxAttempts = 10;
+    // track success of reading from sensor
+    int _status;
+    // pressure digital output, counts
+    uint16_t _pressureCounts;      
+    // temperature digital output, counts
+    uint16_t _temperatureCounts;   
+    // min and max pressure, millibar
+    int _pMin;
+    int _pMax;
+    // i2c bus frequency
+    const uint32_t _i2cRate = 400000;
+    // conversion millibar to PA
+    const float _mBar2Pa = 100.0f; 
+    // digital output at minimum pressure
+    const int _digOutPmin = 1638;   
+    // digital output at maximum pressure
+    const int _digOutPmax = 14745;  
+    // min and max pressures, millibar
+    const int AMS5915_0005_D_P_MIN = 0;
+    const int AMS5915_0005_D_P_MAX = 5;
+    const int AMS5915_0010_D_P_MIN = 0;
+    const int AMS5915_0010_D_P_MAX = 10;
+    const int AMS5915_0005_D_B_P_MIN = -5;
+    const int AMS5915_0005_D_B_P_MAX = 5;
+    const int AMS5915_0010_D_B_P_MIN = -10;
+    const int AMS5915_0010_D_B_P_MAX = 10;
+    const int AMS5915_0020_D_P_MIN = 0;
+    const int AMS5915_0020_D_P_MAX = 20;
+    const int AMS5915_0050_D_P_MIN = 0;
+    const int AMS5915_0050_D_P_MAX = 50;
+    const int AMS5915_0100_D_P_MIN = 0;
+    const int AMS5915_0100_D_P_MAX = 100;
+    const int AMS5915_0020_D_B_P_MIN = -20;
+    const int AMS5915_0020_D_B_P_MAX = 20;
+    const int AMS5915_0050_D_B_P_MIN = -50;
+    const int AMS5915_0050_D_B_P_MAX = 50;
+    const int AMS5915_0100_D_B_P_MIN = -100;
+    const int AMS5915_0100_D_B_P_MAX = 100;
+    const int AMS5915_0200_D_P_MIN = 0;
+    const int AMS5915_0200_D_P_MAX = 200;
+    const int AMS5915_0350_D_P_MIN = 0;
+    const int AMS5915_0350_D_P_MAX = 350;
+    const int AMS5915_1000_D_P_MIN = 0;
+    const int AMS5915_1000_D_P_MAX = 1000;
+    const int AMS5915_2000_D_P_MIN = 0;
+    const int AMS5915_2000_D_P_MAX = 2000;
+    const int AMS5915_4000_D_P_MIN = 0;
+    const int AMS5915_4000_D_P_MAX = 4000;
+    const int AMS5915_7000_D_P_MIN = 0;
+    const int AMS5915_7000_D_P_MAX = 7000;
+    const int AMS5915_10000_D_P_MIN = 0;
+    const int AMS5915_10000_D_P_MAX = 10000;
+    const int AMS5915_0200_D_B_P_MIN = -200;
+    const int AMS5915_0200_D_B_P_MAX = 200;
+    const int AMS5915_0350_D_B_P_MIN = -350;
+    const int AMS5915_0350_D_B_P_MAX = 350;
+    const int AMS5915_1000_D_B_P_MIN = -1000;
+    const int AMS5915_1000_D_B_P_MAX = 1000;
+    const int AMS5915_1000_A_P_MIN = 0;
+    const int AMS5915_1000_A_P_MAX = 1000;
+    const int AMS5915_1200_B_P_MIN = 700;
+    const int AMS5915_1200_B_P_MAX = 1200;
+    void getTransducer();
+    int readBytes(uint16_t* pressureCounts, uint16_t* temperatureCounts);
+  protected:
+      sensor::Sensor *temperature_sensor_{nullptr};
+      sensor::Sensor *pressure_sensor_{nullptr};
 };
 
 }  // namespace ams5915
