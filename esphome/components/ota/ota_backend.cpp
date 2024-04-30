437b2540e9a784b80933638e75eee99df0bf0ca9
Keith Burzinski <kbx81x@gmail.com>
2024-04-30 02:13:10 -0500

    Update nolint

--- esphome/components/ota/ota_backend.cpp
+++ esphome/components/ota/ota_backend.cpp
@@ -4,8 +4,7 @@ namespace esphome {
 namespace ota {
 
 #ifdef USE_OTA_STATE_CALLBACK
-OTAGlobalCallback *global_ota_component =
-    new OTAGlobalCallback;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+OTAGlobalCallback *global_ota_component = new OTAGlobalCallback;  // NOLINT(cppcoreguidelines-owning-memory)
 #endif
 
 }  // namespace ota
