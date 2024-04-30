68cb348a8b6eb61e36efe69e6cb9c5fcf95bd3c8
Keith Burzinski <kbx81x@gmail.com>
2024-04-30 02:07:47 -0500

    No walruses were harmed in the committing of this commit

--- esphome/components/ota/ota_backend.cpp
+++ esphome/components/ota/ota_backend.cpp
@@ -4,8 +4,9 @@ namespace esphome {
 namespace ota {
 
 #ifdef USE_OTA_STATE_CALLBACK
-OTAGlobalCallback *global_ota_component =
-    new OTAGlobalCallback;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+OTAGlobalCallback *global_ota_component = nullptr;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+
+OTAGlobalCallback::OTAGlobalCallback() { global_ota_component = this; }
 #endif
 
 }  // namespace ota
