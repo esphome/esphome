8427a50719c7a63a8449c10391ab41f75c62491c
Erik <erik.vegting@predata.ch>
2024-05-01 17:18:24 +0200

    Bugfixes

--- esphome/components/ain4_20ma/ain4_20ma.h
+++ esphome/components/ain4_20ma/ain4_20ma.h
@@ -12,11 +12,9 @@ namespace ain4_20ma {
 class Ain4_20maComponent : public i2c::I2CDevice, public PollingComponent, public sensor::Sensor {
  public:
   float get_setup_priority() const override { return setup_priority::DATA; }
-
   void dump_config() override;
-
   void update() override;
 };
 
-}  
-}
+}  // namespace ain4_20ma 
+}  // namespace esphome
