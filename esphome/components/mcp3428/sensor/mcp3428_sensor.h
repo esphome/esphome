5b9898814fbb3629d60b655a108d50b4400fd272
Michael Doppler <76885460+mdop@users.noreply.github.com>
2024-05-02 10:23:20 +0000

    Include delay function in sensor

--- esphome/components/mcp3428/sensor/mcp3428_sensor.h
+++ esphome/components/mcp3428/sensor/mcp3428_sensor.h
@@ -2,6 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/helpers.h"
+#include "esphome/core/hal.h"
 
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/voltage_sampler/voltage_sampler.h"
