54368a0678e1aba819b14273f850a8512a1d1ea6
Tomasz Duda <tomaszduda23@gmail.com>
2024-05-05 21:19:13 +0200

    fix pylint

--- esphome/components/nrf52/boards_zephyr.py
+++ esphome/components/nrf52/boards_zephyr.py
@@ -1,5 +1,5 @@
-from .const import BOOTLOADER_ADAFRUIT
 from esphome.components.zephyr.const import KEY_BOOTLOADER
+from .const import BOOTLOADER_ADAFRUIT
 
 BOARDS_ZEPHYR = {
     "adafruit_itsybitsy_nrf52840": {KEY_BOOTLOADER: BOOTLOADER_ADAFRUIT},
