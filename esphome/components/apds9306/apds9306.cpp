9a3b4674ede01fd522dfbc37cac13eab5b906fb6
Aodren Auffrédou-Heinicke <54121510+aodrenah@users.noreply.github.com>
2024-05-08 23:16:17 -0700

    reordered update

--- esphome/components/apds9306/apds9306.cpp
+++ esphome/components/apds9306/apds9306.cpp
@@ -34,6 +34,71 @@ void APDS9306::setup() {
     return;
   }
 
+  // MAIN_CTRL (0x00)
+  // Trigger software reset
+  APDS9306_WRITE_BYTE(0x00, 0x10);
+  // Put in standby mode
+  APDS9306_WRITE_BYTE(0x00, 0x00);
+
+  // ALS_MEAS_RATE (0x04)
+  uint8_t als_meas_rate = 0;
+  APDS9306_ERROR_CHECK(this->read_byte(0x04, &als_meas_rate));
+  als_meas_rate &= 0x77;
+  // ALS resolution, see datasheet or init.py for options
+  als_meas_rate |= (this->bit_width_ & 0b111) << 4;
+  // ALS measurement rate, see datasheet or init.py for options
+  als_meas_rate |= (this->measurement_rate_ & 0b111);
+
+  APDS9306_WRITE_BYTE(0x04, als_meas_rate);
+
+  // ALS_GAIN (0x05)
+  uint8_t als_gain = 0;
+  APDS9306_ERROR_CHECK(this->read_byte(0x05, &als_gain));
+  als_gain &= 0x07;
+  // ALS gain, see datasheet or init.py for options
+  als_gain |= (this->gain_ & 0b111);
+
+  APDS9306_WRITE_BYTE(0x05, als_gain);
+
+  // MAIN_CTRL (0x00)
+  // Set to active mode
+  APDS9306_WRITE_BYTE(0x00, 0x02);
+}
+
+void APDS9306::dump_config() {
+  LOG_SENSOR("", TAG, this);
+  LOG_I2C_DEVICE(this);
+
+  if (this->is_failed()) {
+    switch (this->error_code_) {
+      case COMMUNICATION_FAILED:
+        ESP_LOGE(TAG, "Communication with APDS9306 failed!");
+        break;
+      case WRONG_ID:
+        ESP_LOGE(TAG, "APDS9306 has invalid id!");
+        break;
+      default:
+        ESP_LOGE(TAG, "Setting up APDS9306 registers failed!");
+        break;
+    }
+  }
+
+  ESP_LOGCONFIG(TAG, "  Gain: %d", gain_val_);
+  ESP_LOGCONFIG(TAG, "  Measurement rate: %dms", rate_val_);
+  ESP_LOGCONFIG(TAG, "  Measurement Resolution/Bit width: %d", bit_width_val_);
+
+  LOG_UPDATE_INTERVAL(this);
+}
+
+void APDS9306::update() {
+  uint8_t status;
+  APDS9306_WARNING_CHECK(this->read_byte(0x07, &status), "Reading status bit failed.");
+
+  this->status_clear_warning();
+
+  if (!(status &= 0b00001000)) // No new data
+    return;
+
   // Conversions
   switch (gain_) {
     case 0:
@@ -95,69 +160,6 @@ void APDS9306::setup() {
       break;
   }
 
-  // MAIN_CTRL (0x00)
-  // Trigger software reset
-  APDS9306_WRITE_BYTE(0x00, 0x10);
-  // Put in standby mode
-  APDS9306_WRITE_BYTE(0x00, 0x00);
-
-  // ALS_MEAS_RATE (0x04)
-  uint8_t als_meas_rate = 0;
-  APDS9306_ERROR_CHECK(this->read_byte(0x04, &als_meas_rate));
-  als_meas_rate &= 0x77;
-  // ALS resolution, see datasheet or init.py for options
-  als_meas_rate |= (this->bit_width_ & 0b111) << 4;
-  // ALS measurement rate, see datasheet or init.py for options
-  als_meas_rate |= (this->measurement_rate_ & 0b111);
-
-  APDS9306_WRITE_BYTE(0x04, als_meas_rate);
-
-  // ALS_GAIN (0x05)
-  uint8_t als_gain = 0;
-  APDS9306_ERROR_CHECK(this->read_byte(0x05, &als_gain));
-  als_gain &= 0x07;
-  // ALS gain, see datasheet or init.py for options
-  als_gain |= (this->gain_ & 0b111);
-
-  APDS9306_WRITE_BYTE(0x05, als_gain);
-
-  // MAIN_CTRL (0x00)
-  // Set to active mode
-  APDS9306_WRITE_BYTE(0x00, 0x02);
-}
-
-void APDS9306::dump_config() {
-  LOG_SENSOR("", TAG, this);
-  LOG_I2C_DEVICE(this);
-
-  if (this->is_failed()) {
-    switch (this->error_code_) {
-      case COMMUNICATION_FAILED:
-        ESP_LOGE(TAG, "Communication with APDS9306 failed!");
-        break;
-      case WRONG_ID:
-        ESP_LOGE(TAG, "APDS9306 has invalid id!");
-        break;
-      default:
-        ESP_LOGE(TAG, "Setting up APDS9306 registers failed!");
-        break;
-    }
-  }
-
-  ESP_LOGCONFIG(TAG, "  Gain: %d", gain_val_);
-  ESP_LOGCONFIG(TAG, "  Measurement rate: %dms", rate_val_);
-  ESP_LOGCONFIG(TAG, "  Measurement Resolution/Bit width: %d", bit_width_val_);
-
-  LOG_UPDATE_INTERVAL(this);
-}
-
-void APDS9306::update() {
-  uint8_t status;
-  APDS9306_WARNING_CHECK(this->read_byte(0x07, &status), "Reading status bit failed.");
-
-  if (!(status &= 0b00001000)) // No new data
-    return;
-
   uint8_t als_data[3];
   APDS9306_WARNING_CHECK(this->read_byte(0x0D, als_data, 3), "Reading ALS data has failed.");
 
@@ -167,7 +169,6 @@ void APDS9306::update() {
 
   ESP_LOGD(TAG, "Got illuminance=%.1flx", lux);
   this->publish_state(lux);
-  this->status_clear_warning();
 }
 
 } // namespace apds9306
