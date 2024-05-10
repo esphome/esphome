b34e288b007c53d69600d721495c9d760737b883
Tomasz Duda <tomaszduda23@gmail.com>
2024-05-10 18:26:42 +0200

    main supply status

--- esphome/components/debug/debug_zephyr.cpp
+++ esphome/components/debug/debug_zephyr.cpp
@@ -3,6 +3,7 @@
 #include <climits>
 #include "esphome/core/log.h"
 #include <zephyr/drivers/hwinfo.h>
+#include <hal/nrf_power.h>
 
 namespace esphome {
 namespace debug {
@@ -50,10 +51,26 @@ std::string DebugComponent::get_reset_reason_() {
 
 uint32_t DebugComponent::get_free_heap_() { return INT_MAX; }
 
-void DebugComponent::get_device_info_(std::string &device_info) {}
+void DebugComponent::get_device_info_(std::string &device_info) {
+  std::string supply = "Main supply status: ";
+  if (nrf_power_mainregstatus_get(NRF_POWER) == NRF_POWER_MAINREGSTATUS_NORMAL) {
+    supply += "Normal voltage.";
+  } else {
+    supply += "High voltage.";
+  }
+  ESP_LOGD(TAG, "%s", supply.c_str());
+  device_info += supply;
+}
 
 void DebugComponent::update_platform_() {}
 
 }  // namespace debug
 }  // namespace esphome
 #endif
+
+//   ESP_LOGD(TAG, "bootloader version %lu.%lu.%lu", (bootloaderVersion >> 16) & 0xFF, (bootloaderVersion >> 8) & 0xFF,
+//            bootloaderVersion & 0xFF);
+//   ESP_LOGD(TAG, "MBR bootloader addr 0x%08lx, UICR bootloader addr 0x%08lx", (*((uint32_t *) MBR_BOOTLOADER_ADDR)),
+//            NRF_UICR->NRFFW[0]);
+//   ESP_LOGD(TAG, "MBR param page addr 0x%08lx, UICR param page addr 0x%08lx", (*((uint32_t *) MBR_PARAM_PAGE_ADDR)),
+//            NRF_UICR->NRFFW[1]);
