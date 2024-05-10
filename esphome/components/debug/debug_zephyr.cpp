650f6d458def09e9388b5c6136f6fb52b4fbe515
Tomasz Duda <tomaszduda23@gmail.com>
2024-05-10 21:05:58 +0200

    add power debug

--- esphome/components/debug/debug_zephyr.cpp
+++ esphome/components/debug/debug_zephyr.cpp
@@ -59,7 +59,167 @@ void DebugComponent::get_device_info_(std::string &device_info) {
     supply += "High voltage.";
   }
   ESP_LOGD(TAG, "%s", supply.c_str());
-  device_info += supply;
+  device_info += "|" + supply;
+
+  std::string reg0 = "Regulator stage 0: ";
+  if (nrf_power_mainregstatus_get(NRF_POWER) == NRF_POWER_MAINREGSTATUS_HIGH) {
+    reg0 += nrf_power_dcdcen_vddh_get(NRF_POWER) ? "DC/DC" : "LDO";
+    reg0 += ", ";
+    switch (NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) {
+      case (UICR_REGOUT0_VOUT_DEFAULT << UICR_REGOUT0_VOUT_Pos):
+        reg0 += "1.8V (default)";
+        break;
+      case (UICR_REGOUT0_VOUT_1V8 << UICR_REGOUT0_VOUT_Pos):
+        reg0 += "1.8V";
+        break;
+      case (UICR_REGOUT0_VOUT_2V1 << UICR_REGOUT0_VOUT_Pos):
+        reg0 += "2.1V";
+        break;
+      case (UICR_REGOUT0_VOUT_2V4 << UICR_REGOUT0_VOUT_Pos):
+        reg0 += "2.4V";
+        break;
+      case (UICR_REGOUT0_VOUT_2V7 << UICR_REGOUT0_VOUT_Pos):
+        reg0 += "2.7V";
+        break;
+      case (UICR_REGOUT0_VOUT_3V0 << UICR_REGOUT0_VOUT_Pos):
+        reg0 += "3.0V";
+        break;
+      case (UICR_REGOUT0_VOUT_3V3 << UICR_REGOUT0_VOUT_Pos):
+        reg0 += "3.3V";
+        break;
+      default:
+        reg0 += "???V";
+    }
+  } else {
+    reg0 += "disabled";
+  }
+  ESP_LOGD(TAG, "%s", reg0.c_str());
+  device_info += "|" + reg0;
+
+  std::string reg1 = "Regulator stage 1: ";
+  reg1 += nrf_power_dcdcen_get(NRF_POWER) ? "DC/DC" : "LDO";
+  ESP_LOGD(TAG, "%s", reg1.c_str());
+  device_info += "|" + reg1;
+
+  std::string usb_power = "USB power state: ";
+  if (nrf_power_usbregstatus_vbusdet_get(NRF_POWER)) {
+    if (nrf_power_usbregstatus_outrdy_get(NRF_POWER)) {
+      /**< From the power viewpoint, USB is ready for working. */
+      usb_power += "ready";
+    } else {
+      /**< The USB power is detected, but USB power regulator is not ready. */
+      usb_power += "connected (regulator is not ready)";
+    }
+  } else {
+    /**< No power on USB lines detected. */
+    usb_power += "disconected";
+  }
+  ESP_LOGD(TAG, "%s", usb_power.c_str());
+  device_info += "|" + usb_power;
+
+  bool enabled;
+  nrf_power_pof_thr_t pof_thr;
+
+  pof_thr = nrf_power_pofcon_get(NRF_POWER, &enabled);
+  std::string pof = "Power-fail comparator: ";
+  if (enabled) {
+    switch (pof_thr) {
+      case POWER_POFCON_THRESHOLD_V17:
+        pof += "1.7V";
+        break;
+      case POWER_POFCON_THRESHOLD_V18:
+        pof += "1.8V";
+        break;
+      case POWER_POFCON_THRESHOLD_V19:
+        pof += "1.9V";
+        break;
+      case POWER_POFCON_THRESHOLD_V20:
+        pof += "2.0V";
+        break;
+      case POWER_POFCON_THRESHOLD_V21:
+        pof += "2.1V";
+        break;
+      case POWER_POFCON_THRESHOLD_V22:
+        pof += "2.2V";
+        break;
+      case POWER_POFCON_THRESHOLD_V23:
+        pof += "2.3V";
+        break;
+      case POWER_POFCON_THRESHOLD_V24:
+        pof += "2.4V";
+        break;
+      case POWER_POFCON_THRESHOLD_V25:
+        pof += "2.5V";
+        break;
+      case POWER_POFCON_THRESHOLD_V26:
+        pof += "2.6V";
+        break;
+      case POWER_POFCON_THRESHOLD_V27:
+        pof += "2.7V";
+        break;
+      case POWER_POFCON_THRESHOLD_V28:
+        pof += "2.8V";
+        break;
+    }
+
+    if (nrf_power_mainregstatus_get(NRF_POWER) == NRF_POWER_MAINREGSTATUS_HIGH) {
+      pof += ", VDDH: ";
+      switch (nrf_power_pofcon_vddh_get(NRF_POWER)) {
+        case NRF_POWER_POFTHRVDDH_V27:
+          pof += "2.7V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V28:
+          pof += "2.8V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V29:
+          pof += "2.9V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V30:
+          pof += "3.0V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V31:
+          pof += "3.1V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V32:
+          pof += "3.2V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V33:
+          pof += "3.3V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V34:
+          pof += "3.4V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V35:
+          pof += "3.5V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V36:
+          pof += "3.6V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V37:
+          pof += "3.7V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V38:
+          pof += "3.8V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V39:
+          pof += "3.9V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V40:
+          pof += "4.0V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V41:
+          pof += "4.1V";
+          break;
+        case NRF_POWER_POFTHRVDDH_V42:
+          pof += "4.2V";
+          break;
+      }
+    }
+  } else {
+    pof += "disabled";
+  }
+  ESP_LOGD(TAG, "%s", pof.c_str());
+  device_info += "|" + pof;
 }
 
 void DebugComponent::update_platform_() {}
