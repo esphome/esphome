3a95ab97f9ca21e76b9edb0e765df5d4656bc4fa
Aodren Auffrédou-Heinicke <54121510+aodrenah@users.noreply.github.com>
2024-05-14 17:12:38 -0700

    testing

--- esphome/components/apds9306/sensor.py
+++ esphome/components/apds9306/sensor.py
@@ -61,7 +61,7 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_BIT_WIDTH, default="18"): cv.enum(
                 MEASUREMENT_BIT_WIDTHS, int=True
             ),
-            cv.Optional(CONF_MEASUREMENT_RATE, default="100ms"): cv.All(
+            cv.Optional(CONF_MEASUREMENT_RATE, default="100"): cv.All(
                 cv.positive_time_period_milliseconds,
                 cv.enum(MEASUREMENT_RATES, int=True),
             ),
