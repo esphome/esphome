7d477ac8fd28f2b584411ac22d06c498eaad4975
Keith Burzinski <kbx81x@gmail.com>
2024-05-16 04:12:51 -0500

    Uncouple safe_mode from OTA

--- esphome/components/esphome/ota/__init__.py
+++ esphome/components/esphome/ota/__init__.py
@@ -1,19 +1,13 @@
-from esphome.cpp_generator import RawExpression
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components.ota import BASE_OTA_SCHEMA, ota_to_code, OTAComponent
 from esphome.const import (
     CONF_ID,
-    CONF_NUM_ATTEMPTS,
-    CONF_OTA,
     CONF_PASSWORD,
     CONF_PORT,
-    CONF_REBOOT_TIMEOUT,
-    CONF_SAFE_MODE,
     CONF_VERSION,
-    KEY_PAST_SAFE_MODE,
 )
-from esphome.core import CORE, coroutine_with_priority
+from esphome.core import coroutine_with_priority
 
 
 CODEOWNERS = ["@esphome/core"]
@@ -28,7 +22,6 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(ESPHomeOTAComponent),
-            cv.Optional(CONF_SAFE_MODE, default=True): cv.boolean,
             cv.Optional(CONF_VERSION, default=2): cv.one_of(1, 2, int=True),
             cv.SplitDefault(
                 CONF_PORT,
@@ -39,10 +32,6 @@ CONFIG_SCHEMA = (
                 rtl87xx=8892,
             ): cv.port,
             cv.Optional(CONF_PASSWORD): cv.string,
-            cv.Optional(
-                CONF_REBOOT_TIMEOUT, default="5min"
-            ): cv.positive_time_period_milliseconds,
-            cv.Optional(CONF_NUM_ATTEMPTS, default="10"): cv.positive_not_null_int,
         }
     )
     .extend(BASE_OTA_SCHEMA)
@@ -50,10 +39,8 @@ CONFIG_SCHEMA = (
 )
 
 
-@coroutine_with_priority(50.0)
+@coroutine_with_priority(52.0)
 async def to_code(config):
-    CORE.data[CONF_OTA] = {}
-
     var = cg.new_Pvariable(config[CONF_ID])
     await ota_to_code(var, config)
     cg.add(var.set_port(config[CONF_PORT]))
@@ -63,10 +50,3 @@ async def to_code(config):
     cg.add_define("USE_OTA_VERSION", config[CONF_VERSION])
 
     await cg.register_component(var, config)
-
-    if config[CONF_SAFE_MODE]:
-        condition = var.should_enter_safe_mode(
-            config[CONF_NUM_ATTEMPTS], config[CONF_REBOOT_TIMEOUT]
-        )
-        cg.add(RawExpression(f"if ({condition}) return"))
-        CORE.data[CONF_OTA][KEY_PAST_SAFE_MODE] = True
