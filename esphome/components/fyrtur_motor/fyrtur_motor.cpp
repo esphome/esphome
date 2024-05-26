b6c4aeab758098e6ba2ecd9d047e38ab160d6a55
Simas Bašinskas <simas.basinskas@gmail.com>
2024-05-26 13:24:31 +0000

    WIP

--- esphome/components/fyrtur_motor/fyrtur_motor.cpp
+++ esphome/components/fyrtur_motor/fyrtur_motor.cpp
@@ -130,7 +130,7 @@ void FyrturMotorComponent::get_status() {
   }
 
   float battery_level = response[0];
-  float battery_voltage = (float) response[1] / 31;
+  float battery_voltage = (float) response[1] / 30.5;
   uint8_t speed = response[2];
   uint8_t position = response[3];
 
