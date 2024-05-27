50a87b84bb9311c034a5f5efb16f548b4ad602a9
NP v/d Spek <github_mail@lumensoft.nl>
2024-05-28 00:37:01 +0200

    extend Speaker yaml option with condition  is_stopped and action finish

--- esphome/components/speaker/automation.h
+++ esphome/components/speaker/automation.h
@@ -39,9 +39,19 @@ template<typename... Ts> class StopAction : public Action<Ts...>, public Parente
   void play(Ts... x) override { this->parent_->stop(); }
 };
 
+template<typename... Ts> class FinishAction : public Action<Ts...>, public Parented<Speaker> {
+ public:
+  void play(Ts... x) override { this->parent_->finish(); }
+};
+
 template<typename... Ts> class IsPlayingCondition : public Condition<Ts...>, public Parented<Speaker> {
  public:
   bool check(Ts... x) override { return this->parent_->is_running(); }
+};
+
+template<typename... Ts> class IsStoppedCondition : public Condition<Ts...>, public Parented<Speaker> {
+ public:
+  bool check(Ts... x) override { return this->parent_->is_stopped(); }
 };
 
 }  // namespace speaker
