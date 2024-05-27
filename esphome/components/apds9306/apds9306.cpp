020a37c4a276458ac962237ddf3ea2850b408142
Aodren Auffrédou-Heinicke <54121510+aodrenah@users.noreply.github.com>
2024-05-27 10:54:37 -0700

    added check to see if setup is running

--- esphome/components/apds9306/apds9306.cpp
+++ esphome/components/apds9306/apds9306.cpp
@@ -52,6 +52,7 @@ enum { // APDS9306 registers
 
 void APDS9306::setup() {
   ESP_LOGCONFIG(TAG, "Setting up APDS9306...");
+  ESP_LOGE(TAG, "SETUP RAN");
 
   uint8_t id;
   if (!this->read_byte(APDS9306_PART_ID, &id)) {  // Part ID register
