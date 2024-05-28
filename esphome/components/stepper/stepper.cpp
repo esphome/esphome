d8dd3148eba4fce644d434e6affddc9faae65ac0
cvwillegen <github.com@vanwillegen-vannoort.nl>
2024-05-28 13:01:27 +0200

    Handle clang feedback.

--- esphome/components/stepper/stepper.cpp
+++ esphome/components/stepper/stepper.cpp
@@ -38,7 +38,8 @@ int32_t Stepper::should_step_() {
   // assumes this method is called in a constant interval
   uint32_t dt = now - this->last_step_;
   if (dt >= (1 / this->current_speed_) * 1e6f) {
-    int32_t mag = (rotation_ == ROTATION_BOTH ? (this->target_position > this->current_position ? 1 : -1) : (rotation_ == ROTATION_CW ? 1 : -1));
+    int32_t mag = (rotation_ == ROTATION_BOTH ? (this->target_position > this->current_position ? 1 : -1)
+                                              : (rotation_ == ROTATION_CW ? 1 : -1));
     this->last_step_ = now;
     this->current_position += mag;
     return mag;
