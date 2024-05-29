700ba0e16283243e66a41f86d4a27d2538881726
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-05-29 14:27:08 +1200

    Update esphome/components/voice_assistant/voice_assistant.h

--- esphome/components/voice_assistant/voice_assistant.h
+++ esphome/components/voice_assistant/voice_assistant.h
@@ -71,8 +71,9 @@ struct Timer {
   bool is_active;
 
   std::string to_string() const {
-    return str_sprintf("Timer(id=%s, name=%s, total_seconds=%" PRIu32 ", seconds_left=%" PRIu32 ", is_active=%s)", this->id.c_str(),
-                       this->name.c_str(), this->total_seconds, this->seconds_left, YESNO(this->is_active));
+    return str_sprintf("Timer(id=%s, name=%s, total_seconds=%" PRIu32 ", seconds_left=%" PRIu32 ", is_active=%s)",
+                       this->id.c_str(), this->name.c_str(), this->total_seconds, this->seconds_left,
+                       YESNO(this->is_active));
   }
 };
 
