37848e0660c332bd066d64cb5b3d7b23635a31b7
Aodren Auffrédou-Heinicke <54121510+aodrenah@users.noreply.github.com>
2024-05-29 15:57:26 -0700

    testing

--- esphome/components/apds9306/apds9306.cpp
+++ esphome/components/apds9306/apds9306.cpp
@@ -46,7 +46,7 @@ enum {  // APDS9306 registers
     return; \
   }
 #define APDS9306_WRITE_BYTE(reg, value) \
-  ESP_LOGV(TAG, "Writing %x to %x", value, reg); \
+  ESP_LOGD(TAG, "Writing %x to %x", value, reg); \
   if (!this->write_byte(reg, value)) { \
     ESP_LOGE(TAG, "Failed writing 0x%x to 0x%x", value, reg); \
     this->mark_failed(); \
