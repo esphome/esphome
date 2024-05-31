b0c5b1ddbb5c8fb20906f16797c76c3dc574c29d
cptskippy <github@toyrobotworkshop.com>
2024-05-31 16:17:11 +0000

    bounding buffer copy

--- esphome/components/ld2415h/ld2415h.cpp
+++ esphome/components/ld2415h/ld2415h.cpp
@@ -291,7 +291,7 @@ void LD2415HComponent::parse_firmware_() {
     ++fw;
 
     // Copy string into firmware
-    std::strcpy(this->firmware_, fw);
+    std::strncpy(this->firmware_, fw, sizeof(this->firmware_));
   } else {
     ESP_LOGE(TAG, "Firmware value invalid.");
   }
