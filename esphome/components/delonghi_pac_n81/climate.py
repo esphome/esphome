4ff11cf3d4b33c0b011b3df67f56ac96e212e738
Bernd Zeidler <zeidler.b@gmail.com>
2024-06-03 22:13:52 +0200

    chore: formate component py file

--- esphome/components/delonghi_pac_n81/climate.py
+++ esphome/components/delonghi_pac_n81/climate.py
@@ -6,7 +6,9 @@ from esphome.const import CONF_ID
 AUTO_LOAD = ["climate_ir"]
 
 delonghi_pac_n81_ns = cg.esphome_ns.namespace("delonghi_pac_n81")
-DelonghiClimatePacN81 = delonghi_pac_n81_ns.class_("DelonghiClimate", climate_ir.ClimateIR)
+DelonghiClimatePacN81 = delonghi_pac_n81_ns.class_(
+    "DelonghiClimate", climate_ir.ClimateIR
+)
 
 CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
