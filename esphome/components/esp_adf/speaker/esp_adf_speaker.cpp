85a3789fb9104c1d95461ff6704a88f61e2c1c08
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-06-05 13:41:18 +1200

    Fix i2s channel

--- esphome/components/esp_adf/speaker/esp_adf_speaker.cpp
+++ esphome/components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -64,7 +64,7 @@ void ESPADFSpeaker::player_task(void *params) {
       .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX),
       .sample_rate = 16000,
       .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-      .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
+      .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
       .communication_format = I2S_COMM_FORMAT_STAND_I2S,
       .intr_alloc_flags = ESP_INTR_FLAG_LEVEL2 | ESP_INTR_FLAG_IRAM,
       .dma_buf_count = 8,
