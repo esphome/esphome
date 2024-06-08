960a3f17363964bf4baa9cc53bb9003546e44f18
Keith Burzinski <kbx81x@gmail.com>
2024-06-07 21:18:56 -0500

    Fix "follow redirects" string in config dump

--- esphome/components/http_request/http_request.cpp
+++ esphome/components/http_request/http_request.cpp
@@ -11,7 +11,7 @@ void HttpRequestComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "HTTP Request:");
   ESP_LOGCONFIG(TAG, "  Timeout: %ums", this->timeout_);
   ESP_LOGCONFIG(TAG, "  User-Agent: %s", this->useragent_);
-  ESP_LOGCONFIG(TAG, "  Follow Redirects: %d", this->follow_redirects_);
+  ESP_LOGCONFIG(TAG, "  Follow redirects: %s", YESNO(this->follow_redirects_));
   ESP_LOGCONFIG(TAG, "  Redirect limit: %d", this->redirect_limit_);
   if (this->watchdog_timeout_ > 0) {
     ESP_LOGCONFIG(TAG, "  Watchdog Timeout: %ums", this->watchdog_timeout_);
