23d77ae0cf32e14cfaf65dea1fc3546266a76c4d
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-06-11 10:07:39 +1200

    Add http_request update entity

---
+++ esphome/components/http_request/update/http_request_update.cpp
@@ -0,0 +1,98 @@
+#include "http_request_update.h"
+
+#include "esphome/core/application.h"
+#include "esphome/core/version.h"
+
+#include "esphome/components/json/json_util.h"
+#include "esphome/components/network/util.h"
+
+namespace esphome {
+namespace http_request {
+
+static const char *const TAG = "http_request.update";
+
+static const size_t MAX_READ_SIZE = 256;
+
+void HttpRequestUpdate::update() {
+  auto container = this->request_parent_->get(this->source_url_);
+
+  if (container == nullptr) {
+    std::string msg = str_sprintf("Failed to fetch manifest from %s", this->source_url_.c_str());
+    this->status_set_error(msg.c_str());
+    return;
+  }
+
+  ExternalRAMAllocator<uint8_t> allocator(ExternalRAMAllocator<uint8_t>::ALLOW_FAILURE);
+  uint8_t *data = allocator.allocate(container->content_length);
+
+  size_t read_index = 0;
+  while (container->get_bytes_read() < container->content_length) {
+    int read_bytes = container->read(data + read_index, MAX_READ_SIZE);
+
+    App.feed_wdt();
+    yield();
+
+    read_index += read_bytes;
+  }
+
+  std::string response(reinterpret_cast<char *>(data), read_index);
+  container->end();
+
+  bool valid = json::parse_json(response, [this](JsonObject root) -> bool {
+    if (!root.containsKey("firmware_url") || !root.containsKey("md5") || !root.containsKey("version")) {
+      return false;
+    }
+    this->update_info_.firmware_url = root["firmware_url"].as<std::string>();
+    this->update_info_.md5 = root["md5"].as<std::string>();
+    this->update_info_.latest_version = root["version"].as<std::string>();
+
+    if (root.containsKey("title"))
+      this->update_info_.title = root["title"].as<std::string>();
+    if (root.containsKey("summary"))
+      this->update_info_.summary = root["summary"].as<std::string>();
+    if (root.containsKey("release_url"))
+      this->update_info_.release_url = root["release_url"].as<std::string>();
+    return true;
+  });
+
+  if (!valid) {
+    std::string msg = str_sprintf("Failed to parse JSON from %s", this->source_url_.c_str());
+    this->status_set_error(msg.c_str());
+    return;
+  }
+
+  std::string current_version = this->current_version_;
+  if (current_version.empty()) {
+#ifdef ESPHOME_PROJECT_VERSION
+    current_version = ESPHOME_PROJECT_VERSION;
+#else
+    current_version = ESPHOME_VERSION;
+#endif
+  }
+  this->update_info_.current_version = current_version;
+
+  if (this->update_info_.latest_version.empty()) {
+    this->state_ = update::UPDATE_STATE_NO_UPDATE;
+  } else if (this->update_info_.latest_version != this->current_version_) {
+    this->state_ = update::UPDATE_STATE_AVAILABLE;
+  }
+
+  this->publish_state();
+}
+
+void HttpRequestUpdate::perform() {
+  if (this->state_ != update::UPDATE_STATE_AVAILABLE) {
+    return;
+  }
+
+  this->state_ = update::UPDATE_STATE_INSTALLING;
+  this->publish_state();
+
+  this->ota_parent_->set_md5(this->update_info.md5);
+  this->ota_parent_->set_url(this->update_info.firmware_url);
+  // Flash in the next loop
+  this->defer([this]() { this->ota_parent_->flash(); });
+}
+
+}  // namespace http_request
+}  // namespace esphome
