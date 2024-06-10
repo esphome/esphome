1de1290bbed63b5cd0a0cbf5206b94018fb3e44d
Anton Sergunov <setosha@gmail.com>
2024-06-10 12:25:08 +0600

    Update esphome/components/sntp/sntp_component.h

--- esphome/components/sntp/sntp_component.h
+++ esphome/components/sntp/sntp_component.h
@@ -35,7 +35,7 @@ class SNTPComponent : public time::RealTimeClock {
   std::string servers_[3];
 
  protected:
-#if defined(USE_ESP_IDF)
+#if !defined(USE_ESP_IDF)
   bool has_time_{false};
 #endif
   bool servers_was_setup_{false};
