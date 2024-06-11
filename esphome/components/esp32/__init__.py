a47720e9e505134d0b6c6a5aa4155dfa581bfba6
Hristo Kapanakov <hr.kapanakov@gmail.com>
2024-06-11 16:35:50 +0300

    Allow adding Arduino as ESP-IDF component

--- esphome/components/esp32/__init__.py
+++ esphome/components/esp32/__init__.py
@@ -7,6 +7,7 @@ import esphome.final_validate as fv
 
 from esphome.helpers import copy_file_if_changed, write_file_if_changed, mkdir_p
 from esphome.const import (
+    CONF_ADD_ARDUINO_COMPONENT,
     CONF_ADVANCED,
     CONF_BOARD,
     CONF_COMPONENTS,
@@ -404,6 +405,7 @@ ESP_IDF_FRAMEWORK_SCHEMA = cv.All(
                     }
                 )
             ),
+            cv.Optional(CONF_ADD_ARDUINO_COMPONENT, default=False): cv.boolean,
         }
     ),
     _esp_idf_check_versions,
@@ -475,7 +477,10 @@ async def to_code(config):
     )
 
     if conf[CONF_TYPE] == FRAMEWORK_ESP_IDF:
-        cg.add_platformio_option("framework", "espidf")
+        if conf[CONF_ADD_ARDUINO_COMPONENT]:
+            cg.add_platformio_option("framework", "arduino, espidf")
+        else:
+            cg.add_platformio_option("framework", "espidf")
         cg.add_build_flag("-DUSE_ESP_IDF")
         cg.add_build_flag("-DUSE_ESP32_FRAMEWORK_ESP_IDF")
         cg.add_build_flag("-Wno-nonnull-compare")
