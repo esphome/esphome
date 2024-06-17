b41ef8bd79208b55abe54ebe510f0a44d60499f8
Manuel Kasper <mk@neon1.net>
2024-06-17 19:01:13 +0200

    Check that width/height/offset are positive

--- esphome/components/qspi_amoled/display.py
+++ esphome/components/qspi_amoled/display.py
@@ -44,6 +44,7 @@ DATA_PIN_SCHEMA = pins.internal_gpio_output_pin_schema
 
 
 def validate_dimension(value):
+    value = cv.positive_int(value)
     if value % 2 != 0:
         raise cv.Invalid("Width/height/offset must be divisible by 2")
     return value
@@ -59,18 +60,14 @@ CONFIG_SCHEMA = cv.All(
                     cv.dimensions,
                     cv.Schema(
                         {
-                            cv.Required(CONF_WIDTH): cv.All(
-                                cv.int_, validate_dimension
-                            ),
-                            cv.Required(CONF_HEIGHT): cv.All(
-                                cv.int_, validate_dimension
-                            ),
-                            cv.Optional(CONF_OFFSET_HEIGHT, default=0): cv.All(
-                                cv.int_, validate_dimension
-                            ),
-                            cv.Optional(CONF_OFFSET_WIDTH, default=0): cv.All(
-                                cv.int_, validate_dimension
-                            ),
+                            cv.Required(CONF_WIDTH): validate_dimension,
+                            cv.Required(CONF_HEIGHT): validate_dimension,
+                            cv.Optional(
+                                CONF_OFFSET_HEIGHT, default=0
+                            ): validate_dimension,
+                            cv.Optional(
+                                CONF_OFFSET_WIDTH, default=0
+                            ): validate_dimension,
                         }
                     ),
                 ),
