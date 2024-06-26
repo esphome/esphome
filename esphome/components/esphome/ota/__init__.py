6c085aa14ca4227b5e5481b7441457eb36511e3a
Keith Burzinski <kbx81x@gmail.com>
2024-06-26 17:51:49 -0500

    Merge configs by port

--- esphome/components/esphome/ota/__init__.py
+++ esphome/components/esphome/ota/__init__.py
@@ -1,7 +1,10 @@
+import logging
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 import esphome.final_validate as fv
 from esphome.components.ota import BASE_OTA_SCHEMA, ota_to_code, OTAComponent
+from esphome.config_helpers import merge_config
 from esphome.const import (
     CONF_ESPHOME,
     CONF_ID,
@@ -16,6 +19,8 @@ from esphome.const import (
 )
 from esphome.core import coroutine_with_priority
 
+_LOGGER = logging.getLogger(__name__)
+
 
 CODEOWNERS = ["@esphome/core"]
 AUTO_LOAD = ["md5", "socket"]
@@ -26,16 +31,39 @@ ESPHomeOTAComponent = esphome.class_("ESPHomeOTAComponent", OTAComponent)
 
 
 def ota_esphome_final_validate(config):
-    fconf = fv.full_config.get()[CONF_OTA]
-    used_ports = []
-    for ota_conf in fconf:
+    full_conf = fv.full_config.get()
+    full_ota_conf = full_conf[CONF_OTA]
+    new_ota_conf = []
+    merged_ota_esphome_configs_by_port = {}
+    ports_with_merged_configs = []
+    for ota_conf in full_ota_conf:
         if ota_conf.get(CONF_PLATFORM) == CONF_ESPHOME:
-            if (plat_port := ota_conf.get(CONF_PORT)) not in used_ports:
-                used_ports.append(plat_port)
+            if (
+                conf_port := ota_conf.get(CONF_PORT)
+            ) not in merged_ota_esphome_configs_by_port:
+                merged_ota_esphome_configs_by_port[conf_port] = ota_conf
             else:
-                raise cv.Invalid(
-                    f"Only one instance of the {CONF_ESPHOME} {CONF_OTA} {CONF_PLATFORM} is allowed per port. Note that this error may result from OTA specified in packages"
+                ports_with_merged_configs.append(conf_port)
+                merged_ota_esphome_configs_by_port[conf_port] = merge_config(
+                    merged_ota_esphome_configs_by_port[conf_port], ota_conf
                 )
+        else:
+            new_ota_conf.append(ota_conf)
+
+    for port_conf in merged_ota_esphome_configs_by_port.values():
+        new_ota_conf.append(port_conf)
+
+    full_conf[CONF_OTA] = new_ota_conf
+    fv.full_config.set(full_conf)
+
+    if len(ports_with_merged_configs) > 0:
+        _LOGGER.warning(
+            "Found and merged multiple configurations for %s %s %s port(s) %s",
+            CONF_OTA,
+            CONF_PLATFORM,
+            CONF_ESPHOME,
+            ports_with_merged_configs,
+        )
 
 
 CONFIG_SCHEMA = (
