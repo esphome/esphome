3dab47d701cfcc042ad722d6521f01f5347b0a87
CptSkippy <cptskippy@users.noreply.github.com>
2024-07-02 09:03:50 -0700

    reordering import

--- esphome/components/bme680_bsec/__init__.py
+++ esphome/components/bme680_bsec/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, esp32
-from esphome.const import CONF_ID, CONF_TEMPERATURE_OFFSET, CONF_SAMPLE_RATE
+from esphome.const import CONF_ID, CONF_SAMPLE_RATE, CONF_TEMPERATURE_OFFSET
 
 CODEOWNERS = ["@trvrnrth"]
 DEPENDENCIES = ["i2c"]
