818b6c5696949589d221cec9dbb15bf86dfe236a
soeffi <soeffing@gmx.de>
2024-07-05 00:23:30 +0200

    perform checksum calculation according to selected mode

--- esphome/components/jsn_sr04t/jsn_sr04t.cpp
+++ esphome/components/jsn_sr04t/jsn_sr04t.cpp
@@ -31,7 +31,10 @@ void Jsnsr04tComponent::loop() {
 }
 
 void Jsnsr04tComponent::check_buffer_() {
-  uint8_t checksum = this->buffer_[0] + this->buffer_[1] + this->buffer_[2];
+  uint8_t checksum = this->buffer_[1] + this->buffer_[2];
+  if(!ajsr04m_)
+    checksum += this->buffer_[0];
+  
   if (this->buffer_[3] == checksum) {
     uint16_t distance = encode_uint16(this->buffer_[1], this->buffer_[2]);
     if (distance > 250) {
@@ -49,6 +52,7 @@ void Jsnsr04tComponent::check_buffer_() {
 
 void Jsnsr04tComponent::dump_config() {
   LOG_SENSOR("", "JST_SR04T Sensor", this);
+  ESP_LOGCONFIG(TAG, "  checksum mode: %s", this->ajsr04m_ ? "aj_sr04m (exclude start byte)" : "jsn_sr04t (include start byte)");
   LOG_UPDATE_INTERVAL(this);
 }
 
