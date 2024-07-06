a6dbc2fe0fbf3443dc0a669df0d77a390f08bf0c
NewoPL <27411874+NewoPL@users.noreply.github.com>
2024-07-06 19:41:53 +0200

    bug #6014: workaround for not setting hostname on arduino plarform

--- esphome/components/wifi/wifi_component_esp32_arduino.cpp
+++ esphome/components/wifi/wifi_component_esp32_arduino.cpp
@@ -495,6 +495,7 @@ void WiFiComponent::wifi_event_callback_(esphome_wifi_event_id_t event, esphome_
     case ESPHOME_EVENT_ID_WIFI_STA_START: {
       ESP_LOGV(TAG, "Event: WiFi STA start");
       // apply hostname
+      s_sta_netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
       esp_err_t err = esp_netif_set_hostname(s_sta_netif, App.get_name().c_str());
       if (err != ERR_OK) {
         ESP_LOGW(TAG, "esp_netif_set_hostname failed: %s", esp_err_to_name(err));
