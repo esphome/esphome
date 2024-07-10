5e6a09952cd7f7e0ce3287af9166368f9dcd0d24
Z3LIFF <z3liff@users.noreply.github.com>
2024-07-09 22:01:17 -0400

    Update pmsa003i.cpp
    whitespace -- this is what I get for doing this on a web browser at night...

--- esphome/components/pmsa003i/pmsa003i.cpp
+++ esphome/components/pmsa003i/pmsa003i.cpp
@@ -17,7 +17,7 @@ void PMSA003IComponent::setup() {
   if (!successful_read) {
     for (int i = 0; i < 10; i++) {
       ESP_LOGW(TAG, "Setup read failed. Trying again.");
-      delay(100); // NOLINT
+      delay(100);  // NOLINT
       successful_read = this->read_data_(&data);
       if (successful_read) {
         break;
