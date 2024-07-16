f153a7b0fd20bdf9a6b0772553c99f05021b04b7
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-07-16 19:18:43 +1200

    [ota] Print Arduino update errors (#7096)

--- esphome/components/ota/ota_backend_arduino_esp32.cpp
+++ esphome/components/ota/ota_backend_arduino_esp32.cpp
@@ -1,13 +1,16 @@
 #ifdef USE_ESP32_FRAMEWORK_ARDUINO
 #include "esphome/core/defines.h"
+#include "esphome/core/log.h"
 
-#include "ota_backend_arduino_esp32.h"
 #include "ota_backend.h"
+#include "ota_backend_arduino_esp32.h"
 
 #include <Update.h>
 
 namespace esphome {
 namespace ota {
+
+static const char *const TAG = "ota.arduino_esp32";
 
 std::unique_ptr<ota::OTABackend> make_ota_backend() { return make_unique<ota::ArduinoESP32OTABackend>(); }
 
@@ -20,6 +23,9 @@ OTAResponseTypes ArduinoESP32OTABackend::begin(size_t image_size) {
   uint8_t error = Update.getError();
   if (error == UPDATE_ERROR_SIZE)
     return OTA_RESPONSE_ERROR_ESP32_NOT_ENOUGH_SPACE;
+
+  ESP_LOGE(TAG, "Begin error: %d", error);
+
   return OTA_RESPONSE_ERROR_UNKNOWN;
 }
 
@@ -27,16 +33,25 @@ void ArduinoESP32OTABackend::set_update_md5(const char *md5) { Update.setMD5(md5
 
 OTAResponseTypes ArduinoESP32OTABackend::write(uint8_t *data, size_t len) {
   size_t written = Update.write(data, len);
-  if (written != len) {
-    return OTA_RESPONSE_ERROR_WRITING_FLASH;
+  if (written == len) {
+    return OTA_RESPONSE_OK;
   }
-  return OTA_RESPONSE_OK;
+
+  uint8_t error = Update.getError();
+  ESP_LOGE(TAG, "Write error: %d", error);
+
+  return OTA_RESPONSE_ERROR_WRITING_FLASH;
 }
 
 OTAResponseTypes ArduinoESP32OTABackend::end() {
-  if (!Update.end())
-    return OTA_RESPONSE_ERROR_UPDATE_END;
-  return OTA_RESPONSE_OK;
+  if (Update.end()) {
+    return OTA_RESPONSE_OK;
+  }
+
+  uint8_t error = Update.getError();
+  ESP_LOGE(TAG, "End error: %d", error);
+
+  return OTA_RESPONSE_ERROR_UPDATE_END;
 }
 
 void ArduinoESP32OTABackend::abort() { Update.abort(); }
