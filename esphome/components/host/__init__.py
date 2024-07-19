be60cfd56cd426ea72dbeaea5b50f92b61baaff4
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-07-19 17:04:41 +1000

    Remove libsodium

--- esphome/components/host/__init__.py
+++ esphome/components/host/__init__.py
@@ -1,6 +1,5 @@
-import os.path
-import subprocess
-
+import esphome.codegen as cg
+import esphome.config_validation as cv
 from esphome.const import (
     KEY_CORE,
     KEY_FRAMEWORK_VERSION,
@@ -9,12 +8,7 @@ from esphome.const import (
     PLATFORM_HOST,
     CONF_MAC_ADDRESS,
 )
-from esphome.core import CORE, EsphomeError
-from esphome.helpers import IS_MACOS, IS_LINUX
-from esphome.components.api import CONF_ENCRYPTION
-import esphome.config_validation as cv
-import esphome.codegen as cg
-
+from esphome.core import CORE
 from .const import KEY_HOST
 
 # force import gpio to register pin schema
@@ -41,43 +35,8 @@ CONFIG_SCHEMA = cv.All(
     set_core_data,
 )
 
-MAC_LIBSODIUM = ("/opt/homebrew/lib/libsodium.a", "/usr/local/lib/libsodium.a")
-
-
-def libsodium():
-    if api_conf := CORE.config.get("api"):
-        if CONF_ENCRYPTION in api_conf:
-            if IS_MACOS:
-                files = list(filter(os.path.isfile, MAC_LIBSODIUM))
-                if not files:
-                    raise EsphomeError(
-                        "libsodium required for api encryption - install with `brew install libsodium'"
-                    )
-                # Apple clang does not find this library with -lsodium apparently because there is already a libsodium.a
-                # in the build tree, linked explicitly.
-                cg.add_build_flag(files[0])
-            elif IS_LINUX:
-                try:
-                    with subprocess.Popen(
-                        ("apt", "-qq", "list", "libsodium-dev"),
-                        stderr=subprocess.DEVNULL,
-                        stdout=subprocess.PIPE,
-                    ).stdout as stdout:
-                        sodium = str(stdout.read())
-                        if "libsodium-dev" in sodium and "installed" not in sodium:
-                            raise EsphomeError(
-                                "libsodium required for api encryption - install with `sudo apt install libsodium-dev'"
-                            )
-                except FileNotFoundError:
-                    pass
-                cg.add_build_flag("-lsodium")
-            else:
-                # How to check on Windows? Who knows.
-                cg.add_build_flag("-lsodium")
-
 
 async def to_code(config):
-    libsodium()
     cg.add_build_flag("-DUSE_HOST")
     cg.add_define("USE_ESPHOME_HOST_MAC_ADDRESS", config[CONF_MAC_ADDRESS].parts)
     cg.add_build_flag("-std=c++17")
