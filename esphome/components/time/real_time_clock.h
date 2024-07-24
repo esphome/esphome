94ce0576cf8d63b695401f8777a697331583bce6
Tomasz Duda <tomaszduda23@gmail.com>
2024-07-24 19:38:03 +0200

    fix clang

--- esphome/components/time/real_time_clock.h
+++ esphome/components/time/real_time_clock.h
@@ -37,7 +37,9 @@ class RealTimeClock : public PollingComponent {
 
   void call_setup() override;
 
-  void add_on_time_sync_callback(const std::function<void()> &callback) { this->time_sync_callback_.add(callback); };
+  void add_on_time_sync_callback(std::function<void()> &&callback) {
+    this->time_sync_callback_.add(std::move(callback));
+  };
 
  protected:
   /// Report a unix epoch as current time.
