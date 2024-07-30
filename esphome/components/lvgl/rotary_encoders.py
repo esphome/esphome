3e7c36fe9d7fc75c0305fbc724522796494d9f6f
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-07-30 14:43:15 +1000

    Review changes

--- esphome/components/lvgl/rotary_encoders.py
+++ esphome/components/lvgl/rotary_encoders.py
@@ -25,8 +25,8 @@ ROTARY_ENCODER_CONFIG = cv.ensure_list(
                 cv.use_id(RotaryEncoderSensor),
                 cv.Schema(
                     {
-                        cv.Optional(CONF_LEFT_BUTTON): cv.use_id(BinarySensor),
-                        cv.Optional(CONF_RIGHT_BUTTON): cv.use_id(BinarySensor),
+                        cv.Required(CONF_LEFT_BUTTON): cv.use_id(BinarySensor),
+                        cv.Required(CONF_RIGHT_BUTTON): cv.use_id(BinarySensor),
                     }
                 ),
             ),
@@ -39,23 +39,23 @@ async def rotary_encoders_to_code(var, config):
     for enc_conf in config.get(CONF_ROTARY_ENCODERS, ()):
         lvgl_components_required.add("KEY_LISTENER")
         lvgl_components_required.add("ROTARY_ENCODER")
-        lpt = enc_conf[CONF_LONG_PRESS_TIME].total_milliseconds & 0xFFFF
-        lprt = enc_conf[CONF_LONG_PRESS_REPEAT_TIME].total_milliseconds & 0xFFFF
+        lpt = enc_conf[CONF_LONG_PRESS_TIME].total_milliseconds
+        lprt = enc_conf[CONF_LONG_PRESS_REPEAT_TIME].total_milliseconds
         listener = cg.new_Pvariable(
             enc_conf[CONF_ID], lv_indev_type_t.LV_INDEV_TYPE_ENCODER, lpt, lprt
         )
         await cg.register_parented(listener, var)
-        if sensor := enc_conf.get(CONF_SENSOR):
-            if isinstance(sensor, dict):
-                b_sensor = await cg.get_variable(sensor[CONF_LEFT_BUTTON])
-                lv_add(listener.set_left_button(b_sensor))
-                b_sensor = await cg.get_variable(sensor[CONF_RIGHT_BUTTON])
-                lv_add(listener.set_right_button(b_sensor))
+        if sensor_config := enc_conf.get(CONF_SENSOR):
+            if isinstance(sensor_config, dict):
+                b_sensor = await cg.get_variable(sensor_config[CONF_LEFT_BUTTON])
+                cg.add(listener.set_left_button(b_sensor))
+                b_sensor = await cg.get_variable(sensor_config[CONF_RIGHT_BUTTON])
+                cg.add(listener.set_right_button(b_sensor))
             else:
-                sensor = await cg.get_variable(sensor)
-                lv_add(listener.set_sensor(sensor))
+                sensor_config = await cg.get_variable(sensor_config)
+                lv_add(listener.set_sensor(sensor_config))
         b_sensor = await cg.get_variable(enc_conf[CONF_ENTER_BUTTON])
-        lv_add(listener.set_enter_button(b_sensor))
+        cg.add(listener.set_enter_button(b_sensor))
         if group := add_group(enc_conf.get(CONF_GROUP)):
             lv.indev_set_group(lv_expr.indev_drv_register(listener.get_drv()), group)
         else:
