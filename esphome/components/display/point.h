dec8c94de8c82730fa0908d85d47f0792865d0c5
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-08-06 22:29:37 +1200

    Fix always_inline

--- esphome/components/display/point.h
+++ esphome/components/display/point.h
@@ -11,7 +11,7 @@ class Point {
   int16_t y;
 
   Point() : x(VALUE_NO_SET), y(VALUE_NO_SET){};
-  inline Point(int16_t x, int16_t y) ALWAYS_INLINE : x(x), y(y){};
+  inline Point(int16_t x, int16_t y) ESPHOME_ALWAYS_INLINE : x(x), y(y){};
 };
 
 }  // namespace display
