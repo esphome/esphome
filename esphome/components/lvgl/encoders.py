2ff015f3760655d4f8764b1ccca1d38b9725735b
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-08-06 20:45:55 +1000

    Rename `rotary_encoders` to `encoders`

--- esphome/components/lvgl/encoders.py
+++ esphome/components/lvgl/encoders.py
@@ -5,12 +5,12 @@ import esphome.config_validation as cv
 from esphome.const import CONF_GROUP, CONF_ID, CONF_SENSOR
 
 from .defines import (
+    CONF_ENCODERS,
     CONF_ENTER_BUTTON,
     CONF_LEFT_BUTTON,
     CONF_LONG_PRESS_REPEAT_TIME,
     CONF_LONG_PRESS_TIME,
     CONF_RIGHT_BUTTON,
-    CONF_ROTARY_ENCODERS,
 )
 from .helpers import lvgl_components_required, requires_component
 from .lvcode import lv, lv_add, lv_expr
@@ -18,7 +18,7 @@ from .schemas import ENCODER_SCHEMA
 from .types import lv_indev_type_t
 from .widgets import add_group
 
-ROTARY_ENCODER_CONFIG = cv.ensure_list(
+ENCODERS_CONFIG = cv.ensure_list(
     ENCODER_SCHEMA.extend(
         {
             cv.Required(CONF_ENTER_BUTTON): cv.use_id(BinarySensor),
@@ -38,8 +38,8 @@ ROTARY_ENCODER_CONFIG = cv.ensure_list(
 )
 
 
-async def rotary_encoders_to_code(var, config):
-    for enc_conf in config.get(CONF_ROTARY_ENCODERS, ()):
+async def encoders_to_code(var, config):
+    for enc_conf in config.get(CONF_ENCODERS, ()):
         lvgl_components_required.add("KEY_LISTENER")
         lpt = enc_conf[CONF_LONG_PRESS_TIME].total_milliseconds
         lprt = enc_conf[CONF_LONG_PRESS_REPEAT_TIME].total_milliseconds
