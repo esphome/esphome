20f73c04dcaf2f1dbdc3c078d07ae36e02b221c8
Tomasz Duda <tomaszduda23@gmail.com>
2024-08-06 16:20:44 +0200

    [code-quality] Organise esphome imports

--- esphome/const.py
+++ esphome/const.py
@@ -25,6 +25,7 @@ TARGET_PLATFORMS = [
     PLATFORM_LIBRETINY_OLDSTYLE,
 ]
 
+
 SOURCE_FILE_EXTENSIONS = {".cpp", ".hpp", ".h", ".c", ".tcc", ".ino"}
 HEADER_FILE_EXTENSIONS = {".h", ".hpp", ".tcc"}
 SECRETS_FILES = ("secrets.yaml", "secrets.yml")
