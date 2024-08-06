b29b6cd4373dfd05e67d0e2febc6c8bf23ff783a
Tomasz Duda <tomaszduda23@gmail.com>
2024-08-06 17:46:42 +0200

    fix typo

--- esphome/components/light/__init__.py
+++ esphome/components/light/__init__.py
@@ -188,7 +188,7 @@ async def setup_light_core_(light_var, output_var, config):
 
 async def register_light(output_var, config):
     light_var = cg.new_Pvariable(config[CONF_ID], output_var)
-    cg.add_entity(LightState, output_var)
+    cg.add_entity(LightState, light_var)
     await cg.register_component(light_var, config)
     await setup_light_core_(light_var, output_var, config)
 
