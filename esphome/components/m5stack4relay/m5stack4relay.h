77614b6a1743ab5aca2e85e30b4de50b70b0f4c8
KoenBreeman <121864590+KoenBreeman@users.noreply.github.com>
2024-08-09 17:04:23 +0200

    create m5stack4relay swich

---
+++ esphome/components/m5stack4relay/m5stack4relay.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/i2c/i2c.h"
+
+namespace esphome {
+namespace m5stack4relay {
+
+static constexpr uint8_t UNIT_4RELAY_REG = 0X10;
+static constexpr uint8_t UNIT_4RELAY_RELAY_REG = 0X11;
+
+enum class RelayBit : uint8_t { RELAY1 = 0, RELAY2 = 1, RELAY3 = 2, RELAY4 = 3 };
+
+class M5Stack4Relay : public Component, public i2c::I2CDevice {
+ public:
+  void set_switch_mode(bool mode);
+
+  void relay_write(uint8_t number, bool state);
+
+ protected:
+  void write1_byte_(uint8_t register_address, uint8_t data);
+  uint8_t read1_byte_(uint8_t register_address);
+
+  void dump_config() override;
+
+  void init_(bool mode);
+
+  void setup() override;
+};
+
+}  // namespace m5stack4relay
+}  // namespace esphome
---
+++ esphome/components/m5stack4relay/switch/__init__.py
@@ -0,0 +1,63 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c, switch
+from esphome.const import CONF_CHANNEL, CONF_INTERLOCK  # , CONF_ID,
+
+from .. import m5stack4relay_ns, M5Stack4Relay, CONF_M5STACK4RELAY_ID
+
+DEPENDENCIES = ["m5stack4relay"]
+
+M5StackSwitch = m5stack4relay_ns.class_(
+    "M5Stack4RelaySwitch", cg.Component, i2c.I2CDevice, switch.Switch
+)
+
+CONF_INTERLOCK_WAIT_TIME = "interlock_wait_time"
+
+CONF_Relay_1 = 1
+CONF_Relay_2 = 2
+CONF_Relay_3 = 3
+CONF_Relay_4 = 4
+
+CONF_INTERLOCK_WAIT_TIME = "interlock_wait_time"
+
+RelayBit_ = m5stack4relay_ns.enum("RelayBit", is_class=True)
+
+SWITCH_MAP = {
+    CONF_Relay_1: RelayBit_.RELAY1,
+    CONF_Relay_2: RelayBit_.RELAY2,
+    CONF_Relay_3: RelayBit_.RELAY3,
+    CONF_Relay_4: RelayBit_.RELAY4,
+}
+
+
+CONFIG_SCHEMA = (
+    switch.switch_schema(M5StackSwitch)
+    .extend(
+        {
+            cv.GenerateID(): cv.declare_id(M5StackSwitch),
+            cv.GenerateID(CONF_M5STACK4RELAY_ID): cv.use_id(M5Stack4Relay),
+            cv.Required(CONF_CHANNEL): cv.enum(SWITCH_MAP),
+            cv.Optional(CONF_INTERLOCK): cv.ensure_list(cv.use_id(switch.Switch)),
+            cv.Optional(
+                CONF_INTERLOCK_WAIT_TIME, default="0ms"
+            ): cv.positive_time_period_milliseconds,
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = await switch.new_switch(config)
+    await cg.register_component(var, config)
+    await cg.register_parented(var, config[CONF_M5STACK4RELAY_ID])
+
+    cg.add(var.set_channel(config[CONF_CHANNEL]))
+
+    if CONF_INTERLOCK in config:
+        interlock = []
+        for it in config[CONF_INTERLOCK]:
+            lock = await cg.get_variable(it)
+            interlock.append(lock)
+        cg.add(var.set_interlock(interlock))
+        cg.add(var.set_interlock_wait_time(config[CONF_INTERLOCK_WAIT_TIME]))
