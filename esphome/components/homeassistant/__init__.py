50b165540863f76193f0b368b706c7f9941e72ab
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-08-13 16:22:14 +1200

    Simplify code

--- esphome/components/homeassistant/__init__.py
+++ esphome/components/homeassistant/__init__.py
@@ -4,11 +4,9 @@ from esphome.const import (
     CONF_ATTRIBUTE,
     CONF_ENTITY_ID,
     CONF_INTERNAL,
-    CONF_NAME,
-    CONF_DISABLED_BY_DEFAULT,
 )
 
-CODEOWNERS = ["@OttoWinter"]
+CODEOWNERS = ["@esphome/core", "@OttoWinter"]
 homeassistant_ns = cg.esphome_ns.namespace("homeassistant")
 
 HOME_ASSISTANT_IMPORT_SCHEMA = cv.Schema(
@@ -19,28 +17,15 @@ HOME_ASSISTANT_IMPORT_SCHEMA = cv.Schema(
     }
 )
 
-COMPONENT_CONFIG_SCHEMA = cv.Schema(
+HOME_ASSISTANT_IMPORT_CONTROL_SCHEMA = cv.Schema(
     {
         cv.Required(CONF_ENTITY_ID): cv.entity_id,
+        cv.Optional(CONF_INTERNAL, default=True): cv.boolean,
     }
-).extend(cv.COMPONENT_SCHEMA)
+)
 
 
 def setup_home_assistant_entity(var, config):
     cg.add(var.set_entity_id(config[CONF_ENTITY_ID]))
     if CONF_ATTRIBUTE in config:
         cg.add(var.set_attribute(config[CONF_ATTRIBUTE]))
-
-
-def base_to_code(base, config):
-    cg.add(base.set_entity_id(config[CONF_ENTITY_ID]))
-    if CONF_NAME in config:
-        cg.add(base.set_name(config[CONF_NAME]))
-    if CONF_DISABLED_BY_DEFAULT in config:
-        cg.add(base.set_disabled_by_default(config[CONF_DISABLED_BY_DEFAULT]))
-
-    if CONF_INTERNAL in config:
-        cg.add(base.set_internal(config[CONF_INTERNAL]))
-    else:
-        cg.add(base.set_internal(True))
-    return base
--- esphome/components/homeassistant/number/__init__.py
+++ esphome/components/homeassistant/number/__init__.py
@@ -1,38 +1,30 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import number, homeassistant
-from esphome.const import CONF_ENTITY_ID, CONF_NAME, CONF_ID, CONF_INTERNAL
+from esphome.components import number
 
-CODEOWNERS = ["@landonr"]
-homeassistant_number_ns = cg.esphome_ns.namespace("homeassistant_number")
-
-AUTO_LOAD = ["number", "homeassistant"]
-
-HomeAssistantNumber = homeassistant_number_ns.class_(
-    "HomeAssistantNumber", number.Number, cg.Component, cg.EntityBase
+from .. import (
+    HOME_ASSISTANT_IMPORT_CONTROL_SCHEMA,
+    homeassistant_ns,
+    setup_home_assistant_entity,
 )
 
-CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend(
-    {
-        cv.GenerateID(CONF_ID): cv.declare_id(HomeAssistantNumber),
-        cv.Required(CONF_ENTITY_ID): cv.entity_id,
-        cv.Required(CONF_NAME): cv.string,
-        cv.Optional(CONF_INTERNAL, default=True): cv.boolean,
-    }
-).extend(homeassistant.COMPONENT_CONFIG_SCHEMA)
+CODEOWNERS = ["@landonr"]
+DEPENDENCIES = ["api"]
+
+HomeassistantNumber = homeassistant_ns.class_(
+    "HomeassistantNumber", number.Number, cg.Component
+)
+
+CONFIG_SCHEMA = number.number_schema(HomeassistantNumber).extend(
+    HOME_ASSISTANT_IMPORT_CONTROL_SCHEMA
+)
 
 
 async def to_code(config):
-    cg.add_build_flag("-DUSE_API_NUMBER")
-    var = cg.new_Pvariable(config[CONF_ID])
-
-    await cg.register_component(var, config)
-    await number.register_number(
-        var,
+    var = await number.new_number(
         config,
         min_value=0,
         max_value=0,
         step=0,
     )
-    homeassistant.base_to_code(var, config)
-    return var
+    await cg.register_component(var, config)
+    setup_home_assistant_entity(var, config)
