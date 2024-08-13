1809981879bc5174f5671b55388c108532290ce8
Tomasz Duda <tomaszduda23@gmail.com>
2024-08-14 00:15:07 +0200

    [code-quality] fix performance-unnecessary-value-param

--- esphome/components/lvgl/number/lvgl_number.h
+++ esphome/components/lvgl/number/lvgl_number.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 #include "esphome/components/number/number.h"
 #include "esphome/core/automation.h"
 #include "esphome/core/component.h"
@@ -11,7 +13,7 @@ namespace lvgl {
 class LVGLNumber : public number::Number {
  public:
   void set_control_lambda(std::function<void(float)> control_lambda) {
-    this->control_lambda_ = control_lambda;
+    this->control_lambda_ = std::move(control_lambda);
     if (this->initial_state_.has_value()) {
       this->control_lambda_(this->initial_state_.value());
       this->initial_state_.reset();
--- esphome/components/lvgl/text/lvgl_text.h
+++ esphome/components/lvgl/text/lvgl_text.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 #include "esphome/components/text/text.h"
 #include "esphome/core/automation.h"
 #include "esphome/core/component.h"
@@ -11,7 +13,7 @@ namespace lvgl {
 class LVGLText : public text::Text {
  public:
   void set_control_lambda(std::function<void(const std::string)> control_lambda) {
-    this->control_lambda_ = control_lambda;
+    this->control_lambda_ = std::move(control_lambda);
     if (this->initial_state_.has_value()) {
       this->control_lambda_(this->initial_state_.value());
       this->initial_state_.reset();
