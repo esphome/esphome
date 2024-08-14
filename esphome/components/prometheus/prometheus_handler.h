8d0739aa4adad8345075cadc5be7739467399f0c
Tomasz Duda <tomaszduda23@gmail.com>
2024-08-14 17:56:43 +0200

    fix clang-tidy

--- esphome/components/prometheus/prometheus_handler.h
+++ esphome/components/prometheus/prometheus_handler.h
@@ -4,6 +4,7 @@
 #include <utility>
 
 #include "esphome/components/web_server_base/web_server_base.h"
+#ifdef USE_NETWORK
 #include "esphome/core/component.h"
 #include "esphome/core/controller.h"
 #include "esphome/core/entity_base.h"
@@ -117,3 +118,4 @@ class PrometheusHandler : public AsyncWebHandler, public Component {
 
 }  // namespace prometheus
 }  // namespace esphome
+#endif
