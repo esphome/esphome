1a45a17d1281f75b37d9d573c026a560267527ae
Tomasz Duda <tomaszduda23@gmail.com>
2024-08-14 18:58:36 +0200

    update sdk

--- esphome/components/nrf52/__init__.py
+++ esphome/components/nrf52/__init__.py
@@ -86,7 +86,7 @@ async def to_code(config):
     cg.add_platformio_option(
         "platform_packages",
         [
-            "platformio/framework-zephyr@https://github.com/tomaszduda23/framework-sdk-nrf/archive/refs/tags/v2.6.1.zip",
+            "platformio/framework-zephyr@https://github.com/tomaszduda23/framework-sdk-nrf/archive/refs/tags/v2.6.1-1.zip",
             "platformio/toolchain-gccarmnoneeabi@https://github.com/tomaszduda23/toolchain-sdk-ng/archive/refs/tags/v0.16.1.zip",
         ],
     )
