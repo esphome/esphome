c1774c42c25350cebc4f649184ba9b1374c6397c
Clyde Stubbs <2366188+clydebarrow@users.noreply.github.com>
2024-08-26 08:03:25 +1000

    [lvgl] Fix race condition involving numbers, switches etc. (#7345)

--- esphome/components/lvgl/select/__init__.py
+++ esphome/components/lvgl/select/__init__.py
@@ -15,7 +15,7 @@ from ..lvcode import (
 )
 from ..schemas import LVGL_SCHEMA
 from ..types import LV_EVENT, LvSelect, lvgl_ns
-from ..widgets import get_widgets
+from ..widgets import get_widgets, wait_for_widgets
 
 LVGLSelect = lvgl_ns.class_("LVGLSelect", select.Select)
 
@@ -37,6 +37,7 @@ async def to_code(config):
     options = widget.config.get(CONF_OPTIONS, [])
     selector = await select.new_select(config, options=options)
     paren = await cg.get_variable(config[CONF_LVGL_ID])
+    await wait_for_widgets()
     async with LambdaContext(EVENT_ARG) as pub_ctx:
         pub_ctx.add(selector.publish_index(widget.get_value()))
     async with LambdaContext([(cg.uint16, "v")]) as control:
