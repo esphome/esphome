b73071415707c1a605b507b78e64bfec836c7708
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2024-09-06 21:41:09 +0200

    Lint

--- esphome/components/nextion/display.py
+++ esphome/components/nextion/display.py
@@ -120,11 +120,7 @@ async def to_code(config):
 
     cg.add(var.set_exit_reparse_on_start_internal(config[CONF_EXIT_REPARSE_ON_START]))
 
-    cg.add(
-        var.set_skip_connection_handshake(
-            config[CONF_SKIP_CONNECTION_HANDSHAKE]
-        )
-    )
+    cg.add(var.set_skip_connection_handshake(config[CONF_SKIP_CONNECTION_HANDSHAKE]))
 
     await display.register_display(var, config)
 
