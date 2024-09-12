b063fa17f851ca620c494b7039cc68b32012b4a3
marko <marko@>
2024-09-12 17:33:49 +0200

    MCP3204 differential mode support

--- esphome/components/mcp3204/mcp3204.h
+++ esphome/components/mcp3204/mcp3204.h
@@ -8,17 +8,22 @@ namespace esphome {
 namespace mcp3204 {
 
 class MCP3204 : public Component,
-                public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING,
+                public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, 
+                                      spi::CLOCK_POLARITY_LOW, 
+                                      spi::CLOCK_PHASE_LEADING,
                                       spi::DATA_RATE_1MHZ> {
  public:
   MCP3204() = default;
 
-  void set_reference_voltage(float reference_voltage) { this->reference_voltage_ = reference_voltage; }
+  void set_reference_voltage(float reference_voltage) 
+  { 
+    this->reference_voltage_ = reference_voltage;
+  }
 
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override;
-  float read_data(uint8_t pin);
+  float read_data(uint8_t pin, bool diffmode);
 
  protected:
   float reference_voltage_;
