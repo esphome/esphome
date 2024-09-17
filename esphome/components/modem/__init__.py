1fc9ac0ce13ddad17ebbae1e213462f30c33b1b5
Chelios <palexanndr@gmail.com>
2024-09-17 10:05:37 +0300

    fix CONFIG_SCHEMA in init modem

--- esphome/components/modem/__init__.py
+++ esphome/components/modem/__init__.py
@@ -150,6 +150,6 @@ async def to_code(config):
         add_idf_component(
             name="esp_modem",
             repo="https://github.com/espressif/esp-protocols.git",
-            ref="^1.1.0",
+            ref="modem-v1.1.0",
             path="components/esp_modem",
         )
