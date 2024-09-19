af5cb119ca818fc4b854f1a7afa80e0d4e20ca4c
Colm Buckley <colm@tuatha.org>
2024-09-19 17:53:15 +0100

    Revert all the class member stuff and do the simplest thing possible.

--- esphome/components/rtttl/rtttl.h
+++ esphome/components/rtttl/rtttl.h
@@ -67,7 +67,6 @@ class Rtttl : public Component {
   }
   void finish_();
   void set_state_(State state);
-  const char *state_to_string_(State state);
 
   std::string rtttl_{""};
   size_t position_{0};
