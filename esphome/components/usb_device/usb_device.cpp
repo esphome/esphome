9a496bf6a839e45694283174eae552d7228c5572
Tomasz Duda <tomaszduda23@gmai.com>
2024-10-07 23:50:17 +0200

    fix

--- esphome/components/usb_device/usb_device.cpp
+++ esphome/components/usb_device/usb_device.cpp
@@ -24,7 +24,7 @@ void status_callback(enum usb_dc_status_code cb_status, uint8_t *param) {
       usb_configured = false;
       break;
     case USB_DC_CONFIGURED:
-      usb_configured = false;
+      usb_configured = true;
       break;
     default:
       break;
