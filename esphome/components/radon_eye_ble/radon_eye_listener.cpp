c466870de60332030daa3795482082d0aaea7a25
baldisos <76702976+baldisos@users.noreply.github.com>
2024-10-08 20:18:13 +0200

    format with clang-format

--- esphome/components/radon_eye_ble/radon_eye_listener.cpp
+++ esphome/components/radon_eye_ble/radon_eye_listener.cpp
@@ -1,7 +1,7 @@
 #include "radon_eye_listener.h"
 #include "esphome/core/log.h"
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 #ifdef USE_ESP32
 
@@ -10,23 +10,26 @@ namespace radon_eye_ble {
 
 static const char *const TAG = "radon_eye_ble";
 
-bool RadonEyeListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
+bool RadonEyeListener::parse_device(
+    const esp32_ble_tracker::ESPBTDevice &device) {
   if (not device.get_name().empty()) {
     // Vector containing the prefixes to search for
     std::vector<std::string> prefixes = {"FR:R", "FR:I", "FR:H"};
 
     // Check if the device name starts with any of the prefixes
-  if (std::any_of(prefixes.begin(), prefixes.end(),
-    [&](const std::string &prefix) { return device.get_name().rfind(prefix, 0) == 0; })) {
-       // Device found
-       ESP_LOGD(TAG, "Found Radon Eye device Name: %s (MAC: %s)", device.get_name().c_str(),
-                device.address_str().c_str());
+    if (std::any_of(prefixes.begin(), prefixes.end(),
+                    [&](const std::string &prefix) {
+                      return device.get_name().rfind(prefix, 0) == 0;
+                    })) {
+      // Device found
+      ESP_LOGD(TAG, "Found Radon Eye device Name: %s (MAC: %s)",
+               device.get_name().c_str(), device.address_str().c_str());
     }
   }
   return false;
 }
 
-}  // namespace radon_eye_ble
-}  // namespace esphome
+} // namespace radon_eye_ble
+} // namespace esphome
 
 #endif
