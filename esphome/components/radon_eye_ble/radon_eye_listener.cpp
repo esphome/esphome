3350e76f487874c158191ca3810fa329021725b4
baldisos <76702976+baldisos@users.noreply.github.com>
2024-10-08 20:07:04 +0200

    fix formatting and trailing whitespaces

--- esphome/components/radon_eye_ble/radon_eye_listener.cpp
+++ esphome/components/radon_eye_ble/radon_eye_listener.cpp
@@ -14,14 +14,13 @@ bool RadonEyeListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device
   if (not device.get_name().empty()) {
     // Vector containing the prefixes to search for
     std::vector<std::string> prefixes = {"FR:R", "FR:I", "FR:H"};
-    
+
     // Check if the device name starts with any of the prefixes
-    if (std::any_of(prefixes.begin(), prefixes.end(), [&](const std::string &prefix) {
-          return device.get_name().rfind(prefix, 0) == 0;
-        })) {
-      // Device found
-      ESP_LOGD(TAG, "Found Radon Eye device Name: %s (MAC: %s)", device.get_name().c_str(),
-               device.address_str().c_str());
+  if (std::any_of(prefixes.begin(), prefixes.end(),
+    [&](const std::string &prefix) { return device.get_name().rfind(prefix, 0) == 0; })) {
+       // Device found
+       ESP_LOGD(TAG, "Found Radon Eye device Name: %s (MAC: %s)", device.get_name().c_str(),
+                device.address_str().c_str());
     }
   }
   return false;
