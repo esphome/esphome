306c9778c2d3f4f52bab0842fd4201b60ccad907
Ramil Valitov <ramilvalitov@gmail.com>
2024-10-14 10:44:22 +0300

    [add] check for new ESP IDF fields

--- esphome/components/esp32_ble/const_esp32c6.h
+++ esphome/components/esp32_ble/const_esp32c6.h
@@ -62,6 +62,9 @@ static const esp_bt_controller_config_t BT_CONTROLLER_CONFIG = {
     .cpu_freq_mhz = CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ,
     .ignore_wl_for_direct_adv = 0,
     .enable_pcl = DEFAULT_BT_LE_POWER_CONTROL_ENABLED,
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 3, 1)
+    .csa2_select = DEFAULT_BT_LE_50_FEATURE_SUPPORT,  
+#endif
     .config_magic = CONFIG_MAGIC,
 };
 
