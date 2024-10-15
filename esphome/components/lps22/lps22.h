a1b395dbee2b35d534011cf023d0d0a496fcd991
Simonas Kazlauskas <github@kazlauskas.me>
2024-10-15 17:07:56 +0300

    Update esphome/components/lps22/lps22.h
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/lps22/lps22.h
+++ esphome/components/lps22/lps22.h
@@ -9,8 +9,8 @@ namespace lps22 {
 
 class LPS22Component : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
  public:
-  void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
-  void set_pressure_sensor(sensor::Sensor *pressure_sensor) { pressure_sensor_ = pressure_sensor; }
+  void set_temperature_sensor(sensor::Sensor *temperature_sensor) { this->temperature_sensor_ = temperature_sensor; }
+  void set_pressure_sensor(sensor::Sensor *pressure_sensor) { this->pressure_sensor_ = pressure_sensor; }
 
 
   void setup() override;
