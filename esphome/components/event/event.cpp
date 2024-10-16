e833b004ea8faa2e9bbe66371abe8405dc96d9ed
Nick Kinnan <nkinnan@users.noreply.github.com>
2024-10-15 22:56:00 -0700

    Update event.cpp

--- esphome/components/event/event.cpp
+++ esphome/components/event/event.cpp
@@ -13,8 +13,8 @@ void Event::trigger(const std::string &event_type) {
     ESP_LOGE(TAG, "'%s': invalid event type for trigger(): %s", this->get_name().c_str(), event_type.c_str());
     return;
   }
-  last_event_type_ = &(*found);
-  ESP_LOGD(TAG, "'%s' Triggered event '%s'", this->get_name().c_str(), last_event_type_->c_str());
+  last_event_type = &(*found);
+  ESP_LOGD(TAG, "'%s' Triggered event '%s'", this->get_name().c_str(), last_event_type->c_str());
   this->event_callback_.call(event_type);
 }
 
