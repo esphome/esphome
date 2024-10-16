09e840562181490e3b87972ec952c01d342b8aeb
Tomasz Duda <tomaszduda23@gmai.com>
2024-10-16 15:44:14 +0200

    remove log

--- esphome/writer.py
+++ esphome/writer.py
@@ -304,7 +304,7 @@ def copy_src_tree():
         copy_files = __import__(platform, fromlist=["copy_files"]).copy_files
         copy_files()
     except AttributeError:
-        _LOGGER.info("Module '%s' does not have a 'copy_files' function", platform)
+        pass
 
 
 def generate_defines_h():
