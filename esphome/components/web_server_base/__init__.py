545970fef2f5fac2c0ddd39c88ccd1b2c37151b7
Nick Kinnan <nkinnan@users.noreply.github.com>
2024-10-15 22:52:55 -0700

    Update __init__.py

--- esphome/components/web_server_base/__init__.py
+++ esphome/components/web_server_base/__init__.py
@@ -37,6 +37,4 @@ async def to_code(config):
             cg.add_library("FS", None)
             cg.add_library("Update", None)
         # https://github.com/esphome/ESPAsyncWebServer/blob/master/library.json
-        cg.add_library(
-            "https://github.com/nkinnan/ESPAsyncWebServer#deferred_publish", "3.2.2"
-        )
+        cg.add_library("esphome/ESPAsyncWebServer-esphome", "3.3.0")
