e8c952933778c8467ea06508d7ba363c2a770a64
haudamekki <38363966+haudamekki@users.noreply.github.com>
2024-10-19 21:10:47 +0200

    Update gp8211_output.cpp

--- esphome/components/gp8211/output/gp8211_output.cpp
+++ esphome/components/gp8211/output/gp8211_output.cpp
@@ -8,9 +8,7 @@ static const char *const TAG = "gp8211.output";
 
 static const uint8_t OUTPUT_REGISTER = 0x02;
 
-void GP8211Output::dump_config() {
-  ESP_LOGCONFIG(TAG, "GP8211 Output:");
-}
+void GP8211Output::dump_config() { ESP_LOGCONFIG(TAG, "GP8211 Output:"); }
 
 void GP8211Output::write_state(float state) {
   ESP_LOGD(TAG, "Original brightness state received from Light component: %.5f", state);
@@ -18,7 +16,7 @@ void GP8211Output::write_state(float state) {
   uint16_t value = static_cast<uint16_t>(state * 32767);
   ESP_LOGD(TAG, "Calculated DAC value: %u", value);
 
-  i2c::ErrorCode err = this->parent_->write_register(OUTPUT_REGISTER, (uint8_t *)&value, 2);
+  i2c::ErrorCode err = this->parent_->write_register(OUTPUT_REGISTER, (uint8_t *) &value, 2);
 
   if (err != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "Error writing to GP8211, code %d", err);
