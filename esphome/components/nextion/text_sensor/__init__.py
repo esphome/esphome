2603277146e6738fbdc3d9d672fd0c8c35fc69e5
Petr Kejval <petr.kejval6@gmail.com>
2024-10-21 10:18:52 +0200

    fix lambda

--- esphome/components/nextion/text_sensor/__init__.py
+++ esphome/components/nextion/text_sensor/__init__.py
@@ -60,7 +60,7 @@ async def sensor_nextion_publish_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
 
-    template_ = await cg.templatable(config[CONF_STATE], args, cg.std_string)
+    template_ = await cg.templatable(config[CONF_STATE], args, cg.const_char_ptr)
     cg.add(var.set_state(template_))
 
     template_ = await cg.templatable(config[CONF_PUBLISH_STATE], args, cg.bool_)
