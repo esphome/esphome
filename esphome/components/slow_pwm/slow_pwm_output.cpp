4832fb9c3635ea0ec5083ed469979479213d15ff
tetele <tm.sandu@gmail.com>
2024-10-22 20:37:43 +0000

    Add support for min time on/off

--- esphome/components/slow_pwm/slow_pwm_output.cpp
+++ esphome/components/slow_pwm/slow_pwm_output.cpp
@@ -40,11 +40,47 @@ void SlowPWMOutput::set_output_state_(bool new_state) {
 
 void SlowPWMOutput::loop() {
   uint32_t now = millis();
-  float scaled_state = this->state_ * this->period_;
+  float scaled_state = this->state_ * this->current_period_;
 
-  if (now - this->period_start_time_ >= this->period_) {
-    ESP_LOGVV(TAG, "End of period. State: %f, Scaled state: %f", this->state_, scaled_state);
-    this->period_start_time_ += this->period_;
+  if (this->state_ > 0 && (!this->max_period_ || this->current_period_ < this->max_period_) &&
+      (scaled_state - (float) this->min_time_on_) < -1.0) {  // allow 1ms of floating point error
+    this->current_period_ = (unsigned int) ((float) this->min_time_on_ / this->state_);
+    ESP_LOGVV(TAG, "Current cycle extended to %d ms to prevent on time of just %.0f ms", this->current_period_,
+              scaled_state);
+
+    scaled_state = (float) this->min_time_on_;
+  }
+
+  if (this->state_ < 1.0 && (!this->max_period_ || this->current_period_ < this->max_period_) &&
+      ((float) this->current_period_ - scaled_state - (float) this->min_time_off_) <
+          -1.0) {  // allow 1ms of floating point error
+    float required_time_off = (float) this->current_period_ - scaled_state;
+    this->current_period_ = (unsigned int) ((float) this->min_time_off_ / (1.0 - this->state_));
+    ESP_LOGVV(TAG, "Current cycle extended to %d ms to prevent off time of just %.0f ms", this->current_period_,
+              required_time_off);
+
+    scaled_state = (float) (this->current_period_ - this->min_time_off_);
+  }
+
+  if (this->max_period_ && this->current_period_ > this->max_period_) {
+    this->current_period_ = this->max_period_;
+    ESP_LOGVV(TAG, "Current cycle reduced to %d ms to obey max period", this->current_period_);
+  }
+
+  if (this->state_ == 0)
+    scaled_state = 0;
+  else if (this->state_ == 1)
+    scaled_state = (float) this->current_period_;
+  else {
+    scaled_state = std::min(std::max((float) this->min_time_on_, this->state_ * this->current_period_),
+                            (float) this->current_period_ - this->min_time_off_);
+  }
+
+  if (now - this->period_start_time_ >= this->current_period_) {
+    ESP_LOGVV(TAG, "End of period (%d ms). State: %f, Scaled state: %f", this->current_period_, this->state_,
+              scaled_state);
+    this->period_start_time_ += this->current_period_;
+    this->current_period_ = this->period_;
   }
 
   this->set_output_state_(scaled_state > now - this->period_start_time_);
@@ -64,6 +100,9 @@ void SlowPWMOutput::dump_config() {
   }
   ESP_LOGCONFIG(TAG, "  Period: %d ms", this->period_);
   ESP_LOGCONFIG(TAG, "  Restart cycle on state change: %s", YESNO(this->restart_cycle_on_state_change_));
+  ESP_LOGCONFIG(TAG, "  Minimum time on: %d ms", this->min_time_on_);
+  ESP_LOGCONFIG(TAG, "  Minimum time off: %d ms", this->min_time_off_);
+  ESP_LOGCONFIG(TAG, "  Maximum period length: %d ms", this->max_period_);
   LOG_FLOAT_OUTPUT(this);
 }
 
