d9832f6afe24abb22f4fa14c12408d211373d5ee
Christian Kohlstedde <christian@kohlsted.de>
2025-03-08 20:43:15 +0100

    Remove duplicated code of PyYAML

--- esphome/yaml_util.py
+++ esphome/yaml_util.py
@@ -540,32 +540,6 @@ def is_secret(value):
 
 
 class ESPHomeDumper(yaml.SafeDumper):
-    def represent_mapping(self, tag, mapping, flow_style=None):
-        value = []
-        node = yaml.MappingNode(tag, value, flow_style=flow_style)
-        if self.alias_key is not None:
-            self.represented_objects[self.alias_key] = node
-        best_style = True
-        if hasattr(mapping, "items"):
-            mapping = list(mapping.items())
-        if self.sort_keys:
-            with suppress(TypeError):
-                mapping = sorted(mapping)
-        for item_key, item_value in mapping:
-            node_key = self.represent_data(item_key)
-            node_value = self.represent_data(item_value)
-            if not (isinstance(node_key, yaml.ScalarNode) and not node_key.style):
-                best_style = False
-            if not (isinstance(node_value, yaml.ScalarNode) and not node_value.style):
-                best_style = False
-            value.append((node_key, node_value))
-        if flow_style is None:
-            if self.default_flow_style is not None:
-                node.flow_style = self.default_flow_style
-            else:
-                node.flow_style = best_style
-        return node
-
     def represent_secret(self, value):
         return self.represent_scalar(tag="!secret", value=_SECRET_VALUES[str(value)])
 
