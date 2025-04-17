3dbdf65ffb7969fa5a2447375b9ab6efb81ca266
Jordan Zucker <jordan.zucker@gmail.com>
2025-04-16 18:13:24 -0700

    [prometheus] Add climate metrics (#8247)

--- esphome/components/prometheus/prometheus_handler.h
+++ esphome/components/prometheus/prometheus_handler.h
@@ -8,6 +8,9 @@
 #include "esphome/core/component.h"
 #include "esphome/core/controller.h"
 #include "esphome/core/entity_base.h"
+#ifdef USE_CLIMATE
+#include "esphome/core/log.h"
+#endif
 
 namespace esphome {
 namespace prometheus {
@@ -167,6 +170,20 @@ class PrometheusHandler : public AsyncWebHandler, public Component {
   /// Return the valve state as prometheus data point
   void valve_row_(AsyncResponseStream *stream, valve::Valve *obj, std::string &area, std::string &node,
                   std::string &friendly_name);
+#endif
+
+#ifdef USE_CLIMATE
+  /// Return the type for prometheus
+  void climate_type_(AsyncResponseStream *stream);
+  /// Return the climate state as prometheus data point
+  void climate_row_(AsyncResponseStream *stream, climate::Climate *obj, std::string &area, std::string &node,
+                    std::string &friendly_name);
+  void climate_failed_row_(AsyncResponseStream *stream, climate::Climate *obj, std::string &area, std::string &node,
+                           std::string &friendly_name, std::string &category, bool is_failed_value);
+  void climate_setting_row_(AsyncResponseStream *stream, climate::Climate *obj, std::string &area, std::string &node,
+                            std::string &friendly_name, std::string &setting, const LogString *setting_value);
+  void climate_value_row_(AsyncResponseStream *stream, climate::Climate *obj, std::string &area, std::string &node,
+                          std::string &friendly_name, std::string &category, std::string &climate_value);
 #endif
 
   web_server_base::WebServerBase *base_;
