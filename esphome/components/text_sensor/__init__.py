1483abecb3f9f7361746deb313f945b84f7a186a
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2025-05-13 18:34:26 +1200

    [text_sensor] Fix schema generation (#8773)

--- esphome/components/text_sensor/__init__.py
+++ esphome/components/text_sensor/__init__.py
@@ -156,32 +156,24 @@ _TEXT_SENSOR_SCHEMA = (
 def text_sensor_schema(
     class_: MockObjClass = cv.UNDEFINED,
     *,
-    icon: str = cv.UNDEFINED,
-    entity_category: str = cv.UNDEFINED,
     device_class: str = cv.UNDEFINED,
+    entity_category: str = cv.UNDEFINED,
+    icon: str = cv.UNDEFINED,
 ) -> cv.Schema:
-    schema = _TEXT_SENSOR_SCHEMA
+    schema = {}
+
     if class_ is not cv.UNDEFINED:
-        schema = schema.extend({cv.GenerateID(): cv.declare_id(class_)})
-    if icon is not cv.UNDEFINED:
-        schema = schema.extend({cv.Optional(CONF_ICON, default=icon): cv.icon})
-    if device_class is not cv.UNDEFINED:
-        schema = schema.extend(
-            {
-                cv.Optional(
-                    CONF_DEVICE_CLASS, default=device_class
-                ): validate_device_class
-            }
-        )
-    if entity_category is not cv.UNDEFINED:
-        schema = schema.extend(
-            {
-                cv.Optional(
-                    CONF_ENTITY_CATEGORY, default=entity_category
-                ): cv.entity_category
-            }
-        )
-    return schema
+        schema[cv.GenerateID()] = cv.declare_id(class_)
+
+    for key, default, validator in [
+        (CONF_ICON, icon, cv.icon),
+        (CONF_DEVICE_CLASS, device_class, validate_device_class),
+        (CONF_ENTITY_CATEGORY, entity_category, cv.entity_category),
+    ]:
+        if default is not cv.UNDEFINED:
+            schema[cv.Optional(key, default=default)] = validator
+
+    return _TEXT_SENSOR_SCHEMA.extend(schema)
 
 
 # Remove before 2025.11.0
