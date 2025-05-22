acaf38d8da9ced42c099d94130c19d817cd549d4
Kevin Ahrendt <kahrendt@gmail.com>
2025-05-22 14:20:15 -0500

    [speaker] ensure the pipeline returns an error state before returning its stopped (#8878)

--- esphome/components/speaker/media_player/audio_pipeline.cpp
+++ esphome/components/speaker/media_player/audio_pipeline.cpp
@@ -174,6 +174,16 @@ AudioPipelineState AudioPipeline::process_state() {
     }
   }
 
+  if ((event_bits & EventGroupBits::READER_MESSAGE_ERROR)) {
+    xEventGroupClearBits(this->event_group_, EventGroupBits::READER_MESSAGE_ERROR);
+    return AudioPipelineState::ERROR_READING;
+  }
+
+  if ((event_bits & EventGroupBits::DECODER_MESSAGE_ERROR)) {
+    xEventGroupClearBits(this->event_group_, EventGroupBits::DECODER_MESSAGE_ERROR);
+    return AudioPipelineState::ERROR_DECODING;
+  }
+
   if ((event_bits & EventGroupBits::READER_MESSAGE_FINISHED) &&
       (!(event_bits & EventGroupBits::READER_MESSAGE_LOADED_MEDIA_TYPE) &&
        (event_bits & EventGroupBits::DECODER_MESSAGE_FINISHED))) {
@@ -201,16 +211,6 @@ AudioPipelineState AudioPipeline::process_state() {
     }
     this->is_playing_ = false;
     return AudioPipelineState::STOPPED;
-  }
-
-  if ((event_bits & EventGroupBits::READER_MESSAGE_ERROR)) {
-    xEventGroupClearBits(this->event_group_, EventGroupBits::READER_MESSAGE_ERROR);
-    return AudioPipelineState::ERROR_READING;
-  }
-
-  if ((event_bits & EventGroupBits::DECODER_MESSAGE_ERROR)) {
-    xEventGroupClearBits(this->event_group_, EventGroupBits::DECODER_MESSAGE_ERROR);
-    return AudioPipelineState::ERROR_DECODING;
   }
 
   if (this->pause_state_) {
