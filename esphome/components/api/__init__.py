1a09a7463f7c6f0778a4646a1242e47b57638695
J. Nick Koston <nick@koston.org>
2025-06-30 03:47:04 -0500

    Update libsodium to 1.0.20 (#9240)
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/api/__init__.py
+++ esphome/components/api/__init__.py
@@ -182,7 +182,7 @@ async def to_code(config):
             # and plaintext disabled. Only a factory reset can remove it.
             cg.add_define("USE_API_PLAINTEXT")
         cg.add_define("USE_API_NOISE")
-        cg.add_library("esphome/noise-c", "0.1.6")
+        cg.add_library("esphome/noise-c", "0.1.10")
     else:
         cg.add_define("USE_API_PLAINTEXT")
 
