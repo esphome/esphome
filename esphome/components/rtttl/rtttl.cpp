dc0f4c83b01ed6f7419c4ee7b82d3290a42dca81
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2025-07-05 11:02:04 +0200

    [rtttl] remove unused const vars

--- esphome/components/rtttl/rtttl.cpp
+++ esphome/components/rtttl/rtttl.cpp
@@ -15,11 +15,6 @@ static const uint16_t NOTES[] = {0,    262,  277,  294,  311,  330,  349,  370, 
                                  1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217,
                                  2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951};
 
-static const uint16_t I2S_SPEED = 1000;
-
-#undef HALF_PI
-static const double HALF_PI = 1.5707963267948966192313216916398;
-
 inline double deg2rad(double degrees) {
   static const double PI_ON_180 = 4.0 * atan(1.0) / 180.0;
   return degrees * PI_ON_180;
