5a67fcbb8a70b5f5ef2ce0456e0cba81bd09ace7
Christian Glombek <LorbusChris@users.noreply.github.com>
2025-07-15 21:33:15 +0200

    [ms8607] Fix humidity calc (#9499)

--- esphome/components/ms8607/ms8607.cpp
+++ esphome/components/ms8607/ms8607.cpp
@@ -356,7 +356,7 @@ void MS8607Component::read_humidity_(float temperature_float) {
 
   // map 16 bit humidity value into range [-6%, 118%]
   float const humidity_partial = double(humidity) / (1 << 16);
-  float const humidity_percentage = lerp(humidity_partial, -6.0, 118.0);
+  float const humidity_percentage = std::lerp(-6.0, 118.0, humidity_partial);
   float const compensated_humidity_percentage =
       humidity_percentage + (20 - temperature_float) * MS8607_H_TEMP_COEFFICIENT;
   ESP_LOGD(TAG, "Compensated for temperature, humidity=%.2f%%", compensated_humidity_percentage);
