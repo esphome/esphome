24f2ed4bfb08862caeb4727e0f69b93c66fd6616
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2025-07-23 10:56:00 +1200

    [udp] Move ``on_receive`` to const (#9811)

--- esphome/components/const/__init__.py
+++ esphome/components/const/__init__.py
@@ -5,5 +5,6 @@ CODEOWNERS = ["@esphome/core"]
 CONF_BYTE_ORDER = "byte_order"
 CONF_COLOR_DEPTH = "color_depth"
 CONF_DRAW_ROUNDING = "draw_rounding"
+CONF_ON_RECEIVE = "on_receive"
 CONF_ON_STATE_CHANGE = "on_state_change"
 CONF_REQUEST_HEADERS = "request_headers"
