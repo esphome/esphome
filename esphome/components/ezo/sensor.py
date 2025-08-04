02c4faaa0dc44b3e0938b15c9b66c2a4a6acbaec
Terpasaurus Midwest <terpasaurus-midwest@protonmail.com>
2025-08-04 03:00:47 -0500

    refactor(ezo): extract shared code to component root
    Move common EZO definitions and classes from sensor.py to
    __init__.py to support multiple component types. Update
    sensor.py to import from parent module.

--- esphome/components/ezo/sensor.py
+++ esphome/components/ezo/sensor.py
@@ -4,41 +4,24 @@ from esphome.components import i2c, sensor
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_TRIGGER_ID
 
+from . import (
+    CONF_ON_CALIBRATION,
+    CONF_ON_CUSTOM,
+    CONF_ON_DEVICE_INFORMATION,
+    CONF_ON_LED,
+    CONF_ON_SLOPE,
+    CONF_ON_T,
+    CalibrationTrigger,
+    CustomTrigger,
+    DeviceInformationTrigger,
+    EZOSensor,
+    LedTrigger,
+    SlopeTrigger,
+    TTrigger,
+)
+
 CODEOWNERS = ["@ssieb"]
-
-DEPENDENCIES = ["i2c"]
-
-CONF_ON_LED = "on_led"
-CONF_ON_DEVICE_INFORMATION = "on_device_information"
-CONF_ON_SLOPE = "on_slope"
-CONF_ON_CALIBRATION = "on_calibration"
-CONF_ON_T = "on_t"
-CONF_ON_CUSTOM = "on_custom"
-
-ezo_ns = cg.esphome_ns.namespace("ezo")
-
-EZOSensor = ezo_ns.class_(
-    "EZOSensor", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice
-)
-
-CustomTrigger = ezo_ns.class_(
-    "CustomTrigger", automation.Trigger.template(cg.std_string)
-)
-
-
-TTrigger = ezo_ns.class_("TTrigger", automation.Trigger.template(cg.std_string))
-
-SlopeTrigger = ezo_ns.class_("SlopeTrigger", automation.Trigger.template(cg.std_string))
-
-CalibrationTrigger = ezo_ns.class_(
-    "CalibrationTrigger", automation.Trigger.template(cg.std_string)
-)
-
-DeviceInformationTrigger = ezo_ns.class_(
-    "DeviceInformationTrigger", automation.Trigger.template(cg.std_string)
-)
-
-LedTrigger = ezo_ns.class_("LedTrigger", automation.Trigger.template(cg.bool_))
+DEPENDENCIES = ["ezo"]
 
 CONFIG_SCHEMA = (
     sensor.sensor_schema(EZOSensor)
