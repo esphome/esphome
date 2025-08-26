056de289121ab3523b5942ce27ecb799da680226
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-08-26 11:15:02 +1000

    More backwards compatible

--- esphome/components/i2c/i2c_bus.h
+++ esphome/components/i2c/i2c_bus.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
 #include <utility>
 #include <vector>
 
@@ -18,6 +19,18 @@ enum ErrorCode {
   ERROR_TOO_LARGE = 5,         ///< requested a transfer larger than buffers can hold
   ERROR_UNKNOWN = 6,           ///< miscellaneous I2C error during execution
   ERROR_CRC = 7,               ///< bytes received with a CRC error
+};
+
+/// @brief the ReadBuffer structure stores a pointer to a read buffer and its length
+struct ReadBuffer {
+  uint8_t *data;  ///< pointer to the read buffer
+  size_t len;     ///< length of the buffer
+};
+
+/// @brief the WriteBuffer structure stores a pointer to a write buffer and its length
+struct WriteBuffer {
+  const uint8_t *data;  ///< pointer to the write buffer
+  size_t len;           ///< length of the buffer
 };
 
 /// @brief This Class provides the methods to read and write bytes from an I2CBus.
@@ -44,14 +57,41 @@ class I2CBus {
 
   // Legacy functions for compatibility
 
-  [[deprecated("Use write_readv() instead. This will be removed from ESPHome v2026.3.0")]] ErrorCode read(
-      uint8_t address, uint8_t *buffer, size_t len) {
+  ErrorCode read(uint8_t address, uint8_t *buffer, size_t len) {
     return this->write_readv(address, nullptr, 0, buffer, len);
   }
 
-  [[deprecated("Use write_readv() instead. This will be removed from ESPHome v2026.3.0")]] ErrorCode write(
-      uint8_t address, const uint8_t *buffer, size_t len, bool stop = true) {
+  ErrorCode write(uint8_t address, const uint8_t *buffer, size_t len, bool stop = true) {
     return this->write_readv(address, buffer, len, nullptr, 0);
+  }
+
+  [[deprecated("Use write_readv() instead. This will be removed from ESPHome v2026.3.0")]] ErrorCode readv(
+      uint8_t address, ReadBuffer *read_buffers, size_t count) {
+    size_t total_len = 0;
+    for (size_t i = 0; i != count; i++) {
+      total_len += read_buffers[i].len;
+    }
+    std::vector<uint8_t> buffer(total_len);
+    auto err = this->write_readv(address, nullptr, 0, buffer.data(), total_len);
+    if (err != ERROR_OK)
+      return err;
+    size_t pos = 0;
+    for (size_t i = 0; i != count; i++) {
+      if (read_buffers[i].len != 0) {
+        std::memcpy(read_buffers[i].data, buffer.data() + pos, read_buffers[i].len);
+        pos += read_buffers[i].len;
+      }
+    }
+    return ERROR_OK;
+  }
+
+  [[deprecated("Use write_readv() instead. This will be removed from ESPHome v2026.3.0")]] ErrorCode writev(
+      uint8_t address, const WriteBuffer *write_buffers, size_t count, bool stop = true) {
+    std::vector<uint8_t> buffer{};
+    for (size_t i = 0; i != count; i++) {
+      buffer.insert(buffer.end(), write_buffers[i].data, write_buffers[i].data + write_buffers[i].len);
+    }
+    return this->write_readv(address, buffer.data(), buffer.size(), nullptr, 0);
   }
 
  protected:
