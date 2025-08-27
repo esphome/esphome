c71bf43e5b011b49fb56d701b47fa8b12b8a7823
Keith Baker <syzygy@specialks.com>
2025-08-27 19:13:16 -0400

    Update waveshare.py
    more cleanup, these are RGB only AFAIK so setting here

--- esphome/components/mipi_dsi/models/waveshare.py
+++ esphome/components/mipi_dsi/models/waveshare.py
@@ -21,12 +21,9 @@ DriverChip(
     pclk_frequency="80MHz",
     lane_bit_rate="1.5Gbps",
     swap_xy=cv.UNDEFINED,
-    # color_order="RGB",
+    color_order="RGB",
     lcd_rst=27,
-    # i2c_sda_pin=7,
-    # i2c_scl_pin=8,
     rst_pin=-1,
-    # i2c_clock_speed=100000,
     initsequence=[
         (0xE0, 0x00),
         (0xE1, 0x93), (0xE2, 0x65), (0xE3, 0xF8),
