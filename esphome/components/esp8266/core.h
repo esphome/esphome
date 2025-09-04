27594869e25ae3c04b90f3896c29a8e48d3d9afc
J. Nick Koston <nick@home-assistant.io>
2025-09-03 22:58:14 -0500

    fixes

--- esphome/components/esp8266/core.h
+++ esphome/components/esp8266/core.h
@@ -5,8 +5,8 @@
 #include <cstdint>
 #include "esphome/core/hal.h"
 
-extern const uint8_t ESPHOME_ESP8266_GPIO_INITIAL_MODE[16] PROGMEM;
-extern const uint8_t ESPHOME_ESP8266_GPIO_INITIAL_LEVEL[16] PROGMEM;
+extern const uint8_t ESPHOME_ESP8266_GPIO_INITIAL_MODE[16];
+extern const uint8_t ESPHOME_ESP8266_GPIO_INITIAL_LEVEL[16];
 
 namespace esphome {
 namespace esp8266 {}  // namespace esp8266
