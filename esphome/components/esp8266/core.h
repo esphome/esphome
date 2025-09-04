897bb4d13ffa6b4fa0e50947981c69dd999f0359
J. Nick Koston <nick@home-assistant.io>
2025-09-03 22:53:24 -0500

    [esp8266] Store GPIO initialization arrays in PROGMEM to save RAM

--- esphome/components/esp8266/core.h
+++ esphome/components/esp8266/core.h
@@ -3,9 +3,10 @@
 #ifdef USE_ESP8266
 
 #include <cstdint>
+#include <pgmspace.h>
 
-extern const uint8_t ESPHOME_ESP8266_GPIO_INITIAL_MODE[16];
-extern const uint8_t ESPHOME_ESP8266_GPIO_INITIAL_LEVEL[16];
+extern const uint8_t ESPHOME_ESP8266_GPIO_INITIAL_MODE[16] PROGMEM;
+extern const uint8_t ESPHOME_ESP8266_GPIO_INITIAL_LEVEL[16] PROGMEM;
 
 namespace esphome {
 namespace esp8266 {}  // namespace esp8266
