48858198818e0712b56069f6ca073e04bc88ad50
J. Nick Koston <nick@home-assistant.io>
2025-09-04 12:21:48 -0500

    use helper

--- esphome/components/pcf8574/pcf8574.h
+++ esphome/components/pcf8574/pcf8574.h
@@ -3,11 +3,16 @@
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
 #include "esphome/components/i2c/i2c.h"
+#include "esphome/components/gpio_expander/cached_gpio.h"
 
 namespace esphome {
 namespace pcf8574 {
 
-class PCF8574Component : public Component, public i2c::I2CDevice {
+// PCF8574(8 pins)/PCF8575(16 pins) always read/write all pins in a single I2C transaction
+// so we use uint16_t as bank type to ensure all pins are in one bank and cached together
+class PCF8574Component : public Component,
+                         public i2c::I2CDevice,
+                         public gpio_expander::CachedGpioExpander<uint16_t, 16> {
  public:
   PCF8574Component() = default;
 
@@ -17,8 +22,6 @@ class PCF8574Component : public Component, public i2c::I2CDevice {
   void setup() override;
   /// Invalidate cache at start of each loop
   void loop() override;
-  /// Helper function to read the value of a pin.
-  bool digital_read(uint8_t pin);
   /// Helper function to write the value of a pin.
   void digital_write(uint8_t pin, bool value);
   /// Helper function to set the pin mode of a pin.
@@ -30,8 +33,11 @@ class PCF8574Component : public Component, public i2c::I2CDevice {
   void dump_config() override;
 
  protected:
-  bool read_gpio_();
+  bool digital_read_hw(uint8_t pin) override;
+  bool digital_read_cache(uint8_t pin) override;
+  void digital_write_hw(uint8_t pin, bool value) override;
 
+  bool read_gpio_();
   bool write_gpio_();
 
   /// Mask for the pin mode - 1 means output, 0 means input
@@ -40,8 +46,6 @@ class PCF8574Component : public Component, public i2c::I2CDevice {
   uint16_t output_mask_{0x00};
   /// The state read in read_gpio_ - 1 means HIGH, 0 means LOW
   uint16_t input_mask_{0x00};
-  /// Cache validity flag - true if we've read inputs this loop cycle
-  bool cache_valid_{false};
   bool pcf8575_;  ///< TRUE->16-channel PCF8575, FALSE->8-channel PCF8574
 };
 
