9da6065a93b7e036425f402c97ce16e4c1d08986
Rodrigoah <Rodrigoah@users.noreply.github.com>
2025-09-04 22:49:09 +0200

    fixing climate.py

--- esphome/components/hitachi_168bit/climate.py
+++ esphome/components/hitachi_168bit/climate.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 from esphome.components import climate_ir
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_MODEL
+from esphome.const import CONF_MODEL
 
 AUTO_LOAD = ["climate_ir"]
 CODEOWNERS = ["@Rodrigoah"]
@@ -29,6 +29,5 @@ CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(Hitachi168bitClimate)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await climate_ir.register_climate_ir(var, config)
-    cg.add(var.set_model(MODEL_MAP[config[CONF_MODEL]]))
+    var = await climate_ir.new_climate_ir(config)
+    cg.add(var.set_model(config[CONF_MODEL]))
