ba98ee0aa52cf8880d205970a8e21cc91f117b76
Jordan Zucker <jordan.zucker@gmail.com>
2025-09-05 22:11:59 -0700

    One more try

--- esphome/components/loki/__init__.py
+++ esphome/components/loki/__init__.py
@@ -1,21 +1,39 @@
 import esphome.codegen as cg
+from esphome.components.http_request import CONF_HTTP_REQUEST_ID, HttpRequestComponent
+from esphome.components.logger import LOG_LEVELS, is_log_level
+from esphome.components.time import RealTimeClock
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_LEVEL, CONF_PORT, CONF_TIME_ID, CONF_URL
+from esphome.cpp_types import Component, Parented
 
 CODEOWNERS = ["@jzucker2"]
-DEPENDENCIES = ["network"]
 
-loki_component_ns = cg.esphome_ns.namespace("loki")
-LokiComponent = loki_component_ns.class_("LokiComponent", cg.Component)
+AUTO_LOAD = ["json"]
+DEPENDENCIES = ["network", "logger", "time", "http_request"]
 
-CONFIG_SCHEMA = cv.Schema(
+loki_ns = cg.esphome_ns.namespace("loki")
+Loki = loki_ns.class_("Loki", Component, Parented.template(HttpRequestComponent))
+
+CONF_STRIP = "strip"
+CONF_FACILITY = "facility"
+CONFIG_SCHEMA = HttpRequestComponent.HTTP_REQUEST_POST_ACTION_SCHEMA.extend(
     {
-        cv.GenerateID(): cv.declare_id(LokiComponent),
-        cv.Optional("loki_host"): cv.url,
+        cv.GenerateID(): cv.declare_id(Loki),
+        cv.GenerateID(CONF_HTTP_REQUEST_ID): cv.use_id(HttpRequestComponent),
+        cv.GenerateID(CONF_TIME_ID): cv.use_id(RealTimeClock),
+        cv.Required(CONF_URL): cv.url,
+        cv.Optional(CONF_PORT, default=3100): cv.port,
+        cv.Optional(CONF_LEVEL, default="DEBUG"): is_log_level,
+        cv.Optional(CONF_STRIP, default=True): cv.boolean,
     }
-).extend(cv.COMPONENT_SCHEMA)
+)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    parent = await cg.get_variable(config[CONF_HTTP_REQUEST_ID])
+    time = await cg.get_variable(config[CONF_TIME_ID])
+    level = LOG_LEVELS[config[CONF_LEVEL]]
+    var = cg.new_Pvariable(config[CONF_ID], level, time)
     await cg.register_component(var, config)
+    await cg.register_parented(var, parent)
+    cg.add(var.set_strip(config[CONF_STRIP]))
