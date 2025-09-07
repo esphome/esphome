8e5d1844afe4c0088d17ff7ca0a3d1d517e0d598
Rodrigoah <Rodrigoah@users.noreply.github.com>
2025-09-07 16:24:49 +0200

    fix: update header verification values for Hitachi168bit climate control

--- esphome/components/hitachi_168bit/hitachi_168bit.cpp
+++ esphome/components/hitachi_168bit/hitachi_168bit.cpp
@@ -211,7 +211,7 @@ bool Hitachi168bitClimate::on_receive(remote_base::RemoteReceiveData data) {
       remote_state[18], remote_state[19], remote_state[20]);
 
   // verify header remote code
-  if (remote_state[0] != 0x83 || remote_state[1] != 0x06)
+  if (remote_state[0] != 0x95 || remote_state[1] != 0x9A)
     return false;
 
   // powr on/off button
