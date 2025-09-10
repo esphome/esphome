d8889f1330619c666ec016bc47e723246980a51a
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-09-10 09:10:28 -0400

    Apply suggestions from code review
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/wts01/wts01.h
+++ esphome/components/wts01/wts01.h
@@ -13,24 +13,14 @@ class WTS01Sensor : public sensor::Sensor, public uart::UARTDevice, public Compo
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  float get_temperature() const { return this->current_temperature_; }
-
  protected:
-  static const uint8_t PACKET_SIZE = 9;
-  static const uint8_t HEADER_1 = 0x55;
-  static const uint8_t HEADER_2 = 0x01;
-  static const uint8_t HEADER_3 = 0x01;
-  static const uint8_t HEADER_4 = 0x04;
 
   uint8_t buffer_[PACKET_SIZE];
   uint8_t buffer_pos_{0};
-  float current_temperature_{NAN};
 
   void handle_char_(uint8_t c);
   void process_packet_();
 
- private:
-  static constexpr const char *TAG = "wts01";
 };
 
 }  // namespace wts01
