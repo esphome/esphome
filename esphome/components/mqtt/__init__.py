324bc859e508e6efd7f9e569fb02f823aa3c5a11
GraphicHealer <raymondella2001@gmail.com>
2025-09-11 10:49:48 -0400

    Remove default to prevent errors

--- esphome/components/mqtt/__init__.py
+++ esphome/components/mqtt/__init__.py
@@ -225,7 +225,7 @@ CONFIG_SCHEMA = cv.All(
             cv.Required(CONF_BROKER): cv.string_strict,
             cv.Optional(CONF_ENABLE_ON_BOOT, default=True): cv.boolean,
             cv.Optional(CONF_PORT, default=1883): cv.port,
-            cv.SplitDefault(CONF_TRANSPORT, esp32="TCP"): cv.All(
+            cv.SplitDefault(CONF_TRANSPORT): cv.All(
                 cv.enum(TRANSPORTS, upper=True), cv.only_on_esp32
             ),
             cv.Optional(CONF_USERNAME, default=""): cv.string,
