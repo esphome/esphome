007df1a73bfb7dab3dea6c52581a9b123ac32d89
GraphicHealer <raymondella2001@gmail.com>
2025-09-11 08:07:07 -0400

    Switch to SplitDefault
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/mqtt/__init__.py
+++ esphome/components/mqtt/__init__.py
@@ -228,8 +228,8 @@ CONFIG_SCHEMA = cv.All(
             cv.Required(CONF_BROKER): cv.string_strict,
             cv.Optional(CONF_ENABLE_ON_BOOT, default=True): cv.boolean,
             cv.Optional(CONF_PORT, default=1883): cv.port,
-            cv.Optional(CONF_TRANSPORT, default="tcp"): cv.All(
-                validate_transport, cv.only_with_esp_idf, cv.only_on_esp32
+            cv.SplitDefault(CONF_TRANSPORT, esp32="tcp"): cv.All(
+              cv.enum(TRANSPORTS, upper=True), cv.only_on_esp32
             ),
             cv.Optional(CONF_USERNAME, default=""): cv.string,
             cv.Optional(CONF_PASSWORD, default=""): cv.string,
