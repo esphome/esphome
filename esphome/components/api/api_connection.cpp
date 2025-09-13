38ef33fe5a117ed3882d31d11442010c58f140ac
J. Nick Koston <nick@home-assistant.io>
2025-09-13 17:16:14 -0500

    [api] Optimize HelloResponse server_info to reduce memory usage

--- esphome/components/api/api_connection.cpp
+++ esphome/components/api/api_connection.cpp
@@ -42,7 +42,6 @@ static constexpr uint8_t MAX_PING_RETRIES = 60;
 static constexpr uint16_t PING_RETRY_INTERVAL = 1000;
 static constexpr uint32_t KEEPALIVE_DISCONNECT_TIMEOUT = (KEEPALIVE_TIMEOUT_MS * 5) / 2;
 
-// Compile-time StringRef constant for ESPHome version
 static constexpr auto ESPHOME_VERSION_REF = StringRef::from_lit(ESPHOME_VERSION);
 
 static const char *const TAG = "api.connection";
