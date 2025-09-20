5115e6ba1ad0e13562744246b6957da9fd7f44ce
Oliver Kleinecke <okleinecke@web.de>
2025-09-20 14:02:15 +0200

    Update __init__.py

--- esphome/components/usb_host/__init__.py
+++ esphome/components/usb_host/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 from esphome.components.esp32 import (
     VARIANT_ESP32S2,
     VARIANT_ESP32S3,
+    VARIANT_ESP32P4,
     add_idf_sdkconfig_option,
     only_on_variant,
 )
@@ -47,7 +48,7 @@ CONFIG_SCHEMA = cv.All(
         }
     ),
     cv.only_with_esp_idf,
-    only_on_variant(supported=[VARIANT_ESP32S2, VARIANT_ESP32S3]),
+    only_on_variant(supported=[VARIANT_ESP32S2, VARIANT_ESP32S3, VARIANT_ESP32P4]),
 )
 
 
