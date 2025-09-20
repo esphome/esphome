5f7b429c41f401a6a57d9d04b2421b6fcce96530
Oliver Kleinecke <okleinecke@web.de>
2025-09-20 14:03:24 +0200

    Update ch34x.cpp

--- esphome/components/usb_uart/ch34x.cpp
+++ esphome/components/usb_uart/ch34x.cpp
@@ -1,4 +1,4 @@
-#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3)
+#if defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3) || defined(USE_ESP32_VARIANT_ESP32P4)
 #include "usb_uart.h"
 #include "usb/usb_host.h"
 #include "esphome/core/log.h"
