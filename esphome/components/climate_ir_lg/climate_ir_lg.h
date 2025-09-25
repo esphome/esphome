b4cd931ae662f6c8ff0a11933986e4abe9e9cdaa
IreuN <gatkowski.michal@gmail.com>
2025-09-25 21:41:05 +0200

    In `alternative_mode_` swing mode is not reset after unit power off

--- esphome/components/climate_ir_lg/climate_ir_lg.h
+++ esphome/components/climate_ir_lg/climate_ir_lg.h
@@ -22,8 +22,8 @@ class LgIrClimate : public climate_ir::ClimateIR {
   /// Override control to change settings of the climate device.
   void control(const climate::ClimateCall &call) override {
     this->send_swing_cmd_ = call.get_swing_mode().has_value();
-    // swing resets after unit powered off
-    if (call.get_mode().has_value() && *call.get_mode() == climate::CLIMATE_MODE_OFF)
+    // swing resets after unit powered off, except when alternative_mode_ is set
+    if (call.get_mode().has_value() && *call.get_mode() == climate::CLIMATE_MODE_OFF && !(this->alternative_mode_))
       this->swing_mode = climate::CLIMATE_SWING_OFF;
     climate_ir::ClimateIR::control(call);
   }
