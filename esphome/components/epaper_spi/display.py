3db9e9e2b54b322dd504eea9f0c2ff4822551f2e
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2025-09-29 16:27:56 +1300

    Remove unused stuff

--- esphome/components/epaper_spi/display.py
+++ esphome/components/epaper_spi/display.py
@@ -5,7 +5,6 @@ import esphome.config_validation as cv
 from esphome.const import (
     CONF_BUSY_PIN,
     CONF_DC_PIN,
-    CONF_FULL_UPDATE_EVERY,
     CONF_ID,
     CONF_LAMBDA,
     CONF_MODEL,
@@ -30,21 +29,6 @@ MODELS = {
 }
 
 
-def validate_full_update_every_only_types_ac(value):
-    if CONF_FULL_UPDATE_EVERY not in value:
-        return value
-    if MODELS[value[CONF_MODEL]][0] == "b":
-        full_models = []
-        for key, val in sorted(MODELS.items()):
-            if val[0] != "b":
-                full_models.append(key)
-        raise cv.Invalid(
-            "The 'full_update_every' option is only available for models "
-            + ", ".join(full_models)
-        )
-    return value
-
-
 CONFIG_SCHEMA = cv.All(
     display.FULL_DISPLAY_SCHEMA.extend(
         {
@@ -53,16 +37,14 @@ CONFIG_SCHEMA = cv.All(
             cv.Required(CONF_MODEL): cv.one_of(*MODELS, lower=True, space="-"),
             cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
             cv.Optional(CONF_BUSY_PIN): pins.gpio_input_pin_schema,
-            cv.Optional(CONF_FULL_UPDATE_EVERY): cv.int_range(min=1, max=4294967295),
             cv.Optional(CONF_RESET_DURATION): cv.All(
                 cv.positive_time_period_milliseconds,
                 cv.Range(max=core.TimePeriod(milliseconds=500)),
             ),
         }
     )
-    .extend(cv.polling_component_schema("1s"))
+    .extend(cv.polling_component_schema("60s"))
     .extend(spi.spi_device_schema()),
-    validate_full_update_every_only_types_ac,
     cv.has_at_most_one_key(CONF_PAGES, CONF_LAMBDA),
 )
 
@@ -92,9 +74,7 @@ async def to_code(config):
         reset = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
         cg.add(var.set_reset_pin(reset))
     if CONF_BUSY_PIN in config:
-        reset = await cg.gpio_pin_expression(config[CONF_BUSY_PIN])
-        cg.add(var.set_busy_pin(reset))
-    if CONF_FULL_UPDATE_EVERY in config:
-        cg.add(var.set_full_update_every(config[CONF_FULL_UPDATE_EVERY]))
+        busy = await cg.gpio_pin_expression(config[CONF_BUSY_PIN])
+        cg.add(var.set_busy_pin(busy))
     if CONF_RESET_DURATION in config:
         cg.add(var.set_reset_duration(config[CONF_RESET_DURATION]))
