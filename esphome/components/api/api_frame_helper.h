65384ef31aad3643af57a8464119297629ba7a97
J. Nick Koston <nick@home-assistant.io>
2025-10-01 21:08:31 +0200

    style

--- esphome/components/api/api_frame_helper.h
+++ esphome/components/api/api_frame_helper.h
@@ -80,7 +80,7 @@ class APIFrameHelper {
   virtual APIError init() = 0;
   virtual APIError loop();
   virtual APIError read_packet(ReadPacketBuffer *buffer) = 0;
-  bool can_write_without_blocking() { return state_ == State::DATA && tx_buf_count_ == 0; }
+  bool can_write_without_blocking() { return this->state_ == State::DATA && this->tx_buf_count_ == 0; }
   std::string getpeername() { return socket_->getpeername(); }
   int getpeername(struct sockaddr *addr, socklen_t *addrlen) { return socket_->getpeername(addr, addrlen); }
   APIError close() {
