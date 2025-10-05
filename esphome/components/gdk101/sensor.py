0308854fa96062b16623ed191165ab4fd0964de2
szewcu <szewcson@gmail.com>
2025-10-05 19:26:16 +0200

    fix gdk101 fw version reporting
    Signed-off-by: szewcu <szewcson@gmail.com>

--- esphome/components/gdk101/sensor.py
+++ esphome/components/gdk101/sensor.py
@@ -4,7 +4,6 @@ import esphome.config_validation as cv
 from esphome.const import (
     CONF_MEASUREMENT_DURATION,
     CONF_STATUS,
-    CONF_VERSION,
     DEVICE_CLASS_DURATION,
     DEVICE_CLASS_EMPTY,
     ENTITY_CATEGORY_DIAGNOSTIC,
@@ -40,10 +39,6 @@ CONFIG_SCHEMA = cv.Schema(
             device_class=DEVICE_CLASS_EMPTY,
             state_class=STATE_CLASS_MEASUREMENT,
         ),
-        cv.Optional(CONF_VERSION): sensor.sensor_schema(
-            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-            accuracy_decimals=1,
-        ),
         cv.Optional(CONF_STATUS): sensor.sensor_schema(
             entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
             accuracy_decimals=0,
@@ -70,10 +65,6 @@ async def to_code(config):
     if radiation_dose_per_10m := config.get(CONF_RADIATION_DOSE_PER_10M):
         sens = await sensor.new_sensor(radiation_dose_per_10m)
         cg.add(hub.set_rad_10m_sensor(sens))
-
-    if version_config := config.get(CONF_VERSION):
-        sens = await sensor.new_sensor(version_config)
-        cg.add(hub.set_fw_version_sensor(sens))
 
     if status_config := config.get(CONF_STATUS):
         sens = await sensor.new_sensor(status_config)
