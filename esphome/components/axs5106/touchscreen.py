51bd547f062e8b57c761e7b0587c2f5d6e930c88
Widget <widget@stdin.co.uk>
2025-10-05 19:33:44 +0100

    feat: mark interrupt and reset as required
    without these the component won't work

--- esphome/components/axs5106/touchscreen.py
+++ esphome/components/axs5106/touchscreen.py
@@ -21,8 +21,8 @@ CONFIG_SCHEMA = (
     .extend(
         {
             cv.GenerateID(): cv.declare_id(AXS5106Component),
-            cv.Optional(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,
-            cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
+            cv.Required(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,
+            cv.Required(CONF_RESET_PIN): pins.gpio_output_pin_schema,
         }
     )
     .extend(i2c.i2c_device_schema(0x63))
