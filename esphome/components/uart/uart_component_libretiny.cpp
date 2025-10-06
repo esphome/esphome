aa9a39b5fcb6b44f3974c470e2454ee293a3b60d
Oliver Kleinecke <okleinecke@web.de>
2025-10-06 23:32:29 +0200

    Update uart_component_libretiny.cpp

--- esphome/components/uart/uart_component_libretiny.cpp
+++ esphome/components/uart/uart_component_libretiny.cpp
@@ -119,7 +119,7 @@ void LibreTinyUARTComponent::write_array(const uint8_t *data, size_t len) {
   this->serial_->write(data, len);
 #ifdef USE_UART_DEBUGGER
   for (size_t i = 0; i < len; i++) {
-    this->debug_callback_.call(UART_DIRECTION_TX, data[i], this->debug_prefix_);
+    this->debug_callback_.call(UART_DIRECTION_TX, data[i], this->debug_prefix_, this->get_debug_settings_string());
   }
 #endif
 }
@@ -137,7 +137,7 @@ bool LibreTinyUARTComponent::read_array(uint8_t *data, size_t len) {
   this->serial_->readBytes(data, len);
 #ifdef USE_UART_DEBUGGER
   for (size_t i = 0; i < len; i++) {
-    this->debug_callback_.call(UART_DIRECTION_RX, data[i], this->debug_prefix_);
+    this->debug_callback_.call(UART_DIRECTION_RX, data[i], this->debug_prefix_, this->get_debug_settings_string());
   }
 #endif
   return true;
