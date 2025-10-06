f84f4d5cd4713e155d215f3490c58c810f7af2a4
Oliver Kleinecke <okleinecke@web.de>
2025-10-07 00:45:11 +0200

    Update uart_debugger.cpp

--- esphome/components/uart/uart_debugger.cpp
+++ esphome/components/uart/uart_debugger.cpp
@@ -12,7 +12,6 @@ namespace uart {
 static const char *const TAG = "uart_debug";
 
 UARTDebugger::UARTDebugger(UARTComponent *parent) {
-  parent->set_debug_add_settings(this->debug_add_settings_);
   parent->add_debug_callback([this](UARTDirection direction, uint8_t byte, std::string debug_prefix, std::string settings_string) {
     if (!this->is_my_direction_(direction) || this->is_recursive_()) {
       return;
