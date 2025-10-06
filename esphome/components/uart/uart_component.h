dad8ffe8600e688dfea2ac9e4545d395db293f15
Oliver Kleinecke <okleinecke@web.de>
2025-10-06 22:09:25 +0200

    Update uart_component.h

--- esphome/components/uart/uart_component.h
+++ esphome/components/uart/uart_component.h
@@ -154,7 +154,7 @@ class UARTComponent {
 
   // get&set bool if settings shall be logged
   void set_debug_add_settings(bool debug_add_settings) { this->debug_add_settings_ = debug_add_settings; }
-  std::string get_debug_add_settings() const { return this->debug_add_settings_; }
+  bool get_debug_add_settings() const { return this->debug_add_settings_; }
 #endif
 
 #if defined(USE_ESP8266) || defined(USE_ESP32)
