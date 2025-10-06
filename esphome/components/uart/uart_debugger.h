937e4929a0d737c6adea246f77606754eb7fbd3c
Oliver Kleinecke <okleinecke@web.de>
2025-10-07 01:29:31 +0200

    Update uart_debugger.h

--- esphome/components/uart/uart_debugger.h
+++ esphome/components/uart/uart_debugger.h
@@ -61,7 +61,6 @@ class UARTDebugger : public Component, public Trigger<UARTDirection, std::vector
   bool is_triggering_{false};
   std::string debug_prefix_{""};
   bool debug_add_settings_{false};
-  std::string settings_string_{""};
 
   bool is_my_direction_(UARTDirection direction);
   bool is_recursive_();
