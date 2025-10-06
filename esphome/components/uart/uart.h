f1533d9e55c185ec7cd47fb8cb4758bdde31675c
Oliver Kleinecke <okleinecke@web.de>
2025-10-06 21:45:32 +0200

    Update uart.h

--- esphome/components/uart/uart.h
+++ esphome/components/uart/uart.h
@@ -70,12 +70,16 @@ class UARTDevice {
   void check_uart_settings(uint32_t baud_rate, uint8_t stop_bits = 1,
                            UARTParityOptions parity = UART_CONFIG_PARITY_NONE, uint8_t data_bits = 8);
 #ifdef USE_UART_DEBUGGER
-  void set_debug_prefix(std::string debug_prefix) { this->parent_->set_debug_prefix(debug_prefix); }
-  void set_debug_add_settings(bool add_settings) { this->parent_->set_debug_add_settings(add_settings); }
+  void set_debug_prefix(std::string debug_prefix) { this->debug_prefix_ = debug_prefix; }
+  void set_debug_add_settings(bool add_settings) { this->debug_add_settings_ = add_settings; }
 #endif
 
  protected:
   UARTComponent *parent_{nullptr};
+#ifdef USE_UART_DEBUGGER
+  std::string debug_prefix_{""};
+  bool debug_add_settings_{false};
+#endif
 };
 
 }  // namespace uart
