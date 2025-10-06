26935c6e4ce31b3407407079257c38c02264d1ce
J. Nick Koston <nick@home-assistant.io>
2025-10-06 10:49:57 -0500

    [st7789v] Fix clang-tidy sign comparison errors

--- esphome/components/st7789v/st7789v.cpp
+++ esphome/components/st7789v/st7789v.cpp
@@ -176,8 +176,8 @@ void ST7789V::write_display_data() {
   if (this->eightbitcolor_) {
     uint8_t temp_buffer[TEMP_BUFFER_SIZE];
     size_t temp_index = 0;
-    for (int line = 0; line < this->get_buffer_length_(); line = line + this->get_width_internal()) {
-      for (int index = 0; index < this->get_width_internal(); ++index) {
+    for (size_t line = 0; line < this->get_buffer_length_(); line = line + this->get_width_internal()) {
+      for (size_t index = 0; index < this->get_width_internal(); ++index) {
         auto color = display::ColorUtil::color_to_565(
             display::ColorUtil::to_color(this->buffer_[index + line], display::ColorOrder::COLOR_ORDER_RGB,
                                          display::ColorBitness::COLOR_BITNESS_332, true));
