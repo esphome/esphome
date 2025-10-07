b1a0d178c096aec16215906cb1ca08afe2184d5f
larshacken <lars@hackenenco.nl>
2025-10-07 18:08:26 +0200

    Update waveshare_epaper.cpp

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4504,7 +4504,7 @@ void HOT WaveshareEPaper7P5InHDB::display() {
   this->data(0xAf);
   this->data(0x02);
 
-  /*// BLACK
+  // BLACK
   this->command(0x24);
   this->start_data_();
   this->write_array(this->buffer_, this->get_buffer_length_());
@@ -4513,24 +4513,9 @@ void HOT WaveshareEPaper7P5InHDB::display() {
   // RED
   this->command(0x26);
   this->start_data_();
-  for (size_t i = 0; i < this->get_buffer_length_(); i++)
-    this->write_byte(0x00);
-  this->end_data_();*/
-  uint32_t buf_len = this->get_buffer_length_();
-    // COMMAND DATA START TRANSMISSION 1 (BLACK)
-    this->command(0x24);
-    delay(2);
-    for (uint32_t i = 0; i < buf_len; i++) {
-      this->data(this->buffer_[i]);
-    }
-    delay(2);
-  
-    // COMMAND DATA START TRANSMISSION 2  (RED)
-    this->command(0x26);
-    delay(2);
-    for (uint32_t i = 0; i < buf_len; i++) {
-      this->data(this->buffer_[i]);
-    }
+  this->write_array(this->buffer_, this->get_buffer_length_());
+  this->end_data_();
+
   this->command(0x22);
   this->data(0xC7);
   this->command(0x20);
