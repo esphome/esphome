c2b1992bd4976e7bea52c8a685b099d172f0a749
larshacken <lars@hackenenco.nl>
2025-10-07 22:26:47 +0200

    Update waveshare_epaper.h

--- esphome/components/waveshare_epaper/waveshare_epaper.h
+++ esphome/components/waveshare_epaper/waveshare_epaper.h
@@ -1013,6 +1013,24 @@ class WaveshareEPaper7P5InHDB : public WaveshareEPaper {
   int get_height_internal() override;
 };
 
+class WaveshareEPaper7P5InHDBWR : public WaveshareEPaperBWR {
+ public:
+  void initialize() override;
+  void display() override;
+  void dump_config() override;
+
+  void deep_sleep() override {
+    // Deep sleep
+    this->command(0x10);
+    this->data(0x01);
+  }
+
+ protected:
+  int get_width_internal() override;
+
+  int get_height_internal() override;
+};
+
 class WaveshareEPaper2P13InDKE : public WaveshareEPaper {
  public:
   void initialize() override;
