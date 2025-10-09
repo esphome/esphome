067b38d02709776a078d1718154a9949e855ae92
Jerzy Żygłowicz <jerzy.zyglowicz@doverfs.com>
2025-10-09 10:08:07 +0200

    typo fix

--- esphome/components/opentherm/__init__.py
+++ esphome/components/opentherm/__init__.py
@@ -66,7 +66,7 @@ CONFIG_SCHEMA = cv.All(
                     ),
                 }
             ),
-            cv.Optional(CONF_MESSAGE_LOOP_DELAY, 100): cv.int
+            cv.Optional(CONF_MESSAGE_LOOP_DELAY, 100): cv.int_
         }
     )
     .extend(
