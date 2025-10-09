9d18b1a94b12c0721b4c80c00e614d8c9101e957
Oliver Kleinecke <okleinecke@web.de>
2025-10-09 19:30:57 +0200

    Update uart_debugger.h

--- esphome/components/uart/uart_debugger.h
+++ esphome/components/uart/uart_debugger.h
@@ -46,21 +46,21 @@ class UARTDebugger : public Component, public Trigger<UARTDirection, std::vector
   static std::string get_debug_prefix(std::string debug_prefix, bool debug_add_settings, uint32_t baud_rate, uint8_t data_bits, uint8_t stop_bits, uint8_t parity) {
     if (!debug_add_settings)
        return debug_prefix;
-     std::string res = "|" + baud_rate;
-     res += ":" + data_bits;
-     res += ":" + stop_bits;
+     std::string res = "|" + std::to_string(baud_rate);
+     res += ":" + std::to_string(data_bits);
+     res += ":" + std::to_string(stop_bits);
      switch (parity) {
        case UART_CONFIG_PARITY_NONE:
-         res += "NONE";
+         res += ":NONE";
          break;
        case UART_CONFIG_PARITY_EVEN:
-         res += "EVEN";
+         res += ":EVEN";
          break;
        case UART_CONFIG_PARITY_ODD:
-         res += "ODD";
+         res += ":ODD";
          break;
      default:
-       res += "UNKNOWN";
+       res += ":UNKNOWN";
        break;
      }
      return res + "|" + debug_prefix;
