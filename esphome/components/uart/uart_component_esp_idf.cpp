baf0baa47ec2474d638bb31787388e0b34a65d82
Oliver Kleinecke <okleinecke@web.de>
2025-10-09 21:15:31 +0200

    Update uart_component_esp_idf.cpp

--- esphome/components/uart/uart_component_esp_idf.cpp
+++ esphome/components/uart/uart_component_esp_idf.cpp
@@ -229,7 +229,7 @@ void IDFUARTComponent::write_array(const uint8_t *data, size_t len) {
   xSemaphoreGive(this->lock_);
 #ifdef USE_UART_DEBUGGER
   for (size_t i = 0; i < len; i++) {
-    this->debug_callback_.call(UART_DIRECTION_TX, data[i], "");
+    this->debug_callback_.call(UART_DIRECTION_TX, data[i]);
   }
 #endif
 }
@@ -269,7 +269,7 @@ bool IDFUARTComponent::read_array(uint8_t *data, size_t len) {
   xSemaphoreGive(this->lock_);
 #ifdef USE_UART_DEBUGGER
   for (size_t i = 0; i < len; i++) {
-    this->debug_callback_.call(UART_DIRECTION_RX, data[i], "");
+    this->debug_callback_.call(UART_DIRECTION_RX, data[i]);
   }
 #endif
   return true;
