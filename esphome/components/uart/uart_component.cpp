753db2cd70e8dcb1c09f05fe6ffbd81430a3820c
Oliver Kleinecke <okleinecke@web.de>
2025-10-09 14:24:20 +0200

    Update uart_component.cpp

--- esphome/components/uart/uart_component.cpp
+++ esphome/components/uart/uart_component.cpp
@@ -28,5 +28,27 @@ void UARTComponent::set_rx_full_threshold_ms(uint8_t time) {
   this->set_rx_full_threshold(val);
 }
 
+#ifdef USE_UART_DEBUGGER
+  // return settings string or empty if not desired
+  std::string get_debug_settings_string(bool debug_add_settings_) {
+    this->debug_add_settings_ = true;
+    std::string res = "|" + std::to_string(this->get_baud_rate()) + ":";
+    res += std::to_string(this->get_data_bits()) + ":";
+    res += std::to_string(this->get_stop_bits()) + ":";
+    switch(this->get_parity()) {
+      case 0:
+        res += "none|";
+        return res;
+      case 1:
+        res += "even|";
+        return res;
+      case 2:
+        res += "odd|";
+        return res;
+    }
+    return res;
+  }
+#endif
+
 }  // namespace uart
 }  // namespace esphome
