8f595df644f0d2677516069df2f87079e245faa3
Oliver Kleinecke <okleinecke@web.de>
2025-10-09 16:34:11 +0200

    Update uart_component.h

--- esphome/components/uart/uart_component.h
+++ esphome/components/uart/uart_component.h
@@ -148,11 +148,6 @@ class UARTComponent {
   uint32_t get_baud_rate() const { return baud_rate_; }
 
 #ifdef USE_UART_DEBUGGER
-  // get&set user defined log-string prefix
-  void set_debug_prefix(std::string debug_prefix) { this->debug_prefix_ = debug_prefix; }
-  std::string get_debug_prefix() const { return this->debug_prefix_; }
-
-  void set_debug_add_settings(bool debug_add_settings) { this->debug_add_settings_ = debug_add_settings; }
   // return settings string or empty if not desired
   std::string get_debug_settings_string();
 #endif
