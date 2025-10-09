b55f3c2adea332506675f2f97ae48668e7b64c2e
Oliver Kleinecke <okleinecke@web.de>
2025-10-09 19:22:46 +0200

    Update uart_debugger.h

--- esphome/components/uart/uart_debugger.h
+++ esphome/components/uart/uart_debugger.h
@@ -50,7 +50,6 @@ class UARTDebugger : public Component, public Trigger<UARTDirection, std::vector
     this->debug_add_settings_ = debug_add_settings;
     this->debug_prefix_= get_debug_prefix(this->debug_prefix_, this->debug_add_settings_, this->baud_rate_,
                                           this->data_bits_, this->stop_bits_, this->parity_);
-    this->trigger_after_direction_change_(direction);
   }
   
  protected:
