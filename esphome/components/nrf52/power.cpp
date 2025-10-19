764ac1fcc0cd30452fbb0d56cb0ab2ed92f55e94
Tomasz Duda <tomaszduda23@gmail.com>
2025-10-19 23:11:49 +0200

    fix

--- esphome/components/nrf52/power.cpp
+++ esphome/components/nrf52/power.cpp
@@ -6,9 +6,8 @@
 namespace esphome::nrf52 {
 
 static int board_esphome_init(void) {
-  /* if the board is powered from USB
-   * (high voltage mode), GPIO output voltage is set to 1.8 volts by
-   * default.
+  /* If the board is powered from USB (high voltage mode),
+   * GPIO output voltage is set to 1.8 volts by default.
    */
   if ((nrf_power_mainregstatus_get(NRF_POWER) == NRF_POWER_MAINREGSTATUS_HIGH) &&
       ((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) == (UICR_REGOUT0_VOUT_DEFAULT << UICR_REGOUT0_VOUT_Pos))) {
