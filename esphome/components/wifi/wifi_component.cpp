0fcd211814fa3cfb5c72f677701a44584a2a4bda
J. Nick Koston <nick@home-assistant.io>
2025-10-19 08:58:41 -1000

    readability

--- esphome/components/wifi/wifi_component.cpp
+++ esphome/components/wifi/wifi_component.cpp
@@ -647,7 +647,7 @@ void WiFiComponent::check_scanning_finished() {
       // Clear channel and BSSID for hidden networks - there might be multiple hidden networks
       // but we can't know which one is the correct one. Rely on probe-req with just SSID.
       selected.set_channel(0);
-      selected.set_bssid({});
+      selected.set_bssid(optional<bssid_t>{});
     } else {
       // selected network is visible, we use the data from the scan
       // limit the connect params to only connect to exactly this network
