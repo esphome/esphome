d6c48aba452c69b676be4f5e14805183c84b7fae
leejoow <leo.schelvis+git@gmail.com>
2025-10-20 08:09:28 +0200

    Update esphome/components/one_wire/one_wire.cpp
    Co-authored-by: Samuel Sieb <samuel-github@sieb.net>

--- esphome/components/one_wire/one_wire.cpp
+++ esphome/components/one_wire/one_wire.cpp
@@ -24,7 +24,7 @@ bool OneWireDevice::check_address_or_index_() {
   auto devices = this->bus_->get_devices();
 
   if (this->index_ != 255) {
-    if (this->index_ > devices.size() - 1) {
+    if (this->index_ >= devices.size()) {
       ESP_LOGE(TAG, "Index %d out of range, only %d devices found", this->index_, devices.size());
       return false;
     }
