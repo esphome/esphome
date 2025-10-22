777e73fd041ecc67539e339fe1ad1c5d4bfe4af2
J. Nick Koston <nick@home-assistant.io>
2025-10-21 21:54:44 -1000

    Extract ColorModeMask into EnumBitmask helper

--- esphome/components/light/light_traits.h
+++ esphome/components/light/light_traits.h
@@ -28,7 +28,7 @@ class LightTraits {
 
   bool supports_color_mode(ColorMode color_mode) const { return this->supported_color_modes_.contains(color_mode); }
   bool supports_color_capability(ColorCapability color_capability) const {
-    return this->supported_color_modes_.has_capability(color_capability);
+    return has_capability(this->supported_color_modes_, color_capability);
   }
 
   float get_min_mireds() const { return this->min_mireds_; }
