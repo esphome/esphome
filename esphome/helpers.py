af321edf8046707ce1059d0d51db64fa34edbd08
Links2004 <help.markus+gitk@gmail.com>
2025-10-23 17:15:45 +0000

    [core] handle mixed IP and DNS addresses correctly in resolve_ip_address
    do not raise error if some addresses are IPs and
    the mDNS / DNS resolution fails for others
    
    fix: #11501

--- esphome/helpers.py
+++ esphome/helpers.py
@@ -250,34 +250,42 @@ def resolve_ip_address(
 
     # If we have uncached hosts (only non-IP hostnames), resolve them
     if uncached_hosts:
-        from esphome.resolver import AsyncResolver
+        from esphome.core import EsphomeError
 
-        resolver = AsyncResolver(uncached_hosts, port)
-        addr_infos = resolver.resolve()
-        # Convert aioesphomeapi AddrInfo to our format
-        for addr_info in addr_infos:
-            sockaddr = addr_info.sockaddr
-            if addr_info.family == socket.AF_INET6:
-                # IPv6
-                sockaddr_tuple = (
-                    sockaddr.address,
-                    sockaddr.port,
-                    sockaddr.flowinfo,
-                    sockaddr.scope_id,
+        try:
+            from esphome.resolver import AsyncResolver
+
+            resolver = AsyncResolver(uncached_hosts, port)
+            addr_infos = resolver.resolve()
+            # Convert aioesphomeapi AddrInfo to our format
+            for addr_info in addr_infos:
+                sockaddr = addr_info.sockaddr
+                if addr_info.family == socket.AF_INET6:
+                    # IPv6
+                    sockaddr_tuple = (
+                        sockaddr.address,
+                        sockaddr.port,
+                        sockaddr.flowinfo,
+                        sockaddr.scope_id,
+                    )
+                else:
+                    # IPv4
+                    sockaddr_tuple = (sockaddr.address, sockaddr.port)
+
+                res.append(
+                    (
+                        addr_info.family,
+                        addr_info.type,
+                        addr_info.proto,
+                        "",  # canonname
+                        sockaddr_tuple,
+                    )
                 )
+        except EsphomeError as err:
+            if len(res) > 0:
+                _LOGGER.warning(err)
             else:
-                # IPv4
-                sockaddr_tuple = (sockaddr.address, sockaddr.port)
-
-            res.append(
-                (
-                    addr_info.family,
-                    addr_info.type,
-                    addr_info.proto,
-                    "",  # canonname
-                    sockaddr_tuple,
-                )
-            )
+                raise err
 
     # Sort by preference
     res.sort(key=addr_preference_)
