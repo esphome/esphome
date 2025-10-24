09f97d86e68761ed8c79decd65f960b6b0ab055d
J. Nick Koston <nick@home-assistant.io>
2025-10-24 04:31:16 -0700

    [select] Store options in flash to reduce RAM usage

--- esphome/components/select/select_traits.cpp
+++ esphome/components/select/select_traits.cpp
@@ -5,7 +5,12 @@ namespace select {
 
 void SelectTraits::set_options(std::initializer_list<const char *> options) { this->options_ = options; }
 
-void SelectTraits::set_options(const FixedVector<const char *> &options) { this->options_ = options; }
+void SelectTraits::set_options(const FixedVector<const char *> &options) {
+  this->options_.init(options.size());
+  for (size_t i = 0; i < options.size(); i++) {
+    this->options_[i] = options[i];
+  }
+}
 
 const FixedVector<const char *> &SelectTraits::get_options() const { return this->options_; }
 
