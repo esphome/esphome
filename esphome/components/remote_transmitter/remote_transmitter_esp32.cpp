94880df211ef2514ecd05c2afb44b9ae18696104
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-10-24 16:42:51 -0400

    Fix order

--- esphome/components/remote_transmitter/remote_transmitter_esp32.cpp
+++ esphome/components/remote_transmitter/remote_transmitter_esp32.cpp
@@ -229,8 +229,8 @@ void RemoteTransmitterComponent::send_internal(uint32_t send_times, uint32_t sen
 
   // encode any delay at the start of the buffer to simplify the encoder callback
   // this will be skipped the first time around
-  send_wait = this->from_microseconds_(static_cast<uint32_t>(send_wait));
   total_duration += send_wait;
+  send_wait = this->from_microseconds_(static_cast<uint32_t>(send_wait));
   while (send_wait > 0) {
     int32_t duration = std::min(send_wait, uint32_t(RMT_SYMBOL_DURATION_MAX));
     this->rmt_temp_.push_back({
@@ -247,8 +247,8 @@ void RemoteTransmitterComponent::send_internal(uint32_t send_times, uint32_t sen
     if (!level) {
       value = -value;
     }
-    value = this->from_microseconds_(static_cast<uint32_t>(value));
     total_duration += value;
+    value = this->from_microseconds_(static_cast<uint32_t>(value));
     while (value > 0) {
       int32_t duration = std::min(value, int32_t(RMT_SYMBOL_DURATION_MAX));
       this->rmt_temp_.push_back({
