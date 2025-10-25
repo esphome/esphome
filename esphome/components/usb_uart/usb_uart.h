6cfca87ca7f2ea450706027f7100974f28173779
J. Nick Koston <nick@home-assistant.io>
2025-10-25 14:39:28 -0700

    safer

--- esphome/components/usb_uart/usb_uart.h
+++ esphome/components/usb_uart/usb_uart.h
@@ -143,6 +143,7 @@ class USBUartComponent : public usb_host::USBClient {
  protected:
   void defer_input_retry_(USBUartChannel *channel);
   void reset_input_state_(USBUartChannel *channel);
+  void restart_input_(USBUartChannel *channel);
   std::vector<USBUartChannel *> channels_{};
 };
 
