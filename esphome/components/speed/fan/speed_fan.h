47cbe74453ba42283b3bbd90b94d1f0ac747d73c
J. Nick Koston <nick@home-assistant.io>
2025-10-28 22:41:13 -0500

    cleanup

--- esphome/components/speed/fan/speed_fan.h
+++ esphome/components/speed/fan/speed_fan.h
@@ -16,7 +16,7 @@ class SpeedFan : public Component, public fan::Fan {
   void set_output(output::FloatOutput *output) { this->output_ = output; }
   void set_oscillating(output::BinaryOutput *oscillating) { this->oscillating_ = oscillating; }
   void set_direction(output::BinaryOutput *direction) { this->direction_ = direction; }
-  void set_preset_modes(std::initializer_list<std::string> presets) { this->preset_modes_ = presets; }
+  void set_preset_modes(std::initializer_list<const char *> presets) { this->preset_modes_ = presets; }
   fan::FanTraits get_traits() override { return this->traits_; }
 
  protected:
@@ -28,7 +28,7 @@ class SpeedFan : public Component, public fan::Fan {
   output::BinaryOutput *direction_{nullptr};
   int speed_count_{};
   fan::FanTraits traits_;
-  std::vector<std::string> preset_modes_{};
+  FixedVector<const char *> preset_modes_{};
 };
 
 }  // namespace speed
