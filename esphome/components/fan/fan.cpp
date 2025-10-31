79e2340588fdf6c538b32d71360ee162017f0229
J. Nick Koston <nick@home-assistant.io>
2025-10-31 11:06:18 -0500

    wip

--- esphome/components/fan/fan.cpp
+++ esphome/components/fan/fan.cpp
@@ -121,14 +121,12 @@ void FanRestoreState::apply(Fan &fan) {
   fan.speed = this->speed;
   fan.direction = this->direction;
 
-  auto traits = fan.get_traits();
-  if (traits.supports_preset_modes()) {
-    // Use stored preset index to get preset name
-    const auto &preset_modes = traits.supported_preset_modes();
-    if (this->preset_mode < preset_modes.size()) {
-      fan.set_preset_mode_(preset_modes[this->preset_mode]);
-    }
+  // Use stored preset index to get preset name from traits
+  const auto &preset_modes = fan.get_traits().supported_preset_modes();
+  if (this->preset_mode < preset_modes.size()) {
+    fan.set_preset_mode_(preset_modes[this->preset_mode]);
   }
+
   fan.publish_state();
 }
 
