a91358899bf973dc1f6c4463cdd629f8950f7897
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-11-01 20:29:34 +1000

    Add component.is_idle condition

--- esphome/automation.py
+++ esphome/automation.py
@@ -1,3 +1,5 @@
+import voluptuous
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import (
@@ -15,7 +17,7 @@ from esphome.const import (
     CONF_TYPE_ID,
     CONF_UPDATE_INTERVAL,
 )
-from esphome.core import ID
+from esphome.core import ID, Lambda
 from esphome.cpp_generator import (
     LambdaExpression,
     MockObj,
@@ -61,7 +63,9 @@ def register_action(name: str, action_type: MockObjClass, schema: cv.Schema):
     return ACTION_REGISTRY.register(name, action_type, schema)
 
 
-def register_condition(name: str, condition_type: MockObjClass, schema: cv.Schema):
+def register_condition(
+    name: str, condition_type: MockObjClass, schema: voluptuous.Schema
+):
     return CONDITION_REGISTRY.register(name, condition_type, schema)
 
 
@@ -308,6 +312,30 @@ async def for_condition_to_code(
     templ = await cg.templatable(config[CONF_TIME], args, cg.uint32)
     cg.add(var.set_time(templ))
     return var
+
+
+@register_condition(
+    "component.is_idle",
+    LambdaCondition,
+    maybe_simple_id(
+        {
+            cv.Required(CONF_ID): cv.use_id(cg.Component),
+        }
+    ),
+)
+async def component_is_idle_condition_to_code(
+    config: ConfigType,
+    condition_id: ID,
+    template_arg: cg.TemplateArguments,
+    args: TemplateArgsType,
+) -> MockObj:
+    comp = await cg.get_variable(config[CONF_ID])
+    lambda_ = await cg.process_lambda(
+        Lambda(f"return {comp}->is_idle();"), args, return_type=bool
+    )
+    return new_lambda_pvariable(
+        condition_id, lambda_, StatelessLambdaCondition, template_arg
+    )
 
 
 @register_action(
