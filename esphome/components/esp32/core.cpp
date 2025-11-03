d2ed0c1de20b64d11df69179c6dbdef26f26129d
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-11-03 10:22:08 -0500

    Fix defines

--- esphome/components/esp32/core.cpp
+++ esphome/components/esp32/core.cpp
@@ -1,5 +1,6 @@
 #ifdef USE_ESP32
 
+#include "esphome/core/defines.h"
 #include "esphome/core/hal.h"
 #include "esphome/core/helpers.h"
 #include "preferences.h"
@@ -85,9 +86,6 @@ uint32_t arch_get_cpu_freq_hz() {
 }
 
 #ifdef USE_ESP_IDF
-#ifndef ESPHOME_APP_MAIN_TASK_STACK_SIZE
-#define ESPHOME_APP_MAIN_TASK_STACK_SIZE 8192
-#endif
 TaskHandle_t loop_task_handle = nullptr;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
 void loop_task(void *pv_params) {
