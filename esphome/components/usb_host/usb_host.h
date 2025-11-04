bb57cb13d30a4804f40bec4868af171aee5969e2
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-11-04 14:43:49 +0000

    add custom uart changes

--- esphome/components/usb_host/usb_host.h
+++ esphome/components/usb_host/usb_host.h
@@ -55,6 +55,12 @@ static const uint8_t USB_DIR_IN = 1 << 7;
 static const uint8_t USB_DIR_OUT = 0;
 static const size_t SETUP_PACKET_SIZE = 8;
 
+#ifdef USE_ESP32_VARIANT_ESP32P4
+static constexpr uint16_t USB_MAX_PACKET_SIZE = 512;
+#else
+static constexpr uint8_t USB_MAX_PACKET_SIZE = 64;
+#endif
+
 static constexpr size_t MAX_REQUESTS = USB_HOST_MAX_REQUESTS;  // maximum number of outstanding requests possible.
 static_assert(MAX_REQUESTS >= 1 && MAX_REQUESTS <= 32, "MAX_REQUESTS must be between 1 and 32");
 
@@ -67,8 +73,10 @@ static_assert(MAX_REQUESTS >= 1 && MAX_REQUESTS <= 32, "MAX_REQUESTS must be bet
 using trq_bitmask_t = std::conditional<(MAX_REQUESTS <= 16), uint16_t, uint32_t>::type;
 static constexpr trq_bitmask_t ALL_REQUESTS_IN_USE = MAX_REQUESTS == 32 ? ~0 : (1 << MAX_REQUESTS) - 1;
 
-static constexpr size_t USB_EVENT_QUEUE_SIZE = 32;   // Size of event queue between USB task and main loop
-static constexpr size_t USB_TASK_STACK_SIZE = 4096;  // Stack size for USB task (same as ESP-IDF USB examples)
+static constexpr size_t USB_EVENT_QUEUE_SIZE = 32;  // Size of event queue between USB task and main loop
+#ifndef USB_TASK_STACK_SIZE
+static constexpr size_t USB_TASK_STACK_SIZE = 4096;  // Stack size for USB task
+#endif
 static constexpr UBaseType_t USB_TASK_PRIORITY = 5;  // Higher priority than main loop (tskIDLE_PRIORITY + 5)
 
 // used to report a transfer status
@@ -148,6 +156,7 @@ class USBClient : public Component {
   EventPool<UsbEvent, USB_EVENT_QUEUE_SIZE> event_pool;
 
  protected:
+  bool register_();
   TransferRequest *get_trq_();  // Lock-free allocation using atomic bitmask (multi-consumer safe)
   virtual void disconnect();
   virtual void on_connected() {}
