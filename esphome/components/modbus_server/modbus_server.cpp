fa7999802ce03c1c469b7fa8e45c9e6ea80c544b
Bonne Eggleston <bonne@exciton.com.au>
2025-11-04 22:26:21 -0800

    Share modbus_controller helpers with modbus and modbus_server

--- esphome/components/modbus_server/modbus_server.cpp
+++ esphome/components/modbus_server/modbus_server.cpp
@@ -35,7 +35,7 @@ void ModbusServer::on_modbus_read_registers(uint8_t function_code, uint16_t star
 
         std::vector<uint16_t> payload;
         payload.reserve(server_register->register_count * 2);
-        number_to_payload(payload, value, server_register->value_type);
+        modbus::number_to_payload(payload, value, server_register->value_type);
         sixteen_bit_response.insert(sixteen_bit_response.end(), payload.cbegin(), payload.cend());
         current_address += server_register->register_count;
         found = true;
@@ -137,7 +137,7 @@ void ModbusServer::on_modbus_write_registers(uint8_t function_code, const std::v
 
   // Actually write to the registers:
   if (!for_each_register([&data](ServerRegister *server_register, uint16_t offset) {
-        int64_t number = payload_to_number(data, server_register->value_type, offset, 0xFFFFFFFF);
+        int64_t number = modbus::payload_to_number(data, server_register->value_type, offset, 0xFFFFFFFF);
         return server_register->write_lambda(number);
       })) {
     this->send_error(function_code, ModbusExceptionCode::SERVICE_DEVICE_FAILURE);
@@ -170,135 +170,6 @@ void ModbusServer::dump_config() {
                   static_cast<uint8_t>(r->value_type), r->register_count);
   }
 #endif
-}
-
-// TODO: Share this with controller
-void number_to_payload(std::vector<uint16_t> &data, int64_t value, SensorValueType value_type) {
-  switch (value_type) {
-    case SensorValueType::U_WORD:
-    case SensorValueType::S_WORD:
-      data.push_back(value & 0xFFFF);
-      break;
-    case SensorValueType::U_DWORD:
-    case SensorValueType::S_DWORD:
-    case SensorValueType::FP32:
-      data.push_back((value & 0xFFFF0000) >> 16);
-      data.push_back(value & 0xFFFF);
-      break;
-    case SensorValueType::U_DWORD_R:
-    case SensorValueType::S_DWORD_R:
-    case SensorValueType::FP32_R:
-      data.push_back(value & 0xFFFF);
-      data.push_back((value & 0xFFFF0000) >> 16);
-      break;
-    case SensorValueType::U_QWORD:
-    case SensorValueType::S_QWORD:
-      data.push_back((value & 0xFFFF000000000000) >> 48);
-      data.push_back((value & 0xFFFF00000000) >> 32);
-      data.push_back((value & 0xFFFF0000) >> 16);
-      data.push_back(value & 0xFFFF);
-      break;
-    case SensorValueType::U_QWORD_R:
-    case SensorValueType::S_QWORD_R:
-      data.push_back(value & 0xFFFF);
-      data.push_back((value & 0xFFFF0000) >> 16);
-      data.push_back((value & 0xFFFF00000000) >> 32);
-      data.push_back((value & 0xFFFF000000000000) >> 48);
-      break;
-    default:
-      ESP_LOGE(TAG, "Invalid data type for modbus number to payload conversation: %d",
-               static_cast<uint16_t>(value_type));
-      break;
-  }
-}
-
-// TODO: Share this with controller
-int64_t payload_to_number(const std::vector<uint8_t> &data, SensorValueType sensor_value_type, uint8_t offset,
-                          uint32_t bitmask) {
-  int64_t value = 0;  // int64_t because it can hold signed and unsigned 32 bits
-
-  size_t size = data.size() - offset;
-  bool error = false;
-  switch (sensor_value_type) {
-    case SensorValueType::U_WORD:
-      if (size >= 2) {
-        value = mask_and_shift_by_rightbit(get_data<uint16_t>(data, offset), bitmask);  // default is 0xFFFF ;
-      } else {
-        error = true;
-      }
-      break;
-    case SensorValueType::U_DWORD:
-    case SensorValueType::FP32:
-      if (size >= 4) {
-        value = get_data<uint32_t>(data, offset);
-        value = mask_and_shift_by_rightbit((uint32_t) value, bitmask);
-      } else {
-        error = true;
-      }
-      break;
-    case SensorValueType::U_DWORD_R:
-    case SensorValueType::FP32_R:
-      if (size >= 4) {
-        value = get_data<uint32_t>(data, offset);
-        value = static_cast<uint32_t>(value & 0xFFFF) << 16 | (value & 0xFFFF0000) >> 16;
-        value = mask_and_shift_by_rightbit((uint32_t) value, bitmask);
-      } else {
-        error = true;
-      }
-      break;
-    case SensorValueType::S_WORD:
-      if (size >= 2) {
-        value = mask_and_shift_by_rightbit(get_data<int16_t>(data, offset),
-                                           bitmask);  // default is 0xFFFF ;
-      } else {
-        error = true;
-      }
-      break;
-    case SensorValueType::S_DWORD:
-      if (size >= 4) {
-        value = mask_and_shift_by_rightbit(get_data<int32_t>(data, offset), bitmask);
-      } else {
-        error = true;
-      }
-      break;
-    case SensorValueType::S_DWORD_R: {
-      if (size >= 4) {
-        value = get_data<uint32_t>(data, offset);
-        // Currently the high word is at the low position
-        // the sign bit is therefore at low before the switch
-        uint32_t sign_bit = (value & 0x8000) << 16;
-        value = mask_and_shift_by_rightbit(
-            static_cast<int32_t>(((value & 0x7FFF) << 16 | (value & 0xFFFF0000) >> 16) | sign_bit), bitmask);
-      } else {
-        error = true;
-      }
-    } break;
-    case SensorValueType::U_QWORD:
-    case SensorValueType::S_QWORD:
-      // Ignore bitmask for QWORD
-      if (size >= 8) {
-        value = get_data<uint64_t>(data, offset);
-      } else {
-        error = true;
-      }
-      break;
-    case SensorValueType::U_QWORD_R:
-    case SensorValueType::S_QWORD_R: {
-      // Ignore bitmask for QWORD
-      if (size >= 8) {
-        uint64_t tmp = get_data<uint64_t>(data, offset);
-        value = (tmp << 48) | (tmp >> 48) | ((tmp & 0xFFFF0000) << 16) | ((tmp >> 16) & 0xFFFF0000);
-      } else {
-        error = true;
-      }
-    } break;
-    case SensorValueType::RAW:
-    default:
-      break;
-  }
-  if (error)
-    ESP_LOGE(TAG, "not enough data for value");
-  return value;
 }
 
 }  // namespace modbus_server
