d70fe126f66c1b0890fbc27bd5394e001838c176
J. Nick Koston <nick@home-assistant.io>
2025-11-04 21:13:46 -0600

    preen

--- esphome/components/select/select.h
+++ esphome/components/select/select.h
@@ -92,18 +92,6 @@ class Select : public EntityBase {
 
   size_t active_index_{0};
 
-  /** Set the value of the select by index, this is an optional virtual method.
-   *
-   * IMPORTANT: At least ONE of the two control() methods must be overridden by derived classes.
-   * Overriding this index-based version is PREFERRED as it avoids string conversions.
-   *
-   * This method is called by the SelectCall when the index is already known.
-   * Default implementation converts to string and calls control(const std::string&).
-   *
-   * @param index The index as validated by the SelectCall.
-   */
-  virtual void control(size_t index) { this->control(this->option_at(index)); }
-
   /** Set the value of the select, this is a virtual method that each select integration can implement.
    *
    * IMPORTANT: At least ONE of the two control() methods must be overridden by derived classes.
