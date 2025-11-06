6e685f1b2d84d787edac3fefcde0a9aa1a77abf5
J. Nick Koston <nick@home-assistant.io>
2025-11-06 17:15:57 -0600

    dry

--- esphome/components/wifi/wifi_component.cpp
+++ esphome/components/wifi/wifi_component.cpp
@@ -381,19 +381,16 @@ WiFiAP WiFiComponent::build_wifi_ap_from_selected_() const {
     params.set_bssid(scan.get_bssid());
     params.set_channel(scan.get_channel());
     // hidden defaults to false, no need to set explicitly
-  } else if (config->get_hidden()) {
-    // For hidden networks, use config SSID but clear BSSID and channel
+  } else {
+    // No scan data - use config SSID
+    params.set_ssid(config->get_ssid());
+    const bool hidden = config->get_hidden();
+    params.set_hidden(hidden);
+    // Hidden network - clear BSSID and channel even if set in config
     // There might be multiple hidden networks with same SSID but we can't know which is correct
     // Rely on probe-req with just SSID. Empty channel triggers ALL_CHANNEL_SCAN.
-    params.set_ssid(config->get_ssid());
-    params.set_hidden(true);
-    params.set_bssid(optional<bssid_t>{});
-    params.set_channel(optional<uint8_t>{});
-  } else {
-    // No scan data, visible network - use all config values
-    params.set_ssid(config->get_ssid());
-    params.set_bssid(config->get_bssid());
-    params.set_channel(config->get_channel());
+    params.set_bssid(hidden ? optional<bssid_t>{} : config->get_bssid());
+    params.set_channel(hidden ? optional<uint8_t>{} : config->get_channel());
   }
 
   return params;
