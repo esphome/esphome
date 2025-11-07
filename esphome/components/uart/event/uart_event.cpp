1435e5c963f6491a9ab753fcfcf9ef740f56cf9b
茅晓栋 <maoxiaodong@meilijian.com>
2025-11-07 16:39:43 +0800

    Add an event component to the UART platform.

---
+++ esphome/components/uart/event/uart_event.cpp
@@ -0,0 +1,72 @@
+#include "uart_event.h"
+#include "esphome/core/log.h"
+#include "esphome/core/helpers.h"
+#include <algorithm>
+
+namespace esphome {
+namespace uart {
+
+static const char *const TAG = "uart.event";
+
+void UARTEvent::setup() {
+  buffer_.clear();
+}
+
+void UARTEvent::dump_config() {
+  LOG_EVENT("", "UART Event", this);
+}
+
+void UARTEvent::loop() {
+  read_data();
+}
+
+
+void UARTEvent::add_event_matcher(const std::string &event_name, const std::string &match_string) {
+  std::vector<uint8_t> data(match_string.begin(), match_string.end());
+  this->matchers_.push_back({event_name, data});
+  if (data.size() > this->max_matcher_len_) {
+    this->max_matcher_len_ = data.size();
+  }
+}
+
+void UARTEvent::add_event_matcher(const std::string &event_name, const std::vector<uint8_t> &match_binary) {
+  this->matchers_.push_back({event_name, match_binary});
+  if (match_binary.size() > this->max_matcher_len_) {
+    this->max_matcher_len_ = match_binary.size();
+  }
+}
+
+
+void UARTEvent::read_data() {
+  while (available()) {
+    uint8_t data;
+    read_byte(&data);
+    this->buffer_.push_back(data);
+
+    bool match_found = false;
+    for (const auto &matcher : this->matchers_) {
+      const auto& match_data = matcher.binary_data;
+      
+      if (this->buffer_.size() < match_data.size()) {
+        continue;
+      }
+
+      if (std::equal(match_data.begin(), match_data.end(),
+                     this->buffer_.end() - match_data.size())) {
+        
+        this->trigger(matcher.event_name);
+        this->buffer_.clear();
+        match_found = true;
+        break;
+      }
+    }
+
+    if (!match_found && this->max_matcher_len_ > 0 && this->buffer_.size() > this->max_matcher_len_) {
+      this->buffer_.erase(this->buffer_.begin());
+    }
+  }
+}
+
+
+} // namespace uart
+} // namespace esphome
---
+++ esphome/components/uart/event/uart_event.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/event/event.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/log.h"
+#include <vector>
+#include <string>
+#include <map>
+
+namespace esphome {
+namespace uart {
+
+class UARTEvent :public event::Event
+                         ,public UARTDevice
+                         ,public Component{
+ protected:
+  struct EventMatcher {
+    std::string event_name;
+    std::vector<uint8_t> binary_data;
+  };
+
+  void read_data();
+  std::vector<EventMatcher> matchers_;
+  std::vector<uint8_t> buffer_;
+  size_t max_matcher_len_ = 0;
+  
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+
+  void add_event_matcher(const std::string &event_name, const std::string &match_string);
+  void add_event_matcher(const std::string &event_name, const std::vector<uint8_t> &match_binary);
+};
+
+} // namespace uart
+} // namespace esphome
