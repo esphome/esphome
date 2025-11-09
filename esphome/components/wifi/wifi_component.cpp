c754b0f71dbd2a1e4f8492d3c25dd5e0ec5e750b
J. Nick Koston <nick@home-assistant.io>
2025-11-09 09:23:05 -0600

    wip

--- esphome/components/wifi/wifi_component.cpp
+++ esphome/components/wifi/wifi_component.cpp
@@ -407,7 +407,12 @@ void WiFiComponent::save_wifi_sta(const std::string &ssid, const std::string &pa
 }
 
 void WiFiComponent::start_connecting(const WiFiAP &ap, bool two) {
-  ESP_LOGI(TAG, "Connecting to '%s'", ap.get_ssid().c_str());
+  if (ap.get_bssid().has_value()) {
+    ESP_LOGI(TAG, "Connecting to '%s' (%s)", ap.get_ssid().c_str(),
+             format_mac_address_pretty(ap.get_bssid()->data()).c_str());
+  } else {
+    ESP_LOGI(TAG, "Connecting to '%s'", ap.get_ssid().c_str());
+  }
 #ifdef ESPHOME_LOG_HAS_VERBOSE
   ESP_LOGV(TAG, "Connection Params:");
   ESP_LOGV(TAG, "  SSID: '%s'", ap.get_ssid().c_str());
