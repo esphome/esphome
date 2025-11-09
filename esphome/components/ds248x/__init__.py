2e0cef2791b1273f5e32f605c02d87c094ace385
Philipp Luchscheider <p.luchscheider@gmx.net>
2025-11-09 16:28:10 +0100

    added codeownership

--- esphome/components/ds248x/__init__.py
+++ esphome/components/ds248x/__init__.py
@@ -1,15 +1,16 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome import pins
-from esphome.const import (
-    CONF_ID,
-    CONF_SLEEP_PIN,
-    CONF_DS248X_ID,
-    CONF_ADDRESS,
-    CONF_INDEX,
-)
+import esphome.codegen as cg
 from esphome.components import i2c, sensor
+import esphome.config_validation as cv
+from esphome.const import (
+    CONF_ADDRESS,
+    CONF_DS248X_ID,
+    CONF_ID,
+    CONF_INDEX,
+    CONF_SLEEP_PIN,
+)
 
+CODEOWNERS = ["@vielleicht"]
 MULTI_CONF = True
 AUTO_LOAD = ["sensor"]
 DEPENDENCIES = ["i2c"]
