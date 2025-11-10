d34e915f29672171a32a73b61b5cbd71099f81db
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-10 02:57:41 +0100

    Update waveshare.py

--- esphome/components/mipi_dsi/models/waveshare.py
+++ esphome/components/mipi_dsi/models/waveshare.py
@@ -94,3 +94,30 @@ DriverChip(
         (0x29, 0x00),
     ],
 )
+
+DriverChip(
+    "WAVESHARE-ESP32-P4-WIFI6-TOUCH-LCD-7B",
+    height=1024,
+    width=600,
+    hsync_back_porch=160,
+    hsync_pulse_width=10,
+    hsync_front_porch=160,
+    vsync_back_porch=23,
+    vsync_pulse_width=1,
+    vsync_front_porch=12,
+    pclk_frequency="52MHz",
+    pixel_mode="24bit",
+    lane_bit_rate="1Gbps",
+    swap_xy=cv.UNDEFINED,
+    color_order="RGB",
+    reset_pin=33,
+    initsequence=[
+        (0x80, 0x8B),
+        (0x81, 0x78),
+        (0x82, 0x84),
+        (0x83, 0x88),
+        (0x84, 0xA8),
+        (0x85, 0xE3),
+        (0x86, 0x88),
+    ],
+)
