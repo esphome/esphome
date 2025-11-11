bd715c64b8d64c1fec25c13460f4dd8932df33d2
Tomasz Duda <tomaszduda23@gmail.com>
2025-11-11 12:31:19 +0100

    typo

--- esphome/components/nrf52/__init__.py
+++ esphome/components/nrf52/__init__.py
@@ -104,7 +104,7 @@ DeviceFirmwareUpdate = nrf52_ns.class_("DeviceFirmwareUpdate", cg.Component)
 
 CONF_DFU = "dfu"
 CONF_REG0 = "reg0"
-CONF_UCIR_ERASE = "ucir_erase"
+CONF_UICR_ERASE = "uicr_erase"
 
 VOLTAGE_LEVELS = [1.8, 2.1, 2.4, 2.7, 3.0, 3.3, None, "default"]
 
@@ -130,7 +130,7 @@ CONFIG_SCHEMA = cv.All(
                         ),
                         cv.one_of(*VOLTAGE_LEVELS[-1:], lower=True),
                     ),
-                    cv.Optional(CONF_UCIR_ERASE, default=False): cv.boolean,
+                    cv.Optional(CONF_UICR_ERASE, default=False): cv.boolean,
                 }
             ),
         }
@@ -203,7 +203,7 @@ async def to_code(config: ConfigType) -> None:
     if reg0_config := config.get(CONF_REG0):
         value = VOLTAGE_LEVELS.index(reg0_config[CONF_VOLTAGE])
         cg.add_define("USE_NRF52_REG0_VOUT", value)
-        if reg0_config[CONF_UCIR_ERASE]:
+        if reg0_config[CONF_UICR_ERASE]:
             cg.add_define("USE_NRF52_UICR_ERASE")
 
 
