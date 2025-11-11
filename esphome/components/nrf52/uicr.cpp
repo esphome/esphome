b12923e1b9b3d442a77c9d8a8e4964d90a97c55c
Tomasz Duda <tomaszduda23@gmail.com>
2025-11-11 12:20:51 +0100

    fix

--- esphome/components/nrf52/uicr.cpp
+++ esphome/components/nrf52/uicr.cpp
@@ -13,10 +13,6 @@ nrfx_err_t nrfx_nvmc_uicr_erase(void);
 
 namespace esphome::nrf52 {
 
-static bool regout0_ok() {
-  return (NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) == (USE_NRF52_REG0_VOUT << UICR_REGOUT0_VOUT_Pos);
-}
-
 enum class StatusFlags : uint8_t {
   OK = 0x00,
   NEED_RESET = 0x01,
@@ -32,20 +28,41 @@ constexpr bool operator&(StatusFlags a, StatusFlags b) {
   return (static_cast<uint8_t>(a) & static_cast<uint8_t>(b)) != 0;
 }
 
+static bool regout0_ok() {
+  return (NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) == (USE_NRF52_REG0_VOUT << UICR_REGOUT0_VOUT_Pos);
+}
+
 static StatusFlags set_regout0() {
   /* If the board is powered from USB (high voltage mode),
    * GPIO output voltage is set to 1.8 volts by default.
    */
-  printk("REGOUT0 %ld, %d\n", NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk, USE_NRF52_REG0_VOUT << UICR_REGOUT0_VOUT_Pos);
   if (!regout0_ok()) {
     nvmc_config(NVMC_CONFIG_WEN_Wen);
     NRF_UICR->REGOUT0 =
         (NRF_UICR->REGOUT0 & ~((uint32_t) UICR_REGOUT0_VOUT_Msk)) | (USE_NRF52_REG0_VOUT << UICR_REGOUT0_VOUT_Pos);
     nvmc_wait();
     nvmc_config(NVMC_CONFIG_WEN_Ren);
-    printk("REGOUT0 %ld, %d\n", NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk,
-           USE_NRF52_REG0_VOUT << UICR_REGOUT0_VOUT_Pos);
     return regout0_ok() ? StatusFlags::NEED_RESET : StatusFlags::NEED_ERASE;
+  }
+  return StatusFlags::OK;
+}
+
+// https://github.com/adafruit/Adafruit_nRF52_Bootloader/blob/6a9a6a3e6d0f86918e9286188426a279976645bd/lib/sdk11/components/libraries/bootloader_dfu/dfu_types.h#L61
+constexpr uint32_t BOOTLOADER_REGION_START = 0x000F4000;
+constexpr uint32_t BOOTLOADER_MBR_PARAMS_PAGE_ADDRESS = 0x000FE000;
+
+static bool bootloader_ok() {
+  return NRF_UICR->NRFFW[0] == BOOTLOADER_REGION_START && NRF_UICR->NRFFW[1] == BOOTLOADER_MBR_PARAMS_PAGE_ADDRESS;
+}
+
+static StatusFlags fix_bootloader() {
+  if (!bootloader_ok()) {
+    nvmc_config(NVMC_CONFIG_WEN_Wen);
+    NRF_UICR->NRFFW[0] = BOOTLOADER_REGION_START;
+    NRF_UICR->NRFFW[1] = BOOTLOADER_MBR_PARAMS_PAGE_ADDRESS;
+    nvmc_wait();
+    nvmc_config(NVMC_CONFIG_WEN_Ren);
+    return bootloader_ok() ? StatusFlags::NEED_RESET : StatusFlags::NEED_ERASE;
   }
   return StatusFlags::OK;
 }
@@ -53,12 +70,13 @@ static StatusFlags set_regout0() {
 static StatusFlags set_uicr() {
   StatusFlags status = StatusFlags::OK;
   status |= set_regout0();
+#ifndef USE_BOOTLOADER_MCUBOOT
+  status |= fix_bootloader();
+#endif
   return status;
 }
 
 static int board_esphome_init(void) {
-  printk("board_esphome_init\n");
-
   bool need_reset = false;
   StatusFlags status = set_uicr();
 
