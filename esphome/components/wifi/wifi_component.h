15722f602eaceb70256995e16e2a91e5efe3330f
Flo <florian.hess@azon.ai>
2025-11-11 23:22:31 +0100

    Add is_ap_active method to wifi_component

--- esphome/components/wifi/wifi_component.h
+++ esphome/components/wifi/wifi_component.h
@@ -295,6 +295,7 @@ class WiFiComponent : public Component {
 
   bool has_sta() const;
   bool has_ap() const;
+  bool is_ap_active() const;
 
 #ifdef USE_WIFI_11KV_SUPPORT
   void set_btm(bool btm);
