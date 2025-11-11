0cdf0ab6d5965e73d7236d87f5ce2c3f312ac895
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-11 06:47:04 +0100

    Update waveshare.py

--- esphome/components/mipi_dsi/models/waveshare.py
+++ esphome/components/mipi_dsi/models/waveshare.py
@@ -4,8 +4,8 @@ import esphome.config_validation as cv
 # fmt: off
 DriverChip(
     "WAVESHARE-ESP32-P4-WIFI6-TOUCH-LCD-7B",
-    height=600,
-    width=1024,
+    height=1024,
+    width=600,
     hsync_back_porch=160,
     hsync_pulse_width=10,
     hsync_front_porch=160,
