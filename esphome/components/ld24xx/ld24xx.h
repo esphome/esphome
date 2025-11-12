ab5037671fa8571b03796312095892bd8f6e3c35
J. Nick Koston <nick@home-assistant.io>
2025-11-12 17:34:34 -0600

    still de-dupe

--- esphome/components/ld24xx/ld24xx.h
+++ esphome/components/ld24xx/ld24xx.h
@@ -34,11 +34,10 @@
   }
 
 #define SAFE_PUBLISH_SENSOR_WITHOUT_FILTERS(sensor, value) \
-  if ((sensor) != nullptr) { \
-    if ((sensor)->publish_dedup.next(value)) { \
-      (sensor)->sens->raw_state = static_cast<float>(value); \
-      (sensor)->sens->internal_send_state_to_frontend(static_cast<float>(value)); \
-    } \
+  if ((sensor) != nullptr && (sensor)->sens->state != static_cast<float>(value)) { \
+    (sensor)->publish_dedup.next(value); \
+    (sensor)->sens->raw_state = static_cast<float>(value); \
+    (sensor)->sens->internal_send_state_to_frontend(static_cast<float>(value)); \
   }
 
 #define highbyte(val) (uint8_t)((val) >> 8)
