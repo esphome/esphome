f449da8857df50b8e211c6d3509c53f076dd817b
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-11-13 21:58:02 +1100

    pylint

--- esphome/components/epaper_spi/models/ssd1677.py
+++ esphome/components/epaper_spi/models/ssd1677.py
@@ -7,7 +7,7 @@ class SSD1677(EpaperModel):
 
     # fmt: off
     def get_init_sequence(self, config: dict):
-        width, height = self.get_dimensions(config)
+        width, _height = self.get_dimensions(config)
         return (
             (0x18, 0x80),    # Select internal Temp sensor
             (0x0C, 0xAE, 0xC7, 0xC3, 0xC0, 0x80),  # inrush current level 2
