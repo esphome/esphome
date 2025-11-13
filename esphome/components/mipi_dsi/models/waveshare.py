055d6e940bcf7cdeceeb4f9390e9ad376ffe5c61
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-13 05:50:41 +0100

    Update waveshare.py

--- esphome/components/mipi_dsi/models/waveshare.py
+++ esphome/components/mipi_dsi/models/waveshare.py
@@ -1,11 +1,11 @@
 from esphome.components.mipi import DriverChip
 import esphome.config_validation as cv
 
+# fmt: off
 DriverChip(
     "WAVESHARE-ESP32-P4-WIFI6-TOUCH-LCD-7B",
-    height=600,
     width=1024,
-    # Timings issus du driver officiel Waveshare
+    height=600,
     hsync_back_porch=160,
     hsync_pulse_width=10,
     hsync_front_porch=160,
@@ -13,13 +13,11 @@ DriverChip(
     vsync_pulse_width=1,
     vsync_front_porch=12,
     pclk_frequency="52MHz",
-    lane_bit_rate="1Gbps",  # = 1000 Mbps
+    lane_bit_rate="1Gbps",
     swap_xy=cv.UNDEFINED,
     color_order="RGB",
     reset_pin=33,
-    # INIT SEQUENCE OFFICIELLE EK79007
     initsequence=[
-        # Vendor-specific sequence EK79007
         (0x80, 0x8B),
         (0x81, 0x78),
         (0x82, 0x84),
@@ -27,12 +25,9 @@ DriverChip(
         (0x84, 0xA8),
         (0x85, 0xE3),
         (0x86, 0x88),
-        # PAD_CONTROL : DSI 2 lanes
         (0xB2, 0x10),
-        # Sortie de veille
-        (0x11,),  # Sleep Out
-        ("delay", 120),
-        # Affichage ON
-        (0x29,),  # Display On
-    ],
+        (0x11, 0x00),
+        (0x29, 0x00),
+    ]),
 )
+
