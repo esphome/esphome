f76ba4d10d0b92b12b29b75310f1848228737866
Douwe <61123717+dhoeben@users.noreply.github.com>
2025-11-17 12:15:50 +0100

    Add support for GDEQ0426T82 e-paper display

--- esphome/components/waveshare_epaper/display.py
+++ esphome/components/waveshare_epaper/display.py
@@ -155,6 +155,7 @@ MODELS = {
     "4.20in": ("b", WaveshareEPaper4P2In),
     "4.20in-bv2": ("b", WaveshareEPaper4P2InBV2),
     "4.20in-bv2-bwr": ("b", WaveshareEPaper4P2InBV2BWR),
+    "gdeq0426t82": ("c", GDEQ0426T82),
     "5.65in-f": ("b", WaveshareEPaper5P65InF),
     "5.83in": ("b", WaveshareEPaper5P8In),
     "5.83inv2": ("b", WaveshareEPaper5P8InV2),
