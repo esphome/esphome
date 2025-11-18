dc277e64f4359ef8b36f5fdcaf087f7e983d8967
J. Nick Koston <nick@home-assistant.io>
2025-11-17 18:00:15 -0600

    tweak

--- esphome/core/helpers.h
+++ esphome/core/helpers.h
@@ -1055,12 +1055,7 @@ void get_mac_address_into_buffer(std::span<char, 13> buf);
 /// Get the device MAC address into the given buffer, in colon-separated uppercase hex notation.
 /// Buffer must be exactly 18 bytes (17 for "XX:XX:XX:XX:XX:XX" + null terminator).
 /// Returns pointer to the buffer for convenience.
-inline const char *get_mac_address_pretty_into_buffer(std::span<char, 18> buf) {
-  uint8_t mac[6];
-  get_mac_address_raw(mac);
-  format_mac_addr_upper(mac, buf.data());
-  return buf.data();
-}
+const char *get_mac_address_pretty_into_buffer(std::span<char, 18> buf);
 
 #ifdef USE_ESP32
 /// Set the MAC address to use from the provided byte array (6 bytes).
