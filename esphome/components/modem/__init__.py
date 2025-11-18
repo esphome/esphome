c77f0f4c1deff0edde1f96992108c3074921191d
oarcher <olivier.archer@gmail.com>
2025-11-18 18:29:38 +0100

    bump esp_modem to 1.4.2

--- esphome/components/modem/__init__.py
+++ esphome/components/modem/__init__.py
@@ -351,7 +351,7 @@ async def to_code(config):
     add_idf_component(
         name="esp_modem",
         repo="https://github.com/espressif/esp-protocols.git",
-        ref="modem-v1.4.0",
+        ref="modem-v1.4.2",
         path="components/esp_modem",
     )
 
