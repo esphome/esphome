4533b8f92c4b363c23d282ca61415913abaa30af
J. Nick Koston <nick@home-assistant.io>
2025-11-19 23:20:24 -0600

    tweaks

--- esphome/components/api/api_connection.cpp
+++ esphome/components/api/api_connection.cpp
@@ -1537,8 +1537,8 @@ void APIConnection::on_home_assistant_state_response(const HomeAssistantStateRes
   for (auto &it : this->parent_->get_state_subs()) {
     // Compare entity_id and attribute with message fields
     bool entity_match = (strcmp(it.entity_id_, msg.entity_id.c_str()) == 0);
-    bool attribute_match =
-        it.has_attribute_ ? (strcmp(it.attribute_, msg.attribute.c_str()) == 0) : msg.attribute.empty();
+    bool attribute_match = (it.has_attribute_ && strcmp(it.attribute_, msg.attribute.c_str()) == 0) ||
+                           (!it.has_attribute_ && msg.attribute.empty());
 
     if (entity_match && attribute_match) {
       it.callback_(msg.state);
