e37885ade546da1ee1fc01c26ddc79ff132513e4
J. Nick Koston <nick@home-assistant.io>
2025-11-20 12:12:42 -0600

    syntax

--- esphome/core/component.h
+++ esphome/core/component.h
@@ -218,7 +218,7 @@ class Component {
   void status_set_warning(const LogString *message);
 
   // Remove before 2026.12.0
-  ESPDEPRECATED("Use status_set_error(LOG_STR(\"message\")) instead. Removed in 2026.12.0", "2025.6.0");
+  ESPDEPRECATED("Use status_set_error(LOG_STR(message)) instead. Removed in 2026.12.0", "2025.6.0")
   void status_set_error(const char *message = nullptr);
   void status_set_error(const LogString *message);
 
