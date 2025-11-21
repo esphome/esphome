3ecdf16d7555a80baa712273a1ca5c5134e6cf04
Kevin Ahrendt <kevin.ahrendt@openhomefoundation.org>
2025-11-21 09:25:27 -0500

    upstream MDNS bug maybe?

--- esphome/components/mdns/mdns_component.cpp
+++ esphome/components/mdns/mdns_component.cpp
@@ -138,7 +138,8 @@ void MDNSComponent::compile_records_(StaticVector<MDNSService, MDNS_SERVICE_COUN
 
 #ifdef USE_DASHBOARD_IMPORT
     MDNS_STATIC_CONST_CHAR(TXT_PACKAGE_IMPORT_URL, "package_import_url");
-    txt_records.push_back({MDNS_STR(TXT_PACKAGE_IMPORT_URL), MDNS_STR(dashboard_import::get_package_import_url())});
+    txt_records.push_back({MDNS_STR(TXT_PACKAGE_IMPORT_URL),
+                           MDNS_STR(this->add_dynamic_txt_value(dashboard_import::get_package_import_url()))});
 #endif
   }
 #endif  // USE_API
