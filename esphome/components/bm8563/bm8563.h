1e3f608e15c7e21d224a61e79f9339e94b8ee962
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-11-21 15:04:00 -0500

    Fix

--- esphome/components/bm8563/bm8563.h
+++ esphome/components/bm8563/bm8563.h
@@ -36,19 +36,19 @@ class BM8563 : public time::RealTimeClock, public i2c::I2CDevice {
 
 template<typename... Ts> class WriteAction : public Action<Ts...>, public Parented<BM8563> {
  public:
-  void play(Ts... x) override { this->parent_->write_time(); }
+  void play(const Ts &...x) override { this->parent_->write_time(); }
 };
 
 template<typename... Ts> class ReadAction : public Action<Ts...>, public Parented<BM8563> {
  public:
-  void play(Ts... x) override { this->parent_->read_time(); }
+  void play(const Ts &...x) override { this->parent_->read_time(); }
 };
 
 template<typename... Ts> class TimerAction : public Action<Ts...>, public Parented<BM8563> {
  public:
   TEMPLATABLE_VALUE(uint32_t, duration)
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     auto duration = this->duration_.value(x...);
     this->parent_->start_timer(duration);
   }
