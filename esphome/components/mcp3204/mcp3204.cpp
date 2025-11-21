cde1c904c5652b30c522166099170db094989c11
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-11-21 14:11:01 -0500

    Clean up

--- esphome/components/mcp3204/mcp3204.cpp
+++ esphome/components/mcp3204/mcp3204.cpp
@@ -29,7 +29,7 @@ float MCP3204::read_data(uint8_t pin, bool differential) {
   b1 = this->transfer_byte(0x00);
   this->disable();
 
-  uint16_t digital_value = (b0 << 4) | (b1 >> 4);
+  uint16_t digital_value = encode_uint16(b0, b1) >> 4;
   return float(digital_value) / 4096.000 * this->reference_voltage_;  // in V
 }
 
