f5578e1b90b1b3b462f3dee5dea736142d12c101
J. Nick Koston <nick@home-assistant.io>
2025-11-24 12:02:31 -0600

    fix esp8266

--- esphome/components/prometheus/prometheus_handler.cpp
+++ esphome/components/prometheus/prometheus_handler.cpp
@@ -141,9 +141,14 @@ void PrometheusHandler::add_friendly_name_label_(AsyncResponseStream *stream, st
   }
 }
 
+#ifdef USE_ESP8266
 void PrometheusHandler::print_metric_labels_(AsyncResponseStream *stream, const __FlashStringHelper *metric_name,
                                              EntityBase *obj, std::string &area, std::string &node,
                                              std::string &friendly_name) {
+#else
+void PrometheusHandler::print_metric_labels_(AsyncResponseStream *stream, const char *metric_name, EntityBase *obj,
+                                             std::string &area, std::string &node, std::string &friendly_name) {
+#endif
   stream->print(metric_name);
   stream->print(ESPHOME_F("{id=\""));
   stream->print(relabel_id_(obj).c_str());
