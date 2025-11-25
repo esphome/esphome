f494c174471d7c0855e142ccf5efbb0d0b388783
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-11-25 18:53:27 +1100

    Fix spinner arc length

--- esphome/components/lvgl/widgets/spinner.py
+++ esphome/components/lvgl/widgets/spinner.py
@@ -11,19 +11,10 @@ CONF_SPINNER = "spinner"
 
 SPINNER_SCHEMA = cv.Schema(
     {
-        cv.Optional(CONF_ARC_LENGTH, default=200): cv.All(
-            lv_angle_degrees, cv.int_range(min=180, max=360)
+        cv.Optional(CONF_ARC_LENGTH, default=240): cv.All(
+            lv_angle_degrees, cv.int_range(min=0, max=360)
         ),
-        cv.Optional(CONF_SPIN_TIME, default="1s"): lv_milliseconds,
-    }
-)
-
-SPINNER_MODIFY_SCHEMA = cv.Schema(
-    {
-        cv.Optional(CONF_ARC_LENGTH): cv.All(
-            lv_angle_degrees, cv.int_range(min=180, max=360)
-        ),
-        cv.Optional(CONF_SPIN_TIME): lv_milliseconds,
+        cv.Optional(CONF_SPIN_TIME, default="2s"): lv_milliseconds,
     }
 )
 
@@ -35,14 +26,14 @@ class SpinnerType(WidgetType):
             LvType("lv_spinner_t"),
             (CONF_MAIN, CONF_INDICATOR),
             SPINNER_SCHEMA,
-            modify_schema=SPINNER_MODIFY_SCHEMA,
         )
 
     async def to_code(self, w: Widget, config):
         spin_time = await lv_milliseconds.process(config.get(CONF_SPIN_TIME))
-        arc_length = await lv_angle_degrees.process(config.get(CONF_ARC_LENGTH))
-        if arc_length and spin_time:
-            lv.spinner_set_anim_params(w.obj, spin_time, arc_length)
+        arc_length = int(config[CONF_ARC_LENGTH])
+        if arc_length < 180:
+            arc_length += 180
+        lv.spinner_set_anim_params(w.obj, spin_time, arc_length)
 
     def get_uses(self):
         return (CONF_ARC,)
