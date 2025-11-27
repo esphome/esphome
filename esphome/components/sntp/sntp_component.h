0e2556eccb3310c8ed46ce1be337b9cfd0527787
Anton Sergunov <setosha@gmail.com>
2025-11-27 05:25:00 +0000

    cleanup

--- esphome/components/sntp/sntp_component.h
+++ esphome/components/sntp/sntp_component.h
@@ -36,12 +36,10 @@ class SNTPComponent : public time::RealTimeClock {
  protected:
   void setup_servers_();
 
- protected:
   // Store const char pointers to string literals
   // ESP8266: strings in rodata (RAM), but avoids std::string overhead (~24 bytes each)
   // Other platforms: strings in flash
   std::array<const char *, SNTP_SERVER_COUNT> servers_;
-
 #if !defined(USE_ESP32)
   bool has_time_{false};
 #endif
