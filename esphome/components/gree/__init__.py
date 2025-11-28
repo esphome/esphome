100646897e2d44c09dede9cc7b99d42e5b57af2d
H. Árkosi Róbert <robreg@zsurob.hu>
2025-11-28 13:06:39 +0100

    Restore Gree switch state on boot

---
+++ esphome/components/gree/__init__.py
@@ -0,0 +1,4 @@
+import esphome.codegen as cg
+
+
+gree_ns = cg.esphome_ns.namespace("gree")
---
+++ esphome/components/gree/switch/gree_switch.cpp
@@ -0,0 +1,59 @@
+#include "gree_switch.h"
+
+namespace esphome {
+namespace gree {
+
+void GreeTurboSwitch::setup() {
+  auto initial = this->get_initial_state_with_restore_mode();
+  if (initial.has_value()) {
+    this->write_state(*initial);
+  }
+}
+
+void GreeTurboSwitch::dump_config() { LOG_SWITCH("  ", "Gree Turbo Switch", this); }
+
+void GreeTurboSwitch::write_state(bool state) {
+  this->parent_->set_turbo_mode(state);
+}
+
+void GreeLightSwitch::setup() {
+  auto initial = this->get_initial_state_with_restore_mode();
+  if (initial.has_value()) {
+    this->write_state(*initial);
+  }
+}
+
+void GreeLightSwitch::dump_config() { LOG_SWITCH("  ", "Gree Light Switch", this); }
+
+void GreeLightSwitch::write_state(bool state) {
+  this->parent_->set_light_mode(state);
+}
+
+void GreeHealthSwitch::setup() {
+  auto initial = this->get_initial_state_with_restore_mode();
+  if (initial.has_value()) {
+    this->write_state(*initial);
+  }
+}
+
+void GreeHealthSwitch::dump_config() { LOG_SWITCH("  ", "Gree Health Switch", this); }
+
+void GreeHealthSwitch::write_state(bool state) {
+  this->parent_->set_health_mode(state);
+}
+
+void GreeXfanSwitch::setup() {
+  auto initial = this->get_initial_state_with_restore_mode();
+  if (initial.has_value()) {
+    this->write_state(*initial);
+  }
+}
+
+void GreeXfanSwitch::dump_config() { LOG_SWITCH("  ", "Gree X-FAN Switch", this); }
+
+void GreeXfanSwitch::write_state(bool state) {
+  this->parent_->set_xfan_mode(state);
+}
+
+}  // namespace gree
+}  // namespace esphome
