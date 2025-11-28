ed0751246ad4468bd345110f1b23c936808e8815
J. Nick Koston <nick@home-assistant.io>
2025-11-28 14:28:02 -0600

    [logger] Conditionally compile log level change listener

--- esphome/components/logger/__init__.py
+++ esphome/components/logger/__init__.py
@@ -406,6 +406,8 @@ async def to_code(config):
             conf,
         )
 
+    CORE.add_job(final_step)
+
 
 def validate_printf(value):
     # https://stackoverflow.com/questions/30011379/how-can-i-parse-a-c-format-string-in-python
@@ -506,3 +508,23 @@ FILTER_SOURCE_FILES = filter_source_files_from_platform(
         },
     }
 )
+
+# Key for CORE.data to track if level listeners are requested
+LOGGER_LEVEL_LISTENERS_KEY = "logger_level_listeners"
+
+
+def request_logger_level_listeners() -> None:
+    """Request that logger level listeners be compiled in.
+
+    Components that need to be notified about log level changes should call this
+    function during their code generation. This enables the add_level_listener()
+    method and compiles in the listener vector.
+    """
+    CORE.data[LOGGER_LEVEL_LISTENERS_KEY] = True
+
+
+@coroutine_with_priority(CoroPriority.FINAL)
+async def final_step():
+    """Final code generation step to configure optional logger features."""
+    if CORE.data.get(LOGGER_LEVEL_LISTENERS_KEY, False):
+        cg.add_define("USE_LOGGER_LEVEL_LISTENERS")
