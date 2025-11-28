264fd6ed748fb5cbf8ba92a49e4deabc9e9c20d3
J. Nick Koston <nick@home-assistant.io>
2025-11-28 15:18:05 -0600

    safer

--- esphome/components/espnow/espnow_component.cpp
+++ esphome/components/espnow/espnow_component.cpp
@@ -158,6 +158,12 @@ bool ESPNowComponent::is_wifi_enabled() {
 }
 
 void ESPNowComponent::setup() {
+#ifndef USE_WIFI
+  // Initialize LwIP stack for wake_loop_threadsafe() socket support
+  // When WiFi component is present, it handles esp_netif_init()
+  ESP_ERROR_CHECK(esp_netif_init());
+#endif
+
   if (this->enable_on_boot_) {
     this->enable_();
   } else {
@@ -178,8 +184,6 @@ void ESPNowComponent::enable() {
 void ESPNowComponent::enable_() {
   if (!this->is_wifi_enabled()) {
     esp_event_loop_create_default();
-    // Initialize LwIP stack for wake_loop_threadsafe() socket support
-    ESP_ERROR_CHECK(esp_netif_init());
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 
