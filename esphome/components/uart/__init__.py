03ad8aac9ff1228bae99b10830fe4334537e66d0
kbx81 <kbx81x@gmail.com>
2025-11-30 01:49:34 -0600

    Implement proposal in #12170

--- esphome/components/uart/__init__.py
+++ esphome/components/uart/__init__.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from logging import getLogger
 import math
 import re
@@ -39,6 +40,8 @@ from esphome.yaml_util import make_data_base
 _LOGGER = getLogger(__name__)
 
 CODEOWNERS = ["@esphome/core"]
+DOMAIN = "uart"
+
 uart_ns = cg.esphome_ns.namespace("uart")
 UARTComponent = uart_ns.class_("UARTComponent")
 
@@ -52,7 +55,6 @@ LibreTinyUARTComponent = uart_ns.class_(
 )
 HostUartComponent = uart_ns.class_("HostUartComponent", UARTComponent, cg.Component)
 
-UART_WAKE_LOOP_ON_RX_KEY = "uart_wake_loop_on_rx"
 
 NATIVE_UART_CLASSES = (
     str(IDFUARTComponent),
@@ -100,6 +102,30 @@ UARTDebugger = uart_ns.class_("UARTDebugger", cg.Component, automation.Action)
 UARTDummyReceiver = uart_ns.class_("UARTDummyReceiver", cg.Component)
 MULTI_CONF = True
 MULTI_CONF_NO_DEFAULT = True
+
+
+@dataclass
+class UARTData:
+    """State data for UART component configuration generation."""
+
+    wake_loop_on_rx: bool = False
+
+
+def _get_data() -> UARTData:
+    """Get UART component data from CORE.data."""
+    if DOMAIN not in CORE.data:
+        CORE.data[DOMAIN] = UARTData()
+    return CORE.data[DOMAIN]
+
+
+def request_wake_loop_on_rx() -> None:
+    """Request that the UART wake the main loop when data is received.
+
+    Components that need low-latency notification of incoming UART data
+    should call this function during their code generation.
+    This enables the RX event task which wakes the main loop when data arrives.
+    """
+    _get_data().wake_loop_on_rx = True
 
 
 def validate_raw_data(value):
@@ -476,20 +502,10 @@ async def uart_write_to_code(config, action_id, template_arg, args):
     return var
 
 
-def request_uart_wake_loop_on_rx() -> None:
-    """Request that the UART wake the main loop when data is received.
-
-    Components that need low-latency notification of incoming UART data
-    should call this function during their code generation.
-    This enables the RX event task which wakes the main loop when data arrives.
-    """
-    CORE.data[UART_WAKE_LOOP_ON_RX_KEY] = True
-
-
 @coroutine_with_priority(CoroPriority.FINAL)
 async def final_step():
     """Final code generation step to configure optional UART features."""
-    if CORE.data.get(UART_WAKE_LOOP_ON_RX_KEY, False):
+    if _get_data().wake_loop_on_rx:
         cg.add_define("USE_UART_WAKE_LOOP_ON_RX")
 
 
