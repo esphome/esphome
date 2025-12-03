2dfdf159c64aa95ea56025a2c63dbc59365ad007
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-12-03 14:13:47 -0500

    Clean up

--- esphome/components/cst816/touchscreen/cst816_touchscreen.cpp
+++ esphome/components/cst816/touchscreen/cst816_touchscreen.cpp
@@ -18,8 +18,8 @@ void CST816Touchscreen::continue_setup_() {
     }
   }
 
+  // CST826/CST836 return 0 for chip ID, need to read from factory ID register
   if (this->chip_id_ == 0) {
-    // CST826/CST836 return 0 for chip ID, need to read from factory ID register
     if (!this->read_byte(REG_FACTORY_ID, &this->chip_id_) && !this->skip_probe_) {
       this->status_set_error(LOG_STR("Failed to read chip ID"));
       this->mark_failed();
