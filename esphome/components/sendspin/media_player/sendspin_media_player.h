08086fa90a88f8644df92c88182748e15b674202
Kevin Ahrendt <kevin.ahrendt@openhomefoundation.org>
2025-12-05 08:03:41 -0500

    don't include sendspin audio chunk if control only media player

--- esphome/components/sendspin/media_player/sendspin_media_player.h
+++ esphome/components/sendspin/media_player/sendspin_media_player.h
@@ -4,13 +4,13 @@
 
 #if defined(USE_ESP_IDF) && defined(USE_MEDIA_PLAYER)
 
-#include "esphome/components/sendspin/sendspin_audio_chunk.h"
 #include "esphome/components/sendspin/sendspin_hub.h"
 
 #include "esphome/components/audio/audio.h"
 #include "esphome/components/audio/audio_chunk_queue.h"
 #include "esphome/components/media_player/media_player.h"
 #if defined(USE_SENDSPIN_AUDIO)
+#include "esphome/components/sendspin/sendspin_audio_chunk.h"
 #include "esphome/components/speaker/speaker.h"
 #endif
 
