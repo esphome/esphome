288b6fe6eb63ae9b5ee0f5087666046d7fbcce08
J. Nick Koston <nick@home-assistant.io>
2025-12-06 08:05:51 -0600

    fix dangling pointer

--- esphome/components/api/api_server.cpp
+++ esphome/components/api/api_server.cpp
@@ -602,21 +602,18 @@ uint32_t APIServer::register_active_action_call(uint32_t client_call_id, APIConn
   this->active_action_calls_.push_back({action_call_id, client_call_id, conn});
 
   // Schedule automatic cleanup after timeout (client will have given up by then)
-  char timeout_name[32];
-  snprintf(timeout_name, sizeof(timeout_name), "action_call_%u", action_call_id);
-  this->set_timeout(timeout_name, USE_API_ACTION_CALL_TIMEOUT_MS, [this, action_call_id]() {
-    ESP_LOGD(TAG, "Action call %u timed out", action_call_id);
-    this->unregister_active_action_call(action_call_id);
-  });
+  this->set_timeout(str_sprintf("action_call_%u", action_call_id), USE_API_ACTION_CALL_TIMEOUT_MS,
+                    [this, action_call_id]() {
+                      ESP_LOGD(TAG, "Action call %u timed out", action_call_id);
+                      this->unregister_active_action_call(action_call_id);
+                    });
 
   return action_call_id;
 }
 
 void APIServer::unregister_active_action_call(uint32_t action_call_id) {
   // Cancel the timeout for this action call
-  char timeout_name[32];
-  snprintf(timeout_name, sizeof(timeout_name), "action_call_%u", action_call_id);
-  this->cancel_timeout(timeout_name);
+  this->cancel_timeout(str_sprintf("action_call_%u", action_call_id));
 
   // Swap-and-pop is more efficient than remove_if for unordered vectors
   for (size_t i = 0; i < this->active_action_calls_.size(); i++) {
@@ -633,9 +630,7 @@ void APIServer::unregister_active_action_calls_for_connection(APIConnection *con
   for (size_t i = 0; i < this->active_action_calls_.size();) {
     if (this->active_action_calls_[i].connection == conn) {
       // Cancel the timeout for this action call
-      char timeout_name[32];
-      snprintf(timeout_name, sizeof(timeout_name), "action_call_%u", this->active_action_calls_[i].action_call_id);
-      this->cancel_timeout(timeout_name);
+      this->cancel_timeout(str_sprintf("action_call_%u", this->active_action_calls_[i].action_call_id));
 
       std::swap(this->active_action_calls_[i], this->active_action_calls_.back());
       this->active_action_calls_.pop_back();
