df20532d0c200ea9719b151da7589b81533fd579
Sébastien Blanchet <120399978+arno1801@users.noreply.github.com>
2025-12-08 13:06:13 -0500

    Correct gpio_esp32_c5.py "is" and return value

--- esphome/components/esp32/gpio_esp32_c5.py
+++ esphome/components/esp32/gpio_esp32_c5.py
@@ -51,8 +51,8 @@ def esp32_c5_validate_supports(value):
 def esp32_c5_validate_lp_i2c(value):
     lp_sda_pin = _ESP32C5_I2C_LP_PINS["SDA"]
     lp_scl_pin = _ESP32C5_I2C_LP_PINS["SCL"]
-    if int(value[CONF_SDA]) == lp_sda_pin and int(value[CONF_SCL]) == lp_scl_pin:
-        return
-    raise cv.Invalid(
-        f"Low power i2c interface is only supported on GPIO{lp_sda_pin} SDA and GPIO{lp_scl_pin} SCL for ESP32-C5"
-    )
+    if int(value[CONF_SDA]) != lp_sda_pin or int(value[CONF_SCL]) != lp_scl_pin:
+        raise cv.Invalid(
+            f"Low power i2c interface is only supported on GPIO{lp_sda_pin} SDA and GPIO{lp_scl_pin} SCL for ESP32-C5"
+        )
+    return value
