fb72b6c1378bc71b3b9a7e2d5fda7772d7a92e7e
David Woodhouse <dwmw2@infradead.org>
2025-12-15 18:05:33 +0000

    Add FNV-1a hash functions
    Add fnv1a_hash() and fnv1a_hash_extend() as preferred alternatives to
    the legacy fnv1_hash(). FNV-1a uses XOR-then-multiply instead of
    multiply-then-XOR, providing better avalanche characteristics.
    
    The implementation supports both C strings and std::string, with
    fnv1a_hash_extend() allowing incremental hashing of multiple strings.

--- esphome/core/helpers.h
+++ esphome/core/helpers.h
@@ -378,8 +378,18 @@ uint16_t crc16be(const uint8_t *data, uint16_t len, uint16_t crc = 0, uint16_t p
                  bool refout = false);
 
 /// Calculate a FNV-1 hash of \p str.
+/// Note: FNV-1a (fnv1a_hash) is preferred for new code due to better avalanche characteristics.
 uint32_t fnv1_hash(const char *str);
 inline uint32_t fnv1_hash(const std::string &str) { return fnv1_hash(str.c_str()); }
+
+/// Extend a FNV-1a hash with additional string data.
+uint32_t fnv1a_hash_extend(uint32_t hash, const char *str);
+inline uint32_t fnv1a_hash_extend(uint32_t hash, const std::string &str) {
+  return fnv1a_hash_extend(hash, str.c_str());
+}
+/// Calculate a FNV-1a hash of \p str.
+inline uint32_t fnv1a_hash(const char *str) { return fnv1a_hash_extend(2166136261UL, str); }
+inline uint32_t fnv1a_hash(const std::string &str) { return fnv1a_hash(str.c_str()); }
 
 /// Return a random 32-bit unsigned integer.
 uint32_t random_uint32();
