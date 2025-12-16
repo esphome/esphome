0b602c99d72a54df602fba33037b63b98b20c73b
Jordan Zucker <jordan.zucker@gmail.com>
2025-12-16 15:44:13 -0800

    Truly the last

--- esphome/components/prometheus/prometheus_handler.cpp
+++ esphome/components/prometheus/prometheus_handler.cpp
@@ -1240,26 +1240,27 @@ void PrometheusHandler::datetime_row_(AsyncResponseStream *stream, datetime::Dat
     // // date_time.recalc_timestamp_utc(false);
     // date_time.recalc_timestamp_local();
     // stream->print(static_cast<int64_t>(date_time.timestamp));
-    //    // Construct a date time object manually to ensure UTC interpretation
-    //    ESPTime date_time_utc = {};
-    //    date_time_utc.year = obj->year;
-    //    date_time_utc.month = obj->month;
-    //    date_time_utc.day_of_month = obj->day;
-    //    date_time_utc.hour = obj->hour;
-    //    date_time_utc.minute = obj->minute;
-    //    date_time_utc.second = obj->second;
-    //    date_time_utc.day_of_week = 1;
-    //    date_time_utc.day_of_year = 1;
-    //    date_time_utc.is_dst = false;
+    // Construct a date time object manually to ensure UTC interpretation
+    ESPTime date_time_utc = {};
+    date_time_utc.year = obj->year;
+    date_time_utc.month = obj->month;
+    date_time_utc.day_of_month = obj->day;
+    date_time_utc.hour = obj->hour;
+    date_time_utc.minute = obj->minute;
+    date_time_utc.second = obj->second;
+    date_time_utc.day_of_week = 1;
+    date_time_utc.day_of_year = 1;
+    date_time_utc.is_dst = false;
     //    // First get local timestamp
-    //    date_time_utc.recalc_timestamp_local();
+    // date_time_utc.recalc_timestamp_local();
     //    // Convert local to UTC by adding the current timezone offset
     //    time_t utc_timestamp = date_time_utc.timestamp + ESPTime::timezone_offset();
     //    stream->print(static_cast<int64_t>(utc_timestamp));
     // Construct a date time object
-    ESPTime date_time = obj->state_as_esptime();
-    time_t final_timestamp = date_time.timestamp - ESPTime::timezone_offset();
-    stream->print(static_cast<int64_t>(final_timestamp));
+    // ESPTime date_time = obj->state_as_esptime();
+    // time_t final_timestamp = date_time.timestamp - ESPTime::timezone_offset();
+    date_time.recalc_timestamp_utc(false);
+    stream->print(static_cast<int64_t>(date_time.timestamp));
     stream->print(ESPHOME_F("\n"));
   } else {
     // Invalid state
