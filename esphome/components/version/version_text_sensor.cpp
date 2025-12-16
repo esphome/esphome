12734ba2581d8bfe821bff87ecb16bbc3de19cd2
David Woodhouse <dwmw2@infradead.org>
2025-12-16 19:03:13 +0000

    Revert "Use PROGMEM format strings to reduce RAM usage on ESP8266"
    This reverts commit da67c47a762568bb3376e591a56b893267267c7e.

--- esphome/components/version/version_text_sensor.cpp
+++ esphome/components/version/version_text_sensor.cpp
@@ -10,16 +10,14 @@ namespace version {
 static const char *const TAG = "version.text_sensor";
 
 void VersionTextSensor::setup() {
-  char version_str[128];
   if (this->hide_timestamp_) {
-    snprintf_P(version_str, sizeof(version_str), PSTR(ESPHOME_VERSION " (%08" PRIx32 ")"), App.get_config_hash());
+    this->publish_state(str_sprintf(ESPHOME_VERSION " (config hash 0x%08" PRIx32 ")", App.get_config_hash()));
   } else {
     char build_time_str[esphome::Application::BUILD_TIME_STR_SIZE];
     App.get_build_time_string(build_time_str);
-    snprintf_P(version_str, sizeof(version_str), PSTR(ESPHOME_VERSION " (%08" PRIx32 ", built: %s)"),
-               App.get_config_hash(), build_time_str);
+    this->publish_state(str_sprintf(ESPHOME_VERSION " (config hash 0x%08" PRIx32 ", built: %s)", App.get_config_hash(),
+                                    build_time_str));
   }
-  this->publish_state(version_str);
 }
 float VersionTextSensor::get_setup_priority() const { return setup_priority::DATA; }
 void VersionTextSensor::set_hide_timestamp(bool hide_timestamp) { this->hide_timestamp_ = hide_timestamp; }
