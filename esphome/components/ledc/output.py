49e47821216af5e1debb940e5d412b7f59aa4ad2
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2025-12-16 01:17:55 +0100

    set `min_included=False`

--- esphome/components/ledc/output.py
+++ esphome/components/ledc/output.py
@@ -46,7 +46,7 @@ CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
         cv.Required(CONF_ID): cv.declare_id(LEDCOutput),
         cv.Required(CONF_PIN): pins.internal_gpio_output_pin_schema,
         cv.Optional(CONF_FREQUENCY, default="1kHz"): cv.All(
-            cv.frequency, cv.float_range(min=0.0)
+            cv.frequency, cv.float_range(min=0, min_included=False)
         ),
         cv.Optional(CONF_CHANNEL): cv.int_range(min=0, max=15),
         cv.Optional(CONF_PHASE_ANGLE): cv.All(
