5e06dfa4924f6f1b926212be9544b6cce44cb6b1
Anna Oake <anna@oa.ke>
2025-12-17 20:08:55 +0100

    [cc1101] Fix default frequency values

--- esphome/components/cc1101/cc1101.cpp
+++ esphome/components/cc1101/cc1101.cpp
@@ -100,10 +100,10 @@ CC1101Component::CC1101Component() {
 
   // Default Settings
   this->set_frequency(433920000);
-  this->set_if_frequency(153);
-  this->set_filter_bandwidth(203);
+  this->set_if_frequency(153000);
+  this->set_filter_bandwidth(203000);
   this->set_channel(0);
-  this->set_channel_spacing(200);
+  this->set_channel_spacing(200000);
   this->set_symbol_rate(5000);
   this->set_sync_mode(SyncMode::SYNC_MODE_NONE);
   this->set_carrier_sense_above_threshold(true);
