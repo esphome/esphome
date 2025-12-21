354e1fb8495e6cf23a10a730b1f7207e7c969a25
MaoXiaodong <mxd@eoas.cn>
2025-12-21 18:15:30 +0800

    Fix file formatting

--- esphome/components/uart/event/__init__.py
+++ esphome/components/uart/event/__init__.py
@@ -2,8 +2,8 @@ import esphome.codegen as cg
 from esphome.components import event, uart
 import esphome.config_validation as cv
 from esphome.const import CONF_EVENT_TYPES, CONF_ID
-from esphome.types import ConfigType
 from esphome.core import ID
+from esphome.types import ConfigType
 
 from .. import uart_ns
 
