5f7d6735956c4b476c8e78648360ace076040670
Martin Ebner <martinebner@me.com>
2025-12-21 09:47:45 +0530

    add boilerplate __init__.py

--- esphome/components/sen6x/__init__.py
+++ esphome/components/sen6x/__init__.py
@@ -1,1 +1,12 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
 CODEOWNERS = ["@martgras", "@mebner86"]
+
+# Define the namespace so ESPHome recognizes the component prefix
+sen6x_ns = cg.esphome_ns.namespace("sen6x")
+
+# Minimal schema to allow the component to be loaded
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({}),
+)
