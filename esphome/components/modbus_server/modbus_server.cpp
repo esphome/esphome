f0fac85bebc29565296a0d0092b3e780cae4af32
Bonne Eggleston <bonne@exciton.com.au>
2025-12-21 16:23:56 +0000

    Fix write register response

--- esphome/components/modbus_server/modbus_server.cpp
+++ esphome/components/modbus_server/modbus_server.cpp
@@ -153,9 +153,7 @@ void ModbusServer::on_modbus_write_registers(uint8_t function_code, const std::v
   }
 
   std::vector<uint8_t> response;
-  response.reserve(6);
-  response.push_back(this->address_);
-  response.push_back(function_code);
+  response.reserve(4);
   response.insert(response.end(), data.begin(), data.begin() + 4);
   this->send_raw(response);
 }
