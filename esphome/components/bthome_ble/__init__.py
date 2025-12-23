0816a4bc3988b02aa7a93e930680424dd0fbdb7a
H. Árkosi Róbert <robreg@zsurob.hu>
2025-12-23 18:44:10 +0100

    Handle optional BLE tracker id without config changes

--- esphome/components/bthome_ble/__init__.py
+++ esphome/components/bthome_ble/__init__.py
@@ -6,6 +6,14 @@ from esphome.const import CONF_ID, CONF_MAC_ADDRESS
 DEPENDENCIES = ["esp32_ble_tracker"]
 AUTO_LOAD = ["esp32_ble_tracker"]
 
+BLE_DEVICE_SCHEMA = cv.Schema(
+    {
+        cv.Optional(esp32_ble_tracker.CONF_ESP32_BLE_ID): cv.use_id(
+            esp32_ble_tracker.ESP32BLETracker
+        ),
+    }
+)
+
 bthome_ble_ns = cg.esphome_ns.namespace("bthome_ble")
 BTHomeBLE = bthome_ble_ns.class_(
     "BTHomeBLE", esp32_ble_tracker.ESPBTDeviceListener, cg.Component
@@ -22,7 +30,7 @@ def bthome_ble_base_schema(extra_schema=None):
                 cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
             }
         )
-        .extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA)
+        .extend(BLE_DEVICE_SCHEMA)
         .extend(cv.COMPONENT_SCHEMA)
         .extend(extra_schema)
     )
@@ -30,5 +38,9 @@ def bthome_ble_base_schema(extra_schema=None):
 
 async def setup_bthome_ble(var, config):
     await cg.register_component(var, config)
-    await esp32_ble_tracker.register_ble_device(var, config)
+    tracker_config = dict(config)
+    tracker_config.setdefault(
+        esp32_ble_tracker.CONF_ESP32_BLE_ID, esp32_ble_tracker.DEFAULT_ESP32_BLE_ID
+    )
+    await esp32_ble_tracker.register_ble_device(var, tracker_config)
     cg.add(var.set_address(config[CONF_MAC_ADDRESS].as_hex))
