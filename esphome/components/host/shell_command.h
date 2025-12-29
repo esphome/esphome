cb268252293c203fb5e4ad0b9932e81b0876322c
H. Árkosi Róbert <robreg@zsurob.hu>
2025-12-29 12:06:33 +0100

    Allow selecting shell and environment for host commands

---
+++ esphome/components/host/shell_command.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifdef USE_HOST
+
+#include <string>
+
+namespace esphome {
+namespace host {
+
+struct ShellCommandResult {
+  int exit_code{-1};
+  std::string stdout_output;
+  std::string stderr_output;
+};
+
+struct ShellCommandOptions {
+  std::string shell{"/bin/sh"};
+  std::vector<std::pair<std::string, std::string>> environment;
+};
+
+ShellCommandResult execute_shell_command(const std::string &command,
+                                         const ShellCommandOptions &options = {});
+
+}  // namespace host
+}  // namespace esphome
+
+#endif  // USE_HOST
