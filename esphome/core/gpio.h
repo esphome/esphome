53aa3f539b942175f930ba33c40c443ae6c47e51
J. Nick Koston <nick@home-assistant.io>
2025-12-30 13:04:26 -1000

    copilot suggestion is overkill and breaks things

--- esphome/core/gpio.h
+++ esphome/core/gpio.h
@@ -13,9 +13,9 @@ inline constexpr size_t GPIO_SUMMARY_MAX_LEN = 48;
 
 #define LOG_PIN(prefix, pin) \
   if ((pin) != nullptr) { \
-    char pin_buf_[GPIO_SUMMARY_MAX_LEN]; \
-    (pin)->dump_summary(pin_buf_, sizeof(pin_buf_)); \
-    ESP_LOGCONFIG(TAG, prefix "%s", pin_buf_); \
+    char esphome_pin_buf_[GPIO_SUMMARY_MAX_LEN]; \
+    (pin)->dump_summary(esphome_pin_buf_, sizeof(esphome_pin_buf_)); \
+    ESP_LOGCONFIG(TAG, prefix "%s", esphome_pin_buf_); \
   }
 
 // put GPIO flags in a namespace to not pollute esphome namespace
