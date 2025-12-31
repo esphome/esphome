1ee392823009863af5aa66904d4b3b054ca479f2
estockda <69396603+estockda@users.noreply.github.com>
2025-12-30 19:04:00 -0500

    Update __init__.py
    Adding code ownership

--- esphome/components/distance_uart/__init__.py
+++ esphome/components/distance_uart/__init__.py
@@ -1,3 +1,1 @@
-# This file is intentionally minimal. Its presence allows ESPHome to
-# recognize this folder as a component package. The primary logic is
-# handled in the platform-specific files (e.g., sensor.py).
+CODEOWNERS = ["@estockda"]
