e59fa44dc1cd250c8ff196cdcd0763552042198d
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-01-02 08:32:47 +1100

    Remove redundant declaration

--- esphome/components/bthome_mithermometer/bthome_ble.cpp
+++ esphome/components/bthome_mithermometer/bthome_ble.cpp
@@ -12,8 +12,6 @@ namespace bthome_mithermometer {
 
 static const char *const TAG = "bthome_mithermometer";
 
-static const int MAC_ADDRESS_SIZE = 6;
-
 static std::string format_mac_address(uint64_t address) {
   std::array<uint8_t, MAC_ADDRESS_SIZE> mac{};
   for (size_t i = 0; i < MAC_ADDRESS_SIZE; i++) {
