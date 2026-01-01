4b0530301abb7e79a182510629f2a60eadf63a5e
H. Árkosi Róbert <robreg@zsurob.hu>
2026-01-01 18:46:10 +0100

    Bring MAC constants into namespace scope

--- esphome/components/bthome_mithermometer/bthome_ble.cpp
+++ esphome/components/bthome_mithermometer/bthome_ble.cpp
@@ -10,15 +10,18 @@
 namespace esphome {
 namespace bthome_mithermometer {
 
+using esphome::MAC_ADDRESS_PRETTY_BUFFER_SIZE;
+using esphome::MAC_ADDRESS_SIZE;
+
 static const char *const TAG = "bthome_mithermometer";
 
 static std::string format_mac_address(uint64_t address) {
-  std::array<uint8_t, ::esphome::MAC_ADDRESS_SIZE> mac{};
-  for (size_t i = 0; i < ::esphome::MAC_ADDRESS_SIZE; i++) {
-    mac[i] = (address >> ((::esphome::MAC_ADDRESS_SIZE - 1 - i) * 8)) & 0xFF;
+  std::array<uint8_t, MAC_ADDRESS_SIZE> mac{};
+  for (size_t i = 0; i < MAC_ADDRESS_SIZE; i++) {
+    mac[i] = (address >> ((MAC_ADDRESS_SIZE - 1 - i) * 8)) & 0xFF;
   }
 
-  char buffer[::esphome::MAC_ADDRESS_PRETTY_BUFFER_SIZE];
+  char buffer[MAC_ADDRESS_PRETTY_BUFFER_SIZE];
   format_mac_addr_upper(mac.data(), buffer);
   return buffer;
 }
