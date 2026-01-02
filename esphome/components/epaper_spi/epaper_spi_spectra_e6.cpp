a090aea03af890471a0faf1fce02cee5a7bfd998
Stuart Parmenter <pavlov@pavlov.net>
2026-01-01 19:44:45 -0800

    linter really wants us to call base class

--- esphome/components/epaper_spi/epaper_spi_spectra_e6.cpp
+++ esphome/components/epaper_spi/epaper_spi_spectra_e6.cpp
@@ -99,7 +99,7 @@ void EPaperSpectraE6::deep_sleep() {
 void EPaperSpectraE6::fill(Color color) {
   // If clipping is active, fall back to base implementation
   if (this->get_clipping().is_set()) {
-    display::Display::fill(color);
+    EPaperBase::fill(color);
     return;
   }
 
