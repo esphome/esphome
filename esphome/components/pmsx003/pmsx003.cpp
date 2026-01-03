411ef051abaf67123d387ebbc3c3505378b85d73
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2026-01-03 22:09:01 +0100

    [pmsx003] add docu

--- esphome/components/pmsx003/pmsx003.cpp
+++ esphome/components/pmsx003/pmsx003.cpp
@@ -179,6 +179,7 @@ optional<bool> PMSX003Component::check_byte_() {
 }
 
 bool PMSX003Component::check_payload_length_(uint16_t payload_length) {
+  // https://avaldebe.github.io/PyPMS/sensors/Plantower/
   switch (this->type_) {
     case Type::PMS1003:
     case Type::PMS3003:
