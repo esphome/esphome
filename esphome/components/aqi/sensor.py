57348c9642e5fb9843ebf8c688cd519eab2f7191
jas <jas@asspa.in>
2026-01-03 20:22:53 -0800

    [aqi] Add standalone AQI sensor platform
    Add a new sensor platform under the existing `aqi` component that
    computes Air Quality Index from any PM2.5 and PM10 sensor sources.
    
    Supports both US EPA AQI (0-500 scale) and European CAQI (0-100+ scale).
    
    Example usage:
    
      sensor:
        - platform: aqi
          name: "Air Quality Index"
          pm_2_5: pm25_sensor_id
          pm_10_0: pm10_sensor_id
          calculation_type: AQI

---
+++ esphome/components/aqi/sensor.py
@@ -0,0 +1,48 @@
+import esphome.codegen as cg
+from esphome.components import sensor
+import esphome.config_validation as cv
+from esphome.const import DEVICE_CLASS_AQI, STATE_CLASS_MEASUREMENT
+
+from . import AQI_CALCULATION_TYPE, CONF_CALCULATION_TYPE, aqi_ns
+
+CODEOWNERS = ["@jasstrong"]
+DEPENDENCIES = ["sensor"]
+
+CONF_PM_2_5_SENSOR = "pm_2_5"
+CONF_PM_10_0_SENSOR = "pm_10_0"
+UNIT_INDEX = "index"
+
+AQISensor = aqi_ns.class_("AQISensor", sensor.Sensor, cg.Component)
+
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        AQISensor,
+        unit_of_measurement=UNIT_INDEX,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_AQI,
+        state_class=STATE_CLASS_MEASUREMENT,
+    )
+    .extend(
+        {
+            cv.Required(CONF_PM_2_5_SENSOR): cv.use_id(sensor.Sensor),
+            cv.Required(CONF_PM_10_0_SENSOR): cv.use_id(sensor.Sensor),
+            cv.Required(CONF_CALCULATION_TYPE): cv.enum(
+                AQI_CALCULATION_TYPE, upper=True
+            ),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = await sensor.new_sensor(config)
+    await cg.register_component(var, config)
+
+    pm_2_5_sensor = await cg.get_variable(config[CONF_PM_2_5_SENSOR])
+    cg.add(var.set_pm_2_5_sensor(pm_2_5_sensor))
+
+    pm_10_0_sensor = await cg.get_variable(config[CONF_PM_10_0_SENSOR])
+    cg.add(var.set_pm_10_0_sensor(pm_10_0_sensor))
+
+    cg.add(var.set_aqi_calculation_type(config[CONF_CALCULATION_TYPE]))
