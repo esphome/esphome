a94c40ddd03f6cc027cb9959267cb0b56d109394
J. Nick Koston <nick@home-assistant.io>
2026-01-03 14:03:24 -1000

    adjust

--- esphome/components/template/water_heater/__init__.py
+++ esphome/components/template/water_heater/__init__.py
@@ -80,8 +80,7 @@ async def to_code(config: ConfigType) -> None:
     if CONF_MODE in config:
         conf = config[CONF_MODE]
         if not isinstance(conf, cv.Lambda):
-            enum_value_str = str(conf).rsplit("::", maxsplit=1)[-1]
-            conf = cv.Lambda(f"return water_heater::{enum_value_str};")
+            conf = cv.Lambda(f"return {conf};")
 
         template_ = await cg.process_lambda(
             conf,
