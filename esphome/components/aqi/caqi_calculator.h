2c24d1926a34ef73bed76e08a66002bed4126104
jas <jas@asspa.in>
2026-01-04 17:05:06 -0800

    Fix static method naming and constant style
    - Remove trailing underscores from static methods (clang-tidy fix)
    - Rename AMOUNT_OF_LEVELS to NUM_LEVELS to match ESPHome conventions

--- esphome/components/aqi/caqi_calculator.h
+++ esphome/components/aqi/caqi_calculator.h
@@ -7,23 +7,23 @@ namespace esphome::aqi {
 class CAQICalculator : public AbstractAQICalculator {
  public:
   uint16_t get_aqi(uint16_t pm2_5_value, uint16_t pm10_0_value) override {
-    int pm2_5_index = calculate_index_(pm2_5_value, PM2_5_GRID);
-    int pm10_0_index = calculate_index_(pm10_0_value, PM10_0_GRID);
+    int pm2_5_index = calculate_index(pm2_5_value, PM2_5_GRID);
+    int pm10_0_index = calculate_index(pm10_0_value, PM10_0_GRID);
 
     return (pm2_5_index < pm10_0_index) ? pm10_0_index : pm2_5_index;
   }
 
  protected:
-  static constexpr int AMOUNT_OF_LEVELS = 5;
+  static constexpr int NUM_LEVELS = 5;
 
-  static constexpr int INDEX_GRID[AMOUNT_OF_LEVELS][2] = {{0, 25}, {26, 50}, {51, 75}, {76, 100}, {101, 400}};
+  static constexpr int INDEX_GRID[NUM_LEVELS][2] = {{0, 25}, {26, 50}, {51, 75}, {76, 100}, {101, 400}};
 
-  static constexpr int PM2_5_GRID[AMOUNT_OF_LEVELS][2] = {{0, 15}, {16, 30}, {31, 55}, {56, 110}, {111, 400}};
+  static constexpr int PM2_5_GRID[NUM_LEVELS][2] = {{0, 15}, {16, 30}, {31, 55}, {56, 110}, {111, 400}};
 
-  static constexpr int PM10_0_GRID[AMOUNT_OF_LEVELS][2] = {{0, 25}, {26, 50}, {51, 90}, {91, 180}, {181, 400}};
+  static constexpr int PM10_0_GRID[NUM_LEVELS][2] = {{0, 25}, {26, 50}, {51, 90}, {91, 180}, {181, 400}};
 
-  static int calculate_index_(uint16_t value, const int array[AMOUNT_OF_LEVELS][2]) {
-    int grid_index = get_grid_index_(value, array);
+  static int calculate_index(uint16_t value, const int array[NUM_LEVELS][2]) {
+    int grid_index = get_grid_index(value, array);
     if (grid_index == -1) {
       return -1;
     }
@@ -36,8 +36,8 @@ class CAQICalculator : public AbstractAQICalculator {
     return (value - conc_lo) * (aqi_hi - aqi_lo) / (conc_hi - conc_lo) + aqi_lo;
   }
 
-  static int get_grid_index_(uint16_t value, const int array[AMOUNT_OF_LEVELS][2]) {
-    for (int i = 0; i < AMOUNT_OF_LEVELS; i++) {
+  static int get_grid_index(uint16_t value, const int array[NUM_LEVELS][2]) {
+    for (int i = 0; i < NUM_LEVELS; i++) {
       if (value >= array[i][0] && value <= array[i][1]) {
         return i;
       }
