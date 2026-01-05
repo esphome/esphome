1b1052b215fa5761cdb340fd8c100908ee6dd4b6
jas <jas@asspa.in>
2026-01-04 16:47:20 -0800

    Revert "[pmsx003][hm3301] Remove embedded AQI sensor support"
    This reverts commit 9932dc6d7a4f98208c33501f87a8638c083e0bb9.

--- esphome/components/hm3301/hm3301.h
+++ esphome/components/hm3301/hm3301.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
+#include "esphome/components/aqi/aqi_calculator_factory.h"
 
 namespace esphome {
 namespace hm3301 {
@@ -16,6 +17,9 @@ class HM3301Component : public PollingComponent, public i2c::I2CDevice {
   void set_pm_1_0_sensor(sensor::Sensor *pm_1_0_sensor) { pm_1_0_sensor_ = pm_1_0_sensor; }
   void set_pm_2_5_sensor(sensor::Sensor *pm_2_5_sensor) { pm_2_5_sensor_ = pm_2_5_sensor; }
   void set_pm_10_0_sensor(sensor::Sensor *pm_10_0_sensor) { pm_10_0_sensor_ = pm_10_0_sensor; }
+  void set_aqi_sensor(sensor::Sensor *aqi_sensor) { aqi_sensor_ = aqi_sensor; }
+
+  void set_aqi_calculation_type(aqi::AQICalculatorType aqi_calc_type) { aqi_calc_type_ = aqi_calc_type; }
 
   void setup() override;
   void dump_config() override;
@@ -35,6 +39,10 @@ class HM3301Component : public PollingComponent, public i2c::I2CDevice {
   sensor::Sensor *pm_1_0_sensor_{nullptr};
   sensor::Sensor *pm_2_5_sensor_{nullptr};
   sensor::Sensor *pm_10_0_sensor_{nullptr};
+  sensor::Sensor *aqi_sensor_{nullptr};
+
+  aqi::AQICalculatorType aqi_calc_type_;
+  aqi::AQICalculatorFactory aqi_calculator_factory_ = aqi::AQICalculatorFactory();
 
   bool validate_checksum_(const uint8_t *data);
   uint16_t get_sensor_value_(const uint8_t *data, uint8_t i);
