4b2e31ccae583fa635b72111753515215551370d
J. Nick Koston <nick@home-assistant.io>
2026-01-06 22:29:32 -1000

    tweaks

--- esphome/core/helpers.h
+++ esphome/core/helpers.h
@@ -391,15 +391,6 @@ constexpr uint32_t FNV1_OFFSET_BASIS = 2166136261UL;
 /// FNV-1 32-bit prime
 constexpr uint32_t FNV1_PRIME = 16777619UL;
 
-/// Extend a FNV-1 hash with an integer (hashes each byte).
-template<std::integral T> constexpr uint32_t fnv1_hash_extend(uint32_t hash, T value) {
-  for (size_t i = 0; i < sizeof(T); i++) {
-    hash *= FNV1_PRIME;
-    hash ^= (value >> (i * 8)) & 0xFF;
-  }
-  return hash;
-}
-
 /// Extend a FNV-1a hash with additional string data.
 constexpr uint32_t fnv1a_hash_extend(uint32_t hash, const char *str) {
   if (str) {
@@ -415,8 +406,10 @@ inline uint32_t fnv1a_hash_extend(uint32_t hash, const std::string &str) {
 }
 /// Extend a FNV-1a hash with an integer (hashes each byte).
 template<std::integral T> constexpr uint32_t fnv1a_hash_extend(uint32_t hash, T value) {
+  using UnsignedT = std::make_unsigned_t<T>;
+  UnsignedT uvalue = static_cast<UnsignedT>(value);
   for (size_t i = 0; i < sizeof(T); i++) {
-    hash ^= (value >> (i * 8)) & 0xFF;
+    hash ^= (uvalue >> (i * 8)) & 0xFF;
     hash *= FNV1_PRIME;
   }
   return hash;
