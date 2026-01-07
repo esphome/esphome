d94cc8ab006fb0cab21ad725cfff596ed5002c62
Glen Robertson <robertson.glen@gmail.com>
2026-01-07 16:45:27 -0500

    Remove is_idle virtual

--- esphome/components/epaper_spi/epaper_spi.h
+++ esphome/components/epaper_spi/epaper_spi.h
@@ -106,7 +106,7 @@ class EPaperBase : public Display,
   void process_state_();
 
   const char *epaper_state_to_string_();
-  virtual bool is_idle() const;
+  bool is_idle_() const;
   void setup_pins_() const;
   virtual bool reset();
   void initialise_();
