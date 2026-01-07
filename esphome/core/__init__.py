a51656a574bebcde30d8262cf19cd898e5f1491f
Mariusz Kryński <mrk@sed.pl>
2026-01-08 00:30:23 +0100

    add CORE.is_stm32

--- esphome/core/__init__.py
+++ esphome/core/__init__.py
@@ -27,6 +27,7 @@ from esphome.const import (
     PLATFORM_NRF52,
     PLATFORM_RP2040,
     PLATFORM_RTL87XX,
+    PLATFORM_STM32,
 )
 
 # pylint: disable=unused-import
@@ -790,6 +791,10 @@ class EsphomeCore:
     @property
     def is_rtl87xx(self):
         return self.target_platform == PLATFORM_RTL87XX
+
+    @property
+    def is_stm32(self):
+        return self.target_platform == PLATFORM_STM32
 
     @property
     def is_ln882x(self):
