b8da3b32656fe8b7620ea68134550b6ac76f43ea
J. Nick Koston <nick@home-assistant.io>
2026-01-07 20:00:40 -1000

    [web_server] Use centralized length constants for buffer sizing

--- esphome/core/entity_base.h
+++ esphome/core/entity_base.h
@@ -16,7 +16,8 @@ namespace esphome {
 // Maximum device name length - keep in sync with validate_hostname() in esphome/core/config.py
 static constexpr size_t ESPHOME_DEVICE_NAME_MAX_LEN = 31;
 
-// Maximum friendly name length - keep in sync with friendly_name cv.Length(max=120) in esphome/core/config.py
+// Maximum friendly name length for entities and sub-devices - keep in sync with cv.Length(max=120) in
+// esphome/core/config.py
 static constexpr size_t ESPHOME_FRIENDLY_NAME_MAX_LEN = 120;
 
 // Maximum domain length (longest: "alarm_control_panel" = 19)
