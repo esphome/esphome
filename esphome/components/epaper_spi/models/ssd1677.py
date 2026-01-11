d95ef0212e44fb80e312db72847562f81d8391b4
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-01-11 19:14:48 +1100

    Add waveshare 4.26 as specific panel

--- esphome/components/epaper_spi/models/ssd1677.py
+++ esphome/components/epaper_spi/models/ssd1677.py
@@ -22,11 +22,15 @@ class SSD1677(EpaperModel):
 
 ssd1677 = SSD1677("ssd1677")
 
-ssd1677.extend(
-    "seeed-ee04-mono-4.26",
+wave_4_26 = ssd1677.extend(
+    "waveshare-4.26in",
     width=800,
     height=480,
     mirror_x=True,
+)
+
+wave_4_26.extend(
+    "seeed-ee04-mono-4.26",
     cs_pin=44,
     dc_pin=10,
     reset_pin=38,
