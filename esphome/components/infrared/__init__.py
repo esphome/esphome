e7cc27d0373b37d1b44cc448094be9e5ef9cc7b8
Keith Burzinski <kbx81x@gmail.com>
2026-01-10 21:42:55 -0600

    Remove dependency on `api`
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- esphome/components/infrared/__init__.py
+++ esphome/components/infrared/__init__.py
@@ -5,7 +5,7 @@ from esphome.core import CORE
 from esphome.core.entity_helpers import setup_entity
 
 CODEOWNERS = ["@kbx81"]
-DEPENDENCIES = ["api", "remote_base"]
+DEPENDENCIES = ["remote_base"]
 
 IS_PLATFORM_COMPONENT = True
 
