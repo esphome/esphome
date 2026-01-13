11c9922d2e1294063bf19ef065bff14534967d25
J. Nick Koston <nick@home-assistant.io>
2026-01-12 17:27:46 -1000

    [ci] Add format_hex_pretty to heap-allocating helper lint check

--- script/ci-custom.py
+++ script/ci-custom.py
@@ -683,6 +683,7 @@ def lint_trailing_whitespace(fname, match):
 # These return std::string and should be replaced with stack-based alternatives.
 HEAP_ALLOCATING_HELPERS = {
     "format_hex": "format_hex_to() with a stack buffer",
+    "format_hex_pretty": "format_hex_pretty_to() with a stack buffer",
     "format_mac_address_pretty": "format_mac_addr_upper() with a stack buffer",
     "get_mac_address": "get_mac_address_into_buffer() with a stack buffer",
     "get_mac_address_pretty": "get_mac_address_pretty_into_buffer() with a stack buffer",
@@ -698,6 +699,7 @@ HEAP_ALLOCATING_HELPERS = {
     # get_mac_address(?!_) ensures we don't match get_mac_address_into_buffer, etc.
     r"[^\w]("
     r"format_hex(?!_)|"
+    r"format_hex_pretty(?!_)|"
     r"format_mac_address_pretty|"
     r"get_mac_address_pretty(?!_)|"
     r"get_mac_address(?!_)|"
