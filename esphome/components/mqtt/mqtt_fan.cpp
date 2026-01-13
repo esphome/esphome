f563e075fcfd2659f5b61c34026222c6c4e37338
J. Nick Koston <nick@home-assistant.io>
2026-01-12 16:30:55 -1000

    address pendantic bot

--- esphome/components/mqtt/mqtt_fan.cpp
+++ esphome/components/mqtt/mqtt_fan.cpp
@@ -174,7 +174,7 @@ bool MQTTFanComponent::publish_state() {
   }
   auto traits = this->state_->get_traits();
   if (traits.supports_speed()) {
-    char buf[4];
+    char buf[12];
     int len = snprintf(buf, sizeof(buf), "%d", this->state_->speed);
     bool success = this->publish(this->get_speed_level_state_topic(), buf, len);
     failed = failed || !success;
