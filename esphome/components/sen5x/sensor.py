42bb3656335712cc24338d5ffdca41ecace21c74
Big Mike <mike@bigmike.land>
2026-01-16 14:40:22 -0600

    Revert dev only building

--- esphome/components/sen5x/sensor.py
+++ esphome/components/sen5x/sensor.py
@@ -6,34 +6,34 @@ import esphome.codegen as cg
 from esphome.components import i2c, sensirion_common, sensor
 import esphome.config_validation as cv
 from esphome.const import (
-    CONF_ALGORITHM_TUNING,
+    # CONF_ALGORITHM_TUNING,
     CONF_ALTITUDE_COMPENSATION,
     CONF_AMBIENT_PRESSURE_COMPENSATION_SOURCE,
     CONF_AUTOMATIC_SELF_CALIBRATION,
     CONF_CO2,
     CONF_GAIN_FACTOR,
-    CONF_GATING_MAX_DURATION_MINUTES,
+    # CONF_GATING_MAX_DURATION_MINUTES,
     CONF_HUMIDITY,
     CONF_ID,
-    CONF_INDEX_OFFSET,
-    CONF_LEARNING_TIME_GAIN_HOURS,
-    CONF_LEARNING_TIME_OFFSET_HOURS,
+    # CONF_INDEX_OFFSET,
+    # CONF_LEARNING_TIME_GAIN_HOURS,
+    # CONF_LEARNING_TIME_OFFSET_HOURS,
     CONF_MODEL,
-    CONF_NORMALIZED_OFFSET_SLOPE,
-    CONF_NOX,
+    # CONF_NORMALIZED_OFFSET_SLOPE,
+    # CONF_NOX,
     CONF_OFFSET,
     CONF_PM_1_0,
     CONF_PM_2_5,
     CONF_PM_4_0,
     CONF_PM_10_0,
-    CONF_STD_INITIAL,
+    # CONF_STD_INITIAL,
     CONF_STORE_BASELINE,
     CONF_TEMPERATURE,
     CONF_TEMPERATURE_COMPENSATION,
-    CONF_TIME_CONSTANT,
+    # CONF_TIME_CONSTANT,
     CONF_TYPE,
     CONF_VALUE,
-    CONF_VOC,
+    # CONF_VOC,
     DEVICE_CLASS_AQI,
     DEVICE_CLASS_CARBON_DIOXIDE,
     DEVICE_CLASS_HUMIDITY,
@@ -54,16 +54,16 @@ from esphome.const import (
     UNIT_PERCENT,
 )
 
-# CONF_ALGORITHM_TUNING = "algorithm_tuning"
-# CONF_GATING_MAX_DURATION_MINUTES = "gating_max_duration_minutes"
-# CONF_INDEX_OFFSET = "index_offset"
-# CONF_LEARNING_TIME_GAIN_HOURS = "learning_time_gain_hours"
-# CONF_LEARNING_TIME_OFFSET_HOURS = "learning_time_offset_hours"
-# CONF_NORMALIZED_OFFSET_SLOPE = "normalized_offset_slope"
-# CONF_NOX = "nox"
-# CONF_STD_INITIAL = "std_initial"
-# CONF_TIME_CONSTANT = "time_constant"
-# CONF_VOC = "voc"
+CONF_ALGORITHM_TUNING = "algorithm_tuning"
+CONF_GATING_MAX_DURATION_MINUTES = "gating_max_duration_minutes"
+CONF_INDEX_OFFSET = "index_offset"
+CONF_LEARNING_TIME_GAIN_HOURS = "learning_time_gain_hours"
+CONF_LEARNING_TIME_OFFSET_HOURS = "learning_time_offset_hours"
+CONF_NORMALIZED_OFFSET_SLOPE = "normalized_offset_slope"
+CONF_NOX = "nox"
+CONF_STD_INITIAL = "std_initial"
+CONF_TIME_CONSTANT = "time_constant"
+CONF_VOC = "voc"
 
 CODEOWNERS = ["@martgras", "@mikelawrence"]
 DEPENDENCIES = ["i2c"]
