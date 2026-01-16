6d0908ea94cce74c4e802a1d0f335665bb8e7f10
Tomasz Duda <tomaszduda23@gmail.com>
2026-01-16 18:57:57 +0100

    fix

--- esphome/components/zephyr_coredump/__init__.py
+++ esphome/components/zephyr_coredump/__init__.py
@@ -23,8 +23,9 @@ async def to_code(config):
     zephyr_add_prj_conf("DEBUG_COREDUMP", True)
     zephyr_add_prj_conf("DEBUG_COREDUMP_BACKEND_FLASH_PARTITION", True)
     zephyr_add_prj_conf("DEBUG_COREDUMP_MEMORY_DUMP_THREADS", True)
+    cg.add_build_flag("-Wl,--wrap=z_arm_fatal_error")
+    cg.add_build_flag("-Wl,--wrap=z_arm_fault")
     # zephyr_add_prj_conf("EXTRA_EXCEPTION_INFO", True)
-    # https://github.com/zephyrproject-rtos/zephyr/pull/79622
     zephyr_add_pm_static(
         [Section("coredump_partition", 0xE4000, 0x10000, "flash_primary")]
     )
--- esphome/components/zephyr_coredump/coredump.cpp
+++ esphome/components/zephyr_coredump/coredump.cpp
@@ -118,6 +118,50 @@ void print_coredump() {
     print_stored_dump();
   }
 }
-
 }  // namespace esphome::zephyr_coredump
+
+// https://github.com/zephyrproject-rtos/zephyr/pull/79622
+extern "C" {
+
+#define EXC_RETURN_STACK_FRAME_TYPE_Pos 4
+#define EXC_RETURN_STACK_FRAME_TYPE_Msk BIT(EXC_RETURN_STACK_FRAME_TYPE_Pos)
+#define EXC_RETURN_STACK_FRAME_TYPE_EXTENDED 0
+#define EXC_RETURN_STACK_FRAME_TYPE_STANDARD EXC_RETURN_STACK_FRAME_TYPE_Msk
+
+uint32_t global_exc_return = 0;
+void __real_z_arm_fault(uint32_t msp, uint32_t psp, uint32_t exc_return, _callee_saved_t *callee_regs);
+void __wrap_z_arm_fault(uint32_t msp, uint32_t psp, uint32_t exc_return, _callee_saved_t *callee_regs) {
+  global_exc_return = exc_return;
+  __real_z_arm_fault(msp, psp, exc_return, callee_regs);
+}
+
+void __real_z_arm_fatal_error(unsigned int reason, const struct arch_esf *esf);
+void __wrap_z_arm_fatal_error(unsigned int reason, const struct arch_esf *esf) {
+#if defined(CONFIG_ARMV7_M_ARMV8_M_MAINLINE) || defined(CONFIG_ARMV6_M_ARMV8_M_BASELINE)
+  /* Gdb expects a stack pointer that does not include the exception stack frame in order to
+   * unwind. So adjust the stack pointer accordingly.
+   */
+  z_arm_coredump_fault_sp += sizeof(esf->basic);
+
+#if defined(CONFIG_FPU) && defined(CONFIG_FPU_SHARING)
+  /* Assess whether thread had been using the FP registers and add size of additional
+   * registers if necessary
+   */
+  if ((global_exc_return & EXC_RETURN_STACK_FRAME_TYPE_STANDARD) == EXC_RETURN_STACK_FRAME_TYPE_EXTENDED) {
+    z_arm_coredump_fault_sp += sizeof(esf->fpu);
+  }
+#endif /* CONFIG_FPU && CONFIG_FPU_SHARING */
+
+#ifndef CONFIG_ARMV8_M_MAINLINE
+  if ((esf->basic.xpsr & SCB_CCR_STKALIGN_Msk) == SCB_CCR_STKALIGN_Msk) {
+    /* Adjust stack alignment after PSR bit[9] detected */
+    z_arm_coredump_fault_sp |= 0x4;
+  }
+#endif /* !CONFIG_ARMV8_M_MAINLINE */
+
+#endif /* CONFIG_ARMV7_M_ARMV8_M_MAINLINE || CONFIG_ARMV6_M_ARMV8_M_BASELINE */
+  __real_z_arm_fatal_error(reason, esf);
+}
+}
+
 #endif
