917c40dce064c8ed289d1fec05eef7c2422d6d4c
Tomasz Duda <tomaszduda23@gmail.com>
2026-01-17 12:18:23 +0100

    fix

--- esphome/components/zephyr_coredump/coredump.cpp
+++ esphome/components/zephyr_coredump/coredump.cpp
@@ -118,10 +118,8 @@ void print_coredump() {
 // https://github.com/zephyrproject-rtos/zephyr/pull/79622
 extern "C" {
 
-#define EXC_RETURN_STACK_FRAME_TYPE_Pos 4
-#define EXC_RETURN_STACK_FRAME_TYPE_Msk BIT(EXC_RETURN_STACK_FRAME_TYPE_Pos)
-#define EXC_RETURN_STACK_FRAME_TYPE_EXTENDED 0
-#define EXC_RETURN_STACK_FRAME_TYPE_STANDARD EXC_RETURN_STACK_FRAME_TYPE_Msk
+static const uint8_t EXC_RETURN_STACK_FRAME_TYPE_Pos = 4;
+static const uint8_t EXC_RETURN_STACK_FRAME_TYPE_EXTENDED = 0;
 
 uint32_t global_exc_return = 0;
 extern void __real_z_arm_fault(uint32_t msp, uint32_t psp, uint32_t exc_return, _callee_saved_t *callee_regs);
@@ -147,7 +145,7 @@ void __wrap_z_arm_fatal_error(unsigned int reason, const z_arch_esf_t *esf)
   /* Assess whether thread had been using the FP registers and add size of additional
    * registers if necessary
    */
-  if ((global_exc_return & EXC_RETURN_STACK_FRAME_TYPE_STANDARD) == EXC_RETURN_STACK_FRAME_TYPE_EXTENDED) {
+  if ((global_exc_return & BIT(EXC_RETURN_STACK_FRAME_TYPE_Pos)) == EXC_RETURN_STACK_FRAME_TYPE_EXTENDED) {
     z_arm_coredump_fault_sp += sizeof(esf->fpu);
   }
 #endif /* CONFIG_FPU && CONFIG_FPU_SHARING */
