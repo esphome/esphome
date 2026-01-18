c0d7c9d325864fff31326eee928242d9ae39e851
rawsludge <rawsludge@hotmail.com>
2026-01-18 13:00:52 +0300

    FIX missing function error

--- esphome/components/fendt_caravan/sensor/caravan_device.cpp
+++ esphome/components/fendt_caravan/sensor/caravan_device.cpp
@@ -10,13 +10,13 @@ void CaravanDevice::decode(const std::string &name, const std::string &value) {
     variable->decode(value);
 };
 void CaravanDevice::update() {
-  ESP_LOGD(this->get_tag(), "Update called");
+  ESP_LOGD(TAG, "Update called");
   if (!this->log_variables_)
     return;
-  ESP_LOGI(this->get_tag(), "Variable Count :%d", this->variables_.size());
+  ESP_LOGI(TAG, "Variable Count :%d", this->variables_.size());
   for (auto *var : this->variables_) {
     if (var->is_active()) {
-      ESP_LOGI(this->get_tag(), "Variable: %s, raw value: %s", var->get_name().c_str(), var->get_raw_value().c_str());
+      ESP_LOGI(TAG, "Variable: %s, raw value: %s", var->get_name().c_str(), var->get_raw_value().c_str());
     }
   }
   this->log_variables_ = false;
