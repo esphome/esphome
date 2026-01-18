48dc1331a43054f17cec2dbabfe9f3364538db46
J. Nick Koston <nick@home-assistant.io>
2026-01-17 14:16:27 -1000

    [pipsolar] Store command strings in flash

--- esphome/components/pipsolar/switch/pipsolar_switch.cpp
+++ esphome/components/pipsolar/switch/pipsolar_switch.cpp
@@ -10,11 +10,11 @@ static const char *const TAG = "pipsolar.switch";
 void PipsolarSwitch::dump_config() { LOG_SWITCH("", "Pipsolar Switch", this); }
 void PipsolarSwitch::write_state(bool state) {
   if (state) {
-    if (!this->on_command_.empty()) {
+    if (this->on_command_ != nullptr) {
       this->parent_->queue_command(this->on_command_);
     }
   } else {
-    if (!this->off_command_.empty()) {
+    if (this->off_command_ != nullptr) {
       this->parent_->queue_command(this->off_command_);
     }
   }
