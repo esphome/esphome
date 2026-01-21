f0c7306ad517761219889a98c3f1b83db688590a
J. Nick Koston <nick@home-assistant.io>
2026-01-20 21:04:52 -1000

    log scan complete

--- esphome/components/wifi/wifi_component.cpp
+++ esphome/components/wifi/wifi_component.cpp
@@ -424,7 +424,7 @@ bool WiFiComponent::matches_configured_network_(const char *ssid, const uint8_t 
   return false;
 }
 
-void WiFiComponent::log_discarded_scan_result_(const char *ssid, const uint8_t *bssid, int8_t rssi, uint8_t channel) {
+void WiFiComponent::log_discarded_scan_result(const char *ssid, const uint8_t *bssid, int8_t rssi, uint8_t channel) {
 #if ESPHOME_LOG_LEVEL >= ESPHOME_LOG_LEVEL_VERBOSE
   char bssid_s[MAC_ADDRESS_PRETTY_BUFFER_SIZE];
   format_mac_addr_upper(bssid, bssid_s);
