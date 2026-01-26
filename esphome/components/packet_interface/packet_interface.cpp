4923ff5a0168adea25ea3b6beba4b6918d4ba290
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-01-27 10:43:06 +1100

    namespace syntax

--- esphome/components/packet_interface/packet_interface.cpp
+++ esphome/components/packet_interface/packet_interface.cpp
@@ -1,11 +1,9 @@
 #include "packet_interface.h"
 
-namespace esphome {
-namespace packet_interface {
+namespace esphome::packet_interface {
 void PacketInterface::on_receive_from_interface_(const std::vector<uint8_t> &data, PacketMetaData meta_data) {
   char logbuf[80];
   ESP_LOGV(TAG, "Received data %s", format_hex_pretty_to(logbuf, data));
   this->callback_.call(data, meta_data);
 }
-}  // namespace packet_interface
-}  // namespace esphome
+}  // namespace esphome::packet_interface
