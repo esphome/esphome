9d25e385d93994f8185c0808a8adbee5f29020e2
J. Nick Koston <nick@home-assistant.io>
2026-01-27 23:54:08 -1000

    [runtime_stats] Eliminate heap churn by using stack-allocated buffer for sorting

--- esphome/components/runtime_stats/runtime_stats.h
+++ esphome/components/runtime_stats/runtime_stats.h
@@ -77,17 +77,6 @@ class ComponentRuntimeStats {
   uint32_t total_max_time_ms_;
 };
 
-// For sorting components by run time
-struct ComponentStatPair {
-  Component *component;
-  const ComponentRuntimeStats *stats;
-
-  bool operator>(const ComponentStatPair &other) const {
-    // Sort by period time as that's what we're displaying in the logs
-    return stats->get_period_time_ms() > other.stats->get_period_time_ms();
-  }
-};
-
 class RuntimeStatsCollector {
  public:
   RuntimeStatsCollector();
