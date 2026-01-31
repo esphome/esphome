1b6ae4348b389bb14515ebcef3b2d8426d36b085
J. Nick Koston <nick@home-assistant.io>
2026-01-30 21:22:04 -0600

    tweaks

--- esphome/core/progmem.h
+++ esphome/core/progmem.h
@@ -55,7 +55,7 @@ template<size_t N> struct FixedString {
 /// Example:
 ///   PROGMEM_STRING_TABLE(MyStrings, "foo", "bar", "baz");
 ///   ProgmemStr str = MyStrings::get_progmem_str(index);  // For ArduinoJson
-///   const LogString *log_str = MyStrings::get_log_str(index);  // For ESP_LOG*
+///   const LogString *log_str = MyStrings::get_log_str(index);  // For logging
 ///
 template<FixedString... Strs> struct ProgmemStringTable {
   static constexpr size_t COUNT = sizeof...(Strs);
