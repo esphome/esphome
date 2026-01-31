c69e6e43639634d37817304949067c78e96eb75f
J. Nick Koston <nick@home-assistant.io>
2026-01-30 22:18:20 -0600

    tweak

--- esphome/components/logger/logger.cpp
+++ esphome/components/logger/logger.cpp
@@ -296,7 +296,7 @@ float Logger::get_setup_priority() const { return setup_priority::BUS + 500.0f; 
 PROGMEM_STRING_TABLE(LogLevelStrings, "NONE", "ERROR", "WARN", "INFO", "CONFIG", "DEBUG", "VERBOSE", "VERY_VERBOSE");
 constexpr uint8_t LOG_LEVEL_LAST = ESPHOME_LOG_LEVEL_VERY_VERBOSE;
 
-static const LogString *get_log_level_str_(uint8_t level) {
+static const LogString *get_log_level_str(uint8_t level) {
   if (level > LOG_LEVEL_LAST)
     level = LOG_LEVEL_LAST;
   return LogLevelStrings::get_log_str(level);
@@ -307,8 +307,8 @@ void Logger::dump_config() {
                 "Logger:\n"
                 "  Max Level: %s\n"
                 "  Initial Level: %s",
-                LOG_STR_ARG(get_log_level_str_(ESPHOME_LOG_LEVEL)),
-                LOG_STR_ARG(get_log_level_str_(this->current_level_)));
+                LOG_STR_ARG(get_log_level_str(ESPHOME_LOG_LEVEL)),
+                LOG_STR_ARG(get_log_level_str(this->current_level_)));
 #ifndef USE_HOST
   ESP_LOGCONFIG(TAG,
                 "  Log Baud Rate: %" PRIu32 "\n"
@@ -327,7 +327,7 @@ void Logger::dump_config() {
 
 #ifdef USE_LOGGER_RUNTIME_TAG_LEVELS
   for (auto &it : this->log_levels_) {
-    ESP_LOGCONFIG(TAG, "  Level for '%s': %s", it.first, LOG_STR_ARG(get_log_level_str_(it.second)));
+    ESP_LOGCONFIG(TAG, "  Level for '%s': %s", it.first, LOG_STR_ARG(get_log_level_str(it.second)));
   }
 #endif
 }
@@ -336,7 +336,7 @@ void Logger::set_log_level(uint8_t level) {
   if (level > ESPHOME_LOG_LEVEL) {
     level = ESPHOME_LOG_LEVEL;
     ESP_LOGW(TAG, "Cannot set log level higher than pre-compiled %s",
-             LOG_STR_ARG(get_log_level_str_(ESPHOME_LOG_LEVEL)));
+             LOG_STR_ARG(get_log_level_str(ESPHOME_LOG_LEVEL)));
   }
   this->current_level_ = level;
 #ifdef USE_LOGGER_LEVEL_LISTENERS
