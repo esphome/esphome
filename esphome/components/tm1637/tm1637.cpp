59f7c6e2ceaacb4b0a12aa203ebb5db7faed5697
Tobias Stanzel <tobi.stanzel@gmail.com>
2026-02-01 21:30:23 +0100

    Add buffer manipulation methods to TM1637Display class

--- esphome/components/tm1637/tm1637.cpp
+++ esphome/components/tm1637/tm1637.cpp
@@ -348,6 +348,16 @@ uint8_t TM1637Display::print(uint8_t start_pos, const char *str) {
   return pos - start_pos;
 }
 uint8_t TM1637Display::print(const char *str) { return this->print(0, str); }
+
+void TM1637Display::set_buffer(u_int8_t b0, u_int8_t b1, u_int8_t b2, u_int8_t b3, u_int8_t b4, u_int8_t b5) {
+  this->buffer_[0] = b0;
+  this->buffer_[1] = b1;
+  this->buffer_[2] = b2;
+  this->buffer_[3] = b3;
+  this->buffer_[4] = b4;
+  this->buffer_[5] = b5;
+}
+
 uint8_t TM1637Display::printf(uint8_t pos, const char *format, ...) {
   va_list arg;
   va_start(arg, format);
--- esphome/components/tm1637/tm1637.h
+++ esphome/components/tm1637/tm1637.h
@@ -47,6 +47,11 @@ class TM1637Display : public PollingComponent {
   /// Print `str` at position 0.
   uint8_t print(const char *str);
 
+  /// Set first 4 raw buffer bytes (remaining bytes are cleared).
+  void set_buffer(u_int8_t b0, u_int8_t b1, u_int8_t b2, u_int8_t b3);
+  /// Set all 6 raw buffer bytes.
+  void set_buffer(u_int8_t b0, u_int8_t b1, u_int8_t b2, u_int8_t b3, u_int8_t b4, u_int8_t b5);
+
   void set_intensity(uint8_t intensity) { this->intensity_ = intensity; }
   void set_inverted(bool inverted) { this->inverted_ = inverted; }
   void set_length(uint8_t length) { this->length_ = length; }
