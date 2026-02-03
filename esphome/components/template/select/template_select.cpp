5a2774876ab32572e335accfdcde02189885829d
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-02-03 13:14:09 +1100

    Use templates to customise classes

--- esphome/components/template/select/template_select.cpp
+++ esphome/components/template/select/template_select.cpp
@@ -3,66 +3,17 @@
 
 namespace esphome::template_ {
 
-static const char *const TAG = "template.select";
-
-void TemplateSelect::setup() {
-  if (this->f_.has_value())
-    return;
-
-  size_t index = this->initial_option_index_;
-  if (this->restore_value_) {
-    this->pref_ = this->make_entity_preference<size_t>();
-    size_t restored_index;
-    if (this->pref_.load(&restored_index) && this->has_index(restored_index)) {
-      index = restored_index;
-      ESP_LOGD(TAG, "State from restore: %s", this->option_at(index));
-    } else {
-      ESP_LOGD(TAG, "State from initial (could not load or invalid stored index): %s", this->option_at(index));
-    }
+void dump_config_helper(BaseTemplateSelect *this_, bool optimistic, bool has_lambda, const size_t initial_option_index,
+                        bool restore_value) {
+  LOG_SELECT("", "Template Select", this_);
+  if (has_lambda) {
+    LOG_UPDATE_INTERVAL((PollingComponent *) this_);
   } else {
-    ESP_LOGD(TAG, "State from initial: %s", this->option_at(index));
-  }
-
-  this->publish_state(index);
-}
-
-void TemplateSelect::update() {
-  if (!this->f_.has_value())
-    return;
-
-  auto val = this->f_();
-  if (val.has_value()) {
-    if (!this->has_option(*val)) {
-      ESP_LOGE(TAG, "Lambda returned an invalid option: %s", (*val).c_str());
-      return;
-    }
-    this->publish_state(*val);
+    ESP_LOGCONFIG(TAG,
+                  "  Optimistic: %s\n"
+                  "  Initial Option: %s\n"
+                  "  Restore Value: %s",
+                  YESNO(optimistic), this_->option_at(initial_option_index), YESNO(restore_value));
   }
 }
-
-void TemplateSelect::control(size_t index) {
-  if (this->optimistic_)
-    this->publish_state(index);
-
-  if (this->restore_value_)
-    this->pref_.save(&index);
-}
-
-void TemplateSelectWithSetAction::control(size_t index) {
-  this->set_trigger_.trigger(StringRef(this->option_at(index)));
-  TemplateSelect::control(index);
-}
-
-void TemplateSelect::dump_config() {
-  LOG_SELECT("", "Template Select", this);
-  LOG_UPDATE_INTERVAL(this);
-  if (this->f_.has_value())
-    return;
-  ESP_LOGCONFIG(TAG,
-                "  Optimistic: %s\n"
-                "  Initial Option: %s\n"
-                "  Restore Value: %s",
-                YESNO(this->optimistic_), this->option_at(this->initial_option_index_), YESNO(this->restore_value_));
-}
-
 }  // namespace esphome::template_
--- esphome/components/template/select/template_select.h
+++ esphome/components/template/select/template_select.h
@@ -8,38 +8,82 @@
 #include "esphome/core/template_lambda.h"
 
 namespace esphome::template_ {
+static const char *const TAG = "template.select";
+struct Empty {};
+class BaseTemplateSelect : public select::Select, public PollingComponent {};
+
+void dump_config_helper(BaseTemplateSelect *this_, bool optimistic, bool has_lambda, size_t initial_option_index,
+                        bool restore_value);
 
 /// Base template select class - used when no set_action is configured
-class TemplateSelect : public select::Select, public PollingComponent {
- public:
-  template<typename F> void set_template(F &&f) { this->f_.set(std::forward<F>(f)); }
 
-  void setup() override;
-  void update() override;
-  void dump_config() override;
+template<bool HAS_LAMBDA, bool OPTIMISTIC, bool RESTORE_VALUE, size_t INITIAL_OPTION_INDEX>
+class TemplateSelect : public BaseTemplateSelect {
+ public:
+  template<typename F> void set_lambda(F &&f) {
+    if constexpr (HAS_LAMBDA) {
+      this->f_.set(std::forward<F>(f));
+    }
+  }
+
+  void setup() override {
+    if constexpr (!HAS_LAMBDA) {
+      size_t index = INITIAL_OPTION_INDEX;
+      if constexpr (RESTORE_VALUE) {
+        this->pref_ = this->template make_entity_preference<size_t>();
+        if (this->pref_.load(&index) && this->has_index(index)) {
+          esph_log_d(TAG, "State from restore: %s", this->option_at(index));
+        } else {
+          index = INITIAL_OPTION_INDEX;
+          esph_log_d(TAG, "State from initial (no valid stored index): %s", this->option_at(INITIAL_OPTION_INDEX));
+        }
+      } else {
+        esph_log_d(TAG, "State from initial: %s", this->option_at(INITIAL_OPTION_INDEX));
+      }
+      this->publish_state(index);
+    }
+  }
+
+  void update() override {
+    if constexpr (HAS_LAMBDA) {
+      auto val = this->f_();
+      if (val.has_value()) {
+        if (!this->has_option(*val)) {
+          esph_log_e(TAG, "Lambda returned an invalid option: %s", (*val).c_str());
+          return;
+        }
+        this->publish_state(*val);
+      }
+    }
+  }
+  void dump_config() override {
+    dump_config_helper(this, OPTIMISTIC, HAS_LAMBDA, INITIAL_OPTION_INDEX, RESTORE_VALUE);
+  };
   float get_setup_priority() const override { return setup_priority::HARDWARE; }
 
-  void set_optimistic(bool optimistic) { this->optimistic_ = optimistic; }
-  void set_initial_option_index(size_t initial_option_index) { this->initial_option_index_ = initial_option_index; }
-  void set_restore_value(bool restore_value) { this->restore_value_ = restore_value; }
-
  protected:
-  void control(size_t index) override;
-  bool optimistic_ = false;
-  size_t initial_option_index_{0};
-  bool restore_value_ = false;
-  TemplateLambda<std::string> f_;
-
-  ESPPreferenceObject pref_;
+  void control(size_t index) override {
+    if constexpr (OPTIMISTIC)
+      this->publish_state(index);
+    if constexpr (RESTORE_VALUE)
+      this->pref_.save(&index);
+  }
+  [[no_unique_address]] std::conditional_t<HAS_LAMBDA, TemplateLambda<std::string>, Empty> f_{};
+  [[no_unique_address]] std::conditional_t<RESTORE_VALUE, ESPPreferenceObject, Empty> pref_{};
 };
 
 /// Template select with set_action trigger - only instantiated when set_action is configured
-class TemplateSelectWithSetAction final : public TemplateSelect {
+template<bool HAS_LAMBDA, bool OPTIMISTIC, bool RESTORE_VALUE, size_t INITIAL_OPTION_INDEX>
+class TemplateSelectWithSetAction final
+    : public TemplateSelect<HAS_LAMBDA, OPTIMISTIC, RESTORE_VALUE, INITIAL_OPTION_INDEX> {
  public:
   Trigger<StringRef> *get_set_trigger() { return &this->set_trigger_; }
 
  protected:
-  void control(size_t index) override;
+  void control(size_t index) override {
+    this->set_trigger_.trigger(StringRef(this->option_at(index)));
+    TemplateSelect<HAS_LAMBDA, OPTIMISTIC, RESTORE_VALUE, INITIAL_OPTION_INDEX>::control(index);
+  }
   Trigger<StringRef> set_trigger_;
 };
 
