857c8be1c90bae40613134a3649e4fa69de79b64
J. Nick Koston <nick@home-assistant.io>
2026-02-05 14:32:05 +0100

    Revert "tidy"
    This reverts commit 9d43a9326e37364e925e3cfb088dd14ffc444a82.

--- esphome/components/api/api_connection.cpp
+++ esphome/components/api/api_connection.cpp
@@ -1035,7 +1035,7 @@ void APIConnection::try_send_camera_image_() {
     msg.device_id = camera::Camera::instance()->get_device_id();
 #endif
 
-    if (!this->send_message(msg, CameraImageResponse::MESSAGE_TYPE)) {
+    if (!this->send_message_(msg, CameraImageResponse::MESSAGE_TYPE)) {
       return;  // Send failed, try again later
     }
     this->image_reader_->consume_data(to_send);
@@ -1443,7 +1443,7 @@ bool APIConnection::try_send_log_message(int level, const char *tag, const char 
   SubscribeLogsResponse msg;
   msg.level = static_cast<enums::LogLevel>(level);
   msg.set_message(reinterpret_cast<const uint8_t *>(line), message_len);
-  return this->send_message(msg, SubscribeLogsResponse::MESSAGE_TYPE);
+  return this->send_message_(msg, SubscribeLogsResponse::MESSAGE_TYPE);
 }
 
 void APIConnection::complete_authentication_() {
@@ -1768,11 +1768,7 @@ bool APIConnection::try_to_clear_buffer(bool log_out_of_space) {
   }
   return false;
 }
-bool APIConnection::send_message(const ProtoMessage &msg, uint8_t message_type) {
-#ifdef HAS_PROTO_MESSAGE_DUMP
-  DumpBuffer dump_buf;
-  this->log_send_message_(msg.message_name(), msg.dump_to(dump_buf));
-#endif
+bool APIConnection::send_message_(const ProtoMessage &msg, uint8_t message_type) {
   ProtoSize size;
   msg.calculate_size(size);
   std::vector<uint8_t> &shared_buf = this->parent_->get_shared_buffer_ref();
