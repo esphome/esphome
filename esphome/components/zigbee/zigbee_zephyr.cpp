ea3be2ae47d65a81cd9718d4574f5c726ae01184
Tomasz Duda <tomaszduda23@gmail.com>
2026-02-09 23:14:11 +0100

    fix

--- esphome/components/zigbee/zigbee_zephyr.cpp
+++ esphome/components/zigbee/zigbee_zephyr.cpp
@@ -305,6 +305,7 @@ void ZigbeeComponent::after_reporting_info(zb_zcl_configure_reporting_req_t *con
 
 extern "C" {
 void zboss_signal_handler(zb_uint8_t param) { esphome::zigbee::global_zigbee->zboss_signal_handler_esphome(param); }
+
 extern zb_ret_t __real_zb_zcl_put_reporting_info_from_req(zb_zcl_configure_reporting_req_t *config_rep_req,
                                                           zb_zcl_attr_addr_info_t *attr_addr_info);
 
