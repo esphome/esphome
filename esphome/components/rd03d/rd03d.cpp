6670c2b6c422a1b9961a4484fbdc7d6a6589b5bf
J. Nick Koston <nick@home-assistant.io>
2026-02-09 09:30:24 -0600

    Add comment explaining early guard

--- esphome/components/rd03d/rd03d.cpp
+++ esphome/components/rd03d/rd03d.cpp
@@ -81,6 +81,8 @@ void RD03DComponent::dump_config() {
 }
 
 void RD03DComponent::loop() {
+  // Early return avoids stack adjustment for the batch buffer below.
+  // loop() runs ~7000/min so most calls have nothing to read.
   int avail = this->available();
   if (avail <= 0)
     return;
