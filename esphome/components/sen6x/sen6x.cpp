300e1b4849a86b80739cd8736f44e0a30b4c4053
Martin Ebner <martinebner@me.com>
2026-02-10 19:09:03 +0530

    Log product name, serial and firmware in dump_config
    These are useful for troubleshooting but were previously only
    logged at DEBUG level during setup, which is not visible when
    connecting via API after boot.

--- esphome/components/sen6x/sen6x.cpp
+++ esphome/components/sen6x/sen6x.cpp
@@ -302,6 +302,9 @@ void SEN6XComponent::finish_setup_() {
 void SEN6XComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "sen6x:");
   LOG_I2C_DEVICE(this);
+  ESP_LOGCONFIG(TAG, "  Product: %s", this->product_name_.c_str());
+  ESP_LOGCONFIG(TAG, "  Serial: %s", this->serial_number_.c_str());
+  ESP_LOGCONFIG(TAG, "  Firmware: %u.%u", this->firmware_version_major_, this->firmware_version_minor_);
 
   if (this->is_failed()) {
     switch (this->error_code_) {
