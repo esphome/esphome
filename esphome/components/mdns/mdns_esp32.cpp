2d13f9e8d2d0c1058759a07c759a40a48025e042
Kevin Ahrendt <kevin.ahrendt@openhomefoundation.org>
2026-02-12 11:22:18 -0500

    use old approach to get a clean commit hash that will work with ESPHome 2026.1.5

--- esphome/components/mdns/mdns_esp32.cpp
+++ esphome/components/mdns/mdns_esp32.cpp
@@ -28,14 +28,13 @@ static void register_esp32(MDNSComponent *comp, StaticVector<MDNSService, MDNS_S
   mdns_instance_name_set(hostname);
 
   for (const auto &service : services) {
-    // Stack buffer for up to 16 txt records, heap fallback for more
-    SmallBufferWithHeapFallback<16, mdns_txt_item_t> txt_records(service.txt_records.size());
+    auto txt_records = std::make_unique<mdns_txt_item_t[]>(service.txt_records.size());
     for (size_t i = 0; i < service.txt_records.size(); i++) {
       const auto &record = service.txt_records[i];
       // key and value are either compile-time string literals in flash or pointers to dynamic_txt_values_
       // Both remain valid for the lifetime of this function, and ESP-IDF makes internal copies
-      txt_records.get()[i].key = MDNS_STR_ARG(record.key);
-      txt_records.get()[i].value = MDNS_STR_ARG(record.value);
+      txt_records[i].key = MDNS_STR_ARG(record.key);
+      txt_records[i].value = MDNS_STR_ARG(record.value);
     }
     uint16_t port = const_cast<TemplatableValue<uint16_t> &>(service.port).value();
     err = mdns_service_add(nullptr, MDNS_STR_ARG(service.service_type), MDNS_STR_ARG(service.proto), port,
@@ -45,6 +44,24 @@ static void register_esp32(MDNSComponent *comp, StaticVector<MDNSService, MDNS_S
       ESP_LOGW(TAG, "Failed to register service %s: %s", MDNS_STR_ARG(service.service_type), esp_err_to_name(err));
     }
   }
+  // for (const auto &service : services) {
+  //   // Stack buffer for up to 16 txt records, heap fallback for more
+  //   SmallBufferWithHeapFallback<16, mdns_txt_item_t> txt_records(service.txt_records.size());
+  //   for (size_t i = 0; i < service.txt_records.size(); i++) {
+  //     const auto &record = service.txt_records[i];
+  //     // key and value are either compile-time string literals in flash or pointers to dynamic_txt_values_
+  //     // Both remain valid for the lifetime of this function, and ESP-IDF makes internal copies
+  //     txt_records.get()[i].key = MDNS_STR_ARG(record.key);
+  //     txt_records.get()[i].value = MDNS_STR_ARG(record.value);
+  //   }
+  //   uint16_t port = const_cast<TemplatableValue<uint16_t> &>(service.port).value();
+  //   err = mdns_service_add(nullptr, MDNS_STR_ARG(service.service_type), MDNS_STR_ARG(service.proto), port,
+  //                          txt_records.get(), service.txt_records.size());
+
+  //   if (err != ESP_OK) {
+  //     ESP_LOGW(TAG, "Failed to register service %s: %s", MDNS_STR_ARG(service.service_type), esp_err_to_name(err));
+  //   }
+  // }
 #endif
 }
 
