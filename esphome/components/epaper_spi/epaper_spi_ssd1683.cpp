0e48752c8a338b7b8734913fede678b46f25f8e9
Elvin Luff <elvin@eelviny.me>
2026-02-13 15:10:58 +0100

    fix: 0x05 does nothing different according to datasheet, set back to 0x01

--- esphome/components/epaper_spi/epaper_spi_ssd1683.cpp
+++ esphome/components/epaper_spi/epaper_spi_ssd1683.cpp
@@ -9,7 +9,7 @@ static constexpr const char *const TAG = "epaper_spi.mono";
 
 void EPaperSSD1683::refresh_screen(bool partial) {
   ESP_LOGV(TAG, "Refresh screen");
-  this->cmd_data(0x3C, {partial ? (uint8_t) 0x80 : (uint8_t) 0x05});
+  this->cmd_data(0x3C, {partial ? (uint8_t) 0x80 : (uint8_t) 0x01});
   // On partial update, set red RAM to inverse to remove BW ghosting
   this->cmd_data(0x21, {partial ? (uint8_t) 0x80 : (uint8_t) 0x40, (uint8_t) 0x00});
   this->cmd_data(0x22, {partial ? (uint8_t) 0xFC : (uint8_t) 0xD7});
