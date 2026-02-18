3a53c5a689669196541843c6fd4533b233d63ddc
J. Nick Koston <nick@home-assistant.io>
2026-02-18 16:58:08 -0600

    [web_server] Fix water_heater traits in JSON state events
    Move min/max temperature and step traits into DETAIL_ALL block
    so they are only sent once on initial connection instead of on
    every state update. Rename keys from min_temperature/max_temperature
    to min_temp/max_temp to match what the frontend expects.
    
    Discovered while reviewing #14061.

--- esphome/components/web_server/web_server.cpp
+++ esphome/components/web_server/web_server.cpp
@@ -1922,6 +1922,9 @@ std::string WebServer::water_heater_json_(water_heater::WaterHeater *obj, JsonDe
     JsonArray modes = root[ESPHOME_F("modes")].to<JsonArray>();
     for (auto m : traits.get_supported_modes())
       modes.add(PSTR_LOCAL(water_heater::water_heater_mode_to_string(m)));
+    root[ESPHOME_F("min_temp")] = traits.get_min_temperature();
+    root[ESPHOME_F("max_temp")] = traits.get_max_temperature();
+    root[ESPHOME_F("step")] = traits.get_target_temperature_step();
     this->add_sorting_info_(root, obj);
   }
 
@@ -1943,10 +1946,6 @@ std::string WebServer::water_heater_json_(water_heater::WaterHeater *obj, JsonDe
     if (!std::isnan(target))
       root[ESPHOME_F("target_temperature")] = target;
   }
-
-  root[ESPHOME_F("min_temperature")] = traits.get_min_temperature();
-  root[ESPHOME_F("max_temperature")] = traits.get_max_temperature();
-  root[ESPHOME_F("step")] = traits.get_target_temperature_step();
 
   if (traits.get_supports_away_mode()) {
     root[ESPHOME_F("away")] = obj->is_away();
