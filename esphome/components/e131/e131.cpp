588e7ce4a30f0227345d756db857167a4439d1ff
J. Nick Koston <nick@home-assistant.io>
2026-02-20 10:56:10 -0600

    fix merge

--- esphome/components/e131/e131.cpp
+++ esphome/components/e131/e131.cpp
@@ -75,7 +75,6 @@ void E131Component::loop() {
   // Drain all queued packets so multi-universe frames are applied
   // atomically before the light writes. Without this, each universe
   // packet would trigger a separate full-strip write causing tearing.
-#if defined(USE_SOCKET_IMPL_BSD_SOCKETS) || defined(USE_SOCKET_IMPL_LWIP_SOCKETS)
   while ((len = this->read_(buf, sizeof(buf))) > 0) {
     if (!this->packet_(buf, (size_t) len, universe, packet)) {
       ESP_LOGV(TAG, "Invalid packet received of size %d.", (int) len);
@@ -86,22 +85,6 @@ void E131Component::loop() {
       ESP_LOGV(TAG, "Ignored packet for %d universe of size %d.", universe, packet.count);
     }
   }
-#elif defined(USE_SOCKET_IMPL_LWIP_TCP)
-  while (auto packet_size = this->udp_.parsePacket()) {
-    auto len = this->udp_.read(buf, sizeof(buf));
-    if (len <= 0)
-      continue;
-
-    if (!this->packet_(buf, (size_t) len, universe, packet)) {
-      ESP_LOGV(TAG, "Invalid packet received of size %d.", (int) len);
-      continue;
-    }
-
-    if (!this->process_(universe, packet)) {
-      ESP_LOGV(TAG, "Ignored packet for %d universe of size %d.", universe, packet.count);
-    }
-  }
-#endif
 }
 
 void E131Component::add_effect(E131AddressableLightEffect *light_effect) {
