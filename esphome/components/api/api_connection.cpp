e0072a4b710ad0e53cad73fd5054c4e246b08c35
J. Nick Koston <nick@home-assistant.io>
2026-02-20 08:37:12 -0600

    reduce

--- esphome/components/api/api_connection.cpp
+++ esphome/components/api/api_connection.cpp
@@ -1534,11 +1534,10 @@ bool APIConnection::send_hello_response_(const HelloRequest &msg) {
   ESP_LOGV(TAG, "Hello from client: '%s' | %s | API Version %" PRIu16 ".%" PRIu16, this->helper_->get_client_name(),
            this->helper_->get_peername_to(peername), this->client_api_version_major_, this->client_api_version_minor_);
 
+  // TODO: Remove before 2026.8.0 (one version after get_object_id backward compat removal)
   if (!this->client_supports_api_version(1, 14)) {
-    ESP_LOGW(TAG,
-             "Client '%s' is using API version %" PRIu16 ".%" PRIu16
-             " which is outdated. Update the client to a version that supports API 1.14",
-             this->helper_->get_client_name(), this->client_api_version_major_, this->client_api_version_minor_);
+    ESP_LOGW(TAG, "'%s' using outdated API %" PRIu16 ".%" PRIu16 ", update to 1.14+", this->helper_->get_client_name(),
+             this->client_api_version_major_, this->client_api_version_minor_);
   }
 
   HelloResponse resp;
