ac42d5e75e6bff28c47903038e3b9a7dbba5c56b
J. Nick Koston <nick@home-assistant.io>
2026-02-22 14:24:31 -0600

    [core] Conditionally compile get_loop_priority with USE_LOOP_PRIORITY

--- esphome/components/wifi/__init__.py
+++ esphome/components/wifi/__init__.py
@@ -536,6 +536,13 @@ async def to_code(config):
         cg.add_library("ESP8266WiFi", None)
     elif CORE.is_rp2040:
         cg.add_library("WiFi", None)
+        # RP2040's mDNS library (LEAmDNS) relies on LwipIntf::stateUpCB() to restart
+        # mDNS when the network interface reconnects. However, this callback is disabled
+        # in the arduino-pico framework. As a workaround, we block component setup until
+        # WiFi is connected via can_proceed(), ensuring mDNS.begin() is called with an
+        # active connection. This define enables the loop priority sorting infrastructure
+        # used during the setup blocking phase.
+        cg.add_define("USE_LOOP_PRIORITY")
 
     if CORE.is_esp32:
         if config[CONF_ENABLE_BTM] or config[CONF_ENABLE_RRM]:
