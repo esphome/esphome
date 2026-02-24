e5e33ac30684f1fde7ab0173e805d495613ce11a
ritschys <117527311+ritschys@users.noreply.github.com>
2026-02-24 19:13:51 +0100

    Update pylontech_sensor.cpp

--- esphome/components/pylontech/sensor/pylontech_sensor.cpp
+++ esphome/components/pylontech/sensor/pylontech_sensor.cpp
@@ -1,5 +1,4 @@
 #include "pylontech_sensor.h"
-#include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
@@ -7,28 +6,16 @@ namespace pylontech {
 
 static const char *const TAG = "pylontech.sensor";
 
-PylontechSensor::PylontechSensor(int8_t bat_num) { this->bat_num_ = bat_num; }
-
 void PylontechSensor::dump_config() {
-  ESP_LOGCONFIG(TAG,
-                "Pylontech Sensor:\n"
-                " Battery %d",
-                this->bat_num_);
-  LOG_SENSOR("  ", "Voltage", this->voltage_sensor_);
-  LOG_SENSOR("  ", "Current", this->current_sensor_);
-  LOG_SENSOR("  ", "Temperature", this->temperature_sensor_);
-  LOG_SENSOR("  ", "Temperature low", this->temperature_low_sensor_);
-  LOG_SENSOR("  ", "Temperature high", this->temperature_high_sensor_);
-  LOG_SENSOR("  ", "Voltage low", this->voltage_low_sensor_);
-  LOG_SENSOR("  ", "Voltage high", this->voltage_high_sensor_);
-  LOG_SENSOR("  ", "Coulomb", this->coulomb_sensor_);
-  LOG_SENSOR("  ", "MOS Temperature", this->mos_temperature_sensor_);
+  ESP_LOGCONFIG(TAG, "Pylontech Sensor:");
+  ESP_LOGCONFIG(TAG, "  Battery %d", this->bat_num_);
 }
 
 void PylontechSensor::on_line_read(PylontechListener::LineContents *line) {
   if (this->bat_num_ != line->bat_num) {
     return;
   }
+
   if (this->voltage_sensor_ != nullptr) {
     this->voltage_sensor_->publish_state(((float) line->volt) / 1000.0f);
   }
@@ -55,6 +42,18 @@ void PylontechSensor::on_line_read(PylontechListener::LineContents *line) {
   }
   if (this->mos_temperature_sensor_ != nullptr) {
     this->mos_temperature_sensor_->publish_state(((float) line->mostempr) / 1000.0f);
+  }
+}
+
+void PylontechSensor::on_cell_data(const PylontechListener::CellContents *c) {
+  if (this->bat_num_ != c->battery_id) {
+    return; 
+  }
+
+  if (c->cell_id >= 0 && c->cell_id < 15) {
+    if (this->cell_voltages_[c->cell_id] != nullptr) {
+      this->cell_voltages_[c->cell_id]->publish_state(c->voltage);
+    }
   }
 }
 
