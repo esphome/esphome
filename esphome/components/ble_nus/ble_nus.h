2965511c6509d30fe19e9d1bdd86e3a7c683c35e
Tomasz Duda <tomaszduda23@gmail.com>
2026-02-26 19:33:53 +0100

    [nrf52,ble_nus] add uart support

--- esphome/components/ble_nus/ble_nus.h
+++ esphome/components/ble_nus/ble_nus.h
@@ -3,14 +3,19 @@
 #include "esphome/core/defines.h"
 #include "esphome/core/component.h"
 #ifdef USE_LOGGER
+#include "esphome/components/uart/uart_component.h"
 #include "esphome/components/logger/logger.h"
 #endif
 #include <shell/shell_bt_nus.h>
 #include <atomic>
 
+#ifdef USE_UART_DEBUGGER
+using namespace esphome::uart;  // NOLINT(google-global-names-in-headers)
+#endif
+
 namespace esphome::ble_nus {
 
-class BLENUS : public Component {
+class BLENUS : public uart::UARTComponent, public Component {
   enum TxStatus {
     TX_DISABLED,
     TX_ENABLED,
@@ -21,7 +26,12 @@ class BLENUS : public Component {
   void setup() override;
   void dump_config() override;
   void loop() override;
-  size_t write_array(const uint8_t *data, size_t len);
+  void write_array(const uint8_t *data, size_t len) override;
+  bool peek_byte(uint8_t *data) override;
+  bool read_array(uint8_t *data, size_t len) override;
+  size_t available() override;
+  void flush() override;
+  void check_logger_conflict() override {}
   void set_expose_log(bool expose_log) { this->expose_log_ = expose_log; }
 #ifdef USE_LOGGER
   void on_log(uint8_t level, const char *tag, const char *message, size_t message_len);
@@ -37,6 +47,12 @@ class BLENUS : public Component {
   std::atomic<bt_conn *> conn_ = nullptr;
   bool expose_log_ = false;
   atomic_t tx_status_ = ATOMIC_INIT(TX_DISABLED);
+  std::atomic<bool> connected_{};
+#ifdef ESPHOME_BLE_RX_RING_BUFFER_SIZE
+  // RX buffer for peek functionality
+  uint8_t peek_buffer_{0};
+  bool has_peek_{false};
+#endif
 };
 
 }  // namespace esphome::ble_nus
