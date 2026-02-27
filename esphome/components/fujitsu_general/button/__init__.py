260693f88f3c70ca84cb761e08b94df5fc8626c4
William Howell <wiiam24@gmail.com>
2026-02-28 00:37:57 +1300

    Remove name defaults

--- esphome/components/fujitsu_general/button/__init__.py
+++ esphome/components/fujitsu_general/button/__init__.py
@@ -15,19 +15,16 @@ FujitsuGeneralButton = fujitsu_general_ns.class_("FujitsuGeneralButton", button.
 BUTTON_CONFIGS = (
     (
         CONF_STEP_VERTICAL,
-        "Step Vertical Vane",
         "FUJITSU_GENERAL_MESSAGE_TYPE_NUDGE_VERTICAL",
         "mdi:arrow-up-down-bold",
     ),
     (
         CONF_STEP_HORIZONTAL,
-        "Step Horizontal Vane",
         "FUJITSU_GENERAL_MESSAGE_TYPE_NUDGE_HORIZONTAL",
         "mdi:arrow-left-right-bold",
     ),
     (
         CONF_ECONOMY,
-        "Economy Toggle",
         "FUJITSU_GENERAL_MESSAGE_TYPE_ECONOMY",
         "mdi:leaf",
     ),
@@ -38,7 +35,7 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Required(CONF_FUJITSU_GENERAL_ID): cv.use_id(FujitsuGeneralClimate),
         **{
             cv.Optional(key): button.button_schema(FujitsuGeneralButton, icon=icon)
-            for key, _, _, icon in BUTTON_CONFIGS
+            for key, _, icon in BUTTON_CONFIGS
         },
     }
 )
@@ -46,7 +43,7 @@ CONFIG_SCHEMA = cv.Schema(
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_FUJITSU_GENERAL_ID])
-    for key, default_name, command, _ in BUTTON_CONFIGS:
+    for key, command, _ in BUTTON_CONFIGS:
         if button_conf := config.get(key):
             var = cg.new_Pvariable(
                 button_conf[cv.CONF_ID], getattr(fujitsu_general_ns, command)
