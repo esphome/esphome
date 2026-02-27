a12873c213d877d917ebdcdd896e22596e0d3f42
William Howell <wiiam24@gmail.com>
2026-02-27 23:37:18 +1300

    Move byte constants to header

--- esphome/components/fujitsu_general/fujitsu_general.h
+++ esphome/components/fujitsu_general/fujitsu_general.h
@@ -8,14 +8,85 @@
 namespace esphome {
 namespace fujitsu_general {
 
-const uint8_t FUJITSU_GENERAL_TEMP_MIN = 16;  // Celsius // TODO 16 for heating, 18 for cooling, unsupported in ESPH
-const uint8_t FUJITSU_GENERAL_TEMP_MAX = 30;  // Celsius
+// TODO 16 for heating, 18 for cooling, unsupported in ESPH
+static constexpr uint8_t FUJITSU_GENERAL_TEMP_MIN = 16;  // Celsius
 
-// Util messages
-extern const uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_OFF;
-extern const uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_ECONOMY;
-extern const uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_NUDGE_VERTICAL;
-extern const uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_NUDGE_HORIZONTAL;
+static constexpr uint8_t FUJITSU_GENERAL_TEMP_MAX = 30;  // Celsius
+
+// Common header
+static constexpr uint8_t FUJITSU_GENERAL_COMMON_LENGTH = 6;
+static constexpr uint8_t FUJITSU_GENERAL_COMMON_BYTE0 = 0x14;
+static constexpr uint8_t FUJITSU_GENERAL_COMMON_BYTE1 = 0x63;
+static constexpr uint8_t FUJITSU_GENERAL_COMMON_BYTE2 = 0x00;
+static constexpr uint8_t FUJITSU_GENERAL_COMMON_BYTE3 = 0x10;
+static constexpr uint8_t FUJITSU_GENERAL_COMMON_BYTE4 = 0x10;
+static constexpr uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_BYTE = 5;
+
+// State message - temp & fan etc.
+static constexpr uint8_t FUJITSU_GENERAL_STATE_MESSAGE_LENGTH = 16;
+static constexpr uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_STATE = 0xFE;
+
+// Util messages - off & eco etc.
+static constexpr uint8_t FUJITSU_GENERAL_UTIL_MESSAGE_LENGTH = 7;
+static constexpr uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_OFF = 0x02;
+static constexpr uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_ECONOMY = 0x09;
+static constexpr uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_NUDGE_VERTICAL = 0x6C;
+static constexpr uint8_t FUJITSU_GENERAL_MESSAGE_TYPE_NUDGE_HORIZONTAL = 0x79;
+
+// State header
+static constexpr uint8_t FUJITSU_GENERAL_STATE_HEADER_BYTE0 = 0x09;
+static constexpr uint8_t FUJITSU_GENERAL_STATE_HEADER_BYTE1 = 0x30;
+
+// State footer
+static constexpr uint8_t FUJITSU_GENERAL_STATE_FOOTER_BYTE0 = 0x20;
+
+// Temperature
+static constexpr uint8_t FUJITSU_GENERAL_TEMPERATURE_NIBBLE = 16;
+
+// Power on
+static constexpr uint8_t FUJITSU_GENERAL_POWER_ON_NIBBLE = 17;
+static constexpr uint8_t FUJITSU_GENERAL_POWER_OFF = 0x00;
+static constexpr uint8_t FUJITSU_GENERAL_POWER_ON = 0x01;
+
+// Mode
+static constexpr uint8_t FUJITSU_GENERAL_MODE_NIBBLE = 19;
+static constexpr uint8_t FUJITSU_GENERAL_MODE_AUTO = 0x00;
+static constexpr uint8_t FUJITSU_GENERAL_MODE_COOL = 0x01;
+static constexpr uint8_t FUJITSU_GENERAL_MODE_DRY = 0x02;
+static constexpr uint8_t FUJITSU_GENERAL_MODE_FAN = 0x03;
+static constexpr uint8_t FUJITSU_GENERAL_MODE_HEAT = 0x04;
+// static constexpr uint8_t FUJITSU_GENERAL_MODE_10C = 0x0B;
+
+// Swing
+static constexpr uint8_t FUJITSU_GENERAL_SWING_NIBBLE = 20;
+static constexpr uint8_t FUJITSU_GENERAL_SWING_NONE = 0x00;
+static constexpr uint8_t FUJITSU_GENERAL_SWING_VERTICAL = 0x01;
+static constexpr uint8_t FUJITSU_GENERAL_SWING_HORIZONTAL = 0x02;
+static constexpr uint8_t FUJITSU_GENERAL_SWING_BOTH = 0x03;
+
+// Fan
+static constexpr uint8_t FUJITSU_GENERAL_FAN_NIBBLE = 21;
+static constexpr uint8_t FUJITSU_GENERAL_FAN_AUTO = 0x00;
+static constexpr uint8_t FUJITSU_GENERAL_FAN_HIGH = 0x01;
+static constexpr uint8_t FUJITSU_GENERAL_FAN_MEDIUM = 0x02;
+static constexpr uint8_t FUJITSU_GENERAL_FAN_LOW = 0x03;
+static constexpr uint8_t FUJITSU_GENERAL_FAN_SILENT = 0x04;
+
+// TODO Outdoor Unit Low Noise
+// static constexpr uint8_t FUJITSU_GENERAL_OUTDOOR_UNIT_LOW_NOISE_BYTE14 = 0xA0;
+// static constexpr uint8_t FUJITSU_GENERAL_STATE_BYTE14 = 0x20;
+
+static constexpr uint16_t FUJITSU_GENERAL_HEADER_MARK = 3300;
+static constexpr uint16_t FUJITSU_GENERAL_HEADER_SPACE = 1600;
+
+static constexpr uint16_t FUJITSU_GENERAL_BIT_MARK = 420;
+static constexpr uint16_t FUJITSU_GENERAL_ONE_SPACE = 1200;
+static constexpr uint16_t FUJITSU_GENERAL_ZERO_SPACE = 420;
+
+static constexpr uint16_t FUJITSU_GENERAL_TRL_MARK = 420;
+static constexpr uint16_t FUJITSU_GENERAL_TRL_SPACE = 8000;
+
+static constexpr uint32_t FUJITSU_GENERAL_CARRIER_FREQUENCY = 38000;
 
 // clang-format off
 /**
