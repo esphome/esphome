125dc4d170db6ad84fdec8649d8001ec7b6f33bb
Tomasz Duda <tomaszduda23@gmail.com>
2026-03-01 16:14:21 +0100

    [zephyr_ble_server] add support for on_numeric_comparison_request

--- esphome/components/zephyr_ble_server/__init__.py
+++ esphome/components/zephyr_ble_server/__init__.py
@@ -1,28 +1,38 @@
+from esphome import automation
 import esphome.codegen as cg
 from esphome.components.zephyr import zephyr_add_prj_conf
 import esphome.config_validation as cv
-from esphome.const import CONF_ESPHOME, CONF_ID, CONF_NAME, Framework
-import esphome.final_validate as fv
+from esphome.const import CONF_ID, CONF_TRIGGER_ID, Framework
+from esphome.core import CORE
 
 zephyr_ble_server_ns = cg.esphome_ns.namespace("zephyr_ble_server")
 BLEServer = zephyr_ble_server_ns.class_("BLEServer", cg.Component)
+
+BLENumericComparisonRequestTrigger = zephyr_ble_server_ns.class_(
+    "BLENumericComparisonRequestTrigger",
+    automation.Trigger.template(cg.uint32),
+)
+
+CONF_ON_NUMERIC_COMPARISON_REQUEST = "on_numeric_comparison_request"
+CONF_ACCEPT = "accept"
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(BLEServer),
+            cv.Optional(
+                CONF_ON_NUMERIC_COMPARISON_REQUEST
+            ): automation.validate_automation(
+                {
+                    cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
+                        BLENumericComparisonRequestTrigger
+                    ),
+                }
+            ),
         }
     ).extend(cv.COMPONENT_SCHEMA),
     cv.only_with_framework(Framework.ZEPHYR),
 )
-
-
-def _final_validate(_):
-    full_config = fv.full_config.get()
-    zephyr_add_prj_conf("BT_DEVICE_NAME", full_config[CONF_ESPHOME][CONF_NAME])
-
-
-FINAL_VALIDATE_SCHEMA = _final_validate
 
 
 async def to_code(config):
@@ -30,5 +40,44 @@ async def to_code(config):
     zephyr_add_prj_conf("BT", True)
     zephyr_add_prj_conf("BT_PERIPHERAL", True)
     zephyr_add_prj_conf("BT_RX_STACK_SIZE", 1536)
-    # zephyr_add_prj_conf("BT_LL_SW_SPLIT", True)
+    zephyr_add_prj_conf("BT_DEVICE_NAME", CORE.name)
     await cg.register_component(var, config)
+    for conf in config.get(CONF_ON_NUMERIC_COMPARISON_REQUEST, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(cg.uint32, "passkey")], conf)
+    if len(config.get(CONF_ON_NUMERIC_COMPARISON_REQUEST, [])) > 0:
+        zephyr_add_prj_conf("BT_SMP", True)
+        zephyr_add_prj_conf("BT_SETTINGS", True)
+        zephyr_add_prj_conf("BT_SMP_SC_ONLY", True)
+        zephyr_add_prj_conf("BT_KEYS_OVERWRITE_OLDEST", True)
+
+
+BLENumericComparisonReplyAction = zephyr_ble_server_ns.class_(
+    "BLENumericComparisonReplyAction", automation.Action
+)
+
+BLE_NUMERIC_COMPARISON_REPLY_ACTION_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_ID): cv.use_id(BLEServer),
+        cv.Required(CONF_ACCEPT): cv.templatable(cv.boolean),
+    }
+)
+
+
+@automation.register_action(
+    "ble_server.numeric_comparison_reply",
+    BLENumericComparisonReplyAction,
+    BLE_NUMERIC_COMPARISON_REPLY_ACTION_SCHEMA,
+)
+async def numeric_comparison_reply_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, parent)
+
+    accept = config[CONF_ACCEPT]
+    if cg.is_template(accept):
+        templ = await cg.templatable(accept, args, cg.bool_)
+        cg.add(var.set_value_template(templ))
+    else:
+        cg.add(var.set_value_simple(accept))
+
+    return var
--- esphome/components/zephyr_ble_server/ble_server.h
+++ esphome/components/zephyr_ble_server/ble_server.h
@@ -1,18 +1,62 @@
 #pragma once
 #ifdef USE_ZEPHYR
 #include "esphome/core/component.h"
+#include <zephyr/bluetooth/conn.h>
+#include "esphome/core/automation.h"
 
 namespace esphome::zephyr_ble_server {
 
 class BLEServer : public Component {
  public:
   void setup() override;
-  void loop() override;
-  void on_shutdown() override;
+  void dump_config() override;
+  void add_passkey_callback(std::function<void(uint32_t)> &&cb) { this->passkey_cb_.add(std::move(cb)); }
+  void bt_conn_auth(bool confirm);
 
  protected:
-  void resume_();
-  bool suspended_ = false;
+  static void connected(bt_conn *conn, uint8_t err);
+  static void disconnected(bt_conn *conn, uint8_t reason);
+  static void auth_passkey_confirm(struct bt_conn *conn, unsigned int passkey);
+  bt_conn *conn_{};
+  CallbackManager<void(uint32_t)> passkey_cb_;
+};
+
+class BLENumericComparisonRequestTrigger : public Trigger<uint32_t> {
+ public:
+  explicit BLENumericComparisonRequestTrigger(BLEServer *parent) {
+    parent->add_passkey_callback([this](uint32_t key) { this->trigger(key); });
+  }
+};
+
+template<typename... Ts> class BLENumericComparisonReplyAction : public Action<Ts...> {
+ public:
+  BLENumericComparisonReplyAction(BLEServer *parent) { this->parent_ = parent; }
+
+  void play(const Ts &...x) override {
+    if (has_simple_value_) {
+      this->parent_->bt_conn_auth(this->value_.simple);
+    } else {
+      this->parent_->bt_conn_auth(this->value_.template_func(x...));
+    }
+  }
+
+  void set_value_template(bool (*func)(Ts...)) {
+    this->value_.template_func = func;
+    this->has_simple_value_ = false;
+  }
+
+  void set_value_simple(const bool &value) {
+    this->value_.simple = value;
+    this->has_simple_value_ = true;
+  }
+
+ private:
+  BLEServer *parent_{nullptr};
+  bool has_simple_value_ = true;
+  union {
+    bool simple;
+    bool (*template_func)(Ts...);
+  } value_{.simple = false};
 };
 
 }  // namespace esphome::zephyr_ble_server
