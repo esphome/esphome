157c6193f808dfab29e79dec3f310b59161b7eca
Kevin Ahrendt <kahrendt@gmail.com>
2026-03-03 17:38:42 -0500

    Moves to a standalone core file instead of staying in helpers.
    Reuses the existing stack if a newly requested one is smaller and deallocates/creates a new stack buffer is a larger size is requested
    
    Adds a note that this should only be used for calling outside the task. Self deleting tasks cause problems on multicore devices with rapid starts/stops

--- esphome/core/helpers.cpp
+++ esphome/core/helpers.cpp
@@ -856,49 +856,4 @@ void IRAM_ATTR HOT delay_microseconds_safe(uint32_t us) {
     ;
 }
 
-#ifdef USE_ESP32
-bool StaticTask::create(TaskFunction_t fn, const char *name, uint32_t stack_size, void *param, UBaseType_t priority,
-                        bool use_psram) {
-  if (this->handle_ != nullptr) {
-    // Task is already running; must call destroy() first
-    return false;
-  }
-
-  if (this->stack_buffer_ == nullptr) {
-    this->stack_size_ = stack_size;
-    this->use_psram_ = use_psram;
-    RAMAllocator<StackType_t> allocator(use_psram ? RAMAllocator<StackType_t>::ALLOC_EXTERNAL
-                                                  : RAMAllocator<StackType_t>::ALLOC_INTERNAL);
-    this->stack_buffer_ = allocator.allocate(stack_size);
-  }
-  if (this->stack_buffer_ == nullptr) {
-    return false;
-  }
-
-  this->handle_ = xTaskCreateStatic(fn, name, stack_size, param, priority, this->stack_buffer_, &this->tcb_);
-  if (this->handle_ == nullptr) {
-    this->deallocate();
-    return false;
-  }
-  return true;
-}
-
-void StaticTask::destroy() {
-  if (this->handle_ != nullptr) {
-    vTaskDelete(this->handle_);
-    this->handle_ = nullptr;
-  }
-}
-
-void StaticTask::deallocate() {
-  this->destroy();
-  if (this->stack_buffer_ != nullptr) {
-    RAMAllocator<StackType_t> allocator(this->use_psram_ ? RAMAllocator<StackType_t>::ALLOC_EXTERNAL
-                                                         : RAMAllocator<StackType_t>::ALLOC_INTERNAL);
-    allocator.deallocate(this->stack_buffer_, this->stack_size_);
-    this->stack_buffer_ = nullptr;
-  }
-}
-#endif  // USE_ESP32
-
 }  // namespace esphome
--- esphome/core/helpers.h
+++ esphome/core/helpers.h
@@ -36,7 +36,6 @@
 #if defined(USE_ESP32)
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
-#include <freertos/task.h>
 #elif defined(USE_LIBRETINY)
 #include <FreeRTOS.h>
 #include <semphr.h>
@@ -1894,41 +1893,6 @@ template<class T> class RAMAllocator {
 };
 
 template<class T> using ExternalRAMAllocator = RAMAllocator<T>;
-
-#ifdef USE_ESP32
-/** Helper for FreeRTOS static task management.
- * Bundles TaskHandle_t, StaticTask_t, and the stack buffer into one object with create/destroy methods.
- */
-class StaticTask {
- public:
-  /// @brief Check if the task has been created and not yet destroyed.
-  bool is_running() const { return this->handle_ != nullptr; }
-
-  /// @brief Allocate stack and create task.
-  /// @param fn         Task function
-  /// @param name       Task name (for debug)
-  /// @param stack_size Stack size in StackType_t words
-  /// @param param      Parameter passed to task function
-  /// @param priority   FreeRTOS task priority
-  /// @param use_psram  If true, allocate stack in PSRAM; otherwise internal RAM
-  /// @return true on success
-  bool create(TaskFunction_t fn, const char *name, uint32_t stack_size, void *param, UBaseType_t priority,
-              bool use_psram);
-
-  /// @brief Delete the task but keep the stack buffer allocated for reuse by a subsequent create() call.
-  void destroy();
-
-  /// @brief Delete the task (if running) and free the stack buffer.
-  void deallocate();
-
- protected:
-  TaskHandle_t handle_{nullptr};
-  StaticTask_t tcb_;
-  StackType_t *stack_buffer_{nullptr};
-  uint32_t stack_size_{0};
-  bool use_psram_{false};
-};
-#endif  // USE_ESP32
 
 /**
  * Functions to constrain the range of arithmetic values.
