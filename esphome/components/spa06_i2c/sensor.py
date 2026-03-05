5430865182316a29a3e4859314350eff1326784d
Daniel Kent <git@danielkent.net>
2026-03-05 13:21:58 -0500

    Add measurement timing checks to I2C

--- esphome/components/spa06_i2c/sensor.py
+++ esphome/components/spa06_i2c/sensor.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 from esphome.components import i2c
 import esphome.config_validation as cv
 
-from ..spa06_base import CONFIG_SCHEMA_BASE, to_code_base
+from ..spa06_base import CONFIG_SCHEMA_BASE, measurement_timing_check, to_code_base
 
 AUTO_LOAD = ["spa06_base"]
 CODEOWNERS = ["@danielkent-net"]
@@ -13,9 +13,12 @@ SPA06I2CComponent = spa06_ns.class_(
     "SPA06I2CComponent", cg.PollingComponent, i2c.I2CDevice
 )
 
-CONFIG_SCHEMA = CONFIG_SCHEMA_BASE.extend(
-    i2c.i2c_device_schema(default_address=0x77)
-).extend({cv.GenerateID(): cv.declare_id(SPA06I2CComponent)})
+CONFIG_SCHEMA = cv.All(
+    CONFIG_SCHEMA_BASE.extend(i2c.i2c_device_schema(default_address=0x77)).extend(
+        {cv.GenerateID(): cv.declare_id(SPA06I2CComponent)}
+    ),
+    measurement_timing_check,
+)
 
 
 async def to_code(config):
