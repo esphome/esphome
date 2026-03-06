b06020988dad41dee339976c8bbdfbb896955303
Tomasz Duda <tomaszduda23@gmail.com>
2026-03-06 16:05:08 +0100

    fix

--- esphome/components/esp32/__init__.py
+++ esphome/components/esp32/__init__.py
@@ -465,9 +465,7 @@ def only_on_variant(*, supported=None, unsupported=None, msg_prefix="This featur
 
     def validator_(obj):
         if not CORE.is_esp32:
-            raise cv.Invalid(
-                f"{msg_prefix} is only available on ESP32"
-            )
+            raise cv.Invalid(f"{msg_prefix} is only available on ESP32")
         variant = get_esp32_variant()
         if supported is not None and variant not in supported:
             raise cv.Invalid(
