8cef454fd9ea2323fb14bbc2a02a14862e06858e
J. Nick Koston <nick@home-assistant.io>
2026-03-07 13:44:15 -1000

    [rp2040_ble] Add BLE component for RP2040/RP2350 (BTstack init only)

---
+++ esphome/components/rp2040_ble/rp2040_ble.cpp
@@ -0,0 +1,100 @@
+#include "rp2040_ble.h"
+
+#ifdef USE_RP2040_BLE
+
+#include "esphome/core/log.h"
+
+namespace esphome::rp2040_ble {
+
+static const char *const TAG = "rp2040_ble";
+
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+RP2040BLE *global_ble = nullptr;
+
+void RP2040BLE::setup() {
+  global_ble = this;
+
+  if (this->enable_on_boot_) {
+    this->enable();
+  } else {
+    this->state_ = BLEComponentState::DISABLED;
+  }
+}
+
+void RP2040BLE::enable() {
+  if (this->state_ == BLEComponentState::ACTIVE) {
+    return;
+  }
+
+  ESP_LOGD(TAG, "Enabling BLE...");
+  this->state_ = BLEComponentState::ENABLING;
+
+  l2cap_init();
+  sm_init();
+
+  // Register HCI event handler
+  this->hci_event_callback_registration_.callback = &RP2040BLE::packet_handler_;
+  hci_add_event_handler(&this->hci_event_callback_registration_);
+
+  // Register SM event handler
+  sm_add_event_handler(&this->hci_event_callback_registration_);
+
+  hci_power_control(HCI_POWER_ON);
+}
+
+void RP2040BLE::disable() {
+  if (this->state_ == BLEComponentState::DISABLED || this->state_ == BLEComponentState::OFF) {
+    return;
+  }
+
+  ESP_LOGD(TAG, "Disabling BLE...");
+  this->state_ = BLEComponentState::DISABLING;
+
+  hci_power_control(HCI_POWER_OFF);
+
+  this->state_ = BLEComponentState::DISABLED;
+  ESP_LOGD(TAG, "BLE disabled");
+}
+
+void RP2040BLE::loop() {
+  if (this->state_ == BLEComponentState::ACTIVE && !this->active_logged_) {
+    this->active_logged_ = true;
+    ESP_LOGI(TAG, "BLE active");
+  }
+}
+
+void RP2040BLE::dump_config() {
+  ESP_LOGCONFIG(TAG, "RP2040 BLE:");
+  ESP_LOGCONFIG(TAG, "  Enable on boot: %s", YESNO(this->enable_on_boot_));
+  ESP_LOGCONFIG(TAG, "  State: %d", static_cast<int>(this->state_));
+}
+
+float RP2040BLE::get_setup_priority() const { return setup_priority::BLUETOOTH; }
+
+void RP2040BLE::packet_handler_(uint8_t type, uint16_t channel, uint8_t *packet, uint16_t size) {
+  if (global_ble == nullptr) {
+    return;
+  }
+
+  if (type != HCI_EVENT_PACKET) {
+    return;
+  }
+
+  uint8_t event_type = hci_event_packet_get_type(packet);
+
+  switch (event_type) {
+    case BTSTACK_EVENT_STATE: {
+      uint8_t state = btstack_event_state_get_state(packet);
+      if (state == HCI_STATE_WORKING) {
+        global_ble->state_ = BLEComponentState::ACTIVE;
+      }
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+}  // namespace esphome::rp2040_ble
+
+#endif  // USE_RP2040_BLE
---
+++ esphome/components/rp2040_ble/rp2040_ble.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/defines.h"
+
+#ifdef USE_RP2040_BLE
+
+#include <btstack.h>
+
+namespace esphome::rp2040_ble {
+
+enum class BLEComponentState : uint8_t {
+  OFF = 0,
+  ENABLING,
+  ACTIVE,
+  DISABLING,
+  DISABLED,
+};
+
+class RP2040BLE : public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  float get_setup_priority() const override;
+
+  void enable();
+  void disable();
+  bool is_active() const { return this->state_ == BLEComponentState::ACTIVE; }
+
+  void set_enable_on_boot(bool enable_on_boot) { this->enable_on_boot_ = enable_on_boot; }
+
+ protected:
+  static void packet_handler_(uint8_t type, uint16_t channel, uint8_t *packet, uint16_t size);
+
+  btstack_packet_callback_registration_t hci_event_callback_registration_{};
+
+  BLEComponentState state_{BLEComponentState::OFF};
+  bool enable_on_boot_{true};
+  bool active_logged_{false};
+};
+
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+extern RP2040BLE *global_ble;
+
+}  // namespace esphome::rp2040_ble
+
+#endif  // USE_RP2040_BLE
