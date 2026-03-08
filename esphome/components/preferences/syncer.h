1c7aae82a9d3011e750876dccbb7a590c9ea25a4
rwrozelle <rwrozelle@gmail.com>
2026-03-08 13:11:21 -0400

    rework back to Component

--- esphome/components/preferences/syncer.h
+++ esphome/components/preferences/syncer.h
@@ -6,25 +6,44 @@
 namespace esphome {
 namespace preferences {
 
-class IntervalSyncer : public PollingComponent {
+class IntervalSyncer : public Component {
  public:
-  void set_write_interval(uint32_t write_interval) { this->set_update_interval(write_interval); }
-  void update() override { global_preferences->sync(); }
+  void set_write_interval(uint32_t write_interval) { this->write_interval_ = write_interval; }
   void setup() override {
-    if (this->update_interval_ == 0) {
-      this->stop_poller();
-    } else {
+    if (this->write_interval_ != 0) {
+      this->start_poller();
       // When using interval-based syncing, we don't need the loop
       this->disable_loop();
     }
   }
   void loop() override {
-    if (this->update_interval_ == 0) {
+    if (this->write_interval_ == 0) {
       global_preferences->sync();
     }
   }
   void on_shutdown() override { global_preferences->sync(); }
   float get_setup_priority() const override { return setup_priority::BUS; }
+  void start_poller() {
+    // Reregister interval.
+    if (this->write_interval_ != 0) {
+      this->set_interval(InternalSchedulerID::POLLING_UPDATE, this->write_interval_,
+                         []() { global_preferences->sync(); });
+    } else {
+      // doesn't do anything if state is not COMPONENT_STATE_LOOP_DONE
+      this->enable_loop();
+    }
+  }
+  void stop_poller() {
+    if (this->write_interval_ != 0) {
+      // Clear the interval to suspend component
+      this->cancel_interval(InternalSchedulerID::POLLING_UPDATE);
+    } else {
+      this->disable_loop();
+    }
+  }
+
+ protected:
+  uint32_t write_interval_{60000};
 };
 
 }  // namespace preferences
