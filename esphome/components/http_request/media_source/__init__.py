ff8ce89556748509d7ee8724e12d9d43d3c8c1e8
Kevin Ahrendt <kahrendt@gmail.com>
2026-03-09 05:28:18 -0400

    add typing to to_code

--- esphome/components/http_request/media_source/__init__.py
+++ esphome/components/http_request/media_source/__init__.py
@@ -48,7 +48,7 @@ CONFIG_SCHEMA = cv.All(
 )
 
 
-async def to_code(config):
+async def to_code(config: ConfigType) -> None:
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await media_source.register_media_source(var, config)
