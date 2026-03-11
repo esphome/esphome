bdd5b34d9db3a60cb4af62a0757acec1f891782c
fakuivan <fakuivan@gmail.com>
2026-03-11 20:52:52 -0300

    Add workaround for no-consts-in-defines

--- tests/components/esphome/include_order/before_globals_include.h
+++ tests/components/esphome/include_order/before_globals_include.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#define DEFINED_FIRST 2
+// Workaround for the linter to not complain about #define being used for
+// constants
+constexpr auto defined_first = 2;
+
+#define DEFINED_FIRST defined_first
 
 struct CustomGlobalType {
   int value;
