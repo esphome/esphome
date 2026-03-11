2ffe91495b69e4bf6a515d65376acfbc16549a51
Daniel Kent <git@danielkent.net>
2026-03-11 09:04:01 -0400

    [spa06] Remove config check in spi that was put in base

--- esphome/components/spa06_spi/sensor.py
+++ esphome/components/spa06_spi/sensor.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 from esphome.components import spi
 import esphome.config_validation as cv
 
-from ..spa06_base import CONFIG_SCHEMA_BASE, measurement_timing_check, to_code_base
+from ..spa06_base import CONFIG_SCHEMA_BASE, to_code_base
 
 AUTO_LOAD = ["spa06_base"]
 CODEOWNERS = ["@danielkent-net"]
@@ -13,12 +13,9 @@ SPA06SPIComponent = spa06_ns.class_(
     "SPA06SPIComponent", cg.PollingComponent, spi.SPIDevice
 )
 
-CONFIG_SCHEMA = cv.All(
-    CONFIG_SCHEMA_BASE.extend(spi.spi_device_schema(default_mode="mode3")).extend(
-        {cv.GenerateID(): cv.declare_id(SPA06SPIComponent)}
-    ),
-    measurement_timing_check,
-)
+CONFIG_SCHEMA = CONFIG_SCHEMA_BASE.extend(
+    spi.spi_device_schema(default_mode="mode3")
+).extend({cv.GenerateID(): cv.declare_id(SPA06SPIComponent)})
 
 
 async def to_code(config):
