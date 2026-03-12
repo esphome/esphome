550785f6707e4b4ab65f0db6a55f1a7b7a830311
Daniel Kent <git@danielkent.net>
2026-03-12 15:50:25 -0400

    [spa06_i2c] Remove unused TAG

--- esphome/components/spa06_i2c/spa06_i2c.h
+++ esphome/components/spa06_i2c/spa06_i2c.h
@@ -4,8 +4,6 @@
 
 namespace esphome::spa06_i2c {
 
-static const char *const TAG = "spa06_i2c.sensor";
-
 class SPA06I2CComponent : public spa06_base::SPA06Component, public i2c::I2CDevice {
  public:
   bool spa_read_byte(uint8_t a_register, uint8_t *data) override { return read_byte(a_register, data); }
