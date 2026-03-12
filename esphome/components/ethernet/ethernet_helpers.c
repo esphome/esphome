3eb29cb05eeda7842ec0415526a709be1e4404a1
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2026-03-11 20:19:51 -0400

    Move to c file

--- esphome/components/ethernet/ethernet_helpers.c
+++ esphome/components/ethernet/ethernet_helpers.c
@@ -1,4 +1,4 @@
-#include "esp_eth_mac.h"
+#include "esp_eth_mac_esp.h"
 
 // ETH_ESP32_EMAC_DEFAULT_CONFIG() uses out-of-order designated initializers
 // which are valid in C but not in C++. This wrapper allows C++ code to get
