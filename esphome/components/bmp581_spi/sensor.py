547c6d03a48b861dc10514c3a67d7f7d52991eba
Daniel Kent <git@danielkent.net>
2026-03-13 17:05:37 -0400

    [bmp581_spi] Fixed some
     - Added mode check with warning to configuration check
     - Set default SPIComponent parameters to SPI Mode3

--- esphome/components/bmp581_spi/sensor.py
+++ esphome/components/bmp581_spi/sensor.py
@@ -1,5 +1,8 @@
+import logging
+
 import esphome.codegen as cg
 from esphome.components import spi
+from esphome.components.spi import CONF_SPI_MODE
 import esphome.config_validation as cv
 
 from ..bmp581_base import CONFIG_SCHEMA_BASE, to_code_base
@@ -8,14 +11,39 @@ AUTO_LOAD = ["bmp581_base"]
 CODEOWNERS = ["@kahrendt", "@danielkent-net"]
 DEPENDENCIES = ["spi"]
 
+_LOGGER = logging.getLogger(__name__)
+
+VALID_SPI_MODES = {
+    0: "MODE0",
+    "0": "MODE0",
+    "MODE0": "MODE0",
+    3: "MODE3",
+    "3": "MODE3",
+    "MODE3": "MODE3",
+}
+
 bmp581_ns = cg.esphome_ns.namespace("bmp581_spi")
 BMP581SPIComponent = bmp581_ns.class_(
     "BMP581SPIComponent", cg.PollingComponent, spi.SPIDevice
 )
 
-CONFIG_SCHEMA = CONFIG_SCHEMA_BASE.extend(
-    spi.spi_device_schema(default_mode="mode3")
-).extend({cv.GenerateID(): cv.declare_id(BMP581SPIComponent)})
+
+def check_spi_mode(config):
+    spi_mode = config.get(CONF_SPI_MODE)
+    if spi_mode not in VALID_SPI_MODES:
+        _LOGGER.warning(
+            "BMP581 only supports SPI mode0 and mode3. "
+            "Sensor may not work correctly or at all."
+        )
+    return config
+
+
+CONFIG_SCHEMA = cv.All(
+    CONFIG_SCHEMA_BASE.extend(spi.spi_device_schema(default_mode="mode3")).extend(
+        {cv.GenerateID(): cv.declare_id(BMP581SPIComponent)}
+    ),
+    check_spi_mode,
+)
 
 
 async def to_code(config):
