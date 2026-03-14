2f318e3fbabe4e8a6d4ea9a5ee36078ecdc6d04d
J. Nick Koston <nick@home-assistant.io>
2026-03-14 11:21:04 -1000

    Revert "Fix shared buffer bug between reset reason and wakeup cause"
    This reverts commit a15f357c2a9b8d968f5f6ed0e72bd35a590922e4.

--- esphome/components/debug/debug_esp32.cpp
+++ esphome/components/debug/debug_esp32.cpp
@@ -239,10 +239,9 @@ size_t DebugComponent::get_device_info_(std::span<char, DEVICE_INFO_BUFFER_SIZE>
   uint32_t cpu_freq_mhz = arch_get_cpu_freq_hz() / 1000000;
   pos = buf_append_printf(buf, size, pos, "|CPU Frequency: %" PRIu32 " MHz", cpu_freq_mhz);
 
-  char reset_buffer[RESET_REASON_BUFFER_SIZE];
-  char wakeup_buffer[RESET_REASON_BUFFER_SIZE];
-  const char *reset_reason = get_reset_reason_(std::span<char, RESET_REASON_BUFFER_SIZE>(reset_buffer));
-  const char *wakeup_cause = get_wakeup_cause_(std::span<char, RESET_REASON_BUFFER_SIZE>(wakeup_buffer));
+  char reason_buffer[RESET_REASON_BUFFER_SIZE];
+  const char *reset_reason = get_reset_reason_(std::span<char, RESET_REASON_BUFFER_SIZE>(reason_buffer));
+  const char *wakeup_cause = get_wakeup_cause_(std::span<char, RESET_REASON_BUFFER_SIZE>(reason_buffer));
 
   uint8_t mac[6];
   get_mac_address_raw(mac);
