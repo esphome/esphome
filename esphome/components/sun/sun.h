d914f423d3ce873cae4847770e6b5137f41e7ada
Pat Satyshur <patrick@satyshur.net>
2026-03-15 19:34:27 -0500

    Add function to read the offset.

--- esphome/components/sun/sun.h
+++ esphome/components/sun/sun.h
@@ -59,6 +59,7 @@ class Sun {
   void set_latitude(double latitude) { location_.latitude = latitude; }
   void set_longitude(double longitude) { location_.longitude = longitude; }
   void set_offset(int offset) { offset_ = offset; }
+  int get_offset() {return this->offset_;}
 
   // Check if the sun is above the horizon, with a default elevation angle of -0.83333 (standard for sunrise/set).
   bool is_above_horizon(double elevation = -0.83333) { return this->elevation() > elevation; }
