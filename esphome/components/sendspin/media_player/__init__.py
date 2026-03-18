3890374858328cd50c349b305e2c0868afc60d97
Kevin Ahrendt <kahrendt@gmail.com>
2026-03-18 17:48:34 -0400

    cleanup sendspin media player python

--- esphome/components/sendspin/media_player/__init__.py
+++ esphome/components/sendspin/media_player/__init__.py
@@ -3,20 +3,14 @@
 import esphome.codegen as cg
 from esphome.components import media_player
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_SPEAKER, CONF_TASK_STACK_IN_PSRAM
+from esphome.const import CONF_ID
 
 from .. import CONF_SENDSPIN_ID, SendspinHub, sendspin_ns
 
-# media_player is needed to control the stream
-AUTO_LOAD = ["audio", "media_player"]
 CODEOWNERS = ["@kahrendt"]
 DEPENDENCIES = ["sendspin"]
 
-CONF_CONTROL_ONLY = "control_only"
-CONF_ON_SERVER_SETTINGS = "on_server_settings"
 CONF_VOLUME_INCREMENT = "volume_increment"
-CONF_VOLUME_MIN = "volume_min"
-CONF_VOLUME_MAX = "volume_max"
 
 
 SendspinMediaPlayer = sendspin_ns.class_(
@@ -30,24 +24,6 @@ CONFIG_SCHEMA = media_player.media_player_schema(SendspinMediaPlayer).extend(
         cv.GenerateID(): cv.declare_id(SendspinMediaPlayer),
         cv.GenerateID(CONF_SENDSPIN_ID): cv.use_id(SendspinHub),
         cv.Optional(CONF_VOLUME_INCREMENT, default=5): cv.int_range(min=1, max=50),
-        cv.Optional(CONF_CONTROL_ONLY): cv.invalid(
-            f"The {CONF_CONTROL_ONLY} option is deprecated, see https://github.com/esphome/esphome/pull/12284 for latest changes"
-        ),
-        cv.Optional(CONF_ON_SERVER_SETTINGS): cv.invalid(
-            f"The {CONF_ON_SERVER_SETTINGS} option is deprecated, see https://github.com/esphome/esphome/pull/12284 for latest changes"
-        ),
-        cv.Optional(CONF_SPEAKER): cv.invalid(
-            f"The {CONF_SPEAKER} option is deprecated, see https://github.com/esphome/esphome/pull/12284 for latest changes"
-        ),
-        cv.Optional(CONF_TASK_STACK_IN_PSRAM): cv.invalid(
-            f"The {CONF_TASK_STACK_IN_PSRAM} option is deprecated, see https://github.com/esphome/esphome/pull/12284 for latest changes"
-        ),
-        cv.Optional(CONF_VOLUME_MIN): cv.invalid(
-            f"The {CONF_VOLUME_MIN} option is deprecated, see https://github.com/esphome/esphome/pull/12284 for latest changes"
-        ),
-        cv.Optional(CONF_VOLUME_MAX): cv.invalid(
-            f"The {CONF_VOLUME_MAX} option is deprecated, see https://github.com/esphome/esphome/pull/12284 for latest changes"
-        ),
     }
 )
 
