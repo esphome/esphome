42e1d933989cad1299eb5095e9614bdcd41d8a76
J. Nick Koston <nick@home-assistant.io>
2026-03-22 13:57:08 -1000

    cleanup

--- esphome/core/helpers.h
+++ esphome/core/helpers.h
@@ -11,11 +11,9 @@
 #include <iterator>
 #include <limits>
 #include <memory>
-#include <new>
 #include <span>
 #include <string>
 #include <type_traits>
-#include <utility>
 #include <vector>
 #include <concepts>
 #include <strings.h>
