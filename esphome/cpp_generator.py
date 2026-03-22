85c970e1ddb46e8d6817de65664afe56c84ef7a7
J. Nick Koston <nick@home-assistant.io>
2026-03-22 11:23:46 -1000

    globals is special

--- esphome/cpp_generator.py
+++ esphome/cpp_generator.py
@@ -600,7 +600,11 @@ def Pvariable(id_: ID, rhs: SafeExpType, type_: "MockObj" = None) -> "MockObj":
         )
         # Extract args from the CallExpression and rebuild as placement new
         call_expr = rhs.base  # CallExpression("new Type", args...)
-        placement_new = CallExpression(f"new({id_.id}) {the_type}", *call_expr.args)
+        placement_args: list[Expression] = []
+        if call_expr.template_args is not None:
+            placement_args.append(call_expr.template_args)
+        placement_args.extend(call_expr.args)
+        placement_new = CallExpression(f"new({id_.id}) {the_type}", *placement_args)
         CORE.add(ExpressionStatement(placement_new))
     else:
         decl = VariableDeclarationExpression(id_.type, "*", id_, static=True)
