047a744ed387a9e4d5f702fca83ae48e5d38b524
J. Nick Koston <nick@home-assistant.io>
2026-03-22 16:02:51 -1000

    [binary_sensor] Inline send_state_internal into header

--- esphome/components/binary_sensor/binary_sensor.cpp
+++ esphome/components/binary_sensor/binary_sensor.cpp
@@ -32,10 +32,6 @@ void BinarySensor::publish_initial_state(bool new_state) {
   this->invalidate_state();
   this->publish_state(new_state);
 }
-void BinarySensor::send_state_internal(bool new_state) {
-  // set_new_state handles de-duplication, updating this->state via set_state_value_(), and triggering callbacks
-  this->set_new_state(new_state);
-}
 
 bool BinarySensor::set_new_state(const optional<bool> &new_state) {
   if (StatefulEntityBase::set_new_state(new_state)) {
