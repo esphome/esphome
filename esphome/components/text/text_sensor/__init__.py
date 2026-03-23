bb23baf4d095edb14717f3a3200f63eeed29e0a5
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-03-23 13:18:47 +1100

    Cleanup

--- esphome/components/text/text_sensor/__init__.py
+++ esphome/components/text/text_sensor/__init__.py
@@ -1,11 +1,9 @@
 import esphome.codegen as cg
 from esphome.components import text_sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_SOURCE_ID
+from esphome.const import CONF_SOURCE_ID
 
 from .. import Text, text_ns
-
-CODEOWNERS = ["@clydebarrow"]
 
 TextTextSensor = text_ns.class_("TextTextSensor", text_sensor.TextSensor, cg.Component)
 
@@ -23,6 +21,5 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     source = await cg.get_variable(config[CONF_SOURCE_ID])
-    var = cg.new_Pvariable(config[CONF_ID], source)
-    await text_sensor.register_text_sensor(var, config)
+    var = await text_sensor.new_text_sensor(config, source)
     await cg.register_component(var, config)
