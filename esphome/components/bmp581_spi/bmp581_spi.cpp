c5c9cfe79434c5e69ad6ae77542fcd5c4121d73e
Daniel Kent <git@danielkent.net>
2026-03-23 15:32:28 -0400

    [bmp581_spi] Multiple improvements
     - Added dump_config() override with new LOG_SPI_COMPONENT
     - Include formatting uses quotes
     - Convert bitmask functions to inline equivalents for R/W bits
     - Explicitly added public/protected keywords in header

--- esphome/components/bmp581_spi/bmp581_spi.cpp
+++ esphome/components/bmp581_spi/bmp581_spi.cpp
@@ -2,20 +2,22 @@
 #include <cstddef>
 
 #include "bmp581_spi.h"
-#include <esphome/components/bmp581_base/bmp581_base.h>
+#include "esphome/components/bmp581_base/bmp581_base.h"
+#include "esphome/components/spi/spi.h"
 
 namespace esphome::bmp581_spi {
 
 static const char *const TAG = "bmp581_spi";
 
-uint8_t set_bit(uint8_t num, uint8_t position) {
-  uint8_t mask = 1 << position;
-  return num | mask;
-}
+// OR (|) register with BMP_SPI_READ for read
+inline constexpr uint8_t BMP_SPI_READ = 0x80;
 
-uint8_t clear_bit(uint8_t num, uint8_t position) {
-  uint8_t mask = 1 << position;
-  return num & ~mask;
+// AND (&) register with BMP_SPI_WRITE for write
+inline constexpr uint8_t BMP_SPI_WRITE = 0x7F;
+
+void BMP581SPIComponent::dump_config() {
+  BMP581Component::dump_config();
+  LOG_SPI_DEVICE(this);
 }
 
 void BMP581SPIComponent::setup() {
@@ -33,11 +35,13 @@ void BMP581SPIComponent::activate_protocol() {
 // and replaced by a read/write bit (RW = ‘0’ for write and RW = ‘1’ for read).
 // Example: address 0xF7 is accessed by using SPI register address 0x77. For write access, the byte
 // 0x77 is transferred, for read access, the byte 0xF7 is transferred.
+// The expressions BMP_SPI_READ (| with register) and BMP_SPI_WRITE (& with register)
+// are defined for readability.
 // https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bmp581-ds004.pdf
 
 bool BMP581SPIComponent::bmp_read_byte(uint8_t a_register, uint8_t *data) {
   this->enable();
-  this->transfer_byte(set_bit(a_register, 7));
+  this->transfer_byte(a_register | BMP_SPI_READ);
   *data = this->transfer_byte(0);
   this->disable();
   return true;
@@ -45,7 +49,7 @@ bool BMP581SPIComponent::bmp_read_byte(uint8_t a_register, uint8_t *data) {
 
 bool BMP581SPIComponent::bmp_write_byte(uint8_t a_register, uint8_t data) {
   this->enable();
-  this->transfer_byte(clear_bit(a_register, 7));
+  this->transfer_byte(a_register & BMP_SPI_WRITE);
   this->transfer_byte(data);
   this->disable();
   return true;
@@ -53,7 +57,7 @@ bool BMP581SPIComponent::bmp_write_byte(uint8_t a_register, uint8_t data) {
 
 bool BMP581SPIComponent::bmp_read_bytes(uint8_t a_register, uint8_t *data, size_t len) {
   this->enable();
-  this->transfer_byte(set_bit(a_register, 7));
+  this->transfer_byte(a_register | BMP_SPI_READ);
   this->read_array(data, len);
   this->disable();
   return true;
@@ -61,7 +65,7 @@ bool BMP581SPIComponent::bmp_read_bytes(uint8_t a_register, uint8_t *data, size_
 
 bool BMP581SPIComponent::bmp_write_bytes(uint8_t a_register, uint8_t *data, size_t len) {
   this->enable();
-  this->transfer_byte(set_bit(a_register, 7));
+  this->transfer_byte(a_register & BMP_SPI_WRITE);
   this->write_array(data, len);
   this->disable();
   return true;
--- esphome/components/bmp581_spi/bmp581_spi.h
+++ esphome/components/bmp581_spi/bmp581_spi.h
@@ -9,11 +9,15 @@ namespace esphome::bmp581_spi {
 class BMP581SPIComponent : public esphome::bmp581_base::BMP581Component,
                            public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_HIGH,
                                                  spi::CLOCK_PHASE_TRAILING, spi::DATA_RATE_200KHZ> {
+ public:
   void setup() override;
   bool bmp_read_byte(uint8_t a_register, uint8_t *data) override;
   bool bmp_write_byte(uint8_t a_register, uint8_t data) override;
   bool bmp_read_bytes(uint8_t a_register, uint8_t *data, size_t len) override;
   bool bmp_write_bytes(uint8_t a_register, uint8_t *data, size_t len) override;
+  void dump_config() override;
+
+ protected:
   void activate_protocol() override;
 };
 
