710ea497e09ab7b3df1ce8fd5e25d70e081c2e44
J. Nick Koston <nick+github@koston.org>
2026-03-24 09:12:34 -1000

    Apply suggestion from @bdraco

--- esphome/core/progmem.h
+++ esphome/core/progmem.h
@@ -105,6 +105,8 @@ struct LogString;
     static const char *get_(uint8_t idx, uint8_t fallback) { \
       if (idx >= COUNT) \
         idx = fallback; \
+       // std::launder is used here to prevent the inter-procedural analysis that
+       // causes the false positive that the string is not null terminated
       return std::launder(&BLOB[::esphome::progmem_read_byte(&OFFSETS[idx])]); \
     } \
     static ::ProgmemStr get_progmem_str(uint8_t idx, uint8_t fallback) { \
