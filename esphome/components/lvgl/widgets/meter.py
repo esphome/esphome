9b377abd17b5691604618224b0f033dc52714e67
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-03-24 17:18:37 +1100

    [lvgl] Fix meter drawing without ticks

--- esphome/components/lvgl/widgets/meter.py
+++ esphome/components/lvgl/widgets/meter.py
@@ -79,6 +79,7 @@ from ..types import (
 from . import Widget, WidgetType, get_widgets, widget_to_code
 from .arc import CONF_ARC
 from .img import CONF_IMAGE
+from .label import CONF_LABEL
 from .line import CONF_LINE
 
 CONF_ANGLE_RANGE = "angle_range"
@@ -227,7 +228,7 @@ SCALE_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(lv_scale_t),
         cv.Optional(CONF_TICKS): cv.Schema(
             {
-                cv.Optional(CONF_COUNT, default=12): cv.positive_int,
+                cv.Optional(CONF_COUNT, default=12): cv.int_range(min=2),
                 cv.Optional(CONF_WIDTH, default=2): cv.positive_int,
                 cv.Optional(CONF_LENGTH, default=10): size,
                 cv.Optional(CONF_RADIAL_OFFSET, default=0): size,
@@ -329,7 +330,7 @@ class MeterType(WidgetType):
         )
 
     def get_uses(self):
-        return CONF_SCALE, CONF_LINE, CONF_IMAGE
+        return CONF_SCALE, CONF_LINE, CONF_IMAGE, CONF_LABEL
 
     def validate(self, value):
         return cv.has_at_most_one_key(CONF_INDICATOR, CONF_PIVOT)(value)
@@ -478,6 +479,8 @@ class MeterType(WidgetType):
                     await iw.set_property(CONF_SRC, await lv_image.process(src))
                     await set_indicator_values(iw, v)
 
+            # Hide the scale line
+            lv.obj_set_style_arc_opa(scale_var, LV_OPA.TRANSP, LV_PART.MAIN)
             if ticks := scale_conf.get(CONF_TICKS):
                 # Set total tick count
                 lv.scale_set_total_tick_count(scale_var, ticks[CONF_COUNT])
@@ -503,8 +506,6 @@ class MeterType(WidgetType):
                     LV_PART.ITEMS,
                 )
 
-                # Hide the scale line
-                lv.obj_set_style_arc_opa(scale_var, LV_OPA.TRANSP, LV_PART.MAIN)
                 if CONF_MAJOR in ticks:
                     major = ticks[CONF_MAJOR]
                     # Set major tick frequency
@@ -547,7 +548,11 @@ class MeterType(WidgetType):
                 else:
                     lv.scale_set_major_tick_every(scale_var, 0)
             else:
-                lv.scale_set_total_tick_count(scale_var, 0)
+                # Must have at least 2 ticks otherwise the scale isn't even drawn
+                lv.scale_set_total_tick_count(scale_var, 2)
+                # Hide the ticks by making them 0 width
+                lv_obj.set_style_line_width(scale_var, 0, LV_PART.ITEMS)
+                lv.scale_set_major_tick_every(scale_var, 0)
 
         # Add a pivot
         # Get the default style
