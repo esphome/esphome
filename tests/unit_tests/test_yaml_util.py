479d33e10e1d7750e574ca9e3f94baac663e88ee
Javier Peletier <jm@epiclabs.io>
2026-03-25 19:28:20 +0100

    fix tests

--- tests/unit_tests/test_yaml_util.py
+++ tests/unit_tests/test_yaml_util.py
@@ -74,7 +74,9 @@ def test_parsing_with_custom_loader(fixture_path):
         loader_calls.append(fname)
 
     with yaml_file.open(encoding="utf-8") as f_handle:
-        yaml_util.parse_yaml(yaml_file, f_handle, custom_loader)
+        config = yaml_util.parse_yaml(yaml_file, f_handle, custom_loader)
+        # substite config to expand includes:
+        substitutions.substitute(config, [], {}, False)
 
     assert len(loader_calls) == 3
     assert loader_calls[0].parts[-2:] == ("includes", "included.yaml")
