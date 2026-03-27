1c054db1f1e7e7e5efe69d7a4a8fd47de43fa536
J. Nick Koston <nick@home-assistant.io>
2026-03-26 15:26:51 -1000

    Fix to_code signature: add ConfigType annotation

--- tests/integration/fixtures/external_components/legacy_climate_component/climate/__init__.py
+++ tests/integration/fixtures/external_components/legacy_climate_component/climate/__init__.py
@@ -3,6 +3,7 @@
 import esphome.codegen as cg
 from esphome.components import climate
 import esphome.config_validation as cv
+from esphome.types import ConfigType
 
 legacy_climate_ns = cg.esphome_ns.namespace("legacy_climate_test")
 LegacyClimate = legacy_climate_ns.class_("LegacyClimate", climate.Climate, cg.Component)
@@ -10,6 +11,6 @@ LegacyClimate = legacy_climate_ns.class_("LegacyClimate", climate.Climate, cg.Co
 CONFIG_SCHEMA = climate.climate_schema(LegacyClimate).extend(cv.COMPONENT_SCHEMA)
 
 
-async def to_code(config):
+async def to_code(config: ConfigType) -> None:
     var = await climate.new_climate(config)
     await cg.register_component(var, config)
