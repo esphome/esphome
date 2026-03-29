2c5b2b1aa84c6579ef2cf5622ff9f7cf2e0793f3
Jake <106696989+JakeLC15@users.noreply.github.com>
2026-03-29 12:07:17 -0400

    Add percentage sensor configuration for DS1603L

--- esphome/components/ds1603l/sensor.py
+++ esphome/components/ds1603l/sensor.py
@@ -9,6 +9,7 @@ Ds1603l = ds1603l_ns.class_("Ds1603l", cg.PollingComponent, uart.UARTDevice)
 
 CONF_DS1603L_LIQUID_LEVEL = "ds1603l_liquid_level"
 CONF_DS1603L_LIQUID_VOLUME = "ds1603l_liquid_volume"
+CONF_DS1603L_PERCENTAGE = "ds1603l_percentage"
 CONF_DS1603L_MIN_VOLUME = "ds1603l_min_volume"
 CONF_DS1603L_MAX_VOLUME = "ds1603l_max_volume"
 CONF_DS1603L_MIN_LEVEL = "ds1603l_min_level"
@@ -16,17 +17,26 @@ CONF_DS1603L_MAX_LEVEL = "ds1603l_max_level"
 
 # Configuration schema for ds1603l
 CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend(
+
     {
         cv.GenerateID(): cv.declare_id(Ds1603l),
         cv.Optional(CONF_DS1603L_LIQUID_LEVEL): sensor.sensor_schema(
             unit_of_measurement=UNIT_EMPTY,  # Unit is customizable in YAML
-            icon=ICON_EMPTY,  # Icon is customizable in YAML
-            accuracy_decimals=0,  # Default to 0 decimal place
+            icon=ICON_EMPTY,                # Icon is customizable in YAML
+            accuracy_decimals=0,            #Default to 0 decimal place
+            state_class="measurement",
         ),
         cv.Optional(CONF_DS1603L_LIQUID_VOLUME): sensor.sensor_schema(
             unit_of_measurement=UNIT_EMPTY,  # Unit is customizable in YAML
-            icon=ICON_EMPTY,  # Icon is customizable in YAML
-            accuracy_decimals=0,  # Default to 0 decimal place
+            icon=ICON_EMPTY,                # Icon is customizable in YAML
+            accuracy_decimals=0,            # Default to 0 decimal place
+            state_class="measurement",
+        ),
+        cv.Optional(CONF_DS1603L_PERCENTAGE): sensor.sensor_schema(
+            unit_of_measurement="%",
+            icon=ICON_EMPTY,                # Icon is customizable in YAML
+            accuracy_decimals=0,            # Default to 0 decimal place
+            state_class="measurement",
         ),
         cv.Optional(CONF_DS1603L_MIN_VOLUME, default=0.0): cv.float_,
         cv.Optional(CONF_DS1603L_MAX_VOLUME, default=1000.0): cv.float_,
@@ -34,7 +44,6 @@ CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend(
         cv.Optional(CONF_DS1603L_MAX_LEVEL, default=1000.0): cv.float_,
     }
 ).extend(cv.polling_component_schema("60s"))  # Default update interval is 60s
-
 
 # Generate the C++ code from YAML
 async def to_code(config):
@@ -48,6 +57,9 @@ async def to_code(config):
     if CONF_DS1603L_LIQUID_VOLUME in config:
         sens = await sensor.new_sensor(config[CONF_DS1603L_LIQUID_VOLUME])
         cg.add(var.set_ds1603l_liquid_volume_sensor(sens))
+    if CONF_DS1603L_PERCENTAGE in config:
+        sens = await sensor.new_sensor(config[CONF_DS1603L_PERCENTAGE])
+        cg.add(var.set_ds1603l_percentage_sensor(sens))
     if CONF_DS1603L_MIN_VOLUME in config:
         cg.add(var.set_ds1603l_min_volume(config[CONF_DS1603L_MIN_VOLUME]))
     if CONF_DS1603L_MAX_VOLUME in config:
