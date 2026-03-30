3ccaf952ed9011fb649434fb7a1097e42c7c3660
guillempages <git@guillempages.cat>
2026-03-30 21:40:04 +0200

    [tm1637] Add set_brightness method
    Add a method to set the display brightness as a percentage of total
    brightness.
    In addition to the already existing "set_intensity" method; which
    accepts the intensity level but requires the knowledge of how many
    levels the devices supports, a new "set_brightness" method accepting a
    float will allow a normalized brigntess to be set.
    
    Additionally setting a brightness of "0" will turn off the display, and
    setting any value > 0 will turn on the display (the current
    set_intensity method is independent of the display on/off)

--- esphome/components/tm1637/tm1637.cpp
+++ esphome/components/tm1637/tm1637.cpp
@@ -347,6 +347,18 @@ uint8_t TM1637Display::print(uint8_t start_pos, const char *str) {
   }
   return pos - start_pos;
 }
+
+void TM1637Display::set_brightness(float brightness) {
+  auto intensity = brightness * 7;
+  this->set_on(brightness > 0);
+  if (intensity < 0) {
+    intensity = 0;
+  } else if (intensity > 7) {
+    intensity = 7;
+  }
+  this->set_intensity(intensity);
+}
+
 uint8_t TM1637Display::print(const char *str) { return this->print(0, str); }
 
 void TM1637Display::set_buffer(const uint8_t *data, uint8_t length) {
