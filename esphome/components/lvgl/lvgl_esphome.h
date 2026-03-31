9639d8491108226cd5a6a45f8d1926df2777c3e6
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-03-31 21:19:56 +1100

    [lvgl] Implement bitmap mask for images

--- esphome/components/lvgl/lvgl_esphome.h
+++ esphome/components/lvgl/lvgl_esphome.h
@@ -78,6 +78,10 @@ inline void lv_image_set_src(lv_obj_t *obj, esphome::image::Image *image) {
   lv_image_set_src(obj, image->get_lv_image_dsc());
 }
 
+inline void lv_obj_set_style_bitmap_mask_src(lv_obj_t *obj, image::Image *image, lv_style_selector_t selector) {
+  lv_obj_set_style_bitmap_mask_src(obj, image->get_lv_image_dsc(), selector);
+}
+
 inline void lv_obj_set_style_bg_image_src(lv_obj_t *obj, esphome::image::Image *image, lv_style_selector_t selector) {
   lv_obj_set_style_bg_image_src(obj, image->get_lv_image_dsc(), selector);
 }
