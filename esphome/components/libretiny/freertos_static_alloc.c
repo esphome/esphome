249a4bdcf039bc735102b73ebad4f094f1b323d9
J. Nick Koston <nick@home-assistant.io>
2026-03-31 22:27:19 -1000

    Add configASSERT checks for pvPortMalloc failures
    Matches ESP-IDF's assert pattern for the static allocation callbacks.

--- esphome/components/libretiny/freertos_static_alloc.c
+++ esphome/components/libretiny/freertos_static_alloc.c
@@ -23,6 +23,8 @@ void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackTyp
    * so the stack does not grow into the TCB. */
   StackType_t *stack = (StackType_t *) pvPortMalloc(configMINIMAL_STACK_SIZE * sizeof(StackType_t));
   StaticTask_t *tcb = (StaticTask_t *) pvPortMalloc(sizeof(StaticTask_t));
+  configASSERT(stack != NULL);
+  configASSERT(tcb != NULL);
 
   *ppxIdleTaskTCBBuffer = tcb;
   *ppxIdleTaskStackBuffer = stack;
@@ -35,6 +37,8 @@ void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackT
                                     uint32_t *pulTimerTaskStackSize) {
   StackType_t *stack = (StackType_t *) pvPortMalloc(configTIMER_TASK_STACK_DEPTH * sizeof(StackType_t));
   StaticTask_t *tcb = (StaticTask_t *) pvPortMalloc(sizeof(StaticTask_t));
+  configASSERT(stack != NULL);
+  configASSERT(tcb != NULL);
 
   *ppxTimerTaskTCBBuffer = tcb;
   *ppxTimerTaskStackBuffer = stack;
