cc749d5039ad54a0794b6d4cb8fc02616948badd
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2026-04-02 09:54:12 -0400

    Dedup

--- esphome/components/zigbee/zigbee_zephyr.cpp
+++ esphome/components/zigbee/zigbee_zephyr.cpp
@@ -255,22 +255,25 @@ void ZigbeeComponent::factory_reset() {
   ZB_SCHEDULE_APP_CALLBACK(zb_bdb_reset_via_local_action, 0);
 }
 
+static void log_reporting_info_(zb_zcl_reporting_info_t *rep_info) {
+  auto now = millis();
+  ESP_LOGD(TAG, "Reporting: endpoint %d, cluster_id 0x%04X, attr_id 0x%04X, flags 0x%02X, report in %ums", rep_info->ep,
+           rep_info->cluster_id, rep_info->attr_id, rep_info->flags,
+           ZB_ZCL_GET_REPORTING_FLAG(rep_info, ZB_ZCL_REPORT_TIMER_STARTED)
+               ? ZB_TIME_BEACON_INTERVAL_TO_MSEC(rep_info->run_time) - now
+               : 0);
+  ESP_LOGD(TAG, "  min_interval %ds, max_interval %ds, def_min_interval %ds, def_max_interval %ds",
+           rep_info->u.send_info.min_interval, rep_info->u.send_info.max_interval,
+           rep_info->u.send_info.def_min_interval, rep_info->u.send_info.def_max_interval);
+}
+
 void ZigbeeComponent::dump_reporting_() {
 #ifdef ESPHOME_LOG_HAS_VERBOSE
-  auto now = millis();
   for (zb_uint8_t j = 0; j < ZCL_CTX().device_ctx->ep_count; j++) {
     if (ZCL_CTX().device_ctx->ep_desc_list[j]->reporting_info) {
       zb_zcl_reporting_info_t *rep_info = ZCL_CTX().device_ctx->ep_desc_list[j]->reporting_info;
       for (zb_uint8_t i = 0; i < ZCL_CTX().device_ctx->ep_desc_list[j]->rep_info_count; i++) {
-        ESP_LOGV(TAG,
-                 "Endpoint: %d, cluster_id %d, attr_id %d, flags %d, report in %ums\n"
-                 "  Min_interval %ds, max_interval %ds, def_min_interval %ds, def_max_interval %ds",
-                 rep_info->ep, rep_info->cluster_id, rep_info->attr_id, rep_info->flags,
-                 ZB_ZCL_GET_REPORTING_FLAG(rep_info, ZB_ZCL_REPORT_TIMER_STARTED)
-                     ? ZB_TIME_BEACON_INTERVAL_TO_MSEC(rep_info->run_time) - now
-                     : 0,
-                 rep_info->u.send_info.min_interval, rep_info->u.send_info.max_interval,
-                 rep_info->u.send_info.def_min_interval, rep_info->u.send_info.def_max_interval);
+        log_reporting_info_(rep_info);
         rep_info++;
       }
     }
@@ -291,16 +294,7 @@ void ZigbeeComponent::after_reporting_info(zb_zcl_configure_reporting_req_t *con
              attr_addr_info->manuf_code);
     return;
   }
-  auto now = millis();
-  ESP_LOGD(TAG,
-           "Endpoint: %d, cluster_id %d, attr_id %d, flags %d, report in %ums\n"
-           "  Min_interval %ds, max_interval %ds, def_min_interval %ds, def_max_interval %ds",
-           rep_info->ep, rep_info->cluster_id, rep_info->attr_id, rep_info->flags,
-           ZB_ZCL_GET_REPORTING_FLAG(rep_info, ZB_ZCL_REPORT_TIMER_STARTED)
-               ? ZB_TIME_BEACON_INTERVAL_TO_MSEC(rep_info->run_time) - now
-               : 0,
-           rep_info->u.send_info.min_interval, rep_info->u.send_info.max_interval,
-           rep_info->u.send_info.def_min_interval, rep_info->u.send_info.def_max_interval);
+  log_reporting_info_(rep_info);
 #endif
 }
 
