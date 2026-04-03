b62bea51317fec53c9cd6fc4bacbcc834f867272
Edvard Filistovič <efilistovic@gmail.com>
2026-04-03 14:04:06 +0300

    internal_temperature: add constexpr for magic numbers, remove unused TAG

--- esphome/components/internal_temperature/internal_temperature_ln882x.cpp
+++ esphome/components/internal_temperature/internal_temperature_ln882x.cpp
@@ -10,13 +10,13 @@ uint16_t hal_adc_get_data(uint32_t adc_base, uint32_t ch);
 namespace esphome {
 namespace internal_temperature {
 
-static const char *const TAG = "internal_temperature.ln882x";
-
 void InternalTemperatureSensor::update() {
   // Read from internal temperature sensor via ADC channel 0.
   // ADC_BASE = 0x40000800, ADC_CH0 = 1 (bit 0 set).
+  static constexpr float ADC_TEMP_SCALE = 2.54f;
+  static constexpr float ADC_TEMP_OFFSET = 278.15f;
   uint16_t raw = hal_adc_get_data(0x40000800U, 1U);
-  float temperature = (raw & 0xFFF) / 2.54f - 278.15f;
+  float temperature = (raw & 0xFFF) / ADC_TEMP_SCALE - ADC_TEMP_OFFSET;
   this->publish_state(temperature);
 }
 
