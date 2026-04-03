3c591bf8ccce8a2e31e23f16465c3c10ad0a0b88
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-04-03 21:08:27 +1100

    [ili9xxx, st7735] Add deprecation warnings

--- esphome/components/ili9xxx/display.py
+++ esphome/components/ili9xxx/display.py
@@ -219,6 +219,9 @@ FINAL_VALIDATE_SCHEMA = final_validate
 
 
 async def to_code(config):
+    LOGGER.warning(
+        "The 'ili9xxx' component is deprecated, it is recommended to use 'mipi_spi' instead."
+    )
     rhs = MODELS[config[CONF_MODEL]].new()
     var = cg.Pvariable(config[CONF_ID], rhs)
 
