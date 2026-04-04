d3586c76baf652cbc289426d947929d9ef03fa8a
J. Nick Koston <nick@home-assistant.io>
2026-04-04 10:52:24 -1000

    Deduplicate wakeable_delay call in yield_with_select_

--- esphome/core/application.h
+++ esphome/core/application.h
@@ -915,10 +915,8 @@ inline void ESPHOME_ALWAYS_INLINE Application::yield_with_select_(uint32_t delay
 
   // Sleep with instant wake via FreeRTOS task notification.
   // Woken by: callback wrapper (socket data), wake_loop_threadsafe() (background tasks), or timeout.
-  esphome::wakeable_delay(delay_ms);
-#else
-  esphome::wakeable_delay(delay_ms);
 #endif
+  esphome::wakeable_delay(delay_ms);
 }
 #endif  // !USE_SOCKET_SELECT_SUPPORT
 
