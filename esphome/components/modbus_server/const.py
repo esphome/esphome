1be6e643c86e362bd05c0880251aac32002482d2
Bonne Eggleston <bonne@exciton.com.au>
2026-04-07 03:09:15 +0000

    [modbus] Split modbus_server from modbus_controller

---
+++ esphome/components/modbus_server/const.py
@@ -0,0 +1,5 @@
+CONF_ENABLED = "enabled"
+CONF_REGISTER_LAST_ADDRESS = "register_last_address"
+CONF_REGISTER_VALUE = "register_value"
+CONF_VALUE_TYPE = "value_type"
+CONF_SERVER_COURTESY_RESPONSE = "server_courtesy_response"
