de20f28ed6bdde43896f87653cc5258145a4bb25
Jasper van Gelder <jvgelder@users.noreply.github.com>
2026-04-09 00:57:04 +0200

    chore(csi_camera): Add csi camera component

--- esphome/core/defines.h
+++ esphome/core/defines.h
@@ -47,6 +47,7 @@
 #define USE_BLE_DEVICE_IRK
 #define USE_BUTTON
 #define USE_CAMERA
+#define USE_CSI_CAMERA
 #define USE_CLIMATE
 #define USE_CLIMATE_VISUAL_OVERRIDES
 #define USE_CONTROLLER_REGISTRY
@@ -460,7 +461,7 @@
 #define USE_SHD_FIRMWARE_MINOR_VERSION 5
 // clang-format off
 #define USE_SHD_FIRMWARE_DATA \
-  {}
+{}
 // clang-format on
 
 #define USE_WEBSERVER
