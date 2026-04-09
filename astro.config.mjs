50337422087fce426b230e8e654bd46bcd7f898b
J. Nick Koston <nick@koston.org>
2026-04-08 18:29:03 -1000

    Add version badge to navbar linking to latest changelog (#6421)

--- astro.config.mjs
+++ astro.config.mjs
@@ -172,6 +172,7 @@ export default defineConfig({
       components: {
         Footer: "./src/components/Footer.astro",
         Head: "./src/components/Head.astro",
+        SiteTitle: "./src/components/SiteTitle.astro",
       },
       customCss: ["./src/styles/custom.css", "katex/dist/katex.min.css"],
       sidebar: [
