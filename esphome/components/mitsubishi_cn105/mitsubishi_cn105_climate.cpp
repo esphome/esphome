e11a50dcc1b0dabd913a94a15efcd6952ac0f81e
Boris Krivonog <boris.krivonog@inova.si>
2026-04-09 11:52:35 +0200

    more descriptive log

--- esphome/components/mitsubishi_cn105/mitsubishi_cn105_climate.cpp
+++ esphome/components/mitsubishi_cn105/mitsubishi_cn105_climate.cpp
@@ -62,9 +62,9 @@ void MitsubishiCN105Climate::dump_config() {
   }
   ESP_LOGCONFIG(TAG,
                 "  Update interval: %" PRIu32 " ms\n"
-                "  Temperature mapping: %s\n"
+                "  Use Fahrenheit: %s\n"
                 "  UART: baud_rate=%" PRIu32 " data_bits=%u parity=%s stop_bits=%u",
-                this->hp_.get_update_interval(), this->temperature_mapping_.get_fahrenheit() ? "F" : "C",
+                this->hp_.get_update_interval(), YESNO(this->temperature_mapping_.get_fahrenheit()),
                 this->parent_->get_baud_rate(), this->parent_->get_data_bits(),
                 LOG_STR_ARG(parity_to_str(this->parent_->get_parity())), this->parent_->get_stop_bits());
 }
