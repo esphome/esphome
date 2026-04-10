f10f7c6b6a60582f9a33f2516e8ce9b0cac24bf1
J. Nick Koston <nick@home-assistant.io>
2026-04-09 14:46:50 -1000

    [core] Use ENTITY_TYPES_H_TARGET constant for consistency

--- esphome/writer.py
+++ esphome/writer.py
@@ -171,6 +171,7 @@ VERSION_H_FORMAT = """\
 DEFINES_H_TARGET = "esphome/core/defines.h"
 VERSION_H_TARGET = "esphome/core/version.h"
 BUILD_INFO_DATA_H_TARGET = "esphome/core/build_info_data.h"
+ENTITY_TYPES_H_TARGET = "esphome/core/entity_types.h"
 ESPHOME_README_TXT = """
 THIS DIRECTORY IS AUTO-GENERATED, DO NOT MODIFY
 
@@ -198,7 +199,7 @@ def copy_src_tree():
     # Build #include list for esphome.h
     # X-macro files are included multiple times with different macro definitions
     # and must not be included bare in esphome.h
-    esphome_h_exclude = {Path("esphome/core/entity_types.h")}
+    esphome_h_exclude = {Path(ENTITY_TYPES_H_TARGET)}
     include_l = []
     for target, _ in source_files_l:
         if target.suffix in HEADER_FILE_EXTENSIONS and target not in esphome_h_exclude:
