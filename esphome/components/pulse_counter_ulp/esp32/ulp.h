455f89f6cab4f36a4c548d069a2519235f71badb
brisk <bskblake@gmail.com>
2026-04-11 13:37:50 +0930

    Dummy file pass linting

--- esphome/components/pulse_counter_ulp/esp32/ulp.h
+++ esphome/components/pulse_counter_ulp/esp32/ulp.h
@@ -1,2 +1,9 @@
 // This file has been created to satisfy clang-tidy, but is included with the
 // esp-idf tools
+
+#pragma once
+
+// Required to pass linting
+namespace esphome {
+namespace pulse_counter_ulp {}  // namespace pulse_counter_ulp
+}  // namespace esphome
