f5287806e1339085ee3619535c298855bb869c11
Rapsssito <contact@rodrigomartin.dev>
2026-04-14 14:47:30 +0200

    Improve clarity of flags

--- esphome/components/bq25186/binary_sensor/__init__.py
+++ esphome/components/bq25186/binary_sensor/__init__.py
@@ -2,12 +2,11 @@ import esphome.codegen as cg
 from esphome.components import binary_sensor
 import esphome.config_validation as cv
 from esphome.const import (
+    DEVICE_CLASS_BATTERY,
     DEVICE_CLASS_BATTERY_CHARGING,
     DEVICE_CLASS_OPENING,
     DEVICE_CLASS_PLUG,
-    DEVICE_CLASS_PROBLEM,
     DEVICE_CLASS_RUNNING,
-    DEVICE_CLASS_SAFETY,
 )
 
 from .. import CONF_BQ25186_ID, BQ25186Component, bq25186_ns
@@ -24,23 +23,23 @@ CONF_THERMREG = "thermreg"
 CONF_POWER_GOOD = "power_good"
 CONF_VIN_OVP = "vin_ovp"
 CONF_BATTERY_UVLO = "battery_uvlo"
-CONF_SAFETY_TIMER_FAULT = "safety_timer_fault"
+CONF_SAFETY_TIMER_FLAG = "safety_timer_flag"
 CONF_WAKE1_FLAG = "wake1_flag"
 CONF_WAKE2_FLAG = "wake2_flag"
-CONF_TS_FAULT = "ts_fault"
-CONF_ILIM_FAULT = "ilim_fault"
-CONF_VDPPM_FAULT = "vdppm_fault"
-CONF_VINDPM_FAULT = "vindpm_fault"
+CONF_TS_FLAG = "ts_flag"
+CONF_ILIM_FLAG = "ilim_flag"
+CONF_VDPPM_FLAG = "vdppm_flag"
+CONF_VINDPM_FLAG = "vindpm_flag"
 CONF_THERMREG_ACTIVE = "thermreg_active"
-CONF_VIN_OVP_FAULT = "vin_ovp_fault"
-CONF_BATTERY_UVLO_FAULT = "battery_uvlo_fault"
-CONF_BATTERY_OCP_FAULT = "battery_ocp_fault"
+CONF_VIN_OVP_FLAG = "vin_ovp_flag"
+CONF_BATTERY_UVLO_FLAG = "battery_uvlo_flag"
+CONF_BATTERY_OCP_FLAG = "battery_ocp_flag"
 
 _BINARY_SENSOR_SPECS = (
     # STAT0
     (CONF_TS, "BQ25186TsOpenBinarySensor", DEVICE_CLASS_OPENING),
     (CONF_CHARGING, "BQ25186ChargingBinarySensor", DEVICE_CLASS_BATTERY_CHARGING),
-    (CONF_CHARGE_DONE, "BQ25186ChargeDoneBinarySensor", None),
+    (CONF_CHARGE_DONE, "BQ25186ChargeDoneBinarySensor", DEVICE_CLASS_BATTERY),
     (CONF_ILIM, "BQ25186IlimStatusBinarySensor", DEVICE_CLASS_RUNNING),
     (CONF_VDPPM, "BQ25186VdppmStatusBinarySensor", DEVICE_CLASS_RUNNING),
     (CONF_VINDPM, "BQ25186VindpmStatusBinarySensor", DEVICE_CLASS_RUNNING),
@@ -49,30 +48,18 @@ _BINARY_SENSOR_SPECS = (
     # STAT1
     (CONF_VIN_OVP, "BQ25186VinOvpStatusBinarySensor", DEVICE_CLASS_RUNNING),
     (CONF_BATTERY_UVLO, "BQ25186BatteryUvloStatusBinarySensor", DEVICE_CLASS_RUNNING),
-    (
-        CONF_SAFETY_TIMER_FAULT,
-        "BQ25186SafetyTimerFaultBinarySensor",
-        DEVICE_CLASS_PROBLEM,
-    ),
+    (CONF_SAFETY_TIMER_FLAG, "BQ25186SafetyTimerFlagBinarySensor", None),
     (CONF_WAKE1_FLAG, "BQ25186Wake1FlagBinarySensor", None),
     (CONF_WAKE2_FLAG, "BQ25186Wake2FlagBinarySensor", None),
     # FLAG0
-    (CONF_TS_FAULT, "BQ25186TsFaultBinarySensor", DEVICE_CLASS_PROBLEM),
-    (CONF_ILIM_FAULT, "BQ25186IlimFaultBinarySensor", DEVICE_CLASS_PROBLEM),
-    (CONF_VDPPM_FAULT, "BQ25186VdppmFaultBinarySensor", DEVICE_CLASS_PROBLEM),
-    (CONF_VINDPM_FAULT, "BQ25186VindpmFaultBinarySensor", DEVICE_CLASS_PROBLEM),
-    (CONF_THERMREG_ACTIVE, "BQ25186ThermregActiveBinarySensor", DEVICE_CLASS_SAFETY),
-    (CONF_VIN_OVP_FAULT, "BQ25186VinOvpFaultBinarySensor", DEVICE_CLASS_PROBLEM),
-    (
-        CONF_BATTERY_UVLO_FAULT,
-        "BQ25186BatteryUvloActiveFaultBinarySensor",
-        DEVICE_CLASS_PROBLEM,
-    ),
-    (
-        CONF_BATTERY_OCP_FAULT,
-        "BQ25186BatteryOcpFaultBinarySensor",
-        DEVICE_CLASS_PROBLEM,
-    ),
+    (CONF_TS_FLAG, "BQ25186TsFlagBinarySensor", None),
+    (CONF_ILIM_FLAG, "BQ25186IlimFlagBinarySensor", None),
+    (CONF_VDPPM_FLAG, "BQ25186VdppmFlagBinarySensor", None),
+    (CONF_VINDPM_FLAG, "BQ25186VindpmFlagBinarySensor", None),
+    (CONF_THERMREG_ACTIVE, "BQ25186ThermregActiveBinarySensor", None),
+    (CONF_VIN_OVP_FLAG, "BQ25186VinOvpFlagBinarySensor", None),
+    (CONF_BATTERY_UVLO_FLAG, "BQ25186BatteryUvloActiveFlagBinarySensor", None),
+    (CONF_BATTERY_OCP_FLAG, "BQ25186BatteryOcpFlagBinarySensor", None),
 )
 
 _BINARY_SENSOR_TYPES = {
--- esphome/components/bq25186/binary_sensor/bq25186_binary_sensor.h
+++ esphome/components/bq25186/binary_sensor/bq25186_binary_sensor.h
@@ -33,16 +33,16 @@ using BQ25186PowerGoodBinarySensor = StatusBinarySensor<BQ25186_REG_STAT0, 0, 0x
 // STAT1
 using BQ25186VinOvpStatusBinarySensor = StatusBinarySensor<BQ25186_REG_STAT1, 7, 0x01, 0x01>;
 using BQ25186BatteryUvloStatusBinarySensor = StatusBinarySensor<BQ25186_REG_STAT1, 6, 0x01, 0x01>;
-using BQ25186SafetyTimerFaultBinarySensor = StatusBinarySensor<BQ25186_REG_STAT1, 2, 0x01, 0x01>;
+using BQ25186SafetyTimerFlagBinarySensor = StatusBinarySensor<BQ25186_REG_STAT1, 2, 0x01, 0x01>;
 using BQ25186Wake1FlagBinarySensor = StatusBinarySensor<BQ25186_REG_STAT1, 1, 0x01, 0x01>;
 using BQ25186Wake2FlagBinarySensor = StatusBinarySensor<BQ25186_REG_STAT1, 0, 0x01, 0x01>;
 // FLAG0
-using BQ25186TsFaultBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 7, 0x01, 0x01>;
-using BQ25186IlimFaultBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 6, 0x01, 0x01>;
-using BQ25186VdppmFaultBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 5, 0x01, 0x01>;
-using BQ25186VindpmFaultBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 4, 0x01, 0x01>;
+using BQ25186TsFlagBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 7, 0x01, 0x01>;
+using BQ25186IlimFlagBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 6, 0x01, 0x01>;
+using BQ25186VdppmFlagBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 5, 0x01, 0x01>;
+using BQ25186VindpmFlagBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 4, 0x01, 0x01>;
 using BQ25186ThermregActiveBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 3, 0x01, 0x01>;
-using BQ25186VinOvpFaultBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 2, 0x01, 0x01>;
-using BQ25186BatteryUvloActiveFaultBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 1, 0x01, 0x01>;
-using BQ25186BatteryOcpFaultBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 0, 0x01, 0x01>;
+using BQ25186VinOvpFlagBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 2, 0x01, 0x01>;
+using BQ25186BatteryUvloActiveFlagBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 1, 0x01, 0x01>;
+using BQ25186BatteryOcpFlagBinarySensor = StatusBinarySensor<BQ25186_REG_FLAG0, 0, 0x01, 0x01>;
 }  // namespace esphome::bq25186
