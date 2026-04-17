e4f15030af967548a49b735ea8fe379f8fc95be8
rwrozelle <rwrozelle@gmail.com>
2026-04-17 10:52:25 -0400

    add back in set_write_interval and update tests

--- esphome/components/preferences/syncer.h
+++ esphome/components/preferences/syncer.h
@@ -8,6 +8,7 @@ namespace preferences {
 
 class IntervalSyncer final : public PollingComponent {
  public:
+  void set_write_interval(uint32_t write_interval) { this->set_update_interval(write_interval); }
   void update() override { global_preferences->sync(); }
   void on_shutdown() override { global_preferences->sync(); }
 };
