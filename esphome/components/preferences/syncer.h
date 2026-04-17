7afc9ff09179fa70e1f44c24e13d076ae8f483ca
rwrozelle <rwrozelle@gmail.com>
2026-04-17 11:08:26 -0400

    Add back in get_setup_priority

--- esphome/components/preferences/syncer.h
+++ esphome/components/preferences/syncer.h
@@ -11,6 +11,7 @@ class IntervalSyncer final : public PollingComponent {
   void set_write_interval(uint32_t write_interval) { this->set_update_interval(write_interval); }
   void update() override { global_preferences->sync(); }
   void on_shutdown() override { global_preferences->sync(); }
+  float get_setup_priority() const override { return setup_priority::BUS; }
 };
 
 }  // namespace preferences
