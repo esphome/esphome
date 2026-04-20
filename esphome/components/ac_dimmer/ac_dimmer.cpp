aa41e0f9ce3fc6dcb453cdce56da19f1b27e6511
aselafernando <25498128+aselafernando@users.noreply.github.com>
2026-04-20 23:13:08 +1000

    [ac_dimmer] Zero-crossing interrupt type added to config dump

--- esphome/components/ac_dimmer/ac_dimmer.cpp
+++ esphome/components/ac_dimmer/ac_dimmer.cpp
@@ -229,8 +229,15 @@ void AcDimmer::dump_config() {
                 "   Min Power: %.1f%%\n"
                 "   Init with half cycle: %s",
                 this->store_.min_power / 10.0f, YESNO(this->init_with_half_cycle_));
-  LOG_PIN("  Output Pin: ", this->gate_pin_);
-  LOG_PIN("  Zero-Cross Pin: ", this->zero_cross_pin_);
+  LOG_PIN("   Output Pin: ", this->gate_pin_);
+  LOG_PIN("   Zero-Cross Pin: ", this->zero_cross_pin_);
+  if (this->zero_cross_interrupt_type_ == gpio::INTERRUPT_RISING_EDGE) {
+    ESP_LOGCONFIG(TAG, "    Interrupt Type : rising");
+  } else if (this->zero_cross_interrupt_type_ == gpio::INTERRUPT_FALLING_EDGE) {
+    ESP_LOGCONFIG(TAG, "    Interrupt Type : falling");
+  } else {
+    ESP_LOGCONFIG(TAG, "    Interrupt Type : any");
+  }
   if (method_ == DIM_METHOD_LEADING_PULSE) {
     ESP_LOGCONFIG(TAG, "   Method: leading pulse");
   } else if (method_ == DIM_METHOD_LEADING) {
@@ -238,9 +245,8 @@ void AcDimmer::dump_config() {
   } else {
     ESP_LOGCONFIG(TAG, "   Method: trailing");
   }
-
   LOG_FLOAT_OUTPUT(this);
-  ESP_LOGV(TAG, "  Estimated Frequency: %.3fHz", 1e6f / this->store_.cycle_time_us / 2);
+  ESP_LOGV(TAG, "   Estimated Frequency: %.3fHz", 1e6f / this->store_.cycle_time_us / 2);
 }
 
 }  // namespace esphome::ac_dimmer
