1b6ec178683e005d69d85b8936e1f944d0d248a3
Johan Henkens <johan@henkens.com>
2026-04-21 16:31:48 -0700

    Add if around UnitTemperature

--- esphome/components/api/api_pb2_dump.cpp
+++ esphome/components/api/api_pb2_dump.cpp
@@ -297,6 +297,7 @@ template<> const char *proto_enum_to_string<enums::SupportsResponseType>(enums::
   }
 }
 #endif
+#if defined(USE_CLIMATE) || defined(USE_WATER_HEATER)
 template<> const char *proto_enum_to_string<enums::TemperatureUnit>(enums::TemperatureUnit value) {
   switch (value) {
     case enums::TEMPERATURE_UNIT_CELSIUS:
@@ -309,6 +310,7 @@ template<> const char *proto_enum_to_string<enums::TemperatureUnit>(enums::Tempe
       return ESPHOME_PSTR("UNKNOWN");
   }
 }
+#endif
 #ifdef USE_CLIMATE
 template<> const char *proto_enum_to_string<enums::ClimateMode>(enums::ClimateMode value) {
   switch (value) {
