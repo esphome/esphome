ddbc46ea00a2c5d51f30ea6315faf51aeff6e090
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2026-04-22 14:51:49 -0400

    Clean

--- esphome/components/ac_dimmer/ac_dimmer.cpp
+++ esphome/components/ac_dimmer/ac_dimmer.cpp
@@ -226,27 +226,27 @@ void AcDimmer::write_state(float state) {
 void AcDimmer::dump_config() {
   ESP_LOGCONFIG(TAG,
                 "AcDimmer:\n"
-                "   Min Power: %.1f%%\n"
-                "   Init with half cycle: %s",
+                "  Min Power: %.1f%%\n"
+                "  Init with half cycle: %s",
                 this->store_.min_power / 10.0f, YESNO(this->init_with_half_cycle_));
-  LOG_PIN("   Output Pin: ", this->gate_pin_);
-  LOG_PIN("   Zero-Cross Pin: ", this->zero_cross_pin_);
+  LOG_PIN("  Output Pin: ", this->gate_pin_);
+  LOG_PIN("  Zero-Cross Pin: ", this->zero_cross_pin_);
   if (this->zero_cross_interrupt_type_ == gpio::INTERRUPT_RISING_EDGE) {
-    ESP_LOGCONFIG(TAG, "    Interrupt Type : rising");
+    ESP_LOGCONFIG(TAG, "  Interrupt Type: rising");
   } else if (this->zero_cross_interrupt_type_ == gpio::INTERRUPT_FALLING_EDGE) {
-    ESP_LOGCONFIG(TAG, "    Interrupt Type : falling");
+    ESP_LOGCONFIG(TAG, "  Interrupt Type: falling");
   } else {
-    ESP_LOGCONFIG(TAG, "    Interrupt Type : any");
+    ESP_LOGCONFIG(TAG, "  Interrupt Type: any");
   }
   if (method_ == DIM_METHOD_LEADING_PULSE) {
-    ESP_LOGCONFIG(TAG, "   Method: leading pulse");
+    ESP_LOGCONFIG(TAG, "  Method: leading pulse");
   } else if (method_ == DIM_METHOD_LEADING) {
-    ESP_LOGCONFIG(TAG, "   Method: leading");
+    ESP_LOGCONFIG(TAG, "  Method: leading");
   } else {
-    ESP_LOGCONFIG(TAG, "   Method: trailing");
+    ESP_LOGCONFIG(TAG, "  Method: trailing");
   }
   LOG_FLOAT_OUTPUT(this);
-  ESP_LOGV(TAG, "   Estimated Frequency: %.3fHz", 1e6f / this->store_.cycle_time_us / 2);
+  ESP_LOGV(TAG, "  Estimated Frequency: %.3fHz", 1e6f / this->store_.cycle_time_us / 2);
 }
 
 }  // namespace esphome::ac_dimmer
