12c76bd27ad296cdb232674c187da898a970dd8e
Craig Andrews <candrews@integralblue.com>
2026-04-22 13:02:52 -0400

    [tfluna] namespace syntax cleanup in reset_button

--- esphome/components/tfluna/button/reset_button.cpp
+++ esphome/components/tfluna/button/reset_button.cpp
@@ -1,9 +1,7 @@
 #include "reset_button.h"
 
-namespace esphome {
-namespace tfluna {
+namespace esphome::tfluna {
 
 void ResetButton::press_action() { this->parent_->factory_reset(); }
 
-}  // namespace tfluna
-}  // namespace esphome
+}  // namespace esphome::tfluna
