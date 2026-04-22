45eb35316c8cb0bef1999212885b61fd543f2930
Johan Henkens <jhenkens@users.noreply.github.com>
2026-04-22 07:40:52 -0700

    Update esphome/components/api/api_connection.cpp
    Co-authored-by: J. Nick Koston <nick+github@koston.org>

--- esphome/components/api/api_connection.cpp
+++ esphome/components/api/api_connection.cpp
@@ -753,7 +753,6 @@ uint16_t APIConnection::try_send_climate_info(EntityBase *entity, APIConnection 
   msg.supports_action = traits.has_feature_flags(climate::CLIMATE_SUPPORTS_ACTION);
   // Current feature flags and other supported parameters
   msg.feature_flags = traits.get_feature_flags();
-  msg.temperature_unit = enums::TEMPERATURE_UNIT_CELSIUS;
   msg.supported_modes = &traits.get_supported_modes();
   msg.visual_min_temperature = traits.get_visual_min_temperature();
   msg.visual_max_temperature = traits.get_visual_max_temperature();
