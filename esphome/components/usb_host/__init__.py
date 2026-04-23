367920e6a321aecef51b8038f88cc769163cc6b7
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2026-04-23 10:40:59 +0000

    fix adding mps to CORE.data["usb_host] instead of CORE["usb_host"]

--- esphome/components/usb_host/__init__.py
+++ esphome/components/usb_host/__init__.py
@@ -44,9 +44,9 @@ def usb_device_schema(cls=USBClient, vid: int = None, pid: int = None) -> cv.Sch
     return schema
 
 def add_usb_mps(mps_value: int):
-    if not hasattr(CORE, DOMAIN):
-        CORE.DOMAIN = {}
-    CORE.DOMAIN[CONF_MAX_PACKET_SIZE] = mps_value
+    if not hasattr(CORE.data, DOMAIN):
+        CORE.data.DOMAIN = {}
+    CORE.data.DOMAIN[CONF_MAX_PACKET_SIZE] = mps_value
 
 CONFIG_SCHEMA = cv.All(
     cv.COMPONENT_SCHEMA.extend(
