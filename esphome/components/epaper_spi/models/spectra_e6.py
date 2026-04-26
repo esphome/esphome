4dfea8504b94876affe622e5eed26b7911bfdfa7
Calvin Walton <calvin.walton@kepstin.ca>
2026-04-26 12:28:35 -0400

    [epaper_spi] Add 4.0in-Spectra-E6 display
    Clean up the handling of the initsequence to allow the Spectra E6 class
    to use the `initsequence` and `add_init_sequence` keys in the class
    initializers. This allows the common parts of the init sequence between
    multiple displays to be factored out.
    
    Add comments explaining the origin of the initsequence values and
    mentioning command names where known. The custom controller in the
    "ED2208" Spectra 6 displays is apparently derived from the Soloman
    SPD1656, which was used for the 4-color Spectra 3100 displays.

--- esphome/components/epaper_spi/models/spectra_e6.py
+++ esphome/components/epaper_spi/models/spectra_e6.py
@@ -11,32 +11,75 @@ class SpectraE6(EpaperModel):
     def get_init_sequence(self, config: dict):
         width, height = self.get_dimensions(config)
         return (
-            (0xAA, 0x49, 0x55, 0x20, 0x08, 0x09, 0x18,),
-            (0x01, 0x3F,),
-            (0x00, 0x5F, 0x69,),
-            (0x03, 0x00, 0x54, 0x00, 0x44,),
-            (0x05, 0x40, 0x1F, 0x1F, 0x2C,),
-            (0x06, 0x6F, 0x1F, 0x17, 0x49,),
-            (0x08, 0x6F, 0x1F, 0x1F, 0x22,),
-            (0x30, 0x03,),
-            (0x50, 0x3F,),
-            (0x60, 0x02, 0x00,),
+            *self.initsequence,
+            # TRES
             (0x61, width // 256, width % 256, height // 256, height % 256,),
-            (0x84, 0x01,),
-            (0xE3, 0x2F,),
         )
 
     def get_default(self, key, fallback: Any = False) -> Any:
         return self.defaults.get(key, fallback)
 
+# No documentation for the Spectra E6 controller is available, but it appears
+# to use a similar command set to the Solomon SPD1656 or Jadard JD79660.
+spectra_e6 = SpectraE6(
+    "spectra-e6",
+    data_rate="20MHz",
+    minimum_update_interval="30s",
+    # Common values adapted from Waveshare example code (MIT license)
+    initsequence=(
+        # CMDH
+        (0xAA, 0x49, 0x55, 0x20, 0x08, 0x09, 0x18,),
+        # PSR
+        (0x00, 0x5F, 0x69,),
+        # PWR
+        (0x01, 0x3F,),
+        # PFS
+        (0x03, 0x00, 0x54, 0x00, 0x44,),
+        # BTST1
+        (0x05, 0x40, 0x1F, 0x1F, 0x2C,),
+        # BTST2 value is display specific.
+        # The controller has a default which works but won't be optimal.
+        # BTST3
+        (0x08, 0x6F, 0x1F, 0x1F, 0x22,),
+        # PLL
+        (0x30, 0x08,),
+        # CDI
+        (0x50, 0x3F,),
+        # TCON
+        (0x60, 0x02, 0x00,),
+        # T_VDCS
+        (0x84, 0x01,),
+        # PWS
+        (0xE3, 0x2F,),
+    )
+)
 
-spectra_e6 = SpectraE6("spectra-e6", minimum_update_interval="30s")
+spectra_e6_4 = spectra_e6.extend(
+    "4.0in-Spectra-E6",
+    width=400,
+    height=600,
+    add_init_sequence=(
+        # Values from Waveshare "4inch e-Paper (E)" example code (MIT license)
+        # https://github.com/waveshareteam/e-Paper/blob/master/E-paper_Separate_Program/4inch_e-Paper_E
+        # BTST2
+        # For this display, (0x6F, 0x1F, 0x17, 0x17) is used during init,
+        # but updated to (0x6F, 0x1F, 0x17, 0x27) before doing a refresh.
+        # I believe this allows for the power on to run faster.
+        # Since there's no way to do this right now, set the final value here.
+        (0x06, 0x6F, 0x1F, 0x17, 0x27,),
+    )
+)
 
 spectra_e6_7p3 = spectra_e6.extend(
     "7.3in-Spectra-E6",
     width=800,
     height=480,
-    data_rate="20MHz",
+    add_init_sequence=(
+        # Values from Waveshare "7.3inch e-Paper (E)" example code (MIT license)
+        # https://github.com/waveshareteam/e-Paper/blob/master/RaspberryPi_JetsonNano/c/lib/e-Paper/EPD_7in3e.c
+        # BTST2
+        (0x06, 0x6F, 0x1F, 0x17, 0x49,),
+    )
 )
 
 spectra_e6_7p3.extend(
