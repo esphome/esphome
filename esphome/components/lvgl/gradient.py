7065f6325037807ca7e3c71d11043f7fa6724186
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-04-27 20:31:09 +1000

    [lvgl] Add utility gradient function

--- esphome/components/lvgl/gradient.py
+++ esphome/components/lvgl/gradient.py
@@ -11,6 +11,7 @@ from esphome.core import ID
 from esphome.cpp_generator import MockObj
 
 from .defines import CONF_GRADIENTS, CONF_OPA, LV_DITHER, add_define, add_warning
+from .helpers import add_lv_use
 from .lv_validation import lv_color, lv_percentage, opacity
 from .lvcode import lv
 from .types import lv_color_t, lv_gradient_t, lv_opa_t
@@ -50,6 +51,7 @@ GRADIENT_SCHEMA = cv.ensure_list(
 
 
 async def gradients_to_code(config):
+    add_lv_use("gradient")
     max_stops = 2
     if any(CONF_DITHER in x for x in config.get(CONF_GRADIENTS, ())):
         add_warning(
