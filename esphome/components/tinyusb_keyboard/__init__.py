68e11b1a3eedf14f04a0fed7096bc1f02eed2164
Frederik Bolding <frederik.bolding@gmail.com>
2026-04-28 21:54:50 +0200

    Address PR comments

--- esphome/components/tinyusb_keyboard/__init__.py
+++ esphome/components/tinyusb_keyboard/__init__.py
@@ -1,11 +1,18 @@
 from esphome import automation
 from esphome.automation import maybe_simple_id
 import esphome.codegen as cg
-from esphome.components.esp32 import add_idf_sdkconfig_option
+from esphome.components.esp32 import (
+    VARIANT_ESP32P4,
+    VARIANT_ESP32S2,
+    VARIANT_ESP32S3,
+    add_idf_sdkconfig_option,
+    only_on_variant,
+)
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_KEY
 
 CODEOWNERS = ["@frederikbolding"]
+DEPENDENCIES = ["tinyusb"]
 
 CONF_MODIFIERS = "modifiers"
 CONF_USAGE = "usage"
@@ -19,11 +26,16 @@ ReleaseAction = tinyusb_keyboard_ns.class_("ReleaseAction", automation.Action)
 MediaPressAction = tinyusb_keyboard_ns.class_("MediaPressAction", automation.Action)
 MediaReleaseAction = tinyusb_keyboard_ns.class_("MediaReleaseAction", automation.Action)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(TinyUSBKeyboard),
-    }
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(TinyUSBKeyboard),
+        }
+    ).extend(cv.COMPONENT_SCHEMA),
+    only_on_variant(
+        supported=[VARIANT_ESP32P4, VARIANT_ESP32S2, VARIANT_ESP32S3],
+    ),
+)
 
 
 async def to_code(config):
@@ -37,8 +49,8 @@ async def to_code(config):
 PRESS_ACTION_SCHEMA = maybe_simple_id(
     {
         cv.GenerateID(): cv.use_id(TinyUSBKeyboard),
-        cv.Required(CONF_KEY): cv.Any(cv.string, cv.positive_int),
-        cv.Optional(CONF_MODIFIERS, default=0): cv.positive_int,
+        cv.Required(CONF_KEY): cv.Any(cv.All(cv.string, cv.Length(1)), cv.uint8_t),
+        cv.Optional(CONF_MODIFIERS, default=0): cv.uint8_t,
     }
 )
 
@@ -51,7 +63,7 @@ RELEASE_ACTION_SCHEMA = maybe_simple_id(
 MEDIA_PRESS_SCHEMA = maybe_simple_id(
     {
         cv.GenerateID(): cv.use_id(TinyUSBKeyboard),
-        cv.Required(CONF_USAGE): cv.positive_int,
+        cv.Required(CONF_USAGE): cv.uint16_t,
     }
 )
 
@@ -70,8 +82,8 @@ async def tinyusb_keyboard_press_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg, parent)
 
     key = config[CONF_KEY]
-    if isinstance(key, str) and len(key) == 1:
-        key_template = await cg.templatable(key, args, cg.std_string)
+    if isinstance(key, str):
+        key_template = await cg.templatable(key.lower(), args, cg.std_string)
         cg.add(var.set_key(key_template))
     else:
         key_template = await cg.templatable(key, args, cg.uint8)
