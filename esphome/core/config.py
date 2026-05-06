71dbe53df38ca0380c0a95f60686da572424dd0f
J. Nick Koston <nick@home-assistant.io>
2026-05-06 06:55:11 -0500

    Handle empty looping_components and add ordering tests

--- esphome/core/config.py
+++ esphome/core/config.py
@@ -581,7 +581,7 @@ async def _add_looping_components() -> None:
     terms = [
         f"({count} * HasLoopOverride<{cpp_type}>::value)"
         for cpp_type, count in type_counts.items()
-    ]
+    ] or ["0"]
     constexpr_expr = " + \\\n  ".join(terms)
     cg.add_global(
         cg.RawStatement(
