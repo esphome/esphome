3a15c20754bc63d725255aa6d59a963f444c62ea
rwalker777 <49888088+rwalker777@users.noreply.github.com>
2026-05-11 11:29:18 -0500

    Add QUEUED_STATE_CHANGED event to DashboardEvent enum

--- esphome/dashboard/const.py
+++ esphome/dashboard/const.py
@@ -15,6 +15,7 @@ class DashboardEvent(StrEnum):
     ENTRY_STATE_CHANGED = "entry_state_changed"
     IMPORTABLE_DEVICE_ADDED = "importable_device_added"
     IMPORTABLE_DEVICE_REMOVED = "importable_device_removed"
+    QUEUED_STATE_CHANGED = "queued_state_changed"
     INITIAL_STATE = "initial_state"  # Sent on WebSocket connection
     PONG = "pong"  # Response to client ping
 
