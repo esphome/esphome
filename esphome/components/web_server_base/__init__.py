496a679e64b65606b8f7c0eb059eb86839e9c92f
ardumine <david.codeco@gmail.com>
2026-05-12 21:56:10 +0100

    [web_server_base] Ignore framework WebServer library on rp2040 to fix CI build
    The rp2040 platform uses lib_ldf_mode = chain+ which causes PlatformIO's
    Library Dependency Finder to pull in the Arduino Pico framework's bundled
    WebServer library when ESPAsyncWebServer is included. That bundled library
    includes WiFiServer.h which does not exist on the rp2040 platform.
    
    Add WebServer to the existing lib_ignore list for rp2040 in web_server_base.

--- esphome/components/web_server_base/__init__.py
+++ esphome/components/web_server_base/__init__.py
@@ -64,9 +64,12 @@ async def to_code(config):
         if CORE.is_libretiny:
             CORE.add_platformio_option("lib_ignore", ["ESPAsyncTCP", "RPAsyncTCP"])
         if CORE.is_rp2040:
-            # Ignore bundled AsyncTCP libraries - we use RPAsyncTCP from async_tcp component
+            # Ignore bundled AsyncTCP and WebServer libraries - we use RPAsyncTCP from
+            # async_tcp component and ESPAsyncWebServer from an external library.
+            # The framework's WebServer pulls in WiFiServer.h which doesn't exist on rp2040.
             CORE.add_platformio_option(
-                "lib_ignore", ["ESPAsyncTCP", "AsyncTCP", "AsyncTCP_RP2040W"]
+                "lib_ignore",
+                ["ESPAsyncTCP", "AsyncTCP", "AsyncTCP_RP2040W", "WebServer"],
             )
             # ESPAsyncWebServer uses Hash library for sha1() on RP2040
             cg.add_library("Hash", None)
