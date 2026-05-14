0353b05b098897b6b050d966e63e1e8a7ee4c524
mschnaubelt <you@example.com>
2026-05-14 20:54:24 +0200

    fixed test

--- esphome/components/axp2101/axp2101.h
+++ esphome/components/axp2101/axp2101.h
@@ -2,9 +2,9 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
-#include "esphome/components/i2c/i2c.h"
 
 namespace esphome::axp2101 {
 
