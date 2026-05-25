f61b8b9bb85b3ee3f2ffce3538fb5c56be1d71cc
Boris Krivonog <boris.krivonog@inova.si>
2026-05-25 22:43:09 +0200

    added missing log

--- esphome/components/mitsubishi_cn105/mitsubishi_cn105_climate.cpp
+++ esphome/components/mitsubishi_cn105/mitsubishi_cn105_climate.cpp
@@ -66,6 +66,7 @@ void MitsubishiCN105Climate::dump_config() {
                 this->hp_.get_update_interval(), this->parent_->get_baud_rate(), this->parent_->get_data_bits(),
                 LOG_STR_ARG(parity_to_str(this->parent_->get_parity())), this->parent_->get_stop_bits());
   LOG_SELECT("  ", "Vertical vane direction", this->vertical_vane_direction_select_);
+  LOG_SELECT("  ", "Horizontal vane direction", this->horizontal_vane_direction_select_);
 }
 
 void MitsubishiCN105Climate::setup() { this->hp_.initialize(); }
