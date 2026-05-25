1bdd246db67760fe83ef68e57f321484620e6213
behnam <fahimyfar@gmail.com>
2026-05-25 22:10:34 +0300

    feat: Make both username and password templatable in web_server\n\nEnable both auth username and password to support template expressions\n(lambda, substitutions, etc.), providing complete flexibility for dynamic\nauthentication configuration at runtime.

--- esphome/components/web_server/__init__.py
+++ esphome/components/web_server/__init__.py
@@ -191,7 +191,7 @@ CONFIG_SCHEMA = cv.All(
             cv.Optional(CONF_AUTH): cv.Schema(
                 {
                     cv.Required(CONF_USERNAME): cv.All(
-                        cv.string_strict, cv.Length(min=1)
+                        cv.templatable(cv.string_strict), cv.Length(min=1)
                     ),
                     cv.Required(CONF_PASSWORD): cv.All(
                         cv.templatable(cv.string_strict), cv.Length(min=1)
@@ -321,7 +321,13 @@ async def to_code(config):
         cg.add_define("USE_WEBSERVER_PRIVATE_NETWORK_ACCESS")
     if CONF_AUTH in config:
         cg.add_define("USE_WEBSERVER_AUTH")
-        cg.add(paren.set_auth_username(config[CONF_AUTH][CONF_USERNAME]))
+        username_config = config[CONF_AUTH][CONF_USERNAME]
+        # Handle both static and templatable username
+        if isinstance(username_config, cg.TemplateArguments):
+            cg.add(paren.set_auth_username_template(username_config))
+        else:
+            cg.add(paren.set_auth_username(username_config))
+        
         password_config = config[CONF_AUTH][CONF_PASSWORD]
         # Handle both static and templatable password
         if isinstance(password_config, cg.TemplateArguments):
--- esphome/components/web_server_base/web_server_base.h
+++ esphome/components/web_server_base/web_server_base.h
@@ -50,9 +50,11 @@ class MiddlewareHandler : public AsyncWebHandler {
 struct Credentials {
   std::string username;
   std::string password;
-  // Function pointer for templatable password
+  // Function pointers for templatable credentials
+  std::function<std::string()> username_template;
   std::function<std::string()> password_template;
-  bool has_template{false};
+  bool username_is_template{false};
+  bool password_is_template{false};
 };
 
 class AuthMiddlewareHandler : public MiddlewareHandler {
@@ -61,13 +63,19 @@ class AuthMiddlewareHandler : public MiddlewareHandler {
       : MiddlewareHandler(next), credentials_(credentials) {}
 
   bool check_auth(AsyncWebServerRequest *request) {
+    // Get the current username (either static or from template)
+    std::string current_username = credentials_->username;
+    if (credentials_->username_is_template && credentials_->username_template) {
+      current_username = credentials_->username_template();
+    }
+    
     // Get the current password (either static or from template)
     std::string current_password = credentials_->password;
-    if (credentials_->has_template && credentials_->password_template) {
+    if (credentials_->password_is_template && credentials_->password_template) {
       current_password = credentials_->password_template();
     }
     
-    bool success = request->authenticate(credentials_->username.c_str(), current_password.c_str());
+    bool success = request->authenticate(current_username.c_str(), current_password.c_str());
     if (!success) {
       request->requestAuthentication();
     }
@@ -127,14 +135,21 @@ class WebServerBase {
   AsyncWebServer *get_server() const { return this->server_; }
 
 #ifdef USE_WEBSERVER_AUTH
-  void set_auth_username(std::string auth_username) { credentials_.username = std::move(auth_username); }
+  void set_auth_username(std::string auth_username) { 
+    credentials_.username = std::move(auth_username);
+    credentials_.username_is_template = false;
+  }
+  void set_auth_username_template(std::function<std::string()> username_template) {
+    credentials_.username_template = username_template;
+    credentials_.username_is_template = true;
+  }
   void set_auth_password(std::string auth_password) { 
     credentials_.password = std::move(auth_password);
-    credentials_.has_template = false;
+    credentials_.password_is_template = false;
   }
   void set_auth_password_template(std::function<std::string()> password_template) {
     credentials_.password_template = password_template;
-    credentials_.has_template = true;
+    credentials_.password_is_template = true;
   }
 #endif
 
