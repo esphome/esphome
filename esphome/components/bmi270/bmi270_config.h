445f618b930ebb5d9eb7a124ec996841690a5c29
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-06-02 12:28:43 +1000

    Add copyright and license to config file

--- esphome/components/bmi270/bmi270_config.h
+++ esphome/components/bmi270/bmi270_config.h
@@ -3,14 +3,47 @@
 
 namespace esphome::bmi270 {
 
-// ---------------------------------------------------------------------------
-// BMI270 configuration file (chip ID 0x24, firmware v2.86.1)
-// Source: Bosch Sensortec BMI270_SensorAPI (BSD-3-Clause)
-//         https://github.com/boschsensortec/BMI270_SensorAPI
-//
-// This blob MUST be written to the chip's internal INIT_DATA register
-// after every power cycle, before any sensor data can be read.
-// ---------------------------------------------------------------------------
+/**
+ BMI270 configuration file (chip ID 0x24, firmware v2.86.1)
+ Source: Bosch Sensortec BMI270_SensorAPI (BSD-3-Clause)
+         https://github.com/boschsensortec/BMI270_SensorAPI
+
+Copyright (c) 2023 Bosch Sensortec GmbH. All rights reserved.
+
+BSD-3-Clause
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+
+ This blob MUST be written to the chip's internal INIT_DATA register
+ after every power cycle, before any sensor data can be read.
+ --------------------------------------------------------------------------- */
+
 static constexpr uint8_t BMI270_CONFIG_FILE[] = {
     0xc8, 0x2e, 0x00, 0x2e, 0x80, 0x2e, 0x3d, 0xb1, 0xc8, 0x2e, 0x00, 0x2e, 0x80, 0x2e, 0x91, 0x03, 0x80, 0x2e, 0xbc,
     0xb0, 0x80, 0x2e, 0xa3, 0x03, 0xc8, 0x2e, 0x00, 0x2e, 0x80, 0x2e, 0x00, 0xb0, 0x50, 0x30, 0x21, 0x2e, 0x59, 0xf5,
