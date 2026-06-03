9d91e9ea0ad981d2354f9bebef16b81d929c8051
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-06-03 17:18:26 +1000

    Fix test

--- tests/component_tests/sensor/test_sensor.py
+++ tests/component_tests/sensor/test_sensor.py
@@ -13,7 +13,7 @@ def test_sensor_device_class_set(generate_main):
     main_cpp = generate_main("tests/component_tests/sensor/test_sensor.yaml")
 
     # Then
-    assert 's_1->configure_entity_("test s1", 1997041708, 257);' in main_cpp
+    assert 'App.register_sensor(s_1, "test s1", 1997041708, 257);' in main_cpp
     assert (
         "threshold_id->set_upper_threshold([]() -> float {\n    return s_1->state;"
         in main_cpp
