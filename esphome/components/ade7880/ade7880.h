be9d29212dabeb7900b4e1b581b9e222c2b92fe8
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2026-06-04 21:22:00 -0400

    [ade7880] Fix reverse active energy reading from reserved register
    0xE406-0xE408 are reserved on the ADE7880; the previously-added ARWATTHR/
    BRWATTHR/CRWATTHR registers do not exist on this device. The chip has no
    separate forward/reverse active energy accumulators, so read the signed
    total active energy register (xWATTHR) and split the value by sign into the
    forward and reverse running totals.

--- esphome/components/ade7880/ade7880.h
+++ esphome/components/ade7880/ade7880.h
@@ -105,7 +105,10 @@ class ADE7880 : public i2c::I2CDevice, public PollingComponent {
   // the callable will be passed a 'float' value and is expected to return a 'float'
   template<typename F> void update_sensor_from_s24zp_register16_(sensor::Sensor *sensor, uint16_t a_register, F &&f);
   template<typename F> void update_sensor_from_s16_register16_(sensor::Sensor *sensor, uint16_t a_register, F &&f);
-  template<typename F> void update_sensor_from_s32_register16_(sensor::Sensor *sensor, uint16_t a_register, F &&f);
+
+  // Reads the signed active energy register and splits the accumulated value into the forward and
+  // reverse running totals based on its sign (the ADE7880 has no separate forward/reverse registers).
+  void update_active_energy_(PowerChannel *channel, uint16_t a_register);
 
   void reset_device_();
 
