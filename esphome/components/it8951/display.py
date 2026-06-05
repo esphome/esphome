de80198e71a96867124cce63189d0f3b5cdac3fc
alorente <gitmaster@passific.fr>
2026-06-05 15:09:17 +0200

    Fix wrong config

--- esphome/components/it8951/display.py
+++ esphome/components/it8951/display.py
@@ -295,12 +295,12 @@ def _customise_schema(config):
     width, height = model.get_dimensions(model_config)
 
     display.add_metadata(
-        config[CONF_ID],
+        model_config[CONF_ID],
         width,
         height,
         has_hardware_rotation=True,
         has_writer=any(
-            config.get(key) for key in (CONF_LAMBDA, CONF_PAGES, CONF_SHOW_TEST_CARD)
+            model_config.get(key) for key in (CONF_LAMBDA, CONF_PAGES, CONF_SHOW_TEST_CARD)
         ),
     )
 
