88b852060d77ddea035e08025bf79fe0f277e181
Tomasz Witke <kyvaith@gmail.com>
2026-06-07 21:12:04 +0200

    [lvgl] Fix display staging lint

--- esphome/components/lvgl/lvgl_esphome.h
+++ esphome/components/lvgl/lvgl_esphome.h
@@ -323,7 +323,7 @@ class LvglComponent : public PollingComponent {
 
   void write_random_();
   void draw_buffer_(const lv_area_t *area, lv_color_data *ptr);
-  void sync_direct_framebuffer_area_(const lv_area_t *area, uint8_t *color_p);
+  void sync_direct_framebuffer_area_(const lv_area_t *area, const uint8_t *color_p);
   void sync_direct_other_buffer_(const lv_area_t *area, const uint8_t *color_p);
   uint8_t *next_direct_render_buffer_() const;
   void present_direct_render_buffer_(uint8_t *buffer);
