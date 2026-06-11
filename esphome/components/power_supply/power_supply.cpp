aad5696717e7445125b4719cfcd27882947071e5
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-06-11 12:10:15 +1000

    [power_supply] Make enable_on_boot high priority

--- esphome/components/power_supply/power_supply.cpp
+++ esphome/components/power_supply/power_supply.cpp
@@ -21,7 +21,9 @@ void PowerSupply::dump_config() {
   LOG_PIN("  Pin: ", this->pin_);
 }
 
-float PowerSupply::get_setup_priority() const { return setup_priority::IO; }
+float PowerSupply::get_setup_priority() const {
+  return this->enable_on_boot_ ? setup_priority::POWER : setup_priority::IO;
+}
 
 bool PowerSupply::is_enabled() const { return this->active_requests_ != 0; }
 
