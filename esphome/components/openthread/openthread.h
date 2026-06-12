56a234cb7d0ca2ac55c2fb2987daceb83d7653c1
ardumine <david.codeco@gmail.com>
2026-06-12 10:57:14 +0100

    fix

--- esphome/components/openthread/openthread.h
+++ esphome/components/openthread/openthread.h
@@ -44,10 +44,10 @@ class OpenThreadComponent : public Component {
 #endif
   void set_output_power(int8_t output_power) { this->output_power_ = output_power; }
   void set_connected(bool connected) { this->connected_ = connected; }
+  static void on_state_changed(otChangedFlags flags, void *context);
 
  protected:
   std::optional<otIp6Address> get_omr_address_(InstanceLock &lock);
-  static void on_state_changed(otChangedFlags flags, void *context);
   otInstance *get_openthread_instance_();
   int openthread_stop_();
   std::function<void()> factory_reset_external_callback_;
