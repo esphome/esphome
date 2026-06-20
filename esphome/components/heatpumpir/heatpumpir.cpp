3eb52d45e6006e60f6c1a1b773a67520e7d0e3d7
BugHaver <43462320+bughaver@users.noreply.github.com>
2026-06-20 21:27:02 +1000

    [heatpumpir] Use library constants for ZJ/ZMP HIPOWER, separate by protocol

--- esphome/components/heatpumpir/heatpumpir.cpp
+++ esphome/components/heatpumpir/heatpumpir.cpp
@@ -313,7 +313,6 @@ bool HeatpumpIRClimate::on_receive(remote_base::RemoteReceiveData data) {
   this->target_temperature = 17 + ((~temp_nibble) & 0x0F);
 
   uint8_t fan = frame[7] & 0xE0;
-  bool is_zmp = (this->protocol_ == PROTOCOL_MITSUBISHI_HEAVY_ZMP);
 
   if (fan == MITSUBISHI_HEAVY_ZJ_FAN_AUTO) {
     this->fan_mode = climate::CLIMATE_FAN_AUTO;
@@ -327,17 +326,19 @@ bool HeatpumpIRClimate::on_receive(remote_base::RemoteReceiveData data) {
   } else if (fan == MITSUBISHI_HEAVY_ZJ_FAN3) {
     this->fan_mode = climate::CLIMATE_FAN_HIGH;
     this->preset = climate::CLIMATE_PRESET_NONE;
-  } else if (fan == (is_zmp ? 0x20 : 0x40)) {
-    this->preset = climate::CLIMATE_PRESET_BOOST;
-  } else if (fan == MITSUBISHI_HEAVY_ZJ_ECONO) {
-    this->preset = climate::CLIMATE_PRESET_ECO;
+  } else if (this->protocol_ == PROTOCOL_MITSUBISHI_HEAVY_ZMP) {
+    if (fan == MITSUBISHI_HEAVY_ZMP_HIPOWER) {
+      this->preset = climate::CLIMATE_PRESET_BOOST;
+    } else if (fan == MITSUBISHI_HEAVY_ZMP_ECONO) {
+      this->preset = climate::CLIMATE_PRESET_ECO;
+    }
+  } else {
+    if (fan == MITSUBISHI_HEAVY_ZJ_HIPOWER) {
+      this->preset = climate::CLIMATE_PRESET_BOOST;
+    } else if (fan == MITSUBISHI_HEAVY_ZJ_ECONO) {
+      this->preset = climate::CLIMATE_PRESET_ECO;
+    }
   }
-
-  uint8_t swing_h = frame[5] & 0xDC;
-  uint8_t swing_v = (frame[5] & 0x02) | (frame[7] & 0x18);
-
-  bool h_swing = (swing_h == 0x4C) || (swing_h == 0x5C);
-  bool v_swing = (swing_v == MITSUBISHI_HEAVY_ZJ_VS_SWING);
 
   if (h_swing && v_swing) {
     this->swing_mode = climate::CLIMATE_SWING_BOTH;
