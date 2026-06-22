cf331808203b6089aabf7db4d0fcba1b87bfe063
Bonne Eggleston <bonne@exciton.com.au>
2026-06-22 01:28:41 +0000

    Add set_parent back to client device registration

--- esphome/components/modbus/__init__.py
+++ esphome/components/modbus/__init__.py
@@ -118,6 +118,7 @@ def final_validate_modbus_device(
 
 async def register_modbus_client_device(var, config):
     parent = await cg.get_variable(config[CONF_MODBUS_ID])
+    cg.add(var.set_parent(parent))
     cg.add(var.set_address(config[CONF_ADDRESS]))
 
 
