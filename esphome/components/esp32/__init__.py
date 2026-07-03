e8472012fa2bea30a9c05a6f42a4bd42f1a3bef3
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2026-07-03 10:30:08 -0400

    [esp32] Keep 6.0.1 and 6.0.0 on the pioarduino platform

--- esphome/components/esp32/__init__.py
+++ esphome/components/esp32/__init__.py
@@ -860,10 +860,10 @@ ESP_IDF_PLATFORM_VERSION_LOOKUP = {
     ): "https://github.com/swoboda1337/platform-espressif32.git#prep_IDF6",
     cv.Version(
         6, 0, 1
-    ): "https://github.com/swoboda1337/platform-espressif32.git#prep_IDF6",
+    ): "https://github.com/pioarduino/platform-espressif32.git#prep_IDF6",
     cv.Version(
         6, 0, 0
-    ): "https://github.com/swoboda1337/platform-espressif32.git#prep_IDF6",
+    ): "https://github.com/pioarduino/platform-espressif32.git#prep_IDF6",
     cv.Version(5, 5, 4): cv.Version(55, 3, 39),
     cv.Version(5, 5, 3, "1"): cv.Version(55, 3, 37),
     cv.Version(5, 5, 3): cv.Version(55, 3, 37),
