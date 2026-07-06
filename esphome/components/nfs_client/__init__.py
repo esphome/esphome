7be2199bb31c9184ab85127cfb675544dcec74d0
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2026-07-06 02:54:39 +0200

    test nfs_client with task safety enabled

--- esphome/components/nfs_client/__init__.py
+++ esphome/components/nfs_client/__init__.py
@@ -1,5 +1,5 @@
 import esphome.codegen as cg
-from esphome.components.storage import request_storage_device
+from esphome.components.storage import request_storage_device, request_storage_worker
 import esphome.config_validation as cv
 from esphome.const import (
     CONF_ID,
