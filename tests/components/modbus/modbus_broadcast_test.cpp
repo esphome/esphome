f3861c39da708134929cc243030f4d121aee7e93
Josef Zweck <josef@zweck.dev>
2026-07-09 06:40:19 +0000

    guard virtual

--- tests/components/modbus/modbus_broadcast_test.cpp
+++ tests/components/modbus/modbus_broadcast_test.cpp
@@ -54,6 +54,10 @@ class RecordingUART : public uart::UARTComponent {
   size_t available() override { return 0; }
   uart::UARTFlushResult flush() override { return uart::UARTFlushResult::UART_FLUSH_RESULT_ASSUMED_SUCCESS; }
   void check_logger_conflict() override {}
+  // USE_ESP32 only: the USE_ESP8266 token would pull this gtest file into the ESP8266 clang-tidy scan.
+#ifdef USE_ESP32
+  void load_settings(bool dump_config) override {}
+#endif
 
   std::vector<uint8_t> written;
 };
