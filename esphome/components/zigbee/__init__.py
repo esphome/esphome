d1e8c05bf6dec140e00adc06505533981a77e8f8
Pranjal Joshi <joshi.pranjal5@gmail.com>
2026-07-11 22:36:04 +0530

    [zigbee] Fix missing CLUSTER_ROLE import in zigbee_ep_esp32.py

--- esphome/components/zigbee/__init__.py
+++ esphome/components/zigbee/__init__.py
@@ -32,6 +32,7 @@ from .const import (
     ZigbeeComponent,
     zigbee_ns,
 )
+from .const_esp32 import CONF_CLUSTER
 from .const_zephyr import (
     CONF_IEEE802154_VENDOR_OUI,
     CONF_MAX_EP_NUMBER_ZEPHYR,
@@ -39,7 +40,6 @@ from .const_zephyr import (
     CONF_ZIGBEE_ID,
     KEY_EP_NUMBER,
 )
-from .const_esp32 import CONF_CLUSTER
 from .zigbee_esp32 import (
     final_validate_esp32,
     validate_binary_sensor_esp32,
@@ -91,11 +91,15 @@ BASE_SCHEMA = cv.Schema(
 )
 BINARY_SENSOR_SCHEMA = cv.Schema({}).extend(BASE_SCHEMA).extend(zephyr_binary_sensor)
 SENSOR_SCHEMA = cv.Schema({}).extend(BASE_SCHEMA).extend(zephyr_sensor)
-SWITCH_SCHEMA = cv.Schema(
-    {
-        cv.Optional(CONF_CLUSTER): cv.one_of("binary_output", "on_off"),
-    }
-).extend(BASE_SCHEMA).extend(zephyr_switch)
+SWITCH_SCHEMA = (
+    cv.Schema(
+        {
+            cv.Optional(CONF_CLUSTER): cv.one_of("binary_output", "on_off"),
+        }
+    )
+    .extend(BASE_SCHEMA)
+    .extend(zephyr_switch)
+)
 NUMBER_SCHEMA = cv.Schema({}).extend(zephyr_number)
 
 
