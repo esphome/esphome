f543be1202074cf7b400b5367926112be2617965
Pablo Ventura <pablogventura@gmail.com>
2026-07-13 11:01:04 -0300

    refactor(epaper_spi): delegate full init to EpaperWaveshare in 2.9in-v2
    Partial refresh still needs the panel-specific 0x37 sequence after LUT load.

--- esphome/components/epaper_spi/epaper_waveshare_2p9_v2.cpp
+++ esphome/components/epaper_spi/epaper_waveshare_2p9_v2.cpp
@@ -3,18 +3,16 @@
 namespace esphome::epaper_spi {
 
 bool EpaperWaveshare2P9V2::initialise(bool partial) {
-  this->send_init_sequence_(this->init_sequence_, this->init_sequence_length_);
-  if (partial) {
-    this->cmd_data(0x32, this->partial_lut_, this->partial_lut_length_);
-    this->cmd_data(0x37, {0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00});
-    this->cmd_data(0x3C, {0x80});
-    this->cmd_data(0x22, {0xC0});
-    this->command(0x20);
-    this->next_delay_ = 100;
-  } else {
-    this->cmd_data(0x32, this->lut_, this->lut_length_);
-    this->cmd_data(0x3C, {0x05});
+  if (!partial) {
+    return EpaperWaveshare::initialise(false);
   }
+  this->send_init_sequence_(this->init_sequence_, this->init_sequence_length_);
+  this->cmd_data(0x32, this->partial_lut_, this->partial_lut_length_);
+  this->cmd_data(0x37, {0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00});
+  this->cmd_data(0x3C, {0x80});
+  this->cmd_data(0x22, {0xC0});
+  this->command(0x20);
+  this->next_delay_ = 100;
   this->send_red_ = true;
   return true;
 }
