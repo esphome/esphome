38546bff95c4d829bc7da305ac468969c6e90318
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2026-07-14 00:23:29 +0200

    fix NFSClient to be final

--- esphome/components/nfs_client/nfs_client.h
+++ esphome/components/nfs_client/nfs_client.h
@@ -284,10 +284,10 @@ class RPCClient {
 // NFS Client Component
 //========================================================================
 
-class NFSClient : public storage::NetworkStorage, public storage::MountableStorage {
+class NFSClient final : public storage::NetworkStorage, public storage::MountableStorage {
  public:
   NFSClient() = default;
-  ~NFSClient();
+  ~NFSClient() override;
 
   void setup() override;
   void loop() override;
