b51d17220325b8d04ebe7b0432f02d1c866dc4e6
raul <raul11@freenet.de>
2026-07-17 19:15:09 +0200

    fix lambda call

--- esphome/components/zigbee/zigbee_attribute_esp32.h
+++ esphome/components/zigbee/zigbee_attribute_esp32.h
@@ -47,7 +47,7 @@ class ZigbeeAttribute final : public Component {
   void set_report(ZigbeeReportT report);
 #ifdef USE_SENSOR
   template<typename T> void connect(sensor::Sensor *sensor);
-  template<typename T> void connect(sensor::Sensor *sensor, std::function<T(float)> &&f);
+  template<typename T, typename F> void connect(sensor::Sensor *sensor, F &&f);
 #endif
 #ifdef USE_BINARY_SENSOR
   template<typename T> void connect(binary_sensor::BinarySensor *sensor);
@@ -89,8 +89,8 @@ template<typename T> void ZigbeeAttribute::set_attr(const T &value) {
 template<typename T> void ZigbeeAttribute::connect(sensor::Sensor *sensor) {
   sensor->add_on_state_callback([this](float value) { this->set_attr((T) (this->scale_ * value)); });
 }
-template<typename T> void ZigbeeAttribute::connect(sensor::Sensor *sensor, std::function<T(float)> &&f) {
-  sensor->add_on_state_callback([f, this](float value) { this->set_attr(f(value)); });
+template<typename T, typename F> void ZigbeeAttribute::connect(sensor::Sensor *sensor, F &&f) {
+  sensor->add_on_state_callback([f = std::forward<F>(f), this](float value) { this->set_attr((T) f(value)); });
 }
 #endif
 #ifdef USE_BINARY_SENSOR
