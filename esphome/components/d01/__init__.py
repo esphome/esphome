69cd695def8c53c0a3a35ff65d3f92d8224e1aec
Andrej Walilko <awalilko@liquidweb.com>
2026-07-22 15:51:36 -0400

    rurff

--- esphome/components/d01/__init__.py
+++ esphome/components/d01/__init__.py
@@ -4,4 +4,3 @@ CODEOWNERS = ["@ch604"]
 
 d01_ns = cg.esphome_ns.namespace("d01")
 D01Component = d01_ns.class_("D01Component", cg.PollingComponent)
-
--- esphome/components/d01/sensor.py
+++ esphome/components/d01/sensor.py
@@ -12,16 +12,20 @@ from . import D01Component
 
 DEPENDENCIES = ["uart"]
 
-CONFIG_SCHEMA = (cv.Schema({
-    cv.GenerateID():
-    cv.declare_id(D01Component),
-    cv.Required(CONF_PM_2_5):
-    sensor.sensor_schema(
-        unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
-        accuracy_decimals=0,
-        device_class=DEVICE_CLASS_PM25,
-    ),
-}).extend(cv.polling_component_schema("10s")).extend(uart.UART_DEVICE_SCHEMA))
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(D01Component),
+            cv.Required(CONF_PM_2_5): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_PM25,
+            ),
+        }
+    )
+    .extend(cv.polling_component_schema("10s"))
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
 
 
 async def to_code(config):
