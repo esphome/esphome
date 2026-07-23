b1f00d43ae8801bb355fb1cf7f15434a81386bb5
Anton Viktorov <anton.viktorov@live.com>
2026-07-23 23:12:37 +0200

    [husb238] Fix status_set_error call to use LOG_STR

--- esphome/components/husb238/husb238.cpp
+++ esphome/components/husb238/husb238.cpp
@@ -141,7 +141,7 @@ void Husb238Component::update() {
   bool is_changed{false};
   if (!this->read_all_(is_changed)) {
     is_changed = !this->status_has_error();
-    this->status_set_error("Unable to communicate with HUSB238 chip");
+    this->status_set_error(LOG_STR("Unable to communicate with HUSB238 chip"));
     std::fill(std::begin(this->registers_.raw), std::end(this->registers_.raw), 0);
   } else {
     this->status_clear_error();
