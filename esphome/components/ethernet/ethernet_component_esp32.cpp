f898541e984a988ba5174e52c056bd5c5b3e8c7f
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2026-07-27 09:40:37 -0400

    Move CS hold time setup next to the other ENC28J60 config

--- esphome/components/ethernet/ethernet_component_esp32.cpp
+++ esphome/components/ethernet/ethernet_component_esp32.cpp
@@ -214,9 +214,6 @@ void EthernetComponent::ethernet_lazy_init_() {
 #elif defined(USE_ETHERNET_DM9051)
   eth_dm9051_config_t dm9051_config = ETH_DM9051_DEFAULT_CONFIG(host, &devcfg);
 #elif defined(USE_ETHERNET_ENC28J60)
-  // CS must stay asserted for the chip's CS hold time (t10, 210 ns) after the
-  // last clock or MAC/MII register reads fail ("wrong chip ID" at driver init)
-  devcfg.cs_ena_posttrans = enc28j60_cal_spi_cs_hold_time(this->clock_speed_ / 1000000);
   eth_enc28j60_config_t enc28j60_config = ETH_ENC28J60_DEFAULT_CONFIG(host, &devcfg);
 #endif
 
@@ -236,6 +233,9 @@ void EthernetComponent::ethernet_lazy_init_() {
 #endif
 #elif defined(USE_ETHERNET_ENC28J60)
   enc28j60_config.int_gpio_num = this->interrupt_pin_;
+  // CS must stay asserted for the chip's CS hold time (t10, 210 ns) after the
+  // last clock or MAC/MII register reads fail ("wrong chip ID" at driver init)
+  devcfg.cs_ena_posttrans = enc28j60_cal_spi_cs_hold_time(this->clock_speed_ / 1000000);
   // ENC28J60 does not support poll_period_ms
 #endif
 
