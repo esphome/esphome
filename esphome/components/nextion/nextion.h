ee2ed70367af0491c55a2ddc1db0c9bfb65e94b3
Daniel Szyndzielorz <daniel@vcard24.de>
2026-07-29 19:20:23 +0200

    refactor(nextion): inline test wrapper methods in header
    Move set_numeric_return and set_string_return wrapper methods from
    cpp file to header as inline functions. This reduces code duplication
    and improves test visibility while maintaining the same functionality
    for GTEST_TESTING builds.

--- esphome/components/nextion/nextion.h
+++ esphome/components/nextion/nextion.h
@@ -1462,8 +1462,8 @@ class Nextion final : public NextionBase, public PollingComponent, public uart::
 #ifdef GTEST_TESTING
  public:
   std::list<NextionQueue *> nextion_queue_;
-  void set_numeric_return(int value);
-  void set_string_return(const std::string &value);
+  inline void set_numeric_return(int value) { this->set_numeric_return_(value); }
+  inline void set_string_return(const std::string &value) { this->set_string_return_(value); }
 
  protected:
 #else
