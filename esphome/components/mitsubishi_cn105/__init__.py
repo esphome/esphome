e4eb0b99afd1f04dcd6415c80e2d46c7678a5907
Boris Krivonog <boris.krivonog@inova.si>
2026-08-11 10:06:31 +0200

    pin namespace to simplify lambda

--- esphome/components/mitsubishi_cn105/__init__.py
+++ esphome/components/mitsubishi_cn105/__init__.py
@@ -80,6 +80,7 @@ async def register_mitsubishi_cn105_device(var: MockObj, config: ConfigType) -> 
 
 
 async def to_code(config: ConfigType) -> None:
+    cg.add_global(mitsubishi_ns.using)
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
