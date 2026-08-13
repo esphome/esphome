5da33da3a8b4c618371b541a4645618ccc55b5b5
Andrej Walilko <awalilko@liquidweb.com>
2026-08-12 22:42:22 -0400

    make d01 sensor.py closer to pm1006's, add state class and final_validate_schema

--- esphome/components/d01/sensor.py
+++ esphome/components/d01/sensor.py
@@ -5,33 +5,52 @@ from esphome.const import (
     CONF_ID,
     CONF_PM_2_5,
     DEVICE_CLASS_PM25,
+    ICON_BLUR,
+    STATE_CLASS_MEASUREMENT,
     UNIT_MICROGRAMS_PER_CUBIC_METER,
 )
 
-from . import D01Component
-
+CODEOWNERS = ["@ch604"]
 DEPENDENCIES = ["uart"]
 
-CONFIG_SCHEMA = (
+d01_ns = cg.esphome_ns.namespace("d01")
+D01Component = d01_ns.class_("D01Component", uart.UARTDevice,
+                             cg.PollingComponent)
+
+
+CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(D01Component),
             cv.Required(CONF_PM_2_5): sensor.sensor_schema(
                 unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+                icon=ICON_BLUR,
                 accuracy_decimals=0,
                 device_class=DEVICE_CLASS_PM25,
+                state_class=STATE_CLASS_MEASUREMENT,
             ),
         }
     )
-    .extend(cv.polling_component_schema("10s"))
+    .extend(cv.COMPONENT_SCHEMA)
     .extend(uart.UART_DEVICE_SCHEMA)
+    .extend(cv.polling_component_schema("10s")),
 )
 
+def validate_interval_uart(config):
+    uart.final_validate_device_schema(
+        "d01",
+        baud_rate=9600,
+        require_rx=True,
+        require_tx=False,
+    )(config)
+
+FINAL_VALIDATE_SCHEMA = validate_interval_uart
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    sens = await sensor.new_sensor(config[CONF_PM_2_5])
-    cg.add(var.set_pm25_sensor(sens))
+    if CONF_PM_2_5 in config:
+        sens = await sensor.new_sensor(config[CONF_PM_2_5])
+        cg.add(var.set_pm25_sensor(sens))
