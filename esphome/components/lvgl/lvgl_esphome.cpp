b34444f8391bbb2ac92a2a66daaf2ab20aee5fc3
clyde <2366188+clydebarrow@users.noreply.github.com>
2026-08-17 13:38:18 +1000

    Use range iteration instead of indexing

--- esphome/components/lvgl/lvgl_esphome.cpp
+++ esphome/components/lvgl/lvgl_esphome.cpp
@@ -552,9 +552,9 @@ void LvTableType::set_obj(lv_obj_t *lv_obj) {
 }
 
 void LvTableType::add_column_width_pct(uint32_t col, uint8_t pct) {
-  for (size_t i = 0; i < this->column_pct_.size(); i++) {
-    if (this->column_pct_[i].col == col) {
-      this->column_pct_[i].pct = pct;
+  for (auto &i : this->column_pct_) {
+    if (i.col == col) {
+      i.pct = pct;
       this->update_column_widths_();
       return;
     }
