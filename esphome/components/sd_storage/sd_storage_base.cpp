05dbb75d28b852f156c930f9417222ff80130dc5
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2026-08-18 19:41:38 +0200

    fix missing bracket in sd_storage_base.cpp

--- esphome/components/sd_storage/sd_storage_base.cpp
+++ esphome/components/sd_storage/sd_storage_base.cpp
@@ -509,7 +509,7 @@ void SdStorageBase::log_mount_result_(storage::StorageError err) const {
   if (err == storage::StorageError::STORAGE_ERROR_OK) {
     ESP_LOGI(TAG_BASE, "Card mounted via automation");
   } else {
-    ESP_LOGE(TAG_BASE, storage::error_to_string(err);
+    ESP_LOGE(TAG_BASE, storage::error_to_string(err));
   }
 }
 
