343e34edf741696fdef1fc2940869a473aec3545
J. Nick Koston <nick@home-assistant.io>
2026-08-19 17:17:52 -0500

    fix stdlib build

--- tests/components/runtime_image/__init__.py
+++ tests/components/runtime_image/__init__.py
@@ -1,3 +1,4 @@
+import esphome.codegen as cg
 from esphome.components.runtime_image import enable_format
 from esphome.types import ConfigType
 from tests.testing_helpers import ComponentManifestOverride
@@ -11,5 +12,9 @@ def override_manifest(manifest: ComponentManifestOverride) -> None:
         enable_format("BMP")
         enable_format("PNG")
         enable_format("JPEG")
+        # JPEGDEC's host detection checks __MACH__/__LINUX__, but gcc only
+        # predefines the lowercase __linux__, so without this it tries to
+        # include Arduino.h on the Linux CI runners.
+        cg.add_build_flag("-D__LINUX__")
 
     manifest.to_code = to_code_testing
