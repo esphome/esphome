c35cbbc92bcc27eea8eb8ceab68ec3653918ea9b
J. Nick Koston <nick@home-assistant.io>
2026-08-23 16:54:51 -0500

    [gpio] Fix linker error when binary sensor only uses expander pins

--- esphome/components/gpio/binary_sensor/gpio_binary_sensor.h
+++ esphome/components/gpio/binary_sensor/gpio_binary_sensor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/core/defines.h"
 #include "esphome/core/hal.h"
 #include "esphome/core/helpers.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
@@ -10,6 +11,7 @@ namespace esphome::gpio {
 // Store class for ISR data and configuration (no vtables, ISR-safe)
 class GPIOBinarySensorStore {
  public:
+#ifdef USE_GPIO_BINARY_SENSOR_INTERRUPT
   void setup(InternalGPIOPin *pin, Component *component);
 
   static void gpio_intr(GPIOBinarySensorStore *arg);
@@ -29,15 +31,18 @@ class GPIOBinarySensorStore {
     // Separate method to clear the flag
     this->changed_ = false;
   }
+#endif
 
  protected:
   friend class GPIOBinarySensor;
+#ifdef USE_GPIO_BINARY_SENSOR_INTERRUPT
   ISRInternalGPIOPin isr_pin_;
   Component *component_{nullptr};  // Pointer to the component for enable_loop_soon_any_context()
   volatile bool state_{false};
   volatile bool changed_{false};
-  bool use_interrupt_{true};
   gpio::InterruptType interrupt_type_{gpio::INTERRUPT_ANY_EDGE};
+#endif
+  bool use_interrupt_{true};
 };
 
 class GPIOBinarySensor final : public binary_sensor::BinarySensor, public Component {
@@ -47,7 +52,9 @@ class GPIOBinarySensor final : public binary_sensor::BinarySensor, public Compon
 
   void set_pin(GPIOPin *pin) { this->pin_ = pin; }
   void set_use_interrupt(bool use_interrupt) { this->store_.use_interrupt_ = use_interrupt; }
+#ifdef USE_GPIO_BINARY_SENSOR_INTERRUPT
   void set_interrupt_type(gpio::InterruptType type) { this->store_.interrupt_type_ = type; }
+#endif
   // ========== INTERNAL METHODS ==========
   // (In most use cases you won't need these)
   /// Setup pin
