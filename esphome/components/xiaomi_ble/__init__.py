321335ff30540e319cc476a1d82b5312db33f350
kabongsteve <kabong.steve@gmail.com>
2026-08-24 14:54:16 +1000

    Add mccgq02hl sensor

--- esphome/components/xiaomi_ble/__init__.py
+++ esphome/components/xiaomi_ble/__init__.py
@@ -1,26 +1,22 @@
 import esphome.codegen as cg
-from esphome.components import ble_device_base
+from esphome.components import esp32_ble_tracker
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome.types import ConfigType
 
-AUTO_LOAD = ["ble_device_base"]
+DEPENDENCIES = ["esp32_ble_tracker"]
 
 xiaomi_ble_ns = cg.esphome_ns.namespace("xiaomi_ble")
 XiaomiListener = xiaomi_ble_ns.class_(
-    "XiaomiListener", ble_device_base.ESPBTDeviceListener
+    "XiaomiListener", esp32_ble_tracker.ESPBTDeviceListener
 )
 
-CONFIG_SCHEMA = cv.All(
-    ble_device_base.rename_legacy_hub_id("xiaomi_ble"),
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(XiaomiListener),
-        }
-    ).extend(ble_device_base.BLE_DEVICE_SCHEMA),
-)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(XiaomiListener),
+    }
+).extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA)
 
 
-async def to_code(config: ConfigType) -> None:
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await ble_device_base.register_ble_device(var, config)
+    await esp32_ble_tracker.register_ble_device(var, config)
