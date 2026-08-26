84f60d2ba1e0918efc6ebe904bcdc0670d5c9323
Oliver Kleinecke <okleinecke@web.de>
2026-08-26 16:26:04 +0200

    Remove dual host support from USBHost class
    Removed dual host functionality and related comments from USBHost class.

--- esphome/components/usb_host/usb_host.h
+++ esphome/components/usb_host/usb_host.h
@@ -267,10 +267,6 @@ class USBHost final : public Component {
   void loop() override;
   void setup() override;
 
-  // Enable simultaneous HS + FS USB host on ESP32-P4 (requires espressif/usb >= 1.4.0).
-  // peripheral_map = BIT0 | BIT1 -> both controllers; default BIT0 = HS only.
-  void set_dual_host(bool enable) { this->dual_host_ = enable; }
-
   // -- Submission engine (called by USBClient thin forwarders) ----------------
 
   // Bulk / interrupt IN and OUT -- always compiled if any client uses them
@@ -307,7 +303,6 @@ class USBHost final : public Component {
 
  protected:
   std::vector<USBClient *> clients_{};
-  bool dual_host_{false};
 };
 
 // Returns the global USBHost singleton, set during USBHost::setup().
