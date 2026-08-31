b3b4f04ecfcdea02bf9133095ad509456f1d22a6
J. Nick Koston <nick@home-assistant.io>
2026-08-30 21:26:22 -0500

    Leave lvgl unchanged

--- esphome/components/lvgl/lvgl_esphome.cpp
+++ esphome/components/lvgl/lvgl_esphome.cpp
@@ -1059,10 +1059,8 @@ static void *lv_alloc_draw_buf(size_t size, bool internal) {
   void *buffer;
   size = LV_ROUND_UP(size, LV_DRAW_BUF_ALIGN);
   buffer = heap_caps_aligned_alloc(LV_DRAW_BUF_ALIGN, size, internal ? MALLOC_CAP_8BIT : cap_bits);  // NOLINT
-  if (buffer == nullptr) {
-    ESP_LOGW(esphome::lvgl::TAG, "Failed to allocate %zu bytes for %sdraw buffer", size,
-             internal ? LOG_STR_LITERAL("internal ") : LOG_STR_LITERAL(""));
-  }
+  if (buffer == nullptr)
+    ESP_LOGW(esphome::lvgl::TAG, "Failed to allocate %zu bytes for %sdraw buffer", size, internal ? "internal " : "");
   return buffer;
 }
 
