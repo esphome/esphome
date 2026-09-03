b0873868b153b989e24cdae5deed2b3c72048a13
KoenBreeman <121864590+KoenBreeman@users.noreply.github.com>
2026-09-03 23:12:29 +0200

    And added interlocking

--- esphome/components/seeedmultichannelrelay/switch/__init__.py
+++ esphome/components/seeedmultichannelrelay/switch/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 from esphome.components import i2c, switch
 import esphome.config_validation as cv
-from esphome.const import CONF_CHANNEL
+from esphome.const import CONF_INTERLOCK, CONF_CHANNEL
 
 from .. import (
     CONF_SEEED_MULTI_CHANNEL_RELAY_ID,
@@ -28,6 +28,10 @@ CONFIG_SCHEMA = (
                 SeeedMultiChannelRelay
             ),
             cv.Required(CONF_CHANNEL): cv.int_range(min=1, max=8),
+            cv.Optional(CONF_INTERLOCK): cv.ensure_list(cv.use_id(switch.Switch)),
+            cv.Optional(
+                CONF_INTERLOCK_WAIT_TIME, default="0ms"
+            ): cv.positive_time_period_milliseconds,
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -40,3 +44,11 @@ async def to_code(config):
     await cg.register_parented(var, config[CONF_SEEED_MULTI_CHANNEL_RELAY_ID])
 
     cg.add(var.set_channel(config[CONF_CHANNEL]))
+    if CONF_INTERLOCK in config:
+        cg.add_define("USE_SWITCH_INTERLOCK")
+        interlock = []
+        for it in config[CONF_INTERLOCK]:
+            lock = await cg.get_variable(it)
+            interlock.append(lock)
+        cg.add(var.set_interlock(interlock))
+        cg.add(var.set_interlock_wait_time(config[CONF_INTERLOCK_WAIT_TIME]))
--- esphome/components/seeedmultichannelrelay/switch/seeed_multi_channel_relay_switch.h
+++ esphome/components/seeedmultichannelrelay/switch/seeed_multi_channel_relay_switch.h
@@ -10,21 +10,21 @@ namespace seeedmultichannelrelay {
 
 class Seeed_Multi_Channel_Relay_Switch : public Component, public switch_::Switch, public Parented<seeed_multi_channel_relay> {
  public:
-  float get_setup_priority() const override;
-
   void setup() override;
   void dump_config() override;
   void write_state(bool state) override;
 
   void set_channel(uint8_t channel) { this->channel_ = channel; }
-
+#ifdef USE_SWITCH_INTERLOCK
   void set_interlock(const std::vector<Switch *> &interlock);
   void set_interlock_wait_time(uint32_t interlock_wait_time) { interlock_wait_time_ = interlock_wait_time; }
-
+#endif
  protected:
   uint8_t channel_;
+#ifdef USE_SWITCH_INTERLOCK
   std::vector<Switch *> interlock_;
   uint32_t interlock_wait_time_{0};
+#endif
 };
 
 }  // namespace seeed_multi_channel_relay
