3d36ba9aab263a97b07f1d966f367708b0901604
Petapton <petapton@gmail.com>
2026-09-04 21:44:01 +0000

    refactor(daikin_madoka): streamline and fix argument parsing in parse_cb_ function

--- esphome/components/daikin_madoka/daikin_madoka.cpp
+++ esphome/components/daikin_madoka/daikin_madoka.cpp
@@ -2,6 +2,7 @@
 
 #include "esphome/core/log.h"
 #include <utility>
+#include <algorithm>
 
 #ifdef USE_ESP32
 
@@ -346,6 +347,12 @@ void DaikinMadoka::query_(uint16_t cmd, std::vector<uint8_t> &args) {
   }
 }
 
+using ArgPair = std::pair<uint16_t, std::vector<uint8_t>>;
+
+static auto find_arg(const std::vector<ArgPair> &args, uint16_t arg_id) {
+  return std::find_if(args.begin(), args.end(), [arg_id](const ArgPair &p) { return p.first == arg_id; });
+};
+
 void DaikinMadoka::parse_cb_(std::vector<uint8_t> &msg) {
   if (msg.size() < 4) {
     ESP_LOGE(TAG, "Discarding message: invalid length.");
@@ -355,35 +362,30 @@ void DaikinMadoka::parse_cb_(std::vector<uint8_t> &msg) {
   size_t i = 4;
   const size_t message_size = msg.size();
 
-  switch (function_id) {
-    case CMD_GET_SETTING_STATUS:
-      while (i < message_size) {
-        uint8_t argument_id = msg[i++];
-        uint8_t len = msg[i++];
-        if (argument_id == 0x20) {
-          std::vector<uint8_t> val(msg.begin() + i, msg.begin() + i + len);
-          this->cur_status_.status = val[0];
-        }
-        i += len;
-      }
-      break;
-    case CMD_GET_OPERATION_MODE:
-      while (i < message_size) {
-        uint8_t argument_id = msg[i++];
-        uint8_t len = msg[i++];
-        if (argument_id == 0x20) {
-          std::vector<uint8_t> val(msg.begin() + i, msg.begin() + i + len);
-          this->cur_status_.mode = val[0];
-        }
-        i += len;
-      }
-      break;
-    default:
-      break;
+  std::vector<ArgPair> parsed_args;
+  while (i <= message_size - 2) {  // `-2` accounts for arg_id and len bytes
+    uint8_t argument_id = msg[i++];
+    uint8_t len = msg[i++];
+    if (i + len > message_size) {
+      ESP_LOGE(TAG, "Discarding message: invalid argument length.");
+      return;
+    }
+    parsed_args.emplace_back(argument_id, std::vector<uint8_t>(msg.begin() + i, msg.begin() + i + len));
+    i += len;
   }
+
   switch (function_id) {
     case CMD_GET_SETTING_STATUS:
-    case CMD_GET_OPERATION_MODE:
+    case CMD_GET_OPERATION_MODE: {
+      auto arg = find_arg(parsed_args, 0x20);
+      if (arg == parsed_args.end() || arg->second.size() != 1) {
+        break;
+      }
+      if (function_id == CMD_GET_SETTING_STATUS) {
+        this->cur_status_.status = arg->second[0] == 1;
+      } else if (function_id == CMD_GET_OPERATION_MODE) {
+        this->cur_status_.mode = arg->second[0];
+      }
       // ESP_LOGI(TAG, "status: %d, mode: %d", this->cur_status_.status, this->cur_status_.mode);
       if (this->cur_status_.status) {
         switch (this->cur_status_.mode) {
@@ -407,38 +409,37 @@ void DaikinMadoka::parse_cb_(std::vector<uint8_t> &msg) {
         this->mode = climate::CLIMATE_MODE_OFF;
       }
       break;
-    case CMD_GET_SETPOINT:
-      while (i < message_size) {
-        uint8_t argument_id = msg[i++];
-        uint8_t len = msg[i++];
-        switch (argument_id) {
-          case 0x20: {
-            std::vector<uint8_t> val(msg.begin() + i, msg.begin() + i + len);
-            this->target_temperature_high = (float) (val[0] << 8 | val[1]) / 128;
-            break;
-          }
-          case 0x21: {
-            std::vector<uint8_t> val(msg.begin() + i, msg.begin() + i + len);
-            this->target_temperature_low = (float) (val[0] << 8 | val[1]) / 128;
-            break;
-          }
-        }
-        i += len;
+    }
+    case CMD_GET_SETPOINT: {
+      auto arg_high = find_arg(parsed_args, 0x20);
+      if (arg_high != parsed_args.end() && arg_high->second.size() == 2) {
+        this->target_temperature_high = (float) (arg_high->second[0] << 8 | arg_high->second[1]) / 128;
+      }
+
+      auto arg_low = find_arg(parsed_args, 0x21);
+      if (arg_low != parsed_args.end() && arg_low->second.size() == 2) {
+        this->target_temperature_low = (float) (arg_low->second[0] << 8 | arg_low->second[1]) / 128;
       }
       break;
+    }
     case CMD_GET_FAN_SPEED: {
-      uint8_t fan_mode = 255;
-      while (i < message_size) {
-        uint8_t argument_id = msg[i++];
-        uint8_t len = msg[i++];
-        if (this->cur_status_.mode == 1) {
-        } else if ((argument_id == 0x21 && len == 1 && this->cur_status_.mode == 4) ||
-                   (argument_id == 0x20 && len == 1 && this->cur_status_.mode != 4)) {
-          fan_mode = msg[i];
-        }
-        i += len;
+      auto arg_fan = parsed_args.cend();
+      switch (this->cur_status_.mode) {
+        case 0:
+        case 2:
+        case 3:
+          arg_fan = find_arg(parsed_args, 0x20);
+          break;
+        case 4:
+          arg_fan = find_arg(parsed_args, 0x21);
+          break;
+        default:
+          break;
       }
-      switch (fan_mode) {
+      if (arg_fan == parsed_args.end() || arg_fan->second.size() != 1) {
+        break;
+      }
+      switch (arg_fan->second[0]) {
         case 0:
           this->fan_mode = climate::CLIMATE_FAN_AUTO;
           break;
@@ -458,17 +459,13 @@ void DaikinMadoka::parse_cb_(std::vector<uint8_t> &msg) {
       }
       break;
     }
-    case CMD_GET_SENSOR_INFORMATION:
-      while (i < message_size) {
-        uint8_t argument_id = msg[i++];
-        uint8_t len = msg[i++];
-        if (argument_id == 0x40) {
-          std::vector<uint8_t> val(msg.begin() + i, msg.begin() + i + len);
-          this->current_temperature = val[0];
-        }
-        i += len;
+    case CMD_GET_SENSOR_INFORMATION: {
+      auto arg_temp = find_arg(parsed_args, 0x40);
+      if (arg_temp != parsed_args.end() && arg_temp->second.size() == 1) {
+        this->current_temperature = (float) arg_temp->second[0];
       }
       break;
+    }
     default:
       break;
   }
