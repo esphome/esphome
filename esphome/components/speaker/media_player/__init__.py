85203e048fd0638a62d642dfebd37cb6e2626461
J. Nick Koston <nick@home-assistant.io>
2026-09-10 16:10:19 -0500

    [speaker] Keep codec_support_enabled as a removal error stub

--- esphome/components/speaker/media_player/__init__.py
+++ esphome/components/speaker/media_player/__init__.py
@@ -1,7 +1,5 @@
 """Speaker Media Player Setup."""
 
-import logging
-
 from esphome import automation
 import esphome.codegen as cg
 from esphome.components import (
@@ -33,9 +31,6 @@ from esphome.const import (
     CONF_TASK_STACK_IN_PSRAM,
 )
 
-_LOGGER = logging.getLogger(__name__)
-
-
 AUTO_LOAD = ["audio"]
 DEPENDENCIES = ["network"]
 
@@ -44,6 +39,7 @@ DOMAIN = "media_player"
 
 CONF_ANNOUNCEMENT = "announcement"
 CONF_ANNOUNCEMENT_PIPELINE = "announcement_pipeline"
+CONF_CODEC_SUPPORT_ENABLED = "codec_support_enabled"  # Remove before 2027.4.0
 CONF_ENQUEUE = "enqueue"
 CONF_MEDIA_FILE = "media_file"
 CONF_MEDIA_PIPELINE = "media_pipeline"
@@ -140,6 +136,12 @@ CONFIG_SCHEMA = cv.All(
             cv.Optional(CONF_MEDIA_PIPELINE): PIPELINE_SCHEMA,
             cv.Optional(CONF_BUFFER_SIZE, default=1000000): cv.int_range(
                 min=4000, max=4000000
+            ),
+            # Removed in 2026.10.0 - kept to provide helpful error message
+            cv.Optional(CONF_CODEC_SUPPORT_ENABLED): cv.invalid(
+                "The 'codec_support_enabled' option has been removed in ESPHome 2026.10.0.\n"
+                "Codec support is now determined from the pipeline 'format' setting.\n"
+                "Set 'format: NONE' on the pipeline to enable all codecs."
             ),
             cv.Optional(CONF_FILES): audio_file.audio_files_schema(),
             cv.Optional(CONF_TASK_STACK_IN_PSRAM): psram.validate_task_stack_in_psram,
