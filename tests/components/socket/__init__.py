db1cda7d414acb5fafa19afc92e31d7f255a4155
J. Nick Koston <nick@home-assistant.io>
2026-09-11 09:09:00 -0500

    [api] Round the overflow reserve once, drop the socket test override that shadowed the stdlib module

--- tests/components/socket/__init__.py
+++
@@ -1,7 +0,0 @@
-from tests.testing_helpers import ComponentManifestOverride
-
-
-def override_manifest(manifest: ComponentManifestOverride) -> None:
-    # socket must run its to_code to define USE_SOCKET_IMPL_BSD_SOCKETS for
-    # the api overflow buffer tests.
-    manifest.enable_codegen()
