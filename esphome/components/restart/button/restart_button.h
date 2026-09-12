45069c07c5ebc5a762d0f0e1fd2b105784bbbb5f
J. Nick Koston <nick@home-assistant.io>
2026-09-12 05:35:30 -0500

    [restart] Use a user provided default constructor for RestartButton

--- esphome/components/restart/button/restart_button.h
+++ esphome/components/restart/button/restart_button.h
@@ -7,6 +7,9 @@ namespace esphome::restart {
 
 class RestartButton final : public button::Button, public Component {
  public:
+  // User provided, not "= default": `new(p) RestartButton()` would zero-fill .bss that is already zero.
+  RestartButton() {}
+
   void dump_config() override;
 
  protected:
