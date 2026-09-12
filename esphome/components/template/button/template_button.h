8df5a627ef667edb98cc35b449f41f33bc202797
J. Nick Koston <nick@home-assistant.io>
2026-09-12 05:09:43 -0500

    [template] Use a user provided default constructor for TemplateButton

--- esphome/components/template/button/template_button.h
+++ esphome/components/template/button/template_button.h
@@ -6,7 +6,8 @@ namespace esphome::template_ {
 
 class TemplateButton final : public button::Button {
  public:
-  // Implements the abstract `press_action` but the `on_press` trigger already handles the press.
+  // User provided, not "= default": `new(p) TemplateButton()` would zero-fill .bss that is already zero.
+  TemplateButton() {}  // Implements the abstract `press_action` but the `on_press` trigger already handles the press.
   void press_action() override{};
 };
 
