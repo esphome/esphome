e62b4e83c2ca1d96c25f2dfd31e7326ea23c757a
J. Nick Koston <nick@home-assistant.io>
2026-09-12 05:25:27 -0500

    [internal_temperature] Use a user provided default constructor for InternalTemperatureSensor

--- esphome/components/internal_temperature/internal_temperature.h
+++ esphome/components/internal_temperature/internal_temperature.h
@@ -13,8 +13,9 @@ namespace esphome::internal_temperature {
 
 class InternalTemperatureSensor final : public sensor::Sensor, public PollingComponent {
  public:
-#if defined(USE_ESP32) || (defined(USE_ZEPHYR) && defined(USE_NRF52))
-  void setup() override;
+  // User provided, not "= default": `new(p) InternalTemperatureSensor()` would zero-fill .bss that is already zero.
+  InternalTemperatureSensor() {}
+#if defined(USE_ESP32) || (defined(USE_ZEPHYR) && defined(USE_NRF52)) void setup() override;
 #endif  // USE_ESP32 || (USE_ZEPHYR && USE_NRF52)
   void dump_config() override;
 
