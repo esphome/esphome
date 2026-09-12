a2a94a5c06181370ca649ba6757e04d73de42baf
J. Nick Koston <nick@home-assistant.io>
2026-09-12 05:30:09 -0500

    [ld2450] Use a user provided default constructor for PresenceTimeoutNumber

--- esphome/components/ld2450/number/presence_timeout_number.h
+++ esphome/components/ld2450/number/presence_timeout_number.h
@@ -7,7 +7,8 @@ namespace esphome::ld2450 {
 
 class PresenceTimeoutNumber : public number::Number, public Parented<LD2450Component> {
  public:
-  PresenceTimeoutNumber() = default;
+  // User provided, not "= default": `new(p) PresenceTimeoutNumber()` would zero-fill .bss that is already zero.
+  PresenceTimeoutNumber() {}
 
  protected:
   void control(float value) override;
