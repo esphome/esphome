f5bd1b7ce246befd26ee48aab2395899511c6359
J. Nick Koston <nick@home-assistant.io>
2026-09-12 05:29:20 -0500

    [ld2412] Use a user provided default constructor for BluetoothSwitch

--- esphome/components/ld2412/switch/bluetooth_switch.h
+++ esphome/components/ld2412/switch/bluetooth_switch.h
@@ -7,7 +7,8 @@ namespace esphome::ld2412 {
 
 class BluetoothSwitch final : public switch_::Switch, public Parented<LD2412Component> {
  public:
-  BluetoothSwitch() = default;
+  // User provided, not "= default": `new(p) BluetoothSwitch()` would zero-fill .bss that is already zero.
+  BluetoothSwitch() {}
 
  protected:
   void write_state(bool state) override;
