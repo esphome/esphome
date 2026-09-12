7d2a2608f0726778798d97a9f2e9f28c81f8f0cd
J. Nick Koston <nick@home-assistant.io>
2026-09-12 05:28:08 -0500

    [ld2412] Use a user provided default constructor for RestartButton

--- esphome/components/ld2412/button/restart_button.h
+++ esphome/components/ld2412/button/restart_button.h
@@ -7,7 +7,8 @@ namespace esphome::ld2412 {
 
 class RestartButton final : public button::Button, public Parented<LD2412Component> {
  public:
-  RestartButton() = default;
+  // User provided, not "= default": `new(p) RestartButton()` would zero-fill .bss that is already zero.
+  RestartButton() {}
 
  protected:
   void press_action() override;
