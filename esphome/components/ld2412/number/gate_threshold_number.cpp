025b77755e179bb65a768ae6114b4520ae2fae07
J. Nick Koston <nick@home-assistant.io>
2026-09-11 20:34:03 -0500

    [ld2412] Drop the unused gate index from GateThresholdNumber

--- esphome/components/ld2412/number/gate_threshold_number.cpp
+++ esphome/components/ld2412/number/gate_threshold_number.cpp
@@ -2,8 +2,6 @@
 
 namespace esphome::ld2412 {
 
-GateThresholdNumber::GateThresholdNumber(uint8_t gate) : gate_(gate) {}
-
 void GateThresholdNumber::control(float value) {
   this->publish_state(value);
   this->parent_->set_gate_threshold();
--- esphome/components/ld2412/number/gate_threshold_number.h
+++ esphome/components/ld2412/number/gate_threshold_number.h
@@ -7,10 +7,9 @@ namespace esphome::ld2412 {
 
 class GateThresholdNumber final : public number::Number, public Parented<LD2412Component> {
  public:
-  GateThresholdNumber(uint8_t gate);
+  GateThresholdNumber() = default;
 
  protected:
-  uint8_t gate_;
   void control(float value) override;
 };
 
