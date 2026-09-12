6ba4f1220d0f6c5752fe93e91639b808ad6d3163
J. Nick Koston <nick@home-assistant.io>
2026-09-12 05:32:36 -0500

    [internal_temperature] Restore the setup declaration on its own line

--- esphome/components/internal_temperature/internal_temperature.h
+++ esphome/components/internal_temperature/internal_temperature.h
@@ -15,7 +15,9 @@ class InternalTemperatureSensor final : public sensor::Sensor, public PollingCom
  public:
   // User provided, not "= default": `new(p) InternalTemperatureSensor()` would zero-fill .bss that is already zero.
   InternalTemperatureSensor() {}
-#if defined(USE_ESP32) || (defined(USE_ZEPHYR) && defined(USE_NRF52)) void setup() override;
+
+#if defined(USE_ESP32) || (defined(USE_ZEPHYR) && defined(USE_NRF52))
+  void setup() override;
 #endif  // USE_ESP32 || (USE_ZEPHYR && USE_NRF52)
   void dump_config() override;
 
