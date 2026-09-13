c4582b0b66cfd3e491b7f9695b5b5455feb9d616
J. Nick Koston <nick@home-assistant.io>
2026-09-13 08:53:11 -0500

    Allocate the OTA auth buffer the same way, std::make_unique aborts on OOM too

--- esphome/components/esphome/ota/ota_esphome.h
+++ esphome/components/esphome/ota/ota_esphome.h
@@ -145,7 +145,7 @@ class ESPHomeOTAComponent final : public ota::OTAComponent {
 
 #ifdef USE_OTA_PASSWORD
   std::string password_;
-  std::unique_ptr<uint8_t[]> auth_buf_;
+  RAMUniquePtr<uint8_t[]> auth_buf_;
 #endif  // USE_OTA_PASSWORD
 #ifdef USE_OTA_ENCRYPTION
 #ifndef USE_OTA_ENCRYPTION_FROM_API
