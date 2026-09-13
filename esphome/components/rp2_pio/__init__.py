cc237fe55ef22fe41aadd091ba49bd786e95e6aa
emes30 <35312919+emes30@users.noreply.github.com>
2026-09-13 21:28:01 +0200

    Update PIOASM repository to version 4.1.0
    Update pioasm toolchain version to 4.1.0, solves problem with W5500 component on RP2350.

--- esphome/components/rp2_pio/__init__.py
+++ esphome/components/rp2_pio/__init__.py
@@ -7,9 +7,9 @@ from esphome.types import ConfigType
 DEPENDENCIES = ["rp2"]
 
 
-PIOASM_REPO_VERSION = "1.5.0-b"
+PIOASM_REPO_VERSION = "4.1.0"
 PIOASM_REPO_BASE = f"https://github.com/earlephilhower/pico-quick-toolchain/releases/download/{PIOASM_REPO_VERSION}"
-PIOASM_VERSION = "pioasm-2e6142b.230216"
+PIOASM_VERSION = "pioasm-9fdfe11.250530"
 PIOASM_DOWNLOADS = {
     "linux": {
         "aarch64": f"aarch64-linux-gnu.{PIOASM_VERSION}.tar.gz",
