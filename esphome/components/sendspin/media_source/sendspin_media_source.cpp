3115f2f9bcf642eb28b5aa425a5205518ee09fdf
Kevin Ahrendt <kahrendt@gmail.com>
2026-09-16 16:50:35 -0400

    Log error on media_source commands when disabled

--- esphome/components/sendspin/media_source/sendspin_media_source.cpp
+++ esphome/components/sendspin/media_source/sendspin_media_source.cpp
@@ -87,6 +87,10 @@ bool SendspinMediaSource::play_uri(const std::string &uri) {
 
 // THREAD CONTEXT: Main loop (media_source.h documents handle_command as main-loop only)
 void SendspinMediaSource::handle_command(media_source::MediaSourceCommand command) {
+  if (!this->parent_->is_enabled()) {
+    ESP_LOGE(TAG, "Cannot handle command: Sendspin is disabled");
+    return;
+  }
   switch (command) {
     case media_source::MediaSourceCommand::STOP: {
       if (!this->pending_start_) {
