5843a6133eb5f1826df92aca58fc01506a610c35
luar123 <49960470+luar123@users.noreply.github.com>
2026-09-21 07:43:15 +0200

    Allow building for single core targets.

--- esphome/components/snapclient/media_player/__init__.py
+++ esphome/components/snapclient/media_player/__init__.py
@@ -22,7 +22,7 @@ AUTO_LOAD = ["mdns", "socket"]
 CONF_HOSTNAME = "hostname"
 CONF_MUTE_PIN = "mute_pin"
 
-SNAPCLIENT_GIT_VERSION = "b57403522820067818fa69989e512b96b58f31fd"
+SNAPCLIENT_GIT_VERSION = "774268009d1a6c3d1664fad533409a22e5a41e83"
 SNAPCLIENT_GIT_REPO = "https://github.com/luar123/snapclient.git"
 
 snapclient_ns = cg.esphome_ns.namespace("snapclient")
