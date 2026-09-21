4972a35d56368014eafdea4ec0cfa6fb97d529da
Tomasz Duda <tomaszduda23@gmail.com>
2026-09-21 17:16:12 +0200

    [nrf52] disable USB if not configured by esphome to save power

--- esphome/components/zephyr/__init__.py
+++ esphome/components/zephyr/__init__.py
@@ -176,7 +176,8 @@ def zephyr_to_code(config: ConfigType) -> None:
 
 @coroutine_with_priority(CoroPriority.FINAL)
 async def _cdc_acm_to_code(config: ConfigType) -> None:
-    if "CONFIG_CDC_ACM_DTE_RATE_CALLBACK_SUPPORT" in zephyr_data()[KEY_PRJ_CONF][""]:
+    need_cdc_cb = zephyr_data()[KEY_PRJ_CONF][""].get("CONFIG_CDC_ACM_DTE_RATE_CALLBACK_SUPPORT", (False,))[0]
+    if need_cdc_cb:
         var = cg.new_Pvariable(config[CONF_CDC_ACM])
         await cg.register_component(var, {})
 
