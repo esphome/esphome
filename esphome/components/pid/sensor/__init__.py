89787773824d3df32bef93f26514e8bbacb4af17
Noe Gros <i@noegros.com>
2026-09-22 16:18:22 +0000

    Fix deadband parameters units, and being multiplied by 100

--- esphome/components/pid/sensor/__init__.py
+++ esphome/components/pid/sensor/__init__.py
@@ -2,7 +2,15 @@ import esphome.codegen as cg
 from esphome.components import sensor
 from esphome.components.const import CONF_CLIMATE_ID
 import esphome.config_validation as cv
-from esphome.const import CONF_TYPE, ICON_GAUGE, STATE_CLASS_MEASUREMENT, UNIT_PERCENT
+from esphome.const import (
+    CONF_TYPE,
+    CONF_UNIT_OF_MEASUREMENT,
+    ICON_GAUGE,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_CELSIUS,
+    UNIT_EMPTY,
+    UNIT_PERCENT,
+)
 from esphome.types import ConfigType
 
 from ..climate import PIDClimate, pid_ns
@@ -28,10 +36,32 @@ PID_CLIMATE_SENSOR_TYPES = {
     "KD_DEADBAND_MULTIPLIER": PIDClimateSensorType.PID_SENSOR_TYPE_KD_DEADBAND_MULTIPLIER,
 }
 
-CONFIG_SCHEMA = (
+DEADBAND_THRESHOLD_SENSOR_TYPES = (
+    PIDClimateSensorType.PID_SENSOR_TYPE_DEADBAND_THRESHOLD_HIGH,
+    PIDClimateSensorType.PID_SENSOR_TYPE_DEADBAND_THRESHOLD_LOW,
+)
+
+DEADBAND_MULTIPLIER_SENSOR_TYPES = (
+    PIDClimateSensorType.PID_SENSOR_TYPE_KP_DEADBAND_MULTIPLIER,
+    PIDClimateSensorType.PID_SENSOR_TYPE_KI_DEADBAND_MULTIPLIER,
+    PIDClimateSensorType.PID_SENSOR_TYPE_KD_DEADBAND_MULTIPLIER,
+)
+
+
+def set_default_unit_of_measurement(config: ConfigType) -> ConfigType:
+    sensor_type = config[CONF_TYPE]
+    if sensor_type in DEADBAND_THRESHOLD_SENSOR_TYPES:
+        config.setdefault(CONF_UNIT_OF_MEASUREMENT, UNIT_CELSIUS)
+    elif sensor_type in DEADBAND_MULTIPLIER_SENSOR_TYPES:
+        config.setdefault(CONF_UNIT_OF_MEASUREMENT, UNIT_EMPTY)
+    else:
+        config.setdefault(CONF_UNIT_OF_MEASUREMENT, UNIT_PERCENT)
+    return config
+
+
+CONFIG_SCHEMA = cv.All(
     sensor.sensor_schema(
         PIDClimateSensor,
-        unit_of_measurement=UNIT_PERCENT,
         icon=ICON_GAUGE,
         accuracy_decimals=1,
         state_class=STATE_CLASS_MEASUREMENT,
@@ -42,7 +72,8 @@ CONFIG_SCHEMA = (
             cv.Required(CONF_TYPE): cv.enum(PID_CLIMATE_SENSOR_TYPES, upper=True),
         }
     )
-    .extend(cv.COMPONENT_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA),
+    set_default_unit_of_measurement,
 )
 
 
