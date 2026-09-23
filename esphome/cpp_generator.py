e8d3c06b60f5a44d6ae31a902cc2e4f5b055a98f
J. Nick Koston <nick@koston.org>
2026-09-23 10:36:42 +0100

    [core][cover] Add register_apply_action for actions that only forward values (#19469)

--- esphome/cpp_generator.py
+++ esphome/cpp_generator.py
@@ -1211,8 +1211,8 @@ def call_lambda(lamb: LambdaExpression) -> Expression:
     # Developer error if this is called with a lambda that doesn't have a return type
     assert lamb.return_type is not None, "Lambda must have a return type to be called"
     expr = lamb.content.strip()
-    if re.match(r"^return\b", expr) and expr.endswith(";"):
-        # Convert a lambda returning a simple expression to just that expression
+    # A lone `return <expr>;` reduces to the expression; anything longer is called as is.
+    if re.match(r"^return\b", expr) and expr.endswith(";") and expr.count(";") == 1:
         expr = RawExpression(expr[6:-1].strip())
         # Don't cast if the return type is a class
         if isinstance(lamb.return_type, MockObjClass):
