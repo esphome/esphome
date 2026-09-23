2028a175e835aedd7e391af0abee0f1fcc5152af
J. Nick Koston <nick@home-assistant.io>
2026-09-23 18:18:22 +0100

    [text_sensor] Use register_apply_condition for text_sensor.state

--- esphome/components/text_sensor/automation.h
+++ esphome/components/text_sensor/automation.h
@@ -22,16 +22,4 @@ class TextSensorStateRawTrigger final : public Trigger<std::string> {
   }
 };
 
-template<typename... Ts> class TextSensorStateCondition final : public Condition<Ts...> {
- public:
-  explicit TextSensorStateCondition(TextSensor *parent) : parent_(parent) {}
-
-  TEMPLATABLE_VALUE(std::string, state)
-
-  bool check(const Ts &...x) override { return this->parent_->state == this->state_.value(x...); }
-
- protected:
-  TextSensor *parent_;
-};
-
 }  // namespace esphome::text_sensor
