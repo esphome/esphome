33d18a89ba7d6d9ca2a6d25b3dabb6efba201d82
J. Nick Koston <nick@home-assistant.io>
2026-09-23 11:47:08 +0100

    [hoermann_hcp] Use register_apply_action for the announce_pause action

--- esphome/components/hoermann_hcp/__init__.py
+++ esphome/components/hoermann_hcp/__init__.py
@@ -3,8 +3,6 @@ import esphome.codegen as cg
 from esphome.components import modbus
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome.core import ID
-from esphome.cpp_generator import MockObj, TemplateArgsType
 from esphome.types import ConfigType
 
 CODEOWNERS = ["@zweckj"]
@@ -16,9 +14,6 @@ CONF_HOERMANN_HCP_ID = "hoermann_hcp_id"
 hoermann_hcp_ns = cg.esphome_ns.namespace("hoermann_hcp")
 HoermannHcp = hoermann_hcp_ns.class_(
     "HoermannHcp", cg.PollingComponent, modbus.ModbusServerDevice
-)
-AnnouncePauseAction = hoermann_hcp_ns.class_(
-    "AnnouncePauseAction", automation.Action, cg.Parented.template(HoermannHcp)
 )
 
 # The Hoermann UAP module answers on Modbus server address 2.
@@ -33,21 +28,11 @@ FINAL_VALIDATE_SCHEMA = modbus.final_validate_modbus_device(
 )
 
 
-@automation.register_action(
+automation.register_apply_action(
     "hoermann_hcp.announce_pause",
-    AnnouncePauseAction,
     automation.maybe_simple_id({cv.GenerateID(): cv.use_id(HoermannHcp)}),
-    synchronous=True,
+    automation.ApplyCall("announce_pause()"),
 )
-async def announce_pause_action_to_code(
-    config: ConfigType,
-    action_id: ID,
-    template_arg: cg.TemplateArguments,
-    args: TemplateArgsType,
-) -> MockObj:
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
 
 
 async def to_code(config: ConfigType) -> None:
