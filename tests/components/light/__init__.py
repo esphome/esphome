7e308bb83df7f726f6f14f1fae712cbc1bb32c6b
J. Nick Koston <nick@koston.org>
2026-09-24 14:58:54 +0100

    [tests] Define USE_WEBSERVER_PORT in the light unit test override (#19584)

--- tests/components/light/__init__.py
+++ tests/components/light/__init__.py
@@ -6,6 +6,8 @@ def override_manifest(manifest: ComponentManifestOverride) -> None:
     # Enables light_json_schema.cpp without USE_MQTT, which pulls mqtt code into core/util.cpp
     async def to_code_testing(config):
         cg.add_define("USE_WEBSERVER")
+        # api_connection.cpp reports the port whenever USE_WEBSERVER is set
+        cg.add_define("USE_WEBSERVER_PORT", 80)
 
     manifest.to_code = to_code_testing
     manifest.dependencies = manifest.dependencies + ["json"]
