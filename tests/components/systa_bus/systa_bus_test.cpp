dc221674836c013972a4f9b5553a9f233e608dd4
J. Nick Koston <nick@home-assistant.io>
2026-09-26 02:10:30 +0100

    [systa_bus] Move the test UART mock into common.h
    The ESP8266 clang-tidy job selects .cpp files that mention USE_ESP8266 and then fails compiling googletest against the xtensa toolchain. The mock's platform guard kept pulling the test into that job; like the other component tests, the mock now lives in a header the job does not scan.

--- tests/components/systa_bus/systa_bus_test.cpp
+++ tests/components/systa_bus/systa_bus_test.cpp
@@ -1,41 +1,13 @@
-#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include <algorithm>
 #include <cstdint>
-#include <initializer_list>
 #include <vector>
 
-#include "esphome/components/systa_bus/systa_bus.h"
-#include "esphome/components/uart/uart_component.h"
+#include "common.h"
 
 namespace esphome::systa_bus::testing {
 
 namespace {
-
-class MockUARTComponent : public uart::UARTComponent {
- public:
-  std::vector<uint8_t> rx;
-
-  void push_rx(const std::vector<uint8_t> &data) { this->rx.insert(this->rx.end(), data.begin(), data.end()); }
-
-  void write_array(const uint8_t *data, size_t len) override {}
-  bool read_array(uint8_t *data, size_t len) override {
-    if (this->rx.size() < len)
-      return false;
-    std::copy(this->rx.begin(), this->rx.begin() + len, data);
-    this->rx.erase(this->rx.begin(), this->rx.begin() + len);
-    return true;
-  }
-  size_t available() override { return this->rx.size(); }
-
-  MOCK_METHOD(bool, peek_byte, (uint8_t * data), (override));
-  MOCK_METHOD(uart::UARTFlushResult, flush, (), (override));
-  MOCK_METHOD(void, check_logger_conflict, (), (override));
-#if defined(USE_ESP8266) || defined(USE_ESP32)
-  void load_settings(bool dump_config) override {}
-#endif
-};
 
 class TestListener final : public SystaBusListener {
  public:
